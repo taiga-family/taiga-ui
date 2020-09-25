08eb99994bcf0d8cffcbb036da9651057c87d1a4
MarsiBarsi <darragon-nn@yandex.ru>
2020-09-25 21:51:21 +0300

    chore(ci): test alternative flow

--- projects/cdk/tokens/is-ioss.ts
+++ projects/cdk/tokens/is-ioss.ts
@@ -2,7 +2,7 @@ import {inject, InjectionToken} from '@angular/core';
 import {USER_AGENT} from '@ng-web-apis/common';
 import {TUI_IS_MOBILE} from './is-mobile';
 
-const IOS_REG_EXP = /iphone|ipad|ipod/;
+const IOS_REG_EXP = /ipad|iphone|ipod/;
 
 export const TUI_IS_IOS = new InjectionToken<boolean>('iOS browser detection', {
     providedIn: 'root',
