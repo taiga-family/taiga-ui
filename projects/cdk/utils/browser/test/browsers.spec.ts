85580c35b43068337b4fe47aa4d427aaa9046c17
Roman Sedov <darragon-nn@yandex.ru>
2020-09-25 23:32:33 +0300

    chore(tests): add some new tests (#3)

---
+++ projects/cdk/utils/browser/test/browsers.spec.ts
@@ -0,0 +1,23 @@
+import {isEdge} from '../is-edge';
+import {isEdgeOlderThan} from '../is-edge-older-than';
+import {isFirefox} from '../is-firefox';
+import {isIE} from '../is-iE';
+
+describe('Browsers', () => {
+    it('isIE', () => {
+        expect(isIE('trident')).toBe(true);
+    });
+
+    it('isEdge', () => {
+        expect(isEdge('edge')).toBe(true);
+    });
+
+    it('isFirefox', () => {
+        expect(isFirefox('firefox')).toBe(true);
+    });
+
+    it('isEdgeOlderThan', () => {
+        expect(isEdgeOlderThan(17, 'edge/16')).toBe(true);
+        expect(isEdgeOlderThan(17, 'edge/18')).toBe(false);
+    });
+});
