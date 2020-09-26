808514ddef8a0df20d87f1d847368ec2d3efa186
Roman Sedov <darragon-nn@yandex.ru>
2020-09-26 09:35:28 +0300

    chore(cdk): remove rus texts (#7)

--- projects/cdk/interfaces/index.ts
+++ projects/cdk/interfaces/index.ts
@@ -5,7 +5,6 @@ export * from './context-with-implicit';
 export * from './day-like';
 export * from './emerging-content-context';
 export * from './focusable-element-accessor';
-export * from './items-service';
 export * from './month-like';
 export * from './time-like';
 export * from './year-like';
