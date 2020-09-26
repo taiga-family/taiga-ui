ad4a52f340c5572df39afa3528e2bb1b3b69d3e9
Vladimir <v.potekhin@tinkoff.ru>
2020-09-26 20:54:29 +0300

    chore(all): add some tests

---
+++ projects/cdk/utils/focus/tests/is-native-focused-in.spec.ts
@@ -0,0 +1,15 @@
+import {isNativeFocusedIn} from '../is-native-focused-in';
+
+describe('isNativeFocusedIn', () => {
+    it('element is not focused in', () => {
+        const element = document.createElement('buton');
+
+        expect(isNativeFocusedIn(element)).toEqual(false);
+    });
+
+    it('should return false if ownerDocument is null', () => {
+        const element = {ownerDocument: null} as Node;
+
+        expect(isNativeFocusedIn(element)).toEqual(false);
+    });
+});
