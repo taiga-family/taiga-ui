18d4ccae68f3527c13676df535fc6ec13206401f
waterplea <alexander@inkin.ru>
2020-09-26 11:52:08 +0300

    fix build

--- projects/cdk/components/dialog-host/dialog-host.component.ts
+++ projects/cdk/components/dialog-host/dialog-host.component.ts
@@ -4,6 +4,7 @@ import {TuiAriaDialogContext} from '@taiga-ui/cdk/interfaces';
 import {TUI_DIALOGS} from '@taiga-ui/cdk/tokens';
 import {merge, Observable} from 'rxjs';
 
+// @dynamic
 @Component({
     selector: 'tui-dialog-host',
     templateUrl: './dialog-host.template.html',
