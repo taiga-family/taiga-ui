ec3003c698599f57e146125cd265f7d456936d05
MarsiBarsi <darragon-nn@yandex.ru>
2020-10-05 09:28:47 +0300

    chore(tests): add

---
+++ projects/cdk/utils/dom/test/getClosestElement.spec.ts
@@ -0,0 +1,43 @@
+import {getClosestElement} from '../get-closest-element';
+
+describe('getClosestElement', () => {
+    it('works with closest', () => {
+        const element = document.createElement('div');
+        const div = document.createElement('div');
+
+        document.body.appendChild(element);
+        document.body.appendChild(div);
+
+        expect(getClosestElement(element, 'div')).toEqual(div);
+    });
+
+    it('fallback works without closest and matches element', () => {
+        const savedCLosest = Element.prototype.closest;
+
+        Element.prototype.closest = undefined as any;
+
+        const element = document.createElement('div');
+        const div = document.createElement('div');
+
+        document.body.appendChild(element);
+        document.body.appendChild(div);
+
+        expect(getClosestElement(element, 'div')).toEqual(div);
+
+        Element.prototype.closest = savedCLosest;
+    });
+
+    it('fallback works without closest and with nested element', () => {
+        const savedCLosest = Element.prototype.closest;
+
+        Element.prototype.closest = undefined as any;
+
+        const div = document.createElement('div');
+
+        document.body.appendChild(div);
+
+        expect(getClosestElement(div, 'img')).toEqual(null);
+
+        Element.prototype.closest = savedCLosest;
+    });
+});
