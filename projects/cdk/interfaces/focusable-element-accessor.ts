395593861d8c1151bb450fd1e868a3c9b293018c
MarsiBarsi <darragon-nn@yandex.ru>
2020-11-13 18:47:48 +0300

    chore(cdk): add tmp TuiNativeFocusableElement interface

--- projects/cdk/interfaces/focusable-element-accessor.ts
+++ projects/cdk/interfaces/focusable-element-accessor.ts
@@ -1,7 +1,10 @@
+// TODO: remove after TS fix (https://github.com/microsoft/TypeScript/issues/41529)
+export interface TuiNativeFocusableElement extends Element, HTMLOrSVGElement {}
+
 /**
  * Public interface for any focusable component or directive
  */
 export interface TuiFocusableElementAccessor {
-    nativeFocusableElement: HTMLOrSVGElement | null;
+    nativeFocusableElement: TuiNativeFocusableElement | null;
     focused: boolean;
 }
