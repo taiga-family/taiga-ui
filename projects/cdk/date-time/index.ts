658e382fda9b5ad95a5d8109c1b835c000586e91
MarsiBarsi <darragon-nn@yandex.ru>
2020-11-25 11:56:47 +0300

    chore(cdk): export fillers file

--- projects/cdk/date-time/index.ts
+++ projects/cdk/date-time/index.ts
@@ -1,6 +1,7 @@
 export * from './day';
 export * from './days.const';
 export * from './day-range';
+export * from './date-fillers';
 export * from './date-time';
 export * from './month';
 export * from './month-range';
