e6f0c63d05bfde890e7f5b65cdf35dde352fcfca
MarsiBarsi <darragon-nn@yandex.ru>
2020-12-04 18:27:23 +0300

    chore(demo): start i18n

--- projects/demo/src/main.server.ts
+++ projects/demo/src/main.server.ts
@@ -1,3 +1,7 @@
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 export {AppServerModule} from './modules/app/app.server.module';
 
 export {renderModule, renderModuleFactory} from '@angular/platform-server';
