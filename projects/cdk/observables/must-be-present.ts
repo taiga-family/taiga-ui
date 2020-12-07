2aef2f2dc42b9d3f77c3246eceef16fc279fd98e
waterplea <alexander@inkin.ru>
2020-12-07 23:56:32 +0300

    chore(cdk): fix comments

--- projects/cdk/observables/must-be-present.ts
+++ projects/cdk/observables/must-be-present.ts
@@ -2,7 +2,7 @@ import {isPresent} from '@taiga-ui/cdk/utils/miscellaneous';
 import {OperatorFunction} from 'rxjs';
 import {map} from 'rxjs/operators';
 
-export function mustPresent<T>(): OperatorFunction<T | undefined | null, T> {
+export function mustBePresent<T>(): OperatorFunction<T | undefined | null, T> {
     return map(value => {
         if (!isPresent(value)) {
             throw new Error('Value must present');
