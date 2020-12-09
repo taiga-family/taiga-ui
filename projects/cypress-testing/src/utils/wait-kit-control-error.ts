a978c49770846ad8fff0252e520929a99c98470b
Roman Sedov <darragon-nn@yandex.ru>
2020-12-09 12:51:04 +0300

    ci(release): add script and workflow + remove unused package (#56)

--- projects/cypress-testing/src/utils/wait-kit-control-error.ts
+++
@@ -1,5 +0,0 @@
-import {waitAnimation} from './wait-animation';
-
-export function waitKitControlError(containerSelector: string) {
-    waitAnimation(cy.get(containerSelector).find('[automation-id="tui-error__text"]'));
-}
--- projects/cypress-testing/src/utils/wait-kit-preview.ts
+++
@@ -1,6 +0,0 @@
-export function waitKitPreview() {
-    cy.get('tui-preview')
-        .should('be.visible')
-        .should('not.have.class', 'ng-animating')
-        .wait(500);
-}
