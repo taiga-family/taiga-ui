d9af38d3e366035fec8a3afb36a075653f6847fd
MarsiBarsi <darragon-nn@yandex.ru>
2020-12-21 10:38:00 +0300

    chore(demo): i18n editor

--- projects/demo/src/modules/components/editor/examples/1/index.ts
+++ projects/demo/src/modules/components/editor/examples/1/index.ts
@@ -11,13 +11,14 @@ import {encapsulation} from '../../../../../view-encapsulation';
 })
 export class TuiEditorExample1 {
     model =
-        'WYSIWYG (What you see is what you get) — Rich Text Editor для использования с формами Angular.' +
-        '<p><font size="6">Заголовок</font></p><font size="4">Отделом УФМС России по <font color="#ff78a7">' +
-        'Камчатскому краю</font> в центральном районе города Петропаловск-Камчатский Отделом УФМС ' +
-        '<span style="background-color: rgb(163, 129, 255);">России</span></font><blockquote>Расплачивайтесь ' +
-        '<u>дебетовой картой</u> <b>везде</b>, где возможно, и пользуйтесь предложениями от банка и партнеров, ' +
-        'чтобы получать максимум кэшбэка</blockquote><p style="text-align: right;">Полная статистика по счетам и ' +
-        'картам, любые платежи и переводы, круглосуточная помощь в чате — в удобном мобильном и интернет-банке</p>';
+        'WYSIWYG (What you see is what you get) — Rich Text Editor for using with Angular forms.' +
+        '<p><font size="6">Heading</font></p><font size="4">Lorem ipsum dolor sit amet <font color="#ff78a7">' +
+        'consectetur adipiscing elit</font>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' +
+        '<span style="background-color: rgb(163, 129, 255);">Ut enim </span></font><blockquote>ad minim veniam, ' +
+        '<u>quis nostrud exercitation</u> <b>ullamco</b>, laboris nisi ut aliquip ex ea commodo consequat. ' +
+        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</blockquote>' +
+        '<p style="text-align: right;">Excepteur sint occaecat ' +
+        'cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
 
     onClick() {
         this.model = '<b>HTML FTW!</b>';
