25af9cfbea43272ebbb3abf6fc133b837c1b1baf
WynieCronje <wyniecronje@gmail.com>
2021-01-27 16:13:18 +0200

    fix(browser-detection): fix isFirefox check

--- projects/cdk/utils/browser/test/browsers.spec.ts
+++ projects/cdk/utils/browser/test/browsers.spec.ts
@@ -14,6 +14,7 @@ describe('Browsers', () => {
 
     it('isFirefox', () => {
         expect(isFirefox('firefox')).toBe(true);
+        expect(isFirefox('Firefox')).toBe(true);
     });
 
     it('isEdgeOlderThan', () => {
