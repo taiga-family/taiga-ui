eef9b4fc125b5c58d0b2bfe3a4f8435f589dab6e
Roman Sedov <darragon-nn@yandex.ru>
2021-01-30 22:23:11 +0300

    chore(kit): fix directives import

--- projects/kit/components/avatar/avatar.module.ts
+++ projects/kit/components/avatar/avatar.module.ts
@@ -1,6 +1,6 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {TuiLazyLoadingModule} from '@taiga-ui/kit/directives/lazy-loading';
+import {TuiLazyLoadingModule} from '@taiga-ui/kit/directives';
 import {TuiAvatarComponent} from './avatar.component';
 
 @NgModule({
