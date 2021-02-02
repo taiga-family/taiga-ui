c3100839c0111e561c804b9d1eff942f93fca44e
MarsiBarsi <darragon-nn@yandex.ru>
2021-02-02 17:52:33 +0300

    chore(i18n): add german to exports

--- projects/i18n/languages/index.ts
+++ projects/i18n/languages/index.ts
@@ -1,3 +1,4 @@
 export * from '@taiga-ui/i18n/languages/english';
+export * from '@taiga-ui/i18n/languages/german';
 export * from '@taiga-ui/i18n/languages/russian';
 export * from '@taiga-ui/i18n/languages/spanish';
