21ebd8958d2fa58df2a278b63f6fb509a49ba62b
Daniel Reiser <daniel.reiser@mhp.com>
2021-02-02 11:15:36 +0100

    feat(i18n): Add german localisation

---
+++ projects/i18n/languages/german/addon-table.ts
@@ -0,0 +1,10 @@
+import {LanguageTable} from '@taiga-ui/i18n/interfaces';
+
+export const TUI_GERMAN_LANGUAGE_ADDON_TABLE: LanguageTable = {
+    showHideText: 'Anzeigen/Ausblenden',
+    paginationTexts: {
+        pages: 'Seiten',
+        linesPerPage: 'Zeilen pro Seite',
+        of: 'von',
+    },
+};
---
+++ projects/i18n/languages/german/core.ts
@@ -0,0 +1,23 @@
+import {LanguageCore} from '@taiga-ui/i18n/interfaces';
+
+export const TUI_GERMAN_LANGUAGE_CORE: LanguageCore = {
+    months: [
+        'Januar',
+        'Februar',
+        'März',
+        'April',
+        'Mai',
+        'Juni',
+        'Juli',
+        'August',
+        'September',
+        'Oktober',
+        'November',
+        'Dezember',
+    ],
+    close: 'Schließen',
+    nothingFoundMessage: 'Keine Ergebnisse',
+    defaultErrorMessage: 'Wert ist ungültig',
+    spinTexts: ['Zurück', 'Weiter'],
+    shortWeekDays: ['Mon', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],
+};
