8142df40ce4a4050648acf2770a4e5524666082e
MarsiBarsi <darragon-nn@yandex.ru>
2021-02-03 12:20:36 +0300

    chore(i18n): add Turkish to exports

--- projects/i18n/languages/index.ts
+++ projects/i18n/languages/index.ts
@@ -2,3 +2,4 @@ export * from '@taiga-ui/i18n/languages/english';
 export * from '@taiga-ui/i18n/languages/german';
 export * from '@taiga-ui/i18n/languages/russian';
 export * from '@taiga-ui/i18n/languages/spanish';
+export * from '@taiga-ui/i18n/languages/turkish';
