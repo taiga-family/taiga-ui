a4de9d983edd6fa453019be6c424f496d8d882fd
Nevzat Topçu <c1311057@student.cankaya.edu.tr>
2021-02-03 12:19:46 +0300

    feat(i18n): add Turkish with 100% support (#200)

---
+++ projects/i18n/languages/turkish/addon-table.ts
@@ -0,0 +1,10 @@
+import {LanguageTable} from '@taiga-ui/i18n/interfaces';
+
+export const TUI_TURKISH_LANGUAGE_ADDON_TABLE: LanguageTable = {
+    showHideText: 'Göster/Hizle',
+    paginationTexts: {
+        pages: 'Sayfa',
+        linesPerPage: 'Görüntülenen',
+        of: 'toplam',
+    },
+};
