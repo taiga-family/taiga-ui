9718013cdbbcd8c023ed4a050b44f135f354563d
Alex Inkin <alexander@inkin.ru>
2021-02-04 22:28:15 +0300

    Update spaces.module.ts

--- projects/demo/src/modules/markup/spaces/spaces.module.ts
+++ projects/demo/src/modules/markup/spaces/spaces.module.ts
@@ -1,4 +1,4 @@
-import { ClipboardModule } from '@angular/cdk/clipboard';
+import {ClipboardModule} from '@angular/cdk/clipboard';
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
