1d30fd78c98323ac205794bfe5faaf51324df5de
waterplea <alexander@inkin.ru>
2021-02-15 19:21:53 +0300

    feat(core): `NightThemeService` add new service

---
+++ projects/core/services/night-theme.service.ts
@@ -0,0 +1,20 @@
+import {Inject, Injectable} from '@angular/core';
+import {WINDOW} from '@ng-web-apis/common';
+import {fromEvent, Observable} from 'rxjs';
+import {map, share, startWith} from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class NightThemeService extends Observable<boolean> {
+    constructor(@Inject(WINDOW) windowRef: Window) {
+        const media = windowRef.matchMedia('(prefers-color-scheme: dark)');
+        const media$ = fromEvent(media, 'change').pipe(
+            startWith(null),
+            map(() => media.matches),
+            share(),
+        );
+
+        super(subscriber => media$.subscribe(subscriber));
+    }
+}
