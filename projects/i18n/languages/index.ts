f3045543b28bfb341a706e972e0fee69c790ddad
MarsiBarsi <darragon-nn@yandex.ru>
2021-02-19 14:59:33 +0300

    chore(i18n): add ukrainian to index

--- projects/i18n/languages/index.ts
+++ projects/i18n/languages/index.ts
@@ -4,3 +4,4 @@ export * from '@taiga-ui/i18n/languages/german';
 export * from '@taiga-ui/i18n/languages/russian';
 export * from '@taiga-ui/i18n/languages/spanish';
 export * from '@taiga-ui/i18n/languages/turkish';
+export * from '@taiga-ui/i18n/languages/ukrainian';
