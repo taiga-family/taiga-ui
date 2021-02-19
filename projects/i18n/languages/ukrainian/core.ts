31ae0203b3e12914bb01fdcbe950dc4af957586f
Dmytro Sych <dmytro.sych@evry.com>
2021-02-19 12:51:53 +0200

    feat(i18n): add Ukrainian support

---
+++ projects/i18n/languages/ukrainian/core.ts
@@ -0,0 +1,23 @@
+import {LanguageCore} from '@taiga-ui/i18n/interfaces';
+
+export const TUI_UKRAINIAN_LANGUAGE_CORE: LanguageCore = {
+    months: [
+        'Січень',
+        'Лютий',
+        'Березень',
+        'Квітень',
+        'Травень',
+        'Червень',
+        'Липень',
+        'Серпень',
+        'Вересень',
+        'Жовтень',
+        'Листопад',
+        'Грудень',
+    ],
+    close: 'Закрити',
+    nothingFoundMessage: 'Нічого не знайдено',
+    defaultErrorMessage: 'Поле заповнено невірно',
+    spinTexts: ['Попередній', 'Наступний'],
+    shortWeekDays: ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'НД'],
+};
