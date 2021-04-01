6e90cad33d6eae1a592765fa71f85fb7d8ce0396
johnguzenko <johnguzenko@gmail.com>
2021-04-01 17:38:06 +0300

    fix(kit): fix apple device detection (#320)
    Co-authored-by: Evgenii Guzenko <e.guzenko@tinkoff.ru>

--- projects/cdk/tokens/is-ios.ts
+++ projects/cdk/tokens/is-ios.ts
@@ -1,11 +1,9 @@
 import {inject, InjectionToken} from '@angular/core';
 import {USER_AGENT} from '@ng-web-apis/common';
-import {TUI_IS_MOBILE} from './is-mobile';
 
 const IOS_REG_EXP = /ipad|iphone|ipod/;
 
 export const TUI_IS_IOS = new InjectionToken<boolean>('iOS browser detection', {
     providedIn: 'root',
-    factory: () =>
-        inject(TUI_IS_MOBILE) && IOS_REG_EXP.test(inject(USER_AGENT).toLowerCase()),
+    factory: () => IOS_REG_EXP.test(inject(USER_AGENT).toLowerCase()),
 });
