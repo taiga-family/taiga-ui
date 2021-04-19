3e46fd60ba0d556e46281293ac800cb082b5b96d
waterplea <alexander@inkin.ru>
2021-04-19 15:23:14 +0300

    chore(demo): `TreeView` add example

--- projects/demo/src/modules/components/tree-view/tree-view.module.ts
+++ projects/demo/src/modules/components/tree-view/tree-view.module.ts
@@ -1,8 +1,8 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {generateRoutes, TuiAddonDocModule} from '@taiga-ui/addon-doc';
+import {TuiSvgModule} from '@taiga-ui/core';
 import {TuiTreeViewModule} from '@taiga-ui/kit';
 import {TuiTreeViewExample1} from './examples/1';
 import {ExampleTuiTreeViewComponent} from './tree-view.component';
@@ -10,9 +10,8 @@ import {ExampleTuiTreeViewComponent} from './tree-view.component';
 @NgModule({
     imports: [
         TuiTreeViewModule,
+        TuiSvgModule,
         CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
         TuiAddonDocModule,
         RouterModule.forChild(generateRoutes(ExampleTuiTreeViewComponent)),
     ],
