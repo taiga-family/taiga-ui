a8357d25198fda33ce23914f44ff5f5b6cb09cab
waterplea <alexander@inkin.ru>
2021-04-19 15:23:14 +0300

    chore(demo): `TreeView` add example

--- projects/demo/src/modules/components/tree-view/examples/1/index.ts
+++ projects/demo/src/modules/components/tree-view/examples/1/index.ts
@@ -1,11 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {changeDetection} from '../../../../../change-detection-strategy';
-import {encapsulation} from '../../../../../view-encapsulation';
 
 @Component({
     selector: 'tui-tree-view-example-1',
     templateUrl: './index.html',
+    styleUrls: ['./index.less'],
+    encapsulation: ViewEncapsulation.None,
     changeDetection,
-    encapsulation,
 })
-export class TuiTreeViewExample1 {}
+export class TuiTreeViewExample1 {
+    readonly data = [
+        {text: 'Top level 1', icon: 'tuiIconHeart', level: 1},
+        [
+            {text: 'Another item', level: 2},
+            [
+                {text: 'Next level 1', icon: 'tuiIconHeart', level: 3},
+                {text: 'Next level 2', icon: 'tuiIconHeart', level: 3},
+                {text: 'Next level 3', level: 3},
+            ],
+        ],
+        {text: 'Top level 2', level: 1},
+        {text: 'Top level 3', level: 1},
+        [
+            {text: 'Test 1', level: 2},
+            {text: 'Test 2', level: 2, icon: 'tuiIconHeart'},
+        ],
+    ];
+}
