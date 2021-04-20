1e4ba18b7f9b008c3faf1dd5b0f56b30ff4d00a3
waterplea <alexander@inkin.ru>
2021-04-20 19:24:15 +0300

    chore(build): fix

--- projects/kit/components/input-number/input-number.module.ts
+++ projects/kit/components/input-number/input-number.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {TuiMapperPipeModule} from '@taiga-ui/cdk';
 import {TuiPrimitiveTextfieldModule, TuiTextfieldControllerModule} from '@taiga-ui/core';
+import {TuiValueAccessorModule} from '@taiga-ui/kit/directives';
 import {TextMaskModule} from 'angular2-text-mask';
 import {TuiInputNumberComponent} from './input-number.component';
 
@@ -10,6 +11,7 @@ import {TuiInputNumberComponent} from './input-number.component';
         TuiMapperPipeModule,
         TuiPrimitiveTextfieldModule,
         TuiTextfieldControllerModule,
+        TuiValueAccessorModule,
     ],
     declarations: [TuiInputNumberComponent],
     exports: [TuiInputNumberComponent],
