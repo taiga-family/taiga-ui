0c1d1ad678dab8ce6f319285e7c188e8fd956572
MarsiBarsi <darragon-nn@yandex.ru>
2021-04-28 11:06:06 +0300

    chore: small refactors

--- projects/kit/components/multi-select/hide-selected.pipe.ts
+++ projects/kit/components/multi-select/hide-selected.pipe.ts
@@ -1,21 +1,17 @@
-import {Inject, Optional, Pipe, PipeTransform} from '@angular/core';
+import {Inject, Pipe, PipeTransform} from '@angular/core';
 import {TuiIdentityMatcher, tuiPure} from '@taiga-ui/cdk';
 import {TuiMultiSelectComponent} from './multi-select.component';
 
-// TODO: Remove Optional and second argument when Angular is upgraded to 9+
 @Pipe({
     name: 'tuiHideSelected',
     pure: false,
 })
 export class TuiHideSelectedPipe<T> implements PipeTransform {
     constructor(
-        @Optional()
         @Inject(TuiMultiSelectComponent)
         private readonly component: TuiMultiSelectComponent<T>,
     ) {}
 
-    transform(items: null, component?: unknown): null;
-    transform(items: ReadonlyArray<T>, component?: TuiMultiSelectComponent<T>): null;
     transform(
         items: ReadonlyArray<T> | null,
         {value, identityMatcher}: TuiMultiSelectComponent<T> = this.component,
