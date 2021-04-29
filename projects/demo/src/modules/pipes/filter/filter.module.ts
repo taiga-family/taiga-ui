ff378bd70c465eae2ba4f88de087a4230dae3e08
Nikita Dubinsky <61326619+nikitadubinsk@users.noreply.github.com>
2021-04-29 13:30:55 +0300

    docs(demo): examples are written for mapperPipe and filterPipe (#357)
    * docs(demo): wripe example about mapperFilter
    
    * docs(demo): write example about filterPipe
    
    * docs(demo): write examples about filterPipe and MapperPipe
    
    * docs(demo): fix docs about pipes
    
    * docs(demo): fix some problem
    
    Co-authored-by: Dubinskiy Nikita I <n.dubinskiy@tcsbank.ru>

--- projects/demo/src/modules/pipes/filter/filter.module.ts
+++ projects/demo/src/modules/pipes/filter/filter.module.ts
@@ -4,6 +4,8 @@ import {RouterModule} from '@angular/router';
 import {TuiMoneyModule} from '@taiga-ui/addon-commerce';
 import {generateRoutes, TuiAddonDocModule} from '@taiga-ui/addon-doc';
 import {TuiFilterPipeModule} from '@taiga-ui/cdk';
+import {TuiTableModeModule} from '@taiga-ui/core';
+import {TuiFilterExample1} from './examples/1/component';
 import {ExampleTuiFilterComponent} from './filter.component';
 
 @NgModule({
@@ -12,9 +14,10 @@ import {ExampleTuiFilterComponent} from './filter.component';
         TuiMoneyModule,
         CommonModule,
         TuiAddonDocModule,
+        TuiTableModeModule,
         RouterModule.forChild(generateRoutes(ExampleTuiFilterComponent)),
     ],
-    declarations: [ExampleTuiFilterComponent],
+    declarations: [ExampleTuiFilterComponent, TuiFilterExample1],
     exports: [ExampleTuiFilterComponent],
 })
 export class ExampleTuiFilterModule {}
--- projects/demo/src/modules/pipes/mapper/mapper.module.ts
+++ projects/demo/src/modules/pipes/mapper/mapper.module.ts
@@ -3,6 +3,8 @@ import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {generateRoutes, TuiAddonDocModule} from '@taiga-ui/addon-doc';
 import {TuiMapperPipeModule} from '@taiga-ui/cdk';
+import {TuiMapperExample1} from './examples/1/component';
+import {TuiMapperExample2} from './examples/2/component';
 import {ExampleTuiMapperComponent} from './mapper.component';
 
 @NgModule({
@@ -12,7 +14,7 @@ import {ExampleTuiMapperComponent} from './mapper.component';
         TuiAddonDocModule,
         RouterModule.forChild(generateRoutes(ExampleTuiMapperComponent)),
     ],
-    declarations: [ExampleTuiMapperComponent],
+    declarations: [ExampleTuiMapperComponent, TuiMapperExample1, TuiMapperExample2],
     exports: [ExampleTuiMapperComponent],
 })
 export class ExampleTuiMapperModule {}
