efbeeeacaa28d1dcbefefc947f05bae391f6f6bd
Filip Winiarski <f.winiarski@tisagroup.ch>
2021-05-19 21:26:10 +0200

    feat(core): pull request improvements

--- projects/core/modules/notifications/notification-alert/notification-alert.component.ts
+++ projects/core/modules/notifications/notification-alert/notification-alert.component.ts
@@ -6,7 +6,7 @@ import {
     Input,
 } from '@angular/core';
 import {TuiDestroyService, tuiPure} from '@taiga-ui/cdk';
-import {fromEvent, race, timer} from 'rxjs';
+import {fromEvent, timer} from 'rxjs';
 import {filter, repeatWhen, takeUntil} from 'rxjs/operators';
 
 import {TuiNotificationContentContext} from '../notification-content-context';
@@ -31,8 +31,9 @@ export class TuiNotificationAlertComponent<O, I> {
     ) {
         timer(ALERT_AUTOCLOSE_TIMEOUT)
             .pipe(
-                takeUntil(race(destroy$, fromEvent(nativeElement, 'mouseenter'))),
+                takeUntil(fromEvent(nativeElement, 'mouseenter')),
                 repeatWhen(() => fromEvent(nativeElement, 'mouseleave')),
+                takeUntil(destroy$),
                 filter(() => this.safeItem.autoClose),
             )
             .subscribe(() => this.closeNotification());
