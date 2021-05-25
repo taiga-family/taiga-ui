307ddad805c51360a0e0c8f85b365da9abdac655
waterplea <alexander@inkin.ru>
2021-05-25 11:39:12 +0300

    chore(core): remove change detection hacks

--- projects/core/components/hints-host/hints-host.component.ts
+++ projects/core/components/hints-host/hints-host.component.ts
@@ -1,19 +1,10 @@
 import {AnimationOptions} from '@angular/animations';
-import {
-    ChangeDetectionStrategy,
-    ChangeDetectorRef,
-    Component,
-    ElementRef,
-    Inject,
-} from '@angular/core';
-import {TuiDestroyService} from '@taiga-ui/cdk';
+import {ChangeDetectionStrategy, Component, ElementRef, Inject} from '@angular/core';
 import {AbstractTuiHint} from '@taiga-ui/core/abstract';
 import {tuiFadeIn} from '@taiga-ui/core/animations';
 import {TuiHintDirective} from '@taiga-ui/core/directives/hint';
 import {TuiHintService} from '@taiga-ui/core/services';
 import {TUI_ANIMATION_OPTIONS} from '@taiga-ui/core/tokens';
-import {Observable} from 'rxjs';
-import {skip, takeUntil} from 'rxjs/operators';
 
 @Component({
     selector: 'tui-hints-host',
@@ -21,18 +12,11 @@ import {skip, takeUntil} from 'rxjs/operators';
     styleUrls: ['./hints-host.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
     animations: [tuiFadeIn],
-    providers: [TuiDestroyService],
     host: {
         'aria-live': 'polite',
     },
 })
 export class TuiHintsHostComponent {
-    /**
-     * @awful TODO: remove hints variable
-     * and use hints$ after resolving issue with Change Detection
-     */
-    hints: ReadonlyArray<AbstractTuiHint> = [];
-
     readonly animation = {
         value: '',
         ...this.options,
@@ -41,15 +25,8 @@ export class TuiHintsHostComponent {
     constructor(
         @Inject(TUI_ANIMATION_OPTIONS) private readonly options: AnimationOptions,
         @Inject(ElementRef) private readonly elementRef: ElementRef<HTMLElement>,
-        @Inject(TuiHintService) hints$: TuiHintService,
-        @Inject(ChangeDetectorRef) private readonly changeDetectorRef: ChangeDetectorRef,
-        @Inject(TuiDestroyService) destroy$: Observable<void>,
-    ) {
-        hints$.pipe(skip(1), takeUntil(destroy$)).subscribe(hints => {
-            this.hints = hints;
-            this.changeDetectorRef.detectChanges();
-        });
-    }
+        @Inject(TuiHintService) readonly hints$: TuiHintService,
+    ) {}
 
     get clientRect(): ClientRect {
         return this.elementRef.nativeElement.getBoundingClientRect();
