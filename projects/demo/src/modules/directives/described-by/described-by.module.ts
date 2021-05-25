63004523ff60c0610732a2878691e3b11cc53000
waterplea <alexander@inkin.ru>
2021-05-25 18:51:36 +0300

    chore(demo): better examples

--- projects/demo/src/modules/directives/described-by/described-by.module.ts
+++ projects/demo/src/modules/directives/described-by/described-by.module.ts
@@ -2,8 +2,12 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {generateRoutes, TuiAddonDocModule} from '@taiga-ui/addon-doc';
-import {TuiDescribedByModule} from '@taiga-ui/core';
-import {TuiTagModule} from '@taiga-ui/kit';
+import {
+    TuiButtonModule,
+    TuiDescribedByModule,
+    TuiHintModule,
+    TuiNotificationModule,
+} from '@taiga-ui/core';
 import {ExampleTuiDescribeByComponent} from './described-by.component';
 import {TuiDescribedByExample1} from './examples/1';
 
@@ -11,7 +15,9 @@ import {TuiDescribedByExample1} from './examples/1';
     imports: [
         CommonModule,
         TuiAddonDocModule,
-        TuiTagModule,
+        TuiNotificationModule,
+        TuiButtonModule,
+        TuiHintModule,
         TuiDescribedByModule,
         RouterModule.forChild(generateRoutes(ExampleTuiDescribeByComponent)),
     ],
