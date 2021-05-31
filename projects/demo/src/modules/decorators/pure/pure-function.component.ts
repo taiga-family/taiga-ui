472d230592f7a07beca89aa0e95ba710516afd5f
Sergey Vakhramov <vakhramoff@xakep.ru>
2021-05-31 12:28:36 +0500

    fix(demo): `Pure` fix input placeholder text

--- projects/demo/src/modules/decorators/pure/pure-function.component.ts
+++ projects/demo/src/modules/decorators/pure/pure-function.component.ts
@@ -5,9 +5,7 @@ import {changeDetection} from '../../../change-detection-strategy';
 @Component({
     selector: 'example-tui-pure-function',
     template: `
-        <tui-input [(ngModel)]="text">
-            Type a text to start computing/tui-pure
-        </tui-input>
+        <tui-input [(ngModel)]="text"> Type a text to start computing </tui-input>
         <div class="tui-space_top-2">Called times: {{ counter.count }}</div>
         <div *ngIf="show" class="tui-space_top-2">
             Result: {{ calculate(counter, text) | json }}
