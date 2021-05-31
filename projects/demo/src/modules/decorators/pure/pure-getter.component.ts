f5c97094fc71492db40dc272f9ee49924e69c2db
Sergey Vakhramov <vakhramoff@xakep.ru>
2021-05-31 12:43:01 +0500

    chore(demo): fix `Pure` example

--- projects/demo/src/modules/decorators/pure/pure-getter.component.ts
+++ projects/demo/src/modules/decorators/pure/pure-getter.component.ts
@@ -5,20 +5,22 @@ import {changeDetection} from '../../../change-detection-strategy';
 @Component({
     selector: 'example-tui-pure-getter',
     template: `
-        <div *ngIf="show">fibonacci(40) = {{ fibonacci40 }}</div>
+        <div *ngIf="show" class="tui-space_bottom-2">
+            fibonacci(42) = {{ fibonacci42 }}
+        </div>
         <button tuiButton type="button" (click)="show = !show">Show/hide</button>
     `,
     changeDetection,
 })
 export class ExampleTuiPureGetterComponent {
     @tuiPure
-    get fibonacci40(): number {
-        return this.fibonacci(40);
+    get fibonacci42(): number {
+        return this.fibonacci(42);
     }
 
     show = false;
 
     private fibonacci(num: number): number {
-        return num <= 1 ? 1 : this.fibonacci(num - 1) + this.fibonacci(num - 2);
+        return num <= 1 ? num : this.fibonacci(num - 1) + this.fibonacci(num - 2);
     }
 }
