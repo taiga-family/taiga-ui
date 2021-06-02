d11da1f78126e088cc7f6ee43f37de6e62d2a359
waterplea <alexander@inkin.ru>
2021-06-02 18:44:26 +0300

    fix(cdk): `ActiveZone` wait for `focusout` event after `mousedown`

--- projects/cdk/tokens/active-element.ts
+++ projects/cdk/tokens/active-element.ts
@@ -1,8 +1,19 @@
 import {inject, InjectionToken} from '@angular/core';
 import {WINDOW} from '@ng-web-apis/common';
 import {typedFromEvent} from '@taiga-ui/cdk/observables';
-import {merge, Observable} from 'rxjs';
-import {filter, map, repeatWhen, share, takeUntil, withLatestFrom} from 'rxjs/operators';
+import {merge, Observable, of, timer} from 'rxjs';
+import {
+    distinctUntilChanged,
+    filter,
+    map,
+    mapTo,
+    repeatWhen,
+    share,
+    switchMap,
+    take,
+    takeUntil,
+    withLatestFrom,
+} from 'rxjs/operators';
 import {TUI_REMOVED_ELEMENT} from './removed-element';
 
 export const TUI_ACTIVE_ELEMENT = new InjectionToken<Observable<EventTarget | null>>(
@@ -11,6 +22,7 @@ export const TUI_ACTIVE_ELEMENT = new InjectionToken<Observable<EventTarget | nu
         factory: () => {
             const skip$ = inject(TUI_REMOVED_ELEMENT);
             const windowRef = inject(WINDOW);
+            const {document} = windowRef;
             const focusout$ = typedFromEvent(windowRef, 'focusout');
             const focusin$ = typedFromEvent(windowRef, 'focusin');
             const blur$ = typedFromEvent(windowRef, 'blur');
@@ -26,11 +38,19 @@ export const TUI_ACTIVE_ELEMENT = new InjectionToken<Observable<EventTarget | nu
                     map(([{relatedTarget}]) => relatedTarget),
                 ),
                 blur$.pipe(
-                    map(() => windowRef.document.activeElement),
+                    map(() => document.activeElement),
                     filter(element => !!element && element.matches('iframe')),
                 ),
-                merge(mousedown$, focusin$).pipe(map(({target}) => target)),
-            ).pipe(share());
+                focusin$.pipe(map(({target}) => target)),
+                mousedown$.pipe(
+                    switchMap(({target}) =>
+                        !document.activeElement ||
+                        document.activeElement === document.body
+                            ? of(target)
+                            : focusout$.pipe(take(1), takeUntil(timer(0)), mapTo(target)),
+                    ),
+                ),
+            ).pipe(distinctUntilChanged(), share());
         },
     },
 );
