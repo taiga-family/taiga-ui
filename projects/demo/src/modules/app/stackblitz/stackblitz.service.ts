5b34add5394603dd309c26fe858a5f91cac54bd7
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2021-06-04 10:38:34 +0300

    chore(demo): exact version of the polymorpheus package for stackblitz (#460)
    * chore(demo): exact version of the polymorpheus package for stackblitz
    
    * chore(demo): fix some stackblitz exapmles
    
    * chore(demo): add some modules to fix some examples

--- projects/demo/src/modules/app/stackblitz/stackblitz.service.ts
+++ projects/demo/src/modules/app/stackblitz/stackblitz.service.ts
@@ -27,7 +27,7 @@ const DEPS: Record<string, string> = {
     '@taiga-ui/addon-tablebars': '*',
     '@taiga-ui/addon-editor': '*',
     '@tinkoff/ng-dompurify': '*',
-    '@tinkoff/ng-polymorpheus': '*',
+    '@tinkoff/ng-polymorpheus': '3.1.8',
     '@ng-web-apis/common': '*',
     '@tinkoff/ng-event-plugins': '*',
     '@ng-web-apis/intersection-observer': '*',
@@ -38,18 +38,29 @@ const DEPS: Record<string, string> = {
     '@angular/cdk': '*',
 };
 
-const COMPONENT = `@Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.less'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+const COMPONENT_SELECTOR = `selector: 'my-app',`;
+const COMPONENT_TEMPLATE = `templateUrl: './app.component.html',`;
+const COMPONENT_STYLES = `styleUrls: ['./app.component.less'],`;
+
+const COMPONENT_NAME = `
 })
-export class AppComponent `;
+export class AppComponent {`;
 
 function prepareTs(content: string): string {
     console.log(content);
 
-    return content.replace(/@Component\({[A-z:'\n\s\-,.\/0-9})]+/gm, COMPONENT);
+    return content
+        .replace(/selector: \'.+,*$/gm, COMPONENT_SELECTOR)
+        .replace(/templateUrl: \'.+,*$/gm, COMPONENT_TEMPLATE)
+        .replace(/styleUrls: \['.+,*$/gm, COMPONENT_STYLES)
+        .replace(/^}\)\nexport class [\w ]+{/m, COMPONENT_NAME);
+}
+
+function prepareLess(content: string): string {
+    return content.replace(
+        '~@taiga-ui/core/styles/taiga-ui-local',
+        '@taiga-ui/core/styles/taiga-ui-local.less',
+    );
 }
 
 @Injectable()
@@ -78,7 +89,7 @@ export class StackblitzService implements CodeEditor {
                 'src/app/app.module.ts': appModuleTs,
                 'src/app/app.component.ts': prepareTs(content.TypeScript || ''),
                 'src/app/app.component.html': `<tui-root>\n\n${content.HTML}\n</tui-root>`,
-                'src/app/app.component.less': content.LESS || '',
+                'src/app/app.component.less': prepareLess(content.LESS || ''),
             },
             tags: ['Angular', 'Taiga UI', 'Angular components', 'UI Kit'],
         });
