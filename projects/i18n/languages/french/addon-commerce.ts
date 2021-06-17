983d20a4f054483531e0bf2510a1df44499ec95c
Kodjo Laurent Egbakou <egbakoukodjolaurent@gmail.com>
2021-06-17 04:51:32 +0200

    Add French language support

---
+++ projects/i18n/languages/french/addon-commerce.ts
@@ -0,0 +1,6 @@
+import {LanguageCommerce} from '@taiga-ui/i18n/interfaces';
+
+export const TUI_ENGLISH_LANGUAGE_ADDON_COMMERCE: LanguageCommerce = {
+    cardNumber: ['Number', 'Numéro de carte'],
+    cardExpiry: ['Expiry', 'Date d"Expiration'],
+};
