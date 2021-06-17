0359dbb3b046dcb66e2f5d459364f2413de5d37b
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-06-17 11:49:30 +0300

    feat(core): new `CloseDialogGuard`

---
+++ projects/core/components/dialog/closeDialog.guard.ts
@@ -0,0 +1,40 @@
+import {Inject, Injectable} from '@angular/core';
+import {CanDeactivate} from '@angular/router';
+import {
+    TUI_DIALOGS,
+    TUI_IS_BACK_FORWARD_NAVIGATION_STREAM,
+    TuiDialog,
+} from '@taiga-ui/cdk';
+import {TuiDialogOptions} from '@taiga-ui/core';
+import {combineLatest, merge, Observable, ReplaySubject} from 'rxjs';
+import {map, take} from 'rxjs/operators';
+
+@Injectable()
+export class CloseDialogGuard implements CanDeactivate<unknown> {
+    constructor(
+        @Inject(TUI_IS_BACK_FORWARD_NAVIGATION_STREAM)
+        private isBrowserNavigationLatest$: ReplaySubject<boolean>,
+        @Inject(TUI_DIALOGS)
+        private readonly dialogs: Observable<
+            TuiDialog<TuiDialogOptions<unknown>, unknown>[]
+        >[],
+    ) {}
+
+    canDeactivate(): Observable<boolean> {
+        return combineLatest([
+            this.isBrowserNavigationLatest$,
+            merge(...this.dialogs),
+        ]).pipe(
+            take(1),
+            map(([isBrowserNavigationLatest, dialogs]) => {
+                if (dialogs.length && isBrowserNavigationLatest) {
+                    dialogs[dialogs.length - 1].$implicit.complete();
+
+                    return false;
+                }
+
+                return true;
+            }),
+        );
+    }
+}
--- projects/core/components/dialog/index.ts
+++ projects/core/components/dialog/index.ts
@@ -2,3 +2,4 @@ export * from './dialog.component';
 export * from './dialog.module';
 export * from './dialog.providers';
 export * from './dialog.service';
+export * from './closeDialog.guard';
