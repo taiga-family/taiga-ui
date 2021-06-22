651d5d144612431de91f57ce5c4bada12b080811
Vladimir <vladimir.potekh@gmail.com>
2021-06-22 17:08:05 +0300

    chore(demo-integrations): add launch options

--- projects/demo-integrations/cypress/plugins/index.js
+++ projects/demo-integrations/cypress/plugins/index.js
@@ -2,4 +2,12 @@ const {addMatchImageSnapshotPlugin} = require('cypress-image-snapshot/plugin');
 
 module.exports = (on, config) => {
     addMatchImageSnapshotPlugin(on, config);
+    on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome') {
+            launchOptions.args.push('--disable-dev-shm-usage');
+            launchOptions.args.push('--force-device-scale-factor=1');
+        }
+
+        return launchOptions;
+    });
 };
