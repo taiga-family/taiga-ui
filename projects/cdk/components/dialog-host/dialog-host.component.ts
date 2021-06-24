a00ab9e42da1d25d7bad0aacde15ea993fcab8e1
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-06-24 17:33:57 +0300

    feat(cdk): move all logic with backNavigation to `TuiDialogHostComponent`

--- projects/cdk/components/dialog-host/dialog-host.component.ts
+++ projects/cdk/components/dialog-host/dialog-host.component.ts
@@ -1,9 +1,14 @@
 import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {WINDOW} from '@ng-web-apis/common';
 import {TUI_PARENT_ANIMATION} from '@taiga-ui/cdk/constants';
-import {TuiAriaDialogContext} from '@taiga-ui/cdk/interfaces';
-import {TUI_DIALOGS} from '@taiga-ui/cdk/tokens';
+import {TuiDestroyService} from '@taiga-ui/cdk/services';
+import {TUI_DIALOGS, TUI_POPSTATE_STREAM} from '@taiga-ui/cdk/tokens';
+import {TuiDialog} from '@taiga-ui/cdk/types';
 import {combineLatest, Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {map, pairwise, takeUntil, tap} from 'rxjs/operators';
+
+const FAKE_IGNORE_HISTORY_STATE = {label: 'ignoreMe'} as const;
 
 // @dynamic
 @Component({
@@ -13,17 +18,73 @@ import {map} from 'rxjs/operators';
     changeDetection: ChangeDetectionStrategy.OnPush,
     animations: [TUI_PARENT_ANIMATION],
 })
-export class TuiDialogHostComponent<T extends TuiAriaDialogContext> {
+export class TuiDialogHostComponent<T extends TuiDialog<unknown, unknown>> {
+    private lastDialogsStack: T[] = [];
+    private watchBrowserNavigation = false;
+
     readonly dialogs$ = combineLatest(this.dialogsByType).pipe(
         map(allTypesDialogs =>
             new Array<T>()
                 .concat(...allTypesDialogs)
                 .sort((a, b) => a.createdAt - b.createdAt),
         ),
+        pairwise(),
+        tap(dialogsStates => this.mutateBrowserHistory(dialogsStates)),
+        map(([_, currentDialogState]) => currentDialogState),
+        tap(dialogs => (this.lastDialogsStack = [...dialogs])),
     );
 
     constructor(
+        @Inject(WINDOW) private readonly windowRef: Window,
         @Inject(TUI_DIALOGS)
         private readonly dialogsByType: Observable<readonly T[]>[],
-    ) {}
+        @Inject(Title) private readonly titleService: Title,
+        @Inject(TUI_POPSTATE_STREAM) browserNavigation$: Observable<PopStateEvent>,
+        @Inject(TuiDestroyService) destroy$: TuiDestroyService,
+    ) {
+        browserNavigation$.pipe(takeUntil(destroy$)).subscribe(() => {
+            if (this.watchBrowserNavigation) {
+                this.closeLastDialog(this.lastDialogsStack);
+            } else {
+                this.watchBrowserNavigation = true;
+            }
+        });
+    }
+
+    private checkIsFakeHistoryStateOnTop(
+        history: History = this.windowRef.history,
+    ): boolean {
+        return history.state?.label === FAKE_IGNORE_HISTORY_STATE.label;
+    }
+
+    private closeLastDialog(dialogs: ReadonlyArray<T>) {
+        const [last] = dialogs.slice(-1);
+
+        if (last) {
+            last.$implicit.complete();
+        }
+    }
+
+    private mutateBrowserHistory([prevDialogState, currentDialogState]: [
+        ReadonlyArray<T>,
+        ReadonlyArray<T>,
+    ]) {
+        const wasNewPopupOpened = currentDialogState.length > prevDialogState.length;
+        const wasPopupClosed = currentDialogState.length < prevDialogState.length;
+
+        if (wasNewPopupOpened) {
+            this.watchBrowserNavigation = true;
+            this.windowRef.history.pushState(
+                FAKE_IGNORE_HISTORY_STATE,
+                this.titleService.getTitle(),
+            );
+        } else if (
+            wasPopupClosed &&
+            this.checkIsFakeHistoryStateOnTop() &&
+            !currentDialogState.length
+        ) {
+            this.watchBrowserNavigation = false;
+            this.windowRef.history.back();
+        }
+    }
 }
--- projects/cdk/components/dialog-host/dialog-host.module.ts
+++ projects/cdk/components/dialog-host/dialog-host.module.ts
@@ -2,11 +2,13 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {TuiFocusTrapModule} from '@taiga-ui/cdk/directives/focus-trap';
 import {TuiOverscrollModule} from '@taiga-ui/cdk/directives/overscroll';
+import {TuiDestroyService} from '@taiga-ui/cdk/services';
 import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
 import {TuiDialogHostComponent} from './dialog-host.component';
 
 @NgModule({
     imports: [CommonModule, PolymorpheusModule, TuiOverscrollModule, TuiFocusTrapModule],
+    providers: [TuiDestroyService],
     declarations: [TuiDialogHostComponent],
     exports: [TuiDialogHostComponent],
 })
