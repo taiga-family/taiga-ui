390e7188115595adba7af46b32d21562b979b339
Barsukov Nikita <35179038+nsbarsukov@users.noreply.github.com>
2021-06-24 16:36:49 +0300

    fix(cdk): opening many different types dialogs together (#503)
    Co-authored-by: Barsukov Nikita <n.barsukov@tinkoff.ru>

--- projects/cdk/interfaces/aria-dialog-context.ts
+++ projects/cdk/interfaces/aria-dialog-context.ts
@@ -3,4 +3,5 @@ import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus';
 export interface TuiAriaDialogContext {
     readonly component: PolymorpheusContent<any>;
     readonly id: string;
+    readonly createdAt: number;
 }
