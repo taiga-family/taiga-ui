188e4304bf24a49b53d1687fc4b140782d730cc1
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-06-24 13:18:56 +0300

    chore(cdk): `TUI_POPSTATE_STREAM` refactor

--- projects/cdk/tokens/popstate-stream.ts
+++ projects/cdk/tokens/popstate-stream.ts
@@ -1,20 +1,11 @@
 import {inject, InjectionToken} from '@angular/core';
-import {NavigationStart, Router} from '@angular/router';
+import {WINDOW} from '@ng-web-apis/common';
+import {typedFromEvent} from '@taiga-ui/cdk/observables';
 import {Observable} from 'rxjs';
-import {filter, map, shareReplay} from 'rxjs/operators';
 
-export const TUI_POPSTATE_STREAM = new InjectionToken<Observable<boolean>>(
-    'Is backward/forward browser navigation event stream',
+export const TUI_POPSTATE_STREAM = new InjectionToken<Observable<PopStateEvent>>(
+    'Backward/forward browser navigation event stream',
     {
-        providedIn: 'root',
-        factory: () => {
-            const router = inject(Router);
-
-            return router.events.pipe(
-                filter((e): e is NavigationStart => e instanceof NavigationStart),
-                map(event => event.navigationTrigger === 'popstate'),
-                shareReplay(1),
-            );
-        },
+        factory: () => typedFromEvent(inject(WINDOW), 'popstate'),
     },
 );
