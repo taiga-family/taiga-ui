10b817ff86f70876ea195783a8a9c2824441fc21
waterplea <alexander@inkin.ru>
2021-06-25 14:52:22 +0300

    refactor(demo): refactor navigation sync

---
+++ projects/addon-doc/src/directives/scroll-into-view/scroll-into-view.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {ScrollIntoViewDirective} from './scroll-into-view.directive';
+
+@NgModule({
+    declarations: [ScrollIntoViewDirective],
+    exports: [ScrollIntoViewDirective],
+})
+export class ScrollIntoViewModule {}
