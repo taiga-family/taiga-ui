1821f68289f2416427347288519106c45035d767
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-07-13 12:44:20 +0300

    fix(core): interface `TuiTextMaskOptions` update js-doc links

--- projects/core/mask/text-mask-options.ts
+++ projects/core/mask/text-mask-options.ts
@@ -3,36 +3,36 @@ import {TuiTextMaskListHandler} from './text-mask-list-handler';
 import {TuiTextMaskPipeHandler} from './text-mask-pipe-handler';
 
 /**
- * {@link https://github.com/text-mask/text-mask/blob/master/component-documentation.md#mask-array}
+ * {@link https://github.com/text-mask/text-mask/blob/master/componentDocumentation.md}
  */
 export interface TuiTextMaskOptions {
     /**
-     * {@link https://github.com/text-mask/text-mask/blob/master/component-documentation.md#mask}
+     * {@link https://github.com/text-mask/text-mask/blob/master/componentDocumentation.md#mask}
      */
     mask: TuiTextMaskList | TuiTextMaskListHandler;
 
     /**
-     * {@link https://github.com/text-mask/text-mask/blob/master/component-documentation.md#guide}
+     * {@link https://github.com/text-mask/text-mask/blob/master/componentDocumentation.md#guide}
      */
     guide?: boolean;
 
     /**
-     * {@link https://github.com/text-mask/text-mask/blob/master/component-documentation.md#placeholderchar}
+     * {@link https://github.com/text-mask/text-mask/blob/master/componentDocumentation.md#placeholderchar}
      */
     placeholderChar?: string;
 
     /**
-     * {@link https://github.com/text-mask/text-mask/blob/master/component-documentation.md#pipe}
+     * {@link https://github.com/text-mask/text-mask/blob/master/componentDocumentation.md#pipe}
      */
     pipe?: TuiTextMaskPipeHandler;
 
     /**
-     * {@link https://github.com/text-mask/text-mask/blob/master/component-documentation.md#keepcharpositions}
+     * {@link https://github.com/text-mask/text-mask/blob/master/componentDocumentation.md#keepcharpositions}
      */
     keepCharPositions?: boolean;
 
     /**
-     * {@link https://github.com/text-mask/text-mask/blob/master/component-documentation.md#showmask}
+     * {@link https://github.com/text-mask/text-mask/blob/master/componentDocumentation.md#showmask}
      */
     showMask?: boolean;
 }
