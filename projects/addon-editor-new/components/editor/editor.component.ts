1da02e69907dca0126c405aa03af0a15745b676e
Vladimir <vladimir.potekh@gmail.com>
2021-07-13 11:51:00 +0300

    chore: add new package

---
+++ projects/addon-editor-new/components/editor/editor.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'tui-editor',
+    templateUrl: './editor.component.html',
+})
+export class EditorComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
---
+++ projects/addon-editor-new/components/editor/editor.module.ts
@@ -0,0 +1,9 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {EditorComponent} from './editor/editor.component';
+
+@NgModule({
+    declarations: [EditorComponent],
+    imports: [CommonModule],
+})
+export class EditorModule {}
