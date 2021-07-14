c41224b2a5b6218317d7c34793629959977efb33
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-07-14 13:45:00 +0300

    feat(cdk): new type `TuiDateMode`

--- projects/cdk/date-time/date-fillers.ts
+++ projects/cdk/date-time/date-fillers.ts
@@ -1,10 +1,18 @@
 import {inject, InjectionToken} from '@angular/core';
+import {TuiDateMode} from '@taiga-ui/cdk/types';
 import {RANGE_SEPARATOR_CHAR} from './date-time';
 
-// TODO: think about yyyy.mm.dd format
-export const TUI_DATE_FILLER = new InjectionToken<string>('date filler for Taiga UI', {
-    factory: () => `dd.mm.yyyy`,
-});
+/**
+ * TODO: think about yyyy.mm.dd format
+ * TODO: Remove return type string in 3.0
+ * @deprecated return type will be narrowed down to just TuiDateMode (without string) in next major version
+ */
+export const TUI_DATE_FILLER = new InjectionToken<TuiDateMode | string>(
+    'date filler for Taiga UI',
+    {
+        factory: () => `dd.mm.yyyy`,
+    },
+);
 
 export const TUI_DATE_RANGE_FILLER = new InjectionToken<string>(
     'date range filler for Taiga UI',
---
+++ projects/cdk/types/date-mode.ts
@@ -0,0 +1,1 @@
+export type TuiDateMode = 'dd.mm.yyyy';
