728cad3b0634a0cfcd9353405c2063cec0047401
k.demyanenko <k.demyanenko@tinkoff.ru>
2021-07-19 15:33:05 +0300

    fix(kit): `getClipboardDataText` return text/plain format as fallback

--- projects/cdk/utils/dom/get-clipboard-data-text.ts
+++ projects/cdk/utils/dom/get-clipboard-data-text.ts
@@ -1,11 +1,14 @@
+const DEFAULT_FORMAT = 'text/plain';
+
 /**
  * Gets text from data of clipboardEvent, it also works in IE and Edge browsers
  */
 export function getClipboardDataText(
     event: ClipboardEvent,
-    format: string = 'text/plain',
+    format: string = DEFAULT_FORMAT,
 ): string {
     return 'clipboardData' in event && event.clipboardData !== null
-        ? event.clipboardData.getData(format)
+        ? event.clipboardData.getData(format) ||
+              event.clipboardData.getData(DEFAULT_FORMAT)
         : (event as any).target.ownerDocument.defaultView.clipboardData.getData('text');
 }
