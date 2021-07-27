50024f1fc499debe7c860243466cf0fd31ed9626
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-07-27 10:38:23 +0300

    chore(demo-integrations): speed boost of demo app's cypress tests (#578)
    * chore(demo-integrations): increase exec speed of demo app's cypress tests
    
    * chore(demo-integrations): speed boost of deep API cypress tests

--- projects/demo-integrations/cypress/tests/demo/demo.spec.ts
+++ projects/demo-integrations/cypress/tests/demo/demo.spec.ts
@@ -2,6 +2,21 @@ import {DEMO_PATHS} from '../demo-paths';
 import {EXAMPLE_ID} from '../shared.entities';
 import {excluded} from './exclusions';
 
+// not taiga ui icons
+const EXTERNAL_ICONS = ['web-api.svg'];
+
+const stubIcons = (icons: string[]): void => {
+    icons.forEach(iconName => {
+        cy.intercept(
+            {
+                method: 'GET',
+                url: new RegExp(`.*${iconName}`),
+            },
+            {fixture: `icons-stubs/${iconName}`},
+        );
+    });
+};
+
 describe('Demo', () => {
     beforeEach(() => {
         cy.viewport(1280, 720);
@@ -9,8 +24,9 @@ describe('Demo', () => {
 
     DEMO_PATHS.forEach(path => {
         it(`${path}`, () => {
+            stubIcons(EXTERNAL_ICONS);
+
             cy.goToDemoPage(path);
-            cy.wait(700);
 
             cy.get('[tuidocheader]').invoke(
                 'attr',
@@ -26,6 +42,7 @@ describe('Demo', () => {
                 return cy
                     .wrap(sample)
                     .scrollIntoView({offset: {top: 64, left: 0}})
+                    .wait(100)
                     .matchImageSnapshot(`${path}/${index + 1}`);
             });
         });
