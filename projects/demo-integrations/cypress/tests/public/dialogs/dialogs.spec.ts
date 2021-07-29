dd2e343dff97e55b8c7d67ba46cc6f0eac8a0ce7
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-07-29 11:27:56 +0300

    feat(demo-integrations): `Dialogs + browser back navigation` tests

--- projects/demo-integrations/cypress/tests/public/dialogs/dialogs.spec.ts
+++ projects/demo-integrations/cypress/tests/public/dialogs/dialogs.spec.ts
@@ -5,17 +5,15 @@ export function waitKitDialog() {
         .should('not.have.class', 'ng-animating');
 }
 
-xdescribe('Dialogs', () => {
-    before(() => {
-        cy.visit('/');
-        cy.wait(500);
-        cy.viewport(720, 720);
+const DIALOG_PAGE_URL = 'services/dialog-service';
+
+describe('Dialogs', () => {
+    beforeEach(() => {
+        cy.viewport(720, 900);
     });
 
     it('A dialog and a nested dialog are open correctly', () => {
-        cy.visit('services/dialog-service', {failOnStatusCode: false});
-
-        cy.wait(500);
+        cy.goToDemoPage(DIALOG_PAGE_URL);
 
         cy.get(`tui-dialog-example-2 button`).first().click();
         waitKitDialog();
@@ -28,12 +26,52 @@ xdescribe('Dialogs', () => {
     });
 
     it('Mobile dialog works', () => {
-        cy.viewport(720, 900);
-        cy.visit('services/dialog-service', {failOnStatusCode: false});
-        cy.wait(500);
+        cy.goToDemoPage(DIALOG_PAGE_URL);
 
         cy.get(`tui-dialog-example-4 button`).first().click();
         waitKitDialog();
         cy.get('tui-dialog').matchImageSnapshot('3');
+    });
+});
+
+describe('Dialogs + browser back navigation', () => {
+    /**
+     * Alternative to cy.go('back', {timeout: 0}), which doesnt wait page load event
+     */
+    const goBack = () => {
+        cy.window().then(win => win.history.back());
+    };
+
+    beforeEach(() => {
+        cy.viewport(720, 900);
+    });
+
+    it('closes dialog on browser back navigation', () => {
+        const baseUrl = Cypress.config().baseUrl;
+
+        cy.visit('/'); // need to check that browser back navigation is not broken after closing all dialogs
+        cy.goToDemoPage(DIALOG_PAGE_URL);
+
+        cy.getByAutomationId('tui-doc-example')
+            .as('wrapper')
+            .eq(1)
+            .find('button')
+            .first()
+            .click();
+        waitKitDialog();
+
+        cy.get('tui-dialog').get('button').contains('Show one more dialog').click();
+        waitKitDialog();
+
+        cy.window().matchImageSnapshot('4-1-opened-all-dialogs', {capture: 'viewport'});
+        goBack();
+        cy.window().matchImageSnapshot('4-2-after-first-back-nav', {capture: 'viewport'});
+        goBack();
+        cy.window().matchImageSnapshot('4-3-after-second-back-nav', {
+            capture: 'viewport',
+        });
+        cy.url().should('equal', `${baseUrl}/${DIALOG_PAGE_URL}`);
+        goBack();
+        cy.url().should('equal', `${baseUrl}/`);
     });
 });
