c4ddad660dcb6fa68fda3ef55187a43b68626e3d
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-07-29 12:24:39 +0300

    feat(demo-integrations): more `Dialogs + browser back navigation` tests

--- projects/demo-integrations/cypress/tests/public/dialogs/dialogs.spec.ts
+++ projects/demo-integrations/cypress/tests/public/dialogs/dialogs.spec.ts
@@ -42,22 +42,21 @@ describe('Dialogs + browser back navigation', () => {
         cy.window().then(win => win.history.back());
     };
 
+    const getFullUrl = (path: string): string =>
+        `${Cypress.config().baseUrl || ''}/${path}`.replace(
+            new RegExp('([^:]/)/+', 'g'),
+            '$1',
+        );
+
     beforeEach(() => {
         cy.viewport(720, 900);
-    });
-
-    it('closes dialog on browser back navigation', () => {
-        const baseUrl = Cypress.config().baseUrl;
 
         cy.visit('/'); // need to check that browser back navigation is not broken after closing all dialogs
         cy.goToDemoPage(DIALOG_PAGE_URL);
+    });
 
-        cy.getByAutomationId('tui-doc-example')
-            .as('wrapper')
-            .eq(1)
-            .find('button')
-            .first()
-            .click();
+    it('closes dialog on browser back navigation', () => {
+        cy.getByAutomationId('tui-doc-example').eq(1).find('button').first().click();
         waitKitDialog();
 
         cy.get('tui-dialog').get('button').contains('Show one more dialog').click();
@@ -70,8 +69,22 @@ describe('Dialogs + browser back navigation', () => {
         cy.window().matchImageSnapshot('4-3-after-second-back-nav', {
             capture: 'viewport',
         });
-        cy.url().should('equal', `${baseUrl}/${DIALOG_PAGE_URL}`);
+        cy.url().should('equal', getFullUrl(DIALOG_PAGE_URL));
         goBack();
-        cy.url().should('equal', `${baseUrl}/`);
+        cy.url().should('equal', getFullUrl('/'));
+    });
+
+    it('doesnt break browser back navigation after closing dialog in the usual way (without back button)', () => {
+        /* open dialog */
+        cy.getByAutomationId('tui-doc-example').first().find('button').first().click();
+
+        waitKitDialog();
+
+        /* close dialog */
+        cy.get('tui-dialog').find('button').first().click();
+
+        cy.url().should('equal', getFullUrl(DIALOG_PAGE_URL));
+        goBack();
+        cy.url().should('equal', getFullUrl('/'));
     });
 });
