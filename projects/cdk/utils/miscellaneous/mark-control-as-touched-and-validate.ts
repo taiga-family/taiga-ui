410b8040f52743f29f82a5aba32d6ee088ebcb0f
uberpwner <misha.aksenov@gmail.com>
2021-07-29 14:52:56 +0300

    fix(cdk): `markControlAsTouchedAndValidate` delete unnecessary return statements

--- projects/cdk/utils/miscellaneous/mark-control-as-touched-and-validate.ts
+++ projects/cdk/utils/miscellaneous/mark-control-as-touched-and-validate.ts
@@ -1,22 +1,18 @@
 import {AbstractControl, FormArray, FormGroup} from '@angular/forms';
 
 export function markControlAsTouchedAndValidate(control: AbstractControl) {
-    control.markAsTouched();
-    control.updateValueAndValidity();
-
     if (control instanceof FormArray) {
         control.controls.forEach(nestedControl => {
             markControlAsTouchedAndValidate(nestedControl);
         });
-
-        return;
     }
 
     if (control instanceof FormGroup) {
         Object.values(control.controls).forEach(nestedControl => {
             markControlAsTouchedAndValidate(nestedControl);
         });
-
-        return;
     }
+
+    control.markAsTouched();
+    control.updateValueAndValidity();
 }
