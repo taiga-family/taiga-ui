72168df13cab48c1185fb357147d6420476f75d2
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-08-24 18:24:21 +0300

    feat(demo-integrations): stub all demo app's assets

--- projects/demo-integrations/cypress/tests/demo/demo.spec.ts
+++ projects/demo-integrations/cypress/tests/demo/demo.spec.ts
@@ -5,7 +5,7 @@ import {excluded} from './exclusions';
 // not taiga ui icons
 const EXTERNAL_ICONS = ['web-api.svg'];
 
-const stubIcons = (icons: string[]): void => {
+const stubExternalIcons = (icons: string[]): void => {
     icons.forEach(iconName => {
         cy.intercept(
             {
@@ -17,6 +17,23 @@ const stubIcons = (icons: string[]): void => {
     });
 };
 
+const stubAllDemoAssets = (): void => {
+    cy.intercept(
+        {
+            method: 'GET',
+            url: new RegExp(`https://taiga-ui.dev/next/assets.*`),
+        },
+        req => {
+            req.url = req.url.replace(
+                'https://taiga-ui.dev/next',
+                'http://localhost:3333',
+            );
+
+            req.continue();
+        },
+    );
+};
+
 describe('Demo', () => {
     beforeEach(() => {
         cy.viewport(1280, 720);
@@ -24,7 +41,8 @@ describe('Demo', () => {
 
     DEMO_PATHS.forEach(path => {
         it(`${path}`, () => {
-            stubIcons(EXTERNAL_ICONS);
+            stubExternalIcons(EXTERNAL_ICONS);
+            stubAllDemoAssets();
 
             cy.goToDemoPage(path);
 
