8de302216f4c82b87c530d1a9b415a8988106792
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-08-26 10:32:15 +0300

    chore(i18n): add `portuguese` language export

--- projects/i18n/languages/index.ts
+++ projects/i18n/languages/index.ts
@@ -2,6 +2,7 @@ export * from '@taiga-ui/i18n/languages/dutch';
 export * from '@taiga-ui/i18n/languages/english';
 export * from '@taiga-ui/i18n/languages/french';
 export * from '@taiga-ui/i18n/languages/german';
+export * from '@taiga-ui/i18n/languages/portuguese';
 export * from '@taiga-ui/i18n/languages/russian';
 export * from '@taiga-ui/i18n/languages/spanish';
 export * from '@taiga-ui/i18n/languages/turkish';
