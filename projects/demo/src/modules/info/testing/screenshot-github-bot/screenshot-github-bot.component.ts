f4d85c40e164e143a6b535834222b55819cbace4
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-09-02 16:47:36 +0300

    feat(demo): add mention of screenshot bot in `Friendly libraries`

--- projects/demo/src/modules/info/testing/screenshot-github-bot/screenshot-github-bot.component.ts
+++ projects/demo/src/modules/info/testing/screenshot-github-bot/screenshot-github-bot.component.ts
@@ -1,12 +1,15 @@
 import {Component} from '@angular/core';
 
+export const SCREENSHOT_BOT_NAME = 'Lumberjack';
+export const SCREENSHOT_BOT_LINK = 'https://github.com/apps/lumberjack-bot';
+
 @Component({
     selector: 'screenshot-github-bot',
     templateUrl: './screenshot-github-bot.template.html',
     styleUrls: ['./screenshot-github-bot.style.less'],
 })
 export class ScreenshotGithubBotComponent {
-    readonly botName = 'Lumberjack';
-    readonly botLink = 'https://github.com/apps/lumberjack-bot';
+    readonly botName = SCREENSHOT_BOT_NAME;
+    readonly botAppLink = SCREENSHOT_BOT_LINK;
     readonly botRepoLink = 'https://github.com/TinkoffCreditSystems/argus';
 }
