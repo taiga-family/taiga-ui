7dff268f32100a56ff37c65ea1f28ad1625846f0
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-09-02 18:53:52 +0300

    feat(kit): `ProgressBar` old Edge support

--- projects/kit/components/progress-bar/progress-bar.component.ts
+++ projects/kit/components/progress-bar/progress-bar.component.ts
@@ -4,5 +4,12 @@ import {Component} from '@angular/core';
     selector: '[tuiProgressBar]',
     template: '',
     styleUrls: ['./progress-bar.component.less'],
+    host: {
+        '[style.--tui-progress-color]': 'color',
+        '[style.--tui-progress-color-old-edge]': 'oldEdgeColor',
+    },
 })
-export class TuiProgressBarComponent {}
+export class TuiProgressBarComponent {
+    oldEdgeColor = 'var(--tui-primary)';
+    color = 'linear-gradient(to left, lime 25%, red 25% 50%, cyan 50% 75%, yellow 75%)';
+}
