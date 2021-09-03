ae4377555b041011a35fdea4983af160e11e4aa6
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-09-03 17:21:01 +0300

    feat(demo): `ProgressBar` API page

--- projects/demo/src/modules/components/progress-bar/progress-bar.component.ts
+++ projects/demo/src/modules/components/progress-bar/progress-bar.component.ts
@@ -11,15 +11,36 @@ import {default as example3Less} from '!!raw-loader!./examples/3/index.less';
 import {default as example3Ts} from '!!raw-loader!./examples/3/index.ts';
 
 import {Component} from '@angular/core';
+import {TuiSizeS} from '@taiga-ui/core';
 import {changeDetection} from '../../../change-detection-strategy';
 import {FrontEndExample} from '../../interfaces/front-end-example';
 
 @Component({
     selector: 'example-tui-progress-bar',
     templateUrl: './progress-bar.component.html',
+    styleUrls: ['./progress-bar.component.less'],
     changeDetection,
 })
 export class ExampleProgressBarComponent {
+    value = 60;
+    max = 100;
+
+    readonly sizeVariants: ReadonlyArray<TuiSizeS> = ['m', 's'];
+    size: TuiSizeS = this.sizeVariants[0];
+
+    readonly colorVariants: ReadonlyArray<string | string[]> = [
+        'var(--tui-primary)',
+        'var(--tui-support-09)',
+        [
+            'var(--tui-support-01)',
+            'var(--tui-support-21)',
+            'lightskyblue',
+            '#3682db',
+            'var(--tui-support-22)',
+        ],
+    ];
+    colors = this.colorVariants[0];
+
     readonly basicExample: FrontEndExample = {
         HTML: example1Html,
         TypeScript: example1Ts,
--- projects/demo/src/modules/components/progress-bar/progress-bar.module.ts
+++ projects/demo/src/modules/components/progress-bar/progress-bar.module.ts
@@ -2,6 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {generateRoutes, TuiAddonDocModule} from '@taiga-ui/addon-doc';
+import {TuiLinkModule} from '@taiga-ui/core';
 import {TuiProgressBarModule} from '@taiga-ui/kit';
 import {TuiProgressBarExample1} from './examples/1';
 import {TuiProgressBarExample2} from './examples/2';
@@ -13,6 +14,7 @@ import {ExampleProgressBarComponent} from './progress-bar.component';
         CommonModule,
         TuiAddonDocModule,
         TuiProgressBarModule,
+        TuiLinkModule,
         RouterModule.forChild(generateRoutes(ExampleProgressBarComponent)),
     ],
     declarations: [
