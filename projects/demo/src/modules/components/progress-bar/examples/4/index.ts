b9dc75166ee8c48df0170814ca18ddf505b5c4bb
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-09-07 15:52:33 +0300

    chore(demo): `ProgressBar` update example with label

--- projects/demo/src/modules/components/progress-bar/examples/4/index.ts
+++ projects/demo/src/modules/components/progress-bar/examples/4/index.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {TUI_IS_CYPRESS} from '@taiga-ui/cdk';
+import {of, timer} from 'rxjs';
+import {map, startWith, takeWhile} from 'rxjs/operators';
 import {changeDetection} from '../../../../../change-detection-strategy';
 import {encapsulation} from '../../../../../view-encapsulation';
 
@@ -10,5 +13,14 @@ import {encapsulation} from '../../../../../view-encapsulation';
     encapsulation,
 })
 export class TuiProgressBarExample4 {
-    value = 40;
+    readonly max = 100;
+    readonly value$ = this.isCypress
+        ? of(30)
+        : timer(300, 300).pipe(
+              map(i => i + 30),
+              startWith(30),
+              takeWhile(value => value <= this.max),
+          );
+
+    constructor(@Inject(TUI_IS_CYPRESS) private readonly isCypress: boolean) {}
 }
