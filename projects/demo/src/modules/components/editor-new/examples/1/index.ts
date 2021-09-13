340b5d517e37a78e50dd29ac70b8c2a4f78154ef
Vladimir <vladimir.potekh@gmail.com>
2021-09-13 13:36:45 +0300

    chore(addon-editor): improve example

--- projects/demo/src/modules/components/editor-new/examples/1/index.ts
+++ projects/demo/src/modules/components/editor-new/examples/1/index.ts
@@ -27,11 +27,14 @@ import {encapsulation} from '../../../../../view-encapsulation';
     encapsulation,
 })
 export class TuiEditorNewExample1 {
-    control = new FormControl('<p>Hello world</p>', Validators.required);
+    control = new FormControl(
+        '<p><strong>WYSIWYG</strong> (<em>What you see is what you get</em>) — <u>Rich Text Editor</u> for using with Angular<sup> forms.</sup></p><h1>Heading</h1><p>Lorem ipsum dolor sit amet consectetur <a target="_blank" rel="noopener noreferrer nofollow" href="http://taiga-ui.dev">adipiscing elit</a>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p><blockquote><p>ad minim veniam, quis nostrud exercitation <span style="color: rgb(196, 11, 8); background-color: rgb(221, 228, 237)">ullamco</span>, laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p></blockquote><p style="text-align: right">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><pre><code>export class EditorExample {}</code></pre><table><tbody><tr><th colspan="1" rowspan="1"><p></p></th><th colspan="1" rowspan="1"><p>Free</p></th><th colspan="1" rowspan="1"><p>Pro</p></th></tr><tr><td colspan="1" rowspan="1"><p>24/7 support</p></td><td colspan="1" rowspan="1"><p>+</p></td><td colspan="1" rowspan="1"><p>+</p></td></tr></tbody></table><p style="text-align: center"><code>Code in text</code></p><blockquote></blockquote>',
+        Validators.required,
+    );
 
     onClick() {
         this.control.setValue(
-            '<p>Control</p><h2>is</h2><h1><span style="color: #e01f19">updated!</span></h1>',
+            '<p>Control</p><h2>is</h2><h1><span style="color: #e01f19">updated</span></h1>',
         );
     }
 }
