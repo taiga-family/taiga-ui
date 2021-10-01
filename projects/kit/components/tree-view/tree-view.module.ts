5dfbd8328d1cbb731a32bfc6a254ebfb4b473e29
waterplea <alexander@inkin.ru>
2021-10-01 11:15:29 +0300

    chore: temporary commit

--- projects/kit/components/tree-view/tree-view.module.ts
+++ projects/kit/components/tree-view/tree-view.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {TuiExpandModule} from '@taiga-ui/core';
 import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
 import {TuiTreeViewComponent} from './tree-view.component';
 
 @NgModule({
-    imports: [CommonModule, PolymorpheusModule],
+    imports: [CommonModule, PolymorpheusModule, TuiExpandModule],
     declarations: [TuiTreeViewComponent],
     exports: [TuiTreeViewComponent],
 })
