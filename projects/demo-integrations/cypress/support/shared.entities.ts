e682646c6b7202fc90628ac238aa132029b6a87f
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-10-13 15:36:28 +0300

    fix(addon-editor): `Editor[new]` broken `ActiveZone` inside toolbar buttons (#845)
    * fix(addon-editor): `Editor[new]` broken `ActiveZone` inside toolbar buttons
    
    * chore(demo-integrations): `Editor[new]` new toolbar cypress test
    
    * chore(demo-integrations): move `EDITOR_PAGE_URL` to shared entries of tests
    
    * chore(addon-editor): small refactor in `Toolbar[new]`

--- projects/demo-integrations/cypress/support/shared.entities.ts
+++ projects/demo-integrations/cypress/support/shared.entities.ts
@@ -2,3 +2,4 @@ export const NIGHT_THEME_KEY = 'night';
 export const EXAMPLE_ID = 'tui-doc-example';
 export const DIALOG_PAGE_URL = 'components/dialog';
 export const DROPDOWN_CONTEXT_PAGE_URL = 'directives/dropdown-context';
+export const EDITOR_PAGE_URL = 'components/editor-new';
