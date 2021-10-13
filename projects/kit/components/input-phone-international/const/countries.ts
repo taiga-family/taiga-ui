0ec8271127c639ac5bd8a9dbf31f9efe58ac6c72
Ramil Valitov <ramilvalitov@gmail.com>
2021-10-13 14:46:56 +0300

    [fix] French number grouping

--- projects/kit/components/input-phone-international/const/countries.ts
+++ projects/kit/components/input-phone-international/const/countries.ts
@@ -86,7 +86,7 @@ export const COUNTRIES: Record<string, Country> = {
     [TuiCountryIsoCode.FJ]: {mask: '+679##-#####', name: 'Фиджи'},
     [TuiCountryIsoCode.FK]: {mask: '+500#####', name: 'Фолклендские острова'},
     [TuiCountryIsoCode.FM]: {mask: '+691###-####', name: 'Ф.Ш. Микронезии'},
-    [TuiCountryIsoCode.FR]: {mask: '+33(###) ###-###', name: 'Франция'},
+    [TuiCountryIsoCode.FR]: {mask: '+33 ## ## ## ## ##', name: 'Франция'},
     [TuiCountryIsoCode.GA]: {mask: '+241#-##-##-##', name: 'Габон'},
     [TuiCountryIsoCode.GD]: {mask: '+1(473) ###-####', name: 'Гренада'},
     [TuiCountryIsoCode.GE]: {mask: '+995(###) ###-###', name: 'Грузия'},
