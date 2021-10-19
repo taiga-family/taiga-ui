f7ed0f9d509e6d96ccf3daad4432bb77710f9cd1
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-10-19 17:33:06 +0300

    chore(addon-editor): change folder structure inside `addon-editor`

--- projects/addon-editor/components/color-selector/color-picker/color-picker.module.ts
+++ projects/addon-editor/components/color-selector/color-picker/color-picker.module.ts
@@ -1,8 +1,7 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {TuiFlatPickerModule} from '@taiga-ui/addon-editor/components/flat-picker';
-import {TuiLinearPickerModule} from '@taiga-ui/addon-editor/components/linear-picker';
-
+import {TuiFlatPickerModule} from '../flat-picker';
+import {TuiLinearPickerModule} from '../linear-picker';
 import {TuiColorPickerComponent} from './color-picker.component';
 
 @NgModule({
