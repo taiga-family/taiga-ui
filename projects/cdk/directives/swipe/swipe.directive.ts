068091b61ff16ad8300e6101c4dfdaeddb3d0f3d
Vladimir <vladimir.potekh@gmail.com>
2021-10-22 16:27:33 +0300

    chore: remove destroy service

--- projects/cdk/directives/swipe/swipe.directive.ts
+++ projects/cdk/directives/swipe/swipe.directive.ts
@@ -1,12 +1,11 @@
 import {Directive, Inject} from '@angular/core';
-import {TuiDestroyService} from '@taiga-ui/cdk/services';
 import {TuiSwipeService} from '@taiga-ui/cdk/services';
 
 // @dynamic
 @Directive({
     selector: '[tuiSwipe]',
     outputs: ['tuiSwipe'],
-    providers: [TuiDestroyService, TuiSwipeService],
+    providers: [TuiSwipeService],
 })
 export class TuiSwipeDirective {
     constructor(@Inject(TuiSwipeService) readonly tuiSwipe: TuiSwipeService) {}
