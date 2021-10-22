900198d378ecb8afd881b9c314e810cad504772e
Vladimir <vladimir.potekh@gmail.com>
2021-10-22 15:35:19 +0300

    chore: use DOCUMENT, remove takeUntill

--- projects/cdk/services/swipe.service.ts
+++ projects/cdk/services/swipe.service.ts
@@ -1,3 +1,4 @@
+import {DOCUMENT} from '@angular/common';
 import {ElementRef, Inject, Injectable} from '@angular/core';
 import {TuiSwipe, TuiSwipeOptions} from '@taiga-ui/cdk/interfaces';
 import {typedFromEvent} from '@taiga-ui/cdk/observables';
@@ -5,8 +6,7 @@ import {TUI_SWIPE_OPTIONS} from '@taiga-ui/cdk/tokens';
 import {isPresent} from '@taiga-ui/cdk/utils';
 import {getSwipeDirection} from '@taiga-ui/cdk/utils/miscellaneous';
 import {merge, Observable} from 'rxjs';
-import {filter, map, pairwise, takeUntil} from 'rxjs/operators';
-import {TuiDestroyService} from './destroy.service';
+import {filter, map, pairwise} from 'rxjs/operators';
 
 /**
  * @dynamic
@@ -16,18 +16,12 @@ export class TuiSwipeService extends Observable<TuiSwipe> {
     constructor(
         @Inject(ElementRef) {nativeElement}: ElementRef<Element>,
         @Inject(TUI_SWIPE_OPTIONS) {timeout, threshold}: TuiSwipeOptions,
-        @Inject(TuiDestroyService) destroy$: TuiDestroyService,
+        @Inject(DOCUMENT) documentRef: Document,
     ) {
-        const {ownerDocument} = nativeElement;
-
-        if (!ownerDocument) {
-            throw new Error('element does not have ownerDocument');
-        }
-
         super(subscriber => {
             merge(
                 typedFromEvent(nativeElement, 'touchstart', {passive: true}),
-                typedFromEvent(ownerDocument, 'touchend'),
+                typedFromEvent(documentRef, 'touchend'),
             )
                 .pipe(
                     pairwise(),
@@ -58,7 +52,6 @@ export class TuiSwipeService extends Observable<TuiSwipe> {
                         return null;
                     }),
                     filter(isPresent),
-                    takeUntil(destroy$),
                 )
                 .subscribe(subscriber);
         });
