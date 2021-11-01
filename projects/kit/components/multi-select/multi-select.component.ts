8eb4d1cb0b2f4791fe83efb6d50d7d7be42a9994
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-11-01 19:01:01 +0300

    fix(kit): `Select`, `ComboBox`, `MultiSelect` remove arrow icon if `disabled` or `readonly` (#955)
    * fix(kit): `Select` remove arrow icon if `disabled` or `readonly`
    
    * fix(kit): `ComboBox` remove arrow icon if `disabled` or `readonly`
    
    * fix(kit): `MultiSelect` remove arrow icon if `disabled` or `readonly`
    
    * chore(kit): delete `iconBlank`

--- projects/kit/components/multi-select/multi-select.component.ts
+++ projects/kit/components/multi-select/multi-select.component.ts
@@ -43,13 +43,11 @@ import {
     TuiDataListDirective,
     TuiDataListHost,
     TuiHostedDropdownComponent,
-    TuiSvgService,
     TuiTextfieldController,
     TuiTextfieldLabelOutsideDirective,
 } from '@taiga-ui/core';
 import {TuiStringifiableItem} from '@taiga-ui/kit/classes';
 import {TuiInputTagComponent} from '@taiga-ui/kit/components/input-tag';
-import {iconBlank} from '@taiga-ui/kit/constants';
 import {FIXED_DROPDOWN_CONTROLLER_PROVIDER} from '@taiga-ui/kit/providers';
 import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus';
 
@@ -141,15 +139,12 @@ export class TuiMultiSelectComponent<T>
         @Inject(NgControl)
         control: NgControl | null,
         @Inject(ChangeDetectorRef) changeDetectorRef: ChangeDetectorRef,
-        @Inject(TuiSvgService) svgService: TuiSvgService,
         @Inject(TUI_TEXTFIELD_LABEL_OUTSIDE)
         readonly textfieldLabelOutside: TuiTextfieldLabelOutsideDirective,
         @Inject(TUI_TEXTFIELD_WATCHED_CONTROLLER)
         readonly controller: TuiTextfieldController,
     ) {
         super(control, changeDetectorRef);
-
-        svgService.define({iconBlank});
     }
 
     get nativeFocusableElement(): HTMLInputElement | null {
@@ -174,10 +169,6 @@ export class TuiMultiSelectComponent<T>
 
     get searchString(): string {
         return this.search === null ? '' : this.search;
-    }
-
-    get tagIcon(): string {
-        return this.interactive ? 'iconBlank' : this.arrowIcon;
     }
 
     get interactive(): boolean {
