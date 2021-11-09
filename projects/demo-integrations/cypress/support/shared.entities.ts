0dabdf4ece5e95776a7449764a9e5d2d11c93069
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-11-09 10:09:08 +0300

    chore(demo-integrations): `MultiSelect` cypress test

--- projects/demo-integrations/cypress/support/shared.entities.ts
+++ projects/demo-integrations/cypress/support/shared.entities.ts
@@ -3,3 +3,4 @@ export const EXAMPLE_ID = 'tui-doc-example';
 export const DIALOG_PAGE_URL = 'components/dialog';
 export const DROPDOWN_CONTEXT_PAGE_URL = 'directives/dropdown-context';
 export const EDITOR_PAGE_URL = 'components/editor-new';
+export const MULTI_SELECT_PAGE_URL = 'components/multi-select';
