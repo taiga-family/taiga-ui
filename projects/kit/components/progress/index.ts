4e46d6168cc7d6180f0a3e8125683981d7595a4c
Maksim Ivanov <omaxphp@yandex.ru>
2021-11-18 13:07:59 +0300

    chore(kit): add TuiProgressColorSegmentsAsyncPipe (#1016)

--- projects/kit/components/progress/index.ts
+++ projects/kit/components/progress/index.ts
@@ -3,4 +3,6 @@ export * from './progress-bar/progress-bar.component';
 export * from './progress-circle/progress-circle.component';
 export * from './progress-segmented/progress-segmented.component';
 export * from './progress-label/progress-label.component';
+export * from './pipes/abstract-color-segments';
 export * from './pipes/progress-color-segments.pipe';
+export * from './pipes/progress-color-segments-async.pipe';
