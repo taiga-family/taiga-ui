fb7423d57f9aa27c7a61627a7c93421cae0b2a10
splincode <splincodewd@yandex.ru>
2021-11-18 15:23:34 +0300

    ci: migrate to @tinkoff/prettier-config/angular

--- prettier.config.js
+++ prettier.config.js
@@ -1,22 +1,13 @@
 module.exports = {
     ...require('@tinkoff/prettier-config/angular'),
-    printWidth: 90,
     overrides: [
+        {
+            files: ['*.js', '*.ts'],
+            options: {printWidth: 90, parser: 'typescript'},
+        },
         {
             files: '*.html',
             options: {printWidth: 80, parser: 'html'},
-        },
-        {
-            files: '*.md',
-            options: {printWidth: 120, parser: 'markdown'},
-        },
-        {
-            files: '*.svg',
-            options: {printWidth: 120, parser: 'xml'},
-        },
-        {
-            files: ['*.yml', '*.yaml'],
-            options: {printWidth: 120, parser: 'yaml'},
         },
     ],
 };
