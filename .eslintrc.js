f0b79ebcb5e8429d21e2107dc7ff382bd034793d
splincode <splincodewd@yandex.ru>
2021-12-08 19:49:33 +0300

    ci: upgrade @tinkoff/prettier-config/angular

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,33 +4,5 @@ module.exports = {
     extends: ['@tinkoff/eslint-config-angular'],
     rules: {
         '@typescript-eslint/no-useless-constructor': 'off',
-        // TODO(splincode): move to @tinkoff/eslint-config-angular
-        'no-case-declarations': 'error',
-        '@typescript-eslint/lines-between-class-members': [
-            'error',
-            'always',
-            {exceptAfterSingleLine: true, exceptAfterOverload: true},
-        ],
-        '@typescript-eslint/padding-line-between-statements': [
-            'error',
-            {blankLine: 'always', prev: '*', next: 'block'},
-            {blankLine: 'always', prev: 'block', next: '*'},
-            {blankLine: 'always', prev: '*', next: 'block-like'},
-            {blankLine: 'always', prev: 'block-like', next: '*'},
-            {blankLine: 'always', prev: '*', next: 'return'},
-            {blankLine: 'always', prev: 'directive', next: '*'},
-            {blankLine: 'always', prev: '*', next: ['interface', 'type']},
-            {blankLine: 'always', prev: ['const', 'let', 'var'], next: '*'},
-            {
-                blankLine: 'any',
-                prev: ['const', 'let', 'var', 'export'],
-                next: ['const', 'let', 'var', 'export'],
-            },
-            {blankLine: 'any', prev: '*', next: ['case', 'default']},
-            {blankLine: 'any', prev: ['case', 'default'], next: '*'},
-            {blankLine: 'any', prev: '*', next: 'class'},
-            {blankLine: 'any', prev: 'class', next: '*'},
-            {blankLine: 'any', prev: 'directive', next: 'directive'},
-        ],
     },
 };
--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,18 @@
 module.exports = {
     ...require('@tinkoff/prettier-config/angular'),
     overrides: [
+        ...require('@tinkoff/prettier-config/angular').overrides,
         {
             files: ['*.js', '*.ts'],
             options: {printWidth: 90, parser: 'typescript'},
         },
         {
-            files: '*.html',
+            files: ['*.html'],
             options: {printWidth: 80, parser: 'html'},
+        },
+        {
+            files: ['*.component.html', '*.template.html'],
+            options: {printWidth: 90, parser: 'angular'},
         },
     ],
 };
