06734a6901ae57dfed088be0581bdfca4ccb3269
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-12-27 18:47:58 +0300

    feat(kit): `InputDateTime` add full support of `TUI_DATE_FORMAT`

---
+++ projects/demo/src/modules/components/input-date-time/examples/3/index.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TUI_DATE_FORMAT, TUI_DATE_SEPARATOR, TuiDay, TuiTime} from '@taiga-ui/cdk';
+
+@Component({
+    selector: 'tui-input-date-time-example-3',
+    templateUrl: './index.html',
+    styleUrls: ['./index.less'],
+    providers: [
+        {provide: TUI_DATE_FORMAT, useValue: 'YMD'},
+        {provide: TUI_DATE_SEPARATOR, useValue: '/'},
+    ],
+    changeDetection,
+    encapsulation,
+})
+export class TuiInputDateTimeExample3 {
+    readonly control = new FormControl([new TuiDay(2017, 2, 15), new TuiTime(12, 30)]);
+}
