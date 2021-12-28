556a4b8523c08a2bb531744a025e2a5d987ddd6b
waterplea <alexander@inkin.ru>
2021-12-28 10:57:55 +0000

    Deploying to gh-pages from @ Tinkoff/taiga-ui@6ea8dd2e122a9083aec133d1c0c37c90ddb4d0f9 🚀

---
+++ next/245-es2015.497354704c9c707b1b8f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[245],{"Wb/1":function(n,i,t){"use strict";t.r(i),i.default="@import 'taiga-ui-local';\n\n.input {\n    width: 50%;\n\n    @media @mobile {\n        width: 100%;\n    }\n}\n"}}]);
