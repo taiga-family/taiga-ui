e74e2319154b109a1d102d10d12490b98d8349a9
limitofzero <limitofzero@gmail.com>
2022-01-07 14:21:30 +0300

    chore(core): add fn description

--- projects/core/utils/format/get-fractional-part-padded.ts
+++ projects/core/utils/format/get-fractional-part-padded.ts
@@ -1,5 +1,12 @@
 import {numberToStringWithoutExp} from './number-to-string-without-exp';
 
+/**
+ * Return fractional part of number
+ *
+ * @param value the number
+ * @param precision number of digits of decimal part, null to keep untouched
+ * @return the fractional part of number
+ */
 export function getFractionPartPadded(value: number, precision?: number | null): string {
     const [, fractionPartPadded = ''] = numberToStringWithoutExp(value).split('.');
 
