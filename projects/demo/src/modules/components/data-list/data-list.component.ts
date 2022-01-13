d348e34327033116b44acf22a6b14b1507477caa
waterplea <alexander@inkin.ru>
2022-01-13 11:50:10 +0300

    fix(kit): `Tree` fix change detection in async children

--- projects/demo/src/modules/components/data-list/data-list.component.ts
+++ projects/demo/src/modules/components/data-list/data-list.component.ts
@@ -1,49 +1,43 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 
-import {default as example1Html} from '!!raw-loader!./examples/1/index.html';
-import {default as example1Ts} from '!!raw-loader!./examples/1/index.ts';
-import {default as example2Html} from '!!raw-loader!./examples/2/index.html';
-import {default as example2Ts} from '!!raw-loader!./examples/2/index.ts';
-import {default as example3Html} from '!!raw-loader!./examples/3/index.html';
-import {default as example3Ts} from '!!raw-loader!./examples/3/index.ts';
-import {default as example4Component} from '!!raw-loader!./examples/4/custom-list/custom-list.component.ts';
-import {default as example4Template} from '!!raw-loader!./examples/4/custom-list/custom-list.template.html';
-import {default as example4Html} from '!!raw-loader!./examples/4/index.html';
-import {default as example4Ts} from '!!raw-loader!./examples/4/index.ts';
-import {default as exampleImportModule} from '!!raw-loader!./examples/import/import-module.txt';
-import {default as exampleInsertTemplate} from '!!raw-loader!./examples/import/insert-template.txt';
-
-import {FrontEndExample} from '../../interfaces/front-end-example';
-
 @Component({
     selector: 'example-tui-data-list',
     templateUrl: './data-list.template.html',
     changeDetection,
 })
 export class ExampleTuiDataListComponent {
-    readonly exampleImportModule = exampleImportModule;
-    readonly exampleInsertTemplate = exampleInsertTemplate;
+    readonly exampleImportModule = import(
+        '!!raw-loader!./examples/import/import-module.txt'
+    );
 
-    readonly example1: FrontEndExample = {
-        TypeScript: example1Ts,
-        HTML: example1Html,
+    readonly exampleInsertTemplate = import(
+        '!!raw-loader!./examples/import/insert-template.txt'
+    );
+
+    readonly example1 = {
+        TypeScript: import('!!raw-loader!./examples/1/index.ts'),
+        HTML: import('!!raw-loader!./examples/1/index.html'),
     };
 
-    readonly example2: FrontEndExample = {
-        TypeScript: example2Ts,
-        HTML: example2Html,
+    readonly example2 = {
+        TypeScript: import('!!raw-loader!./examples/2/index.ts'),
+        HTML: import('!!raw-loader!./examples/2/index.html'),
     };
 
-    readonly example3: FrontEndExample = {
-        TypeScript: example3Ts,
-        HTML: example3Html,
+    readonly example3 = {
+        TypeScript: import('!!raw-loader!./examples/3/index.ts'),
+        HTML: import('!!raw-loader!./examples/3/index.html'),
     };
 
     readonly example4 = {
-        TypeScript: example4Ts,
-        HTML: example4Html,
-        'custom-list.component.ts': example4Component,
-        'custom-list.template.html': example4Template,
+        TypeScript: import('!!raw-loader!./examples/4/index.ts'),
+        HTML: import('!!raw-loader!./examples/4/index.html'),
+        'custom-list.component.ts': import(
+            '!!raw-loader!./examples/4/custom-list/custom-list.component.ts'
+        ),
+        'custom-list.template.html': import(
+            '!!raw-loader!./examples/4/custom-list/custom-list.template.html'
+        ),
     };
 }
--- projects/demo/src/modules/components/tree/tree.component.ts
+++ projects/demo/src/modules/components/tree/tree.component.ts
@@ -1,81 +1,59 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 
-import {default as example1Html} from '!!raw-loader!./examples/1/index.html';
-import {default as example1Ts} from '!!raw-loader!./examples/1/index.ts';
-import {default as example2Html} from '!!raw-loader!./examples/2/index.html';
-import {default as example2Less} from '!!raw-loader!./examples/2/index.less';
-import {default as example2Ts} from '!!raw-loader!./examples/2/index.ts';
-import {default as example3Html} from '!!raw-loader!./examples/3/index.html';
-import {default as example3Less} from '!!raw-loader!./examples/3/index.less';
-import {default as example3Ts} from '!!raw-loader!./examples/3/index.ts';
-import {default as example4Html} from '!!raw-loader!./examples/4/index.html';
-import {default as example4Ts} from '!!raw-loader!./examples/4/index.ts';
-import {default as example5Content} from '!!raw-loader!./examples/5/content';
-import {default as example5ContentLess} from '!!raw-loader!./examples/5/content.less';
-import {default as example5Html} from '!!raw-loader!./examples/5/index.html';
-import {default as example5Less} from '!!raw-loader!./examples/5/index.less';
-import {default as example5Ts} from '!!raw-loader!./examples/5/index.ts';
-import {default as example6Html} from '!!raw-loader!./examples/6/index.html';
-import {default as example6Ts} from '!!raw-loader!./examples/6/index.ts';
-import {default as example7Html} from '!!raw-loader!./examples/7/index.html';
-import {default as example7Less} from '!!raw-loader!./examples/7/index.less';
-import {default as example7Ts} from '!!raw-loader!./examples/7/index.ts';
-import {default as example7Service} from '!!raw-loader!./examples/7/service';
-import {default as exampleImportModule} from '!!raw-loader!./examples/import/import-module.txt';
-import {default as exampleInsertTemplate} from '!!raw-loader!./examples/import/insert-template.txt';
-
-import {FrontEndExample} from '../../interfaces/front-end-example';
-
 @Component({
     selector: 'example-tree',
     templateUrl: './tree.template.html',
     changeDetection,
 })
 export class ExampleTuiTreeComponent {
-    readonly exampleImportModule = exampleImportModule;
+    readonly exampleImportModule = import(
+        '!!raw-loader!./examples/import/import-module.txt'
+    );
 
-    readonly exampleInsertTemplate = exampleInsertTemplate;
+    readonly exampleInsertTemplate = import(
+        '!!raw-loader!./examples/import/insert-template.txt'
+    );
 
-    readonly example1: FrontEndExample = {
-        TypeScript: example1Ts,
-        HTML: example1Html,
+    readonly example1 = {
+        TypeScript: import('!!raw-loader!./examples/1/index.ts'),
+        HTML: import('!!raw-loader!./examples/1/index.html'),
     };
 
-    readonly example2: FrontEndExample = {
-        TypeScript: example2Ts,
-        HTML: example2Html,
-        LESS: example2Less,
+    readonly example2 = {
+        TypeScript: import('!!raw-loader!./examples/2/index.ts'),
+        HTML: import('!!raw-loader!./examples/2/index.html'),
+        LESS: import('!!raw-loader!./examples/2/index.less'),
     };
 
-    readonly example3: FrontEndExample = {
-        TypeScript: example3Ts,
-        HTML: example3Html,
-        LESS: example3Less,
+    readonly example3 = {
+        TypeScript: import('!!raw-loader!./examples/3/index.ts'),
+        HTML: import('!!raw-loader!./examples/3/index.html'),
+        LESS: import('!!raw-loader!./examples/3/index.less'),
     };
 
-    readonly example4: FrontEndExample = {
-        TypeScript: example4Ts,
-        HTML: example4Html,
+    readonly example4 = {
+        TypeScript: import('!!raw-loader!./examples/4/index.ts'),
+        HTML: import('!!raw-loader!./examples/4/index.html'),
     };
 
     readonly example5 = {
-        TypeScript: example5Ts,
-        HTML: example5Html,
-        LESS: example5Less,
-        'content.ts': example5Content,
-        'content.less': example5ContentLess,
+        TypeScript: import('!!raw-loader!./examples/5/index.ts'),
+        HTML: import('!!raw-loader!./examples/5/index.html'),
+        LESS: import('!!raw-loader!./examples/5/index.less'),
+        'content.ts': import('!!raw-loader!./examples/5/content'),
+        'content.less': import('!!raw-loader!./examples/5/content.less'),
     };
 
-    readonly example6: FrontEndExample = {
-        TypeScript: example6Ts,
-        HTML: example6Html,
+    readonly example6 = {
+        TypeScript: import('!!raw-loader!./examples/6/index.ts'),
+        HTML: import('!!raw-loader!./examples/6/index.html'),
     };
 
     readonly example7 = {
-        TypeScript: example7Ts,
-        HTML: example7Html,
-        LESS: example7Less,
-        'service.ts': example7Service,
+        TypeScript: import('!!raw-loader!./examples/7/index.ts'),
+        HTML: import('!!raw-loader!./examples/7/index.html'),
+        LESS: import('!!raw-loader!./examples/7/index.less'),
+        'service.ts': import('!!raw-loader!./examples/7/service'),
     };
 }
