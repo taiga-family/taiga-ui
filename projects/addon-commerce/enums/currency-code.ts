23f8e4e3c5672e326899410b7f1ba15ad63a50a4
Sergey Vakhramov <vakhramoff@xakep.ru>
2022-01-18 12:46:21 +0500

    chore(addon-commerce): provide descriptions for TuiCurrencyCode (#1198)

--- projects/addon-commerce/enums/currency-code.ts
+++ projects/addon-commerce/enums/currency-code.ts
@@ -1,21 +1,180 @@
+/**
+ * Active ISO 4217 currency codes
+ *
+ * @description The ISO 4217 classification is used to describe alphabetic and numeric currency codes.
+ * @description Alphabetic code is also called "alfa-3" and the numeric one — "number-3"
+ *
+ * @link https://en.wikipedia.org/wiki/ISO_4217
+ */
 export const enum TuiCurrencyCode {
+    /**
+     * Russian ruble
+     *
+     * @description Alphabetic code: 643
+     * @description Numeric code: RUB
+     * @description Countries and territories: Russia
+     */
     Ruble = '643',
+
+    /**
+     * Евро
+     *
+     * @description Alphabetic currency code: 978
+     * @description Numeric code: EUR
+     * @description Countries and territories: Åland Islands (AX), European Union (EU), Andorra (AD), Austria (AT), Belgium (BE), Cyprus (CY), Estonia (EE), Finland (FI), France (FR), French Southern and Antarctic Lands (TF), Germany (DE), Greece (GR), Guadeloupe (GP), Ireland (IE), Italy (IT), Latvia (LV), Lithuania (LT), Luxembourg (LU), Malta (MT), French Guiana (GF), Martinique (MQ), Mayotte (YT), Monaco (MC), Montenegro (ME), Netherlands (NL), Portugal (PT), Réunion (RE), Saint Barthélemy (BL), Saint Martin (MF), Saint Pierre and Miquelon (PM), San Marino (SM), Slovakia (SK), Slovenia (SI), Spain (ES), Vatican City (VA)
+     */
     Euro = '978',
+
+    /**
+     * United States dollar
+     *
+     * @description Alphabetic currency code: 840
+     * @description Numeric code: USD
+     * @description Countries and territories: United States, American Samoa (AS), British Indian Ocean Territory (IO) (also uses GBP), British Virgin Islands (VG), Caribbean Netherlands (BQ – Bonaire, Sint Eustatius and Saba), Ecuador (EC), El Salvador (SV), Guam (GU), Marshall Islands (MH), Federated States of Micronesia (FM), Northern Mariana Islands (MP), Palau (PW), Panama (PA) (as well as Panamanian Balboa), Puerto Rico (PR), Timor-Leste (TL), Turks and Caicos Islands (TC), U.S. Virgin Islands (VI), United States Minor Outlying Islands (UM)
+     */
     Dollar = '840',
+
+    /**
+     * Pound sterling
+     *
+     * @description Alphabetic currency code: 826
+     * @description Numeric code: GBP
+     * @description Countries and territories: United Kingdom, Isle of Man (IM, see Manx pound), Jersey (JE, see Jersey pound), Guernsey (GG, see Guernsey pound), Tristan da Cunha (SH-TA)
+     */
     Pound = '826',
+
+    /**
+     * Thai baht
+     *
+     * @description Alphabetic currency code: 764
+     * @description Numeric code: THB
+     * @description Countries and territories: Thailand
+     */
     Baht = '764',
+
+    /**
+     * Turkish lira
+     *
+     * @description Alphabetic currency code: 949
+     * @description Numeric code: TRY
+     * @description Countries and territories: Turkey
+     */
     TurkishLira = '949',
+
+    /**
+     * Chinese yuan
+     *
+     * @description Alphabetic currency code: 156
+     * @description Numeric code: CNY
+     * @description Countries and territories: China
+     */
     YuanRenminbi = '156',
+
+    /**
+     * Kazakhstani tenge
+     *
+     * @description Alphabetic currency code: 398
+     * @description Numeric code: KZT
+     * @description Countries and territories: Kazakhstan
+     */
     Tenge = '398',
+
+    /**
+     * Israeli new shekel
+     *
+     * @description Alphabetic currency code: 376
+     * @description Numeric code: ILS
+     * @description Countries and territories: Israel
+     */
     IsraeliShekel = '376',
+
+    /**
+     * Indian rupee
+     *
+     * @description Alphabetic currency code: 356
+     * @description Numeric code: INR
+     * @description Countries and territories: India, Bhutan
+     */
     IndianRupee = '356',
+
+    /**
+     * Japanese yen
+     *
+     * @description Alphabetic currency code: 392
+     * @description Numeric code: JPY
+     * @description Countries and territories: Japan
+     */
     Yen = '392',
+
+    /**
+     * South Korean won
+     *
+     * @description Alphabetic currency code: 410
+     * @description Numeric code: KRW
+     * @description Countries and territories: South Korea
+     */
     Won = '410',
+
+    /**
+     * Swiss franc
+     *
+     * @description Alphabetic currency code: 756
+     * @description Numeric code: CHF
+     * @description Countries and territories: Switzerland, Liechtenstein (LI)
+     */
     SwissFranc = '756',
+
+    /**
+     * Singapore dollar
+     *
+     * @description Alphabetic currency code: 702
+     * @description Numeric code: SGD
+     * @description Countries and territories: Singapore
+     */
     SingaporeDollar = '702',
+
+    /**
+     * Australian dollar
+     *
+     * @description Alphabetic currency code: 036
+     * @description Numeric code: AUD
+     * @description Countries and territories: Australia, Christmas Island (CX), Cocos (Keeling) Islands (CC), Heard Island and McDonald Islands (HM), Kiribati (KI), Nauru (NR), Norfolk Island (NF), Tuvalu (TV)
+     */
     AustralianDollar = '036',
+
+    /**
+     * Hong Kong dollar
+     *
+     * @description Alphabetic currency code: 344
+     * @description Numeric code: HKD
+     * @description Countries and territories: Hong Kong
+     */
     HongKong_dollar = '344',
+
+    /**
+     * Canadian dollar
+     *
+     * @description Alphabetic currency code: 124
+     * @description Numeric code: CAD
+     * @description Countries and territories: Canada
+     */
     CanadianDollar = '124',
+
+    /**
+     * Armenian dram
+     *
+     * @description Alphabetic currency code: 051
+     * @description Numeric code: AMD
+     * @description Countries and territories: Armenia
+     */
     ArmenianDram = '051',
+
+    /**
+     * Ukrainian hryvnia
+     *
+     * @description Alphabetic currency code: 980
+     * @description Numeric code: UAH
+     * @description Countries and territories: Ukraine
+     */
     Hryvnia = '980',
 }
