d7e798a9d4d48b6446059eb242cefed04477bad3
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-01-20 10:45:29 +0300

    chore(kit): fix select icon (#1211)
    * chore(kit): fix select icon
    
    * chore: add e2e
    
    * chore: change test

--- projects/demo-integrations/cypress/support/shared.entities.ts
+++ projects/demo-integrations/cypress/support/shared.entities.ts
@@ -4,3 +4,4 @@ export const DIALOG_PAGE_URL = 'components/dialog';
 export const DROPDOWN_CONTEXT_PAGE_URL = 'directives/dropdown-context';
 export const EDITOR_PAGE_URL = 'components/editor-new';
 export const MULTI_SELECT_PAGE_URL = 'components/multi-select';
+export const SELECT_PAGE_URL = 'components/select';
