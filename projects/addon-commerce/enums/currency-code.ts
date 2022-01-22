7e2508f860d4b6556ccef1839643443619db9957
Sergey Vakhramov <vakhramoff@xakep.ru>
2022-01-22 12:54:19 +0500

    fix(addon-commerce): corrected mistakes in currency codes description

--- projects/addon-commerce/enums/currency-code.ts
+++ projects/addon-commerce/enums/currency-code.ts
@@ -1,8 +1,8 @@
 /**
- * Active ISO 4217 currency codes
+ * **Active ISO 4217 numeric currency codes**
  *
  * @description The ISO 4217 classification is used to describe alphabetic and numeric currency codes.
- * @description Alphabetic code is also called "alfa-3" and the numeric one — "number-3"
+ * @description Numeric code is also called "number-3".
  *
  * @link https://en.wikipedia.org/wiki/ISO_4217
  */
@@ -10,17 +10,17 @@ export const enum TuiCurrencyCode {
     /**
      * Russian ruble
      *
-     * @description Alphabetic code: 643
-     * @description Numeric code: RUB
+     * @description Numeric code: 643
+     * @description Alphabetic code: RUB
      * @description Countries and territories: Russia
      */
     Ruble = '643',
 
     /**
-     * Евро
+     * Euro
      *
-     * @description Alphabetic currency code: 978
-     * @description Numeric code: EUR
+     * @description Numeric code: 978
+     * @description Alphabetic code: EUR
      * @description Countries and territories: Åland Islands (AX), European Union (EU), Andorra (AD), Austria (AT), Belgium (BE), Cyprus (CY), Estonia (EE), Finland (FI), France (FR), French Southern and Antarctic Lands (TF), Germany (DE), Greece (GR), Guadeloupe (GP), Ireland (IE), Italy (IT), Latvia (LV), Lithuania (LT), Luxembourg (LU), Malta (MT), French Guiana (GF), Martinique (MQ), Mayotte (YT), Monaco (MC), Montenegro (ME), Netherlands (NL), Portugal (PT), Réunion (RE), Saint Barthélemy (BL), Saint Martin (MF), Saint Pierre and Miquelon (PM), San Marino (SM), Slovakia (SK), Slovenia (SI), Spain (ES), Vatican City (VA)
      */
     Euro = '978',
@@ -28,8 +28,8 @@ export const enum TuiCurrencyCode {
     /**
      * United States dollar
      *
-     * @description Alphabetic currency code: 840
-     * @description Numeric code: USD
+     * @description Numeric code: 840
+     * @description Alphabetic code: USD
      * @description Countries and territories: United States, American Samoa (AS), British Indian Ocean Territory (IO) (also uses GBP), British Virgin Islands (VG), Caribbean Netherlands (BQ – Bonaire, Sint Eustatius and Saba), Ecuador (EC), El Salvador (SV), Guam (GU), Marshall Islands (MH), Federated States of Micronesia (FM), Northern Mariana Islands (MP), Palau (PW), Panama (PA) (as well as Panamanian Balboa), Puerto Rico (PR), Timor-Leste (TL), Turks and Caicos Islands (TC), U.S. Virgin Islands (VI), United States Minor Outlying Islands (UM)
      */
     Dollar = '840',
@@ -37,8 +37,8 @@ export const enum TuiCurrencyCode {
     /**
      * Pound sterling
      *
-     * @description Alphabetic currency code: 826
-     * @description Numeric code: GBP
+     * @description Numeric code: 826
+     * @description Alphabetic code: GBP
      * @description Countries and territories: United Kingdom, Isle of Man (IM, see Manx pound), Jersey (JE, see Jersey pound), Guernsey (GG, see Guernsey pound), Tristan da Cunha (SH-TA)
      */
     Pound = '826',
@@ -46,8 +46,8 @@ export const enum TuiCurrencyCode {
     /**
      * Thai baht
      *
-     * @description Alphabetic currency code: 764
-     * @description Numeric code: THB
+     * @description Numeric code: 764
+     * @description Alphabetic code: THB
      * @description Countries and territories: Thailand
      */
     Baht = '764',
@@ -55,8 +55,8 @@ export const enum TuiCurrencyCode {
     /**
      * Turkish lira
      *
-     * @description Alphabetic currency code: 949
-     * @description Numeric code: TRY
+     * @description Numeric code: 949
+     * @description Alphabetic code: TRY
      * @description Countries and territories: Turkey
      */
     TurkishLira = '949',
@@ -64,8 +64,8 @@ export const enum TuiCurrencyCode {
     /**
      * Chinese yuan
      *
-     * @description Alphabetic currency code: 156
-     * @description Numeric code: CNY
+     * @description Numeric code: 156
+     * @description Alphabetic code: CNY
      * @description Countries and territories: China
      */
     YuanRenminbi = '156',
@@ -73,8 +73,8 @@ export const enum TuiCurrencyCode {
     /**
      * Kazakhstani tenge
      *
-     * @description Alphabetic currency code: 398
-     * @description Numeric code: KZT
+     * @description Numeric code: 398
+     * @description Alphabetic code: KZT
      * @description Countries and territories: Kazakhstan
      */
     Tenge = '398',
@@ -82,8 +82,8 @@ export const enum TuiCurrencyCode {
     /**
      * Israeli new shekel
      *
-     * @description Alphabetic currency code: 376
-     * @description Numeric code: ILS
+     * @description Numeric code: 376
+     * @description Alphabetic code: ILS
      * @description Countries and territories: Israel
      */
     IsraeliShekel = '376',
@@ -91,8 +91,8 @@ export const enum TuiCurrencyCode {
     /**
      * Indian rupee
      *
-     * @description Alphabetic currency code: 356
-     * @description Numeric code: INR
+     * @description Numeric code: 356
+     * @description Alphabetic code: INR
      * @description Countries and territories: India, Bhutan
      */
     IndianRupee = '356',
@@ -100,8 +100,8 @@ export const enum TuiCurrencyCode {
     /**
      * Japanese yen
      *
-     * @description Alphabetic currency code: 392
-     * @description Numeric code: JPY
+     * @description Numeric code: 392
+     * @description Alphabetic code: JPY
      * @description Countries and territories: Japan
      */
     Yen = '392',
@@ -109,8 +109,8 @@ export const enum TuiCurrencyCode {
     /**
      * South Korean won
      *
-     * @description Alphabetic currency code: 410
-     * @description Numeric code: KRW
+     * @description Numeric code: 410
+     * @description Alphabetic code: KRW
      * @description Countries and territories: South Korea
      */
     Won = '410',
@@ -118,8 +118,8 @@ export const enum TuiCurrencyCode {
     /**
      * Swiss franc
      *
-     * @description Alphabetic currency code: 756
-     * @description Numeric code: CHF
+     * @description Numeric code: 756
+     * @description Alphabetic code: CHF
      * @description Countries and territories: Switzerland, Liechtenstein (LI)
      */
     SwissFranc = '756',
@@ -127,8 +127,8 @@ export const enum TuiCurrencyCode {
     /**
      * Singapore dollar
      *
-     * @description Alphabetic currency code: 702
-     * @description Numeric code: SGD
+     * @description Numeric code: 702
+     * @description Alphabetic code: SGD
      * @description Countries and territories: Singapore
      */
     SingaporeDollar = '702',
@@ -136,8 +136,8 @@ export const enum TuiCurrencyCode {
     /**
      * Australian dollar
      *
-     * @description Alphabetic currency code: 036
-     * @description Numeric code: AUD
+     * @description Numeric code: 036
+     * @description Alphabetic code: AUD
      * @description Countries and territories: Australia, Christmas Island (CX), Cocos (Keeling) Islands (CC), Heard Island and McDonald Islands (HM), Kiribati (KI), Nauru (NR), Norfolk Island (NF), Tuvalu (TV)
      */
     AustralianDollar = '036',
@@ -145,8 +145,8 @@ export const enum TuiCurrencyCode {
     /**
      * Hong Kong dollar
      *
-     * @description Alphabetic currency code: 344
-     * @description Numeric code: HKD
+     * @description Numeric code: 344
+     * @description Alphabetic code: HKD
      * @description Countries and territories: Hong Kong
      */
     HongKong_dollar = '344',
@@ -154,8 +154,8 @@ export const enum TuiCurrencyCode {
     /**
      * Canadian dollar
      *
-     * @description Alphabetic currency code: 124
-     * @description Numeric code: CAD
+     * @description Numeric code: 124
+     * @description Alphabetic code: CAD
      * @description Countries and territories: Canada
      */
     CanadianDollar = '124',
@@ -163,8 +163,8 @@ export const enum TuiCurrencyCode {
     /**
      * Armenian dram
      *
-     * @description Alphabetic currency code: 051
-     * @description Numeric code: AMD
+     * @description Numeric code: 051
+     * @description Alphabetic code: AMD
      * @description Countries and territories: Armenia
      */
     ArmenianDram = '051',
@@ -172,8 +172,8 @@ export const enum TuiCurrencyCode {
     /**
      * Ukrainian hryvnia
      *
-     * @description Alphabetic currency code: 980
-     * @description Numeric code: UAH
+     * @description Numeric code: 980
+     * @description Alphabetic code: UAH
      * @description Countries and territories: Ukraine
      */
     Hryvnia = '980',
