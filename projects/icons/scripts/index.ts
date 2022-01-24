7e9f42b816ea43c3978af9c4dd5b4dc2f545a75e
Maksim Ivanov <omaxphp@yandex.ru>
2022-01-24 08:23:59 +0100

    feat(icons): expose `processIcons` utility (#1102)

--- projects/icons/scripts/index.ts
+++ projects/icons/scripts/index.ts
@@ -2,4 +2,5 @@ export * from './convert-all-compile-file-to-all-file';
 export * from './delete-all-to-compile-file';
 export * from './post-prettier-format';
 export * from './prepare-all-to-compile-file';
+export * from './process-icons';
 export * from './rollup-svgo';
