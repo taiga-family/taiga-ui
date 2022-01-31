b894373db83a1f7d0df929d3b3d82a2332e5bcbc
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-01-31 16:06:28 +0300

    fix(kit): fix cyrillic filler in date-components (#1271)

--- projects/cdk/date-time/date-separator.ts
+++ projects/cdk/date-time/date-separator.ts
@@ -8,4 +8,4 @@ export const TUI_DATE_SEPARATOR = new InjectionToken<string>(
 );
 
 export const changeDateSeparator = (dateString: string, newDateSeparator: string) =>
-    dateString.replace(/[^0-9a-z]/gi, newDateSeparator);
+    dateString.replace(/[^0-9A-Za-zА-Яа-я]/gi, newDateSeparator);
