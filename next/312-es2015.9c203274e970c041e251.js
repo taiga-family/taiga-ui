bde6edd7c24ae367e95c516e3234ce327536d546
splincode <omaxphp@yandex.ru>
2022-01-31 16:04:49 +0000

    Deploying to gh-pages from @ Tinkoff/taiga-ui@d18d1920432ba252085530af78f872907d93d33f 🚀

--- next/312-es2015.9c203274e970c041e251.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[312],{"EQ8+":function(i,n,o){"use strict";o.r(n),n.default="import {FormsModule} from '@angular/forms';\nimport {TuiPrimitiveTextfieldModule} from '@taiga-ui/core';\n\n...\n\n@NgModule({\n    imports: [\n        ...\n        FormsModule,\n        TuiPrimitiveTextfieldModule\n    ],\n...\n"}}]);
--- next/313-es2015.338387a49e6a69c20b1c.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[313],{pxLj:function(i,e,t){"use strict";t.r(e),e.default='<tui-primitive-textfield [(value)]="value">\n    Type something\n</tui-primitive-textfield>\n'}}]);
