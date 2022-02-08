a18b2576cfd4f6104d2ec1e101e254b6e1c7c844
waterplea <alexander@inkin.ru>
2022-02-08 10:46:37 +0300

    chore: fix build

--- projects/kit/providers/value-accessor.provider.ts
+++ projects/kit/providers/value-accessor.provider.ts
@@ -3,8 +3,11 @@ import {NG_VALUE_ACCESSOR} from '@angular/forms';
 import {TUI_VALUE_ACCESSOR} from '@taiga-ui/core/tokens';
 import {identity} from 'rxjs';
 
+// TODO: remove in ivy compilation
+export const IDENTITY = identity;
+
 export const TUI_VALUE_ACCESSOR_PROVIDER: FactoryProvider = {
     provide: TUI_VALUE_ACCESSOR,
     deps: [[new Optional(), new Self(), NG_VALUE_ACCESSOR]],
-    useFactory: identity,
+    useFactory: IDENTITY,
 };
