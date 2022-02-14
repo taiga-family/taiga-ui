a12e31f5d4450d754b3373c79a913e2643217f4d
Maksim Ivanov <omaxphp@yandex.ru>
2022-02-14 17:44:14 +0300

    feat(cdk): support no arguments' function decorated by tuiPure (#1374)

--- projects/cdk/decorators/test/pure.spec.ts
+++ projects/cdk/decorators/test/pure.spec.ts
@@ -58,4 +58,49 @@ describe('tuiPure', () => {
 
         expect(testObject.combine(name, age)).toBe(result);
     });
+
+    it('memoize function without arguments', () => {
+        class TestClass {
+            sideEffect = 0;
+
+            get range() {
+                return this.getRange();
+            }
+
+            @tuiPure
+            getRange() {
+                this.sideEffect++;
+
+                return Array.from({length: 5}, (_item, index: number) => index + 1);
+            }
+        }
+
+        const testObject = new TestClass();
+
+        expect(testObject.range).toEqual([1, 2, 3, 4, 5]);
+        expect(testObject.range).toEqual([1, 2, 3, 4, 5]);
+        expect(testObject.range).toEqual([1, 2, 3, 4, 5]);
+        expect(testObject.sideEffect).toEqual(1);
+    });
+
+    it('memoize function without arguments and return undefined by default', () => {
+        class TestClass {
+            sideEffect = 0;
+
+            @tuiPure
+            voidFn() {
+                this.sideEffect++;
+
+                // necessary for the test
+                return undefined;
+            }
+        }
+
+        const testObject = new TestClass();
+
+        expect(testObject.voidFn()).toEqual(undefined);
+        expect(testObject.voidFn()).toEqual(undefined);
+        expect(testObject.voidFn()).toEqual(undefined);
+        expect(testObject.sideEffect).toEqual(1);
+    });
 });
