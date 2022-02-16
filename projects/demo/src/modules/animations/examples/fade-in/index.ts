bfebaaf5c3c8245f062b482456a60043883f9521
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-02-16 15:30:02 +0300

    chore(demo): new demo page `Animations` (#822)
    * chore(demo): new demo page `Animations` (#684)
    
    * chore(demo): `Animations` add `tuiPure` to examples

---
+++ projects/demo/src/modules/animations/examples/fade-in/index.ts
@@ -0,0 +1,34 @@
+import {Component, Input} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {tuiPure} from '@taiga-ui/cdk';
+import {tuiFadeIn} from '@taiga-ui/core';
+import {from, of} from 'rxjs';
+import {concatMap, delay, repeat, startWith} from 'rxjs/operators';
+
+@Component({
+    selector: 'tui-fade-in-example',
+    templateUrl: './index.html',
+    styleUrls: ['./index.less'],
+    changeDetection,
+    encapsulation,
+    animations: [tuiFadeIn],
+})
+export class TuiFadeInExample {
+    @Input()
+    speed = 0;
+
+    isShown$ = from([false, true]).pipe(
+        concatMap(val => of(val).pipe(delay(1.5 * this.speed))),
+        repeat(),
+        startWith(true),
+    );
+
+    @tuiPure
+    getAnimation(duration: number) {
+        return {
+            value: '',
+            params: {duration},
+        } as const;
+    }
+}
