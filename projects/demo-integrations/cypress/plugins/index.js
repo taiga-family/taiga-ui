c2b1968b0d46e630fe7053b834f29bf6942e4263
Maksim Ivanov <omaxphp@yandex.ru>
2022-02-16 13:11:45 +0300

    ci: support `high-dpi-support` for e2e (#1404)

--- projects/demo-integrations/cypress/plugins/index.js
+++ projects/demo-integrations/cypress/plugins/index.js
@@ -6,8 +6,9 @@ module.exports = (on, config) => {
     on('before:browser:launch', (browser, launchOptions) => {
         if (browser.name === 'chrome') {
             launchOptions.args.push('--disable-dev-shm-usage');
-            launchOptions.args.push('--force-device-scale-factor=1');
+            launchOptions.args.push('--force-device-scale-factor=2');
             launchOptions.args.push('--start-fullscreen');
+            launchOptions.args.push('--high-dpi-support=1');
         }
 
         if (browser.isHeadless) {
