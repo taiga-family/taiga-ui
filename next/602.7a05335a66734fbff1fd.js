be07a553d7a104c7617da2002fe44625685e53f9
waterplea <alexander@inkin.ru>
2022-02-21 11:54:39 +0000

    Deploying to gh-pages from @ Tinkoff/taiga-ui@010c1b41f52d0edb040c5f268d709a8df24a5b3e 🚀

---
+++ next/602.7a05335a66734fbff1fd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[602],{s8uW:function(n,l,s){"use strict";s.r(l),l.default='<input\n    tuiSlider\n    type="range"\n    max="4"\n    size="m"\n    [segments]="4"\n    [formControl]="formControl"\n/>\n\n<div class="labels">\n    <span *ngFor="let label of labels" class="label">${{label}}</span>\n</div>\n'}}]);
