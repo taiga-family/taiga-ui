8fffb27dc225feea827485aa0a9a20a44ab7fcc0
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-02-24 11:45:32 +0300

    feat(kit): `TUI_SLIDER_OPTIONS` (make `Slider` more customizable)

--- projects/kit/components/slider/index.ts
+++ projects/kit/components/slider/index.ts
@@ -2,3 +2,4 @@ export * from './slider.component';
 export * from './slider.module';
 export * from './slider-key-steps.directive';
 export * from './slider-old.component';
+export * from './slider-options';
