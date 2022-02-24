29619f692d22249d7035a8ffcd092ebae17ab178
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-02-24 12:53:43 +0300

    chore(kit): `TUI_SLIDER_OPTIONS` add `tuiSliderOptionsProvider`

--- projects/kit/components/slider/slider-options.ts
+++ projects/kit/components/slider/slider-options.ts
@@ -1,4 +1,4 @@
-import {InjectionToken} from '@angular/core';
+import {InjectionToken, ValueProvider} from '@angular/core';
 import {TuiSizeS} from '@taiga-ui/core';
 
 export interface TuiSliderOptions {
@@ -15,3 +15,10 @@ export const TUI_SLIDER_OPTIONS = new InjectionToken<TuiSliderOptions>(
     'Default parameters for Slider component',
     {factory: () => TUI_SLIDER_DEFAULT_OPTIONS},
 );
+
+export const tuiSliderOptionsProvider = (
+    options: Partial<TuiSliderOptions>,
+): ValueProvider => ({
+    provide: TUI_SLIDER_OPTIONS,
+    useValue: {...TUI_SLIDER_DEFAULT_OPTIONS, ...options},
+});
