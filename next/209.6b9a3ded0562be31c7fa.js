592d6318f10836874259ffcd9eddbee946cc15a6
splincode <omaxphp@yandex.ru>
2022-02-25 13:15:25 +0000

    Deploying to gh-pages from @ Tinkoff/taiga-ui@3884212593b83318fa5f3732afa833169118e51f 🚀

---
+++ next/209.6b9a3ded0562be31c7fa.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[209],{mua0:function(n,s,i){"use strict";i.r(s),s.default='<div class="poster">\n    <h3 *ngIf="isShown$ | async" [@tuiFadeIn]="getAnimation(speed)">\n        STOP KILLING VOLCANOES TO MAKE LAVA LAMPS\n    </h3>\n</div>\n'}}]);
