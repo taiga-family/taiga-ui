3a052eb69c3901ec5c419d92446459ad5c0f2c61
Aleksandr Shatilov <ashatilovdev@gmail.com>
2022-02-25 21:14:39 +0300

    feat(cdk):  TUI_IS_IOS USER_AGENT add new condition IOS devices
    feat(cdk):  TUI_IS_IOS USER_AGENT add new condition IOS devices
    
    Co-Authored-By: AnteaterKit <ashatilovdev@gmail.com>

--- projects/cdk/tokens/is-ios.ts
+++ projects/cdk/tokens/is-ios.ts
@@ -1,8 +1,12 @@
 import {inject, InjectionToken} from '@angular/core';
-import {USER_AGENT} from '@ng-web-apis/common';
+import {NAVIGATOR, USER_AGENT} from '@ng-web-apis/common';
 
 const IOS_REG_EXP = /ipad|iphone|ipod/;
+const APPLE_REG_EXP = /apple/;
 
 export const TUI_IS_IOS = new InjectionToken<boolean>('iOS browser detection', {
-    factory: () => IOS_REG_EXP.test(inject(USER_AGENT).toLowerCase()),
+    factory: () =>
+        IOS_REG_EXP.test(inject(USER_AGENT).toLowerCase()) ||
+        (APPLE_REG_EXP.test(inject(USER_AGENT).toLowerCase()) &&
+            inject(NAVIGATOR).maxTouchPoints > 1),
 });
