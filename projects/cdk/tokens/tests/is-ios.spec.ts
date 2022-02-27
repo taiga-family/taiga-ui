e36e2dd3af0bc45e45943a7e298cd3d91762f203
Aleksandr Shatilov <ashatilovdev@gmail.com>
2022-02-27 20:25:27 +0300

    feat(cdk): tui is ios change tests
    feat(cdk): tui is ios change tests
    
    Co-Authored-By: AnteaterKit <ashatilovdev@gmail.com>

--- projects/cdk/tokens/tests/is-ios.spec.ts
+++ projects/cdk/tokens/tests/is-ios.spec.ts
@@ -4,64 +4,66 @@ import {configureTestSuite} from '@taiga-ui/testing';
 
 import {TUI_IS_IOS} from '../is-ios';
 
-describe('TUI_IS_IOS basic is IOS positive', () => {
-    configureTestSuite(() => {
-        TestBed.configureTestingModule({
-            providers: [
-                {
-                    provide: USER_AGENT,
-                    useValue:
-                        'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1',
-                },
-            ],
+describe('TUI_IS_IOS', () => {
+    describe('basic is IOS positive', () => {
+        configureTestSuite(() => {
+            TestBed.configureTestingModule({
+                providers: [
+                    {
+                        provide: USER_AGENT,
+                        useValue:
+                            'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1',
+                    },
+                ],
+            });
+        });
+
+        it('return true if iPhone', () => {
+            expect(TestBed.inject(TUI_IS_IOS)).toBeTrue();
         });
     });
 
-    it('TUI_IS_IOS is iPhone', () => {
-        expect(TestBed.inject(TUI_IS_IOS)).toBeTrue();
-    });
-});
+    describe('Mac OS mobile positive', () => {
+        configureTestSuite(() => {
+            TestBed.configureTestingModule({
+                providers: [
+                    {
+                        provide: USER_AGENT,
+                        useValue:
+                            'Safari: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15',
+                    },
+                    {
+                        provide: NAVIGATOR,
+                        useValue: {maxTouchPoints: 2},
+                    },
+                ],
+            });
+        });
 
-describe('TUI_IS_IOS apple iPadOS/iPhone positive', () => {
-    configureTestSuite(() => {
-        TestBed.configureTestingModule({
-            providers: [
-                {
-                    provide: USER_AGENT,
-                    useValue:
-                        'Safari: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15',
-                },
-                {
-                    provide: NAVIGATOR,
-                    useValue: {maxTouchPoints: 2},
-                },
-            ],
+        it('return true if apple and maxTouchPoints > 1', () => {
+            expect(TestBed.inject(TUI_IS_IOS)).toBeTrue();
         });
     });
 
-    it('TUI_IS_IOS is IOS', () => {
-        expect(TestBed.inject(TUI_IS_IOS)).toBeTrue();
-    });
-});
-
-describe('TUI_IS_IOS, MacOS Desktop, negative', () => {
-    configureTestSuite(() => {
-        TestBed.configureTestingModule({
-            providers: [
-                {
-                    provide: USER_AGENT,
-                    useValue:
-                        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
-                },
-                {
-                    provide: NAVIGATOR,
-                    useValue: {maxTouchPoints: 0},
-                },
-            ],
+    describe('Mac OS desktop negative', () => {
+        configureTestSuite(() => {
+            TestBed.configureTestingModule({
+                providers: [
+                    {
+                        provide: USER_AGENT,
+                        useValue:
+                            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+                    },
+                    {
+                        provide: NAVIGATOR,
+                        useValue: {maxTouchPoints: 0},
+                    },
+                ],
+            });
         });
-    });
 
-    it('TUI_IS_IOS not IOS', () => {
-        expect(TestBed.inject(TUI_IS_IOS)).toBeFalse();
+        it('return false if MacOS Desktop', () => {
+            expect(TestBed.inject(TUI_IS_IOS)).toBeFalse();
+        });
     });
 });
