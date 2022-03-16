6d649804d64e6f70b1c01dabab2f4b2ebd7596fa
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2022-03-16 16:24:22 +0300

    chore: move schematics, prepare for ng-update

---
+++ projects/cdk/schematics/ng-add/constants/versions.ts
@@ -0,0 +1,6 @@
+const {version} = require('../../../package.json');
+
+export const TAIGA_VERSION = `^${version}`;
+export const NG_DOMPURIFY_VERSION = '3.0.0';
+export const DOMPURIFY_VERSION = '2.2.9';
+export const DOMPURIFY_TYPES_VERSION = '2.2.3';
