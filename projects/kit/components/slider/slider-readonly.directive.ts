e77050eca9cd9e7f832d2339bcffee49a1de0eb3
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-03-29 15:47:38 +0300

    fix(kit): `Slider` improve directive `readonly`

--- projects/kit/components/slider/slider-readonly.directive.ts
+++ projects/kit/components/slider/slider-readonly.directive.ts
@@ -1,5 +1,7 @@
-import {Directive, HostListener, Input} from '@angular/core';
-import {tuiDefaultProp} from '@taiga-ui/cdk';
+import {Directive, ElementRef, HostListener, Inject, Input} from '@angular/core';
+import {tuiDefaultProp, TuiDestroyService, typedFromEvent} from '@taiga-ui/cdk';
+import {Observable} from 'rxjs';
+import {filter, takeUntil} from 'rxjs/operators';
 
 /**
  * Native <input type='range' readonly> doesn't work.
@@ -7,19 +9,34 @@ import {tuiDefaultProp} from '@taiga-ui/cdk';
  */
 @Directive({
     selector: 'input[tuiSlider][readonly]',
+    providers: [TuiDestroyService],
 })
 export class TuiSliderReadonlyDirective {
+    private readonly ARROWS = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
+
     @Input()
     @tuiDefaultProp()
     readonly: '' | boolean = true;
 
+    constructor(
+        @Inject(ElementRef) {nativeElement}: ElementRef,
+        @Inject(TuiDestroyService) destroy$: Observable<unknown>,
+    ) {
+        typedFromEvent(nativeElement, 'keydown')
+            .pipe(
+                filter(({key}) => this.ARROWS.includes(key)),
+                takeUntil(destroy$),
+            )
+            .subscribe(event => this.preventEvent(event));
+    }
+
     @HostListener('mousedown', ['$event'])
     @HostListener('touchstart', ['$event'])
-    @HostListener('keydown.arrowLeft', ['$event'])
-    @HostListener('keydown.arrowRight', ['$event'])
-    @HostListener('keydown.arrowUp', ['$event'])
-    @HostListener('keydown.arrowDown', ['$event'])
-    preventEvents(event: Event) {
+    @HostListener('keydown.home', ['$event'])
+    @HostListener('keydown.end', ['$event'])
+    @HostListener('keydown.pageUp', ['$event'])
+    @HostListener('keydown.pageDown', ['$event'])
+    preventEvent(event: Event) {
         if (this.readonly === '' || this.readonly) {
             event.preventDefault();
         }
