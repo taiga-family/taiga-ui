58b81998a1cbafeffccef0bc196d5ffa2b1689e6
splincode <splincodewd@yandex.ru>
2022-03-29 13:54:35 +0300

    refactor: exclude tests which have no expectations

--- projects/cdk/observables/test/replay-control-value-changes.spec.ts
+++ projects/cdk/observables/test/replay-control-value-changes.spec.ts
@@ -1,41 +1,45 @@
-import {FormControl} from '@angular/forms';
+import {fakeAsync} from '@angular/core/testing';
+import {AbstractControl, FormControl} from '@angular/forms';
+import {tuiReplayedValueChangesFrom} from '@taiga-ui/cdk';
 import {skip} from 'rxjs/operators';
 
-import {tuiReplayedValueChangesFrom} from '../replay-control-value-changes';
-
 describe('tuiReplayedValueChangesFrom', () => {
-    it('takes control and starts with its value', done => {
+    it('takes control and starts with its value', fakeAsync(() => {
+        let actual = '';
         const control = new FormControl('hello');
 
-        tuiReplayedValueChangesFrom(control).subscribe(value => {
-            expect(value).toBe('hello');
-            done();
+        tuiReplayedValueChangesFrom<string>(control).subscribe(value => {
+            actual = value;
         });
-    });
 
-    it('takes control and emits its values', done => {
+        expect(actual).toEqual('hello');
+    }));
+
+    it('takes control and emits its values', fakeAsync(() => {
+        let actual = '';
         const control = new FormControl('hello');
 
-        tuiReplayedValueChangesFrom(control)
+        tuiReplayedValueChangesFrom<string>(control)
             .pipe(skip(1))
             .subscribe(value => {
-                expect(value).toBe('test');
-                done();
+                actual = value;
             });
 
         control.setValue('test');
-    });
 
-    it('throwns an error if there is no valueChanges', done => {
-        const control = {} as any;
+        expect(actual).toBe('test');
+    }));
 
-        tuiReplayedValueChangesFrom(control).subscribe(
-            () => {
-                fail();
-            },
-            () => {
-                done();
+    it('throws an error if there is no valueChanges', fakeAsync(() => {
+        let actual = '';
+
+        tuiReplayedValueChangesFrom({} as AbstractControl).subscribe(
+            () => {},
+            (err: Error) => {
+                actual = err.message;
             },
         );
-    });
+
+        expect(actual).toBe('Control does not have valueChanges');
+    }));
 });
