6355944fb8d294790d88b5a0663f946ea41e8369
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-03-31 15:52:53 +0300

    chore(demo): `Range` extend example with ticks labels

--- projects/demo/src/modules/components/range/examples/3/index.ts
+++ projects/demo/src/modules/components/range/examples/3/index.ts
@@ -10,5 +10,16 @@ import {encapsulation} from '@demo/emulate/encapsulation';
     encapsulation,
 })
 export class TuiRangeExample3 {
+    readonly min = 0;
+    readonly max = 1000;
+    readonly quantum = 250;
+    readonly segments = 4;
+    readonly labels = [...new Array(this.segments + 1).keys()].map(
+        i => this.min + this.quantum * i,
+    );
+
     value = [0, 250];
+
+    // https://angular.io/api/common/I18nPluralPipe
+    pluralMap = {'=0': '0', '=1': '# item', '=1000': 'MAX', other: '# items'};
 }
