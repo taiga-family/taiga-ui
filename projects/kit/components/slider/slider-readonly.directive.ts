0669a716e902fda12cb14f5a42da23c836f01c1f
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-04-25 16:22:51 +0300

    fix(kit): `Slider` readonly state (mobile chrome bug) (#1638)

--- projects/kit/components/slider/slider-readonly.directive.ts
+++ projects/kit/components/slider/slider-readonly.directive.ts
@@ -1,5 +1,13 @@
-import {Directive, HostListener, Input} from '@angular/core';
-import {tuiDefaultProp} from '@taiga-ui/cdk';
+import {DOCUMENT} from '@angular/common';
+import {Directive, ElementRef, HostListener, Inject, Input} from '@angular/core';
+import {
+    tuiCoerceBooleanProperty,
+    tuiDefaultProp,
+    TuiDestroyService,
+    typedFromEvent,
+} from '@taiga-ui/cdk';
+import {combineLatest, merge, Observable} from 'rxjs';
+import {filter, mapTo, takeUntil, tap} from 'rxjs/operators';
 
 const SLIDER_INTERACTION_KEYS = new Set([
     'ArrowLeft',
@@ -16,18 +24,55 @@ const SLIDER_INTERACTION_KEYS = new Set([
  * Native <input type='range' readonly> doesn't work.
  * This directive imitates this native behaviour.
  */
+// @dynamic
 @Directive({
     selector: 'input[tuiSlider][readonly]',
+    providers: [TuiDestroyService],
 })
 export class TuiSliderReadonlyDirective {
     @Input()
     @tuiDefaultProp()
-    readonly: '' | boolean = true;
+    readonly: string | boolean = true;
+
+    constructor(
+        @Inject(ElementRef) elementRef: ElementRef<HTMLInputElement>,
+        @Inject(DOCUMENT) documentRef: Document,
+        @Inject(TuiDestroyService)
+        destroy$: Observable<unknown>,
+    ) {
+        const touchStart$ = typedFromEvent(elementRef.nativeElement, 'touchstart', {
+            passive: false,
+        });
+        const touchMove$ = typedFromEvent(documentRef, 'touchmove', {
+            passive: false,
+        });
+        const touchEnd$ = typedFromEvent(documentRef, 'touchend', {
+            passive: true,
+        });
+
+        const shouldPreventMove$ = merge(
+            touchStart$.pipe(
+                tap(e => this.preventEvent(e)),
+                mapTo(true),
+            ),
+            touchEnd$.pipe(mapTo(false)),
+        );
+
+        /**
+         * @bad TODO think about another solution.
+         * Keep in mind that preventing touch event (on slider) inside `@HostListener('touchstart')` doesn't work for mobile chrome.
+         */
+        combineLatest([touchMove$, shouldPreventMove$])
+            .pipe(
+                filter(([_, shouldPreventMove]) => shouldPreventMove),
+                takeUntil(destroy$),
+            )
+            .subscribe(([moveEvent]) => this.preventEvent(moveEvent));
+    }
 
     @HostListener('mousedown', ['$event'])
-    @HostListener('touchstart', ['$event'])
     preventEvent(event: Event) {
-        if (this.readonly === '' || this.readonly) {
+        if (event.cancelable && tuiCoerceBooleanProperty(this.readonly)) {
             event.preventDefault();
         }
     }
