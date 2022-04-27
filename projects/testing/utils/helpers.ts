76fa5f4da304d7e6e7bb364906bae1ca433019c9
splincode <splincodewd@yandex.ru>
2022-04-28 01:39:08 +0300

    ci: fix problem with re-export abstract `ComponentHarness`

--- projects/testing/utils/helpers.ts
+++ projects/testing/utils/helpers.ts
@@ -5,34 +5,31 @@ import {
     HarnessPredicate,
 } from '@angular/cdk/testing';
 
-interface With {
-    with: (options?: BaseHarnessFilters) => HarnessPredicate<ComponentHarness>;
-}
-
 /**
  * Decorator to add default static 'with' method.
  * Use in conjunction with {@link HarnessWith} mixin.
  */
-export function withPredicate<T extends PublicComponentHarnessConstructor & With>(
-    original: T,
-): T {
+export function withPredicate<
+    T extends ComponentHarnessConstructor<ComponentHarness> & {
+        with: (options?: BaseHarnessFilters) => HarnessPredicate<ComponentHarness>;
+    },
+>(original: T): T {
     original.with = (options: BaseHarnessFilters = {}) =>
-        new HarnessPredicate(original as ComponentHarnessConstructor<any>, options);
+        new HarnessPredicate(original, options);
 
     return original;
 }
-
-export interface PublicComponentHarness extends ComponentHarness {}
-
-export type PublicComponentHarnessConstructor =
-    ComponentHarnessConstructor<PublicComponentHarness> & With;
 
 /**
  * Mixin to extend {@link ComponentHarness} and add typed
  * static 'with' method. Use {@link withPredicate} decorator
  * to monkey-patch default static 'with' method.
  */
-export function HarnessWith<T>(hostSelector: string): PublicComponentHarnessConstructor {
+export function HarnessWith<T>(
+    hostSelector: string,
+): ComponentHarnessConstructor<ComponentHarness> & {
+    with: (options?: BaseHarnessFilters) => HarnessPredicate<ComponentHarness>;
+} {
     return class extends ComponentHarness {
         static hostSelector = hostSelector;
 
@@ -40,5 +37,7 @@ export function HarnessWith<T>(hostSelector: string): PublicComponentHarnessCons
         static with(_options: BaseHarnessFilters = {}): HarnessPredicate<T> {
             throw new Error('Hummus');
         }
-    } as unknown as PublicComponentHarnessConstructor;
+    } as unknown as ComponentHarnessConstructor<ComponentHarness> & {
+        with: (options?: BaseHarnessFilters) => HarnessPredicate<ComponentHarness>;
+    };
 }
