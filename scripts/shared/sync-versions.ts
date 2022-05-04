672089a7c6aba7f9d231ce2da647f4c233f75221
splincode <splincodewd@yandex.ru>
2022-05-04 15:40:21 +0300

    ci: synchronous update of `*/package.json` files

--- scripts/shared/sync-versions.ts
+++ scripts/shared/sync-versions.ts
@@ -6,49 +6,55 @@ import {bumpTuiDeps} from './bump-tui-deps';
 const INDENTATION = 4;
 
 export function syncVersions(
-    rootDir: string,
+    filesOrDirectories: string[],
     version: string,
     ignores: string[] = [],
 ): void {
-    glob(
-        `${rootDir}/**/*(package.json|package-lock.json)`,
-        {ignore: '**/node_modules/**'},
-        (_: Error | null, files: string[]) => {
-            files.forEach(file => {
-                const packageJson = JSON.parse(readFileSync(file).toString());
-                const {dependencies, peerDependencies, devDependencies} = packageJson;
-
-                if (dependencies) {
-                    bumpTuiDeps({deps: dependencies, version, ignores});
-                }
-
-                if (peerDependencies) {
-                    bumpTuiDeps({
-                        deps: peerDependencies,
-                        version,
-                        isPeerDependency: true,
-                        ignores,
-                    });
-                }
-
-                if (devDependencies) {
-                    bumpTuiDeps({deps: devDependencies, version, ignores});
-                }
-
-                writeFileSync(
-                    file,
-                    `${JSON.stringify(
-                        {
-                            ...packageJson,
-                            ...('version' in packageJson && {
-                                version: version,
-                            }),
-                        },
-                        null,
-                        INDENTATION,
-                    )}\n`,
-                );
-            });
-        },
+    const patterns = filesOrDirectories.map(pattern =>
+        pattern.endsWith('.json')
+            ? pattern
+            : `${pattern}/**/*(package.json|package-lock.json)`,
     );
+
+    const files = patterns
+        .map(pattern => glob.sync(pattern, {ignore: '**/node_modules/**'}))
+        .flatMap(files => files);
+
+    for (const file of files) {
+        const packageJson = JSON.parse(readFileSync(file).toString());
+        const {dependencies, peerDependencies, devDependencies} = packageJson;
+
+        if (dependencies) {
+            bumpTuiDeps({deps: dependencies, version, ignores});
+        }
+
+        if (peerDependencies) {
+            bumpTuiDeps({
+                deps: peerDependencies,
+                version,
+                isPeerDependency: true,
+                ignores,
+            });
+        }
+
+        if (devDependencies) {
+            bumpTuiDeps({deps: devDependencies, version, ignores});
+        }
+
+        writeFileSync(
+            file,
+            `${JSON.stringify(
+                {
+                    ...packageJson,
+                    ...('version' in packageJson && {
+                        version: version,
+                    }),
+                },
+                null,
+                INDENTATION,
+            )}\n`,
+        );
+
+        console.info('\x1b[32m%s\x1b[0m', `[synchronized]:`, file);
+    }
 }
