cb3ff8b64e8879f082f8082ca2011223afa27b0a
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-05-06 12:41:20 +0300

    chore(demo-integrations): `AlertService` fix cypress-test (#1730)

---
+++ projects/demo-integrations/cypress/tests/core/alert-service/alert-service.spec.ts
@@ -0,0 +1,13 @@
+describe('AlertService', () => {
+    beforeEach(() => {
+        cy.viewport(720, 720);
+    });
+
+    it('is shown correctly', () => {
+        cy.tuiVisit(`/services/alert-service`);
+
+        cy.get(`tui-alerts-example-1 button`).first().click().wait(1000);
+
+        cy.get('tui-alert').first().matchImageSnapshot('alert');
+    });
+});
