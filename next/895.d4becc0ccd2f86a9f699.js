96669834bf2ceb093c2cd3787b0e8836d26b5e9c
splincode <omaxphp@yandex.ru>
2022-05-12 15:42:56 +0000

    Deploying to gh-pages from @ Tinkoff/taiga-ui@41c4497d1ce18e12d3c20df57339554a9c2f527a 🚀

---
+++ next/895.d4becc0ccd2f86a9f699.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[895],{"XVd/":function(n,t,o){"use strict";o.r(t),t.default='<p i18n>\n    This notification will be removed after router change (see component sample)\n</p>\n<button\n    tuiButton\n    type="button"\n    size="m"\n    (click)="showNotification()"\n>\n    Show\n</button>\n'}}]);
