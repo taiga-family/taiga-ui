4f1026b226bc0fbee187ac387533c5990064a58b
Maksim Ivanov <omaxphp@yandex.ru>
2022-05-16 12:30:03 +0300

    ci: add `postchangelog` and fix some issues (#1753)

--- scripts/postbump.ts
+++ scripts/postbump.ts
@@ -2,14 +2,12 @@ import {execSync} from 'child_process';
 import {resolve} from 'path';
 
 import {version} from '../package.json';
-import {checkPrettierChanges} from './shared/check-prettier-changes';
 import {overwriteVersion} from './shared/overwrite-version';
 import {syncVersions} from './shared/sync-versions';
 
 (function main(): void {
     syncVersions(['./projects'], version);
     overwriteVersion(resolve('./projects/core/constants/version.ts'), version);
-    checkPrettierChanges();
     execSync(`git add **/package.json`, {stdio: 'inherit'});
     execSync(`git add ./projects/core/constants/version.ts`, {stdio: 'inherit'});
 })();
