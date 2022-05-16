48e70ee04a0e44a683df08dd88c545f4f9d1e806
splincode <splincodewd@yandex.ru>
2022-05-16 17:09:41 +0300

    ci: remove `classlist.js` for IE10 and IE11

--- projects/demo/src/polyfills.ts
+++ projects/demo/src/polyfills.ts
@@ -20,10 +20,6 @@ import '@angular/localize/init';
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-/* IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js'; // Run `npm install --save classlist.js`.
-/* IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es6/reflect';
 // Evergreen browsers require these.
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
@@ -33,13 +29,6 @@ import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 import 'prismjs';
 import 'prismjs/components/prism-typescript.min.js';
-
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- */
-// Not needed since Angular 6 (fallback to CSS keyframes is built-in)
-// import 'web-animations-js'; // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
