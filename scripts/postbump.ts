4e23d2db7fbb4f29085554d5f23f8cc89402993c
Maksim Ivanov <omaxphp@yandex.ru>
2022-05-16 14:15:52 +0300

    ci: bump `peerDependencies` after release in `package-lock.json` (#1760)

--- scripts/postbump.ts
+++ scripts/postbump.ts
@@ -6,7 +6,7 @@ import {overwriteVersion} from './shared/overwrite-version';
 import {syncVersions} from './shared/sync-versions';
 
 (function main(): void {
-    syncVersions(['./projects'], version);
+    syncVersions(['./projects', './package-lock.json'], version);
     overwriteVersion(resolve('./projects/core/constants/version.ts'), version);
     execSync(`git add **/package.json`, {stdio: 'inherit'});
     execSync(`git add ./projects/core/constants/version.ts`, {stdio: 'inherit'});
