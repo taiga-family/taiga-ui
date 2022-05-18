0b40e2b010af3f4a938859f15bc74ca8ff46eb6e
waterplea <alexander@inkin.ru>
2022-05-18 15:13:41 +0000

    Deploying to gh-pages from @ Tinkoff/taiga-ui@0950f8eae71142eefbad21e44f80a7a43b1e7216 🚀

--- next/962.20f7c66d20ba9090c45b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[962],{lsfa:function(n,t,e){"use strict";e.r(t),t.default=".table {\n    width: 100%;\n}\n\n.filters {\n    display: flex;\n}\n\n.input {\n    flex: 1;\n}\n\n.columns {\n    width: 10.625rem;\n}\n\n.match {\n    background: var(--tui-selection);\n}\n"}}]);
--- next/967.22d52197331ada54eeda.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[967],{xnG9:function(n,o,e){"use strict";e.r(o),o.default="```ts\nimport {TuiTableModule} from '@taiga-ui/addon-table';\n\n@NgModule({\n  imports: [\n    // ...\n    TuiTableModule,\n  ],\n  // ...\n})\nexport class MyModule {}\n```\n"}}]);
