f9e8ffe87fae97f09e8d59feefda162044bd8020
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-05-18 16:24:16 +0300

    fix(addon-table): `TablePagination` show checkmark for selected `[size]`

---
+++ projects/demo-integrations/cypress/tests/addon-table/table-pagination/table-pagination.spec.ts
@@ -0,0 +1,59 @@
+describe('TablePagination', () => {
+    beforeEach(() => {
+        cy.viewport('macbook-13');
+    });
+
+    describe('Dropdown with `[size]`-options (amount items per page)', () => {
+        it('Basic case', () => {
+            cy.tuiVisit('/components/table-pagination/API');
+            initializeAliases('#demoContent');
+
+            cy.get('@linesPerPageSelect').click();
+            waitForCheckmarkIcon();
+            cy.get('@tablePagination').matchImageSnapshot('0-[size]-dropdown-base', {
+                padding: [0, 0, 150, 0],
+            });
+        });
+
+        it('With very long option name', () => {
+            const LONG_NUMBER = 10000000000000000000;
+
+            cy.tuiVisit(
+                encodeURI(
+                    `/components/table-pagination/API?items=[0, ${LONG_NUMBER}]&size=${LONG_NUMBER}&total=${LONG_NUMBER}&page=0`,
+                ),
+            );
+            initializeAliases('#demoContent');
+
+            cy.get('@linesPerPageSelect').click();
+            waitForCheckmarkIcon();
+            cy.get('@tablePagination').matchImageSnapshot(
+                '1-[size]-dropdown-long-option-name',
+                {
+                    padding: [0, 0, 100, 0],
+                },
+            );
+        });
+    });
+});
+
+function initializeAliases(selectorToTablePagination: string): void {
+    cy.get(`${selectorToTablePagination} tui-table-pagination`)
+        .should('be.visible')
+        .as('tablePagination');
+
+    cy.get('@tablePagination')
+        .find(
+            '[automation-id=tui-table-pagination__lines-per-page-wrapper] tui-hosted-dropdown',
+        )
+        .should('be.visible')
+        .as('linesPerPageSelect');
+}
+
+function waitForCheckmarkIcon(): void {
+    cy.get('tui-dropdown-box')
+        .find('tui-svg[src=tuiIconCheckLarge] use')
+        .should('be.visible')
+        .invoke('height')
+        .should('be.greaterThan', 0);
+}
