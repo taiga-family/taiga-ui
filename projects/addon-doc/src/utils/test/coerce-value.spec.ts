dd73c15d65cd1fb3396cec5c379e6dac7e9c9059
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-05-18 16:46:07 +0300

    chore: add tests for invalid values

--- projects/addon-doc/src/utils/test/coerce-value.spec.ts
+++ projects/addon-doc/src/utils/test/coerce-value.spec.ts
@@ -30,6 +30,10 @@ describe('coercing values', () => {
     it('string -> array', () => {
         expect(coerceValue('[1,2,3]')).toEqual([1, 2, 3]);
         expect(coerceValue('[ 1,2, 3]')).toEqual([1, 2, 3]);
+        expect(coerceValue('[ 1,2,, 3]')).toBe('[ 1,2,, 3]');
+        expect(coerceValue('This is array [1,2,3] inside string')).toBe(
+            'This is array [1,2,3] inside string',
+        );
     });
 
     it('string -> object', () => {
@@ -38,5 +42,14 @@ describe('coercing values', () => {
             id: 1,
             options: [1, 2, 3],
         });
+        expect(coerceValue('{"message": "Extra comma", "options": [1,2,,3]}')).toBe(
+            '{"message": "Extra comma", "options": [1,2,,3]}',
+        );
+        expect(coerceValue('{"message": "No } at the end"')).toBe(
+            '{"message": "No } at the end"',
+        );
+        expect(coerceValue('{message: "No quotes for key"')).toBe(
+            '{message: "No quotes for key"',
+        );
     });
 });
