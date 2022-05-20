3e83fbae05c2afb41ed53cf84637c3cfe68d1369
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-05-20 18:20:45 +0300

    fix(addon-mobile): `Ripple` broken styles in Shadow DOM

--- projects/addon-mobile/directives/ripple/ripple.module.ts
+++ projects/addon-mobile/directives/ripple/ripple.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 
 import {TuiRippleDirective} from './ripple.directive';
+import {TuiRippleStylesComponent} from './ripple-styles.component';
 
 @NgModule({
-    declarations: [TuiRippleDirective],
+    declarations: [TuiRippleDirective, TuiRippleStylesComponent],
     exports: [TuiRippleDirective],
 })
 export class TuiRippleModule {}
