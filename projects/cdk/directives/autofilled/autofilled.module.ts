8656053ac10e37e9fe447a79f50593f559460eda
Maksim Ivanov <omaxphp@yandex.ru>
2022-05-23 13:05:34 +0300

    fix(cdk): refactor autofill directive (#1775)
    Co-authored-by: splincode <splincode@users.noreply.github.com>

--- projects/cdk/directives/autofilled/autofilled.module.ts
+++ projects/cdk/directives/autofilled/autofilled.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 
 import {TuiAutofilledDirective} from './autofilled.directive';
+import {TuiAutofilledStyleComponent} from './autofilled-style.component';
 
 @NgModule({
-    declarations: [TuiAutofilledDirective],
     exports: [TuiAutofilledDirective],
+    entryComponents: [TuiAutofilledStyleComponent],
+    declarations: [TuiAutofilledDirective, TuiAutofilledStyleComponent],
 })
 export class TuiAutofilledModule {}
