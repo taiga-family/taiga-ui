6ea6ff3f44dbe0a10c872047768a31912f83e87d
Alex Inkin <alexander@inkin.ru>
2022-05-23 16:50:14 +0300

    feat(core): `TUI_IS_MOBILE_RES` add new token (#1799)
    * feat(core): `TUI_IS_MOBILE_RES` add new token
    
    * chore: fix build

--- projects/core/tokens/is-mobile-resolution.ts
+++ projects/core/tokens/is-mobile-resolution.ts
@@ -1,6 +1,25 @@
-import {InjectionToken} from '@angular/core';
+import {inject, InjectionToken, NgZone} from '@angular/core';
+import {WINDOW} from '@ng-web-apis/common';
+import {tuiZoneOptimized, typedFromEvent} from '@taiga-ui/cdk';
 import {Observable} from 'rxjs';
+import {distinctUntilChanged, map, share, startWith} from 'rxjs/operators';
+
+import {TUI_MEDIA} from './media';
 
 export const TUI_IS_MOBILE_RES = new InjectionToken<Observable<boolean>>(
     'Mobile resolution stream for private providers',
+    {
+        factory: () => {
+            const windowRef = inject(WINDOW);
+            const {mobile} = inject(TUI_MEDIA);
+
+            return typedFromEvent(windowRef, 'resize').pipe(
+                startWith(null),
+                map(() => windowRef.innerWidth < mobile),
+                distinctUntilChanged(),
+                tuiZoneOptimized(inject(NgZone)),
+                share(),
+            );
+        },
+    },
 );
