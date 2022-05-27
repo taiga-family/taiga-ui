f91b3d53250f4ff5196ff7c40b94465e664ea443
v.potekhin <vladimir.potekh@gmail.com>
2022-05-27 12:25:15 +0300

    chore: forEach instead of for..of

--- projects/cdk/schematics/ng-update/steps/show-warnings.ts
+++ projects/cdk/schematics/ng-update/steps/show-warnings.ts
@@ -3,9 +3,7 @@ import {getNamedImportReferences} from '../../utils/get-named-import-references'
 import {SchematicContext} from '@angular-devkit/schematics';
 
 export function showWarnings(context: SchematicContext): void {
-    for (let warning of MIGRATION_WARNINGS) {
-        showWarning(warning, context);
-    }
+    MIGRATION_WARNINGS.forEach(warning => showWarning(warning, context));
 }
 
 function showWarning(
@@ -14,12 +12,12 @@ function showWarning(
 ): void {
     const references = getNamedImportReferences(name, moduleSpecifier);
 
-    for (let reference of references) {
+    references.forEach(reference => {
         const file = reference.getSourceFile().getFilePath();
         const {line, column} = reference
             .getSourceFile()
             .getLineAndColumnAtPos(reference.getStart());
 
         logger.warn(`[WARNING]: in ${file} at line ${line}, col ${column}:  ${message}`);
-    }
+    });
 }
