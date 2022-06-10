9fa5e1b1b9fc58f13c6b152d88b76b6f32dd5eff
splincode <splincodewd@yandex.ru>
2022-06-10 12:25:20 +0300

    feat(cdk): add `chromium, firefox, webkit` engine detection

---
+++ projects/cdk/tokens/is-webkit.ts
@@ -0,0 +1,9 @@
+import {inject, InjectionToken} from '@angular/core';
+import {WINDOW} from '@ng-web-apis/common';
+
+export const TUI_IS_WEBKIT = new InjectionToken<boolean>(
+    'Webkit browser engine detection',
+    {
+        factory: () => !!inject(WINDOW).webkitConvertPointFromNodeToPage,
+    },
+);
