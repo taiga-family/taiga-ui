ec2607d4775c87adf022cf158a267e276fe0cd18
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-06-21 17:24:16 +0300

    refactor(kit): `tuiPresentChange`-directive use component approach for styles (#1972)

---
+++ projects/kit/directives/present/present-styles.component.ts
@@ -0,0 +1,8 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+    template: '',
+    styleUrls: ['./present.style.less'],
+    encapsulation: ViewEncapsulation.None,
+})
+export class TuiPresentStylesComponent {}
