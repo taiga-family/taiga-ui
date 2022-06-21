a7f48c44fcbe27e737502280abc8db310100016b
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-06-21 15:43:46 +0300

    chore(cdk): `InputRange` migrations for `minLabel` and `maxLabel`

--- projects/cdk/schematics/ng-update/steps/migrate-sliders/index.ts
+++ projects/cdk/schematics/ng-update/steps/migrate-sliders/index.ts
@@ -1,6 +1,8 @@
 import {Tree} from '@angular-devkit/schematics';
 import {migrateInputSlider} from './migrate-input-slider';
+import {migrateInputRange} from './migrate-input-range';
 
 export function migrateSliders(tree: Tree): void {
     migrateInputSlider(tree);
+    migrateInputRange(tree);
 }
