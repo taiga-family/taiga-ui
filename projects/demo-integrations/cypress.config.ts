ca9eec0d906f4d681931e0c6d280226771395b7d
Maksim Ivanov <omaxphp@yandex.ru>
2022-06-22 14:53:21 +0300

    ci: parallelize e2e tests across different jobs (#1953)
    Co-authored-by: splincode <splincode@users.noreply.github.com>

--- projects/demo-integrations/cypress.config.ts
+++ projects/demo-integrations/cypress.config.ts
@@ -6,21 +6,44 @@ export const viewportHeight = 900;
 export default defineConfig({
     projectId: 'sorry-cypress',
     video: false,
-    screenshotsFolder: 'cypress/screenshots',
-    fixturesFolder: 'cypress/fixtures',
     viewportWidth,
     viewportHeight,
+    fixturesFolder: 'cypress/fixtures',
+    screenshotsFolder: 'cypress/screenshots',
+
+    /**
+     * @description:
+     * Time, in milliseconds, to wait until most
+     * DOM based commands are considered timed out.
+     */
     defaultCommandTimeout: 10000,
+
+    /**
+     * @description:
+     * Time, in milliseconds, to wait until a response
+     * in a cy.request(), cy.wait(),
+     */
     responseTimeout: 10000,
-    numTestsKeptInMemory: 1,
+
+    /**
+     * @description:
+     * The number of tests for which snapshots and command data are kept in memory.
+     * Reduce this number if you are experiencing high memory
+     * consumption in your browser during a test run.
+     */
+    numTestsKeptInMemory: 50,
+
     e2e: {
-        // We've imported your old cypress plugins here.
-        // You may want to clean this up later by importing these.
-        setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
-            return require('./cypress/plugins/index.ts').default(on, config);
-        },
         specPattern: 'cypress/tests/**/*.spec.ts',
         supportFile: 'cypress/support/e2e.ts',
         baseUrl: 'http://localhost:3333',
+        /**
+         * @description:
+         * We've imported your old cypress plugins here.
+         * You may want to clean this up later by importing these.
+         */
+        setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
+            return require('./cypress/plugins/index.ts').default(on, config);
+        },
     },
 });
