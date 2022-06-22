05203924f8aad9e72a11a3ddacf693d6be9c9e71
Alex Inkin <alexander@inkin.ru>
2022-06-22 12:03:02 +0300

    fix(core): `PrimitiveTextfield` fix prefix when aligned to the right (#1969)
    * fix(core): `PrimitiveTextfield` fix prefix when aligned to the right
    
    * chore: comment

--- projects/kit/directives/present/present-styles.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component, ViewEncapsulation} from '@angular/core';
-
-@Component({
-    template: '',
-    styleUrls: ['./present.style.less'],
-    encapsulation: ViewEncapsulation.None,
-})
-export class TuiPresentStylesComponent {}
--- projects/kit/directives/present/present.directive.ts
+++ projects/kit/directives/present/present.directive.ts
@@ -7,43 +7,31 @@ import {
     Output,
 } from '@angular/core';
 import {USER_AGENT} from '@ng-web-apis/common';
-import {isCurrentTarget, isFirefox, TuiDirectiveStylesService} from '@taiga-ui/cdk';
-import {Observable, Subject} from 'rxjs';
-import {distinctUntilChanged, skip, startWith} from 'rxjs/operators';
-
-import {TuiPresentStylesComponent} from './present-styles.component';
+import {isCurrentTarget, isFirefox} from '@taiga-ui/cdk';
+import {BehaviorSubject} from 'rxjs';
+import {distinctUntilChanged, skip} from 'rxjs/operators';
 
 @Directive({
     selector: '[tuiPresentChange]',
     host: {
-        class: 'tui-present',
+        '[style.animation]': '"tuiPresent 1s infinite"',
     },
 })
 export class TuiPresentDirective implements OnDestroy {
-    private readonly visibility$ = new Subject<boolean>();
-    private readonly observer?: MutationObserver;
+    private readonly visibility$ = new BehaviorSubject(false);
 
     @Output()
-    readonly tuiPresentChange: Observable<boolean>;
+    readonly tuiPresentChange = this.visibility$.pipe(distinctUntilChanged(), skip(1));
 
     constructor(
         @Inject(ElementRef) {nativeElement}: ElementRef<HTMLElement>,
-        @Inject(TuiDirectiveStylesService) directiveStyles: TuiDirectiveStylesService,
         @Inject(USER_AGENT) userAgent: string,
     ) {
-        directiveStyles.addComponent(TuiPresentStylesComponent);
-
-        this.tuiPresentChange = this.visibility$.pipe(
-            startWith(false),
-            distinctUntilChanged(),
-            skip(1),
-        );
-
         if (isFirefox(userAgent)) {
             return;
         }
 
-        this.observer = new MutationObserver(() => {
+        const observer = new MutationObserver(() => {
             if (
                 !nativeElement.offsetParent &&
                 nativeElement.offsetWidth === 0 &&
@@ -53,7 +41,7 @@ export class TuiPresentDirective implements OnDestroy {
             }
         });
 
-        this.observer.observe(nativeElement, {
+        observer.observe(nativeElement, {
             attributes: true,
             attributeFilter: ['style', 'class'],
         });
