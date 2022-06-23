8fedc5b1cfd86563a03bda6184a3327be99cad87
splincode <splincodewd@yandex.ru>
2022-06-23 11:07:29 +0300

    Revert "fix(addon-editor): dropdown shouldn't visible outside editor area (#1934)"
    This reverts commit e7739776b7f38e3e83e70ab5269102e482b91416.

--- projects/addon-editor/components/editor-new/portal/editor-portal.service.ts
+++
@@ -1,5 +0,0 @@
-import {Injectable} from '@angular/core';
-import {AbstractTuiPortalService} from '@taiga-ui/cdk';
-
-@Injectable()
-export class TuiEditorPortalService extends AbstractTuiPortalService {}
