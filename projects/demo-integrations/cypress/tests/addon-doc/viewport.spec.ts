833864f1808bccbfbde714734bbe9ae9e0ea4c8e
Maksim Ivanov <omaxphp@yandex.ru>
2022-07-05 15:28:01 +0300

    ci: choose another page for testing viewport in e2e (#2045)

--- projects/demo-integrations/cypress/tests/addon-doc/viewport.spec.ts
+++ projects/demo-integrations/cypress/tests/addon-doc/viewport.spec.ts
@@ -1,7 +1,7 @@
 describe('Viewport', () => {
     it('is desktop when device width >= 768px', () => {
         cy.viewport(768, 900);
-        cy.tuiVisit('/getting-started', {hideHeader: false, hideNavigation: false});
+        cy.tuiVisit('/browser-support', {hideHeader: false, hideNavigation: false});
 
         cy.wait(1000).matchImageSnapshot('01-getting-started-desktop-viewport', {
             capture: 'viewport',
@@ -10,7 +10,7 @@ describe('Viewport', () => {
 
     it('is mobile when device width < 768px', () => {
         cy.viewport(767, 900);
-        cy.tuiVisit('/getting-started', {hideHeader: false, hideNavigation: false});
+        cy.tuiVisit('/browser-support', {hideHeader: false, hideNavigation: false});
 
         cy.wait(1000).matchImageSnapshot('02-getting-started-mobile-viewport', {
             capture: 'viewport',
