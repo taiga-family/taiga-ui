06dce13dad84264f87a9cd4f91a2f418798fcdbb
splincode <omaxphp@yandex.ru>
2022-07-05 14:21:06 +0000

    Deploying to gh-pages from @ Tinkoff/taiga-ui@d5429c57d107f891da18c17f86af80db534d668a 🚀

---
+++ next/837.e3ea21fc564cf2fba827.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[837],{Fz73:function(n,o,e){"use strict";e.r(o),o.default='<tui-input-phone\n    class="b-form"\n    [formControl]="control"\n>\n    Type phone number\n</tui-input-phone>\n<pre>Form value: {{ control.value | json }}</pre>\n'}}]);
