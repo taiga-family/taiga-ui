e7e5a9229d369d0899fb906696d564a55788e850
Maksim Ivanov <omaxphp@yandex.ru>
2022-07-06 17:40:46 +0300

    fix(cdk): remove deprecated API (#2055)
    * fix(cdk): remove deprecated API
    
    * chore: fix after review
    
    Co-authored-by: splincode <splincode@users.noreply.github.com>

--- projects/cdk/tokens/is-firefox.ts
+++ projects/cdk/tokens/is-firefox.ts
@@ -5,7 +5,7 @@ export const TUI_IS_FIREFOX = new InjectionToken<boolean>(
     'Firefox browser engine detection',
     {
         factory: () =>
-            typeof (inject(WINDOW) as Window & {InstallTrigger: unknown})
-                ?.InstallTrigger !== 'undefined',
+            typeof (inject(WINDOW) as Window & {mozCancelFullScreen: unknown})
+                ?.mozCancelFullScreen !== 'undefined',
     },
 );
