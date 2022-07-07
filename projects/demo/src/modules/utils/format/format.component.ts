325bfb0d9e643b8b9223db916e0622bcc6c4dd40
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-07 19:59:11 +0300

    refactor!: remove deprecated `tuiCustomEvents`, `tuiPadStart`, `getClosestElement`, `fallbackValue` (#2074)
    * refactor!: remove deprecated `tuiCustomEvents`, `tuiPadStart`, `getClosestElement`, `fallbackValue`

--- projects/demo/src/modules/utils/format/format.component.ts
+++ projects/demo/src/modules/utils/format/format.component.ts
@@ -3,48 +3,42 @@ import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDocExample} from '@taiga-ui/addon-doc';
 
 @Component({
-    selector: `example-format`,
-    templateUrl: `./format.template.html`,
+    selector: 'example-format',
+    templateUrl: './format.template.html',
     changeDetection,
 })
 export class ExampleFormatComponent {
     readonly importComponentExample = import(
-        `!!raw-loader!./examples/import/import-component.md`
+        '!!raw-loader!./examples/import/import-component.md'
     );
 
     readonly example1: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/1/index.ts`),
-        HTML: import(`!!raw-loader!./examples/1/index.html`),
-        LESS: import(`!!raw-loader!./examples/1/index.less`),
-    };
-
-    readonly example2: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/2/index.ts`),
-        HTML: import(`!!raw-loader!./examples/2/index.html`),
-        LESS: import(`!!raw-loader!./examples/2/index.less`),
+        TypeScript: import('!!raw-loader!./examples/1/index.ts'),
+        HTML: import('!!raw-loader!./examples/1/index.html'),
+        LESS: import('!!raw-loader!./examples/1/index.less'),
     };
 
     readonly example3: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/3/index.ts`),
-        HTML: import(`!!raw-loader!./examples/3/index.html`),
-        LESS: import(`!!raw-loader!./examples/3/index.less`),
+        TypeScript: import('!!raw-loader!./examples/3/index.ts'),
+        HTML: import('!!raw-loader!./examples/3/index.html'),
+        LESS: import('!!raw-loader!./examples/3/index.less'),
     };
 
     readonly example4: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/4/index.ts`),
-        HTML: import(`!!raw-loader!./examples/4/index.html`),
-        LESS: import(`!!raw-loader!./examples/4/index.less`),
+        TypeScript: import('!!raw-loader!./examples/4/index.ts'),
+        HTML: import('!!raw-loader!./examples/4/index.html'),
+        LESS: import('!!raw-loader!./examples/4/index.less'),
     };
 
     readonly example5: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/5/index.ts`),
-        HTML: import(`!!raw-loader!./examples/5/index.html`),
-        LESS: import(`!!raw-loader!./examples/5/index.less`),
+        TypeScript: import('!!raw-loader!./examples/5/index.ts'),
+        HTML: import('!!raw-loader!./examples/5/index.html'),
+        LESS: import('!!raw-loader!./examples/5/index.less'),
     };
 
     readonly example6: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/6/index.ts`),
-        HTML: import(`!!raw-loader!./examples/6/index.html`),
-        LESS: import(`!!raw-loader!./examples/6/index.less`),
+        TypeScript: import('!!raw-loader!./examples/6/index.ts'),
+        HTML: import('!!raw-loader!./examples/6/index.html'),
+        LESS: import('!!raw-loader!./examples/6/index.less'),
     };
 }
