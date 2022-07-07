34a5e02ebcdcbb59d5265464a01bc4d71d7b48bf
splincode <splincodewd@yandex.ru>
2022-07-07 14:02:40 +0300

    fix(addon-charts): use `ng-container[ngTemplateOutlet]` instead of structural directive

--- projects/demo-integrations/cypress/tests/addon-charts/line-chart.spec.ts
+++ projects/demo-integrations/cypress/tests/addon-charts/line-chart.spec.ts
@@ -1,7 +1,5 @@
-describe('Addon charts', () => {
-    beforeEach(() => {
-        cy.tuiVisit('components/line-chart');
-    });
+describe('LineChart', () => {
+    beforeEach(() => cy.tuiVisit('components/line-chart'));
 
     it('should not show hint', () => {
         cy.get('tui-line-chart-example-1 tui-line-chart .t-column')
---
+++ projects/demo-integrations/cypress/tests/addon-charts/pie-chart.spec.ts
@@ -0,0 +1,34 @@
+import {DEFAULT_TIMEOUT_BEFORE_ACTION} from '../../support/shared.entities';
+
+describe('PieChart', () => {
+    beforeEach(() => cy.tuiVisit('components/pie-chart'));
+
+    it('should be show hints on charts', () => {
+        cy.get('tui-doc-example[heading="With labels"]')
+            .tuiScrollIntoView()
+            .as('wrapper');
+
+        cy.get('@wrapper')
+            .wait(DEFAULT_TIMEOUT_BEFORE_ACTION)
+            .matchImageSnapshot(`01-pie-chart-with-label-0`);
+
+        cy.get('@wrapper')
+            .findByAutomationId('tui-doc-example')
+            .findByAutomationId('tui-pie-chart__segment')
+            .each((segment, index) => {
+                cy.wrap(segment)
+                    .trigger('mouseover')
+                    .trigger('mouseenter')
+                    .invoke('show')
+                    .click() // emulate hover
+                    .wait(300); // wait animation for skip flaky tests
+
+                cy.get('@wrapper').matchImageSnapshot(
+                    `01-pie-chart-with-label-${index + 1}`,
+                );
+
+                // unfocused pie-chart before next iteration
+                cy.get('@wrapper').click().wait(DEFAULT_TIMEOUT_BEFORE_ACTION);
+            });
+    });
+});
