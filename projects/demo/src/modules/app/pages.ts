294d5fcbf9ae17d5941942b88ec6b88bd5d4b165
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-07 19:59:11 +0300

    refactor!: remove deprecated `tuiCustomEvents`, `tuiPadStart`, `getClosestElement`, `fallbackValue` (#2074)
    * refactor!: remove deprecated `tuiCustomEvents`, `tuiPadStart`, `getClosestElement`, `fallbackValue`

--- projects/demo/src/modules/app/pages.ts
+++ projects/demo/src/modules/app/pages.ts
@@ -1247,7 +1247,7 @@ export const pages: TuiDocPages = [
             {
                 section: $localize`Tools`,
                 title: $localize`Format`,
-                keywords: `Инструменты, утилиты, форматирующие, format, px, padStart, getCurrencySymbol, capitalize,
+                keywords: `Инструменты, утилиты, форматирующие, format, px, getCurrencySymbol, capitalize,
                            formatPhone`,
                 route: `/utils/format`,
             },
