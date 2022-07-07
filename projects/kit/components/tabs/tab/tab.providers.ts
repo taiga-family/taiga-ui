d9370f1ff3d1e7323271e34c0f2e4471dfb93ba5
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-07 19:59:11 +0300

    refactor!: remove deprecated `tuiCustomEvents`, `tuiPadStart`, `getClosestElement`, `fallbackValue` (#2074)
    * refactor!: remove deprecated `tuiCustomEvents`, `tuiPadStart`, `getClosestElement`, `fallbackValue`

--- projects/kit/components/tabs/tab/tab.providers.ts
+++ projects/kit/components/tabs/tab/tab.providers.ts
@@ -1,13 +1,7 @@
-import {DOCUMENT} from '@angular/common';
 import {ElementRef, InjectionToken, Optional, Provider} from '@angular/core';
 import {RouterLinkActive} from '@angular/router';
 import {MutationObserverService} from '@ng-web-apis/mutation-observer';
-import {
-    tuiCustomEvent,
-    TuiDestroyService,
-    TuiFocusVisibleService,
-    typedFromEvent,
-} from '@taiga-ui/cdk';
+import {TuiDestroyService, TuiFocusVisibleService, typedFromEvent} from '@taiga-ui/cdk';
 import {MODE_PROVIDER, TuiRouterLinkActiveService} from '@taiga-ui/core';
 import {EMPTY, identity, merge, Observable} from 'rxjs';
 import {filter, map} from 'rxjs/operators';
@@ -24,7 +18,6 @@ export const TUI_TAB_PROVIDERS: Provider[] = [
         provide: TUI_TAB_EVENT,
         deps: [
             ElementRef,
-            DOCUMENT,
             TuiRouterLinkActiveService,
             [new Optional(), MutationObserverService],
             [new Optional(), RouterLinkActive],
@@ -37,7 +30,6 @@ export const TUI_TAB_PROVIDERS: Provider[] = [
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export function tabActiveFactory(
     {nativeElement}: ElementRef<HTMLElement>,
-    documentRef: Document,
     routerLinkActiveService: Observable<boolean>,
     mutationObserverService: MutationObserverService | null,
     routerLinkActive: RouterLinkActive | null,
@@ -54,7 +46,7 @@ export function tabActiveFactory(
     ).pipe(
         map(() =>
             nativeElement.dispatchEvent(
-                tuiCustomEvent(TUI_TAB_ACTIVATE, {bubbles: true}, documentRef),
+                new CustomEvent(TUI_TAB_ACTIVATE, {bubbles: true}),
             ),
         ),
     );
