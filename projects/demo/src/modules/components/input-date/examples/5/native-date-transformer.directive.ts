4825ab5123ee51927f3fa1a23ce6fdd69c7bc9b9
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-08 15:08:06 +0300

    feat!: update to Angular 10 (#2080)

--- projects/demo/src/modules/components/input-date/examples/5/native-date-transformer.directive.ts
+++ projects/demo/src/modules/components/input-date/examples/5/native-date-transformer.directive.ts
@@ -1,4 +1,4 @@
-import {Directive} from '@angular/core';
+import {Directive, Injectable} from '@angular/core';
 import {TuiControlValueTransformer, TuiDay} from '@taiga-ui/cdk';
 import {TUI_DATE_VALUE_TRANSFORMER} from '@taiga-ui/kit';
 
@@ -6,6 +6,7 @@ type From = TuiDay | null;
 
 type To = Date | null;
 
+@Injectable()
 class ExampleTransformer implements TuiControlValueTransformer<From, To> {
     fromControlValue(controlValue: To): From {
         return controlValue && TuiDay.fromLocalNativeDate(controlValue);
