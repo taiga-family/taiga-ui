d0a31d17157e583f2e2132399735ec64493edaaf
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-08 15:08:06 +0300

    feat!: update to Angular 10 (#2080)

--- projects/cdk/abstract/controller.ts
+++ projects/cdk/abstract/controller.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import {OnChanges} from '@angular/core';
+import {Directive, OnChanges} from '@angular/core';
 import {Subject} from 'rxjs';
 
 /**
  * @deprecated
  * TODO: 3.0 replace with {@link AbstractTuiController}
  */
+@Directive()
 export abstract class TuiController implements OnChanges {
     readonly change$ = new Subject<void>();
 
@@ -14,4 +15,5 @@ export abstract class TuiController implements OnChanges {
     }
 }
 
+@Directive()
 export abstract class AbstractTuiController extends TuiController {}
