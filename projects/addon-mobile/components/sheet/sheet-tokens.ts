5c1a65090b6456933bdc10c1b6719165ce772c57
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-08 15:06:39 +0300

    feat!: update to Angular 11

---
+++ projects/addon-mobile/components/sheet/sheet-tokens.ts
@@ -0,0 +1,23 @@
+import {InjectionToken} from '@angular/core';
+
+import {TuiSheetHost} from './sheet';
+
+export const TUI_SHEET_HOST = new InjectionToken<TuiSheetHost>(
+    '[TUI_SHEET_HOST] Sheet main component',
+);
+
+export const TUI_SHEET_SCROLL = new InjectionToken<number>(
+    'Current scrollTop of a sheet',
+);
+
+export const TUI_SHEET_DRAGGED = new InjectionToken<boolean>(
+    'The sheet is being dragged',
+);
+
+/** @deprecated use option argument for each Sheet */
+export const TUI_SHEET_OFFSET = new InjectionToken<number>(
+    'Offset from the top at which the sheet stops',
+    {
+        factory: () => 16,
+    },
+);
--- projects/addon-mobile/components/sheet/sheet.ts
+++ projects/addon-mobile/components/sheet/sheet.ts
@@ -11,3 +11,8 @@ export interface TuiSheet<O, I = undefined>
     readonly scroll$: Observable<number>;
     completeWith(value: O): void;
 }
+
+export interface TuiSheetHost<T = unknown> {
+    stops: readonly number[];
+    item: TuiSheet<T>;
+}
