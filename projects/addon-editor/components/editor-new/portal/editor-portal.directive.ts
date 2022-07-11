1ff1b33b7f80a460614429e04a13e495cd382b2d
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-11 13:45:24 +0300

    refactor!: remove TuiPortalService, PreviewDialogService and open method in TuiCodeEditor (#2079)

--- projects/addon-editor/components/editor-new/portal/editor-portal.directive.ts
+++ projects/addon-editor/components/editor-new/portal/editor-portal.directive.ts
@@ -1,10 +1,10 @@
 import {Directive} from '@angular/core';
-import {TuiPortalService} from '@taiga-ui/cdk';
+import {TuiDropdownPortalService} from '@taiga-ui/cdk';
 
 import {TuiEditorPortalService} from './editor-portal.service';
 
 @Directive({
     selector: '[tuiEditorPortal]',
-    providers: [{provide: TuiPortalService, useExisting: TuiEditorPortalService}],
+    providers: [{provide: TuiDropdownPortalService, useExisting: TuiEditorPortalService}],
 })
 export class TuiEditorPortalDirective {}
