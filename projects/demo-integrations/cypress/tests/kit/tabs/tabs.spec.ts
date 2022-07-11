30f2bf4d9537f5553ab14054eb3b3c664086db43
splincode <splincodewd@yandex.ru>
2022-07-11 19:20:23 +0300

    fix(kit): clamp activeIndex for `tabs-with-more`

--- projects/demo-integrations/cypress/tests/kit/tabs/tabs.spec.ts
+++ projects/demo-integrations/cypress/tests/kit/tabs/tabs.spec.ts
@@ -1,13 +1,57 @@
-import {EXAMPLE_ID} from '../../../support/shared.entities';
+import {
+    DEFAULT_TIMEOUT_BEFORE_ACTION,
+    EXAMPLE_ID,
+} from '../../../support/shared.entities';
 
 describe('Tabs', () => {
-    it('No extra margin after the last tab', () => {
-        cy.viewport(1700, 900);
-        cy.tuiVisit('/navigation/tabs');
+    beforeEach(() => cy.viewport(1700, 900));
 
-        cy.get('#complex')
-            .findByAutomationId(EXAMPLE_ID)
-            .tuiScrollIntoView()
-            .matchImageSnapshot('01-[wide-screen]-[no-margin-last-tab]');
+    describe('Examples', () => {
+        beforeEach(() => cy.tuiVisit('/navigation/tabs'));
+
+        it('no extra margin after the last tab', () => {
+            cy.get('#complex')
+                .findByAutomationId(EXAMPLE_ID)
+                .tuiScrollIntoView()
+                .matchImageSnapshot('01-[wide-screen]-[no-margin-last-tab]');
+        });
+
+        it('should correctly select tab if child element is also selected', () => {
+            cy.get('#complex')
+                .findByAutomationId(EXAMPLE_ID)
+                .tuiScrollIntoView()
+                .as('complex');
+
+            cy.get('@complex')
+                .get('button[type="button"]')
+                .contains('Collaborators')
+                .click();
+
+            cy.get('@complex')
+                .wait(400) // wait animation for prevent flaky tests
+                .matchImageSnapshot('02-1-complex-select-collaborators');
+
+            cy.get('tui-dropdown-box')
+                .get('button[type="button"]')
+                .contains('Neil Innes')
+                .click();
+
+            cy.get('@complex')
+                .wait(400) // wait animation for prevent flaky tests
+                .matchImageSnapshot('02-2-complex-select-collaborators');
+        });
+    });
+
+    describe('API', () => {
+        for (const index of [-2, -1, 0, 1, 2, 3, 4, 5, 100, 1000]) {
+            it(`clamp active activeItemIndex=${index}`, () => {
+                cy.tuiVisit(`/navigation/tabs/API?tuiMode=null&activeItemIndex=${index}`);
+
+                cy.get('#demoContent')
+                    .tuiScrollIntoView()
+                    .wait(DEFAULT_TIMEOUT_BEFORE_ACTION)
+                    .matchImageSnapshot(`03-activeItemIndex-${index}`);
+            });
+        }
     });
 });
