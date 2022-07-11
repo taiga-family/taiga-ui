39da3cd20842e24d84441910e1807ecf84d4e740
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-11 13:45:24 +0300

    refactor!: remove TuiPortalService, PreviewDialogService and open method in TuiCodeEditor (#2079)

--- projects/addon-doc/src/interfaces/code-editor.ts
+++ projects/addon-doc/src/interfaces/code-editor.ts
@@ -2,15 +2,3 @@ export interface TuiCodeEditor {
     readonly name: string;
     edit(component: string, id: string, files: Record<string, string>): Promise<void>;
 }
-
-/**
- * @deprecated use {@link TuiCodeEditor}
- */
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export interface CodeEditor extends TuiCodeEditor {
-    /**
-     * @deprecated
-     * use `edit` method instead of `open`
-     */
-    open(component: string, sampleId: string, files: Record<string, string>): void;
-}
