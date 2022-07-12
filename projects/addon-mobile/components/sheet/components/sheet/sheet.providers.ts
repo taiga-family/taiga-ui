f1da59a7fd7fd14fd9f1bdd794b40b89428aa82e
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-12 14:59:56 +0300

    feat!: update to Angular 11 (#2097)
    * feat!: update to Angular 11
    
    * refactor: after review fix

--- projects/addon-mobile/components/sheet/components/sheet/sheet.providers.ts
+++ projects/addon-mobile/components/sheet/components/sheet/sheet.providers.ts
@@ -1,27 +1,13 @@
 import {DOCUMENT} from '@angular/common';
-import {ElementRef, InjectionToken, NgZone, Provider} from '@angular/core';
+import {ElementRef, forwardRef, NgZone, Provider} from '@angular/core';
 import {TUI_IS_IOS, tuiZonefree, typedFromEvent} from '@taiga-ui/cdk';
 import {TUI_SCROLL_REF} from '@taiga-ui/core';
 import {merge, Observable} from 'rxjs';
 import {map, mapTo, share} from 'rxjs/operators';
 
 import {iosScrollFactory} from '../../ios.hacks';
-
-export const TUI_SHEET_SCROLL = new InjectionToken<number>(
-    `Current scrollTop of a sheet`,
-);
-
-export const TUI_SHEET_DRAGGED = new InjectionToken<boolean>(
-    `The sheet is being dragged`,
-);
-
-/** @deprecated use option argument for each Sheet */
-export const TUI_SHEET_OFFSET = new InjectionToken<number>(
-    `Offset from the top at which the sheet stops`,
-    {
-        factory: () => 16,
-    },
-);
+import {TUI_SHEET, TUI_SHEET_DRAGGED, TUI_SHEET_SCROLL} from '../../sheet-tokens';
+import {TuiSheetComponent} from './sheet.component';
 
 export const TUI_SHEET_PROVIDERS: Provider[] = [
     {
@@ -38,6 +24,10 @@ export const TUI_SHEET_PROVIDERS: Provider[] = [
         provide: TUI_SCROLL_REF,
         useExisting: ElementRef,
     },
+    {
+        provide: TUI_SHEET,
+        useExisting: forwardRef(() => TuiSheetComponent),
+    },
 ];
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -45,8 +35,8 @@ export function sheetDraggedFactory({
     nativeElement,
 }: ElementRef<HTMLElement>): Observable<boolean> {
     return merge(
-        typedFromEvent(nativeElement, `touchstart`, {passive: true}).pipe(mapTo(true)),
-        typedFromEvent(nativeElement, `touchend`).pipe(mapTo(false)),
+        typedFromEvent(nativeElement, 'touchstart', {passive: true}).pipe(mapTo(true)),
+        typedFromEvent(nativeElement, 'touchend').pipe(mapTo(false)),
     );
 }
 
@@ -60,8 +50,8 @@ export function sheetScrollFactory(
     return isIos
         ? iosScrollFactory(nativeElement, documentRef, ngZone)
         : merge(
-              typedFromEvent(nativeElement, `scroll`),
-              typedFromEvent(nativeElement, `load`, {capture: true}),
+              typedFromEvent(nativeElement, 'scroll'),
+              typedFromEvent(nativeElement, 'load', {capture: true}),
           ).pipe(
               map(() => nativeElement.scrollTop),
               tuiZonefree(ngZone),
