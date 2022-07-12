432f118e574f60d9e97470521aec8ca65577423c
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-12 18:07:36 +0300

    refactor(addon-commerce, addon-chart)!: remove deprecated enums (#2095)

--- projects/addon-commerce/enums/payment-system.ts
+++
@@ -1,9 +0,0 @@
-export const enum TuiPaymentSystem {
-    Visa = `visa`,
-    Electron = `electron`,
-    Mastercard = `mastercard`,
-    Maestro = `maestro`,
-    Mir = `mir`,
-}
-
-export type TuiPaymentSystemT = 'visa' | 'electron' | 'mastercard' | 'maestro' | 'mir';
