eefe1a9ed9b5b57e5133d7e73292925f23c2df6e
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-12 18:07:36 +0300

    refactor(addon-commerce, addon-chart)!: remove deprecated enums (#2095)

--- projects/addon-commerce/components/money/pipes/sign-symbol.pipe.ts
+++ projects/addon-commerce/components/money/pipes/sign-symbol.pipe.ts
@@ -1,11 +1,11 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {TuiMoneySignSymbol, TuiMoneySignT} from '@taiga-ui/addon-commerce/types';
+import {TuiMoneySign, TuiMoneySignSymbol} from '@taiga-ui/addon-commerce/types';
 
 import {tuiFormatSignSymbol} from '../utils/format-sign-symbol';
 
 @Pipe({name: `tuiSignSymbol`})
 export class TuiSignSymbolPipe implements PipeTransform {
-    transform(value: number, sign: TuiMoneySignT): TuiMoneySignSymbol {
+    transform(value: number, sign: TuiMoneySign): TuiMoneySignSymbol {
         return tuiFormatSignSymbol(value, sign);
     }
 }
