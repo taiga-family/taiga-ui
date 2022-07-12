14e41eb8c19cdec37cb9945a159d4621e9b213a2
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-12 18:07:36 +0300

    refactor(addon-commerce, addon-chart)!: remove deprecated enums (#2095)

--- projects/core/components/primitive-textfield/primitive-textfield.component.ts
+++ projects/core/components/primitive-textfield/primitive-textfield.component.ts
@@ -14,11 +14,10 @@ import {
 } from '@angular/core';
 import {
     AbstractTuiInteractive,
-    isNativeFocusedIn,
-    setNativeFocused,
     TuiContextWithImplicit,
     TuiCreditCardAutofillName,
     tuiDefaultProp,
+    tuiIsNativeFocusedIn,
     tuiPure,
 } from '@taiga-ui/cdk';
 import {
@@ -144,7 +143,7 @@ export class TuiPrimitiveTextfieldComponent
     }
 
     get focused(): boolean {
-        return isNativeFocusedIn(this.elementRef.nativeElement);
+        return tuiIsNativeFocusedIn(this.elementRef.nativeElement);
     }
 
     @HostBinding('attr.data-size')
@@ -276,7 +275,7 @@ export class TuiPrimitiveTextfieldComponent
         }
 
         event.preventDefault();
-        setNativeFocused(nativeFocusableElement);
+        nativeFocusableElement.focus();
     }
 
     onModelChange(value: string): void {
