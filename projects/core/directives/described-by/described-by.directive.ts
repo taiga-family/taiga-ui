9a1e141ea687605c1eb3a46a9ce4c40f5e2f14a2
Alex Inkin <alexander@inkin.ru>
2022-07-12 16:37:45 +0300

    Some stuff (#2102)
    * chore: some stuff
    
    * chore: fix tests
    
    * chore: fix tests

--- projects/core/directives/described-by/described-by.directive.ts
+++ projects/core/directives/described-by/described-by.directive.ts
@@ -1,5 +1,6 @@
 import {Directive, HostBinding, Inject, Input} from '@angular/core';
 import {tuiDefaultProp} from '@taiga-ui/cdk';
+import {DESCRIBED_BY} from '@taiga-ui/core/constants';
 import {TuiHintService} from '@taiga-ui/core/services';
 import {Observable} from 'rxjs';
 
@@ -8,19 +9,17 @@ import {
     TUI_DESCRIBED_BY_SHOW,
 } from './described-by.providers';
 
-export const DESCRIBED_BY = `_described-by`;
-
 /**
  * A directive linking focusable elements and hints for accessibility
  */
 @Directive({
-    selector: `[tuiDescribedBy]:not(ng-container)`,
+    selector: '[tuiDescribedBy]:not(ng-container)',
     providers: TUI_DESCRIBED_BY_PROVIDERS,
 })
 export class TuiDescribedByDirective {
     @Input()
     @tuiDefaultProp()
-    tuiDescribedBy = ``;
+    tuiDescribedBy = '';
 
     constructor(
         @Inject(TuiHintService) hintService: TuiHintService,
@@ -39,7 +38,7 @@ export class TuiDescribedByDirective {
         });
     }
 
-    @HostBinding(`attr.aria-describedby`)
+    @HostBinding('attr.aria-describedby')
     get computedDescribedBy(): string | null {
         return this.tuiDescribedBy ? this.tuiDescribedBy + DESCRIBED_BY : null;
     }
