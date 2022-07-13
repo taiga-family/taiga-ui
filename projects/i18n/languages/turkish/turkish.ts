99bff27c4c72f2c48d4c3e6cae39213813b7932e
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-13 11:25:48 +0300

    refactor!: remove deprecated entities (#2108)
    BREAKING CHANGES: remove WithDateMaskPipeConfig, TUI_DATE_MASK, TUI_DATE_RANGE_MASK, TuiProgressColorSegmentsAsyncPipe, TuiNotificationService, TuiProgressColorSegmentsPipe, COUNTRIES, Country, second argument in TuiHideSelectedPipe

--- projects/i18n/languages/turkish/turkish.ts
+++ projects/i18n/languages/turkish/turkish.ts
@@ -1,4 +1,4 @@
-import {Language} from '@taiga-ui/i18n/interfaces';
+import {TuiLanguage} from '@taiga-ui/i18n/interfaces';
 
 import {TUI_TURKISH_LANGUAGE_ADDON_COMMERCE} from './addon-commerce';
 import {TUI_TURKISH_LANGUAGE_ADDON_EDITOR} from './addon-editor';
@@ -7,7 +7,7 @@ import {TUI_TURKISH_LANGUAGE_ADDON_TABLE} from './addon-table';
 import {TUI_TURKISH_LANGUAGE_CORE} from './core';
 import {TUI_TURKISH_LANGUAGE_KIT} from './kit';
 
-export const TUI_TURKISH_LANGUAGE: Language = {
+export const TUI_TURKISH_LANGUAGE: TuiLanguage = {
     ...TUI_TURKISH_LANGUAGE_CORE,
     ...TUI_TURKISH_LANGUAGE_KIT,
     ...TUI_TURKISH_LANGUAGE_ADDON_TABLE,
