6f5746229fc2888d7e6e7478c56b314a5c2b6317
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-14 14:36:53 +0300

    feat!: update to Angular 12 (#2110)
    * feat!: update to Angular 12
    
    * feat(demo): new utility `assets` (to get URL to file from assets)
    
    * chore: run `ng update @angular/cli@12 --migrate-only production-by-default`
    
    * fix(demo-integrations): addition to `stubExternalIcons`
    
    * fix: update `@nguniversal/express-engine`-dependency

--- projects/demo/server.ts
+++ projects/demo/server.ts
@@ -1,5 +1,5 @@
 import '@ng-web-apis/universal/mocks';
-import 'zone.js/dist/zone-node';
+import 'zone.js/node';
 
 import {APP_BASE_HREF} from '@angular/common';
 import {provideLocation, provideUserAgent} from '@ng-web-apis/universal';
