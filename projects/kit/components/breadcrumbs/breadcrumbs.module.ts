ac407fb392eab602320544bc21da3b6040928aa7
vladimirpotekhin <vladimir.potekh@gmail.com>
2022-07-14 12:55:57 +0300

    chore: remove redundant modules

--- projects/kit/components/breadcrumbs/breadcrumbs.module.ts
+++ projects/kit/components/breadcrumbs/breadcrumbs.module.ts
@@ -1,27 +1,12 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {
-    TuiItemDirective,
-    TuiItemModule,
-    TuiLetModule,
-    TuiMapperPipeModule,
-} from '@taiga-ui/cdk';
-import {TuiLinkModule, TuiModeModule, TuiSvgModule} from '@taiga-ui/core';
+import {TuiItemDirective, TuiItemModule} from '@taiga-ui/cdk';
+import {TuiSvgModule} from '@taiga-ui/core';
 
 import {TuiBreadcrumbsComponent} from './breadcrumbs.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        RouterModule,
-        TuiMapperPipeModule,
-        TuiLetModule,
-        TuiLinkModule,
-        TuiSvgModule,
-        TuiModeModule,
-        TuiItemModule,
-    ],
+    imports: [CommonModule, TuiSvgModule, TuiItemModule],
     declarations: [TuiBreadcrumbsComponent],
     exports: [TuiBreadcrumbsComponent, TuiItemDirective],
 })
