0222079ceb4970625eaa2703601e145af8a54020
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-14 14:36:53 +0300

    feat!: update to Angular 12 (#2110)
    * feat!: update to Angular 12
    
    * feat(demo): new utility `assets` (to get URL to file from assets)
    
    * chore: run `ng update @angular/cli@12 --migrate-only production-by-default`
    
    * fix(demo-integrations): addition to `stubExternalIcons`
    
    * fix: update `@nguniversal/express-engine`-dependency

---
+++ projects/demo/src/utils/load-assets.ts
@@ -0,0 +1,6 @@
+export function assets(path: TemplateStringsArray): string {
+    const dirname = new URL('.', import.meta.url);
+    const relativePath = `../assets${path.join('')}`;
+
+    return new URL(relativePath, dirname).toString();
+}
