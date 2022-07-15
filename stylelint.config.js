15cf5bc12be973b044e26163e4f31088ede67116
splincode <splincodewd@yandex.ru>
2022-07-15 19:57:20 +0300

    fix(core): changed `vw/vh` to `percentage` for correct ensure viewport in safari

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,19 @@ module.exports = {
     extends: ['@tinkoff/stylelint-config/less', '@tinkoff/stylelint-config/angular'],
     ignoreFiles: ['**/demo/**', '**/dist/**', '**/coverage/**', '**/node_modules/**'],
     rules: {
+        'unit-allowed-list': [
+            'px',
+            'rem',
+            'em',
+            'deg',
+            's',
+            'ms',
+            'dpcm',
+            'turn',
+            'ch',
+            '%',
+            // 'vw/vh' have a big problem in Safari, when developers set viewport=width~1280px in Application
+        ],
         'function-no-unknown': [
             true,
             {
