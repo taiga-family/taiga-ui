3507d58bc6944a85853e3fa8b746a946aa110884
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-19 15:24:39 +0300

    chore(demo): fix Less-files in examples (#2152)

--- projects/demo/src/modules/app/utils/example-content-processor/less-processor.ts
+++ projects/demo/src/modules/app/utils/example-content-processor/less-processor.ts
@@ -1,6 +1,16 @@
 export function processLess(fileContent: string): string {
-    return fileContent.replace(
-        `@import 'taiga-ui-local';`,
-        `@import '~@taiga-ui/core/styles/taiga-ui-local';`,
+    return trimExportDefault(fileContent).replace(
+        "@import 'taiga-ui-local';",
+        "@import '~@taiga-ui/core/styles/taiga-ui-local';",
     );
+}
+
+// TODO: investigate why raw-loader add `export default "...";` to Less-files
+function trimExportDefault(fileContent: string): string {
+    return fileContent.startsWith('export default')
+        ? fileContent
+              .replace(/^export\sdefault\s["']/gi, '')
+              .replace(/['"];$/gi, '')
+              .replaceAll('\\n', '\n')
+        : fileContent;
 }
