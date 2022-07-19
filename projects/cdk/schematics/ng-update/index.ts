1ee807e86a65e70aa9a5f36f6d455fe63933389a
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-19 12:21:38 +0300

    refactor!: move fonts to separate file (#2132)
    * refactor!: move fonts to separate file
    
    * chore: remove global styles from schematic

--- projects/cdk/schematics/ng-update/index.ts
+++ projects/cdk/schematics/ng-update/index.ts
@@ -1,10 +1,5 @@
-import {Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
-import {
-    createProject,
-    DevkitFileSystem,
-    saveActiveProject,
-    setActiveProject,
-} from 'ng-morph';
+import {chain, Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
+import {createProject, saveActiveProject, setActiveProject} from 'ng-morph';
 import {TAIGA_VERSION} from '../ng-add/constants/versions';
 import {Schema} from '../ng-add/schema';
 import {replaceEnums} from './steps/replace-enums';
@@ -13,25 +8,43 @@ import {replaceConstants} from './steps/replace-const';
 import {replaceDeepImports} from './steps/replace-deep-import';
 import {showWarnings} from './steps/show-warnings';
 import {replaceServices} from './steps/replace-services';
-import {dateTimeMigrations} from './steps/migrate-date-time';
 import {migrateTemplates} from './steps/migrate-templates';
 import {migrateSliders} from './steps/migrate-sliders';
 import {removeModules} from './steps/remove-module';
 import {miscellaneousMigrations} from './steps/miscellaneous';
 import {replaceFunctions} from './steps/replace-functions';
 import {migrateProgress} from './steps/migrate-progress';
+import {DevkitFileSystem} from 'ng-morph/project/classes/devkit-file-system';
 import {FINISH_SYMBOL, START_SYMBOL, titleLog} from '../utils/colored-log';
+import {dateTimeMigrations} from './steps/migrate-date-time';
+import {addStylesToAngularJson} from '../utils/add-styles';
+import {TAIGA_THEME_FONTS} from '../constants/taiga-styles';
 import {ALL_FILES} from '../constants';
 import {getExecutionTime} from '../utils/get-execution-time';
 
-export function updateToV3(_: Schema): Rule {
+
+export function updateToV3(options: Schema): Rule {
+    const t0 = performance.now();
+
+    titleLog(
+        `\n\n${START_SYMBOL} Your packages will be updated to @taiga-ui/*@${TAIGA_VERSION}\n`,
+    );
+
+    return chain([
+        main(options),
+        addTaigaStyles(options),
+        () => {
+            const executionTime = getExecutionTime(t0, performance.now());
+
+            titleLog(
+                `${FINISH_SYMBOL} We migrated packages to @taiga-ui/*@${TAIGA_VERSION} in ${executionTime}. \n`,
+            );
+        },
+    ]);
+}
+
+function main(_: Schema): Rule {
     return async (tree: Tree, context: SchematicContext) => {
-        const t0 = performance.now();
-
-        titleLog(
-            `\n\n${START_SYMBOL} Your packages will be updated to @taiga-ui/*@${TAIGA_VERSION}\n`,
-        );
-
         const fileSystem = getFileSystem(tree);
 
         replaceDeepImports();
@@ -53,12 +66,14 @@ export function updateToV3(_: Schema): Rule {
         replaceFunctions();
         miscellaneousMigrations();
         saveActiveProject();
+    };
+}
 
-        const executionTime = getExecutionTime(t0, performance.now());
+function addTaigaStyles(options: Schema): Rule {
+    return async (_: Tree) => {
+        const taigaStyles = [TAIGA_THEME_FONTS];
 
-        titleLog(
-            `${FINISH_SYMBOL} We migrated packages to @taiga-ui/*@${TAIGA_VERSION} in ${executionTime}. \n`,
-        );
+        return addStylesToAngularJson(options, taigaStyles);
     };
 }
 
