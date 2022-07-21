d5556750691b197c76fb81689e708894103f7040
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-21 10:49:28 +0300

    chore(demo-integrations): new cypress tests for code blocks (#2166)

---
+++ projects/demo-integrations/cypress/tests/addon-doc/code.spec.ts
@@ -0,0 +1,23 @@
+import {DEFAULT_TIMEOUT_BEFORE_ACTION} from '../../support/shared.entities';
+
+describe('Code blocks', () => {
+    ['HTML', 'TypeScript', 'LESS'].forEach((tabName, i) => {
+        it(tabName, () => {
+            cy.tuiVisit('/components/line-clamp');
+            cy.get('#basic tui-tabs-with-more [tuiTab]').contains(tabName).click();
+            cy.get('#basic')
+                .wait(DEFAULT_TIMEOUT_BEFORE_ACTION)
+                .matchImageSnapshot(`01-${i}-code-block-${tabName}`);
+        });
+    });
+
+    it('API page', () => {
+        cy.tuiVisit('/components/line-clamp/Setup');
+
+        cy.get('tui-doc-code').each(($el, index) => {
+            cy.wrap($el)
+                .wait(DEFAULT_TIMEOUT_BEFORE_ACTION)
+                .matchImageSnapshot(`02-${index}-API-page`);
+        });
+    });
+});
