ab21d695381fbdbe2eb4bf62d2d7eef4414c8e2c
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-21 15:20:28 +0300

    refactor(demo): replace `raw-loader` with Webpack 5 asset modules (#2157)
    * refactor(demo): replace `raw-loader` with Webpack 5 asset modules
    
    * fix: no error when `npm run build:ssr`

--- projects/demo/src/modules/app/utils/example-content-processor/less-processor.ts
+++ projects/demo/src/modules/app/utils/example-content-processor/less-processor.ts
@@ -1,16 +1,8 @@
 export function processLess(fileContent: string): string {
-    return trimExportDefault(fileContent).replace(
-        "@import 'taiga-ui-local';",
-        "@import '~@taiga-ui/core/styles/taiga-ui-local';",
-    );
-}
-
-// TODO: investigate why raw-loader add `export default "...";` to Less-files
-function trimExportDefault(fileContent: string): string {
-    return fileContent.startsWith('export default')
-        ? fileContent
-              .replace(/^export\sdefault\s["']/gi, '')
-              .replace(/['"];$/gi, '')
-              .replaceAll('\\n', '\n')
-        : fileContent;
+    return fileContent
+        .replace(/\s\/\*# sourceMappingURL=.*$/, '')
+        .replace(
+            "@import 'taiga-ui-local';",
+            "@import '~@taiga-ui/core/styles/taiga-ui-local';",
+        );
 }
