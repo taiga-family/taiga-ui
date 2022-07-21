2aca2e4a32689a326ade4ed739f5d778d6d4bfbf
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-21 15:20:28 +0300

    refactor(demo): replace `raw-loader` with Webpack 5 asset modules (#2157)
    * refactor(demo): replace `raw-loader` with Webpack 5 asset modules
    
    * fix: no error when `npm run build:ssr`

--- projects/addon-doc/src/utils/raw-load.ts
+++ projects/addon-doc/src/utils/raw-load.ts
@@ -1,11 +1,24 @@
 import {RawLoaderContent} from '../interfaces/page';
 
+// TODO: delete it when all `!!raw-loader!` will be replaced by webpack `asset/source`.
+function trimExportDefault(fileContent: string): string {
+    return fileContent.startsWith('export default "')
+        ? fileContent
+              .replace(/^export\sdefault\s["']/gi, '')
+              .replace(/['"];$/gi, '')
+              .replaceAll('\\n', '\n')
+              .replaceAll('\\"', '"')
+        : fileContent;
+}
+
 /**
  * @deprecated: use {@link tuiRawLoad} instead
  */
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export async function rawLoad(content: RawLoaderContent): Promise<string> {
-    return content instanceof Promise ? (await content).default : content;
+    const unpacked = content instanceof Promise ? (await content).default : content;
+
+    return trimExportDefault(unpacked);
 }
 
 export const tuiRawLoad = rawLoad;
