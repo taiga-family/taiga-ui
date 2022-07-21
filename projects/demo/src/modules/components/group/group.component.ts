bed291d88396c0104ddc0218526835c9ec62fe89
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-21 15:20:28 +0300

    refactor(demo): replace `raw-loader` with Webpack 5 asset modules (#2157)
    * refactor(demo): replace `raw-loader` with Webpack 5 asset modules
    
    * fix: no error when `npm run build:ssr`

--- projects/demo/src/modules/components/group/group.component.ts
+++ projects/demo/src/modules/components/group/group.component.ts
@@ -4,46 +4,46 @@ import {TuiDocExample} from '@taiga-ui/addon-doc';
 import {TuiOrientationT, TuiSizeL} from '@taiga-ui/core';
 
 @Component({
-    selector: `example-group`,
-    templateUrl: `./group.template.html`,
-    styleUrls: [`./group.style.less`],
+    selector: 'example-group',
+    templateUrl: './group.template.html',
+    styleUrls: ['./group.style.less'],
     changeDetection,
 })
 export class ExampleTuiGroupComponent {
-    readonly exampleModule = import(`!!raw-loader!./examples/import/import-module.md`);
-    readonly exampleHtml = import(`!!raw-loader!./examples/import/insert-template.md`);
+    readonly exampleModule = import('./examples/import/import-module.md');
+    readonly exampleHtml = import('./examples/import/insert-template.md');
 
     readonly example1: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/1/index.ts`),
-        HTML: import(`!!raw-loader!./examples/1/index.html`),
-        LESS: import(`!!raw-loader!./examples/1/index.style.less`),
+        TypeScript: import('./examples/1/index.ts?raw'),
+        HTML: import('./examples/1/index.html'),
+        LESS: import('./examples/1/index.style.less'),
     };
 
     readonly example2: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/2/index.ts`),
-        HTML: import(`!!raw-loader!./examples/2/index.html`),
-        LESS: import(`!!raw-loader!./examples/2/index.style.less`),
+        TypeScript: import('./examples/2/index.ts?raw'),
+        HTML: import('./examples/2/index.html'),
+        LESS: import('./examples/2/index.style.less'),
     };
 
     readonly example3: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/3/index.ts`),
-        HTML: import(`!!raw-loader!./examples/3/index.html`),
+        TypeScript: import('./examples/3/index.ts?raw'),
+        HTML: import('./examples/3/index.html'),
     };
 
     readonly example4: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/4/index.ts`),
-        HTML: import(`!!raw-loader!./examples/4/index.html`),
+        TypeScript: import('./examples/4/index.ts?raw'),
+        HTML: import('./examples/4/index.html'),
     };
 
     adaptive = false;
     rounded = true;
     collapsed = false;
 
-    readonly orientationVariants: readonly TuiOrientationT[] = [`horizontal`, `vertical`];
+    readonly orientationVariants: readonly TuiOrientationT[] = ['horizontal', 'vertical'];
 
     orientation: TuiOrientationT = this.orientationVariants[0];
 
-    readonly sizeVariants: readonly TuiSizeL[] = [`m`, `l`];
+    readonly sizeVariants: readonly TuiSizeL[] = ['m', 'l'];
 
     size: TuiSizeL = this.sizeVariants[0];
 }
