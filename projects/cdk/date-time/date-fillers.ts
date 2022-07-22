1a602a0839c2f5bd287c8fa6df4e78a582dca06f
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-22 10:21:32 +0300

    refactor!: remove date-time related deprecations, EMPTY_VALIDATOR (#2164)

--- projects/cdk/date-time/date-fillers.ts
+++ projects/cdk/date-time/date-fillers.ts
@@ -1,27 +1,4 @@
-import {inject, InjectionToken} from '@angular/core';
-
 import {RANGE_SEPARATOR_CHAR} from './date-time';
-
-/**
- * @deprecated dont use it. See {@link TUI_DATE_FORMAT}
- */
-export const TUI_DATE_FILLER = new InjectionToken<string>(`date filler for Taiga UI`, {
-    factory: () => `dd.mm.yyyy`,
-});
-
-/**
- * @deprecated dont use it
- */
-export const TUI_DATE_RANGE_FILLER = new InjectionToken<string>(
-    `date range filler for Taiga UI`,
-    {
-        factory: () => {
-            const dateFiller = inject(TUI_DATE_FILLER);
-
-            return `${dateFiller}${RANGE_SEPARATOR_CHAR}${dateFiller}`;
-        },
-    },
-);
 
 /**
  * @internal 'dd.mm.yyyy'.length
