6e015868c3cbd0214369b31c912ca3d737bd4f50
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-22 10:21:32 +0300

    refactor!: remove date-time related deprecations, EMPTY_VALIDATOR (#2164)

--- projects/addon-mobile/components/sheet/sheet-options.ts
+++ projects/addon-mobile/components/sheet/sheet-options.ts
@@ -1,8 +1,7 @@
-import {inject, InjectionToken} from '@angular/core';
+import {InjectionToken} from '@angular/core';
 import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus';
 
 import {TuiSheet} from './sheet';
-import {TUI_SHEET_OFFSET} from './sheet-tokens';
 
 export interface TuiSheetOptions<I = undefined, O = unknown> {
     readonly image: PolymorpheusContent<TuiSheet<O, I>>;
@@ -28,6 +27,6 @@ export const TUI_SHEET_DEFAULT_OPTIONS: Omit<TuiSheetOptions, 'data'> = {
 export const TUI_SHEET_OPTIONS = new InjectionToken<Omit<TuiSheetOptions, 'data'>>(
     `Default parameters for sheet component`,
     {
-        factory: () => ({...TUI_SHEET_DEFAULT_OPTIONS, offset: inject(TUI_SHEET_OFFSET)}),
+        factory: () => ({...TUI_SHEET_DEFAULT_OPTIONS}),
     },
 );
--- projects/cdk/utils/math/in-range.ts
+++ projects/cdk/utils/math/in-range.ts
@@ -1,15 +1,10 @@
 import {tuiAssert} from '@taiga-ui/cdk/classes';
 
-/**
- * @deprecated: use {@link tuiInRange} instead
- * Checks if the value is in range
- *
- * @param value
- * @param fromInclude lower inclusive limit
- * @param toExclude upper exclusive limit
- */
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export function inRange(value: number, fromInclude: number, toExclude: number): boolean {
+export function tuiInRange(
+    value: number,
+    fromInclude: number,
+    toExclude: number,
+): boolean {
     tuiAssert.assert(!isNaN(value));
     tuiAssert.assert(!isNaN(fromInclude));
     tuiAssert.assert(!isNaN(toExclude));
@@ -17,5 +12,3 @@ export function inRange(value: number, fromInclude: number, toExclude: number): 
 
     return value >= fromInclude && value < toExclude;
 }
-
-export const tuiInRange = inRange;
