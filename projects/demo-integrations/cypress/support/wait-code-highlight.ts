3ef6e88856094b602677b44eca8c8a5409faf173
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-22 12:09:48 +0300

    chore: fix flaky cypress test

--- projects/demo-integrations/cypress/support/wait-code-highlight.ts
+++ projects/demo-integrations/cypress/support/wait-code-highlight.ts
@@ -1,3 +1,9 @@
+import {DEFAULT_TIMEOUT_BEFORE_ACTION} from './shared.entities';
+
 export function tuiWaitCodeHighlight(selector = 'code'): void {
-    cy.get(selector).should('has.class', 'hljs');
+    cy.get(selector)
+        .should('has.class', 'hljs')
+        .find('.hljs-ln-numbers')
+        .should('exist')
+        .wait(DEFAULT_TIMEOUT_BEFORE_ACTION);
 }
