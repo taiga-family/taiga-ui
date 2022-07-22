ceeaac8b7547724d881792e38a9090891e303ccf
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-22 10:01:05 +0300

    chore(demo): remove all `!!raw-loader!`-imports (#2174)

--- projects/demo/src/modules/components/calendar-range/calendar-range.component.ts
+++ projects/demo/src/modules/components/calendar-range/calendar-range.component.ts
@@ -22,26 +22,26 @@ const TWO_DOTS: [TuiColor, TuiColor] = [TuiBaseColor.Primary, TuiBaseColor.Secon
 const ONE_DOT: [TuiColor] = [TuiBaseColor.Success];
 
 @Component({
-    selector: `example-calendar-range`,
-    templateUrl: `./calendar-range.template.html`,
+    selector: 'example-calendar-range',
+    templateUrl: './calendar-range.template.html',
     changeDetection,
 })
 export class ExampleTuiCalendarRangeComponent {
-    readonly exampleModule = import(`!!raw-loader!./examples/import/import-module.md`);
+    readonly exampleModule = import('./examples/import/import-module.md?raw');
 
     readonly example1: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/1/index.ts`),
-        HTML: import(`!!raw-loader!./examples/1/index.html`),
+        TypeScript: import('./examples/1/index.ts?raw'),
+        HTML: import('./examples/1/index.html?raw'),
     };
 
     readonly example2: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/2/index.ts`),
-        HTML: import(`!!raw-loader!./examples/2/index.html`),
+        TypeScript: import('./examples/2/index.ts?raw'),
+        HTML: import('./examples/2/index.html?raw'),
     };
 
     readonly example3: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/3/index.ts`),
-        HTML: import(`!!raw-loader!./examples/3/index.html`),
+        TypeScript: import('./examples/3/index.ts?raw'),
+        HTML: import('./examples/3/index.html?raw'),
     };
 
     readonly minVariants = [
