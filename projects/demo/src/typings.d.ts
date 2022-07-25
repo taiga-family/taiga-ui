90d9be9997dac14beb3086acbe5a14662b89421a
Maksim Ivanov <omaxphp@yandex.ru>
2022-07-25 13:28:48 +0300

    chore: missing changes after rebasing (#2194)

--- projects/demo/src/typings.d.ts
+++ projects/demo/src/typings.d.ts
@@ -16,3 +16,21 @@ declare module '*?raw' {
 
     export = result;
 }
+
+declare module '*.md' {
+    const result: string;
+
+    export = result;
+}
+
+declare module '*.less' {
+    const result: string;
+
+    export = result;
+}
+
+declare module '*.ts?raw' {
+    const result: string;
+
+    export = result;
+}
