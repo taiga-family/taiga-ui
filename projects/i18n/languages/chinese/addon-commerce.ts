d6f726bdcd0fcf673ee041219f3a0e6ff059c78c
guomengyue <guomengyue@coinv.com>
2022-07-25 19:49:38 +0800

    i18n add chinese language

---
+++ projects/i18n/languages/chinese/addon-commerce.ts
@@ -0,0 +1,6 @@
+import {LanguageCommerce} from '@taiga-ui/i18n/interfaces';
+
+export const TUI_CHINESE_LANGUAGE_ADDON_COMMERCE: LanguageCommerce = {
+    cardNumber: ['号码', '信用卡号码'],
+    cardExpiry: ['有效期', '信用卡有效期'],
+};
