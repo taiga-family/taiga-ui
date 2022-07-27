bcb175b47341c1ccf190212034e7e9acccc30c9b
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-27 12:13:55 +0300

    refactor: remove deprecated re-exports (#2199)
    * refactor: remove deprecated re-exports
    
    * chore: fix comments
    
    * chore: rename function

--- projects/addon-preview/components/preview/preview-action/preview-action.directive.ts
+++ projects/addon-preview/components/preview/preview-action/preview-action.directive.ts
@@ -1,20 +1,14 @@
 import {Directive} from '@angular/core';
-import {TUI_BUTTON_OPTIONS, TuiButtonOptions} from '@taiga-ui/core';
-
-// TODO: 3.0 remove in ivy compilation
-export const PREVIEW_ACTION_OPTIONS: TuiButtonOptions = {
-    appearance: `preview-action`,
-    shape: `rounded`,
-    size: `s`,
-};
+import {tuiButtonOptionsProvider} from '@taiga-ui/core';
 
 @Directive({
-    selector: `[tuiPreviewAction]`,
+    selector: '[tuiPreviewAction]',
     providers: [
-        {
-            provide: TUI_BUTTON_OPTIONS,
-            useValue: PREVIEW_ACTION_OPTIONS,
-        },
+        tuiButtonOptionsProvider({
+            appearance: 'preview-action',
+            shape: 'rounded',
+            size: 's',
+        }),
     ],
 })
 export class TuiPreviewActionDirective {}
