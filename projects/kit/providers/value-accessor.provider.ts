1f2abd58e3d265849057ccc5eb4e24018a08d400
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-27 12:13:55 +0300

    refactor: remove deprecated re-exports (#2199)
    * refactor: remove deprecated re-exports
    
    * chore: fix comments
    
    * chore: rename function

--- projects/kit/providers/value-accessor.provider.ts
+++ projects/kit/providers/value-accessor.provider.ts
@@ -3,11 +3,8 @@ import {NG_VALUE_ACCESSOR} from '@angular/forms';
 import {TUI_VALUE_ACCESSOR} from '@taiga-ui/core';
 import {identity} from 'rxjs';
 
-// TODO: 3.0 remove in ivy compilation
-export const IDENTITY = identity;
-
 export const TUI_VALUE_ACCESSOR_PROVIDER: FactoryProvider = {
     provide: TUI_VALUE_ACCESSOR,
     deps: [[new Optional(), new Self(), NG_VALUE_ACCESSOR]],
-    useFactory: IDENTITY,
+    useFactory: identity,
 };
