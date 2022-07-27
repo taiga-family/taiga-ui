f1ec21aa1a5f49e2b2d54875335d286815a14052
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-27 12:13:55 +0300

    refactor: remove deprecated re-exports (#2199)
    * refactor: remove deprecated re-exports
    
    * chore: fix comments
    
    * chore: rename function

--- projects/core/directives/dropdown-controller/dropdown-controller.provider.ts
+++ projects/core/directives/dropdown-controller/dropdown-controller.provider.ts
@@ -1,19 +1,18 @@
 import {ChangeDetectorRef, InjectionToken, Provider} from '@angular/core';
 import {TuiDestroyService} from '@taiga-ui/cdk';
-import {watchedControllerFactory} from '@taiga-ui/core/providers';
+import {tuiWatchedControllerFactory} from '@taiga-ui/core/providers';
 
 import {TUI_DROPDOWN_CONTROLLER} from './dropdown-controller.token';
 
 export const TUI_DROPDOWN_WATCHED_CONTROLLER = new InjectionToken(
     `watched dropdown controller`,
 );
-// TODO: 3.0 remove in ivy compilation
-export const DROPDOWN_CONTROLLER_FACTORY = watchedControllerFactory;
+
 export const DROPDOWN_CONTROLLER_PROVIDER: Provider = [
     TuiDestroyService,
     {
         provide: TUI_DROPDOWN_WATCHED_CONTROLLER,
         deps: [TUI_DROPDOWN_CONTROLLER, ChangeDetectorRef, TuiDestroyService],
-        useFactory: DROPDOWN_CONTROLLER_FACTORY,
+        useFactory: tuiWatchedControllerFactory,
     },
 ];
