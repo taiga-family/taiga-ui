014ab5a8223454030243e3d5446b6d6146fd46d4
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-27 10:16:49 +0300

    feat(kit)!: `InputSlider` remove all deprecated API (#2207)
    * feat(kit)!: `InputSlider` remove all deprecated API
    
    * chore: fix width of the slider
    
    * chore(demo): fix example

--- projects/demo/src/modules/components/pagination/pagination.module.ts
+++ projects/demo/src/modules/components/pagination/pagination.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
+import {TuiTextfieldControllerModule} from '@taiga-ui/core';
 import {
     TuiBadgeModule,
     TuiInputSliderModule,
@@ -27,6 +28,7 @@ import {ExampleTuiPaginationComponent} from './pagination.component';
         FormsModule,
         ReactiveFormsModule,
         TuiAddonDocModule,
+        TuiTextfieldControllerModule,
         RouterModule.forChild(tuiGenerateRoutes(ExampleTuiPaginationComponent)),
         PolymorpheusModule,
     ],
