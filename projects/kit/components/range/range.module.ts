3f1cdc3681809139a73ab5f0d5392a764922d93f
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-27 17:28:30 +0300

    feat(kit)!: `InputRange` remove all deprecated API (#2215)

--- projects/kit/components/range/range.module.ts
+++ projects/kit/components/range/range.module.ts
@@ -4,12 +4,12 @@ import {FormsModule} from '@angular/forms';
 import {TuiFocusableModule} from '@taiga-ui/cdk';
 import {TuiSliderModule} from '@taiga-ui/kit/components/slider';
 
-import {TuiNewRangeDirective, TuiRangeComponent} from './range.component';
+import {TuiRangeComponent} from './range.component';
 import {TuiRangeChangeDirective} from './range-change.directive';
 
 @NgModule({
     imports: [CommonModule, TuiFocusableModule, TuiSliderModule, FormsModule],
-    declarations: [TuiRangeComponent, TuiRangeChangeDirective, TuiNewRangeDirective],
-    exports: [TuiRangeComponent, TuiRangeChangeDirective, TuiNewRangeDirective],
+    declarations: [TuiRangeComponent, TuiRangeChangeDirective],
+    exports: [TuiRangeComponent, TuiRangeChangeDirective],
 })
 export class TuiRangeModule {}
