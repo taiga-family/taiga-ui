c27ce643a00e2037d55f5494aaab13388f2bf4e3
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-27 12:13:55 +0300

    refactor: remove deprecated re-exports (#2199)
    * refactor: remove deprecated re-exports
    
    * chore: fix comments
    
    * chore: rename function

--- projects/cdk/directives/auto-focus/autofocus.options.ts
+++ projects/cdk/directives/auto-focus/autofocus.options.ts
@@ -22,36 +22,32 @@ export const TUI_AUTOFOCUS_HANDLER = new InjectionToken<TuiAutofocusHandler>(
     `Autofocusing handler`,
 );
 
-// TODO: 3.0 remove in ivy compilation
-export function tuiAutofocusHandlerFactory(
-    tuiFocusableComponent: TuiFocusableElementAccessor | null,
-    elementRef: ElementRef<HTMLElement>,
-    animationFrame$: Observable<number>,
-    renderer: Renderer2,
-    ngZone: NgZone,
-    windowRef: Window,
-    isIos: boolean,
-): TuiAutofocusHandler {
-    return isIos
-        ? new TuiIosAutofocusHandler(
-              tuiFocusableComponent,
-              elementRef,
-              renderer,
-              ngZone,
-              windowRef,
-          )
-        : new TuiDefaultAutofocusHandler(
-              tuiFocusableComponent,
-              elementRef,
-              animationFrame$,
-          );
-}
-
 export const TUI_AUTOFOCUS_PROVIDERS = [
     {
         provide: TUI_AUTOFOCUS_HANDLER,
         // TODO: replace to useClass in v3.0
-        useFactory: tuiAutofocusHandlerFactory,
+        useFactory: (
+            tuiFocusableComponent: TuiFocusableElementAccessor | null,
+            elementRef: ElementRef<HTMLElement>,
+            animationFrame$: Observable<number>,
+            renderer: Renderer2,
+            ngZone: NgZone,
+            windowRef: Window,
+            isIos: boolean,
+        ) =>
+            isIos
+                ? new TuiIosAutofocusHandler(
+                      tuiFocusableComponent,
+                      elementRef,
+                      renderer,
+                      ngZone,
+                      windowRef,
+                  )
+                : new TuiDefaultAutofocusHandler(
+                      tuiFocusableComponent,
+                      elementRef,
+                      animationFrame$,
+                  ),
         deps: [
             [new Optional(), new Self(), TUI_FOCUSABLE_ITEM_ACCESSOR],
             ElementRef,
--- projects/kit/components/input-date/input-date.providers.ts
+++ projects/kit/components/input-date/input-date.providers.ts
@@ -1,24 +1,11 @@
 import {forwardRef, Optional, Self} from '@angular/core';
 import {NgControl} from '@angular/forms';
-import {
-    AbstractTuiControl,
-    TUI_FOCUSABLE_ITEM_ACCESSOR,
-    TuiControlValueTransformer,
-    TuiDay,
-} from '@taiga-ui/cdk';
+import {AbstractTuiControl, TUI_FOCUSABLE_ITEM_ACCESSOR} from '@taiga-ui/cdk';
 import {LEFT_ALIGNED_DROPDOWN_CONTROLLER_PROVIDER} from '@taiga-ui/kit/providers';
 import {TUI_CALENDAR_DATA_STREAM, TUI_DATE_VALUE_TRANSFORMER} from '@taiga-ui/kit/tokens';
-import {TuiReplayControlValueChangesFactory} from '@taiga-ui/kit/utils/miscellaneous';
-import {Observable} from 'rxjs';
+import {tuiControlValueFactory} from '@taiga-ui/kit/utils/miscellaneous';
 
 import {TuiInputDateComponent} from './input-date.component';
-
-// TODO: 3.0 remove in ivy compilation
-export const DATE_STREAM_FACTORY = <T extends TuiDay>(
-    control: NgControl | null,
-    valueTransformer: TuiControlValueTransformer<T>,
-): Observable<T | null> | null =>
-    TuiReplayControlValueChangesFactory<T>(control, valueTransformer);
 
 export const TUI_INPUT_DATE_PROVIDERS = [
     {
@@ -31,7 +18,7 @@ export const TUI_INPUT_DATE_PROVIDERS = [
             [new Optional(), new Self(), NgControl],
             [new Optional(), TUI_DATE_VALUE_TRANSFORMER],
         ],
-        useFactory: DATE_STREAM_FACTORY,
+        useFactory: tuiControlValueFactory,
     },
     {
         provide: AbstractTuiControl,
