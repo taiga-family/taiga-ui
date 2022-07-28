fe30bee49177e237a46f1f21dac2031e985fa233
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-28 18:47:27 +0300

    chore(cdk): some schematic fixes (#2232)

--- projects/cdk/schematics/utils/add-styles.ts
+++ projects/cdk/schematics/utils/add-styles.ts
@@ -5,11 +5,19 @@ import {getProjectTargetOptions} from './get-project-target-options';
 import {JsonArray} from '@angular-devkit/core';
 import {updateWorkspace} from '@schematics/angular/utility/workspace';
 
-export function addStylesToAngularJson(options: Schema, taigaStyles: string[]): Rule {
+export function addStylesToAngularJson(
+    options: Schema,
+    taigaStyles: string[],
+    filter?: (styles: JsonArray | undefined) => boolean,
+): Rule {
     return updateWorkspace(workspace => {
         const project = getProject(options, workspace);
         const targetOptions = getProjectTargetOptions(project, 'build');
         const styles = targetOptions.styles as JsonArray | undefined;
+
+        if (filter && filter(styles)) {
+            return;
+        }
 
         targetOptions.styles = styles
             ? Array.from(new Set([...taigaStyles, ...styles]))
