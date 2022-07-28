1399038ed5ec7a100c3bc22260b00f7891c2bfb0
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-28 10:49:47 +0300

    refactor!: remove deprecated pluralize (#2222)

--- projects/core/types/pluralize.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Massive of three plural forms (one, few, many)
- */
-// @bad TODO: 3.0 Use Angular pluralize instead to cover for locales
-export type TuiPluralize = [string, string, string];
