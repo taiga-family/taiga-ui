96707800e2708a4f575a1e01c8d10733c0a9089d
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-07-28 15:47:43 +0300

    feat(kit)!: `Range` remove all deprecated API

--- projects/demo/src/modules/components/range/examples/4/index.ts
+++ projects/demo/src/modules/components/range/examples/4/index.ts
@@ -11,19 +11,20 @@ import {TuiKeySteps} from '@taiga-ui/kit';
     encapsulation,
 })
 export class TuiRangeExample4 {
-    readonly min = 0;
-    readonly max = 1_000_000;
     readonly ticksLabels = [`0`, `10K`, `100K`, `500k`, `1000K`];
     readonly segments = this.ticksLabels.length - 1;
+
+    // 12.5% (of total distance) per step
+    readonly stepPercentage = 100 / (2 * this.segments);
 
     value = [0, 100_000];
 
     readonly keySteps: TuiKeySteps = [
         // [percent, value]
-        [0, this.min],
+        [0, 0],
         [25, 10_000],
         [50, 100_000],
         [75, 500_000],
-        [100, this.max],
+        [100, 1_000_000],
     ];
 }
