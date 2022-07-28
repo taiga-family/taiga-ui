9290c8630db882885bfd9d2a7718d3170bceac75
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-07-28 10:49:47 +0300

    refactor!: remove deprecated pluralize (#2222)

--- projects/core/utils/format/index.ts
+++ projects/core/utils/format/index.ts
@@ -5,5 +5,3 @@ export * from './format-phone';
 export * from './get-fractional-part-padded';
 export * from './number-to-string-without-exp';
 export * from './other-decimal-symbol';
-export * from './pluralize';
-export * from './pluralize-to-icu';
