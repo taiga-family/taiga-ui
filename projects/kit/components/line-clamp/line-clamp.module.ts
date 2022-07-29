2a24854b8608ac2bdfed4f15eca44ffeb343c9e7
splincode <splincodewd@yandex.ru>
2022-07-29 14:39:31 +0300

    fix(kit): use `break-all` for `LineClamp`

--- projects/kit/components/line-clamp/line-clamp.module.ts
+++ projects/kit/components/line-clamp/line-clamp.module.ts
@@ -1,13 +1,19 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {TuiResizeModule} from '@taiga-ui/cdk';
+import {TuiLetModule, TuiResizeModule} from '@taiga-ui/cdk';
 import {TuiHintModule} from '@taiga-ui/core';
 import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
 
 import {TuiLineClampComponent} from './line-clamp.component';
 
 @NgModule({
-    imports: [CommonModule, PolymorpheusModule, TuiHintModule, TuiResizeModule],
+    imports: [
+        CommonModule,
+        PolymorpheusModule,
+        TuiHintModule,
+        TuiResizeModule,
+        TuiLetModule,
+    ],
     declarations: [TuiLineClampComponent],
     exports: [TuiLineClampComponent],
 })
