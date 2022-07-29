b970019b9af21d7d828d7b90eddace4140191de6
vladimirpotekhin <vladimir.potekh@gmail.com>
2022-07-29 18:25:51 +0300

    chore: add some warnings to migration, remove some deprecations

--- projects/i18n/languages/english/addon-editor.ts
+++ projects/i18n/languages/english/addon-editor.ts
@@ -36,6 +36,8 @@ export const TUI_ENGLISH_LANGUAGE_ADDON_EDITOR: TuiLanguageEditor = {
         superscript: `Superscript`,
         subscript: `Subscript`,
         insertTable: `Insert table`,
+        insertGroup: `Insert group`,
+        removeGroup: `Remove group`,
         mergeCells: `Merge cells`,
         splitCells: `Split cells`,
         rowsColumnsManaging: `Managing rows and columns`,
--- projects/kit/utils/files/format-size.ts
+++ projects/kit/utils/files/format-size.ts
@@ -1,4 +1,4 @@
-import {round} from '@taiga-ui/cdk';
+import {tuiRound} from '@taiga-ui/cdk';
 
 const BYTES_PER_KB = 1000;
 const BYTES_PER_MB = 1000 * BYTES_PER_KB;
@@ -23,7 +23,7 @@ export function formatSize(
         return `${(size / BYTES_PER_KB).toFixed(0)} ${units[1]}`;
     }
 
-    return `${round(size / BYTES_PER_MB, 2).toLocaleString(`ru-RU`)} ${units[2]}`;
+    return `${tuiRound(size / BYTES_PER_MB, 2).toLocaleString(`ru-RU`)} ${units[2]}`;
 }
 
 export const tuiFormatSize = formatSize;
