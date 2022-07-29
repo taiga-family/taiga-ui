137eb248f3f8c59c120455204ac8bb37fda665dc
vladimirpotekhin <vladimir.potekh@gmail.com>
2022-07-29 18:25:51 +0300

    chore: add some warnings to migration, remove some deprecations

--- projects/addon-editor/components/toolbar-new/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './toolbar-navigation-manager.directive';
-export * from './toolbar-new.component';
-export * from './toolbar-new.module';
--- projects/cdk/utils/math/test/round.spec.ts
+++ projects/cdk/utils/math/test/round.spec.ts
@@ -1,81 +1,81 @@
-import {ceil, floor, round} from '../round';
+import {tuiCeil, tuiFloor, tuiRound} from '../round';
 
 describe(`round`, () => {
     describe(`round - rounds up and down`, () => {
         it(`to the closest integer`, () => {
-            expect(round(200.134)).toBe(200);
+            expect(tuiRound(200.134)).toBe(200);
         });
 
         it(`to the closest order of magnitude`, () => {
-            expect(round(4321.12, -2)).toBe(4300);
+            expect(tuiRound(4321.12, -2)).toBe(4300);
         });
 
         it(`rounds to 0 if the order of magnitude is more than two times bigger than the number`, () => {
-            expect(round(4321.12, -4)).toBe(0);
+            expect(tuiRound(4321.12, -4)).toBe(0);
         });
 
         it(`rounds up to the order of magnitude if it is less than two times bigger`, () => {
-            expect(round(5000.12, -4)).toBe(10000);
+            expect(tuiRound(5000.12, -4)).toBe(10000);
         });
 
         it(`rounds the last digit to the precision`, () => {
-            expect(round(200.123456789, 4)).toBe(200.1235);
+            expect(tuiRound(200.123456789, 4)).toBe(200.1235);
         });
 
         it(`if precision is floating returns NaN`, () => {
-            expect(round(200.14, 1.6)).toBeNaN();
+            expect(tuiRound(200.14, 1.6)).toBeNaN();
         });
 
         it(`if value is NaN returns NaN`, () => {
-            expect(round(NaN, 1)).toBeNaN();
+            expect(tuiRound(NaN, 1)).toBeNaN();
         });
 
         it(`if value is Infinity returns Infinity`, () => {
-            expect(round(Infinity, 1)).toBe(Infinity);
+            expect(tuiRound(Infinity, 1)).toBe(Infinity);
         });
 
         it(`rounds values with exponent`, () => {
-            expect(round(123.4567e2)).toBe(12346);
+            expect(tuiRound(123.4567e2)).toBe(12346);
         });
 
         it(`rounds values with exponent to the precision`, () => {
-            expect(round(7e-20, 19)).toBe(1e-19);
+            expect(tuiRound(7e-20, 19)).toBe(1e-19);
         });
     });
 
     describe(`floor - rounds down`, () => {
         it(`to the closest integer`, () => {
-            expect(floor(200.987)).toBe(200);
+            expect(tuiFloor(200.987)).toBe(200);
         });
 
         it(`to the closest order of magnitude`, () => {
-            expect(floor(789.12, -2)).toBe(700);
+            expect(tuiFloor(789.12, -2)).toBe(700);
         });
 
         it(`rounds to 0 if the number is below set order of magnitude`, () => {
-            expect(floor(5000.12, -4)).toBe(0);
+            expect(tuiFloor(5000.12, -4)).toBe(0);
         });
 
         it(`rounds the last digit to the precision`, () => {
-            expect(floor(200.123456789, 4)).toBe(200.1234);
+            expect(tuiFloor(200.123456789, 4)).toBe(200.1234);
         });
     });
 
     describe(`ceil - rounds up`, () => {
         it(`to the closest integer`, () => {
-            expect(ceil(200.059)).toBe(201);
+            expect(tuiCeil(200.059)).toBe(201);
         });
 
         it(`to the closest order of magnitude`, () => {
-            expect(ceil(789.12, -2)).toBe(800);
+            expect(tuiCeil(789.12, -2)).toBe(800);
         });
 
         it(`to the closest order of magnitude if number is at least anything above 0`, () => {
-            expect(ceil(789.12, -4)).toBe(10000);
+            expect(tuiCeil(789.12, -4)).toBe(10000);
         });
 
         it(`rounds the last digit to the precision`, () => {
-            expect(ceil(200.123456789, 4)).toBe(200.1235);
+            expect(tuiCeil(200.123456789, 4)).toBe(200.1235);
         });
     });
 });
