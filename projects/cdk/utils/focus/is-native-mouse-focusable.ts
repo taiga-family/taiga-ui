33575477bf07672fc69294cf0bf969fbe6e04b36
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-08-04 12:28:07 +0300

    refactor!: remove redundant export of providers' factories (#2278)

--- projects/cdk/utils/focus/is-native-mouse-focusable.ts
+++ projects/cdk/utils/focus/is-native-mouse-focusable.ts
@@ -1,8 +1,5 @@
 import {tuiIsNativeKeyboardFocusable} from './is-native-keyboard-focusable';
 
-/**
- */
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export function tuiIsNativeMouseFocusable(element: Element): boolean {
     return (
         !element.hasAttribute(`disabled`) &&
