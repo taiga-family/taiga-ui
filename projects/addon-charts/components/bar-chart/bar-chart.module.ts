433b2a50a3e56a1311a5eb1ee9f9622a756f8e8f
Alex Inkin <alexander@inkin.ru>
2022-08-04 16:07:41 +0300

    feat(core): `Hint` add context input (#2273)

--- projects/addon-charts/components/bar-chart/bar-chart.module.ts
+++ projects/addon-charts/components/bar-chart/bar-chart.module.ts
@@ -3,14 +3,12 @@ import {NgModule} from '@angular/core';
 import {TuiBarSetModule} from '@taiga-ui/addon-charts/components/bar-set';
 import {TuiFocusableModule, TuiMapperPipeModule} from '@taiga-ui/cdk';
 import {TuiDescribedByModule, TuiHintModule} from '@taiga-ui/core';
-import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
 
 import {TuiBarChartComponent} from './bar-chart.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        PolymorpheusModule,
         TuiDescribedByModule,
         TuiBarSetModule,
         TuiFocusableModule,
