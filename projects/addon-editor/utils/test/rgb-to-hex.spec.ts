3c85bf2a0a50f3c6d8c6971901b0ec8be78d2cf5
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-08-04 16:50:05 +0300

    feat!: move all color-converter utils (`addon-editor` / `addon-doc` => `cdk`) (#2280)
    * feat!: move all color-converter utils (`addon-editor` / `addon-doc` => `cdk`)
    
    * chore: fix schematics

--- projects/addon-editor/utils/test/rgb-to-hex.spec.ts
+++
@@ -1,9 +0,0 @@
-import {tuiRgbToHex} from '../rgb-to-hex';
-
-describe(`rgbToHex`, () => {
-    it(`works`, () => {
-        expect(tuiRgbToHex(10, 1, 0)).toBe(`0a0100`);
-        expect(tuiRgbToHex(1, 1, 1)).toBe(`010101`);
-        expect(tuiRgbToHex(42, 237, 3)).toBe(`2aed03`);
-    });
-});
