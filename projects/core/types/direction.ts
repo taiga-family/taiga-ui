bbdd68facdf22d457734cf9024d4c6e03def098f
Alex Inkin <alexander@inkin.ru>
2022-08-04 16:07:41 +0300

    feat(core): `Hint` add context input (#2273)

--- projects/core/types/direction.ts
+++ projects/core/types/direction.ts
@@ -1,13 +1,3 @@
 export type TuiHorizontalDirection = 'left' | 'right';
 
 export type TuiVerticalDirection = 'top' | 'bottom';
-
-export type TuiSideDirection =
-    | 'bottom-left'
-    | 'bottom-right'
-    | 'bottom-middle'
-    | 'top-left'
-    | 'top-right'
-    | 'top-middle';
-
-export type TuiDirection = TuiHorizontalDirection | TuiSideDirection;
