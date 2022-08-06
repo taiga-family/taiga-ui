82f8e5d9cb3188ce0077c93bfebf2aad95c48f40
splincode <splincodewd@yandex.ru>
2022-08-06 23:03:45 +0300

    chore: forgot add export for some i18n tokes

--- projects/i18n/index.ts
+++ projects/i18n/index.ts
@@ -2,4 +2,5 @@ export * from '@taiga-ui/i18n/enums';
 export * from '@taiga-ui/i18n/interfaces';
 export * from '@taiga-ui/i18n/languages';
 export * from '@taiga-ui/i18n/switch';
+export * from '@taiga-ui/i18n/tokens';
 export * from '@taiga-ui/i18n/tools';
