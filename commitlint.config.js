32e9cd716fdc7b602f15ec324ac4624452b4c65d
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-08-08 14:45:19 +0300

    chore: `Changelog` add section `Deprecations` (#2305)
    * chore: `Changelog` add section `Deprecations`
    
    * chore: add `deprecate`-type for commitlint
    
    * feat: `commitlint` add strict checks for scope of conventional commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,20 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': () => {
+            const {readdirSync, statSync} = require('fs');
+            const dir = 'projects';
+            const projectsNames = readdirSync(dir).filter(entity =>
+                statSync(`${dir}/${entity}`).isDirectory(),
+            );
+
+            return [2, 'always', projectsNames];
+        },
+        'type-enum': () => {
+            const [level, applicable, types] = require('@commitlint/config-conventional')
+                .rules['type-enum'];
+
+            return [level, applicable, [...types, 'deprecate']];
+        },
+    },
+};
