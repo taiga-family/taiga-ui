a54c09cd9a0791ebe86be6e4f71fd3feb0a1a1d0
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-08-08 12:56:58 +0300

    chore: add `deprecate`-type for commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,14 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': async () =>
+            import('@commitlint/config-conventional')
+                .then(m => m.default)
+                .then(({rules}) => {
+                    // https://commitlint.js.org/#/reference-rules?id=rules
+                    const [level, applicable, types] = rules['type-enum'];
+
+                    return [level, applicable, [...types, 'deprecate']];
+                }),
+    },
+};
