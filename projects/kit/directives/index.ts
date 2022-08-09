f1ea369b0ab0a2c585a60af0e7d45fd15469ffc9
Alex Inkin <alexander@inkin.ru>
2022-08-09 15:11:11 +0300

    feat(kit)!: `DataListDropdownManager` move into separate module (#2317)

--- projects/kit/directives/index.ts
+++ projects/kit/directives/index.ts
@@ -1,3 +1,4 @@
+export * from '@taiga-ui/kit/directives/data-list-dropdown-manager';
 export * from '@taiga-ui/kit/directives/dropdown-context';
 export * from '@taiga-ui/kit/directives/dropdown-hover';
 export * from '@taiga-ui/kit/directives/dropdown-selection';
