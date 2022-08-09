d5277c2e0461cf9a91fd80bd8e88859c20b67805
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-08-09 15:48:12 +0300

    refactor(core)!: `tuiFormatNumber(value, configs)` new function signature (#2309)
    * refactor(core)!: `tuiFormatNumber(value, configs)` new function signature
    
    * refactor(core)!: `TuiNumberFormatSettings` add `decimalLimit`
    
    * refactor(core)!: `FormatNumberPipe` replace all optional arguments with single object
    
    * feat(core): new constant `TUI_DEFAULT_NUMBER_FORMAT`

--- projects/core/interfaces/number-format-settings.ts
+++ projects/core/interfaces/number-format-settings.ts
@@ -2,11 +2,25 @@ import {TuiDecimalSymbol} from '@taiga-ui/core/types';
 
 /**
  * Formatting configuration for displayed numbers
- * decimalSeparator - example: 100,45 (',' by default)
- * thousandSeparator - example: 360 000 (' ' by default)
  */
 export interface TuiNumberFormatSettings {
+    /**
+     * Number of digits of decimal part.
+     * @note Use `Infinity` to keep untouched.
+     */
+    readonly decimalLimit: number;
+    /**
+     * Separator between the integer and the decimal part.
+     * @example 0,42 (',' by default)
+     */
     readonly decimalSeparator: TuiDecimalSymbol;
+    /**
+     * Separator between thousands.
+     * @example 360 000 (' ' by default)
+     */
     readonly thousandSeparator: string;
+    /**
+     * Enable zeros at the end of decimal part.
+     */
     readonly zeroPadding: boolean;
 }
