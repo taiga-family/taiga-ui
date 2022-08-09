d8116849cc09b1d747e962b6bcd0d0d17ab99995
Alex Inkin <alexander@inkin.ru>
2022-08-09 15:11:11 +0300

    feat(kit)!: `DataListDropdownManager` move into separate module (#2317)

--- projects/core/components/data-list/index.ts
+++ projects/core/components/data-list/index.ts
@@ -1,6 +1,5 @@
 export * from './data-list.component';
 export * from './data-list.directive';
 export * from './data-list.module';
-export * from './data-list-dropdown-manager.directive';
 export * from './opt-group.directive';
 export * from './option/option.component';
