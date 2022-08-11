e07334323d90d3f926ed228ab5afb358ba0ba1de
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-08-11 10:58:50 +0300

    refactor!: remove deprecations (#2327)
    * refactor!: remove deprecations
    
    * fix: use `TUI_TAG_OPTIONS` for status
    
    * Revert "fix: use `TUI_TAG_OPTIONS` for status"
    
    This reverts commit d7c095050e235405bc8c8915e5d5ed6d7cb23435.

--- projects/core/utils/index.ts
+++ projects/core/utils/index.ts
@@ -3,4 +3,3 @@ export * from '@taiga-ui/core/utils/format';
 export * from '@taiga-ui/core/utils/mask';
 export * from '@taiga-ui/core/utils/miscellaneous';
 export * from '@taiga-ui/core/utils/mobile';
-export * from '@taiga-ui/core/utils/polyfills';
