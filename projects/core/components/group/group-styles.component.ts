3b8e52fc4808480f0375ec4ace47a51110119de0
Maksim Ivanov <omaxphp@yandex.ru>
2022-08-16 14:04:16 +0300

    feat!: add `@taiga-ui/styles` (#2320)

--- projects/core/components/group/group-styles.component.ts
+++ projects/core/components/group/group-styles.component.ts
@@ -2,7 +2,7 @@ import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
     template: ``,
-    styleUrls: [`../../styles/markup/tui-group.less`],
+    styleUrls: [`./group.style.less`],
     encapsulation: ViewEncapsulation.None,
 })
 export class TuiGroupStylesComponent {}
