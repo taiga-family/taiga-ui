69651217535fd292059830805840f2c756858965
Maksim Ivanov <omaxphp@yandex.ru>
2022-08-16 14:04:16 +0300

    feat!: add `@taiga-ui/styles` (#2320)

--- projects/cdk/schematics/ng-add/schema.ts
+++ projects/cdk/schematics/ng-add/schema.ts
@@ -1,6 +1,7 @@
 export interface Schema {
     readonly project: string;
     readonly addons: ReadonlyArray<string>;
+    readonly addGlobalStyles: boolean;
     readonly addAlertModule: boolean;
     readonly addDialogsModule: boolean;
     readonly addSanitizer: boolean;
--- projects/demo/src/modules/app/stackblitz/project-files/src/stackblitz.less.md
+++ projects/demo/src/modules/app/stackblitz/project-files/src/stackblitz.less.md
@@ -1,7 +1,10 @@
+```less
 /**
 THIS IS A WORKAROUND FOR STACKBLITZ:
 DO NOT REPEAT, read https://taiga-ui.dev/getting-started#less
  */
+@import 'taiga-ui-local.less';
 @import 'taiga-ui-global.less';
 @import 'theme/variables.less';
 @import 'theme/wrapper.less';
+```
