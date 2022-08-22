61251373982182b8074240c5ba4513234ddb76af
Maksim Ivanov <omaxphp@yandex.ru>
2022-08-22 13:43:10 +0300

    fix(core): character descenders overflow the line (#2387)

--- projects/demo-integrations/cypress/tests/kit/input/input.spec.ts
+++ projects/demo-integrations/cypress/tests/kit/input/input.spec.ts
@@ -1,66 +1,93 @@
 import {INPUT_PAGE_URL} from '../../../support/shared.entities';
 
 describe(`Input`, () => {
-    beforeEach(() => {
-        cy.viewport(`macbook-13`);
+    describe(`macbook-13`, () => {
+        beforeEach(() => cy.viewport(`macbook-13`));
+
+        it(`has custom content (text) + cleaner + hint which dont overlapping each others`, () => {
+            cy.tuiVisit(
+                `${INPUT_PAGE_URL}/API?tuiHintContent=Some%20content&tuiTextfieldCleaner=true&tuiTextfieldCustomContent=%3Cspan%3ELongTextContent%3Cspan%3E`,
+            );
+
+            cy.get(`#demoContent`)
+                .should(`be.visible`)
+                .matchImageSnapshot(`01-custom-text-content-cleaner-hint`);
+        });
+
+        it(`correctly aligns single custom content (as large icon)`, () => {
+            cy.tuiVisit(
+                `${INPUT_PAGE_URL}/API?tuiTextfieldCustomContent=tuiIconCalendarLarge`,
+            );
+
+            cy.get(`#demoContent`)
+                .should(`be.visible`)
+                .matchImageSnapshot(`02-custom-large-icon-content`);
+        });
+
+        it(`custom content (as large icon) + cleaner + hint dont overlapping each others`, () => {
+            cy.tuiVisit(
+                `${INPUT_PAGE_URL}/API?tuiTextfieldCleaner=true&tuiTextfieldCustomContent=tuiIconSearchLarge&tuiHintContent=Some%20content`,
+            );
+
+            cy.get(`#demoContent`)
+                .should(`be.visible`)
+                .matchImageSnapshot(`03-custom-large-icon-content-cleaner-hint`);
+        });
+
+        it(`custom content (as normal-size icon) + cleaner + hint dont overlapping each others`, () => {
+            cy.tuiVisit(
+                `${INPUT_PAGE_URL}/API?tuiTextfieldCleaner=true&tuiTextfieldCustomContent=tuiIconVisaMono&tuiHintContent=Some%20content`,
+            );
+
+            cy.get(`#demoContent`)
+                .should(`be.visible`)
+                .matchImageSnapshot(`04-custom-normal-icon-content-cleaner-hint`);
+        });
+
+        it(`input overflow due to placeholder`, () => {
+            cy.viewport(400, 812);
+            cy.tuiVisit(
+                `${INPUT_PAGE_URL}/API?tuiMode=null&icon=tuiIconSearch&iconAlign=left&tuiTextfieldExampleText=Lorem%20ipsum%20dolor%20sit%20amet,%20consectetur%20adipiscing%20elit,%20sed%20do%20eiusmod%20tempor%20incididunt%20ut%20labore&pseudoFocused=true`,
+            );
+
+            cy.get(`.t-input-wrapper`)
+                .find(`input`)
+                .focus()
+                .should(`have.value`, `111`)
+                .focused()
+                .clear()
+                .blur();
+
+            cy.matchImageSnapshot(`05-placeholder-will-be-hidden-inside`, {
+                capture: `viewport`,
+            });
+        });
     });
 
-    it(`has custom content (text) + cleaner + hint which dont overlapping each others`, () => {
-        cy.tuiVisit(
-            `${INPUT_PAGE_URL}/API?tuiHintContent=Some%20content&tuiTextfieldCleaner=true&tuiTextfieldCustomContent=%3Cspan%3ELongTextContent%3Cspan%3E`,
+    it(`character descenders overflow the line`, () => {
+        const characters = `big, placeholder, qwerty, jackson, yellow and more`;
+
+        cy.viewport(450, 300).tuiVisit(
+            `components/input/API?tuiMode=null&tuiTextfieldExampleText=${characters}`,
         );
 
         cy.get(`#demoContent`)
-            .should(`be.visible`)
-            .matchImageSnapshot(`01-custom-text-content-cleaner-hint`);
-    });
-
-    it(`correctly aligns single custom content (as large icon)`, () => {
-        cy.tuiVisit(
-            `${INPUT_PAGE_URL}/API?tuiTextfieldCustomContent=tuiIconCalendarLarge`,
-        );
-
-        cy.get(`#demoContent`)
-            .should(`be.visible`)
-            .matchImageSnapshot(`02-custom-large-icon-content`);
-    });
-
-    it(`custom content (as large icon) + cleaner + hint dont overlapping each others`, () => {
-        cy.tuiVisit(
-            `${INPUT_PAGE_URL}/API?tuiTextfieldCleaner=true&tuiTextfieldCustomContent=tuiIconSearchLarge&tuiHintContent=Some%20content`,
-        );
-
-        cy.get(`#demoContent`)
-            .should(`be.visible`)
-            .matchImageSnapshot(`03-custom-large-icon-content-cleaner-hint`);
-    });
-
-    it(`custom content (as normal-size icon) + cleaner + hint dont overlapping each others`, () => {
-        cy.tuiVisit(
-            `${INPUT_PAGE_URL}/API?tuiTextfieldCleaner=true&tuiTextfieldCustomContent=tuiIconVisaMono&tuiHintContent=Some%20content`,
-        );
-
-        cy.get(`#demoContent`)
-            .should(`be.visible`)
-            .matchImageSnapshot(`04-custom-normal-icon-content-cleaner-hint`);
-    });
-
-    it(`input overflow due to placeholder`, () => {
-        cy.viewport(400, 812);
-        cy.tuiVisit(
-            `${INPUT_PAGE_URL}/API?tuiMode=null&icon=tuiIconSearch&iconAlign=left&tuiTextfieldExampleText=Lorem%20ipsum%20dolor%20sit%20amet,%20consectetur%20adipiscing%20elit,%20sed%20do%20eiusmod%20tempor%20incididunt%20ut%20labore&pseudoFocused=true`,
-        );
-
-        cy.get(`.t-input-wrapper`)
+            .find(`.t-input-wrapper`)
+            .tuiScrollIntoView()
             .find(`input`)
+            .as(`input`);
+
+        cy.get(`@input`)
             .focus()
             .should(`have.value`, `111`)
             .focused()
             .clear()
-            .blur();
+            .matchImageSnapshot(`06-character-descenders`);
 
-        cy.matchImageSnapshot(`05-placeholder-will-be-hidden-inside`, {
-            capture: `viewport`,
-        });
+        cy.get(`@input`)
+            .type(
+                `It has been the industry's standard dummy text ever since the 1500s{enter}`,
+            )
+            .matchImageSnapshot(`07-very-long-text-without-text-ellipsis`);
     });
 });
