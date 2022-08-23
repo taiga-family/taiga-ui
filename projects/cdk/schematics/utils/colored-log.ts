4a30b73cba861beaec1ee3aa8d9fa58a0f3b9862
splincode <splincodewd@yandex.ru>
2022-08-23 15:57:53 +0300

    chore: fixup

--- projects/cdk/schematics/utils/colored-log.ts
+++ projects/cdk/schematics/utils/colored-log.ts
@@ -2,6 +2,10 @@ export function processLog(message: string): void {
     console.info('\x1b[36m%s\x1b[0m', message);
 }
 
+export function errorLog(message: string): void {
+    console.info('\x1B[31m%s\x1B[0m', message);
+}
+
 export function successLog(message: string): void {
     console.info('\x1B[32m%s\x1B[0m', message);
 }
