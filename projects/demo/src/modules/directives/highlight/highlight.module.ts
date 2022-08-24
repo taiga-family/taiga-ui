c8a4f208c95f3ebbd267f648a5159d6a029bc722
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-08-24 14:29:57 +0300

    refactor(demo): delete some legacy code (#2457)

--- projects/demo/src/modules/directives/highlight/highlight.module.ts
+++ projects/demo/src/modules/directives/highlight/highlight.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
+import {TuiTextfieldControllerModule} from '@taiga-ui/core';
 import {TuiHighlightModule, TuiInputModule} from '@taiga-ui/kit';
 
 import {TuiHighlightExample1} from './examples/1';
@@ -16,6 +17,7 @@ import {ExampleTuiHighlightComponent} from './highlight.component';
         TuiHighlightModule,
         TuiAddonDocModule,
         RouterModule.forChild(tuiGenerateRoutes(ExampleTuiHighlightComponent)),
+        TuiTextfieldControllerModule,
     ],
     declarations: [ExampleTuiHighlightComponent, TuiHighlightExample1],
     exports: [ExampleTuiHighlightComponent],
