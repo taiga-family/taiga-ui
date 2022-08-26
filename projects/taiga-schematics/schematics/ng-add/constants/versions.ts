3723a9f13b4150c2b61417b5fea39ac312ff2cca
splincode <splincodewd@yandex.ru>
2022-08-26 18:44:08 +0300

    chore: reuse schematics from @taiga-ui/cdk

--- projects/taiga-schematics/schematics/ng-add/constants/versions.ts
+++
@@ -1,6 +0,0 @@
-import {version} from '../../../package.json';
-
-export const TAIGA_VERSION = `^${version}`;
-export const NG_DOMPURIFY_VERSION = '3.0.0';
-export const DOMPURIFY_VERSION = '2.2.9';
-export const DOMPURIFY_TYPES_VERSION = '2.2.3';
--- projects/taiga-schematics/schematics/ng-add/schema.ts
+++
@@ -1,8 +0,0 @@
-export interface Schema {
-    readonly project: string;
-    readonly addGlobalStyles: boolean;
-    readonly addons: ReadonlyArray<string>;
-    readonly addAlertModule: boolean;
-    readonly addDialogsModule: boolean;
-    readonly addSanitizer: boolean;
-}
