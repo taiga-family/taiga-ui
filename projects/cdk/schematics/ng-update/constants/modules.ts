492d64c9b93b647c5c63aa77c32b560540d3fcae
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-08-29 16:27:37 +0300

    chore(cdk): tuiHintDescribe migration (#2504)

--- projects/cdk/schematics/ng-update/constants/modules.ts
+++ projects/cdk/schematics/ng-update/constants/modules.ts
@@ -7,4 +7,5 @@ export const REMOVED_MODULES: readonly RemovedModule[] = [
     {name: 'TuiResizableColumnModule', moduleSpecifier: '@taiga-ui/addon-table'},
     {name: 'ScrollIntoViewModule', moduleSpecifier: '@taiga-ui/addon-doc'},
     {name: 'TuiPreviewModule', moduleSpecifier: '@taiga-ui/proprietary-banking'},
+    {name: 'TuiDescribedByModule', moduleSpecifier: '@taiga-ui/core'},
 ];
