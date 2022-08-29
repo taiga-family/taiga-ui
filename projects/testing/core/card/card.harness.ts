3eb75f2f01bf3e6cd164d9193196bd1c2bdaf497
Zargham Khan <zarghamkhan980@gmail.com>
2022-08-29 19:01:47 +0200

    feat(testing): add card harness util

---
+++ projects/testing/core/card/card.harness.ts
@@ -0,0 +1,14 @@
+import {tuiHarnessWith, tuiWithPredicate} from '@taiga-ui/testing/utils';
+
+import {TuiSvgHarness} from '../svg/svg.harness';
+
+@tuiWithPredicate
+export class TuiCardHarness extends tuiHarnessWith<TuiCardHarness>(`tui-card`) {
+    async hasPaymentSystemLogo(): Promise<boolean> {
+        const tuiSvg = await this.locatorForOptional(
+            TuiSvgHarness.with({selector: `.t-payment-system-logo`}),
+        )();
+
+        return !!tuiSvg;
+    }
+}
--- projects/testing/core/index.ts
+++ projects/testing/core/index.ts
@@ -1,2 +1,3 @@
 export * from './button/button.harness';
+export * from './card/card.harness';
 export * from './loader/loader.harness';
