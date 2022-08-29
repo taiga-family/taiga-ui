98f9112ffb6fb8bd76f880f1fcd5b48a48f53e7f
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-08-29 11:05:23 +0300

    chore(demo): delete usage of `raw-loader`

--- projects/demo/src/modules/components/push/push.component.ts
+++ projects/demo/src/modules/components/push/push.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiDocExample} from '@taiga-ui/addon-doc';
 import {TuiAlertService} from '@taiga-ui/core';
 
@@ -7,30 +8,27 @@ import {TuiAlertService} from '@taiga-ui/core';
     selector: `example-tui-push`,
     templateUrl: `./push.template.html`,
     changeDetection,
+    encapsulation,
 })
 export class ExampleTuiPushComponent {
-    readonly exampleImportModule = import(
-        `!!raw-loader!./examples/import/import-module.md`
-    );
+    readonly exampleImportModule = import(`./examples/import/import-module.md?raw`);
 
-    readonly exampleInsertTemplate = import(
-        `!!raw-loader!./examples/import/insert-template.md`
-    );
+    readonly exampleInsertTemplate = import(`./examples/import/insert-template.md?raw`);
 
     readonly example1: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/1/index.ts`),
-        HTML: import(`!!raw-loader!./examples/1/index.html`),
-        LESS: import(`!!raw-loader!./examples/1/index.less`),
+        TypeScript: import(`./examples/1/index.ts?raw`),
+        HTML: import(`./examples/1/index.html?raw`),
+        LESS: import(`./examples/1/index.less?raw`),
     };
 
     readonly example2: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/2/index.ts`),
-        HTML: import(`!!raw-loader!./examples/2/index.html`),
+        TypeScript: import(`./examples/2/index.ts?raw`),
+        HTML: import(`./examples/2/index.html?raw`),
     };
 
     readonly example3: TuiDocExample = {
-        TypeScript: import(`!!raw-loader!./examples/3/index.ts`),
-        HTML: import(`!!raw-loader!./examples/3/index.html`),
+        TypeScript: import(`./examples/3/index.ts?raw`),
+        HTML: import(`./examples/3/index.html?raw`),
     };
 
     heading = ``;
