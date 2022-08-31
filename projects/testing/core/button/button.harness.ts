f912803ab8782a4b9e34069458f1d44739d29f0c
Zargham Khan <zarghamkhan980@gmail.com>
2022-08-31 21:48:53 +0200

    feat(testing): format file using prettier

--- projects/testing/core/button/button.harness.ts
+++ projects/testing/core/button/button.harness.ts
@@ -1,9 +1,9 @@
-import {TuiComponentHarness } from '@taiga-ui/testing/utils';
+import {TuiComponentHarness} from '@taiga-ui/testing/utils';
 
 import {TuiLoaderHarness} from '../loader/loader.harness';
 
-export class TuiButtonHarness extends TuiComponentHarness  {
-    static hostSelector = `[tuiButton]`
+export class TuiButtonHarness extends TuiComponentHarness {
+    static hostSelector = `[tuiButton]`;
     private readonly loader = this.locatorForOptional(TuiLoaderHarness);
 
     async isLoading(): Promise<boolean> {
