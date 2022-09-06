f4869b9b54f7c0cf878af7f57eae66258a3a58cc
Maksim Ivanov <omaxphp@yandex.ru>
2022-09-06 18:13:04 +0300

    ci: fix build problem

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -6,24 +6,28 @@ import {
     processLog,
     successLog,
 } from '../projects/cdk/schematics/utils/colored-log';
-import {getValueByFlag, hasFlag} from './shared/argv.utils';
+import {getValueByFlag} from './shared/argv.utils';
 
+const isDryRun =
+    getValueByFlag<'true' | 'false' | 'undefined'>(`--dry-run`, `false`) === `true`;
 const path = getValueByFlag<string>(`--path`, ``);
 const dryRun = hasFlag(`--dry-run`);
 
 (async function main(): Promise<void> {
     const packageJson = await import(resolve(path, `package.json`));
 
-    infoLog(`name: ${packageJson.name}`);
-    infoLog(`version: ${packageJson.version}`);
-
-    if (versionExists()) {
-        successLog(`${packageJson.name}@${packageJson.version} is already published`);
+    if (versions.includes(packageJson.version) && !isDryRun) {
+        errorLog(`${packageJson.name}@${packageJson.version} is already published`);
 
         return;
     }
 
-    const command = `npm publish ${path} ${makeAdditionalFlags()} --access public`;
+    infoLog(`name: ${packageJson.name}`);
+    infoLog(`version: ${packageJson.version}`);
+
+    const dry = isDryRun ? `--dry-run` : ``;
+    const tag = makeTag(packageJson.version, versions);
+    const command = `npm publish ${path} ${tag} ${dry} --access public`;
 
     processLog(command);
     execSync(command, {stdio: `inherit`, encoding: `utf8`});
@@ -44,3 +48,17 @@ const dryRun = hasFlag(`--dry-run`);
         return `${tagNextFlag} ${dryRunFlag}`.trim();
     }
 })();
+
+function getAllVersions(name: string): string[] {
+    return JSON.parse(
+        execSync(`npm view ${name} versions --json || echo "[]"`).toString(),
+    );
+}
+
+function makeTag(version: string, versions: string[]): string {
+    const currentMajor = parseInt(version);
+    const maxMajorVersion = Math.max(...versions.map(x => parseInt(x)), currentMajor);
+    const tagFlag = maxMajorVersion > currentMajor ? `--tag v${currentMajor}-lts` : ``;
+
+    return version.includes(`rc`) ? `--tag next` : tagFlag;
+}
