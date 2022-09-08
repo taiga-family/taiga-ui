003c897046dfe3515aa31b7bd96c68f9283b19ec
Maksim Ivanov <omaxphp@yandex.ru>
2022-09-08 18:31:19 +0300

    chore: add additional description for help (#2650)

--- projects/cdk/services/destroy.service.ts
+++ projects/cdk/services/destroy.service.ts
@@ -2,7 +2,12 @@ import {Injectable, OnDestroy} from '@angular/core';
 import {ReplaySubject} from 'rxjs';
 
 /**
+ * @note:
  * Observable abstraction over ngOnDestroy to use with takeUntil
+ *
+ * Why we use `ReplaySubject` instead of `Subject`?
+ * Well, we’ll use ReplaySubject to emit the last message in case
+ * the subscription is ended after the component is destroyed.
  */
 @Injectable()
 export class TuiDestroyService extends ReplaySubject<void> implements OnDestroy {
