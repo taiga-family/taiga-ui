b373af1c3bf62908a69d276166957b5e0771579b
Maksim Ivanov <omaxphp@yandex.ru>
2022-09-09 14:32:48 +0300

    ci: improved logs (#2653)

--- scripts/shared/sync-versions.ts
+++ scripts/shared/sync-versions.ts
@@ -1,6 +1,7 @@
 import {readFileSync, writeFileSync} from 'fs';
 import {glob} from 'glob';
 
+import {processLog, successLog} from '../../projects/cdk/schematics/utils/colored-log';
 import {updatePackageJsonStructure} from './update-package-json-structure';
 
 const INDENTATION = 4;
@@ -18,20 +19,37 @@ export function syncVersions(
 
     const files = patterns
         .map(pattern => glob.sync(pattern, {ignore: `**/node_modules/**`}))
-        .flatMap(files => files);
+        .flatMap(files => files)
+        .filter(file => !file.includes(`node_modules`));
 
     for (const file of files) {
-        const packageJson = JSON.parse(readFileSync(file).toString());
-        const isPackageLockFile = file.endsWith(`-lock.json`);
+        const originalJSON = `${JSON.stringify(
+            JSON.parse(readFileSync(file).toString()),
+            null,
+            INDENTATION,
+        )}`;
+        const packageJson = JSON.parse(originalJSON);
+        const prevVersion = packageJson.version;
+
+        if (!prevVersion) {
+            continue;
+        }
 
         updatePackageJsonStructure({
+            isPackageLockFile: file.endsWith(`-lock.json`),
             packageJson,
-            prevVersion: packageJson.version,
+            prevVersion,
             newVersion,
-            isPackageLockFile,
             ignores,
         });
-        writeFileSync(file, `${JSON.stringify(packageJson, null, INDENTATION)}\n`);
-        console.info(`\x1B[32m%s\x1B[0m`, `[synchronized]:`, file);
+
+        const updatedJSON = `${JSON.stringify(packageJson, null, INDENTATION)}`;
+
+        if (originalJSON !== updatedJSON) {
+            writeFileSync(file, `${updatedJSON}\n`);
+            successLog(`[synchronized]: ${file}`);
+        } else {
+            processLog(`[no changes]: ${file}`);
+        }
     }
 }
