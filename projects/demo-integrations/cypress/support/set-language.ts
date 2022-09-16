0563991930b75a872ff6809452753b003932ff8f
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-09-16 13:49:47 +0300

    chore(demo-integrations): fix util `tuiSetLanguage`

--- projects/demo-integrations/cypress/support/set-language.ts
+++ projects/demo-integrations/cypress/support/set-language.ts
@@ -1,6 +1,6 @@
 export function tuiSetLanguage(language: string): void {
     cy.get(`tui-language-switcher`, {log: false}).click({log: false});
-    cy.get(`tui-dropdown [tuiOption]`, {log: false})
+    cy.get(`tui-dropdown-box [tuiOption]`, {log: false})
         .contains(language, {matchCase: false, log: false})
         .click({force: true, log: false});
 }
