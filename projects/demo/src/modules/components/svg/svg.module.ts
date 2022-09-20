07d9aeee9cf6b8b54644469b118fb0a3cfa60e63
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-09-20 17:35:35 +0300

    chore(demo): `Svg` improve documentation (#2737)

--- projects/demo/src/modules/components/svg/svg.module.ts
+++ projects/demo/src/modules/components/svg/svg.module.ts
@@ -2,7 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
-import {TuiLinkModule, TuiSvgModule} from '@taiga-ui/core';
+import {TuiLinkModule, TuiNotificationModule, TuiSvgModule} from '@taiga-ui/core';
 import {TuiPresentModule} from '@taiga-ui/kit';
 
 import {TuiSvgExample1} from './examples/1';
@@ -17,6 +17,7 @@ import {ExampleTuiSvgComponent} from './svg.component';
         TuiPresentModule,
         TuiLinkModule,
         TuiAddonDocModule,
+        TuiNotificationModule,
         RouterModule.forChild(tuiGenerateRoutes(ExampleTuiSvgComponent)),
     ],
     declarations: [ExampleTuiSvgComponent, TuiSvgExample1, TuiSvgExample2],
