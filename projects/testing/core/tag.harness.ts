ffb33a3432d6ed29040a2c583733afc126626395
Zargham Khan <zarghamkhan980@gmail.com>
2022-09-21 22:41:37 +0200

    feat(testing): add more methods to tag harness

--- projects/testing/core/tag.harness.ts
+++ projects/testing/core/tag.harness.ts
@@ -1,3 +1,4 @@
+import {TestElement, TestKey} from '@angular/cdk/testing';
 import {TuiComponentHarness} from '@taiga-ui/testing/utils';
 
 import {TuiSvgHarness} from './svg.harness';
@@ -5,11 +6,35 @@ import {TuiSvgHarness} from './svg.harness';
 export class TuiTagHarness extends TuiComponentHarness {
     static hostSelector = `tui-tag`;
 
-    async hasCrossIcon(): Promise<boolean> {
+    async getCrossIcon(): Promise<TuiSvgHarness | null> {
         const svg = await this.locatorForOptional(
             TuiSvgHarness.with({selector: `.t-icon`}),
         )();
 
-        return !!svg;
+        return svg;
+    }
+
+    async getTagDiv(): Promise<TestElement> {
+        const div = await this.locatorFor(`:first-child`)();
+
+        return div;
+    }
+
+    async getInput(): Promise<TestElement | null> {
+        const input = await this.locatorForOptional(`input.t-edit`)();
+
+        return input;
+    }
+
+    async sendEnter(): Promise<void> {
+        return await (await this.host()).sendKeys(TestKey.ENTER);
+    }
+
+    async sendBackspace(): Promise<void> {
+        return await (await this.host()).sendKeys(TestKey.BACKSPACE);
+    }
+
+    async sendDelete(): Promise<void> {
+        return await (await this.host()).sendKeys(TestKey.DELETE);
     }
 }
