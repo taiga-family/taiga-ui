31f4541c2223d35823fdc3f7197e17e5ed3df1a8
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2022-09-27 10:13:12 +0000

    Deploying to gh-pages from @ Tinkoff/taiga-ui@8c078689ee01aa9a242237b70ecd628a329a3fe8 🚀

---
+++ 6688.2b98ffef6d1a3decb443.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[6688],{6688:n=>{n.exports="```ts\nimport {TuiHighlightModule} from '@taiga-ui/kit';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiHighlightModule,\n  ],\n  // ...\n})\nexport class MyModule {}\n```\n"}}]);
