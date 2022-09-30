7111a54e073560c4cc78322b0879e226b28abd15
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-09-30 13:57:34 +0300

    refactor(demo): `Stackblitz` drop legacy code (#2772)

--- projects/addon-doc/src/utils/raw-load.ts
+++ projects/addon-doc/src/utils/raw-load.ts
@@ -1,18 +1,5 @@
 import {RawLoaderContent} from '../interfaces/page';
 
-// TODO: delete it when all `!!raw-loader!` will be replaced by webpack `asset/source`.
-function trimExportDefault(fileContent: string): string {
-    return fileContent.startsWith(`export default "`)
-        ? fileContent
-              .replace(/^export\sdefault\s["']/gi, ``)
-              .replace(/['"];$/gi, ``)
-              .replaceAll(`\\n`, `\n`)
-              .replaceAll(`\\"`, `"`)
-        : fileContent;
-}
-
 export async function tuiRawLoad(content: RawLoaderContent): Promise<string> {
-    const unpacked = content instanceof Promise ? (await content).default : content;
-
-    return trimExportDefault(unpacked);
+    return content instanceof Promise ? (await content).default : content;
 }
