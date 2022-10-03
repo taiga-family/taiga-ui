68e081ed949f9bd614e080f624cf21d5602b3894
Alvaro Martinez <alvaro.mart.mart+dev@gmail.com>
2022-10-03 10:10:12 +0200

    refactor(kit): export MarkerIconHarness

--- projects/testing/core/index.ts
+++ projects/testing/core/index.ts
@@ -3,5 +3,6 @@ export * from './badge.harness';
 export * from './button.harness';
 export * from './island.harness';
 export * from './loader.harness';
+export * from './marker-icon.harness';
 export * from './primitive-textfield.harness';
 export * from './svg.harness';
