02713d63ebfd7290420df66d37b5919db9138c3c
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-10-05 19:25:34 +0400

    feat(kit): `Badge` add overflow elipsis and title (#2806)

--- projects/kit/components/badge/badge.module.ts
+++ projects/kit/components/badge/badge.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {TuiResizeModule} from '@taiga-ui/cdk';
 
 import {TuiBadgeComponent} from './badge.component';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, TuiResizeModule],
     declarations: [TuiBadgeComponent],
     exports: [TuiBadgeComponent],
 })
