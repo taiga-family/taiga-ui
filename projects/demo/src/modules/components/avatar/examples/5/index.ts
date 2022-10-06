d45328dd77bd7b995c0b858bfce4a7846e757b79
waterplea <alexander@inkin.ru>
2022-10-06 18:12:23 +0300

    feat(kit): `Avatar` allow Taiga UI icons

---
+++ projects/demo/src/modules/components/avatar/examples/5/index.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+
+@Component({
+    selector: `tui-avatar-example-5`,
+    templateUrl: `./index.html`,
+    changeDetection,
+    encapsulation,
+})
+export class TuiAvatarExample5 {}
--- projects/kit/components/avatar/avatar.module.ts
+++ projects/kit/components/avatar/avatar.module.ts
@@ -1,11 +1,12 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {TuiSvgModule} from '@taiga-ui/core';
 import {TuiLazyLoadingModule} from '@taiga-ui/kit/directives';
 
 import {TuiAvatarComponent} from './avatar.component';
 
 @NgModule({
-    imports: [CommonModule, TuiLazyLoadingModule],
+    imports: [CommonModule, TuiLazyLoadingModule, TuiSvgModule],
     declarations: [TuiAvatarComponent],
     exports: [TuiAvatarComponent],
 })
