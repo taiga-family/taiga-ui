65d5756e4ccaa0ae9547308d1add0fd1e2dd83e4
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-10-13 16:01:45 +0400

    chore(demo): add request imitation to TuiFiles example (#2892)

--- projects/demo/src/modules/components/input-files/examples/1/index.ts
+++ projects/demo/src/modules/components/input-files/examples/1/index.ts
@@ -3,7 +3,8 @@ import {FormControl} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiFileLike} from '@taiga-ui/kit';
-import {Subject} from 'rxjs';
+import {Observable, of, Subject, timer} from 'rxjs';
+import {finalize, map, switchMap} from 'rxjs/operators';
 
 @Component({
     selector: `tui-input-files-example-1`,
@@ -13,7 +14,12 @@ import {Subject} from 'rxjs';
 })
 export class TuiInputFilesExample1 {
     readonly control = new FormControl();
+
     readonly rejectedFiles$ = new Subject<TuiFileLike | null>();
+    readonly loadingFiles$ = new Subject<TuiFileLike | null>();
+    readonly loadedFiles$ = this.control.valueChanges.pipe(
+        switchMap(file => (file ? this.makeRequest(file) : of(null))),
+    );
 
     onReject(file: TuiFileLike | readonly TuiFileLike[]): void {
         this.rejectedFiles$.next(file as TuiFileLike);
@@ -24,6 +30,24 @@ export class TuiInputFilesExample1 {
     }
 
     clearRejected(): void {
+        this.removeFile();
         this.rejectedFiles$.next(null);
+    }
+
+    makeRequest(file: TuiFileLike): Observable<TuiFileLike | null> {
+        this.loadingFiles$.next(file);
+
+        return timer(1000).pipe(
+            map(() => {
+                if (Math.random() > 0.5) {
+                    return file;
+                }
+
+                this.rejectedFiles$.next(file);
+
+                return null;
+            }),
+            finalize(() => this.loadingFiles$.next(null)),
+        );
     }
 }
