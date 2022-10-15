10e989fb8ab5bae7f19d156878272e84ea78ed0b
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2022-10-15 14:40:06 +0000

    deploy: 7292a709f8df4a3b41d729f6a66b4bb706d3649f

--- next/20554.22312f1c3360db9d9738.js
+++ next/20554.22312f1c3360db9d9738.js
@@ -4,7 +4,7 @@
 /***/ 20554:
 /***/ ((module) => {
 
-module.exports = ".field-large {\n    min-height: 9.375rem;\n}\n\n.field-small {\n    min-height: 3.5rem;\n}\n";
+module.exports = ".field-large {\n    min-height: 9.375rem;\n}\n\n.field-small {\n    min-height: 3.5rem;\n}\n\n.field-medium {\n    min-height: 5rem;\n}\n";
 
 /***/ })
 
