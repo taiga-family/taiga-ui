bb92e9cbf2f8d3e4dd78691eab289a911543086e
waterplea <alexander@inkin.ru>
2022-10-17 11:33:10 +0000

    Update snapshots/demo/next/main to output generated at 71225b0

---
+++ 45592.7fcc7c44bf40d6f73620.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_taiga_ui_components"] = self["webpackChunk_taiga_ui_components"] || []).push([[45592],{
+
+/***/ 45592:
+/***/ ((module) => {
+
+module.exports = "```html\n<head>\n  <link\n    href=\"https://fonts.googleapis.com/css2?family=Manrope:wght@500;800&display=swap\"\n    rel=\"stylesheet\"\n  />\n</head>\n<header class=\"header\">\n  <h1>\n    <img\n      src=\"https://raw.githubusercontent.com/Tinkoff/taiga-ui/1c73e97155678e2616726866484db8ccad4cdb4f/projects/demo/src/assets/images/taiga.svg\"\n      alt=\"taiga ui logo\"\n      width=\"24\"\n    />\n    Taiga UI\n  </h1>\n\n  <iframe\n    src=\"https://ghbtns.com/github-btn.html?user=Tinkoff&repo=taiga-ui&type=star&count=true&size=large\"\n    frameborder=\"0\"\n    scrolling=\"0\"\n    width=\"170\"\n    height=\"30\"\n    title=\"GitHub\"\n  ></iframe>\n</header>\n<my-app>Loading Taiga UI...</my-app>\n```\n";
+
+/***/ })
+
+}]);
