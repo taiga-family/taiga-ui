ccea6d4a45d7f008c75282bd56483ecc7d72ff6e
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2022-10-20 09:56:11 +0000

    Update snapshots/demo/next/main to output generated at 3a14454

---
+++ 90903.caaa64fbf8fdb8264617.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_taiga_ui_components"] = self["webpackChunk_taiga_ui_components"] || []).push([[90903],{
+
+/***/ 90903:
+/***/ ((module) => {
+
+module.exports = "<tui-calendar></tui-calendar>\n";
+
+/***/ })
+
+}]);
