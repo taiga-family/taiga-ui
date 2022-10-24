e17346d9b51426e92b9e460a01955955b19c9d48
Maksim Ivanov <omaxphp@yandex.ru>
2022-10-24 17:51:50 +0500

    fix(kit): `TextArea` fix height for size `m` (#2952)

---
+++ projects/demo-integrations/cypress/tests/core/text-area/text-area.cy.ts
@@ -0,0 +1,8 @@
+describe(`Textarea`, () => {
+    for (const size of [`m`, `l`]) {
+        it(`size of ${size}`, () => {
+            cy.tuiVisit(`components/text-area/API?tuiMode=null&tuiTextfieldSize=${size}`);
+            cy.get(`#demoContent`).should(`be.visible`).matchImageSnapshot();
+        });
+    }
+});
