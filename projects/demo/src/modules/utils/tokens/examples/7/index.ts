21b129d0971e853dff0df0f2bf3d58f8a350c32c
Daniil Dubrava <d.dubrava@tinkoff.ru>
2022-10-28 22:24:09 +0300

    docs: add TUI_IS_MOBILE_RES to tokens page (#2976)

--- projects/demo/src/modules/utils/tokens/examples/7/index.ts
+++ projects/demo/src/modules/utils/tokens/examples/7/index.ts
@@ -1,7 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TUI_MONTHS} from '@taiga-ui/core';
+import {TUI_IS_MOBILE_RES} from '@taiga-ui/core';
 import {Observable} from 'rxjs';
 
 @Component({
@@ -11,5 +11,5 @@ import {Observable} from 'rxjs';
     encapsulation,
 })
 export class TuiTokensExample7 {
-    constructor(@Inject(TUI_MONTHS) readonly months$: Observable<string[]>) {}
+    constructor(@Inject(TUI_IS_MOBILE_RES) readonly isMobileRes$: Observable<boolean>) {}
 }
