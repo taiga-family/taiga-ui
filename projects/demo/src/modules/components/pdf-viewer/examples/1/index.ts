e9c78d45c42384cd13dae57685d613d7e3d96949
splincode <splincodewd@yandex.ru>
2022-10-28 19:23:35 +0500

    chore(demo): add example where use 3rd-party service for mobile pdf

--- projects/demo/src/modules/components/pdf-viewer/examples/1/index.ts
+++ projects/demo/src/modules/components/pdf-viewer/examples/1/index.ts
@@ -2,6 +2,7 @@ import {Component, Inject} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
+import {TUI_IS_MOBILE} from '@taiga-ui/cdk';
 import {TuiPdfViewerOptions, TuiPdfViewerService} from '@taiga-ui/kit';
 import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus';
 
@@ -12,15 +13,28 @@ import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus';
     encapsulation,
 })
 export class TuiPdfViewerExample1 {
+    private readonly pdf = `assets/media/taiga.pdf`;
+
     constructor(
         @Inject(DomSanitizer) private readonly sanitizer: DomSanitizer,
         @Inject(TuiPdfViewerService) private readonly pdfService: TuiPdfViewerService,
+        @Inject(TUI_IS_MOBILE) private readonly isMobile: boolean,
     ) {}
 
+    /**
+     * @description:
+     * Embedded PDFs in mobile doesn't work,
+     * so you can use third-party services
+     * or your own service to render PDF in mobile iframe
+     */
     show(actions: PolymorpheusContent<TuiPdfViewerOptions>): void {
         this.pdfService
             .open(
-                this.sanitizer.bypassSecurityTrustResourceUrl(`/assets/media/taiga.pdf`),
+                this.sanitizer.bypassSecurityTrustResourceUrl(
+                    this.isMobile
+                        ? `https://drive.google.com/viewerng/viewer?embedded=true&url=https://taiga-ui.dev/${this.pdf}`
+                        : this.pdf,
+                ),
                 {
                     label: `Taiga UI`,
                     actions,
