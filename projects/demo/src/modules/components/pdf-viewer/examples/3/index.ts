4bedc4771092f36f4374c4de1abd222fde81b769
Maksim Ivanov <omaxphp@yandex.ru>
2022-10-31 15:54:34 +0500

    chore(demo): add example where use 3rd-party service for mobile pdf (#2980)

--- projects/demo/src/modules/components/pdf-viewer/examples/3/index.ts
+++ projects/demo/src/modules/components/pdf-viewer/examples/3/index.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
+import {TUI_IS_MOBILE} from '@taiga-ui/cdk';
 
 @Component({
     selector: `tui-pdf-viewer-example-3`,
@@ -10,5 +12,24 @@ import {encapsulation} from '@demo/emulate/encapsulation';
     encapsulation,
 })
 export class TuiPdfViewerExample3 {
+    private readonly pdf = `assets/media/taiga.pdf`;
+
     open = false;
+
+    /**
+     * @description:
+     * Embedded PDFs in mobile doesn't work,
+     * so you can use third-party services
+     * or your own service to render PDF in mobile iframe
+     */
+    readonly src = this.sanitizer.bypassSecurityTrustResourceUrl(
+        this.isMobile
+            ? `https://drive.google.com/viewerng/viewer?embedded=true&url=https://taiga-ui.dev/${this.pdf}`
+            : this.pdf,
+    );
+
+    constructor(
+        @Inject(TUI_IS_MOBILE) private readonly isMobile: boolean,
+        @Inject(DomSanitizer) private readonly sanitizer: DomSanitizer,
+    ) {}
 }
