4f6c04768bc5f6550bcacf5e16446ff93dbe0608
splincode <splincodewd@yandex.ru>
2022-11-03 13:01:08 +0500

    feat(addon-editor): improve ux for edit links

--- projects/addon-editor/components/edit-link/edit-link.module.ts
+++ projects/addon-editor/components/edit-link/edit-link.module.ts
@@ -1,11 +1,19 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {TuiAutoFocusModule, TuiLetModule} from '@taiga-ui/cdk';
-import {TuiButtonModule, TuiLinkModule, TuiSvgModule} from '@taiga-ui/core';
+import {TuiAutoFocusModule} from '@taiga-ui/cdk';
+import {
+    TuiButtonModule,
+    TuiDataListModule,
+    TuiLinkModule,
+    TuiScrollbarModule,
+    TuiSvgModule,
+} from '@taiga-ui/core';
 import {TuiInputInlineModule, TuiToggleModule} from '@taiga-ui/kit';
 
 import {TuiEditLinkComponent} from './edit-link.component';
+import {TuiFilterAnchorsPipe} from './pipes/filter-anchors.pipe';
+import {TuiShortUrlPipe} from './pipes/short-url.pipe';
 
 @NgModule({
     imports: [
@@ -17,9 +25,10 @@ import {TuiEditLinkComponent} from './edit-link.component';
         TuiLinkModule,
         TuiInputInlineModule,
         TuiToggleModule,
-        TuiLetModule,
+        TuiDataListModule,
+        TuiScrollbarModule,
     ],
-    declarations: [TuiEditLinkComponent],
+    declarations: [TuiEditLinkComponent, TuiShortUrlPipe, TuiFilterAnchorsPipe],
     exports: [TuiEditLinkComponent],
 })
 export class TuiEditLinkModule {}
