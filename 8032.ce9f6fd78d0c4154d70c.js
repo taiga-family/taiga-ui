f0039acbcd1ff6c0d1257e76da56f4e279332ab8
splincode <omaxphp@yandex.ru>
2022-11-15 16:24:29 +0000

    Update snapshots/demo/next/main to output generated at c91fcb9

---
+++ 8032.ce9f6fd78d0c4154d70c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[8032],{8032:e=>{e.exports=".editor {\n    ::ng-deep .tui-editor-socket .ProseMirror {\n        min-height: 10rem;\n    }\n}\n"}}]);
