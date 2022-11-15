44d3b6be310a0ff14c1f03b97e8369cd6a8d8593
splincode <splincode@users.noreply.github.com>
2022-11-15 07:20:32 +0000

    deploy: db4a29bf617b574815db7df13ffc319f1208dd35

---
+++ next/47322.e1d8ffd7efd35509a5aa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[47322],{47322:n=>{n.exports="```ts\nimport {TuiIsPresentPipeModule} from '@taiga-ui/cdk';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiIsPresentPipeModule,\n  ],\n  // ...\n})\nexport class MyModule {}\n```\n"}}]);
