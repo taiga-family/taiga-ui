e40fbc8e800d2c945a5d7844586c6a22cebd7c81
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-11-17 15:10:11 +0300

    chore(demo): `Stackblitz` use fixed versions from `cdk` & `kit` packages

--- projects/demo/src/modules/app/stackblitz/stackblitz-deps.constants.ts
+++ projects/demo/src/modules/app/stackblitz/stackblitz-deps.constants.ts
@@ -1,4 +1,11 @@
 import {TUI_VERSION} from '@taiga-ui/cdk';
+// eslint-disable-next-line @taiga-ui/no-deep-imports
+import CDK_PACKAGE from '@taiga-ui/cdk/package.json';
+// eslint-disable-next-line @taiga-ui/no-deep-imports
+import KIT_PACKAGE from '@taiga-ui/kit/package.json';
+
+const CDK_DEPS = CDK_PACKAGE.dependencies;
+const KIT_DEPS = KIT_PACKAGE.dependencies;
 
 const ANGULAR_VERSION = `14.x.x`;
 const TAIGA_VERSION = `${TUI_VERSION.split(`.`)[0]}.x.x`;
@@ -28,12 +35,13 @@ export const STACKBLITZ_DEPS: Record<string, string> = {
     '@taiga-ui/addon-table': TAIGA_VERSION,
     '@taiga-ui/addon-tablebars': TAIGA_VERSION,
     '@tinkoff/ng-dompurify': `*`,
-    '@tinkoff/ng-polymorpheus': `*`,
-    '@ng-web-apis/common': `*`,
-    '@tinkoff/ng-event-plugins': `*`,
-    '@ng-web-apis/intersection-observer': `*`,
-    '@ng-web-apis/mutation-observer': `*`,
-    'text-mask-core': `*`,
+    '@tinkoff/ng-polymorpheus': CDK_DEPS[`@tinkoff/ng-polymorpheus`],
+    '@ng-web-apis/common': CDK_DEPS[`@ng-web-apis/common`],
+    '@tinkoff/ng-event-plugins': CDK_DEPS[`@tinkoff/ng-event-plugins`],
+    '@ng-web-apis/intersection-observer': KIT_DEPS[`@ng-web-apis/intersection-observer`],
+    '@ng-web-apis/resize-observer': CDK_DEPS[`@ng-web-apis/resize-observer`],
+    '@ng-web-apis/mutation-observer': CDK_DEPS[`@ng-web-apis/mutation-observer`],
+    'text-mask-core': KIT_DEPS[`text-mask-core`],
     dompurify: `*`,
     '@types/dompurify': `*`,
     'prosemirror-state': `*`,
