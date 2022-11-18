4e7b5c264fd8aff32e46b1dd7ab2490c1252ef48
Tomáš Gordoň <tomasgordon.sk@gmail.com>
2022-11-18 13:04:22 +0100

    feat: add DI values token for `tui-input-files`

--- projects/kit/tokens/index.ts
+++ projects/kit/tokens/index.ts
@@ -2,6 +2,7 @@ export * from './calendar-date-stream';
 export * from './date-inputs-value-transformers';
 export * from './i18n';
 export * from './input-date-options';
+export * from './input-files-content';
 export * from './items-handlers';
 export * from './mobile-calendar';
 export * from './month-formatter';
