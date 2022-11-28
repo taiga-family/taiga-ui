c61af8a69d7b1a0cf8176600626da8a363fca94a
splincode <splincode@users.noreply.github.com>
2022-11-28 15:44:18 +0000

    deploy: 945cf2f75d12758baa52ccc6c8167e981174d026

---
+++ next/5900.d462c706bb2bfe7dbe15.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[5900],{5900:n=>{n.exports="```ts\nimport {Component} from '@angular/core';\nimport {TuiDestroyService} from '@taiga-ui/cdk';\n\n@Component({\n  // ...\n  providers: [TuiDestroyService],\n})\nexport class MyComponent {}\n```\n"}}]);
---
+++ next/81576.82244442879615baafa1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[81576],{81576:n=>{n.exports="```ts\nimport {TuiTilesModule} from '@taiga-ui/kit';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiTilesModule,\n  ],\n  // ...\n})\nexport class MyModule {}\n```\n"}}]);
