442eebd8db93cddea06e11f38c46e125c9cd8f29
waterplea <alexander@inkin.ru>
2022-12-03 08:59:15 +0000

    Update snapshots/demo/next/main to output generated at 91e9f5f

--- 87608.e203029c4919de37fb39.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[87608],{87608:i=>{i.exports='<tui-tiles\n    class="tiles"\n    [(items)]="items"\n>\n    <tui-tile\n        *ngFor="let item of items"\n        tuiTileHandle\n    >\n        <div class="tile">{{ item }}</div>\n    </tui-tile>\n</tui-tiles>\n'}}]);
