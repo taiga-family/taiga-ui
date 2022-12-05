c79a7888cbdbf20686137cd1dd25116feacc612f
Maksim Ivanov <omaxphp@yandex.ru>
2022-12-05 19:36:57 +0500

    refactor: for investigate problem on stackblitz (#3167)

--- projects/cdk/directives/element/element.directive.ts
+++ projects/cdk/directives/element/element.directive.ts
@@ -4,8 +4,26 @@ import {Directive, ElementRef, Inject} from '@angular/core';
     selector: `[tuiElement]`,
     exportAs: `elementRef`,
 })
-export class TuiElementDirective<T extends Element = HTMLElement> extends ElementRef<T> {
+export class TuiElementDirective<T extends Element = HTMLElement>
+    implements ElementRef<T>
+{
+    nativeElement!: T;
+
     constructor(@Inject(ElementRef) {nativeElement}: ElementRef<T>) {
-        super(nativeElement);
+        /**
+         * @note:
+         * Typically, when your constructor is invoked with new,
+         * an object is created, its constructor is assigned to
+         * the invoked constructor and the object is then assigned
+         * to this before executing any operations specified
+         * in your constructor method.
+         *
+         * ERROR TypeError: Class constructor ElementRef cannot be invoked without 'new'
+         * https://github.com/Tinkoff/taiga-ui/issues/3072
+         *
+         * This way we can instantiate object creation
+         * without additional prototype chain for possible fix bug.
+         */
+        return new ElementRef<T>(nativeElement);
     }
 }
