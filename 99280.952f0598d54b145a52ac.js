18b89af2173b5e9812ffc60c8311515db4d07017
splincode <omaxphp@yandex.ru>
2022-12-08 12:10:58 +0000

    Update snapshots/demo/next/main to output generated at adcbbb8

---
+++ 99280.952f0598d54b145a52ac.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[99280],{99280:n=>{n.exports="<p>Formatted number by default: {{ phone | tuiFormatPhone }}</p>\n\n<p>\n    Formatted number with custom params:\n    {{ phone | tuiFormatPhone : undefined : '###-###-##-##' }}\n</p>\n"}}]);
