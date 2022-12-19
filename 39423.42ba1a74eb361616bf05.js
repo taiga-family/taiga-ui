a60a1664d809a529eb814829c83eb7f1bfa749c1
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2022-12-19 13:18:05 +0000

    deploy: b5bfe4f060f85daad81bc9743cad7e98e8599dca

--- 39423.42ba1a74eb361616bf05.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[39423],{39423:t=>{t.exports='<tui-editor\n    class="editor"\n    [formControl]="control"\n    [tools]="builtInTools"\n></tui-editor>\n\n<h4>HTML:</h4>\n<tui-editor-socket [content]="control.value || \'\'"></tui-editor-socket>\n'}}]);
