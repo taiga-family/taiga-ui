cc67478b67b6c6b39ea34caea8e2fb6b755537d9
Maksim Ivanov <omaxphp@yandex.ru>
2022-12-27 13:52:55 +0500

    ci: no duplicate attributes (#3328)

--- scripts/eslint/html.js
+++ scripts/eslint/html.js
@@ -6,6 +6,7 @@ module.exports = {
             parser: '@html-eslint/parser',
             rules: {
                 '@html-eslint/id-naming-convention': ['error', 'kebab-case'],
+                '@html-eslint/no-duplicate-attrs': 'error',
                 'spaced-comment': 'off',
             },
         },
