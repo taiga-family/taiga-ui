1d0d86e446cf42a501885491e7dd706533ce0522
splincode <omaxphp@yandex.ru>
2023-01-09 12:49:27 +0000

    Update snapshots/demo/next/main to output generated at 0d4f52a

--- 14065.142149130e1c4891d381.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[14065],{14065:n=>{n.exports='<button\n    tuiButton\n    (click)="stream$.next()"\n>\n    Show/Hide\n</button>\n'}}]);
---
+++ 98517.62db8f4ee4c6988cf6ff.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[98517],{98517:t=>{t.exports='<button\n    tuiButton\n    type="button"\n>\n    Options\n</button>\n'}}]);
