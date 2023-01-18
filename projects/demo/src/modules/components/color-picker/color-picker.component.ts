43af244431683341d675a7ee2ffc6e4e930814f4
splincode <splincodewd@yandex.ru>
2023-01-18 23:34:48 +0500

    chore(demo): code example is wrong for `example3`

--- projects/demo/src/modules/components/color-picker/color-picker.component.ts
+++ projects/demo/src/modules/components/color-picker/color-picker.component.ts
@@ -23,7 +23,7 @@ export class ExampleTuiColorPickerComponent {
     };
 
     readonly example3: TuiDocExample = {
-        TypeScript: import('./examples/2/index.ts?raw'),
-        HTML: import('./examples/2/index.html?raw'),
+        TypeScript: import('./examples/3/index.ts?raw'),
+        HTML: import('./examples/3/index.html?raw'),
     };
 }
