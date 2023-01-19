038e6d708e61805c5847ee5b57798766b394d099
splincode <splincodewd@yandex.ru>
2023-01-19 15:40:52 +0500

    feat(kit): support override padding for items in carousel

---
+++ projects/demo-integrations/cypress/tests/desktop/kit/carousel/carousel.cy.ts
@@ -0,0 +1,15 @@
+describe(`Carousel`, () => {
+    it(`padding: default`, () => {
+        cy.tuiVisit(`components/carousel/API`);
+        cy.get(`#demo-content`)
+            .should(`be.visible`)
+            .matchImageSnapshot(`carousel-padding-default`);
+    });
+
+    it(`padding: 0`, () => {
+        cy.tuiVisit(`components/carousel/API?style.--tui-carousel-item-padding=0`);
+        cy.get(`#demo-content`)
+            .should(`be.visible`)
+            .matchImageSnapshot(`carousel-padding-0`);
+    });
+});
