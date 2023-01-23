1a39ed1e759005b31e011d61fa6c193967270a71
Maksim Ivanov <omaxphp@yandex.ru>
2023-01-23 12:58:30 +0500

    fix(core): top border of tuiTable first row's td is not visible (#3454)

---
+++ projects/demo-integrations/cypress/tests/desktop/kit/input/input-table.cy.ts
@@ -0,0 +1,45 @@
+import {WAIT_BEFORE_SCREENSHOT} from '@demo-integrations/support/properties/shared.entities';
+
+describe(`InputTable`, () => {
+    it(`editing fields inside a table`, () => {
+        cy.tuiVisit(`components/input#table`);
+        cy.get(`tui-doc-example[heading="Table"]`)
+            .findByAutomationId(`tui-doc-example`)
+            .tuiScrollIntoView()
+            .as(`wrapper`);
+
+        for (const index of [0, 1, 2, 3, 4]) {
+            cy.get(`@wrapper`)
+                .find(`tbody tr td`)
+                .eq(index)
+                .findByAutomationId(`tui-primitive-textfield__wrapper`)
+                .find(`input`)
+                .eq(0)
+                .focus();
+
+            cy.get(`@wrapper`)
+                .wait(WAIT_BEFORE_SCREENSHOT)
+                .matchImageSnapshot(`components/input_table__${index}_1`);
+
+            cy.get(`@wrapper`).find(`thead tr th`).eq(index).click();
+
+            cy.get(`@wrapper`)
+                .wait(WAIT_BEFORE_SCREENSHOT)
+                .matchImageSnapshot(`components/input_table__${index}_2`);
+
+            if (index > 0) {
+                cy.get(`@wrapper`)
+                    .find(`tbody tr td`)
+                    .eq(index - 1)
+                    .findByAutomationId(`tui-primitive-textfield__wrapper`)
+                    .find(`input`)
+                    .eq(0)
+                    .focus();
+
+                cy.get(`@wrapper`)
+                    .wait(WAIT_BEFORE_SCREENSHOT)
+                    .matchImageSnapshot(`components/input_table__${index}_3`);
+            }
+        }
+    });
+});
