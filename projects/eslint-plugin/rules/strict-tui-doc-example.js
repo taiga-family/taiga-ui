31231dc634ffcf7dc8501663b9d3a39a2377c521
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2023-01-31 15:39:09 +0300

    chore: fix extension regex (#3544)

--- projects/eslint-plugin/rules/strict-tui-doc-example.js
+++ projects/eslint-plugin/rules/strict-tui-doc-example.js
@@ -27,7 +27,7 @@ const fileNameToExtension = {
  * @param {string} path
  * @returns {RegExpMatchArray | null}
  */
-const getPathGroups = path => path.match(/(.+)(\.(?:ts|less|scss|js|html))(.*)/);
+const getPathGroups = path => path.match(/(.+)(\.(?:ts|less|scss|js|md|css|html))(.*)/);
 
 /**
  * @type {import(`eslint`).Rule.RuleModule}
