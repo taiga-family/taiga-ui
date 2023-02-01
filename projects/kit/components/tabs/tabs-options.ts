122f46ae3eaedec8fe3e914f35c4287c7b4e91c5
waterplea <alexander@inkin.ru>
2023-02-01 20:15:47 +0800

    chore: add `minMoreWidth` to tabs options

--- projects/kit/components/tabs/tabs-options.ts
+++ projects/kit/components/tabs/tabs-options.ts
@@ -4,12 +4,14 @@ export interface TuiTabsOptions {
     readonly underline: boolean;
     readonly exposeActive: boolean;
     readonly itemsLimit: number;
+    readonly minMoreWidth: number;
 }
 
 export const TUI_TABS_DEFAULT_OPTIONS: TuiTabsOptions = {
     underline: true,
     exposeActive: true,
     itemsLimit: Infinity,
+    minMoreWidth: 0,
 };
 
 export const TUI_TABS_OPTIONS = new InjectionToken<TuiTabsOptions>(
