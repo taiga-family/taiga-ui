8b169311b5430aa45b4277bb3a025ecdaa003573
Maksim Ivanov <omaxphp@yandex.ru>
2023-02-03 12:57:08 +0300

    chore(demo): improve docs (#3574)

--- projects/demo/src/modules/markup/icons/customization/customization-icons.component.ts
+++ projects/demo/src/modules/markup/icons/customization/customization-icons.component.ts
@@ -1,11 +1,20 @@
 import {Component} from '@angular/core';
-import {TUI_SVG_SRC_PROCESSOR} from '@taiga-ui/core';
+import {TUI_SANITIZER, TUI_SVG_SRC_PROCESSOR} from '@taiga-ui/core';
+import {NgDompurifySanitizer} from '@tinkoff/ng-dompurify';
 
 @Component({
     selector: 'customization-icons-example',
     templateUrl: './customization-icons.template.html',
     styleUrls: ['./customization-icons.style.less'],
     providers: [
+        /**
+         * @note:
+         * required for rendering inline svg sources
+         */
+        {
+            provide: TUI_SANITIZER,
+            useClass: NgDompurifySanitizer,
+        },
         {
             provide: TUI_SVG_SRC_PROCESSOR,
             useFactory: () => {
@@ -13,7 +22,8 @@ import {TUI_SVG_SRC_PROCESSOR} from '@taiga-ui/core';
                     const myCustomPrefix = 'icons8::';
 
                     return src.startsWith(myCustomPrefix)
-                        ? `assets/icons8/${src.replace(myCustomPrefix, '')}.svg`
+                        ? // load svg as inline source
+                          `assets/icons8/${src.replace(myCustomPrefix, '')}.svg`
                         : src;
                 };
             },
