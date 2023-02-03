9e85ed7a7c024b501367b08ca90c90a1e8c4f161
Maksim Ivanov <omaxphp@yandex.ru>
2023-02-03 14:31:19 +0300

    chore: fix dropdown.cy.ts

--- projects/demo-integrations/cypress/tests/core/dropdown/dropdown.cy.ts
+++ projects/demo-integrations/cypress/tests/core/dropdown/dropdown.cy.ts
@@ -1,22 +1,23 @@
 describe(`Dropdown`, () => {
-    beforeEach(() => {
-        cy.viewport(720, 720);
-    });
+    beforeEach(() => cy.viewport(720, 720));
 
     it(`TuiDropdown`, () => {
-        cy.tuiVisit(`/directives/dropdown`);
+        cy.tuiVisit(`directives/dropdown`);
 
-        cy.get(`tui-dropdown-example-1 button`).first().click().wait(1000);
-        cy.matchImageSnapshot(`1`);
+        cy.get(`tui-dropdown-example-1 button`).first().tuiScrollIntoView().click();
+        cy.tuiWaitBeforeScreenshot().matchImageSnapshot(`1`);
 
-        cy.get(`tui-dropdown-example-2 input`).first().click().wait(1000);
-        cy.matchImageSnapshot(`2`);
+        cy.get(`tui-dropdown-example-2 input`).first().tuiScrollIntoView().click();
+        cy.tuiWaitBeforeScreenshot().matchImageSnapshot(`2`);
     });
 
     it(`Hosted Dropdown`, () => {
-        cy.tuiVisit(`/components/hosted-dropdown`);
+        cy.tuiVisit(`components/hosted-dropdown`);
 
-        cy.get(`tui-hosted-dropdown-example-1 button`).first().click().wait(1000);
-        cy.matchImageSnapshot(`3`);
+        cy.get(`tui-hosted-dropdown-example-1 button`)
+            .first()
+            .tuiScrollIntoView()
+            .click();
+        cy.tuiWaitBeforeScreenshot().matchImageSnapshot(`3`);
     });
 });
