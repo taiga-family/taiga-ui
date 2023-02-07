9b16d1ba20d41d8f3c44fd12bcf1289c90f5e15e
splincode <splincode@users.noreply.github.com>
2023-02-07 09:16:22 +0000

    deploy: 9189ea97fda903c90622950b4034eb9ade8565ec

---
+++ 19175.50bc0a0f4818c961adf5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[19175],{19175:n=>{n.exports='<tui-elastic-container class="container">\n    <div\n        contenteditable\n        class="editable"\n    >\n        Editable content\n    </div>\n</tui-elastic-container>\n'}}]);
---
+++ 26012.866acc4bec8cbc114d4c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[26012],{26012:e=>{e.exports=".reversed {\n    flex-direction: row-reverse;\n    margin-top: 1rem;\n}\n"}}]);
