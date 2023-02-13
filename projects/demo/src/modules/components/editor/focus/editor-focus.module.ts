be04427c0309126d5d993ebef541777ffeed39d8
Maksim Ivanov <omaxphp@yandex.ru>
2023-02-13 15:33:40 +0300

    feat(addon-editor): support focus extension (#3649)

---
+++ projects/demo/src/modules/components/editor/focus/editor-focus.module.ts
@@ -0,0 +1,35 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
+import {TuiEditorModule, TuiEditorSocketModule} from '@taiga-ui/addon-editor';
+import {TuiActiveZoneModule} from '@taiga-ui/cdk';
+import {
+    TuiButtonModule,
+    TuiHostedDropdownModule,
+    TuiLinkModule,
+    TuiSvgModule,
+} from '@taiga-ui/core';
+
+import {ExampleTuiEditorFocusComponent} from './editor-focus.component';
+import {TuiEditorFocusExample1} from './examples/1';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        TuiActiveZoneModule,
+        TuiSvgModule,
+        TuiLinkModule,
+        TuiHostedDropdownModule,
+        TuiButtonModule,
+        TuiAddonDocModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TuiEditorModule,
+        TuiEditorSocketModule,
+        RouterModule.forChild(tuiGenerateRoutes(ExampleTuiEditorFocusComponent)),
+    ],
+    declarations: [TuiEditorFocusExample1, ExampleTuiEditorFocusComponent],
+})
+export class ExampleTuiEditorFocusModule {}
