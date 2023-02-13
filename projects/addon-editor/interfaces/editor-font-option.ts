ceded78a019b2f29bc87800784cc70e08d35877e
splincode <splincodewd@yandex.ru>
2023-02-13 12:07:35 +0300

    feat(addon-editor): customize css classes for font-tool in toolbar

--- projects/addon-editor/interfaces/editor-font-option.ts
+++ projects/addon-editor/interfaces/editor-font-option.ts
@@ -6,4 +6,5 @@ export interface TuiEditorFontOption {
     family?: string;
     weight?: string;
     headingLevel?: 1 | 2 | 3 | 4 | 5 | 6;
+    ngClass?: Record<string, any> | Set<string> | string[] | string;
 }
