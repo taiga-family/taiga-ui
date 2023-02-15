5e7841d5f3c9d2f64c60a9d0d207435737f31943
splincode <omaxphp@yandex.ru>
2023-02-15 09:51:39 +0000

    Update snapshots/demo/next/main to output generated at 6d37c0f

--- 10899.caa35621007ec1768f3e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[10899],{10899:n=>{n.exports=".hint {\n    color: var(--tui-base-05);\n    height: 100%;\n    display: flex;\n    align-items: center;\n}\n"}}]);
