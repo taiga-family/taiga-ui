9134a4b2f430e13ffe9fbc48d1f5359d298e8ea6
waterplea <alexander@inkin.ru>
2023-02-21 19:05:40 +0800

    chore: fix test

--- projects/core/directives/dropdown/dropdown-driver.directive.ts
+++ projects/core/directives/dropdown/dropdown-driver.directive.ts
@@ -1,6 +1,7 @@
-import {Directive} from '@angular/core';
+import {Directive, Inject, Self} from '@angular/core';
 import {TuiDestroyService} from '@taiga-ui/cdk';
-import {AbstractTuiDriverDirective} from '@taiga-ui/core/abstract';
+import {AbstractTuiDriverDirective, TuiDriver, TuiVehicle} from '@taiga-ui/core/abstract';
+import {Observable} from 'rxjs';
 
 @Directive({
     selector: '[tuiDropdown]',
@@ -8,4 +9,13 @@ import {AbstractTuiDriverDirective} from '@taiga-ui/core/abstract';
 })
 export class TuiDropdownDriverDirective extends AbstractTuiDriverDirective {
     readonly type = 'dropdown';
+
+    // TODO: Figure out why this is necessary under nx test runner
+    constructor(
+        @Self() @Inject(TuiDestroyService) destroy$: Observable<unknown>,
+        @Inject(TuiDriver) drivers: TuiDriver[],
+        @Inject(TuiVehicle) vehicles: TuiVehicle[],
+    ) {
+        super(destroy$, drivers, vehicles);
+    }
 }
--- projects/core/directives/hint/hint-driver.directive.ts
+++ projects/core/directives/hint/hint-driver.directive.ts
@@ -1,6 +1,7 @@
-import {Directive} from '@angular/core';
+import {Directive, Inject, Self} from '@angular/core';
 import {TuiDestroyService} from '@taiga-ui/cdk';
-import {AbstractTuiDriverDirective} from '@taiga-ui/core/abstract';
+import {AbstractTuiDriverDirective, TuiDriver, TuiVehicle} from '@taiga-ui/core/abstract';
+import {Observable} from 'rxjs';
 
 @Directive({
     selector: '[tuiHint]',
@@ -8,4 +9,13 @@ import {AbstractTuiDriverDirective} from '@taiga-ui/core/abstract';
 })
 export class TuiHintDriverDirective extends AbstractTuiDriverDirective {
     readonly type = 'hint';
+
+    // TODO: Figure out why this is necessary under nx test runner
+    constructor(
+        @Self() @Inject(TuiDestroyService) destroy$: Observable<unknown>,
+        @Inject(TuiDriver) drivers: TuiDriver[],
+        @Inject(TuiVehicle) vehicles: TuiVehicle[],
+    ) {
+        super(destroy$, drivers, vehicles);
+    }
 }
