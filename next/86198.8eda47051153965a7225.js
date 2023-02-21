b62a84a0f86a70a573db6da6d9d856e23a4599b5
nsbarsukov <nsbarsukov@users.noreply.github.com>
2023-02-21 07:28:27 +0000

    deploy: e9e4e1a349ea0b7aec1bcc0455a9247d1f3d6d98

---
+++ next/86198.8eda47051153965a7225.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[86198],{86198:n=>{n.exports="```bash\nnx migrate @taiga-ui/cdk\nnx migrate --run-migrations=migrations.json\n```\n"}}]);
