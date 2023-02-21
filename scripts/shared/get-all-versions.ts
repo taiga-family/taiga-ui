d0fba6a49d29c99f5175ec7b1cc9c370a5192db8
splincode <splincodewd@yandex.ru>
2023-02-21 13:15:55 +0300

    chore: fix script

--- scripts/shared/get-all-versions.ts
+++ scripts/shared/get-all-versions.ts
@@ -1,9 +1,13 @@
 import {execute} from './execute';
 
 export function getAllVersions(name: string): string[] {
-    const versions: string[] | string = JSON.parse(
-        execute(`npm view ${name} versions --json || echo "[]"`, {}),
-    );
+    try {
+        const versions: string[] | string = JSON.parse(
+            execute(`npm view ${name} versions --json || echo "[]"`, {}),
+        );
 
-    return Array.isArray(versions) ? versions : [versions];
+        return Array.isArray(versions) ? versions : [versions];
+    } catch {
+        return [];
+    }
 }
