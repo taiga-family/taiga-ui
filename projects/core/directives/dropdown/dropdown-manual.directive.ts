c8cfd43478e07e437b9bcf53806d03871aa44489
waterplea <alexander@inkin.ru>
2023-02-22 13:01:59 +0800

    chore: fix test

--- projects/core/directives/dropdown/dropdown-manual.directive.ts
+++ projects/core/directives/dropdown/dropdown-manual.directive.ts
@@ -1,13 +1,13 @@
 import {Directive, Input, OnChanges} from '@angular/core';
 import {tuiAsDriver, TuiDriver} from '@taiga-ui/core/abstract';
-import {Subject} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 
 @Directive({
     selector: '[tuiDropdown][tuiDropdownManual]',
     providers: [tuiAsDriver(TuiDropdownManualDirective)],
 })
 export class TuiDropdownManualDirective extends TuiDriver implements OnChanges {
-    private readonly stream$ = new Subject<boolean>();
+    private readonly stream$ = new BehaviorSubject(false);
 
     @Input()
     tuiDropdownManual = false;
