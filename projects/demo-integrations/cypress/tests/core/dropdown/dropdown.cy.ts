79b25d73765f6116bf8da85380f2c43c3e697c76
splincode <splincodewd@yandex.ru>
2023-02-22 14:32:21 +0300

    fix(core): escape event doesn't work in Safari

--- projects/demo-integrations/cypress/tests/core/dropdown/dropdown.cy.ts
+++ projects/demo-integrations/cypress/tests/core/dropdown/dropdown.cy.ts
@@ -1,22 +1,56 @@
 describe(`Dropdown`, () => {
-    beforeEach(() => {
-        cy.viewport(720, 720);
+    describe(`720x720`, () => {
+        beforeEach(() => cy.viewport(720, 720));
+
+        it(`TuiDropdown`, () => {
+            cy.tuiVisit(`/directives/dropdown`);
+
+            cy.get(`tui-dropdown-example-1 button`).first().click().wait(1000);
+            cy.matchImageSnapshot(`1`);
+
+            cy.get(`tui-dropdown-example-2 input`).first().click().wait(1000);
+            cy.matchImageSnapshot(`2`);
+        });
+
+        it(`Hosted Dropdown`, () => {
+            cy.tuiVisit(`/components/hosted-dropdown`);
+
+            cy.get(`tui-hosted-dropdown-example-1 button`).first().click().wait(1000);
+            cy.matchImageSnapshot(`3`);
+        });
     });
 
-    it(`TuiDropdown`, () => {
-        cy.tuiVisit(`/directives/dropdown`);
+    describe(`a12y`, () => {
+        beforeEach(() => cy.viewport(1280, 720));
 
-        cy.get(`tui-dropdown-example-1 button`).first().click().wait(1000);
-        cy.matchImageSnapshot(`1`);
+        it(`Esc -> Hosted Dropdown`, () => {
+            cy.tuiVisit(`/components/hosted-dropdown`);
 
-        cy.get(`tui-dropdown-example-2 input`).first().click().wait(1000);
-        cy.matchImageSnapshot(`2`);
-    });
+            cy.get(`tui-hosted-dropdown-example-2 button`)
+                .eq(1)
+                .tuiWaitBeforeAction()
+                .click();
 
-    it(`Hosted Dropdown`, () => {
-        cy.tuiVisit(`/components/hosted-dropdown`);
+            cy.tuiWaitBeforeScreenshot().matchImageSnapshot(`01-opened-hosted-dropdown`, {
+                capture: `viewport`,
+            });
 
-        cy.get(`tui-hosted-dropdown-example-1 button`).first().click().wait(1000);
-        cy.matchImageSnapshot(`3`);
+            cy.get(`tui-dropdown`)
+                .findByAutomationId(`tui-primitive-textfield__native-input`)
+                .tuiWaitBeforeAction()
+                .click();
+
+            cy.tuiWaitBeforeScreenshot().matchImageSnapshot(`02-opened-hosted-dropdown`, {
+                capture: `viewport`,
+            });
+
+            cy.realPress(`Escape`)
+                .tuiWaitBeforeScreenshot()
+                .matchImageSnapshot(`03-opened-hosted-dropdown`, {capture: `viewport`});
+
+            cy.realPress(`Escape`)
+                .tuiWaitBeforeScreenshot()
+                .matchImageSnapshot(`04-opened-hosted-dropdown`, {capture: `viewport`});
+        });
     });
 });
