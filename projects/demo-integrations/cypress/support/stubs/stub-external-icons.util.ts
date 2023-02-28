e14f68b9efcc63f7baf7ae251a05e12616309081
splincode <splincodewd@yandex.ru>
2023-02-28 22:58:37 +0300

    ci: stub `raw.github*`

--- projects/demo-integrations/cypress/support/stubs/stub-external-icons.util.ts
+++ projects/demo-integrations/cypress/support/stubs/stub-external-icons.util.ts
@@ -2,6 +2,7 @@
 export const EXTERNAL_ICONS = [
     `web-api.svg`,
     `avatars.githubusercontent.com`,
+    `raw.githubusercontent.com`,
     `.*ng-polymorpheus.*logo.svg`,
     `bf2e94ae58ee713717faf397958a8e3d.jpg`, // filename - MD5 hash value of file content (waterplea avatar)
     `avatar.jpg`,
