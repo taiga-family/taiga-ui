87001d6fd85091da8add5af914c171ce22459951
Dima Puris <d.puris@tinkoff.ru>
2023-03-08 11:06:21 +0400

    fix(demo): fix bug with unactivated router-outlet

--- projects/demo/src/modules/customization/routable/eager/routable-dialog.component.ts
+++ projects/demo/src/modules/customization/routable/eager/routable-dialog.component.ts
@@ -1,10 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, Inject, Self} from '@angular/core';
+import {Event, NavigationEnd, Router} from '@angular/router';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDocExample} from '@taiga-ui/addon-doc';
+import {TuiDestroyService} from '@taiga-ui/cdk';
+import {filter, map, shareReplay, takeUntil} from 'rxjs/operators';
 
 @Component({
     selector: 'routable-dialog',
     templateUrl: './routable-dialog.template.html',
+    providers: [TuiDestroyService],
     changeDetection,
 })
 export class RoutableDialogComponent {
@@ -27,4 +31,24 @@ export class RoutableDialogComponent {
     readonly addRouterOutlet = import('./examples/setup/add-router-outlet.md?raw');
     readonly importModule = import('./examples/setup/import-module.md?raw');
     readonly useRouteGenerator = import('./examples/setup/use-route-generator.md?raw');
+
+    /**
+     * Helps to destroy router-outlet for named outlet example
+     */
+    readonly isNamedOutletPage$ = this.router.events.pipe(
+        filter(isNavigationEndEvent),
+        map(event => event.url.includes('NamedOutlet')),
+        shareReplay({refCount: true, bufferSize: 1}),
+    );
+
+    constructor(
+        @Inject(Router) private readonly router: Router,
+        @Inject(TuiDestroyService) @Self() destroy$: TuiDestroyService,
+    ) {
+        this.isNamedOutletPage$.pipe(takeUntil(destroy$)).subscribe();
+    }
+}
+
+function isNavigationEndEvent<T>(event: Event | T): event is NavigationEnd {
+    return event instanceof NavigationEnd;
 }
