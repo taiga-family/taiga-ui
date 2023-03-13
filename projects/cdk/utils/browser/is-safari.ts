b095d5812989de4c10cc511bd67381075d7c68e1
splincode <splincodewd@yandex.ru>
2023-03-13 17:41:59 +0300

    fix(cdk): correct detect that is safari

--- projects/cdk/utils/browser/is-safari.ts
+++ projects/cdk/utils/browser/is-safari.ts
@@ -1,6 +1,15 @@
-export function tuiIsSafari(element: Element): boolean {
-    const documentRef = element.ownerDocument;
-    const windowRef = documentRef?.defaultView;
+export function tuiIsSafari({ownerDocument: documentRef}: Element): boolean {
+    const windowRef = documentRef?.defaultView as unknown as Window & {safari?: any};
 
-    return !!windowRef && `safari` in windowRef;
+    const isMacOsSafari =
+        typeof windowRef.safari !== `undefined` &&
+        windowRef.safari?.pushNotification?.toString() ===
+            `[object SafariRemoteNotification]`;
+
+    const isIosSafari =
+        !!windowRef.navigator?.vendor?.includes(`Apple`) &&
+        !windowRef.navigator?.userAgent?.includes(`CriOS`) &&
+        !windowRef.navigator?.userAgent?.includes(`FxiOS`);
+
+    return isMacOsSafari || isIosSafari;
 }
--- projects/cdk/utils/browser/test/browsers.spec.ts
+++ projects/cdk/utils/browser/test/browsers.spec.ts
@@ -1,4 +1,4 @@
-import {tuiIsEdge, tuiIsEdgeOlderThan, tuiIsFirefox} from '@taiga-ui/cdk';
+import {tuiIsEdge, tuiIsEdgeOlderThan, tuiIsFirefox, tuiIsSafari} from '@taiga-ui/cdk';
 
 describe(`Browsers`, () => {
     it(`isEdge`, () => {
@@ -13,5 +13,61 @@ describe(`Browsers`, () => {
     it(`isEdgeOlderThan`, () => {
         expect(tuiIsEdgeOlderThan(17, `edge/16`)).toBe(true);
         expect(tuiIsEdgeOlderThan(17, `edge/18`)).toBe(false);
+    });
+
+    describe(`isSafari`, () => {
+        it(`detect by SafariRemoteNotification`, () => {
+            expect(
+                tuiIsSafari({
+                    ownerDocument: {
+                        defaultView: {
+                            safari: {
+                                pushNotification: new (class {
+                                    toString(): string {
+                                        return `[object SafariRemoteNotification]`;
+                                    }
+                                })(),
+                            },
+                        },
+                    },
+                } as unknown as Element),
+            ).toBe(true);
+
+            expect(
+                tuiIsSafari({
+                    ownerDocument: {
+                        defaultView: {navigator: {}},
+                    },
+                } as unknown as Element),
+            ).toBe(false);
+        });
+
+        it(`detect by vendor`, () => {
+            expect(
+                tuiIsSafari({
+                    ownerDocument: {
+                        defaultView: {
+                            navigator: {
+                                vendor: `Apple Computer, Inc.`,
+                                userAgent: `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15`,
+                            },
+                        },
+                    },
+                } as unknown as Element),
+            ).toBe(true);
+
+            expect(
+                tuiIsSafari({
+                    ownerDocument: {
+                        defaultView: {
+                            navigator: {
+                                vendor: `Google Inc.`,
+                                userAgent: `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36`,
+                            },
+                        },
+                    },
+                } as unknown as Element),
+            ).toBe(false);
+        });
     });
 });
