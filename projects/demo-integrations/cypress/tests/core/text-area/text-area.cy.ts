301a505712d22df2538b0aa3199f5d4e6a3de959
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2023-03-13 12:28:34 +0300

    fix(core): `Textarea` fix line breaks in disabled/readonly state (#3833)

--- projects/demo-integrations/cypress/tests/core/text-area/text-area.cy.ts
+++ projects/demo-integrations/cypress/tests/core/text-area/text-area.cy.ts
@@ -7,4 +7,19 @@ describe(`Textarea`, () => {
                 .matchImageSnapshot(`textarea-tuiTextfieldSize-${size}`);
         });
     }
+
+    it(`line break text`, () => {
+        cy.tuiVisit(`components/text-area/API`);
+        cy.get(`#demo-content tui-text-area textarea`)
+            .eq(1)
+            .type(`1{enter}2{enter}3{enter}4`);
+
+        cy.get(`#demo-content tui-text-area`).matchImageSnapshot(`textarea-line-break`);
+
+        cy.get(`.t-row tui-toggle`).first().click();
+
+        cy.get(`#demo-content tui-text-area`).matchImageSnapshot(
+            `textarea-line-break-disabled`,
+        );
+    });
 });
