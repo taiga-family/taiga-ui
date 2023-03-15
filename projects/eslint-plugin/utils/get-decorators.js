1b892f104432ceafb16a982f021de906a411d92f
Maksim Ivanov <omaxphp@yandex.ru>
2023-03-15 16:54:20 +0300

    refactor: improved `get-decorators.js` (#3873)

--- projects/eslint-plugin/utils/get-decorators.js
+++ projects/eslint-plugin/utils/get-decorators.js
@@ -1,16 +1,41 @@
 /**
  * Return type from field in constructor
- * @param param {*}
- * @returns {Array<{name: string, args: string[], print: string}>}
+ * @param node {*}
+ * @param filterFn {function}
+ * @returns {Array<{name: string, args: string[], rawArgs: string[], pretty: string}>}
  */
-module.exports = function getDecorators(param) {
-    const decorators = param.decorators ?? [];
+module.exports = function getDecorators(node, filterFn) {
+    let decorators = node.decorators ?? [];
+
+    if (filterFn) {
+        decorators = decorators.filter(decorator => filterFn(decorator));
+    }
 
     return decorators.map(({expression}) => {
         const name = expression.callee?.name ?? '';
         const arguments = expression.callee?.parent?.arguments ?? [];
-        const args = arguments.map(arg => arg.name);
 
-        return {name, args, pretty: `@${name}(${args.join(`,`)})`};
+        const args = [];
+        const rawArgs = [];
+
+        for (const arg of arguments) {
+            if (Array.isArray(arg.elements)) {
+                const rawArray = arg.elements.map(item => item.raw);
+                const valueArray = arg.elements.map(item => item.value);
+
+                rawArgs.push(`[${rawArray.join(', ')}]`);
+                args.push(valueArray);
+            } else {
+                rawArgs.push(arg.name ?? arg.raw);
+                args.push(arg.name ?? arg.value);
+            }
+        }
+
+        return {
+            name,
+            args,
+            rawArgs,
+            pretty: `@${name}(${rawArgs.join(`, `)})`,
+        };
     });
 };
