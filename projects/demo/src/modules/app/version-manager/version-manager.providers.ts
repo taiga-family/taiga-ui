11edaefa010ef0eac19ad48c0e9b892caeec0497
splincode <splincodewd@yandex.ru>
2023-03-16 13:35:09 +0300

    chore(demo): refactor for small fix

--- projects/demo/src/modules/app/version-manager/version-manager.providers.ts
+++ projects/demo/src/modules/app/version-manager/version-manager.providers.ts
@@ -1,8 +1,11 @@
 import {LocationStrategy} from '@angular/common';
 import {InjectionToken, Provider} from '@angular/core';
-import {TUI_VERSION} from '@taiga-ui/cdk';
 
-import {TUI_VERSIONS_META_OPTIONS, TuiVersionMeta} from './versions.constants';
+import {
+    TUI_CURRENT_MAJOR_VERSION,
+    TUI_VERSIONS_META_OPTIONS,
+    TuiVersionMeta,
+} from './versions.constants';
 
 export const TUI_SELECTED_VERSION_META = new InjectionToken<TuiVersionMeta | null>(
     `[TUI_SELECTED_VERSION_META]: Meta information about selected version of Taiga docs`,
@@ -16,13 +19,15 @@ export const TUI_VERSION_MANAGER_PROVIDERS: Provider[] = [
             strategy: LocationStrategy,
             versions: readonly TuiVersionMeta[],
         ): TuiVersionMeta | null => {
+            const baseHrefAppVersion =
+                strategy.path().split(`/`).filter(Boolean)?.[0] ?? `/`;
+
             return (
-                versions.find(meta => {
-                    return (
-                        meta.baseHref === strategy.getBaseHref().replace(/\//g, ``) ||
-                        meta.alias === `v${parseInt(TUI_VERSION, 10)}`
-                    );
-                }) ?? null
+                versions.find(
+                    meta =>
+                        meta.baseHref === baseHrefAppVersion ||
+                        meta.alias === `v${TUI_CURRENT_MAJOR_VERSION}`,
+                ) ?? null
             );
         },
     },
