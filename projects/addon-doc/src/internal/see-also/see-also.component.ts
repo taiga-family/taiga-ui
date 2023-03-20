d9ce86fc3d32d9a84cd81a0f4bc0de42485d649e
splincode <splincodewd@yandex.ru>
2023-03-20 18:44:54 +0300

    feat(addon-doc): add `TUI_DOC_MAP_PAGES` for cached map pages

--- projects/addon-doc/src/internal/see-also/see-also.component.ts
+++ projects/addon-doc/src/internal/see-also/see-also.component.ts
@@ -1,8 +1,9 @@
 import {ChangeDetectionStrategy, Component, Inject, Input} from '@angular/core';
+import {tuiPure} from '@taiga-ui/cdk';
 
-import {TuiDocPage, TuiDocPageGroup} from '../../interfaces/page';
+import type {TuiDocPage} from '../../interfaces/page';
 import {TUI_DOC_SEE_ALSO_TEXT} from '../../tokens/i18n';
-import {TUI_DOC_PAGES} from '../../tokens/pages';
+import {TUI_DOC_MAP_PAGES} from '../../tokens/pages';
 
 @Component({
     selector: 'tui-doc-see-also',
@@ -16,22 +17,11 @@ export class TuiDocSeeAlsoComponent {
 
     constructor(
         @Inject(TUI_DOC_SEE_ALSO_TEXT) readonly text: string,
-        @Inject(TUI_DOC_PAGES)
-        private readonly pages: ReadonlyArray<TuiDocPage | TuiDocPageGroup>,
+        @Inject(TUI_DOC_MAP_PAGES) private readonly pages: Map<string, TuiDocPage>,
     ) {}
 
+    @tuiPure
     getRouterLink(pageTitle: string): string {
-        for (let i = 0; i < this.pages.length; i++) {
-            const page = this.pages
-                .map(page => ('subPages' in page ? page.subPages : [page]))
-                .reduce((pages, subPages) => [...pages, ...subPages], [])
-                .find((page: TuiDocPage) => page.title === pageTitle);
-
-            if (page?.route) {
-                return page.route;
-            }
-        }
-
-        return '';
+        return this.pages.get(pageTitle)?.route ?? '';
     }
 }
