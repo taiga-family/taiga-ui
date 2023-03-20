29ef09912de16a30c9072645764acfbaa4c32f25
splincode <splincode@users.noreply.github.com>
2023-03-20 15:14:12 +0000

    deploy: 8797bea743d1fade8bed9ff6e9acdaa21d6d2013

--- 19597.8294e958c05159170f2c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[19597],{19597:n=>{n.exports='<p i18n>This notification will be removed after router change (see component sample)</p>\n<button\n    tuiButton\n    type="button"\n    size="m"\n    (click)="showNotification()"\n>\n    Show\n</button>\n'}}]);
