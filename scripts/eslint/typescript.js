ef82cb2cccebd2e1ca908ac697b60fd4248b24ad
Maksim Ivanov <omaxphp@yandex.ru>
2023-03-20 09:50:21 +0300

    ci: support `no-confusing-non-null-assertion` (#3912)

--- scripts/eslint/typescript.js
+++ scripts/eslint/typescript.js
@@ -18,6 +18,7 @@ module.exports = {
                 '@typescript-eslint/no-use-before-define': 'error',
                 '@typescript-eslint/no-unused-vars': 'error',
                 '@typescript-eslint/consistent-generic-constructors': 'error',
+                '@typescript-eslint/no-confusing-non-null-assertion': 'error',
                 '@typescript-eslint/switch-exhaustiveness-check': 'error',
                 '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
                 '@typescript-eslint/triple-slash-reference': [
