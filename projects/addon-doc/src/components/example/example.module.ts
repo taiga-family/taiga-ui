597d2153c2c9916c2b816463eb0331f5b23441f4
splincode <splincodewd@yandex.ru>
2023-03-20 19:33:51 +0300

    feat(addon-doc): support `icon` for code editor button

--- projects/addon-doc/src/components/example/example.module.ts
+++ projects/addon-doc/src/components/example/example.module.ts
@@ -2,7 +2,7 @@ import {ClipboardModule} from '@angular/cdk/clipboard';
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {TuiMapperPipeModule} from '@taiga-ui/cdk';
-import {TuiButtonModule} from '@taiga-ui/core';
+import {TuiButtonModule, TuiLoaderModule} from '@taiga-ui/core';
 import {TuiTabsModule} from '@taiga-ui/kit';
 import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
 
@@ -22,6 +22,7 @@ import {TuiDocExampleGetTabsPipe} from './example-get-tabs.pipe';
         TuiDocCodeModule,
         PolymorpheusModule,
         TuiMapperPipeModule,
+        TuiLoaderModule,
     ],
     declarations: [
         TuiDocExampleComponent,
--- projects/addon-doc/src/interfaces/code-editor.ts
+++ projects/addon-doc/src/interfaces/code-editor.ts
@@ -1,4 +1,7 @@
+import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus';
+
 export interface TuiCodeEditor {
     readonly name: string;
+    readonly content?: PolymorpheusContent;
     edit(component: string, id: string, files: Record<string, string>): Promise<void>;
 }
