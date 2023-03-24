5b31a387820191d142567c9c17b0212b604ace4e
Maksim Ivanov <omaxphp@yandex.ru>
2023-03-24 17:18:39 +0300

    ci: support `no-unnecessary-type-constraint` (#3971)

--- scripts/eslint/typescript.js
+++ scripts/eslint/typescript.js
@@ -10,6 +10,7 @@ module.exports = {
             parser: '@typescript-eslint/parser',
             plugins: ['@typescript-eslint'],
             rules: {
+                '@typescript-eslint/no-unnecessary-type-constraint': 'error',
                 '@typescript-eslint/no-unsafe-declaration-merging': 'error',
                 '@typescript-eslint/prefer-as-const': 'error',
                 '@typescript-eslint/prefer-string-starts-ends-with': 'error',
