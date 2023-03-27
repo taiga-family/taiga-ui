00eb26b252f4a0c89b22aa0279d2e9d31a975471
Zargham Khan <zarghamkhan980@gmail.com>
2023-03-27 21:15:49 +0200

    feat(cdk): fix for loop

--- projects/demo-integrations/cypress/tests/core/breakpoint-service/breakpoint-serivce.cy.ts
+++ projects/demo-integrations/cypress/tests/core/breakpoint-service/breakpoint-serivce.cy.ts
@@ -1,9 +1,9 @@
 describe(`Breakpoint service`, () => {
-    for (const [, {width, height}] of [
+    for (const {width, height} of [
         {width: 768, height: 900},
         {width: 1024, height: 900},
         {width: 1280, height: 900},
-    ].entries()) {
+    ]) {
         it(`${width}x${height}`, () => {
             cy.viewport(width, height);
             cy.tuiVisit(`/services/breakpoint-service`);
