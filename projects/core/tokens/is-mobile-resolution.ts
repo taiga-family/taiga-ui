6c7777f3edb0998ff1603927ae398d2004199cd5
Zargham Khan <zarghamkhan980@gmail.com>
2023-03-27 21:23:55 +0200

    feat(core): deprecate is-mobile-resolution

--- projects/core/tokens/is-mobile-resolution.ts
+++ projects/core/tokens/is-mobile-resolution.ts
@@ -8,6 +8,8 @@ import {distinctUntilChanged, map, share, startWith} from 'rxjs/operators';
 import {TUI_MEDIA} from './media';
 
 /**
+ * @deprecated use {@link https://taiga-ui.dev/services/breakpoint-service TuiBreakpointService}
+ * TODO: drop in v4.0
  * Mobile resolution stream for private providers
  */
 export const TUI_IS_MOBILE_RES = new InjectionToken<Observable<boolean>>(
