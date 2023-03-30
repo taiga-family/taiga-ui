6a9dc96e44f7d2abf25d155e74d2a31a6cf45e93
vladimirpotekhin <vladimir.potekh@gmail.com>
2023-03-30 10:43:01 +0300

    chore: put expand inside error

--- projects/core/components/error/error.component.ts
+++ projects/core/components/error/error.component.ts
@@ -1,7 +1,6 @@
 import {AnimationOptions} from '@angular/animations';
 import {ChangeDetectionStrategy, Component, Inject, Input} from '@angular/core';
 import {tuiDefaultProp, tuiIsString, TuiValidationError} from '@taiga-ui/cdk';
-import {tuiFadeIn, tuiHeightCollapse} from '@taiga-ui/core/animations';
 import {MODE_PROVIDER} from '@taiga-ui/core/providers';
 import {
     TUI_ANIMATION_OPTIONS,
@@ -9,7 +8,8 @@ import {
     TUI_MODE,
 } from '@taiga-ui/core/tokens';
 import {TuiBrightness} from '@taiga-ui/core/types';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable, of} from 'rxjs';
+import {delay, map, switchMap} from 'rxjs/operators';
 
 @Component({
     selector: 'tui-error',
@@ -17,16 +17,25 @@ import {Observable} from 'rxjs';
     styleUrls: ['./error.style.less'],
     providers: [MODE_PROVIDER],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    animations: [tuiHeightCollapse, tuiFadeIn],
 })
 export class TuiErrorComponent {
+    private readonly error$$ = new BehaviorSubject<TuiValidationError | null>(null);
+
     @Input('error')
     @tuiDefaultProp()
     set errorSetter(error: TuiValidationError | string | null) {
-        this.error = tuiIsString(error) ? new TuiValidationError(error) : error;
+        this.error$$.next(tuiIsString(error) ? new TuiValidationError(error) : error);
     }
 
-    error: TuiValidationError | null = null;
+    error$ = this.error$$.pipe(
+        switchMap(error =>
+            error
+                ? of(error)
+                : of(error).pipe(delay(this.options.params?.duration ?? 300)),
+        ),
+    );
+
+    expanded$ = this.error$$.pipe(map(Boolean));
 
     readonly animation = {value: '', ...this.options} as const;
 
--- projects/core/components/error/error.module.ts
+++ projects/core/components/error/error.module.ts
@@ -1,11 +1,13 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {TuiLetModule} from '@taiga-ui/cdk';
+import {TuiExpandModule} from '@taiga-ui/core/components/expand';
 import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
 
 import {TuiErrorComponent} from './error.component';
 
 @NgModule({
-    imports: [CommonModule, PolymorpheusModule],
+    imports: [CommonModule, PolymorpheusModule, TuiExpandModule, TuiLetModule],
     declarations: [TuiErrorComponent],
     exports: [TuiErrorComponent],
 })
