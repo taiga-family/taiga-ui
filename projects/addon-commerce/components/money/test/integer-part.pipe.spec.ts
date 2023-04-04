c31d8af16b8b007155b42e8bba6f99b198814493
vladimirpotekhin <vladimir.potekh@gmail.com>
2023-04-04 12:36:51 +0300

    chore: add more cases

--- projects/addon-commerce/components/money/test/integer-part.pipe.spec.ts
+++ projects/addon-commerce/components/money/test/integer-part.pipe.spec.ts
@@ -11,5 +11,10 @@ describe(`TuiIntegerPart`, () => {
 
         expect(pipe.transform(10123.123)).toEqual(`10 123`);
         expect(pipe.transform(10123)).toEqual(`10 123`);
+        expect(pipe.transform(null as unknown as number)).toEqual(`0`);
+        expect(pipe.transform(undefined as unknown as number)).toEqual(`NaN`);
+        expect(pipe.transform(`` as unknown as number)).toEqual(`0`);
+        expect(pipe.transform(`10,5` as unknown as number)).toEqual(`NaN`);
+        expect(pipe.transform(`10.5` as unknown as number)).toEqual(`10`);
     });
 });
