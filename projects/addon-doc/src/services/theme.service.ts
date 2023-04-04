aab2beff3a3a6732421db47fc80354596cce9147
Maksim Ivanov <omaxphp@yandex.ru>
2023-04-04 10:56:19 +0300

    fix(addon-doc): correct detect `Open-source` theme (#4098)

--- projects/addon-doc/src/services/theme.service.ts
+++ projects/addon-doc/src/services/theme.service.ts
@@ -2,18 +2,22 @@ import {Inject, Injectable} from '@angular/core';
 import {LOCAL_STORAGE} from '@ng-web-apis/common';
 import {BehaviorSubject} from 'rxjs';
 
-import {TUI_THEME_NAME, TUI_THEME_STORAGE_KEY} from './theme.options';
+import {
+    TUI_THEME_DEFAULT_NAME,
+    TUI_THEME_NAME,
+    TUI_THEME_STORAGE_KEY,
+} from './theme.options';
 
 @Injectable({
     providedIn: `root`,
 })
 export class TuiThemeService extends BehaviorSubject<string> {
     constructor(
-        @Inject(TUI_THEME_NAME) readonly name: string,
+        @Inject(TUI_THEME_NAME) readonly initialTheme: string,
         @Inject(TUI_THEME_STORAGE_KEY) readonly key: string,
         @Inject(LOCAL_STORAGE) private readonly storage: Storage,
     ) {
-        super(storage.getItem(key) || name);
+        super(storage.getItem(key) || initialTheme);
     }
 
     override next(theme: string): void {
@@ -22,6 +26,6 @@ export class TuiThemeService extends BehaviorSubject<string> {
     }
 
     get isDefaultTheme(): boolean {
-        return this.value === this.name;
+        return this.value === TUI_THEME_DEFAULT_NAME;
     }
 }
