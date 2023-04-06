690e58037084a620bf64cb6059ffa2596e5c0057
waterplea <waterplea@users.noreply.github.com>
2023-04-06 10:13:51 +0000

    deploy: 6a1ddc477c5132389abfb9575252d423c2c96aa5

---
+++ next/44170.425dd5cf8aa06145ec53.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[44170],{44170:n=>{n.exports="```ts\nimport {TuiTabBarModule} from '@taiga-ui/kit';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiTabBarModule,\n  ],\n  // ...\n})\nexport class AppModule {}\n```\n"}}]);
---
+++ next/81949.6aa25d9dd2916fec800a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[81949],{81949:n=>{n.exports=".tabs {\n    color: var(--tui-link);\n\n    --tui-active-color: var(--tui-accent);\n}\n"}}]);
