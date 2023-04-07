f7176dbe84d110022bf9508355c4d6b70ead19f1
waterplea <alexander@inkin.ru>
2023-04-07 16:02:59 +0800

    feat(cdk): add `@tuiParentStop` animation

--- projects/cdk/constants/parent-animation.ts
+++ projects/cdk/constants/parent-animation.ts
@@ -1,8 +1,16 @@
 import {animateChild, query, style, transition, trigger} from '@angular/animations';
 
+/**
+ * Add to enable child :leave animation (fixes https://github.com/angular/angular/issues/15753)
+ */
 export const TUI_PARENT_ANIMATION = trigger(`tuiParentAnimation`, [
     transition(`* => void`, [
         style({overflow: `hidden`}),
         query(`:scope > *`, [animateChild()], {optional: true}),
     ]),
 ]);
+
+/**
+ * Add on parent to stop initial :enter animation for children
+ */
+export const TUI_PARENT_STOP = trigger(`tuiParentStop`, [transition(`:enter`, [])]);
