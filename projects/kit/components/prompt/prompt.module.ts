ba19ce06f501d149899b2d1a8b32f73c4d9e1f39
Alex Inkin <alexander@inkin.ru>
2023-04-08 21:21:22 +0800

    feat(kit): `Prompt` add autofocus on OK button (#4155)

--- projects/kit/components/prompt/prompt.module.ts
+++ projects/kit/components/prompt/prompt.module.ts
@@ -1,12 +1,13 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {TuiAutoFocusModule} from '@taiga-ui/cdk';
 import {TuiButtonModule} from '@taiga-ui/core';
 import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
 
 import {TuiPromptComponent} from './prompt.component';
 
 @NgModule({
-    imports: [CommonModule, PolymorpheusModule, TuiButtonModule],
+    imports: [CommonModule, PolymorpheusModule, TuiButtonModule, TuiAutoFocusModule],
     declarations: [TuiPromptComponent],
     exports: [TuiPromptComponent],
 })
