fd002dda9731054c9f24a01a271a5cfacc17cea7
vladimirpotekhin <vladimir.potekh@gmail.com>
2023-04-13 11:47:39 +0300

    fix(kit): fix handling input in legacy mask with native input

--- projects/core/components/primitive-textfield/textfield/textfield.component.ts
+++ projects/core/components/primitive-textfield/textfield/textfield.component.ts
@@ -1,4 +1,10 @@
-import {ChangeDetectionStrategy, Component, ElementRef, Inject} from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    Inject,
+    Optional,
+} from '@angular/core';
 import {TuiIdService} from '@taiga-ui/cdk';
 import {
     TEXTFIELD_CONTROLLER_PROVIDER,
@@ -6,7 +12,7 @@ import {
     TuiTextfieldController,
 } from '@taiga-ui/core/directives';
 import {TuiTextfieldHost} from '@taiga-ui/core/interfaces';
-import {TUI_TEXTFIELD_HOST} from '@taiga-ui/core/tokens';
+import {TUI_LEGACY_MASK, TUI_TEXTFIELD_HOST} from '@taiga-ui/core/tokens';
 
 @Component({
     selector: 'input[tuiTextfield], textarea[tuiTextfield]',
@@ -21,7 +27,7 @@ import {TUI_TEXTFIELD_HOST} from '@taiga-ui/core/tokens';
         '[tabIndex]': 'host.focusable ? 0 : -1',
         '[readOnly]': 'host.readOnly',
         '[value]': 'host.value',
-        '(input)': 'host.onValueChange($event.target.value)',
+        '(input)': '!legacyMask && host.onValueChange($event.target.value)',
     },
     styleUrls: ['./textfield.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -34,6 +40,9 @@ export class TuiTextfieldComponent {
         @Inject(ElementRef) private readonly el: ElementRef<HTMLInputElement>,
         @Inject(TuiIdService)
         private readonly idService: TuiIdService,
+        @Optional()
+        @Inject(TUI_LEGACY_MASK)
+        readonly legacyMask: boolean,
     ) {
         this.host.process(this.el.nativeElement);
     }
---
+++ projects/core/tokens/legacy-mask.ts
@@ -0,0 +1,8 @@
+import {InjectionToken} from '@angular/core';
+
+/**
+ * @internal
+ * @deprecated
+ * Don't use it! Will be removed after changing the mask.
+ */
+export const TUI_LEGACY_MASK = new InjectionToken(`[TUI_LEGACY_MASK]`);
