67136425bc2106bcc5ee4030c627f34074d2f949
Maksim Ivanov <omaxphp@yandex.ru>
2023-04-17 21:23:39 +0300

    ci: support auto update bundled icons (#4225)
    Co-authored-by: tinkoff-bot <tinkoff-bot@users.noreply.github.com>

--- projects/icons/scripts/prepare-all-to-compile-file.ts
+++ projects/icons/scripts/prepare-all-to-compile-file.ts
@@ -1,8 +1,11 @@
 import {readdirSync, writeFileSync} from 'fs';
 
-export function prepareAllToCompileFile(iconsSrc: string, entryPointTs: string): void {
-    const icons: string[] = readdirSync(iconsSrc).filter(
-        file => file.split(`.`).pop() === `svg`,
+export function tuiPrepareAllToCompileFile(iconsSrc: string, entryPointTs: string): void {
+    const icons: string[] = readdirSync(iconsSrc).filter(file => file.endsWith(`.svg`));
+
+    console.info(
+        `\x1B[36m%s\x1B[0m`,
+        `Combine all svg icons into one file for bundling: ${icons.length}`,
     );
 
     let importDeclarations = ``;
@@ -10,8 +13,9 @@ export function prepareAllToCompileFile(iconsSrc: string, entryPointTs: string):
 
     for (const iconPath of icons) {
         const fileName = iconPath.split(`.`).shift();
+        const filePath = `./src/${fileName}.svg`;
 
-        importDeclarations += `import ${fileName} from './src/${fileName}.svg';\n`;
+        importDeclarations += `import ${fileName} from '${filePath}';\n`;
         exportDeclarations += `${fileName}, `;
     }
 
