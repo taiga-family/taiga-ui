b130a52c1a149f8658c142edcf4b26a10e4f8261
Maksim Ivanov <omaxphp@yandex.ru>
2023-04-17 17:48:47 +0300

    fix(addon-editor): use `paragraph` type for correct detect empty (#4232)

--- projects/addon-editor/directives/tiptap-editor/utils/is-empty-paragraph.ts
+++ projects/addon-editor/directives/tiptap-editor/utils/is-empty-paragraph.ts
@@ -1,5 +1,10 @@
 import {JSONContent} from '@tiptap/core';
 
 export function tuiIsEmptyParagraph(json?: JSONContent[]): boolean {
-    return Array.isArray(json) && json.length === 1 && !json[0].hasOwnProperty(`content`);
+    return (
+        Array.isArray(json) &&
+        json.length === 1 &&
+        json[0].type === `paragraph` &&
+        !json[0].hasOwnProperty(`content`)
+    );
 }
