256347d260842371a9ef1c178bfeac72ba18b68b
vladimirpotekhin <vladimir.potekh@gmail.com>
2023-04-18 12:38:09 +0300

    chore: empty

--- projects/core/tokens/is-mobile-resolution.ts
+++ projects/core/tokens/is-mobile-resolution.ts
@@ -1,17 +1,30 @@
-import {inject, InjectionToken} from '@angular/core';
-import {TuiBreakpointService} from '@taiga-ui/core/services';
+import {inject, InjectionToken, NgZone} from '@angular/core';
+import {WINDOW} from '@ng-web-apis/common';
+import {tuiTypedFromEvent, tuiZoneOptimized} from '@taiga-ui/cdk';
+import {tuiIsMobile} from '@taiga-ui/core/utils';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {distinctUntilChanged, map, share, startWith} from 'rxjs/operators';
+
+import {TUI_MEDIA} from './media';
 
 /**
+ * @deprecated use {@link https://taiga-ui.dev/services/breakpoint-service TuiBreakpointService}
+ * TODO: drop in v4.0
  * Mobile resolution stream for private providers
  */
 export const TUI_IS_MOBILE_RES = new InjectionToken<Observable<boolean>>(
     `[TUI_IS_MOBILE_RES]`,
     {
         factory: () => {
-            return inject(TuiBreakpointService).pipe(
-                map(mediaKey => mediaKey === `mobile`),
+            const win = inject(WINDOW);
+            const media = inject(TUI_MEDIA);
+
+            return tuiTypedFromEvent(win, `resize`).pipe(
+                share(),
+                startWith(null),
+                map(() => tuiIsMobile(win, media)),
+                distinctUntilChanged(),
+                tuiZoneOptimized(inject(NgZone)),
             );
         },
     },
--- projects/demo/src/modules/components/block-status/examples/2/index.ts
+++ projects/demo/src/modules/components/block-status/examples/2/index.ts
@@ -1,7 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TUI_IS_MOBILE_RES, TuiSizeL} from '@taiga-ui/core';
+import {TuiBreakpointService, TuiSizeL} from '@taiga-ui/core';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 
@@ -12,9 +12,11 @@ import {map} from 'rxjs/operators';
     encapsulation,
 })
 export class TuiBlockStatusExample2 {
-    size$: Observable<TuiSizeL> = this.isMobileRes.pipe(
-        map(isMobile => (isMobile ? 'm' : 'l')),
+    size$: Observable<TuiSizeL> = this.breakpointService.pipe(
+        map(key => (key === 'mobile' ? 'm' : 'l')),
     );
 
-    constructor(@Inject(TUI_IS_MOBILE_RES) readonly isMobileRes: Observable<boolean>) {}
+    constructor(
+        @Inject(TuiBreakpointService) readonly breakpointService: TuiBreakpointService,
+    ) {}
 }
