8b424d01996d1c4d6affbf405c44c594996e4f26
vladimirpotekhin <vladimir.potekh@gmail.com>
2023-04-26 10:55:57 +0300

    fix(kit): `InputDateTime` fix value setting when using valueTransformer

--- projects/kit/components/input-date-time/input-date-time.component.ts
+++ projects/kit/components/input-date-time/input-date-time.component.ts
@@ -313,7 +313,8 @@ export class TuiInputDateTimeComponent
     override writeValue(value: [TuiDay | null, TuiTime | null] | null): void {
         super.writeValue(value);
 
-        this.nativeValue = value && (value[0] || value[1]) ? this.computedValue : '';
+        this.nativeValue =
+            this.value && (this.value[0] || this.value[1]) ? this.computedValue : '';
     }
 
     protected getFallbackValue(): [TuiDay | null, TuiTime | null] {
