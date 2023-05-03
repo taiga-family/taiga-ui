ddd234d4c7c346804f94b29f077edfefe6c93eba
Maksim Ivanov <omaxphp@yandex.ru>
2023-05-03 13:35:26 +0300

    refactor(addon-editor): post review (#4357)

--- projects/addon-editor/components/edit-link/utils/edit-link-parse-url.ts
+++ projects/addon-editor/components/edit-link/utils/edit-link-parse-url.ts
@@ -1,7 +1,7 @@
 import {
-    TUI_EDITOR_LINK_HASH_PREFIX,
-    TUI_EDITOR_LINK_OSI_PROTOCOL_DIVIDER,
-    TUI_EDITOR_LINK_SIMPLE_PROTOCOL_DIVIDER,
+    TUI_EDITOR_LINK_HASH_PREFIX as HASH_PREFIX,
+    TUI_EDITOR_LINK_OSI_PROTOCOL_DIVIDER as OSI_PROTOCOL_DIVIDER,
+    TUI_EDITOR_LINK_SIMPLE_PROTOCOL_DIVIDER as SIMPLE_PROTOCOL_DIVIDER,
 } from '@taiga-ui/addon-editor/constants';
 import {tuiIsValidUrl} from '@taiga-ui/cdk';
 
@@ -10,41 +10,33 @@ interface TuiEditLinkParsed {
     path: string;
 }
 
-export function tuiEditLinkParseUrl(url?: string): TuiEditLinkParsed {
-    if (url?.startsWith(TUI_EDITOR_LINK_HASH_PREFIX)) {
-        const [anchor = ``] = url?.split(`#`).slice(-1) ?? [];
-
-        return {prefix: TUI_EDITOR_LINK_HASH_PREFIX, path: anchor};
+export function tuiEditLinkParseUrl(url: string = ``): TuiEditLinkParsed {
+    if (url.startsWith(HASH_PREFIX)) {
+        return {prefix: HASH_PREFIX, path: url.slice(1)};
     }
 
-    const [prefix = ``, path = ``] = url?.includes(TUI_EDITOR_LINK_OSI_PROTOCOL_DIVIDER)
-        ? splitOsiProtocol(url ?? ``)
-        : splitSimpleProtocol(url ?? ``);
+    const [prefix = ``, path = ``] = url.includes(OSI_PROTOCOL_DIVIDER)
+        ? splitOsiProtocol(url)
+        : splitSimpleProtocol(url);
 
-    if (path.includes(TUI_EDITOR_LINK_SIMPLE_PROTOCOL_DIVIDER)) {
+    if (path.includes(SIMPLE_PROTOCOL_DIVIDER)) {
         const [protocol, otherPart] = splitSimpleProtocol(path);
 
-        if (!!protocol && !!otherPart) {
+        if (protocol && otherPart) {
             return {prefix: protocol, path: otherPart};
         }
     }
 
-    return {prefix, path: prefix === `` ? url ?? `` : path};
+    return {prefix, path: prefix === `` ? url : path};
 }
 
-function splitOsiProtocol(url?: string): Array<string | undefined> {
-    const protocolPosition = url?.indexOf(TUI_EDITOR_LINK_OSI_PROTOCOL_DIVIDER) ?? -1;
+function splitOsiProtocol(url: string = ``): Array<string | undefined> {
+    const protocolPosition = url.indexOf(OSI_PROTOCOL_DIVIDER) ?? -1;
     const [prefix, path] =
         protocolPosition > -1
             ? [
-                  url?.slice(
-                      0,
-                      protocolPosition + TUI_EDITOR_LINK_OSI_PROTOCOL_DIVIDER.length,
-                  ),
-                  url?.slice(
-                      protocolPosition + TUI_EDITOR_LINK_OSI_PROTOCOL_DIVIDER.length,
-                      url.length,
-                  ),
+                  url.slice(0, protocolPosition + OSI_PROTOCOL_DIVIDER.length),
+                  url.slice(protocolPosition + OSI_PROTOCOL_DIVIDER.length, url.length),
               ]
             : [``, url];
     const result = [prefix, path].filter(Boolean);
@@ -52,12 +44,8 @@ function splitOsiProtocol(url?: string): Array<string | undefined> {
     return path?.includes(`://`) && result.length > 1 ? splitOsiProtocol(path) : result;
 }
 
-function splitSimpleProtocol(url: string): Array<string | undefined> {
+function splitSimpleProtocol(url: string = ``): Array<string | undefined> {
     const [prefix, path] = url.split(/:/).slice(-2).filter(Boolean);
 
-    if (!!prefix && !!path && !tuiIsValidUrl(url)) {
-        return [`${prefix}:`, path];
-    }
-
-    return [];
+    return prefix && path && !tuiIsValidUrl(url) ? [`${prefix}:`, path] : [];
 }
