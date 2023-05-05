6e699885aefc9ae3784435a6ee27e6b758d7c339
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2023-05-05 12:57:05 +0300

    feat(kit): `InputDateRange` allow to use custom value content when using period (#4383)

---
+++ projects/demo/src/modules/components/input-date-range/examples/5/index.ts
@@ -0,0 +1,32 @@
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiDay, TuiDayRange} from '@taiga-ui/cdk';
+import {TuiDayRangePeriod} from '@taiga-ui/kit';
+
+const today = TuiDay.currentLocal();
+const yesterday = today.append({day: -1});
+
+@Component({
+    selector: 'tui-input-date-range-example-5',
+    templateUrl: './index.html',
+    changeDetection,
+    encapsulation,
+})
+export class TuiInputDateRangeExample5 {
+    readonly control = new FormControl(new TuiDayRange(today, today));
+
+    readonly items = [
+        new TuiDayRangePeriod(
+            new TuiDayRange(today, today),
+            'Today',
+            ({$implicit}) => `Today (${$implicit.from})`,
+        ),
+        new TuiDayRangePeriod(
+            new TuiDayRange(yesterday, yesterday),
+            'Yesterday',
+            ({$implicit}) => `Yesterday (${$implicit.from})`,
+        ),
+    ];
+}
--- projects/kit/classes/day-range-period.ts
+++ projects/kit/classes/day-range-period.ts
@@ -1,7 +1,12 @@
-import {TuiDayRange} from '@taiga-ui/cdk';
+import {TuiContextWithImplicit, TuiDayRange} from '@taiga-ui/cdk';
+import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus';
 
 export class TuiDayRangePeriod {
-    constructor(readonly range: TuiDayRange, private readonly name: string) {}
+    constructor(
+        readonly range: TuiDayRange,
+        private readonly name: string,
+        readonly content?: PolymorpheusContent<TuiContextWithImplicit<TuiDayRange>>,
+    ) {}
 
     toString(): string {
         return this.name;
