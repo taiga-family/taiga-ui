040aa6c84087b3818b8640b831812ae356ad1f38
Daniil Dubrava <daniil.dubrava@yandex.ru>
2023-05-11 15:15:11 +0300

    chore: add custom content example to line-clamp (#4409)

--- projects/demo/src/modules/components/line-clamp/line-clamp.component.ts
+++ projects/demo/src/modules/components/line-clamp/line-clamp.component.ts
@@ -40,4 +40,10 @@ export class ExampleTuiLineClampComponent {
         HTML: import('./examples/4/index.html?raw'),
         LESS: import('./examples/4/index.less?raw'),
     };
+
+    readonly example5: TuiDocExample = {
+        TypeScript: import('./examples/5/index.ts?raw'),
+        HTML: import('./examples/5/index.html?raw'),
+        LESS: import('./examples/5/index.less?raw'),
+    };
 }
