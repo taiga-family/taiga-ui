3857594d6d05d85a9cf13c82c9952aa840dd0559
waterplea <alexander@inkin.ru>
2023-05-22 17:30:28 +0400

    fix(kit): `MultiSelect` fix placeholder in dark mode

--- projects/core/components/primitive-textfield/primitive-textfield.component.ts
+++ projects/core/components/primitive-textfield/primitive-textfield.component.ts
@@ -29,9 +29,7 @@ import {
     TuiTextfieldController,
     TuiTextfieldOptions,
 } from '@taiga-ui/core/directives/textfield-controller';
-import {MODE_PROVIDER} from '@taiga-ui/core/providers';
-import {TUI_MODE} from '@taiga-ui/core/tokens';
-import {TuiBrightness, TuiSizeL, TuiSizeS} from '@taiga-ui/core/types';
+import {TuiSizeL, TuiSizeS} from '@taiga-ui/core/types';
 import {tuiGetBorder} from '@taiga-ui/core/utils/miscellaneous';
 import {PolymorpheusContent, PolymorpheusOutletDirective} from '@tinkoff/ng-polymorpheus';
 import {fromEvent, Observable} from 'rxjs';
@@ -50,10 +48,8 @@ const ICON_PADDING = 1.625;
     providers: [
         tuiAsFocusableItemAccessor(TuiPrimitiveTextfieldComponent),
         TEXTFIELD_CONTROLLER_PROVIDER,
-        MODE_PROVIDER,
     ],
     host: {
-        '($.data-mode.attr)': 'mode$',
         '[class._autofilled]': 'autofilled',
         '[class._label-outside]': 'controller.labelOutside',
     },
@@ -118,7 +114,6 @@ export class TuiPrimitiveTextfieldComponent
     autofilled = false;
 
     constructor(
-        @Inject(TUI_MODE) readonly mode$: Observable<TuiBrightness | null>,
         @Inject(TUI_TEXTFIELD_WATCHED_CONTROLLER)
         readonly controller: TuiTextfieldController,
         @Optional()
