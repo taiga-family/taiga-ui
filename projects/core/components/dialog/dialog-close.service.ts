b96026f2a6120a21f28398b0669458d1b245cd97
waterplea <alexander@inkin.ru>
2023-05-29 13:52:21 +0400

    fix(core): `Dialog` fix closing by accident mouseup outside of content

--- projects/core/components/dialog/dialog-close.service.ts
+++ projects/core/components/dialog/dialog-close.service.ts
@@ -4,7 +4,6 @@ import {WINDOW} from '@ng-web-apis/common';
 import {
     tuiContainsOrAfter,
     tuiGetActualTarget,
-    tuiIsCurrentTarget,
     tuiIsElement,
     tuiTypedFromEvent,
 } from '@taiga-ui/cdk';
@@ -16,43 +15,28 @@ const SCROLLBAR_PLACEHOLDER = 17;
 
 @Injectable()
 export class TuiDialogCloseService extends Observable<unknown> {
-    private readonly click$ = tuiTypedFromEvent(this.element, `click`).pipe(
-        filter(tuiIsCurrentTarget),
-    );
-
     private readonly esc$ = tuiTypedFromEvent(this.doc, `keydown`).pipe(
         filter(event => {
-            const key = event.key;
             const target = tuiGetActualTarget(event);
 
             return (
-                key === `Escape` &&
-                tuiIsElement(target) &&
-                (this.element.contains(target) ||
-                    !tuiContainsOrAfter(this.element, target))
+                event.key === `Escape` &&
+                (this.element.contains(target) || this.isOutside(target))
             );
         }),
     );
 
     private readonly mousedown$ = tuiTypedFromEvent(this.doc, `mousedown`).pipe(
-        filter(event => {
-            const target = tuiGetActualTarget(event);
-            const clientX = event.clientX;
-
-            return (
-                tuiIsElement(target) &&
-                tuiGetViewportWidth(this.win) - clientX > SCROLLBAR_PLACEHOLDER &&
-                !tuiContainsOrAfter(this.element, target)
-            );
-        }),
+        filter(
+            event =>
+                tuiGetViewportWidth(this.win) - event.clientX > SCROLLBAR_PLACEHOLDER &&
+                this.isOutside(tuiGetActualTarget(event)),
+        ),
         switchMap(() =>
             tuiTypedFromEvent(this.doc, `mouseup`).pipe(
                 take(1),
                 map(tuiGetActualTarget),
-                filter(
-                    target =>
-                        tuiIsElement(target) && !tuiContainsOrAfter(this.element, target),
-                ),
+                filter(target => this.isOutside(target)),
             ),
         ),
     );
@@ -62,12 +46,17 @@ export class TuiDialogCloseService extends Observable<unknown> {
         @Inject(DOCUMENT) private readonly doc: Document,
         @Inject(ElementRef) private readonly el: ElementRef<HTMLElement>,
     ) {
-        super(subscriber =>
-            merge(this.click$, this.esc$, this.mousedown$).subscribe(subscriber),
-        );
+        super(subscriber => merge(this.esc$, this.mousedown$).subscribe(subscriber));
     }
 
     private get element(): HTMLElement {
         return this.el.nativeElement;
+    }
+
+    private isOutside(target: EventTarget): boolean {
+        return (
+            tuiIsElement(target) &&
+            (!tuiContainsOrAfter(this.element, target) || target === this.element)
+        );
     }
 }
