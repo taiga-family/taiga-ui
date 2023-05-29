4e60f13f9d097c3eab2553652c29584205303ab1
Alex Inkin <alexander@inkin.ru>
2023-05-29 11:44:24 +0300

    feat(addon-mobile): `PullToRefresh` allow customization (#4530)
    (cherry picked from commit f592993c2a54a73b539a9faefed412bd80f9ea1e)

--- projects/addon-mobile/components/pull-to-refresh/pull-to-refresh.component.ts
+++ projects/addon-mobile/components/pull-to-refresh/pull-to-refresh.component.ts
@@ -1,64 +1,46 @@
 import {ChangeDetectionStrategy, Component, Inject, Output} from '@angular/core';
-import {TUI_IS_IOS, tuiPure} from '@taiga-ui/cdk';
+import {TUI_IS_IOS, TuiContextWithImplicit} from '@taiga-ui/cdk';
+import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus';
 import {Observable, of} from 'rxjs';
-import {distinctUntilChanged, filter, map, mapTo} from 'rxjs/operators';
+import {distinctUntilChanged, filter, map} from 'rxjs/operators';
 
+import {TUI_IOS_LOADER} from './loader-ios/loader-ios.component';
 import {
-    MICRO_OFFSET,
-    PULLED_DISTANCE,
-    TUI_PULL_TO_REFRESH_PROVIDERS,
-    TUI_PULLING,
+    TUI_PULL_TO_REFRESH_COMPONENT,
+    TUI_PULL_TO_REFRESH_THRESHOLD,
 } from './pull-to-refresh.providers';
-
-const IOS_LOADING_DISTANCE = PULLED_DISTANCE / 2;
-const ANDROID_MAX_DISTANCE = PULLED_DISTANCE * 1.5;
-
-function translateY(distance: number): string {
-    return `translateY(${distance}px)`;
-}
+import {MICRO_OFFSET, TuiPullToRefreshService} from './pull-to-refresh.service';
 
 @Component({
     selector: 'tui-pull-to-refresh',
     templateUrl: './pull-to-refresh.template.html',
     styleUrls: ['./pull-to-refresh.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: [TUI_PULL_TO_REFRESH_PROVIDERS],
+    providers: [TuiPullToRefreshService],
 })
 export class TuiPullToRefreshComponent {
     @Output()
-    readonly pulled: Observable<void> = this.pulling$.pipe(
+    readonly pulled: Observable<unknown> = this.pulling$.pipe(
         distinctUntilChanged(),
-        filter(distance => distance === PULLED_DISTANCE),
-        mapTo(undefined),
+        filter(distance => distance === this.threshold),
     );
 
-    readonly pulledInPercent$: Observable<number> = this.pulling$.pipe(
-        map(distance => (distance * 100) / PULLED_DISTANCE),
-    );
-
+    // TODO: Move to iOS loader somehow, like contentTransform$ below
     readonly dropped$: Observable<boolean> = this.pulling$.pipe(
-        map(distance => distance <= MICRO_OFFSET || distance === PULLED_DISTANCE),
+        map(distance => distance <= MICRO_OFFSET || distance === this.threshold),
         distinctUntilChanged(),
     );
 
-    readonly contentTransform$: Observable<string | null> = this.isIOS
-        ? this.pulling$.pipe(
-              map(distance =>
-                  distance === PULLED_DISTANCE ? IOS_LOADING_DISTANCE : distance,
-              ),
-              map(translateY),
-          )
-        : of(null);
+    readonly contentTransform$: Observable<string | null> =
+        this.isIOS && this.component === TUI_IOS_LOADER
+            ? this.pulling$.pipe(map(distance => `translateY(${distance / 2}px)`))
+            : of(null);
 
     constructor(
-        @Inject(TUI_IS_IOS) readonly isIOS: boolean,
-        @Inject(TUI_PULLING) private readonly pulling$: Observable<number>,
+        @Inject(TUI_IS_IOS) private readonly isIOS: boolean,
+        @Inject(TUI_PULL_TO_REFRESH_THRESHOLD) private readonly threshold: number,
+        @Inject(TUI_PULL_TO_REFRESH_COMPONENT)
+        readonly component: PolymorpheusContent<TuiContextWithImplicit<number>>,
+        @Inject(TuiPullToRefreshService) readonly pulling$: Observable<number>,
     ) {}
-
-    @tuiPure
-    get loaderTransform$(): Observable<string> {
-        return this.pulling$.pipe(
-            map(distance => translateY(Math.min(distance, ANDROID_MAX_DISTANCE))),
-        );
-    }
 }
