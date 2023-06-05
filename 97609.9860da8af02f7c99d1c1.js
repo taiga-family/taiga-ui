c106f6f03e376ed98450e7742b1d3b0c0efe8f7e
splincode <splincode@users.noreply.github.com>
2023-06-05 14:42:46 +0000

    deploy: 2773afaa3abcadbfd4b5f75580d8332e2e957f9f

---
+++ 97609.9860da8af02f7c99d1c1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[97609],{97609:n=>{n.exports='```html\n<tui-marker-icon [src]="tuiIconSettings"></tui-marker-icon>\n```\n'}}]);
