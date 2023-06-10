d6b267ba3eb642269b0ad84f4a5d4463091a7289
splincode <omaxphp@yandex.ru>
2023-06-10 11:31:05 +0000

    Update snapshots/demo/next/main to output generated at 005bc8f

--- 48218.fba1b9cc9ac8a6e60330.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[48218],{48218:o=>{o.exports='<tui-editor\n    class="editor"\n    [formControl]="control"\n    [tools]="builtInTools"\n></tui-editor>\n\n<h4>HTML:</h4>\n<tui-editor-socket [content]="control.value || \'\'"></tui-editor-socket>\n\n<h4>Text:</h4>\n<p>{{ control.value }}</p>\n'}}]);
