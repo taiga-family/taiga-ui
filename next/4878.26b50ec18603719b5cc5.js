80e2e5c295b548a3edc62bb673c6d5dd19b7c247
splincode <splincode@users.noreply.github.com>
2023-06-13 10:04:48 +0000

    deploy: 29d039e47df5eb130677fd182d7aa48f73d60c42

---
+++ next/4878.26b50ec18603719b5cc5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[4878],{4878:n=>{n.exports='<tui-editor\n    class="heading"\n    [formControl]="control"\n    [tools]="builtInTools"\n></tui-editor>\n\n<h4>HTML:</h4>\n<tui-editor-socket [content]="control.value || \'\'"></tui-editor-socket>\n\n<h4>Text:</h4>\n<p>{{ control.value }}</p>\n'}}]);
