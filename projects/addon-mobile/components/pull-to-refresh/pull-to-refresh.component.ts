bc0abec7959e0d93fc35c478859d3c02185695f9
Alex Inkin <alexander@inkin.ru>
2023-06-29 16:06:51 +0800

    fix(addon-mobile): `PullToRefresh` remove transform CSS (#4799) (#4801)

--- projects/addon-mobile/components/pull-to-refresh/pull-to-refresh.component.ts
+++ projects/addon-mobile/components/pull-to-refresh/pull-to-refresh.component.ts
@@ -14,6 +14,7 @@ import {
     tuiDefaultProp,
     TuiDestroyService,
     TuiHandler,
+    tuiPx,
     tuiScrollFrom,
     tuiZonefree,
 } from '@taiga-ui/cdk';
@@ -37,7 +38,7 @@ export class TuiPullToRefreshComponent {
     @Input()
     @tuiDefaultProp()
     styleHandler: TuiHandler<number, Record<string, any> | null> = this.isIOS
-        ? distance => ({transform: `translateY(${distance / 2}px)`})
+        ? distance => ({top: tuiPx(distance / 2)})
         : () => null;
 
     @Output()
