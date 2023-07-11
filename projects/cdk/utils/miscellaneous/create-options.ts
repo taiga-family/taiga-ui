ed8f27a5509157df264bead3f2af5812eadb224e
waterplea <alexander@inkin.ru>
2023-07-11 16:57:32 +0300

    chore: fix lint

--- projects/cdk/utils/miscellaneous/create-options.ts
+++ projects/cdk/utils/miscellaneous/create-options.ts
@@ -1,6 +1,7 @@
 import {InjectionToken} from '@angular/core';
 
 export function tuiCreateOptions<T>(defaults: T): InjectionToken<T> {
+    // eslint-disable-next-line @taiga-ui/injection-token-description
     return new InjectionToken<T>(``, {
         factory: () => defaults,
     });
