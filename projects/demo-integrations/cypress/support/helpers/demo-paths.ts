80a3b53745186079ff4aa0d8dd19ef3e1bee62e1
splincode <splincodewd@yandex.ru>
2023-07-20 12:49:50 +0300

    ci: add util for filter proprietary pages

--- projects/demo-integrations/cypress/support/helpers/demo-paths.ts
+++ projects/demo-integrations/cypress/support/helpers/demo-paths.ts
@@ -2,15 +2,15 @@ import '@angular/localize/init';
 
 import {TuiDocPage, TuiDocPages} from '@taiga-ui/addon-doc';
 
-import {pages} from '../../../../demo/src/modules/app/pages';
+import {pages as PUBLIC_PAGES} from '../../../../demo/src/modules/app/pages';
 
-const EXCLUSION_SECTIONS = [`Documentation`, `Foundations`, `Tools`, `Testing`];
-const EXCLUSION_ROUTES = [`i18n`, `PrimitiveCheckbox`, `ThemeNight`, `Markdown`];
-
-export const DEMO_PATHS = flatPages(pages)
-    .filter(page => !EXCLUSION_SECTIONS.includes(page.section as unknown as string))
-    .filter(page => !EXCLUSION_ROUTES.includes(page.title))
-    .map(page => page.route.replace(`/`, ``));
+export const EXCLUSION_SECTIONS = [`Documentation`, `Foundations`, `Tools`, `Testing`];
+export const EXCLUSION_ROUTES = [`i18n`, `PrimitiveCheckbox`, `ThemeNight`, `Markdown`];
+export const DEMO_PATHS = tuiMakeCypressDemoPath(
+    PUBLIC_PAGES,
+    EXCLUSION_SECTIONS,
+    EXCLUSION_ROUTES,
+);
 
 export const isEmbedPage = (path: string): boolean => path.startsWith(`editor/embed`);
 export const isTilesPage = (path: string): boolean => path === `components/tiles`;
@@ -18,6 +18,17 @@ export const isScrollbarPage = (path: string): boolean => path === `components/s
 export const isInputNumberPage = (path: string): boolean =>
     path === `components/input-number`;
 export const isIconsPage = (path: string): boolean => path.startsWith(`icons/`);
+
+export function tuiMakeCypressDemoPath(
+    pages: TuiDocPages,
+    exclusionSection: string[] = EXCLUSION_SECTIONS,
+    exclusionRoutes: string[] = EXCLUSION_ROUTES,
+): string[] {
+    return flatPages(pages)
+        .filter(page => !exclusionSection.includes(page.section as unknown as string))
+        .filter(page => !exclusionRoutes.includes(page.title))
+        .map(page => page.route.replace(`/`, ``));
+}
 
 function flatPages(pages: TuiDocPages): readonly TuiDocPage[] {
     return pages.reduce(
