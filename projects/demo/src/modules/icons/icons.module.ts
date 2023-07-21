7e26fc20f77f1825a26d821ce76bec84ef7c410a
splincode <splincodewd@yandex.ru>
2023-07-21 15:26:24 +0300

    chore(demo): customize icon color on demo

--- projects/demo/src/modules/icons/icons.module.ts
+++ projects/demo/src/modules/icons/icons.module.ts
@@ -3,13 +3,16 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
+import {TuiActiveZoneModule} from '@taiga-ui/cdk';
 import {
     TuiButtonModule,
+    TuiHostedDropdownModule,
     TuiLinkModule,
     TuiNotificationModule,
     TuiSvgModule,
 } from '@taiga-ui/core';
 import {TuiCheckboxLabeledModule} from '@taiga-ui/kit';
+import {TuiColorSelectorModule} from '@tinkoff/tui-editor';
 
 import {IconsComponent} from './icons.component';
 import {IconsGroupModule} from './icons-group/icons-group.module';
@@ -26,6 +29,9 @@ import {IconsGroupModule} from './icons-group/icons-group.module';
         IconsGroupModule,
         RouterModule.forChild(tuiGenerateRoutes(IconsComponent)),
         TuiCheckboxLabeledModule,
+        TuiHostedDropdownModule,
+        TuiColorSelectorModule,
+        TuiActiveZoneModule,
     ],
     declarations: [IconsComponent],
     exports: [IconsComponent],
