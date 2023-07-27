c25fef4142414adf5b495bf397a4e5a606e2176b
waterplea <alexander@inkin.ru>
2023-07-27 17:35:10 +0400

    chore: fix tests

--- projects/kit/utils/files/test/format-size.spec.ts
+++ projects/kit/utils/files/test/format-size.spec.ts
@@ -8,22 +8,22 @@ describe(`File size formatting`, () => {
     });
 
     it(`Size in bytes 1 KB, displayed in kilobytes`, () => {
-        expect(tuiFormatSize(units, 1000)).toBe(`1 KB`);
+        expect(tuiFormatSize(units, 1000)).toBe(`1000 B`);
     });
 
     it(`Size in bytes 1025 bytes are output in kilobytes, rounded down to 1 KB`, () => {
-        expect(tuiFormatSize(units, 1025)).toBe(`1 KB`);
+        expect(tuiFormatSize(units, 1025)).toBe(`1 KiB`);
     });
 
     it(`Bytes larger than 1976 are displayed in kilobytes, rounded up to 2 KB`, () => {
-        expect(tuiFormatSize(units, 1977)).toBe(`2 KB`);
+        expect(tuiFormatSize(units, 1977)).toBe(`2 KiB`);
     });
 
     it(`Size in bytes greater than 1 MB is displayed in megabytes`, () => {
-        expect(tuiFormatSize(units, 10 * 1000 * 1000)).toBe(`10 MB`);
+        expect(tuiFormatSize(units, 10 * 1000 * 1000)).toBe(`9,54 MiB`);
     });
 
     it(`Megabytes are rounded to two decimal places`, () => {
-        expect(tuiFormatSize(units, 10 * 1000 * 1000 + 220 * 1000)).toBe(`10,22 MB`);
+        expect(tuiFormatSize(units, 10 * 1000 * 1000 + 220 * 1000)).toBe(`9,75 MiB`);
     });
 });
