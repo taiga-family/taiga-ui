1eaa2c7c73a1759afcf1ffb232946ae3ddcdf98d
waterplea <alexander@inkin.ru>
2023-07-28 10:15:09 +0400

    chore: fix test

--- projects/demo-integrations/cypress/tests/kit/input-files/input-files.cy.ts
+++ projects/demo-integrations/cypress/tests/kit/input-files/input-files.cy.ts
@@ -32,13 +32,13 @@ describe(`InputFiles`, () => {
             cy.get(`[tuiInputFiles]`).selectFile(`cypress/fixtures/stubs/web-api.svg`);
 
             cy.get(`#demo-content tui-file`)
-                .should(`contain.text`, `File is too large 2 KB`)
+                .should(`contain.text`, `File is too large 2 KiB`)
                 .matchImageSnapshot(`02-01-input-files-[english]-file-too-large`);
 
             cy.tuiSetLanguage(`dutch`);
 
             cy.get(`#demo-content tui-file`)
-                .should(`contain.text`, `Bestandsgrootte overschreden 2 KB`)
+                .should(`contain.text`, `Bestandsgrootte overschreden 2 KiB`)
                 .matchImageSnapshot(`02-02-input-files-[dutch]-file-too-large`);
         });
 
