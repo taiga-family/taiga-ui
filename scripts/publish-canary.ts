f3249666f3ef64b072a5507c84b60e3299aac4da
splincode <>
2023-08-01 13:41:21 +0300

    ci: use `canary` tag instead of `dev`

--- scripts/publish-canary.ts
+++ scripts/publish-canary.ts
@@ -3,16 +3,16 @@ import {infoLog} from '../projects/cdk/schematics/utils/colored-log';
 import {execute} from './shared/execute';
 
 (function main(): void {
+    const type = `canary`;
     const commit = execute(`git rev-parse HEAD`, {}).slice(0, 7);
-    const branch = execute(`git symbolic-ref --short HEAD`, {});
     const [major, minor, patch] = version.split(/[.-]/);
 
-    // construct new version from base version x.y.z to become x.y.z-{dev}.{shortSha}
-    const newVersion = `${major}.${minor}.${patch}-dev.${branch}.${commit}`;
+    // construct new version from base version x.y.z to become x.y.z-{type}.{shortSha}
+    const newVersion = `${major}.${minor}.${patch}-${type}.${commit}`;
 
     infoLog(`New dev version - ${newVersion}`);
 
     execute(
-        `npx nx run-many --target publish --all --customTag=dev --customVersion=${newVersion}`,
+        `npx nx run-many --target publish --all --customTag=${type} --customVersion=${newVersion}`,
     );
 })();
