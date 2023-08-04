ea31e438d4307e0d4e97947f42ba09a55f2f9c35
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-08-04 12:22:31 +0300

    chore(demo-playwright): add `Playwright` (#4992)

---
+++ projects/demo-playwright/playwright.config.ts
@@ -0,0 +1,39 @@
+import {defineConfig, devices} from '@playwright/test';
+import {ViewportSize} from 'playwright-core';
+
+const DEFAULT_VIEWPORT: ViewportSize = {width: 700, height: 700};
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+    testDir: __dirname,
+    testMatch: `**/*.spec.ts`,
+    outputDir: `tests-results`,
+    snapshotDir: `snapshots`,
+    reporter: process.env.CI ? `github` : [[`html`, {outputFolder: `tests-report`}]],
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 0 : 0,
+    use: {
+        baseURL: `http://localhost:${process.env.NG_SERVER_PORT || 3333}`,
+        trace: `on-first-retry`,
+        viewport: DEFAULT_VIEWPORT,
+    },
+    projects: [
+        {
+            name: `chromium`,
+            use: {
+                ...devices[`Desktop Chrome`],
+                viewport: DEFAULT_VIEWPORT,
+            },
+        },
+    ],
+    expect: {
+        toHaveScreenshot: {
+            animations: `disabled`,
+            caret: `hide`,
+        },
+    },
+});
