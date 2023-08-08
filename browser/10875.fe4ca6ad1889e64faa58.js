e3229d820f353739ed987e7e032897d77f4ab6e7
splincode <splincode@users.noreply.github.com>
2023-08-08 16:23:38 +0000

    deploy: ea9bd7bda07923add2323b2c6b6b8d2f87df4b9e

---
+++ browser/10875.fe4ca6ad1889e64faa58.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[10875],{10875:n=>{n.exports=".input {\n    width: 20rem;\n    direction: rtl;\n    text-align: right;\n}\n"}}]);
