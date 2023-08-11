98ccdfcfa3e0acdbe3187a9dfc3345c0fefbb353
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-08-11 16:53:51 +0300

    chore(demo-playwright): update test to be compatible with proprietary repo (#5078)

--- projects/demo-playwright/tests/example.spec.ts
+++ projects/demo-playwright/tests/example.spec.ts
@@ -3,7 +3,7 @@ import {expect, test} from '@playwright/test';
 test(`has title`, async ({page}) => {
     await page.goto(`/getting-started`);
 
-    await expect(page).toHaveTitle(`Taiga UI: Getting started`);
+    await expect(page).toHaveTitle(/Taiga UI.*Getting started/);
 });
 
 test(`debug screenshot`, async ({page}) => {
