dbabfc7760d74a4e7e9633be950566eb16f3e638
splincode <omaxphp@yandex.ru>
2023-08-14 20:02:45 +0300

    ci: migrate to external linters

--- projects/eslint-plugin/utils/is-method.js
+++
@@ -1,8 +0,0 @@
-/**
- * Returns whether the node is method definition.
- * @param node {import('eslint').Rule.Node}
- * @returns {boolean}
- */
-module.exports = function isMethodDefinition(node) {
-    return node.type === 'MethodDefinition';
-};
--- projects/prettier-config/options/svg.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    parser: 'html',
-    printWidth: 120,
-    singleAttributePerLine: false,
-    plugins: [require.resolve('prettier-plugin-organize-attributes')],
-    attributeGroups: ['^(id|name)$', '^x$', '^y$', '^xmlns$', '$DEFAULT'],
-};
