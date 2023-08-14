9bd822136cf2b1cf62d9ab513774a8bfea2a4260
Alex Inkin <alexander@inkin.ru>
2023-08-14 13:02:32 +0400

    feat(core): `Alerts` customize position (#5072)
    Co-authored-by: a.inkin <a.inkin@tinkoff.ru>

---
+++ projects/core/components/alert/alert.providers.ts
@@ -0,0 +1,6 @@
+import {inject, InjectionToken} from '@angular/core';
+import {TUI_IS_MOBILE} from '@taiga-ui/cdk';
+
+export const TUI_ALERT_POSITION = new InjectionToken<string>('', {
+    factory: () => (inject(TUI_IS_MOBILE) ? '1rem 1rem 0 auto' : '2rem 3rem 0 auto'),
+});
