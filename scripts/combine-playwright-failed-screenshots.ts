75f10772919b318b5b3cb6f30bc457de640371c8
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-08-15 12:44:57 +0300

    chore: refactor

--- scripts/combine-playwright-failed-screenshots.ts
+++ scripts/combine-playwright-failed-screenshots.ts
@@ -1,16 +1,23 @@
 import {createCanvas, loadImage} from 'canvas';
-import {Dirent, readdirSync, writeFileSync} from 'fs';
+import {readdirSync, writeFileSync} from 'fs';
+
+const FAILED_SCREENSHOTS_PATH = `projects/demo-playwright/tests-results`;
+const DIFF_IMAGE_POSTFIX = `-diff.png`;
 
 (async function main(): Promise<void> {
-    const failedScreenshotTestsPath = `projects/demo-playwright/tests-results`;
-    const testsDirs: string[] = readdirSync(failedScreenshotTestsPath, {
-        withFileTypes: true,
-    }).map(({name}: Dirent) => `${failedScreenshotTestsPath}/${name}`);
+    const testsDirs: string[] = readdirSync(FAILED_SCREENSHOTS_PATH).map(
+        (dirName: string) => `${FAILED_SCREENSHOTS_PATH}/${dirName}`,
+    );
 
     for (const dir of testsDirs) {
         const imagesPaths: string[] = readdirSync(dir).map(
             (fileName: string) => `${dir}/${fileName}`,
         );
+        const diffImage = imagesPaths.find(path => path.endsWith(DIFF_IMAGE_POSTFIX));
+
+        if (!diffImage) {
+            continue;
+        }
 
         const images = await Promise.all(imagesPaths.map(loadImage));
         const totalWidth = images.reduce((acc, {width}) => acc + width, 0);
@@ -26,7 +33,8 @@ import {Dirent, readdirSync, writeFileSync} from 'fs';
         });
 
         const buffer = canvas.toBuffer(`image/png`);
+        const diffImageName = diffImage.split(`/`).pop()!.replace(DIFF_IMAGE_POSTFIX, ``);
 
-        writeFileSync(`${dir}/summary.diff.png`, buffer);
+        writeFileSync(`${dir}/${diffImageName}.diff.png`, buffer);
     }
 })();
