7d6085394e35220a41fb3173577423ca9615d61b
Nikita Barsukov <142782093+n-barsukov@users.noreply.github.com>
2023-08-22 19:03:07 +0300

    chore(demo-cypress): delete `demo.cy.ts` (#5164)

--- projects/demo-cypress/cypress/support/helpers/components-excluded.ts
+++
@@ -1,6 +0,0 @@
-export function tuiComponentsExcluded(path: string, sample: number): boolean {
-    const map = new Map<string, number[]>(Cypress.env(`componentsExclusion`) ?? []);
-    const exclusions = map.get(path) ?? [];
-
-    return !!exclusions?.includes(sample);
-}
