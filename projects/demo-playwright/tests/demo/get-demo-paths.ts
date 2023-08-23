90129374a2e622648dd3dbd6b346f63daff125da
Nikita Barsukov <142782093+n-barsukov@users.noreply.github.com>
2023-08-23 13:22:41 +0300

    chore(demo-playwright): use `process.env` to parameterize `demo.spec.ts` tests (#5173)

--- projects/demo-playwright/tests/demo/get-demo-paths.ts
+++ projects/demo-playwright/tests/demo/get-demo-paths.ts
@@ -1,23 +1,26 @@
 import {TuiDocPage, TuiDocPages} from '@taiga-ui/addon-doc';
 
+export const EXCLUDED_SECTIONS = [`Documentation`, `Foundations`, `Tools`, `Testing`];
+export const EXCLUDED_ROUTES = [
+    `/i18n`,
+    `/components/primitive-checkbox`,
+    `/components/theme-night`,
+    `/components/preview`, // no need take screenshot of buttons
+    `/components/dialog`, // just buttons
+    `/dialog/custom`, // just buttons
+    `/dialog/routable`, // just buttons
+    `/dialog/lazy-routable`, // just buttons
+    `/components/error`,
+    `/icons/overview`,
+    `/icons/mapping`,
+    `/components/mobile-calendar`, // TODO: flaky test, need investigate
+    `/components/carousel`, // TODO: create `TUI_CAROUSEL_OPTIONS` (to set `duration: 0` for E2E tests)
+];
+
 export function tuiGetDemoPathsForE2E(
     pages: TuiDocPages,
-    exclusionSection: string[] = [`Documentation`, `Foundations`, `Tools`, `Testing`],
-    exclusionRoutes: string[] = [
-        `/i18n`,
-        `/components/primitive-checkbox`,
-        `/components/theme-night`,
-        `/components/preview`, // no need take screenshot of buttons
-        `/components/dialog`, // just buttons
-        `/dialog/custom`, // just buttons
-        `/dialog/routable`, // just buttons
-        `/dialog/lazy-routable`, // just buttons
-        `/components/error`,
-        `/icons/overview`,
-        `/icons/mapping`,
-        `/components/mobile-calendar`, // TODO: flaky test, need investigate
-        `/components/carousel`, // TODO: create `TUI_CAROUSEL_OPTIONS` (to set `duration: 0` for E2E tests)
-    ],
+    exclusionSection: string[] = EXCLUDED_SECTIONS,
+    exclusionRoutes: string[] = EXCLUDED_ROUTES,
 ): string[] {
     return Array.from(
         new Set(
