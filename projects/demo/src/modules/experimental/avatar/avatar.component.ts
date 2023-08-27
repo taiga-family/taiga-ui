944423d4e68cd6a387ee14373d1c1b70e20b3787
splincode <omaxphp@yandex.ru>
2023-08-27 10:57:22 +0300

    feat(experimental): support `SafeResourceUrl` in avatar

--- projects/demo/src/modules/experimental/avatar/avatar.component.ts
+++ projects/demo/src/modules/experimental/avatar/avatar.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {RawLoaderContent, TuiDocExample} from '@taiga-ui/addon-doc';
 import {TuiSizeL, TuiSizeS} from '@taiga-ui/core';
@@ -37,13 +38,19 @@ export class ExampleTuiAvatarComponent {
 
     size = this.sizes[1];
 
-    readonly srcVariants: readonly string[] = [
+    readonly srcVariants: ReadonlyArray<SafeResourceUrl | string> = [
         'MW',
         'https://avatars.githubusercontent.com/u/11832552',
+        'https://taiga-ui.dev/assets/images/test-not-found.png',
         'tuiIconUserLarge',
+        this.sanitizer.bypassSecurityTrustResourceUrl(
+            'https://avatars.githubusercontent.com/u/10106368',
+        ),
     ];
 
     src = this.srcVariants[0];
 
     round = false;
+
+    constructor(@Inject(DomSanitizer) private readonly sanitizer: DomSanitizer) {}
 }
