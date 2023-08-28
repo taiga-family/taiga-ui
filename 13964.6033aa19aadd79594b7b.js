ecd322ca6d0152639c3e9e60161ae97142370a8f
splincode <omaxphp@yandex.ru>
2023-08-28 14:01:50 +0000

    Update snapshots/demo/next/main to output generated at ea93326

---
+++ 13964.6033aa19aadd79594b7b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[13964],{13964:o=>{o.exports=".logo {\n    color: var(--tui-base-01);\n    background-color: var(--tui-base-09);\n}\n"}}]);
