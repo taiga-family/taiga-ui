63c760e897c11614af6ea057e678dcaeade61153
splincode <omaxphp@yandex.ru>
2023-08-28 15:00:54 +0000

    Update snapshots/demo/next/main to output generated at 882266f

---
+++ 87011.8321eec31dae9fb1bf0b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[87011],{87011:n=>{n.exports=":host {\n    display: flex;\n    gap: 1rem;\n    align-items: center;\n    flex-wrap: wrap;\n}\n\n.with-border {\n    background: var(--tui-base-01);\n    color: var(--tui-primary);\n    border: 2px solid var(--tui-primary);\n}\n"}}]);
