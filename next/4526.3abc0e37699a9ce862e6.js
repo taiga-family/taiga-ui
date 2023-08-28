837b67ccd0c9e92a47f19205a3a5f6ef3394c6c2
splincode <splincode@users.noreply.github.com>
2023-08-28 14:02:49 +0000

    deploy: ea93326abcf9461df673bfd53063136a81fe52b4

--- next/4526.3abc0e37699a9ce862e6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[4526],{11966:n=>{n.exports='```html\n<tui-card\n  class="logo"\n  cardNumber="1234"\n></tui-card>\n```\n'}}]);
