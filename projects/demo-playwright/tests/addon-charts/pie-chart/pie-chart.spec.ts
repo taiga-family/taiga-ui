b43923542567781caa012c3338e158b79242eaaf
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-08-29 18:41:09 +0300

    chore(demo-playwright): playwright tests for `PieChart` (#5226)

---
+++ projects/demo-playwright/tests/addon-charts/pie-chart/pie-chart.spec.ts
@@ -0,0 +1,24 @@
+import {expect, test} from '@playwright/test';
+
+import {TuiDocumentationPagePO, tuiGoto} from '../../../utils';
+
+test.describe(`PieChart`, () => {
+    test(`should be show hints on charts`, async ({page}) => {
+        await tuiGoto(page, `charts/pie-chart`);
+
+        const example = new TuiDocumentationPagePO(page).getExample(`#labels`);
+        const pieChartSegments = await example
+            .locator(`[automation-id="tui-pie-chart__segment"]`)
+            .all();
+
+        await expect(example).toHaveScreenshot(`01-pie-chart-with-label-no-hover.png`);
+
+        for (const [i, segment] of pieChartSegments.entries()) {
+            await segment.hover();
+            await expect(page.locator(`tui-hint`)).toBeAttached();
+            await expect(example).toHaveScreenshot(
+                `01-pie-chart-with-label--hover-${i + 1}.png`,
+            );
+        }
+    });
+});
