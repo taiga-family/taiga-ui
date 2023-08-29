cde4cd191deae615159287ac60f238dc9c4237fb
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-08-29 18:40:48 +0300

    chore(demo-playwright): tests for `PrimitiveTextfield` (#5229)

---
+++ projects/demo-playwright/tests/core/primitive-textfield/primitive-textfield.spec.ts
@@ -0,0 +1,47 @@
+import {expect, test} from '@playwright/test';
+
+import {TuiDocumentationPagePO, tuiGoto} from '../../../utils';
+
+test.describe(`TuiPrimitiveTextfield`, () => {
+    test.use({viewport: {width: 400, height: 350}});
+
+    test(`Tooltip in primitive-textfield works`, async ({page}) => {
+        await tuiGoto(
+            page,
+            `components/primitive-textfield/API?tuiHintContent=Ivan%20Ivanov`,
+        );
+
+        const {apiPageExample} = new TuiDocumentationPagePO(page);
+
+        await apiPageExample.scrollIntoViewIfNeeded();
+        await apiPageExample.locator(`tui-tooltip`).click();
+
+        await expect(page.locator(`tui-hint`)).toBeAttached();
+        await expect(page).toHaveScreenshot(`01-hint.png`, {
+            mask: [page.locator(`[tuidocheader]`), page.locator(`tui-doc-page header`)],
+        });
+    });
+
+    test(`prefix + postfix align on the same line with input's value`, async ({page}) => {
+        await tuiGoto(
+            page,
+            `components/primitive-textfield/API?value=TEXT&postfix=__!&prefix=!__`,
+        );
+
+        await expect(new TuiDocumentationPagePO(page).apiPageExample).toHaveScreenshot(
+            `02-prefix-postfix.png`,
+        );
+    });
+
+    test(`label should not be visible in focused state with filler`, async ({page}) => {
+        await tuiGoto(
+            page,
+            `components/primitive-textfield/API?tuiTextfieldFiller=filler&tuiTextfieldLabelOutside=true`,
+        );
+
+        const {apiPageExample} = new TuiDocumentationPagePO(page);
+
+        await apiPageExample.getByRole(`textbox`).click();
+        await expect(apiPageExample).toHaveScreenshot(`03-label-filler.png`);
+    });
+});
