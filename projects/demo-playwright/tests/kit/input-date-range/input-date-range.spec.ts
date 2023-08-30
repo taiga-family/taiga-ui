dec8b9912d2adf8cd803e8504b12e84e2279755b
Дондоков Владислав Игоревич <v.dondokov@tinkoff.ru>
2023-08-30 17:18:09 +0300

    chore(demo): input-date-range playwright v1

--- projects/demo-playwright/tests/kit/input-date-range/input-date-range.spec.ts
+++ projects/demo-playwright/tests/kit/input-date-range/input-date-range.spec.ts
@@ -1,39 +1,113 @@
-import {Page, test} from '@playwright/test';
+import {expect, Locator, test} from '@playwright/test';
 
 import {tuiGoto} from '../../../utils';
-
-test.use({
-    viewport: {width: 650, height: 500},
-});
+import {TuiInputDateRangePO} from '../../../utils/page-objects/input-date-range.po';
 
 test.describe(`InputDateRange`, () => {
+    let example!: Locator;
+    let inputDateRange!: TuiInputDateRangePO;
+    let inputValue!: string;
+
+    test.beforeEach(async ({page}) => {
+        test.use({
+            viewport: {width: 650, height: 500},
+        });
+
+        await tuiGoto(page, `components/input-date-range`);
+
+        example = page.locator(`#demo-content .tui-primitive-textfield__native-input`);
+        inputDateRange = new TuiInputDateRangePO(example);
+    });
+
     test.describe(`API`, () => {
         for (const size of [`s`, `m`, `l`]) {
             test(`correct filler display for size ${size.toUpperCase()}`, async ({
                 page,
             }) => {
-                await visitBy(page, size);
+                await tuiGoto(
+                    page,
+                    `components/input-date-range/API?tuiMode=null&tuiTextfieldSize=${size}`,
+                );
 
-                await page.click(getInputSelector());
-                // await expect(page).toHaveScreenshot();
-                //
-                // await page.goto(page);
-                //
-                // const img = await page.screenshot({ fullPage: true });
-                //
-                // expect(img).toMatchImageSnapshot();
+                await inputDateRange.click();
+                await expect(example).toHaveScreenshot(
+                    `input-date-range-size-${size}-empty`,
+                );
+
+                await inputDateRange.type(`01`);
+                await expect(example).toHaveScreenshot(
+                    `input-date-range-size-${size}-set-day`,
+                );
+
+                await inputDateRange.type(`.06.1994`);
+                await expect(example).toHaveScreenshot(
+                    `input-date-range-size-${size}-set-from-date`,
+                );
+
+                await inputDateRange.type(`01.01.2022`);
+                await expect(example).toHaveScreenshot(
+                    `input-date-range-size-${size}-set-to-date`,
+                );
             });
         }
 
-        async function visitBy(page: Page, size: string): Promise<void> {
-            await tuiGoto(
-                page,
-                `components/input-date-range/API?tuiMode=null&tuiTextfieldSize=${size}`,
-            );
-        }
+        test(`Maximum month less than current month`, async ({page}) => {
+            await tuiGoto(page, `components/input-date-range/API?tuiMode=null&min$=3`);
 
-        function getInputSelector(): string {
-            return `#demo-content [data-testid="tui-primitive-textfield__native-input"]`;
-        }
+            await expect(example).toHaveScreenshot(`input-date-range-maximum-month`);
+        });
+
+        test(`Minimum month more than current month`, async ({page}) => {
+            await tuiGoto(page, `components/input-date-range/API?tuiMode=null&min$=3`);
+
+            await expect(example).toHaveScreenshot(`input-date-range-minimum-month`);
+        });
+
+        test.describe(`prevents changes if you enter an invalid date`, () => {
+            test(`day > 31`, async ({page}) => {
+                await tuiGoto(page, `components/input-date-range/API`);
+
+                await inputDateRange.type(`32`);
+                inputValue = await inputDateRange.inputValue;
+
+                expect(inputValue).toBe(`3`);
+
+                await inputDateRange.type(`1`);
+
+                expect(inputValue).toBe(`31`);
+            });
+
+            test(`month > 12`, async ({page}) => {
+                await tuiGoto(page, `components/input-date-range/API`);
+
+                await inputDateRange.type(`2913`);
+                inputValue = await inputDateRange.inputValue;
+
+                expect(inputValue).toBe(`29.1`);
+
+                await inputDateRange.type(`0`);
+                expect(inputValue).toBe(`0`);
+            });
+
+            test(`pads date range if it is less than [minLength]`, async ({page}) => {
+                await tuiGoto(page, `components/input-date-range/API?minLength$=0`); // minLength = {day: 3}
+
+                await inputDateRange.type(`21052023-22052023`);
+                inputValue = await inputDateRange.inputValue;
+
+                // eslint-disable-next-line no-irregular-whitespace
+                expect(inputValue).toBe(`20.05.2023 – 24.05.2023`);
+            });
+
+            test(`cuts date range if it is more than [maxLength]`, async ({page}) => {
+                await tuiGoto(page, `components/input-date-range/API?maxLength$=0`); // maxLength = {day: 5}
+
+                await inputDateRange.type(`20052023-29052023`);
+                inputValue = await inputDateRange.inputValue;
+
+                // eslint-disable-next-line no-irregular-whitespace
+                expect(inputValue).toBe(`20.05.2023 – 24.05.2023`);
+            });
+        });
     });
 });
---
+++ projects/demo-playwright/utils/page-objects/input-date-range.po.ts
@@ -0,0 +1,13 @@
+import {Locator} from '@playwright/test';
+
+export class TuiInputDateRangePO {
+    readonly inputValue = this.host.inputValue();
+    constructor(private readonly host: Locator) {}
+    async click(): Promise<void> {
+        await this.host.click();
+    }
+
+    async type(text: string): Promise<void> {
+        await this.host.type(text);
+    }
+}
