95ae668e11add63d310740509101ae5d5227128f
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-09-07 12:59:32 +0300

    chore(demo-playwright): tests for `MultiSelect` (#5308)

--- projects/demo-playwright/utils/page-objects/index.ts
+++ projects/demo-playwright/utils/page-objects/index.ts
@@ -1,4 +1,6 @@
 export * from './documentation-page.po';
 export * from './input-date-range.po';
 export * from './input-slider.po';
+export * from './input-tag.po';
+export * from './multi-select.po';
 export * from './slider.po';
