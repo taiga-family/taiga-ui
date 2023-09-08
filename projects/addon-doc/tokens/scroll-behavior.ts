4c8435611f01cb727e34203dec0fd0f65b59f852
splincode <omaxphp@yandex.ru>
2023-09-08 15:16:01 +0300

    feat(core): add `TUI_REDUCED_MOTION`

--- projects/addon-doc/tokens/scroll-behavior.ts
+++ projects/addon-doc/tokens/scroll-behavior.ts
@@ -1,6 +1,10 @@
-import {tuiCreateToken} from '@taiga-ui/cdk';
+import {inject} from '@angular/core';
+import {tuiCreateTokenFromFactory} from '@taiga-ui/cdk';
+import {TUI_REDUCED_MOTION} from '@taiga-ui/core';
 
 /**
  * Defines the transition animation for scroll
  */
-export const TUI_DOC_SCROLL_BEHAVIOR = tuiCreateToken<ScrollBehavior>(`smooth`);
+export const TUI_DOC_SCROLL_BEHAVIOR = tuiCreateTokenFromFactory<ScrollBehavior>(() =>
+    inject(TUI_REDUCED_MOTION) ? `auto` : `smooth`,
+);
