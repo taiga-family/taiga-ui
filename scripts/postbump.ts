88feab07755d036952c03be94619ecf0dd668c02
splincode <omaxphp@yandex.ru>
2023-09-12 16:35:08 +0300

    ci: ignore bump release version for taiga-ui config packages

--- scripts/postbump.ts
+++ scripts/postbump.ts
@@ -6,7 +6,14 @@ import {overwriteVersion} from './shared/overwrite-version';
 import {syncVersions} from './shared/sync-versions';
 
 (function main(): void {
-    syncVersions([`./projects`, `./package-lock.json`], version);
+    syncVersions([`./projects`, `./package-lock.json`], version, [
+        `@taiga-ui/browserslist-config`,
+        `@taiga-ui/commitlint-config`,
+        `@taiga-ui/cspell-config`,
+        `@taiga-ui/eslint-plugin-experience`,
+        `@taiga-ui/prettier-config`,
+        `@taiga-ui/stylelint-config`,
+    ]);
     overwriteVersion(resolve(`./projects/cdk/constants/version.ts`), version);
     execute(`git add .`);
 })();
