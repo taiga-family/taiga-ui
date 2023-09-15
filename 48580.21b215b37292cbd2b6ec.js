5765da5d8346654d4769160f734d14b3a557704a
nsbarsukov <nikita.s.barsukov@gmail.com>
2023-09-15 09:26:35 +0000

    Update snapshots/demo/next/main to output generated at 8cc1e76

---
+++ 48580.21b215b37292cbd2b6ec.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[48580],{48580:n=>{n.exports=":host {\n    display: flex;\n    flex-direction: column;\n    gap: 1.5rem;\n}\n\n.green {\n    color: var(--tui-support-20);\n}\n"}}]);
