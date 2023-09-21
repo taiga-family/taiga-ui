bf4d49d06d93dac160584b6f911f0952b7795ea4
splincode <omaxphp@yandex.ru>
2023-09-21 21:41:25 +0400

    ci: don't disable timers before screenshot

--- projects/demo-cypress/cypress/support/commands/snapshot.ts
+++ projects/demo-cypress/cypress/support/commands/snapshot.ts
@@ -1,6 +1,7 @@
 import {tuiAddMatchImageSnapshotCommand} from '@taiga-ui/testing/cypress';
 
 tuiAddMatchImageSnapshotCommand({
+    disableTimersAndAnimations: false,
     allowSizeMismatch: false,
     runInProcess: false,
     failureThreshold: 0.0004,
