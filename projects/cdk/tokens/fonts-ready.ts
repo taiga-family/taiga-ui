690bb49ac1cad8808f671503805440dbf0fb0d6b
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-10-02 11:45:28 +0300

    chore: fix review comments

--- projects/cdk/tokens/fonts-ready.ts
+++ projects/cdk/tokens/fonts-ready.ts
@@ -1,14 +1,14 @@
 import {DOCUMENT} from '@angular/common';
 import {inject, InjectionToken} from '@angular/core';
-import {from, NEVER, Observable} from 'rxjs';
+import {EMPTY, from, Observable} from 'rxjs';
 import {shareReplay} from 'rxjs/operators';
 
 export const TUI_FONTS_READY = new InjectionToken<Observable<unknown>>(
     `[TUI_FONTS_READY]`,
     {
         factory: () =>
-            from((inject(DOCUMENT) as any).fonts?.ready || NEVER).pipe(
-                shareReplay({bufferSize: 1, refCount: true}),
+            from((inject(DOCUMENT) as any).fonts?.ready || EMPTY).pipe(
+                shareReplay({bufferSize: 1, refCount: false}),
             ),
     },
 );
