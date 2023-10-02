406d5fdc9dd10a39114afe8d7c1ff8d8d5c24874
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2023-10-02 12:59:23 +0000

    deploy: 75358a81b7e9babb8c0a9fcb028b0423a1209b0f

---
+++ next/31355.b70c80e7848d9493b9cb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[31355],{31355:t=>{t.exports='```html\n<tui-badged-content>\n  <tui-badge-alert tuiSlot="top">1</tui-badge-alert>\n  <tui-avatar src="tuiIconUserLarge"></tui-avatar>\n  <tui-badge-alert tuiSlot="bottom">1</tui-badge-alert>\n</tui-badged-content>\n```\n'}}]);
