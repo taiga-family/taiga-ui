1454abd26c712a978dfa464db58b34b65f5cb7ed
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-10-09 10:23:26 +0300

    chore(demo-playwright): mock external images during screenshot tests

---
+++ projects/demo-playwright/utils/mock-images.ts
@@ -0,0 +1,27 @@
+import {Page} from '@playwright/test';
+
+const DEFAULT_MOCKS = [
+    {
+        patterns: [
+            /.*avatar.jpg/,
+            /.*avatars.githubusercontent.com.*/,
+            /.*bf2e94ae58ee713717faf397958a8e3d.jpg/, // filename - MD5 hash value of file content (waterplea avatar)
+        ],
+        mockImage: `${__dirname}/../stubs/github-avatar.jpeg`,
+    },
+    {patterns: [/.*web-api.svg/], mockImage: `${__dirname}/../stubs/web-api.svg`},
+] as const;
+
+export async function tuiMockImages(
+    page: Page,
+    imagesMocks: ReadonlyArray<{
+        patterns: readonly RegExp[];
+        mockImage: string;
+    }> = DEFAULT_MOCKS,
+): Promise<void> {
+    for (const {patterns, mockImage} of imagesMocks) {
+        const pattern = new RegExp(patterns.map(reg => reg.source).join(`|`));
+
+        await page.route(pattern, async route => route.fulfill({path: mockImage}));
+    }
+}
