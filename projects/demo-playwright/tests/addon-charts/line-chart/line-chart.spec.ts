356b19e27de79f2094fce959a44ca092cea994c4
Vidhi Rambhia <39163240+VidhiRambhia@users.noreply.github.com>
2023-10-10 13:29:13 -0500

    test(demo-playwright): tests for `LineChart` (#5584)
    Co-authored-by: Nikita Barsukov <nikita.s.barsukov@gmail.com>

---
+++ projects/demo-playwright/tests/addon-charts/line-chart/line-chart.spec.ts
@@ -0,0 +1,15 @@
+import {TuiDocumentationPagePO, tuiGoto} from '@demo-playwright/utils';
+import {expect, test} from '@playwright/test';
+
+test.describe(`LineChart`, () => {
+    test(`should not show hint`, async ({page}) => {
+        await tuiGoto(page, `charts/line-chart`);
+        const example = new TuiDocumentationPagePO(page).getExample(`#line`);
+        const chartColumn = example
+            .locator(`tui-line-chart-example-1 tui-line-chart .t-column`)
+            .first();
+
+        await chartColumn.hover();
+        await expect(example).toHaveScreenshot(`01-line-chart.png`);
+    });
+});
