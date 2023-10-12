a71da980b7b3dc334d692f54409c74707bd1ca1a
Maksim Ivanov <omaxphp@yandex.ru>
2023-10-12 08:37:23 +0300

    chore: fix prettier/eslint conflict (#5608)

--- projects/demo/src/modules/experimental/surface/examples/7/index.ts
+++ projects/demo/src/modules/experimental/surface/examples/7/index.ts
@@ -19,7 +19,7 @@ export class TuiSurfaceExample7 {
         {
             name: 'Alex Inkin',
             time: '3 days ago',
-            body: 'I\'m starting to kind of enjoy this library.',
+            body: 'I am starting to kind of enjoy this library.',
         },
         {
             name: 'Alex Inkin',
