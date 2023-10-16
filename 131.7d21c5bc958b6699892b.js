8ca0a397f64cb46ac26aadfbd021c7cb0bf0a6b2
splincode <omaxphp@yandex.ru>
2023-10-16 09:44:21 +0000

    Update snapshots/demo/next/main to output generated at ca941e2

---
+++ 131.7d21c5bc958b6699892b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[131],{131:a=>{a.exports="@import '@taiga-ui/core/styles/taiga-ui-local.less';\n\ntui-badge-alert {\n    margin: 0.2rem;\n    .gradient(#c86dd7, #3023ae);\n}\n"}}]);
