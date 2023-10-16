51a6c72ad716168ad6e71cec2275fe283d356e25
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2023-10-16 14:22:29 +0000

    deploy: 34e703b34c7496c422d8df0beefad20fe20e06b3

--- 85089.754025547a152372d183.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[85089],{85089:n=>{n.exports=".slider {\n    width: 12.5rem;\n    margin-bottom: 1em;\n}\n"}}]);
