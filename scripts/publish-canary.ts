551e6c23b80c99855cc8c4583b31b5519f1bbf8e
splincode <omaxphp@yandex.ru>
2023-10-16 11:42:44 +0300

    ci: `TUI_VERSION` must be shipped with the canary release

--- scripts/publish-canary.ts
+++ scripts/publish-canary.ts
@@ -1,6 +1,9 @@
+import {resolve} from 'path';
+
 import {version} from '../package.json';
 import {infoLog} from '../projects/cdk/schematics/utils/colored-log';
 import {execute} from './shared/execute';
+import {overwriteVersion} from './shared/overwrite-version';
 
 (function main(): void {
     const type = `canary`;
@@ -8,9 +11,11 @@ import {execute} from './shared/execute';
     const [major, minor, patch] = version.split(/[.-]/);
 
     // construct new version from base version x.y.z to become x.y.z-{type}.{shortSha}
-    const newVersion = `${major}.${minor}.${patch}-${type}.${commit}`;
+    const newVersion = `${major}.${minor + 1}.${patch}-${type}.${commit}`;
 
     infoLog(`New dev version - ${newVersion}`);
+
+    overwriteVersion(resolve(`./projects/cdk/constants/version.ts`), newVersion);
 
     execute(
         `npx nx run-many --target publish --all --customTag=${type} --customVersion=${newVersion}`,
