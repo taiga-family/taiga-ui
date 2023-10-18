cdb1665cd5806a7308d9641e2bfb64c07625d931
splincode <omaxphp@yandex.ru>
2023-10-18 20:42:49 +0300

    feat(i18n): add `Kazakh` language

---
+++ projects/i18n/languages/kazakh/addon-commerce.ts
@@ -0,0 +1,6 @@
+import {TuiLanguageCommerce} from '@taiga-ui/i18n/interfaces';
+
+export const TUI_KAZAKH_LANGUAGE_ADDON_COMMERCE: TuiLanguageCommerce = {
+    cardNumber: [`Сан`, `Карточка нөмірі`],
+    cardExpiry: [`Мерзімі бітеді`, `Арқылы жарамды`],
+};
---
+++ projects/i18n/languages/kazakh/addon-table.ts
@@ -0,0 +1,10 @@
+import {TuiLanguageTable} from '@taiga-ui/i18n/interfaces';
+
+export const TUI_KAZAKH_LANGUAGE_ADDON_TABLE: TuiLanguageTable = {
+    showHideText: `Көрсету/Жасыру`,
+    paginationTexts: {
+        pages: `Беттер`,
+        linesPerPage: `Әр беттегі жолдар`,
+        of: `ның`,
+    },
+};
