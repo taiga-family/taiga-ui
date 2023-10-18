b5c2d3c7f505eac34d7d7611195402dec1a9d3af
splincode <splincode@users.noreply.github.com>
2023-10-18 11:23:47 +0000

    deploy: d27927bb8d65262af4e09e0769e1767ed8c39eb3

--- next/31355.4c94ea61e78a04612d5b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[31355],{31355:t=>{t.exports='```html\n<tui-badged-content>\n  <tui-badge-alert tuiSlot="top">1</tui-badge-alert>\n  <tui-avatar src="tuiIconUserLarge"></tui-avatar>\n  <tui-badge-alert tuiSlot="bottom">1</tui-badge-alert>\n</tui-badged-content>\n```\n'}}]);
--- next/64838.32c2092faf5934256db9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[64838],{64838:e=>{e.exports="```html\n<tui-badge-alert>10</tui-badge-alert>\n```\n"}}]);
