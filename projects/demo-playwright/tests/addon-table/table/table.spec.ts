8eb38643af98495ecd616ade987185de344100fe
Atharv Chandratre <atharvchandratre@gmail.com>
2023-10-18 22:40:10 -0500

    chore(demo-playwright): tests for Table

---
+++ projects/demo-playwright/tests/addon-table/table/table.spec.ts
@@ -0,0 +1,13 @@
+import {TuiDocumentationPagePO, tuiGoto} from '@demo-playwright/utils';
+import {expect, test} from '@playwright/test';
+
+test.describe(`Table`, () => {
+    test(`Dynamic column`, async ({page}) => {
+        await tuiGoto(page, `components/table`);
+        const example = new TuiDocumentationPagePO(page).getExample(`#dynamic`);
+        const dynamicButton = example.locator(`button`).first();
+
+        await dynamicButton.click();
+        await expect(example).toHaveScreenshot(`table-dynamic.png`);
+    });
+});
