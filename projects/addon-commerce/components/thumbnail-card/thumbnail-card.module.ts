f7f8461808cba903e8fff5efb3ce56c43850b3d5
Alex Inkin <alexander@inkin.ru>
2023-10-20 11:48:32 +0400

    chore: remove directive (#5699)

--- projects/addon-commerce/components/thumbnail-card/thumbnail-card.module.ts
+++ projects/addon-commerce/components/thumbnail-card/thumbnail-card.module.ts
@@ -3,11 +3,10 @@ import {NgModule} from '@angular/core';
 import {TuiSvgModule} from '@taiga-ui/core';
 
 import {TuiThumbnailCardComponent} from './thumbnail-card.component';
-import {TuiThumbnailCardDirective} from './thumbnail-card.directive';
 
 @NgModule({
     imports: [CommonModule, TuiSvgModule],
-    declarations: [TuiThumbnailCardComponent, TuiThumbnailCardDirective],
-    exports: [TuiThumbnailCardComponent, TuiThumbnailCardDirective],
+    declarations: [TuiThumbnailCardComponent],
+    exports: [TuiThumbnailCardComponent],
 })
 export class TuiThumbnailCardModule {}
