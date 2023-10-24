bf44feb1cf5d7ed5d446f118fb21bb80ca929678
splincode <omaxphp@yandex.ru>
2023-10-24 17:22:26 +0300

    refactor: use `TuiBreakpointService`

--- projects/core/providers/is-mobile-resolution.provider.ts
+++ projects/core/providers/is-mobile-resolution.provider.ts
@@ -2,6 +2,9 @@ import {ElementRef, Provider, SkipSelf} from '@angular/core';
 import {TUI_IS_MOBILE_RES} from '@taiga-ui/core/tokens';
 import {Observable} from 'rxjs';
 
+/**
+ * @deprecated: drop in v4.0
+ */
 export const TUI_IS_MOBILE_RES_PROVIDER: Provider = {
     provide: TUI_IS_MOBILE_RES,
     deps: [[new SkipSelf(), TUI_IS_MOBILE_RES], ElementRef],
