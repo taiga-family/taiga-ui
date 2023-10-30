443a07cefbf5d8525f839c40e2eebf6544767c0f
splincode <omaxphp@yandex.ru>
2023-10-30 13:32:54 +0000

    Update snapshots/demo/next/main to output generated at 8873fad

--- 17802.9588b4f06c0b448e8c05.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[17802],{17802:(a,_,e)=>{e.r(_),e.d(_,{TUI_POLISH_LANGUAGE_ADDON_COMMERCE:()=>c});const c={cardNumber:["Numer","Numer karty"],cardExpiry:["Wygasa","Data ważności"]}}}]);
