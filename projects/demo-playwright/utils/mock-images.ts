56626d24dce234bcab431aa47e319126a338a4da
Maksim Ivanov <omaxphp@yandex.ru>
2023-10-31 10:09:50 +0300

    chore: add stub (#5799)

--- projects/demo-playwright/utils/mock-images.ts
+++ projects/demo-playwright/utils/mock-images.ts
@@ -6,6 +6,7 @@ const DEFAULT_MOCKS = [
             /.*avatar.jpg/,
             /.*avatars.githubusercontent.com.*/,
             /https:\/\/github.com\/.*.png.*/,
+            /https:\/\/m.media-amazon.com\/.*.jpg.*/,
             /.*bf2e94ae58ee713717faf397958a8e3d.jpg/, // filename - MD5 hash value of file content (waterplea avatar)
         ],
         mockImage: `${__dirname}/../stubs/github-avatar.jpeg`,
