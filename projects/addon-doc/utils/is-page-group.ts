00f10e8d2092bbf94fcb1aea3ea2bd6c33aec42d
Svyatoslav Zaytsev <143048525+MillerSvt@users.noreply.github.com>
2023-11-02 16:37:56 +0600

    feat(addon-doc): add `tuiSortPages` (#5817)

---
+++ projects/addon-doc/utils/is-page-group.ts
@@ -0,0 +1,7 @@
+import {TuiDocPage, TuiDocPageGroup} from '@taiga-ui/addon-doc/interfaces';
+
+export function tuiIsPageGroup(
+    page: TuiDocPage | TuiDocPageGroup,
+): page is TuiDocPageGroup {
+    return `subPages` in page;
+}
