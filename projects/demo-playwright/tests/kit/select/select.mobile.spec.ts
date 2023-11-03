65443342afc93ce4872d2660df5766568fbfb45d
Atharv Chandratre <atharvchandratre@gmail.com>
2023-11-03 02:29:13 -0500

    chore(demo-playwright): test for `select-mobile` (#5839)

---
+++ projects/demo-playwright/tests/kit/select/select.mobile.spec.ts
@@ -0,0 +1,29 @@
+import {tuiGoto} from '@demo-playwright/utils';
+import {expect, test} from '@playwright/test';
+
+import {
+    TUI_PLAYWRIGHT_MOBILE_USER_AGENT,
+    TUI_PLAYWRIGHT_MOBILE_VIEWPORT_HEIGHT,
+    TUI_PLAYWRIGHT_MOBILE_VIEWPORT_WIDTH,
+} from '../../../playwright.options';
+
+test.describe(`Select`, () => {
+    test.use({
+        viewport: {
+            width: TUI_PLAYWRIGHT_MOBILE_VIEWPORT_WIDTH,
+            height: TUI_PLAYWRIGHT_MOBILE_VIEWPORT_HEIGHT,
+        },
+        userAgent: TUI_PLAYWRIGHT_MOBILE_USER_AGENT,
+    });
+
+    test(`native select value`, async ({page}) => {
+        await tuiGoto(page, `components/select`);
+        const dropdown = page.locator(`#native-select select`).first();
+
+        await dropdown.selectOption({index: 1});
+
+        await expect(page.locator(`#native-select`)).toHaveScreenshot(
+            `01-native-select-value.png`,
+        );
+    });
+});
