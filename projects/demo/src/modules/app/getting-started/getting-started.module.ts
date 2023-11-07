6de319fda199227d80744f02ba62568b7849ab99
Maksim Ivanov <omaxphp@yandex.ru>
2023-11-07 14:12:16 +0300

    chore(demo): improve LCP (#5854)

--- projects/demo/src/modules/app/getting-started/getting-started.module.ts
+++ projects/demo/src/modules/app/getting-started/getting-started.module.ts
@@ -1,12 +1,21 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 import {TuiDocPageModule} from '@taiga-ui/addon-doc';
 
 import {GettingStartedComponent} from './getting-started.component';
 
 @NgModule({
-    imports: [CommonModule, TuiDocPageModule],
+    imports: [
+        CommonModule,
+        TuiDocPageModule,
+        RouterModule.forChild([
+            {
+                path: ``,
+                component: GettingStartedComponent,
+            },
+        ]),
+    ],
     declarations: [GettingStartedComponent],
-    exports: [GettingStartedComponent],
 })
 export class GettingStartedModule {}
