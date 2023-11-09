b54b0dde2df7ed82d145592a8cc25259cd909995
Atharv Chandratre <atharvchandratre@gmail.com>
2023-11-09 15:02:10 -0600

    chore(demo-playwright): test for breakpoint-service.spec.ts (#14)

---
+++ projects/demo-playwright/tests/core/breakpoint-service/breakpoint-service.spec.ts
@@ -0,0 +1,23 @@
+import {tuiGoto} from '@demo-playwright/utils';
+import {expect, test} from '@playwright/test';
+
+test.describe(`Breakpoint service`, () => {
+    for (const {width, height} of [
+        {width: 768, height: 900},
+        {width: 1024, height: 900},
+        {width: 1280, height: 900},
+    ]) {
+        test(`${width}x${height}`, async ({page}) => {
+            await page.setViewportSize({width, height});
+            await tuiGoto(page, `/services/breakpoint-service`);
+            const example = page.locator(
+                `tui-doc-example[heading="Basic"] [automation-id=tui-doc-example]`,
+            );
+
+            await example.waitFor({state: `visible`});
+            await example.scrollIntoViewIfNeeded();
+
+            await expect(example).toHaveScreenshot(`breakpoint_${width}x${height}.png`);
+        });
+    }
+});
