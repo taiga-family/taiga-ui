550bdda52329c574f308873851ba520c64580989
splincode <splincode@users.noreply.github.com>
2023-11-09 14:50:39 +0000

    deploy: a6431914e5c662f121bff7f719870a5c17eae24f

---
+++ next/12414.ccc7618852380857f6f8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[12414],{12414:n=>{n.exports='```html\n<progress\n  tuiProgressBar\n  [max]="5"\n  [segments]="5"\n  [value]="3"\n></progress>\n```\n'}}]);
