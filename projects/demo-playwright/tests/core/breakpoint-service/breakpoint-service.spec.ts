05dba376d3c54934001c4dfea1268795a69057ab
Atharv Chandratre <atharvchandratre@gmail.com>
2023-11-10 11:26:49 -0600

    chore(demo-playwright): addressing comments for breakpoint-service.spec.ts

--- projects/demo-playwright/tests/core/breakpoint-service/breakpoint-service.spec.ts
+++ projects/demo-playwright/tests/core/breakpoint-service/breakpoint-service.spec.ts
@@ -1,4 +1,4 @@
-import {tuiGoto} from '@demo-playwright/utils';
+import {TuiDocumentationPagePO, tuiGoto} from '@demo-playwright/utils';
 import {expect, test} from '@playwright/test';
 
 test.describe(`Breakpoint service`, () => {
@@ -10,10 +10,9 @@ test.describe(`Breakpoint service`, () => {
         test(`${width}x${height}`, async ({page}) => {
             await page.setViewportSize({width, height});
             await tuiGoto(page, `/services/breakpoint-service`);
-            const example = new TuiDocumentationPagePO(page).getExample(`#basic`)
+            const example = new TuiDocumentationPagePO(page).getExample(`#basic`);
 
             await example.waitFor({state: `visible`});
-            await example.scrollIntoViewIfNeeded();
 
             await expect(example).toHaveScreenshot(`breakpoint_${width}x${height}.png`);
         });
