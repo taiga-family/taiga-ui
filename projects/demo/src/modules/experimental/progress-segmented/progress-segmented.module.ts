d36d149d0be8f13620e8ba37b79b877dfb52962c
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-11-10 16:00:51 +0300

    feat(experimental): `ProgressSegmented` can be customized to have no round corners

--- projects/demo/src/modules/experimental/progress-segmented/progress-segmented.module.ts
+++ projects/demo/src/modules/experimental/progress-segmented/progress-segmented.module.ts
@@ -15,6 +15,7 @@ import {TuiProgressSegmentedExample1} from './examples/1';
 import {TuiProgressSegmentedExample2} from './examples/2';
 import {TuiProgressSegmentedExample3} from './examples/3';
 import {TuiProgressSegmentedExample4} from './examples/4';
+import {TuiProgressSegmentedExample5} from './examples/5';
 import {ExampleProgressSegmentedComponent} from './progress-segmented.component';
 
 @NgModule({
@@ -35,6 +36,7 @@ import {ExampleProgressSegmentedComponent} from './progress-segmented.component'
         TuiProgressSegmentedExample2,
         TuiProgressSegmentedExample3,
         TuiProgressSegmentedExample4,
+        TuiProgressSegmentedExample5,
     ],
 })
 export class ExampleTuiProgressSegmentedModule {}
