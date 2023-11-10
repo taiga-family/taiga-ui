d04a63896dfadb287db6d763cf05109e7de5ce44
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2023-11-10 14:13:59 +0300

    chore(cdk): `TablePagination` add migration, fix api doc (#5909)

--- projects/cdk/schematics/ng-update/v4/steps/constants/html-comments.ts
+++ projects/cdk/schematics/ng-update/v4/steps/constants/html-comments.ts
@@ -6,4 +6,9 @@ export const HTML_COMMENTS: HtmlComment[] = [
         withAttrs: [`singleColor`, `showLoader`],
         comment: `toggle [singleColor] and [showLoader] inputs have been removed due to design changes`,
     },
+    {
+        tag: `tui-table-pagination`,
+        withAttrs: [`[(page)]`, `[(size)]`, `(pageChange)`, `(sizeChange)`],
+        comment: `(pageChange) and (sizeChange) outputs have been removed. Use (paginationChange) instead`,
+    },
 ];
