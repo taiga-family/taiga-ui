939139282a8d6e18f0dc824af5aefc1f5ffe948a
splincode <splincode@users.noreply.github.com>
2023-11-13 14:03:47 +0000

    deploy: e9c775497b138e5d8aaa8c7f724285972c2d164d

---
+++ 91612.e2c3f16c4442a29181a0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[91612],{91612:n=>{n.exports=":host {\n    display: flex;\n    flex-direction: column;\n    gap: 1.5rem;\n}\n"}}]);
