23e7c653f3236ae3d334e779c79228015b06a7d5
Maksim Ivanov <omaxphp@yandex.ru>
2023-11-14 15:28:37 +0300

    feat(addon-doc): improve UX for copy code (#5918)

--- projects/addon-doc/components/code/code.module.ts
+++ projects/addon-doc/components/code/code.module.ts
@@ -1,13 +1,19 @@
 import {ClipboardModule} from '@angular/cdk/clipboard';
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {TuiButtonModule, TuiSvgModule} from '@taiga-ui/core';
 import {HighlightModule} from 'ngx-highlightjs';
 
-import {TuiDocCopyModule} from '../copy/copy.module';
 import {TuiDocCodeComponent} from './code.component';
 
 @NgModule({
-    imports: [CommonModule, HighlightModule, TuiDocCopyModule, ClipboardModule],
+    imports: [
+        CommonModule,
+        TuiSvgModule,
+        HighlightModule,
+        TuiButtonModule,
+        ClipboardModule,
+    ],
     declarations: [TuiDocCodeComponent],
     exports: [TuiDocCodeComponent],
 })
