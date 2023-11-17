d0711da595516b1636fe24e4108817d7a00d3d7d
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-11-17 19:03:12 +0300

    fix(kit): `InputDateRange` can have empty control value after selecting something from `[items]` (#6006)

---
+++ projects/demo-playwright/utils/page-objects/calendar.po.ts
@@ -0,0 +1,19 @@
+import {Locator} from '@playwright/test';
+
+export class TuiCalendarPO {
+    constructor(private readonly host: Locator) {}
+
+    async getDays(): Promise<Locator[]> {
+        return this.host.locator('[automation-id="tui-primitive-calendar__cell"]').all();
+    }
+
+    async clickOnCalendarDay(day: number): Promise<void> {
+        const cells = await this.getDays();
+
+        for (const cell of cells) {
+            if ((await cell.textContent())?.trim() === day.toString()) {
+                return cell.click();
+            }
+        }
+    }
+}
--- projects/demo-playwright/utils/page-objects/input-date-range.po.ts
+++ projects/demo-playwright/utils/page-objects/input-date-range.po.ts
@@ -1,8 +1,27 @@
-import {Locator} from '@playwright/test';
+import {expect, Locator} from '@playwright/test';
 
 export class TuiInputDateRangePO {
     readonly textfield: Locator = this.host.getByRole(`textbox`);
-    readonly calendar: Locator = this.host.page().locator(`tui-calendar-range`);
+    readonly calendarRange: Locator = this.host.page().locator(`tui-calendar-range`);
+    readonly items = this.calendarRange.locator(
+        '[automation-id="tui-calendar-range__menu"]',
+    );
 
     constructor(private readonly host: Locator) {}
+
+    async getItems(): Promise<Locator[]> {
+        const dataList = this.calendarRange.locator(
+            '[automation-id="tui-calendar-range__menu"]',
+        );
+
+        await expect(dataList).toBeAttached();
+
+        return dataList.locator(`[tuiOption]`).all();
+    }
+
+    async selectItem(index: number): Promise<void> {
+        const items = await this.getItems();
+
+        await items[index].click();
+    }
 }
