d2a261250906cbd8d1c32252cdf7cf9a7bff1b80
Atharv Chandratre <atharvchandratre@gmail.com>
2023-11-21 10:44:12 -0600

    chore(demo-playwright): checkbox.spec.ts

--- projects/demo-playwright/tests/kit/checkbox/checkbox.spec.ts
+++ projects/demo-playwright/tests/kit/checkbox/checkbox.spec.ts
@@ -4,6 +4,7 @@ import {expect, test} from '@playwright/test';
 test.describe(`Checkbox`, () => {
     [`m`, `l`].forEach(size => {
         test(`size=${size}`, async ({page}) => {
+            await page.setViewportSize({width: 300, height: 300});
             await tuiGoto(page, `components/checkbox/API?size=${size}`);
             const {apiPageExample} = new TuiDocumentationPagePO(page);
 
