057ac4b9f114c9f05375629c277cd0049e7c6a4f
splincode <omaxphp@yandex.ru>
2023-11-22 23:07:40 +0300

    chore: remove redundant lines

--- projects/demo-playwright/tests/kit/input-slider/input-slider.spec.ts
+++ projects/demo-playwright/tests/kit/input-slider/input-slider.spec.ts
@@ -1,6 +1,5 @@
 import {TuiDocumentationPagePO, tuiGoto, TuiInputSliderPO} from '@demo-playwright/utils';
 import {expect, test} from '@playwright/test';
-// eslint-disable-next-line @taiga-ui/experience/no-deep-imports
 import {CHAR_MINUS} from '@taiga-ui/cdk/constants';
 
 test.describe(`InputSlider`, () => {
