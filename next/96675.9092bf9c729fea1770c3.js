4de9aec6209026adc7ea65b47f539b7c1ed6362b
splincode <splincode@users.noreply.github.com>
2023-11-23 14:48:33 +0000

    deploy: 84cc4014cc44715d45d3b3156099c190b4637da0

---
+++ next/96675.9092bf9c729fea1770c3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[96675],{96675:t=>{t.exports='```html\n<tui-tooltip\n  content="Please don\'t touch this"\n  direction="left"\n></tui-tooltip>\n```\n'}}]);
