3313db4892546070f7cd6ace8350ec82022a6b30
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-11-23 13:26:54 +0300

    chore(demo-playwright): refactor `table-pagination.spec.ts` (#6073)

--- projects/demo-playwright/tests/addon-table/table-pagination/table-pagination.spec.ts
+++ projects/demo-playwright/tests/addon-table/table-pagination/table-pagination.spec.ts
@@ -6,23 +6,21 @@ import {
 import {expect, test} from '@playwright/test';
 
 test.describe(`TablePagination`, () => {
-    test.describe(`Dropdown with \`[size]\`-options (amount items per page)`, () => {
-        test.use({viewport: {width: 600, height: 600}});
+    test.describe(`Dropdown with [size]-options (amount items per page)`, () => {
+        test.use({viewport: {width: 600, height: 250}});
 
         test(`Basic case`, async ({page}) => {
             await tuiGoto(page, `/components/table-pagination/API`);
 
-            const {apiPageExample} = new TuiDocumentationPagePO(page);
+            const documentationPage = new TuiDocumentationPagePO(page);
 
-            const tablePaginationPO = new TuiTablePaginationPO(apiPageExample);
+            const {linesPerPageButton} = new TuiTablePaginationPO(
+                documentationPage.apiPageExample.locator(`tui-table-pagination`),
+            );
 
-            const {tablePagination, linesPerPageSelect} = tablePaginationPO;
-
-            await linesPerPageSelect.click();
-
-            await tablePaginationPO.waitForCheckmarkIcon(page);
-
-            await expect(tablePagination).toHaveScreenshot(`0-[size]-dropdown-base.png`);
+            await linesPerPageButton.click();
+            await documentationPage.prepareApiPageBeforeScreenshot();
+            await expect(page).toHaveScreenshot(`0-[size]-dropdown-base.png`);
         });
 
         test(`With very long option name`, async ({page}) => {
@@ -33,35 +31,31 @@ test.describe(`TablePagination`, () => {
                 `/components/table-pagination/API?items=[0, ${longNumber}]&size=${longNumber}&total=${longNumber}&page=0`,
             );
 
-            const {apiPageExample} = new TuiDocumentationPagePO(page);
+            const documentationPage = new TuiDocumentationPagePO(page);
+            const tablePagination = new TuiTablePaginationPO(
+                documentationPage.apiPageExample.locator(`tui-table-pagination`),
+            );
 
-            const tablePaginationPO = new TuiTablePaginationPO(apiPageExample);
-
-            const {tablePagination, linesPerPageSelect} = tablePaginationPO;
-
-            await linesPerPageSelect.click();
-
-            await tablePaginationPO.waitForCheckmarkIcon(page);
-
-            await expect(tablePagination).toHaveScreenshot(`1-[size]-dropdown-base.png`);
+            await tablePagination.linesPerPageButton.click();
+            await documentationPage.prepareApiPageBeforeScreenshot();
+            await expect(page).toHaveScreenshot(`1-[size]-dropdown-base.png`);
         });
     });
 
     test(`Custom size-option content`, async ({page}) => {
         await tuiGoto(page, `/components/table-pagination`);
 
-        const {customSizeOptionContent} = new TuiDocumentationPagePO(page);
+        const documentationPage = new TuiDocumentationPagePO(page);
+        const example = documentationPage.getExample(`#custom-size-option-content`);
 
-        const tablePaginationPO = new TuiTablePaginationPO(customSizeOptionContent);
+        const tablePagination = new TuiTablePaginationPO(
+            example.locator(`tui-table-pagination`),
+        );
+        const {linesPerPageButton, linesPerPageDropdown} = tablePagination;
 
-        const {linesPerPageSelect} = tablePaginationPO;
-
-        await linesPerPageSelect.click();
-
-        await tablePaginationPO.waitForCheckmarkIcon(page);
-
-        const dropdown = page.locator(`tui-dropdown`);
-
-        await expect(dropdown).toHaveScreenshot(`2-[sizeOptionContent]-dropdown.png`);
+        await linesPerPageButton.click();
+        await expect(linesPerPageDropdown).toHaveScreenshot(
+            `2-[sizeOptionContent]-dropdown.png`,
+        );
     });
 });
