c4bf3d51caaa7e421e7d286baea510e4f22c1535
VidhiRambhia <vsrambhia@gmail.com>
2023-11-23 01:11:29 -0600

    chore(demo-playwright): writing input-month test using playwright

---
+++ projects/demo-playwright/tests/kit/input-month/input-month.spec.ts
@@ -0,0 +1,39 @@
+import {TuiDocumentationPagePO, tuiGoto, TuiInputMonthPO} from '@demo-playwright/utils';
+import {expect, Locator, test} from '@playwright/test';
+
+const {describe, beforeEach} = test;
+
+describe(`InputMonth`, () => {
+    describe(`API`, () => {
+        let documentationPage: TuiDocumentationPagePO;
+        let example: Locator;
+        let inputMonth: TuiInputMonthPO;
+
+        test.use({
+            viewport: {width: 360, height: 500},
+        });
+
+        beforeEach(async ({page}) => {
+            await tuiGoto(page, `components/input-month/`);
+            documentationPage = new TuiDocumentationPagePO(page);
+            example = documentationPage.apiPageExample;
+            inputMonth = new TuiInputMonthPO(example.locator(`tui-input-month`));
+        });
+
+        test(`Maximum month less than current month`, async ({page}) => {
+            await tuiGoto(page, `components/input-month/API?tuiMode=null&max$=1`);
+            await inputMonth.textfield.click();
+
+            await documentationPage.prepareApiPageBeforeScreenshot();
+            await expect(page).toHaveScreenshot(`input-month-maximum-month.png`);
+        });
+
+        test(`Minimum month more than current month`, async ({page}) => {
+            await tuiGoto(page, `components/input-month/API?tuiMode=null&min$=3`);
+            await inputMonth.textfield.click();
+
+            await documentationPage.prepareApiPageBeforeScreenshot();
+            await expect(page).toHaveScreenshot(`input-month-minimum-month.png`);
+        });
+    });
+});
