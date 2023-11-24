c4ee35cb60a188c73aca32ce5afc66bcfe672812
waterplea <waterplea@users.noreply.github.com>
2023-11-24 09:28:49 +0000

    deploy: 49cd976afb03e68971752d9b4876510866addd12

---
+++ next/42430.69acd4fe581a8db45092.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[42430],{42430:a=>{a.exports='```html\n<tui-chip appearance="primary">{{ value }}</tui-chip>\n```\n'}}]);
