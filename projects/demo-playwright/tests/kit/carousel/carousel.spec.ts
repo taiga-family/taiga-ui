ebff4f7efda5469e498ce087af0ef69ecfcf305e
Atharv Chandratre <atharvchandratre@gmail.com>
2023-11-24 10:37:02 -0600

    chore(demo-playwright): carousel.spec.ts

--- projects/demo-playwright/tests/kit/carousel/carousel.spec.ts
+++ projects/demo-playwright/tests/kit/carousel/carousel.spec.ts
@@ -1,38 +1,45 @@
-import {tuiGoto} from '@demo-playwright/utils';
+import {TuiDocumentationPagePO, tuiGoto} from '@demo-playwright/utils';
 import {expect, test} from '@playwright/test';
 
 test.describe(`Carousel`, () => {
+    test.use({
+        viewport: {width: 360, height: 500},
+    });
+
     test(`default padding`, async ({page}) => {
         await tuiGoto(page, `components/carousel/API`);
+        const {apiPageExample} = new TuiDocumentationPagePO(page);
 
-        const example = page.locator(`#demo-content`);
+        await expect(apiPageExample).toBeVisible();
 
-        await expect(example).toHaveScreenshot(`carousel-padding-default.png`);
+        await expect(apiPageExample).toHaveScreenshot(`carousel-padding-default.png`);
     });
 
     test(`padding has zero value`, async ({page}) => {
         await tuiGoto(page, `components/carousel/API?style.--tui-carousel-padding=0`);
+        const {apiPageExample} = new TuiDocumentationPagePO(page);
 
-        const example = page.locator(`#demo-content`);
+        await expect(apiPageExample).toBeVisible();
 
-        await expect(example).toHaveScreenshot(`carousel-padding-0.png`);
+        await expect(apiPageExample).toHaveScreenshot(`carousel-padding-0.png`);
     });
 
     test(`should show next item after drag`, async ({page}) => {
         await tuiGoto(page, `components/carousel/API?draggable=true`);
+        const {apiPageExample} = new TuiDocumentationPagePO(page);
 
-        await page.locator(`#demo-content .t-item`).first().hover();
+        await expect(apiPageExample).toBeVisible();
+
+        await apiPageExample.locator(`.t-item`).first().hover();
 
         await page.mouse.down();
 
-        await page.mouse.move(600, 100);
+        await page.mouse.move(200, 100);
 
-        await page.mouse.move(-0, 100);
+        await page.mouse.move(0, 100);
 
         await page.mouse.up();
 
-        await expect(page.locator(`#demo-content`)).toHaveScreenshot(
-            `carousel-draggable.png`,
-        );
+        await expect(apiPageExample).toHaveScreenshot(`carousel-draggable.png`);
     });
 });
