7eecfe8dbcf72b0f428c82376358537110f10c42
a.inkin <a.inkin@tinkoff.ru>
2023-11-24 17:28:17 +0400

    chore: fix safari

--- projects/demo/used-icons.ts
+++ projects/demo/used-icons.ts
@@ -5,6 +5,7 @@
  * Array of icons used in taiga-ui components
  */
 export const TUI_USED_ICONS = [
+    `tuiIconDollarSign`,
     `tuiIconCheck`,
     `tuiIconAlertCircleLarge`,
     `tuiIconButton`,
