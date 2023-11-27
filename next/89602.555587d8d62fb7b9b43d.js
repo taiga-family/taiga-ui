7c8b1f4e5b28fe4c22703f3f3bdec89361309f25
splincode <splincode@users.noreply.github.com>
2023-11-27 14:35:27 +0000

    deploy: 900cb646b7e303a8d8bb58c64fba9cc0dd9d1ac1

---
+++ next/89602.555587d8d62fb7b9b43d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[89602],{3e3:n=>{n.exports=".text {\n    display: inline-block;\n    margin: 0.4rem 1rem;\n}\n\n.icon {\n    cursor: context-menu;\n}\n"}}]);
