f167ece1227918f4867679b66635ef5063e6b34d
shoen1x <mohdmuizz22@yahoo.com>
2023-12-03 21:23:08 +0800

    feat(i18n): add Malay language

---
+++ projects/i18n/languages/malay/addon-commerce.ts
@@ -0,0 +1,6 @@
+import {TuiLanguageCommerce} from '@taiga-ui/i18n/interfaces';
+
+export const TUI_MALAY_LANGUAGE_ADDON_COMMERCE: TuiLanguageCommerce = {
+    cardNumber: [`Nombor`, `Nombor kad`],
+    cardExpiry: [`Tamat tempoh`, `Sah sehingga`],
+};
