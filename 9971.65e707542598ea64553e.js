993cf66ab7c84f44f5ab1368141875fac691a6c9
splincode <splincode@users.noreply.github.com>
2023-12-04 13:45:11 +0000

    deploy: 95c92b9139bda2ef38b4d44c9bb32c684f387218

---
+++ 9971.65e707542598ea64553e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[9971],{9971:n=>{n.exports="@import '@taiga-ui/core/styles/taiga-ui-local.less';\n\n.custom-1 {\n    .gradient(#0094cf, #24c0ff);\n    box-shadow: var(--tui-shadow);\n}\n\n.custom-2 {\n    .gradient(#c86dd7, #3023ae);\n}\n"}}]);
--- i18n-lazy-205.91498fe495c84943f578.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[42682],{6597:(e,o,t)=>{t.r(o),t.d(o,{TUI_RUSSIAN_LANGUAGE_ADDON_PREVIEW:()=>_});const _={previewTexts:{rotate:"Повернуть"},zoomTexts:{zoomIn:"Увеличить масштаб",zoomOut:"Уменьшить масштаб",reset:"Сбросить масштаб"}}}}]);
