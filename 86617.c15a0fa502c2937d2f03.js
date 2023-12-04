f6fe9569cf3343fcea0f5fc8d182b86d24ad2ebc
splincode <omaxphp@yandex.ru>
2023-12-04 12:23:32 +0000

    Update snapshots/demo/next/main to output generated at 729bd90

---
+++ 86617.c15a0fa502c2937d2f03.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[86617],{86617:a=>{a.exports="@import '@taiga-ui/core/styles/taiga-ui-local';\n\n.portal {\n    box-shadow: var(--tui-shadow-dropdown);\n    .center-all();\n    position: fixed;\n    padding: 1.5rem;\n    border-radius: var(--tui-radius-l);\n    background: var(--tui-base-01);\n}\n"}}]);
