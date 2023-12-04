a03a9f9733d4e6d459e08f16564926f4a7932e60
VidhiRambhia <vsrambhia@gmail.com>
2023-12-04 12:25:40 -0600

    chore(demo-playwright): test for navigation

--- projects/demo-playwright/tests/addon-doc/navigation.spec.ts
+++ projects/demo-playwright/tests/addon-doc/navigation.spec.ts
@@ -2,17 +2,19 @@ import {tuiGoto} from '@demo-playwright/utils';
 import {expect, test} from '@playwright/test';
 
 test.describe(`Navigation`, () => {
-    test.use({viewport: {width: 1080, height: 522}});
+    test.use({viewport: {width: 1080, height: 600}});
     test(`getting started / [light mode]`, async ({page}) => {
         await tuiGoto(page, `/getting-started`, {
             hideHeader: false,
             hideLanguageSwitcher: true,
             hideVersionManager: true,
         });
-        const example = page.locator(`tui-doc-navigation`);
+        const sideNavigation = page.locator(`tui-doc-navigation`);
 
-        await example.isVisible();
-        await expect(example).toHaveScreenshot(`01-tui-doc-navigation-light-mode.png`);
+        await sideNavigation.isVisible();
+        await expect(sideNavigation).toHaveScreenshot(
+            `01-tui-doc-navigation-light-mode.png`,
+        );
     });
 
     test(`getting started / [night mode]`, async ({page}) => {
@@ -22,19 +24,21 @@ test.describe(`Navigation`, () => {
             hideLanguageSwitcher: true,
             hideVersionManager: true,
         });
-        const example = page.locator(`tui-doc-navigation`);
+        const sideNavigation = page.locator(`tui-doc-navigation`);
 
-        await example.isVisible();
-        await expect(example).toHaveScreenshot(`02-tui-doc-navigation-night-mode.png`);
+        await sideNavigation.isVisible();
+        await expect(sideNavigation).toHaveScreenshot(
+            `02-tui-doc-navigation-night-mode.png`,
+        );
     });
 
     test.describe(`anchor links navigation works`, () => {
-        test(`scroll to \`tui-doc-example\``, async ({page}) => {
+        test(`scroll to "tui-doc-example"`, async ({page}) => {
             await tuiGoto(page, `/components/input#table`);
             await expect(page.locator(`#table`)).toBeInViewport();
         });
 
-        test(`scroll to \`tui-doc-code\``, async ({page}) => {
+        test(`scroll to "tui-doc-code"`, async ({page}) => {
             await tuiGoto(page, `/getting-started#icons`);
             await expect(page.locator(`#icons`)).toBeVisible();
             await expect(page.locator(`#icons`)).toBeInViewport();
@@ -42,10 +46,7 @@ test.describe(`Navigation`, () => {
 
         test(`scroll after click on link with anchor`, async ({page}) => {
             await tuiGoto(page, `/getting-started`);
-            const example = page.locator(`a[fragment="root"]`);
-
-            await expect(example).toBeVisible();
-            await example.click();
+            await page.locator(`a[fragment="root"]`).click();
             await expect(page.locator(`#root`)).toBeInViewport();
         });
     });
