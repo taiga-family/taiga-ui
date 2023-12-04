d60064e7729847331547d398ab791a59a1f33bce
Revenant <mohdmuizz22@yahoo.com>
2023-12-04 15:32:11 +0800

    feat(i18n): add Malay language (#6150)

---
+++ projects/i18n/languages/malay/addon-preview.ts
@@ -0,0 +1,12 @@
+import {TuiLanguagePreview} from '@taiga-ui/i18n/interfaces';
+
+export const TUI_MALAY_LANGUAGE_ADDON_PREVIEW: TuiLanguagePreview = {
+    previewTexts: {
+        rotate: `Putar`,
+    },
+    zoomTexts: {
+        zoomOut: `Zum keluar`,
+        zoomIn: `Zum masuk`,
+        reset: `Set semula`,
+    },
+};
