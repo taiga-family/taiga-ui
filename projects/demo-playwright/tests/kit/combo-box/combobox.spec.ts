1e0bf063c89b7d81e8ca7d48cd4b432f1c1b73b8
hanahana <enigma597@mail.ru>
2023-12-05 20:29:40 +0300

    chore(demo-playwright): moved using of testIdAttribute

--- projects/demo-playwright/tests/kit/combo-box/combobox.spec.ts
+++ projects/demo-playwright/tests/kit/combo-box/combobox.spec.ts
@@ -3,29 +3,29 @@ import {expect, Locator, Page, test} from '@playwright/test';
 
 const {describe} = test;
 
-describe('ComboBox', () => {
-    test("Don't allow disabled options to be selected by typing them", async ({page}) => {
-        await tuiGoto(page, 'components/combo-box');
+describe(`ComboBox`, () => {
+    test(`Don't allow disabled options to be selected by typing them`, async ({page}) => {
+        await tuiGoto(page, `components/combo-box`);
 
         const documentationPage = new TuiDocumentationPagePO(page);
-        const example = documentationPage.getExample('#ignore-disabled');
-        const input = example.locator('tui-combo-box input[tuiTextfield]');
+        const example = documentationPage.getExample(`#ignore-disabled`);
+        const input = example.locator(`tui-combo-box input[tuiTextfield]`);
 
         await example.scrollIntoViewIfNeeded();
         await input.click();
-        await expect(page).toHaveScreenshot('01-combobox-dont-allow-disabled-01.png');
+        await expect(page).toHaveScreenshot(`01-combobox-dont-allow-disabled-01.png`);
 
-        await input.fill('Graham Chapman');
-        await expect(page).toHaveScreenshot('01-combobox-dont-allow-disabled-02.png');
+        await input.fill(`Graham Chapman`);
+        await expect(page).toHaveScreenshot(`01-combobox-dont-allow-disabled-02.png`);
 
-        await page.click('body');
-        await expect(page).toHaveScreenshot('01-combobox-dont-allow-disabled-03.png');
+        await page.click(`body`);
+        await expect(page).toHaveScreenshot(`01-combobox-dont-allow-disabled-03.png`);
 
         await input.click();
-        await expect(page).toHaveScreenshot('01-combobox-dont-allow-disabled-04.png');
+        await expect(page).toHaveScreenshot(`01-combobox-dont-allow-disabled-04.png`);
     });
 
-    describe('API', () => {
+    describe(`API`, () => {
         [true, false].forEach(strict => {
             test(`search shouldn't be reset if an exact match is entered when strict is ${strict}`, async ({
                 page,
@@ -39,7 +39,7 @@ describe('ComboBox', () => {
                 );
 
                 await getInput(apiPageExample).click();
-                await page.keyboard.type('Rubles (500)');
+                await page.keyboard.type(`Rubles (500)`);
                 await waitCheckmark(page);
                 await expect(page).toHaveScreenshot(
                     `search-should-not-be-reset-strict-waited-mark-${strict}.png`,
@@ -52,15 +52,15 @@ describe('ComboBox', () => {
                 );
 
                 await getInput(apiPageExample).click();
-                await page.keyboard.press('Backspace');
+                await page.keyboard.press(`Backspace`);
                 await focusWrapper(apiPageExample);
                 await expect(page).toHaveScreenshot(
                     `search-should-not-be-reset-strict-backspaced-${strict}.png`,
                 );
 
                 await getInput(apiPageExample).click();
-                await page.keyboard.press('Control+A');
-                await page.keyboard.press('Backspace');
+                await page.keyboard.press(`Control+A`);
+                await page.keyboard.press(`Backspace`);
                 await focusWrapper(apiPageExample);
                 await expect(page).toHaveScreenshot(
                     `search-should-not-be-reset-strict-remove-all-${strict}.png`,
