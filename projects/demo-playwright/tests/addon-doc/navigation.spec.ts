61f84b72db6d43b7144b65555100a63e0cfbd327
Vidhi Rambhia <39163240+VidhiRambhia@users.noreply.github.com>
2023-12-07 07:41:00 -0600

    chore(demo-playwright): test for navigation (#6144)
    Co-authored-by: Nikita Barsukov <nikita.s.barsukov@gmail.com>
    Co-authored-by: Atharv Chandratre <atharvchandratre@gmail.com>

---
+++ projects/demo-playwright/tests/addon-doc/navigation.spec.ts
@@ -0,0 +1,53 @@
+import {tuiGoto} from '@demo-playwright/utils';
+import {expect, test} from '@playwright/test';
+
+test.describe(`Navigation`, () => {
+    test.use({viewport: {width: 1080, height: 600}});
+    test(`getting started / [light mode]`, async ({page}) => {
+        await tuiGoto(page, `/getting-started`, {
+            hideHeader: false,
+            hideLanguageSwitcher: true,
+            hideVersionManager: true,
+        });
+        const sideNavigation = page.locator(`tui-doc-navigation`);
+
+        await sideNavigation.isVisible();
+        await expect(sideNavigation).toHaveScreenshot(
+            `01-tui-doc-navigation-light-mode.png`,
+        );
+    });
+
+    test(`getting started / [night mode]`, async ({page}) => {
+        await tuiGoto(page, `/getting-started`, {
+            hideHeader: false,
+            enableNightMode: true,
+            hideLanguageSwitcher: true,
+            hideVersionManager: true,
+        });
+        const sideNavigation = page.locator(`tui-doc-navigation`);
+
+        await sideNavigation.isVisible();
+        await expect(sideNavigation).toHaveScreenshot(
+            `02-tui-doc-navigation-night-mode.png`,
+        );
+    });
+
+    test.describe(`anchor links navigation works`, () => {
+        test(`scroll to "tui-doc-example"`, async ({page}) => {
+            await tuiGoto(page, `/components/input#table`);
+            await expect(page.locator(`#table`)).toBeInViewport();
+        });
+
+        test(`scroll to "tui-doc-code"`, async ({page}) => {
+            await tuiGoto(page, `/getting-started#icons`);
+            await expect(page.locator(`#icons`)).toBeVisible();
+            await expect(page.locator(`#icons`)).toBeInViewport();
+        });
+
+        test(`scroll after click on link with anchor`, async ({page}) => {
+            await tuiGoto(page, `/getting-started`);
+            await page.locator(`a[fragment="root"]`).click();
+            await expect(page.locator(`#root`)).toBeInViewport();
+        });
+    });
+});
