5c2e420e0894cee38c383702cabe15b38b0f9ca9
Maksim Ivanov <splincodewd@yandex.ru>
2023-12-07 18:21:14 +0300

    chore(demo): update disable-animation page (#6167)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

---
+++ projects/addon-doc/components/doc-tab/doc-tab.component.ts
@@ -0,0 +1,12 @@
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+
+@Component({
+    selector: 'tui-doc-tab',
+    templateUrl: './doc-tab.component.html',
+    styleUrls: ['./doc-tab.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TuiDocTabComponent {
+    @Input()
+    src: string = '';
+}
--- projects/demo/src/modules/info/testing/disable-animation/disable-animation.module.ts
+++ projects/demo/src/modules/info/testing/disable-animation/disable-animation.module.ts
@@ -1,8 +1,9 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
+import {TuiAddonDocModule, TuiDocTabModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
 import {TuiLinkModule} from '@taiga-ui/core';
+import {TuiTabsModule} from '@taiga-ui/kit';
 
 import {DisableAnimationComponent} from './disable-animation.component';
 
@@ -11,6 +12,8 @@ import {DisableAnimationComponent} from './disable-animation.component';
         CommonModule,
         TuiAddonDocModule,
         TuiLinkModule,
+        TuiTabsModule,
+        TuiDocTabModule,
         RouterModule.forChild(tuiGenerateRoutes(DisableAnimationComponent)),
     ],
     declarations: [DisableAnimationComponent],
