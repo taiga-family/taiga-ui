7e99f20b15a491b9b4249931c509607212d79bc0
Nils-Börge Margotti <nils.margotti@adesso.de>
2023-12-07 21:05:36 +0100

    Fix lint error

--- projects/layout/components/block-status/block-status.directive.ts
+++ projects/layout/components/block-status/block-status.directive.ts
@@ -5,5 +5,5 @@ import {Directive, Input} from '@angular/core';
 })
 export class TuiBlockStatusDirective {
     @Input()
-    tuiSlot: string | 'top' | 'action' = 'top';
+    tuiSlot: string | 'action' | 'top' = 'top';
 }
