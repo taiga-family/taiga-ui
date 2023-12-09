161fdd0fb523b9017380478de9053923ed7f7dfd
hanahana <enigma597@mail.ru>
2023-12-09 18:05:10 +0300

    chore(demo-playwright): replace locator with getByTestId

--- projects/demo-playwright/tests/kit/field-error/field-error.spec.ts
+++ projects/demo-playwright/tests/kit/field-error/field-error.spec.ts
@@ -3,12 +3,12 @@ import {expect, test} from '@playwright/test';
 
 const {describe} = test;
 
-describe('TuiFieldError', () => {
-    test('Errors of invalid control are shown correctly', async ({page}) => {
-        await tuiGoto(page, '/pipes/field-error');
+describe(`TuiFieldError`, () => {
+    test(`Errors of invalid control are shown correctly`, async ({page}) => {
+        await tuiGoto(page, `/pipes/field-error`);
 
         const errorPipeInput = page
-            .locator('tui-field-error-pipe-example-1 input')
+            .locator(`tui-field-error-pipe-example-1 input`)
             .first();
 
         await errorPipeInput.focus();
@@ -17,12 +17,10 @@ describe('TuiFieldError', () => {
 
         await errorPipeInput.blur();
 
-        const example = page
-            .locator('tui-doc-example [automation-id="tui-doc-example"]')
-            .first();
+        const example = page.getByTestId(`tui-doc-example`).first();
 
-        await expect(example).toHaveScreenshot('shows-error-under-field.png', {
-            animations: 'allow',
+        await expect(example).toHaveScreenshot(`shows-error-under-field.png`, {
+            animations: `allow`,
         });
     });
 });
