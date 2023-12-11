c54d87f734ccae5ff2422f32035b994f5a2531f1
vladimirpotekhin <vladimir.potekh@gmail.com>
2023-12-11 14:22:29 +0300

    chore: fix assets util

--- projects/demo/src/utils/load-assets.ts
+++ projects/demo/src/utils/load-assets.ts
@@ -1,6 +1,3 @@
 export function assets(path: TemplateStringsArray): string {
-    const dirname = new URL(`.`, import.meta.url);
-    const relativePath = `../assets${path.join(``)}`;
-
-    return new URL(relativePath, dirname).toString();
+    return `./assets/${path.join(``)}`;
 }
