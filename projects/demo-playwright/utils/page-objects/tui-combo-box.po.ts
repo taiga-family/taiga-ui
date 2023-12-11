f378f4103d7a7893f8c8baffb9b7403b7531708b
hanahana <enigma597@mail.ru>
2023-12-11 16:22:50 +0300

    chore(demo-playwright): remove redundant timeouts, use inheritated property, import file

--- projects/demo-playwright/utils/page-objects/tui-combo-box.po.ts
+++
@@ -1,8 +0,0 @@
-import {TuiTextfieldWithDataListPO} from '@demo-playwright/utils';
-
-export class TuiComboBoxPO extends TuiTextfieldWithDataListPO {
-    readonly comboboxTextfield = this.host
-        .page()
-        .getByTestId(`tui-combo-box__textfield`)
-        .getByTestId(`tui-primitive-textfield__native-input`);
-}
