cf27a28de1efe6bcf15165ec78df889bdcce6db1
splincode <omaxphp@yandex.ru>
2023-12-12 21:31:57 +0300

    chore(core): small backport from v4.x

--- projects/core/tokens/number-format-observable.ts
+++ projects/core/tokens/number-format-observable.ts
@@ -1,10 +1,12 @@
+import {InjectionToken} from '@angular/core';
 import {tuiCreateToken} from '@taiga-ui/cdk';
 import {TUI_DEFAULT_NUMBER_FORMAT} from '@taiga-ui/core/constants';
-import {of} from 'rxjs';
+import {TuiNumberFormatSettings} from '@taiga-ui/core/interfaces';
+import {Observable, of} from 'rxjs';
 
 /**
  * Formatting configuration for displayed numbers
  */
-export const TUI_NUMBER_FORMAT_OBSERVABLE = tuiCreateToken(
-    of({...TUI_DEFAULT_NUMBER_FORMAT, decimalLimit: NaN}),
-);
+export const TUI_NUMBER_FORMAT_OBSERVABLE: InjectionToken<
+    Observable<TuiNumberFormatSettings>
+> = tuiCreateToken(of({...TUI_DEFAULT_NUMBER_FORMAT, decimalLimit: NaN}));
