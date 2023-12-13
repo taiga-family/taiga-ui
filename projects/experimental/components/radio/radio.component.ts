3552d1b81cb34c55ca7a2b3045ee2c31c58a7bf1
vladimirpotekhin <vladimir.potekh@gmail.com>
2023-12-13 19:04:10 +0300

    chore: fix build

--- projects/experimental/components/radio/radio.component.ts
+++ projects/experimental/components/radio/radio.component.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/core';
 import {NgControl} from '@angular/forms';
 import {TuiNativeValidatorDirective} from '@taiga-ui/cdk';
+import {TuiSizeS} from '@taiga-ui/core';
 import {TuiAppearanceDirective} from '@taiga-ui/experimental/directives/appearance';
 
 import {TUI_RADIO_OPTIONS} from './radio.options';
@@ -40,7 +41,7 @@ export class TuiRadioComponent implements DoCheck {
     private readonly el: HTMLInputElement = inject(ElementRef).nativeElement;
 
     @Input()
-    size = this.options.size;
+    size: TuiSizeS = this.options.size;
 
     readonly control = inject(NgControl, {optional: true});
 
