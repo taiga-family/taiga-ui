b32a1b5bc737cdd4e6a3afa5aed99ffca578f311
waterplea <alexander@inkin.ru>
2023-12-14 09:44:03 +0000

    Update snapshots/demo/next/main to output generated at 3bc2a0c

---
+++ 5187.1b41295bef91f377.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[5187],{5187:e=>{e.exports="```ts\nimport {NgModule} from '@angular/core';\nimport {TuiAppearanceDirective} from '@taiga-ui/experimental';\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiAppearanceDirective,\n  ],\n})\nexport class MyModule {}\n```\n"}}]);
