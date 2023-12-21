6d391269a401e12a65f3d7394befb38dd17e97ca
vladimirpotekhin <vladimir.potekh@gmail.com>
2023-12-21 16:00:28 +0300

    chore: add `expandable` migration

---
+++ projects/cdk/schematics/ng-update/v4/steps/templates/migrate-expandable.ts
@@ -0,0 +1,62 @@
+import {UpdateRecorder} from '@angular-devkit/schematics';
+import {DevkitFileSystem} from 'ng-morph';
+
+import {findElementsWithAttributeOnTag} from '../../../../utils/templates/elements';
+import {
+    getTemplateFromTemplateResource,
+    getTemplateOffset,
+} from '../../../../utils/templates/template-resource';
+import {TemplateResource} from '../../../interfaces';
+
+export function migrateExpandable({
+    resource,
+    recorder,
+    fileSystem,
+}: {
+    fileSystem: DevkitFileSystem;
+    recorder: UpdateRecorder;
+    resource: TemplateResource;
+}): void {
+    const template = getTemplateFromTemplateResource(resource, fileSystem);
+    const templateOffset = getTemplateOffset(resource);
+
+    const elements = findElementsWithAttributeOnTag(
+        template,
+        ['[expandable]'],
+        ['tui-input-tag', 'tui-multi-select', 'tui-input-date'],
+    );
+
+    elements.forEach(({attrs, sourceCodeLocation}) => {
+        const expandableAttr = attrs.find(attr => attr.name === '[expandable]');
+
+        if (!expandableAttr) {
+            return;
+        }
+
+        const expandableValue = expandableAttr?.value;
+        const expandableStart =
+            sourceCodeLocation?.attrs?.[expandableAttr.name].startOffset || 0;
+        const expandableEnd =
+            sourceCodeLocation?.attrs?.[expandableAttr.name].endOffset || 0;
+
+        if (expandableValue === 'false') {
+            const rowsAttr = attrs.find(attr => attr.name === '[rows]');
+
+            const insertTo = (sourceCodeLocation?.startTag.endOffset || 0) - 1;
+
+            recorder.insertRight(templateOffset + insertTo, rowsAttr ? '' : '[rows]="1"');
+        }
+
+        if (expandableValue !== 'false' && expandableValue !== 'true') {
+            recorder.insertRight(
+                templateOffset + (sourceCodeLocation?.startTag.startOffset || 0),
+                '<!-- Taiga migration TODO: "expandable" property has been removed. Use "rows" property instead -->\n',
+            );
+        }
+
+        recorder.remove(
+            templateOffset + expandableStart,
+            expandableEnd - expandableStart,
+        );
+    });
+}
---
+++ projects/cdk/schematics/ng-update/v4/tests/schematic-migrate-expandable.spec.ts
@@ -0,0 +1,134 @@
+import {HostTree} from '@angular-devkit/schematics';
+import {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
+import {TuiSchema} from '@taiga-ui/cdk/schematics/ng-add/schema';
+import {
+    createProject,
+    createSourceFile,
+    resetActiveProject,
+    saveActiveProject,
+    setActiveProject,
+} from 'ng-morph';
+import {join} from 'path';
+
+import {createAngularJson} from '../../../utils/create-angular-json';
+
+const collectionPath = join(__dirname, '../../../migration.json');
+
+const COMPONENT_BEFORE = `
+import { TuiInputTagModule, TuiMultiSelectModule } from "@taiga-ui/kit";
+
+@Component({
+    standalone: true,
+    templateUrl: './test.template.html',
+    imports: [TuiInputTagModule, TuiMultiSelectModule]
+})
+export class TestComponent {
+}`;
+
+const TEMPLATE_BEFORE = `
+<tui-input-tag
+    [expandable]="true"
+    [formControl]="test"
+></tui-input-tag>
+
+<tui-input-tag
+    [expandable]="false"
+    [formControl]="test"
+></tui-input-tag>
+
+<tui-multi-select
+    [expandable]="false"
+>
+    <tui-data-list-wrapper
+        *tuiDataList
+        [items]="filter(search)"
+    ></tui-data-list-wrapper>
+</tui-multi-select>
+
+<tui-input-tag
+    [expandable]="true"
+    [rows]="3"
+    [formControl]="test"
+></tui-input-tag>
+
+<tui-input-tag
+    [expandable]="var"
+    [rows]="3"
+    [formControl]="test"
+></tui-input-tag>
+`;
+
+const TEMPLATE_AFTER = `
+<tui-input-tag
+    ${''}
+    [formControl]="test"
+></tui-input-tag>
+
+<tui-input-tag
+    ${''}
+    [formControl]="test"
+[rows]="1"></tui-input-tag>
+
+<tui-multi-select
+    ${''}
+[rows]="1">
+    <tui-data-list-wrapper
+        *tuiDataList
+        [items]="filter(search)"
+    ></tui-data-list-wrapper>
+</tui-multi-select>
+
+<tui-input-tag
+    ${''}
+    [rows]="3"
+    [formControl]="test"
+></tui-input-tag>
+
+<!-- Taiga migration TODO: "expandable" property has been removed. Use "rows" property instead -->
+<tui-input-tag
+    ${''}
+    [rows]="3"
+    [formControl]="test"
+></tui-input-tag>
+`;
+
+describe('ng-update', () => {
+    let host: UnitTestTree;
+    let runner: SchematicTestRunner;
+
+    beforeEach(() => {
+        host = new UnitTestTree(new HostTree());
+        runner = new SchematicTestRunner('schematics', collectionPath);
+
+        setActiveProject(createProject(host));
+
+        createMainFiles();
+
+        saveActiveProject();
+    });
+
+    it('should migrate expandable in template', async () => {
+        const tree = await runner
+            .runSchematicAsync(
+                'updateToV4',
+                {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
+                host,
+            )
+            .toPromise();
+
+        expect(tree.readContent('test/app/test.template.html')).toEqual(TEMPLATE_AFTER);
+    });
+    afterEach(() => resetActiveProject());
+});
+
+function createMainFiles(): void {
+    createSourceFile('test/app/test.component.ts', COMPONENT_BEFORE);
+
+    createSourceFile('test/app/test.template.html', TEMPLATE_BEFORE);
+
+    createAngularJson();
+    createSourceFile(
+        'package.json',
+        '{"dependencies": {"@angular/core": "~13.0.0", "@taiga-ui/addon-commerce": "~3.42.0"}}',
+    );
+}
