837ae5d746aeffdf697bca7c321ef3cb54c78541
vladimirpotekhin <vladimir.potekh@gmail.com>
2023-12-25 17:41:06 +0300

    refactor(kit)!: remove deprecated mask functions

--- projects/cdk/schematics/ng-update/v4/steps/constants/index.ts
+++ projects/cdk/schematics/ng-update/v4/steps/constants/index.ts
@@ -3,4 +3,5 @@ export * from './attrs-to-replace';
 export * from './html-comments';
 export * from './identifiers-to-replace';
 export * from './inputs-to-remove';
+export * from './migration-warnings';
 export * from './tags-to-replace';
