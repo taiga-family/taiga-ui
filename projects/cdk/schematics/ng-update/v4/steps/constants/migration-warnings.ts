076a33148893cff516b280512b8f9288d8e3819d
vladimirpotekhin <vladimir.potekh@gmail.com>
2023-12-26 17:30:43 +0300

    chore: comments

--- projects/cdk/schematics/ng-update/v4/steps/constants/migration-warnings.ts
+++ projects/cdk/schematics/ng-update/v4/steps/constants/migration-warnings.ts
@@ -5,54 +5,54 @@ export const MIGRATION_WARNINGS: MigrationWarning[] = [
         name: 'tuiCreateTimeMask',
         moduleSpecifier: '@taiga-ui/kit',
         message:
-            'tuiCreateTimeMask has been removed. Use Time mask from Maskito instead https://github.com/taiga-family/maskito',
+            'tuiCreateTimeMask has been removed. Use Time mask from Maskito instead https://maskito.dev/kit/time',
     },
     {
         name: 'tuiCreateDateRangeMask',
         moduleSpecifier: '@taiga-ui/kit',
         message:
-            'tuiCreateDateRangeMask has been removed. Use DateRange mask from Maskito instead https://github.com/taiga-family/maskito',
+            'tuiCreateDateRangeMask has been removed. Use DateRange mask from Maskito instead https://maskito.dev/kit/date-range',
     },
     {
         name: 'tuiCreateDateMask',
         moduleSpecifier: '@taiga-ui/kit',
         message:
-            'tuiCreateDateMask has been removed. Use Date mask from Maskito instead https://github.com/taiga-family/maskito',
+            'tuiCreateDateMask has been removed. Use Date mask from Maskito instead https://maskito.dev/kit/date',
     },
     {
         name: 'tuiCreateAutoCorrectedTimePipe',
         moduleSpecifier: '@taiga-ui/kit',
         message:
-            'tuiCreateAutoCorrectedTimePipe has been removed. Use Time mask from Maskito instead https://github.com/taiga-family/maskito',
+            'tuiCreateAutoCorrectedTimePipe has been removed. Use Time mask from Maskito instead https://maskito.dev/kit/time',
     },
     {
         name: 'tuiCreateAutoCorrectedDateTimePipe',
         moduleSpecifier: '@taiga-ui/kit',
         message:
-            'tuiCreateAutoCorrectedDateTimePipe has been removed. Use DateTime mask from Maskito instead https://github.com/taiga-family/maskito',
+            'tuiCreateAutoCorrectedDateTimePipe has been removed. Use DateTime mask from Maskito instead https://maskito.dev/kit/date-time',
     },
     {
         name: 'tuiCreateAutoCorrectedDateRangePipe',
         moduleSpecifier: '@taiga-ui/kit',
         message:
-            'tuiCreateAutoCorrectedDateRangePipe has been removed. Use DateRange mask from Maskito instead https://github.com/taiga-family/maskito',
+            'tuiCreateAutoCorrectedDateRangePipe has been removed. Use DateRange mask from Maskito instead https://maskito.dev/kit/date-range',
     },
     {
         name: 'tuiCreateAutoCorrectedDatePipe',
         moduleSpecifier: '@taiga-ui/kit',
         message:
-            'tuiCreateAutoCorrectedDatePipe has been removed. Use Date mask from Maskito instead https://github.com/taiga-family/maskito',
+            'tuiCreateAutoCorrectedDatePipe has been removed. Use Date mask from Maskito instead https://maskito.dev/kit/date',
     },
     {
         name: 'TuiAutoCorrectedDatePipeConfigs',
         moduleSpecifier: '@taiga-ui/kit',
         message:
-            'TuiAutoCorrectedDatePipeConfigs has been removed. Use Date mask from Maskito instead https://github.com/taiga-family/maskito',
+            'TuiAutoCorrectedDatePipeConfigs has been removed. Use Date mask from Maskito instead https://maskito.dev/kit/date',
     },
     {
         name: 'tuiNormalizeDateValue',
         moduleSpecifier: '@taiga-ui/kit',
         message:
-            'tuiNormalizeDateValue has been removed. Use Date mask from Maskito instead https://github.com/taiga-family/maskito',
+            'tuiNormalizeDateValue has been removed. Use Date mask from Maskito instead https://maskito.dev/kit/date',
     },
 ];
