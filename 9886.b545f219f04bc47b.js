13033fb5c3165fac771c714197a589a14ea6c433
splincode <omaxphp@yandex.ru>
2023-12-27 19:10:30 +0000

    Update snapshots/demo/next/main to output generated at 861de59

--- 9886.b545f219f04bc47b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[9886],{9886:n=>{n.exports="```ts\nimport {TuiPointerHintModule} from '@taiga-ui/core';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiPointerHintModule,\n  ],\n  // ...\n})\nexport class MyModule {}\n```\n"}}]);
