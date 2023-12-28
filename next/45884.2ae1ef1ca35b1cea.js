53a715df2eee36a2f22ede5ae9085ee01279dce5
splincode <splincode@users.noreply.github.com>
2023-12-28 10:43:37 +0000

    deploy: a2e4ae97e3d80127750506714100f9f98f61c324

--- next/45884.2ae1ef1ca35b1cea.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[45884],{45884:n=>{n.exports="```ts\nimport {FormControl, FormGroup} from '@angular/forms';\n\n// ...\n\n@Component({\n  // ...\n})\nexport class MyComponent {\n  testForm = new FormGroup({\n    testValue: new FormControl(10),\n  });\n}\n```\n"}}]);
