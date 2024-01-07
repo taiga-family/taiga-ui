5ab4c1c6597aed9ec7ccdea295192a65184b1396
Maksim Ivanov <splincodewd@yandex.ru>
2024-01-07 18:03:49 +0300

    chore(e2e): add `dropdown-context.spec.ts` (#6394)

---
+++ projects/demo-playwright/tests/kit/dropdown-context/dropdown-context.spec.ts
@@ -0,0 +1,39 @@
+import {TuiDocumentationPagePO, tuiGoto} from '@demo-playwright/utils';
+import {expect, test} from '@playwright/test';
+
+test.describe('DropdownContext', () => {
+    test.beforeEach(async ({page}) => tuiGoto(page, '/directives/dropdown-context'));
+
+    test('opens dropdown on right click', async ({page}) => {
+        const example = new TuiDocumentationPagePO(page).getExample('#context-menu');
+
+        await example.scrollIntoViewIfNeeded();
+        await example.locator('tr').last().click({button: 'right'});
+        await expect(example).toHaveScreenshot('01-dropdown-context.png');
+    });
+
+    test('closes previous dropdown after new one is opened', async ({page}) => {
+        const example = new TuiDocumentationPagePO(page).getExample('#context-menu');
+
+        await example.scrollIntoViewIfNeeded();
+        await example.locator('tr').nth(1).click({button: 'right'});
+        await expect(example).toHaveScreenshot('02-dropdown-context.png');
+
+        await example.locator('tr').nth(2).click({button: 'right'});
+        await expect(example).toHaveScreenshot('03-dropdown-context.png');
+    });
+
+    test('does not close dropdown when nested dropdown is clicked', async ({page}) => {
+        const example = new TuiDocumentationPagePO(page).getExample('#context-menu');
+
+        await example.scrollIntoViewIfNeeded();
+        await example.locator('tr').nth(1).click({button: 'right'});
+        await page.locator('[tuiOption]').last().click();
+
+        await expect(example).toHaveScreenshot('04-dropdown-context.png');
+
+        await page.locator('[tuiOption]').last().click();
+
+        await expect(example).toHaveScreenshot('05-dropdown-context.png');
+    });
+});
