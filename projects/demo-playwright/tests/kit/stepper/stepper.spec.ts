f46214344d78a49996b0dadc50f10f4058cf0be4
Maksim Ivanov <splincodewd@yandex.ru>
2024-01-07 17:05:57 +0300

    chore(e2e): add `stepper.spec.ts` (#6393)

---
+++ projects/demo-playwright/tests/kit/stepper/stepper.spec.ts
@@ -0,0 +1,34 @@
+import {TuiDocumentationPagePO, tuiGoto} from '@demo-playwright/utils';
+import {expect, test} from '@playwright/test';
+
+test.describe('Stepper', () => {
+    test.use({viewport: {width: 350, height: 170}});
+
+    test('horizontal orientation', async ({page}) => {
+        await tuiGoto(page, '/navigation/stepper/API?orientation=horizontal');
+
+        const po = new TuiDocumentationPagePO(page);
+        const steps = po.apiPageExample.locator('[tuiStep]');
+
+        await steps.nth(1).click();
+        await expect(po.apiPageExample).toHaveScreenshot('01-stepper.png');
+
+        await steps.nth(2).click();
+
+        await expect(po.apiPageExample).toHaveScreenshot('02-stepper.png');
+    });
+
+    test('vertical orientation', async ({page}) => {
+        await tuiGoto(page, '/navigation/stepper/API?orientation=vertical');
+
+        const po = new TuiDocumentationPagePO(page);
+        const steps = po.apiPageExample.locator('[tuiStep]');
+
+        await steps.nth(1).click();
+        await expect(po.apiPageExample).toHaveScreenshot('03-stepper.png');
+
+        await steps.nth(2).click();
+
+        await expect(po.apiPageExample).toHaveScreenshot('04-stepper.png');
+    });
+});
