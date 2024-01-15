3e44db7fd6fcf079fd578f8ea0192dd0ab2b7ed8
hanahana <enigma597@mail.ru>
2024-01-15 21:24:14 +0300

    chore(demo-playwright): from `` to ''

--- projects/demo-playwright/tests/demo/demo.spec.ts
+++ projects/demo-playwright/tests/demo/demo.spec.ts
@@ -3,8 +3,8 @@ import {expect, test} from '@playwright/test';
 
 import {tuiIsFlakyExample} from './is-flaky-examples';
 
-test.describe(`Demo`, () => {
-    const demoPaths: string[] = JSON.parse(process.env[`DEMO_PATHS`]!);
+test.describe('Demo', () => {
+    const demoPaths: string[] = JSON.parse(process.env['DEMO_PATHS']!);
 
     demoPaths.forEach(path => {
         test(path, async ({page}) => {
@@ -12,16 +12,16 @@ test.describe(`Demo`, () => {
             await tuiGoto(page, path);
 
             await expect(async () => {
-                const examples = await page.locator(`tui-doc-example`).all();
+                const examples = await page.locator('tui-doc-example').all();
 
                 expect(examples.length).toBeGreaterThan(0);
 
                 for (const example of examples) {
-                    await expect(example.getByTestId(`tui-doc-example`)).toBeAttached();
+                    await expect(example.getByTestId('tui-doc-example')).toBeAttached();
                 }
             }).toPass();
 
-            const examples = await page.getByTestId(`tui-doc-example`).all();
+            const examples = await page.getByTestId('tui-doc-example').all();
 
             for (const [i, example] of examples.entries()) {
                 if (tuiIsFlakyExample(path, i)) {
@@ -29,7 +29,7 @@ test.describe(`Demo`, () => {
                 }
 
                 await expect(example).toHaveScreenshot([
-                    path.replace(`/`, ``).replace(/\//g, `-`),
+                    path.replace('/', '').replace(/\//g, '-'),
                     `${i + 1}.png`,
                 ]);
             }
