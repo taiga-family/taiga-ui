f86ce50928ed226c5c6731bf919ad021f34babc3
Maksim Ivanov <splincodewd@yandex.ru>
2024-01-17 17:57:44 +0300

    chore(e2e): add `input-table.spec.ts` (#6468)

---
+++ projects/demo-playwright/tests/kit/input-table/input-table.spec.ts
@@ -0,0 +1,40 @@
+import {TuiDocumentationPagePO, tuiGoto} from '@demo-playwright/utils';
+import {expect, test} from '@playwright/test';
+
+test.describe('InputTable', () => {
+    test('editing fields inside a table', async ({page}) => {
+        await tuiGoto(page, '/components/input');
+
+        const example = new TuiDocumentationPagePO(page).getExample('#table');
+
+        await example.scrollIntoViewIfNeeded();
+
+        for (const index of [0, 1, 2, 3, 4]) {
+            await example
+                .locator('tbody tr td')
+                .nth(index)
+                .getByTestId('tui-primitive-textfield__wrapper')
+                .locator('input')
+                .nth(0)
+                .focus();
+
+            await expect(example).toHaveScreenshot(`01-input_table__${index}_1.png`);
+
+            await example.locator('th').nth(index).click();
+
+            await expect(example).toHaveScreenshot(`01-input_table__${index}_2.png`);
+
+            if (index > 0) {
+                await example
+                    .locator('tbody tr td')
+                    .nth(index)
+                    .getByTestId('tui-primitive-textfield__wrapper')
+                    .locator('input')
+                    .nth(0)
+                    .focus();
+
+                await expect(example).toHaveScreenshot(`01-input_table__${index}_3.png`);
+            }
+        }
+    });
+});
