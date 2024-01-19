30ef5c863625ed1b5201cbcea2f4f33898dc8fd7
splincode <omaxphp@yandex.ru>
2024-01-19 14:35:25 +0000

    Update snapshots/demo/next/main to output generated at a7cb3ac

--- 58202.c3db66ae3696161f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[58202],{58202:n=>{n.exports='```html\n<tui-badged-content\n  [contentTop]="contentTop"\n  [colorTop]="colorTop"\n>\n  Content to wrap\n</tui-badged-content>\n```\n'}}]);
