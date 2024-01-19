917565e64a5979c2396f84f08cf2124999fa52c5
vladimirpotekhin <vladimir.potekh@gmail.com>
2024-01-19 11:45:38 +0300

    feat(kit): `Sensitive` increase number of rows for larger sizes

--- projects/experimental/directives/sensitive/sensitive.directive.ts
+++ projects/experimental/directives/sensitive/sensitive.directive.ts
@@ -1,12 +1,22 @@
 import {Directive, Inject, Input} from '@angular/core';
-import {TuiDirectiveStylesService} from '@taiga-ui/cdk';
+import {
+    TuiDestroyService,
+    TuiDirectiveStylesService,
+    TuiResizeService,
+} from '@taiga-ui/cdk';
+import {map} from 'rxjs';
 
 import {TuiSensitiveComponent} from './sensitive.component';
 
+const rowsInSvg = 3;
+
 @Directive({
     selector: '[tuiSensitive]',
+    providers: [TuiResizeService, TuiDestroyService],
     host: {
         '[style.--t-offset.px]': 'offset',
+        '[$.style.--t-mask-height.px]': 'height$',
+        '($.style.--t-mask-height.px)': 'height$',
         '[class.tui-sensitive]': 'tuiSensitive',
     },
 })
@@ -15,9 +25,18 @@ export class TuiSensitiveDirective {
     tuiSensitive: boolean | null = false;
 
     readonly offset = Math.round(Math.random() * 10) * 10;
+    readonly height$ = this.resize$.pipe(
+        map(([entry]) => entry.contentRect.height),
+        map(height => {
+            const rows = Math.max(2, Math.floor(height / 16) + 1);
+
+            return height * (rowsInSvg / rows);
+        }),
+    );
 
     constructor(
         @Inject(TuiDirectiveStylesService) directiveStyles: TuiDirectiveStylesService,
+        @Inject(TuiResizeService) private readonly resize$: TuiResizeService,
     ) {
         directiveStyles.addComponent(TuiSensitiveComponent);
     }
