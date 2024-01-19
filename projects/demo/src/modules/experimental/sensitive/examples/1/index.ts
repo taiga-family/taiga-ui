a3015f438d771de91f8bb479683c769f40b25c8a
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-01-19 15:04:27 +0300

    feat(kit): `Sensitive` increase number of rows for larger sizes (#6491)

--- projects/demo/src/modules/experimental/sensitive/examples/1/index.ts
+++ projects/demo/src/modules/experimental/sensitive/examples/1/index.ts
@@ -5,6 +5,7 @@ import {encapsulation} from '@demo/emulate/encapsulation';
 @Component({
     selector: 'tui-sensitive-example-1',
     templateUrl: './index.html',
+    styleUrls: ['./index.less'],
     encapsulation,
     changeDetection,
 })
