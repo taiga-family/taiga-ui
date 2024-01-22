eef8e7d02e212c91e4283459f53da75b0b23ef80
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-01-22 14:20:35 +0300

    feat(experimental): `Header` add new component (#6455)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

---
+++ projects/demo/src/modules/experimental/header/header.module.ts
@@ -0,0 +1,56 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {tuiGetDocModules} from '@taiga-ui/addon-doc';
+import {
+    TuiNotificationModule,
+    TuiTextfieldControllerModule,
+    TuiTooltipModule,
+} from '@taiga-ui/core';
+import {
+    TuiAvatarModule,
+    TuiBadgeModule,
+    TuiBadgeNotificationModule,
+    TuiButtonModule,
+    TuiHeaderModule,
+    TuiIconModule,
+    TuiSensitiveModule,
+    TuiTitleModule,
+    TuiToggleModule,
+} from '@taiga-ui/experimental';
+import {TuiDataListWrapperModule, TuiSelectModule} from '@taiga-ui/kit';
+
+import {TuiHeaderExample1} from './examples/1';
+import {TuiHeaderExample2} from './examples/2';
+import {TuiHeaderExample3} from './examples/3';
+import {ExampleTuiCellComponent} from './header.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        TuiBadgeModule,
+        TuiBadgeNotificationModule,
+        TuiToggleModule,
+        TuiTooltipModule,
+        TuiHeaderModule,
+        TuiTitleModule,
+        TuiIconModule,
+        TuiSensitiveModule,
+        TuiSelectModule,
+        TuiDataListWrapperModule,
+        TuiTextfieldControllerModule,
+        TuiButtonModule,
+        tuiGetDocModules(ExampleTuiCellComponent),
+        TuiNotificationModule,
+        TuiAvatarModule,
+    ],
+    declarations: [
+        ExampleTuiCellComponent,
+        TuiHeaderExample1,
+        TuiHeaderExample2,
+        TuiHeaderExample3,
+    ],
+    exports: [ExampleTuiCellComponent],
+})
+export class ExampleTuiHeaderModule {}
--- projects/experimental/components/badge-notification/badge-notification.component.ts
+++ projects/experimental/components/badge-notification/badge-notification.component.ts
@@ -7,6 +7,11 @@ import {
     TuiSizeXS,
 } from '@taiga-ui/core';
 
+import {
+    TUI_BADGE_NOTIFICATION_OPTIONS,
+    TuiBadgeNotificationOptions,
+} from './badge-notification.options';
+
 @Component({
     selector: 'tui-badge-notification',
     template: '<ng-content></ng-content>',
@@ -20,7 +25,11 @@ import {
 })
 export class TuiBadgeNotificationComponent {
     @Input()
-    size: TuiSizeL | TuiSizeXS = 'l';
+    size: TuiSizeL | TuiSizeXS = this.options.size;
 
-    constructor(@Inject(TUI_ANIMATION_OPTIONS) readonly animation: TuiAnimationOptions) {}
+    constructor(
+        @Inject(TUI_ANIMATION_OPTIONS) readonly animation: TuiAnimationOptions,
+        @Inject(TUI_BADGE_NOTIFICATION_OPTIONS)
+        private readonly options: TuiBadgeNotificationOptions,
+    ) {}
 }
