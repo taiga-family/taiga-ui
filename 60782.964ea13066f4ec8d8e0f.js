e5f921a480d0fbfeffb29040e2358aad49076b45
splincode <splincode@users.noreply.github.com>
2024-01-22 15:23:08 +0000

    deploy: f86b92cb88d8e1f42065911999a260b9b90c8d83

---
+++ 60782.964ea13066f4ec8d8e0f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[60782],{60782:i=>{i.exports='<tui-badge-notification size="l">10</tui-badge-notification>\n<tui-badge-notification>11</tui-badge-notification>\n<tui-badge-notification size="s">12</tui-badge-notification>\n<tui-badge-notification size="xs"></tui-badge-notification>\n'}}]);
