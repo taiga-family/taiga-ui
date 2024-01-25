2f2e3eae4ea779187b5a0d68c2d015264246fe1b
waterplea <alexander@inkin.ru>
2024-01-25 15:45:07 +0400

    chore: fix tests

--- setup-jest.ts
+++ setup-jest.ts
@@ -4,4 +4,11 @@ import '@taiga-ui/testing/setup-jest';
 
 import {tuiSwitchNgDevMode} from '@taiga-ui/testing';
 
+// TODO: Remove when updated to ng-web-apis 4
+(global as any).ResizeObserver = class {
+    observe(): void {}
+    unobserve(): void {}
+    disconnect(): void {}
+};
+
 tuiSwitchNgDevMode(false);
