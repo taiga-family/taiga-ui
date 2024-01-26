139a86af02f5161a968e0b69fc82d45b5b3b7e4c
Maksim Ivanov <splincodewd@yandex.ru>
2024-01-26 14:56:08 +0300

    chore(demo): set ssr page as standalone (#6566)

--- projects/demo/src/modules/info/ssr/ssr.component.ts
+++ projects/demo/src/modules/info/ssr/ssr.component.ts
@@ -1,9 +1,13 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
+import {TuiDocPageModule} from '@taiga-ui/addon-doc';
+import {TuiLinkModule} from '@taiga-ui/core';
 
 @Component({
+    standalone: true,
     selector: 'ssr',
+    imports: [TuiDocPageModule, TuiLinkModule],
     templateUrl: './ssr.template.html',
     changeDetection,
 })
-export class SsrComponent {}
+export default class SsrComponent {}
