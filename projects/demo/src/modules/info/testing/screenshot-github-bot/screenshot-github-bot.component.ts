243bceded7d4310a39e8aeff763a18be17623c9a
splincode <omaxphp@yandex.ru>
2024-01-26 21:55:42 +0300

    chore(demo): set screenshot-github-bot page as standalone

--- projects/demo/src/modules/info/testing/screenshot-github-bot/screenshot-github-bot.component.ts
+++ projects/demo/src/modules/info/testing/screenshot-github-bot/screenshot-github-bot.component.ts
@@ -1,15 +1,19 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TuiDocPageModule} from '@taiga-ui/addon-doc';
+import {TuiLinkModule} from '@taiga-ui/core';
 
 export const SCREENSHOT_BOT_NAME = 'Lumberjack';
 export const SCREENSHOT_BOT_LINK = 'https://github.com/apps/lumberjack-bot';
 
 @Component({
+    standalone: true,
     selector: 'screenshot-github-bot',
+    imports: [TuiDocPageModule, TuiLinkModule],
     templateUrl: './screenshot-github-bot.template.html',
     styleUrls: ['./screenshot-github-bot.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScreenshotGithubBotComponent {
+export default class ScreenshotGithubBotComponent {
     readonly botName = SCREENSHOT_BOT_NAME;
     readonly botAppLink = SCREENSHOT_BOT_LINK;
     readonly botRepoLink = 'https://github.com/taiga-family/argus';
