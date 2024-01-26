d465b97c7de3a823f740e12576748a47ec04651a
waterplea <alexander@inkin.ru>
2024-01-26 09:54:10 +0000

    Update snapshots/demo/next/main to output generated at a034e20

---
+++ 41662.d8febac04431d19f.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[41662],{41662:e=>{e.exports="```ts\nimport {TuiMediaDirective} from '@taiga-ui/cdk';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiMediaDirective,\n  ],\n  // ...\n})\nexport class MyModule {}\n```\n"}}]);
