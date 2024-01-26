c962bd2e2698b3ae22f151b2b5ec25cc41873257
splincode <omaxphp@yandex.ru>
2024-01-26 18:29:57 +0300

    chore(demo): set disable-animation page as standalone

--- projects/demo/src/modules/info/testing/disable-animation/disable-animation.component.ts
+++ projects/demo/src/modules/info/testing/disable-animation/disable-animation.component.ts
@@ -1,12 +1,30 @@
+import {NgForOf, NgIf} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {
+    TuiAddonDocModule,
+    TuiDocDocumentationModule,
+    TuiDocPageModule,
+} from '@taiga-ui/addon-doc';
+import {TuiLinkModule} from '@taiga-ui/core';
+import {TuiTabsModule} from '@taiga-ui/kit';
 
 @Component({
+    standalone: true,
     selector: 'disable-animation',
+    imports: [
+        NgForOf,
+        TuiDocDocumentationModule,
+        TuiDocPageModule,
+        TuiLinkModule,
+        TuiTabsModule,
+        TuiAddonDocModule,
+        NgIf,
+    ],
     templateUrl: './disable-animation.template.html',
     styleUrls: ['./disable-animation.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DisableAnimationComponent {
+export default class DisableAnimationComponent {
     readonly cypress = import('./examples/cypress.md?raw');
     readonly playwright = import('./examples/playwright.md?raw');
 }
