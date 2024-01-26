69a6c486a78688f953a23b5cfc392a50487c1f65
Maksim Ivanov <splincodewd@yandex.ru>
2024-01-26 16:35:56 +0300

    chore(demo): set changelog page as standalone (#6565)

--- projects/demo/src/modules/info/changelog/changelog.component.ts
+++ projects/demo/src/modules/info/changelog/changelog.component.ts
@@ -1,11 +1,15 @@
+import {AsyncPipe} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TuiDocPageModule, TuiMarkdownPipe} from '@taiga-ui/addon-doc';
 
 @Component({
+    standalone: true,
     selector: 'changelog',
+    imports: [TuiDocPageModule, TuiMarkdownPipe, AsyncPipe],
     templateUrl: './changelog.template.html',
     styleUrls: ['./changelog.component.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChangelogComponent {
+export default class ChangelogComponent {
     readonly changelog = import('../../../../../../CHANGELOG.md?raw');
 }
