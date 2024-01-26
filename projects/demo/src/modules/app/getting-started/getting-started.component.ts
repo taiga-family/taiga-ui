275e4f8da1265aafdd1f64afab0cc65b074781fd
splincode <omaxphp@yandex.ru>
2024-01-26 11:13:59 +0300

    chore(demo): set getting-started as standalone

--- projects/demo/src/modules/app/getting-started/getting-started.component.ts
+++ projects/demo/src/modules/app/getting-started/getting-started.component.ts
@@ -1,5 +1,7 @@
+import {NgComponentOutlet} from '@angular/common';
 import {Component, Inject, Type} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
+import {TuiDocPageModule} from '@taiga-ui/addon-doc';
 import {tuiCreateToken} from '@taiga-ui/cdk';
 
 import {HomeComponent} from '../home/home.component';
@@ -7,10 +9,12 @@ import {HomeComponent} from '../home/home.component';
 export const TUI_HOME_COMPONENT = tuiCreateToken<Type<unknown>>(HomeComponent);
 
 @Component({
+    standalone: true,
     selector: 'getting-started',
+    imports: [TuiDocPageModule, NgComponentOutlet],
     templateUrl: './getting-started.template.html',
     changeDetection,
 })
-export class GettingStartedComponent {
+export default class GettingStartedComponent {
     constructor(@Inject(TUI_HOME_COMPONENT) readonly component: Type<unknown>) {}
 }
