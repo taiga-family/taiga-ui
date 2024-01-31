c29ef4a10ccfbbe6dd50e4068303370ff1c2e0b5
Maksim Ivanov <splincodewd@yandex.ru>
2024-01-31 18:44:35 +0300

    chore(demo): set jet page as standalone (#6572)

--- projects/demo/src/modules/info/testing/jest/jest.component.ts
+++ projects/demo/src/modules/info/testing/jest/jest.component.ts
@@ -1,11 +1,14 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TuiDocCodeModule, TuiDocPageModule} from '@taiga-ui/addon-doc';
 
 @Component({
+    standalone: true,
     selector: 'jest',
+    imports: [TuiDocPageModule, TuiDocCodeModule],
     templateUrl: './jest.template.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class JestComponent {
+export default class JestComponent {
     readonly jestConfigJs = import('./examples/jest-config-js.md?raw');
     readonly packageJson = import('./examples/package-json.md?raw');
     readonly setupJestJs = import('./examples/setup-jest-js.md?raw');
