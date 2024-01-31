872a331d79e1b76c885964b2a17467a245716d35
splincode <splincode@users.noreply.github.com>
2024-01-31 16:30:04 +0000

    deploy: ba2c844254e4bf87ae58b3a5c13efe796f102d5b

---
+++ 4004.eb8dd4a67d59e5b963bc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[4004],{4004:n=>{n.exports=":host {\n    display: flex;\n    gap: 1rem;\n}\n\ntui-avatar {\n    border: 1px solid var(--tui-base-04);\n}\n"}}]);
