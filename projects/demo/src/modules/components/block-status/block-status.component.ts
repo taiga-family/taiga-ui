762a5ac1b5d9404e3d493050e55e732d60702b90
vladimirpotekhin <vladimir.potekh@gmail.com>
2024-01-31 13:43:38 +0300

    feat(layout): `BlockStatus` update mobile paddings

--- projects/demo/src/modules/components/block-status/block-status.component.ts
+++ projects/demo/src/modules/components/block-status/block-status.component.ts
@@ -33,5 +33,11 @@ export class ExampleTuiBlockStatusComponent {
         LESS: import('./examples/4/index.less?raw'),
     };
 
+    readonly example5: TuiDocExample = {
+        TypeScript: import('./examples/5/index.ts?raw'),
+        HTML: import('./examples/5/index.html?raw'),
+        LESS: import('./examples/5/index.less?raw'),
+    };
+
     card = false;
 }
