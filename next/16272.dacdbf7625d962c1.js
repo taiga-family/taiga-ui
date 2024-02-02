b9e8fcb1ff47d697956675f81b34c23e25d07c8e
waterplea <waterplea@users.noreply.github.com>
2024-02-02 08:43:05 +0000

    deploy: c923ad8f1e418f14d55f92efc36f3d2d8ad13c10

--- next/16272.dacdbf7625d962c1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[16272],{16272:e=>{e.exports="```ts\nimport {TuiPdfViewerModule} from '@taiga-ui/kit';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiPdfViewerModule,\n  ],\n  // ...\n})\nexport class AppBrowserModule {}\n```\n"}}]);
--- next/99721.d440644ab9ea0907.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[99721],{99721:e=>{e.exports="```ts\nimport {TuiSheetDialogModule} from '@taiga-ui/addon-mobile';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiSheetDialogModule,\n  ],\n  // ...\n})\nexport class AppModule {}\n```\n"}}]);
