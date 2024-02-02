1d041985d1a7bc6d2998e2a422ba3ac1dd51e475
splincode <omaxphp@yandex.ru>
2024-02-02 12:09:51 +0300

    refactor: svgNodeFilter

--- projects/cdk/constants/test/svg-node-filter.spec.ts
+++ projects/cdk/constants/test/svg-node-filter.spec.ts
@@ -1,16 +1,15 @@
 import {svgNodeFilter} from '@taiga-ui/cdk';
 
-// TODO: 3.0 rewrite tests after IE removing
 describe('SvgNodeFilter', () => {
     it('child SVG', () => {
-        const node: Node = {ownerSVGElement: null} as unknown as Node;
-
-        expect((svgNodeFilter as any)(node)).toBe(NodeFilter.FILTER_REJECT);
+        expect(svgNodeFilter.acceptNode({ownerSVGElement: null} as unknown as Node)).toBe(
+            NodeFilter.FILTER_REJECT,
+        );
     });
 
     it('not child SVG', () => {
-        const node: Node = {} as unknown as Node;
-
-        expect((svgNodeFilter as any)(node)).toBe(NodeFilter.FILTER_ACCEPT);
+        expect(svgNodeFilter.acceptNode({} as unknown as Node)).toBe(
+            NodeFilter.FILTER_ACCEPT,
+        );
     });
 });
