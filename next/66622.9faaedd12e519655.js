cf79295404617c80415aa20b886c727fab94c5cf
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2024-02-05 13:25:59 +0000

    deploy: 6cb738eeacd2eaeb29bdc6056e5ac015f7fed6fb

---
+++ next/66622.9faaedd12e519655.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[66622],{89004:a=>{a.exports='```html\n<tui-tag [value]="tag"></tui-tag>\n\x3c!--Use can use tag as link--\x3e\n<a\n  tuiTag\n  href="https://somesite.com"\n  target="_blank"\n  [value]="tag"\n></a>\n```\n'}}]);
