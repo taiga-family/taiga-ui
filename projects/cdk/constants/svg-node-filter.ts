8dbaf65ccdc90f17e5966b4bfed167f584a170f2
Maksim Ivanov <splincodewd@yandex.ru>
2024-02-08 11:27:49 +0300

    refactor: svgNodeFilter (#6661)

--- projects/cdk/constants/svg-node-filter.ts
+++ projects/cdk/constants/svg-node-filter.ts
@@ -1,6 +1,7 @@
-// Filtering SVGElements for TreeWalker
-// Filter must be a function in IE, other modern browsers are compliant to this format
-export const svgNodeFilter: NodeFilter = ((node: Node) =>
-    'ownerSVGElement' in node
-        ? NodeFilter.FILTER_REJECT
-        : NodeFilter.FILTER_ACCEPT) as any;
+export const svgNodeFilter: Exclude<NodeFilter, (node: Node) => number> = {
+    acceptNode(node: Node): number {
+        return 'ownerSVGElement' in node
+            ? NodeFilter.FILTER_REJECT
+            : NodeFilter.FILTER_ACCEPT;
+    },
+};
