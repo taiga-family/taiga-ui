f4cdc728993a03f194d84094024cc551802ded9d
Maksim Ivanov <splincodewd@yandex.ru>
2024-02-08 18:51:29 +0300

    chore: add `request.cy.ts` (#6648)

---
+++ projects/demo-cypress/src/tests/static-request.cy.ts
@@ -0,0 +1,72 @@
+import {TestBed} from '@angular/core/testing';
+import {TuiStaticRequestService} from '@taiga-ui/cdk';
+
+describe('TuiStaticRequestService', () => {
+    let service: TuiStaticRequestService;
+    const baseUrl = 'http://localhost:8080';
+    const response = {
+        status: 200,
+        contentType: 'text/plain',
+        responseText: 'awesome response',
+    };
+
+    beforeEach(() => {
+        cy.intercept('GET', '**test*', response)
+            .as('request')
+            .then(() => {
+                TestBed.configureTestingModule({
+                    providers: [TuiStaticRequestService],
+                });
+
+                service = TestBed.inject(TuiStaticRequestService);
+            });
+    });
+
+    it('returns the same observable on second request for the same URL', () => {
+        const result = service.request(`${baseUrl}/test`);
+        const second = service.request(`${baseUrl}/test`);
+
+        expect(result).to.eql(second);
+        cy.get('@request.all').should('have.length', 0);
+    });
+
+    it('returns the different observable on second request for different URL', () => {
+        const result = service.request(`${baseUrl}/test1`);
+        const second = service.request(`${baseUrl}/test2`);
+
+        expect(result).not.to.eql(second);
+        cy.get('@request.all').should('have.length', 0);
+    });
+
+    it('the request is made to the url and completes', () => {
+        service.request(`${baseUrl}/test`).subscribe();
+
+        cy.get('@request').its('response.body').should('deep.equal', response);
+    });
+
+    it('the request is only made once, same result is returned on the subsequent calls', () => {
+        let result1 = '';
+        let result2 = '';
+        let completed = false;
+
+        service.request(`${baseUrl}/test`).subscribe(response => {
+            result1 = response;
+        });
+
+        service.request(`${baseUrl}/test`).subscribe({
+            next: response => {
+                result2 = response;
+            },
+            complete: () => {
+                completed = true;
+            },
+        });
+
+        cy.get('@request.all')
+            .should('have.length', 1)
+            .then(() => {
+                expect(result1).to.equal(result2);
+                void expect(completed).to.be.true;
+            });
+    });
+});
