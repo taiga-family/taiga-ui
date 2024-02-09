3e2075fa32b424b8f291223d5d1f2ae127cfb45b
waterplea <waterplea@users.noreply.github.com>
2024-02-09 13:21:01 +0000

    deploy: 9f4206ba5c931a7704dd14c5cf157dbc8386f3d5

--- next/41780.6d96f500c0b03cdc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[41780],{41780:n=>{n.exports="```ts\nimport {TuiGroupModule} from '@taiga-ui/core';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiGroupModule,\n  ],\n  // ...\n})\nexport class MyModule {}\n```\n"}}]);
