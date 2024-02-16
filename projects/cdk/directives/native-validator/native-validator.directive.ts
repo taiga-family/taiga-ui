87469e9f943a6dedb9de724bab2d299cce84e60a
Maksim Ivanov <splincodewd@yandex.ru>
2024-02-16 17:48:10 +0300

    fix(cdk): cancel macrotask in `[tuiNativeValidator]` when element destroyed (#6777)

--- projects/cdk/directives/native-validator/native-validator.directive.ts
+++ projects/cdk/directives/native-validator/native-validator.directive.ts
@@ -1,5 +1,8 @@
 import {Directive, ElementRef, HostListener, inject, Input, NgZone} from '@angular/core';
 import {AbstractControl, NG_VALIDATORS, Validator} from '@angular/forms';
+import {tuiZonefree} from '@taiga-ui/cdk/observables';
+import {TuiDestroyService} from '@taiga-ui/cdk/services';
+import {takeUntil, timer} from 'rxjs';
 
 @Directive({
     standalone: true,
@@ -10,11 +13,13 @@ import {AbstractControl, NG_VALIDATORS, Validator} from '@angular/forms';
             multi: true,
             useExisting: TuiNativeValidatorDirective,
         },
+        TuiDestroyService,
     ],
 })
 export class TuiNativeValidatorDirective implements Validator {
-    private readonly host: HTMLInputElement = inject(ElementRef).nativeElement;
     private readonly zone = inject(NgZone);
+    private readonly host: HTMLInputElement = inject(ElementRef).nativeElement;
+    private readonly destroy$ = inject(TuiDestroyService, {self: true});
     private control?: AbstractControl;
 
     @Input()
@@ -29,7 +34,10 @@ export class TuiNativeValidatorDirective implements Validator {
 
     validate(control: AbstractControl): null {
         this.control = control;
-        this.zone.runOutsideAngular(() => setTimeout(() => this.handleValidation()));
+
+        timer(0)
+            .pipe(tuiZonefree(this.zone), takeUntil(this.destroy$))
+            .subscribe(() => this.handleValidation());
 
         return null;
     }
