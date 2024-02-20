0b1c03b012e2b383efb5a3f064ce62a20168fd8e
waterplea <alexander@inkin.ru>
2024-02-20 15:14:48 +0400

    chore: build

--- projects/experimental/components/navigation/index.ts
+++ projects/experimental/components/navigation/index.ts
@@ -2,4 +2,5 @@ export * from './aside.component';
 export * from './head.component';
 export * from './logo.component';
 export * from './main.component';
+export * from './nav.component';
 export * from './navigation.module';
