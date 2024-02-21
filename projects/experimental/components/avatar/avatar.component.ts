b0e233b35360cd13d62e59880ab7c304e1b55bb2
Maksim Ivanov <splincodewd@yandex.ru>
2024-02-21 12:33:57 +0300

    fix(experimental): `Avatar` fix error when using `SafeResourceUrl` (#6845)

--- projects/experimental/components/avatar/avatar.component.ts
+++ projects/experimental/components/avatar/avatar.component.ts
@@ -28,7 +28,7 @@ import {TUI_AVATAR_OPTIONS, TuiAvatarOptions} from './avatar.options';
     host: {
         '[attr.data-size]': 'size',
         '[attr.data-type]': 'type',
-        '[style.--t-mask]': '"url(" + resolver(src) + ")"',
+        '[style.--t-mask]': '"url(" + resolver(src.toString()) + ")"',
         '[class._round]': 'round',
     },
 })
