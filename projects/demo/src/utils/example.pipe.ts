7d56f14dc28048fe73938cd4bbc6d4e32e1a37e4
Vladimir Potekhin <vladimir.potekh@gmail.com>
2024-02-22 17:54:07 +0300

    chore: fix path

--- projects/demo/src/utils/example.pipe.ts
+++ projects/demo/src/utils/example.pipe.ts
@@ -1,11 +1,15 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {TuiDocExample} from '@taiga-ui/addon-doc';
+import {inject, Pipe, PipeTransform} from '@angular/core';
+import {TuiDocExample, TuiDocPageComponent} from '@taiga-ui/addon-doc';
+
+const toKebab: (str: string) => string = str =>
+    str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? '-' : '') + $.toLowerCase());
 
 @Pipe({name: 'tuiExample', standalone: true})
 export class TuiExample implements PipeTransform {
+    docPage = inject(TuiDocPageComponent);
+
     transform(
         index: number,
-        path: string,
         formats:
             | string
             | 'html,less'
@@ -18,7 +22,9 @@ export class TuiExample implements PipeTransform {
                 .split(',')
                 .map(format => [
                     format.toUpperCase(),
-                    import(`../modules/${path}/examples/${index}/index.${format}?raw`),
+                    import(
+                        `../modules/${this.docPage.type}/${toKebab(this.docPage.header)}/examples/${index}/index.${format}?raw`
+                    ),
                 ]),
         );
     }
