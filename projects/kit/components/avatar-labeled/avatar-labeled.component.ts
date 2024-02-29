d9776ecd342184032e25cb048f4dae8d60511734
splincode <omaxphp@yandex.ru>
2024-02-29 17:15:52 +0300

    feat(kit): support `[label]` in `tui-avatar-labeled`

--- projects/kit/components/avatar-labeled/avatar-labeled.component.ts
+++ projects/kit/components/avatar-labeled/avatar-labeled.component.ts
@@ -1,11 +1,40 @@
-import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    HostBinding,
+    Input,
+    ViewEncapsulation,
+} from '@angular/core';
+import {tuiPure} from '@taiga-ui/cdk';
+import {TuiFadeDirective} from '@taiga-ui/kit/directives';
 
 @Component({
     standalone: true,
     selector: 'tui-avatar-labeled',
-    template: '<ng-content></ng-content>',
+    imports: [NgForOf, NgIf, TuiFadeDirective],
+    template: `
+        <ng-content></ng-content>
+        <ng-container *ngIf="label.length">
+            <span
+                *ngFor="let item of split(label)"
+                tuiFade
+            >
+                {{ item }}
+            </span>
+        </ng-container>
+    `,
     styleUrls: ['./avatar-labeled.styles.less'],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TuiAvatarLabeledComponent {}
+export class TuiAvatarLabeledComponent {
+    @Input()
+    @HostBinding('attr.title')
+    public label = '';
+
+    @tuiPure
+    protected split(label: string): readonly string[] {
+        return label.split(' ');
+    }
+}
