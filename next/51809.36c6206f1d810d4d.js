06a4f297ef33b8a1ffb6bc522f18b459430903ec
splincode <splincode@users.noreply.github.com>
2024-02-29 15:34:02 +0000

    deploy: 04f3ad4445fc2f094f4a88bdae197438c9d613e6

---
+++ next/51809.36c6206f1d810d4d.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[51809],{51809:e=>{e.exports='```html\n<label tuiLabel>\n  <input\n    tuiCheckbox\n    type="checkbox"\n    size="s"\n    [(ngModel)]="selected"\n  />\n  Label text\n</label>\n```\n'}}]);
