8ff95927ba16e402d717dd52fe990a764d461e52
Maksim Ivanov <splincodewd@yandex.ru>
2024-03-01 10:15:45 +0300

    feat(kit): support `[label]` in `tui-avatar-labeled` (#6921)

--- projects/experimental/components/avatar-labeled/avatar-labeled.component.ts
+++ projects/experimental/components/avatar-labeled/avatar-labeled.component.ts
@@ -1,10 +1,36 @@
-import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    HostBinding,
+    Input,
+    ViewEncapsulation,
+} from '@angular/core';
+import {tuiPure} from '@taiga-ui/cdk';
 
 @Component({
     selector: 'tui-avatar-labeled',
-    template: '<ng-content></ng-content>',
+    template: `
+        <ng-content></ng-content>
+        <ng-container *ngIf="label.length">
+            <span
+                *ngFor="let item of split(label)"
+                tuiFade
+            >
+                {{ item }}
+            </span>
+        </ng-container>
+    `,
     styleUrls: ['./avatar-labeled.styles.less'],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TuiAvatarLabeledComponent {}
+export class TuiAvatarLabeledComponent {
+    @Input()
+    @HostBinding('attr.title')
+    label = '';
+
+    @tuiPure
+    split(label: string): readonly string[] {
+        return label.split(' ');
+    }
+}
--- projects/experimental/components/avatar-labeled/avatar-labeled.module.ts
+++ projects/experimental/components/avatar-labeled/avatar-labeled.module.ts
@@ -1,8 +1,12 @@
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+// eslint-disable-next-line @taiga-ui/experience/no-deep-imports
+import {TuiFadeModule} from '@taiga-ui/experimental/directives/fade';
 
 import {TuiAvatarLabeledComponent} from './avatar-labeled.component';
 
 @NgModule({
+    imports: [CommonModule, TuiFadeModule],
     declarations: [TuiAvatarLabeledComponent],
     exports: [TuiAvatarLabeledComponent],
 })
