4fe8da938e158027e8fcaecb112e654354d1fc8b
Maksim Ivanov <splincodewd@yandex.ru>
2024-03-05 19:06:22 +0300

    chore(demo): add new e2e test for field error (#6596)

--- projects/demo/src/modules/pipes/field-error/examples/3/index.ts
+++ projects/demo/src/modules/pipes/field-error/examples/3/index.ts
@@ -8,24 +8,16 @@ import {
 } from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiValidationError} from '@taiga-ui/cdk';
+import {tuiMarkControlAsTouchedAndValidate, TuiValidationError} from '@taiga-ui/cdk';
 import {type PolymorpheusContent} from '@tinkoff/ng-polymorpheus';
 
 const secretRegexTen = /^\d{10}$/;
 const secretRegexTwelve = /^\d{12}$/;
 
-export function innValidator(field: AbstractControl): unknown {
-    return field.value &&
-        (secretRegexTen.test(field.value) || secretRegexTwelve.test(field.value))
-        ? null
-        : {
-              inn: new TuiValidationError('Secret number contains 10 or 12 digits'),
-          };
-}
-
 @Component({
     selector: 'tui-field-error-pipe-example-3',
     templateUrl: './index.html',
+    styleUrls: ['./index.less'],
     encapsulation,
     changeDetection,
 })
@@ -41,10 +33,15 @@ export class TuiFieldErrorPipeExample3 implements OnInit {
     protected readonly testForm = new FormGroup({
         testValue1: new FormControl('', [Validators.required, this.getSecretValidator()]),
         testValue2: this.testValue2,
+        testValue3: new FormControl(false, [Validators.requiredTrue]),
     });
 
     public ngOnInit(): void {
         this.testValue2.setValidators([Validators.required, this.companyValidator]);
+    }
+
+    protected onSubmit(): void {
+        tuiMarkControlAsTouchedAndValidate(this.testForm);
     }
 
     private readonly companyValidator = (
