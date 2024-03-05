a037672c99931b5366b322ec398d151adf77e726
splincode <splincode@users.noreply.github.com>
2024-03-05 19:46:48 +0000

    deploy: 5056e9bf84574bd9561a18a785d6c0ac7c968ae3

---
+++ next/21988.55ba2d9876d3b1d2.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[21988],{21988:e=>{e.exports="```html\n<tui-badge-notification>10</tui-badge-notification>\n```\n"}}]);
---
+++ next/53484.28c3aed4113ec4f9.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[53484],{53484:e=>{e.exports="@import '@taiga-ui/core/styles/taiga-ui-local.less';\n\ntui-badge-notification {\n    margin: 0.2rem;\n    .gradient(#c86dd7, #3023ae);\n}\n"}}]);
