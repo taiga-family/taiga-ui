71cda96b02a3863c01481da8f71d8b3d7b6923e2
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-03-07 15:12:20 +0300

    chore(demo): remove `b-form` global class (#6915)
    (cherry picked from commit 63ed8a318337b17c4280f53465a488d33b9b938f)

--- projects/demo/src/modules/components/badge/examples/3/index.ts
+++ projects/demo/src/modules/components/badge/examples/3/index.ts
@@ -2,13 +2,13 @@ import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiPlatformModule} from '@taiga-ui/cdk';
-import {TuiIconModule} from '@taiga-ui/experimental';
+import {TuiIconComponent} from '@taiga-ui/core';
 import {TuiBadgeDirective} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
     selector: 'tui-badge-example-3',
-    imports: [TuiBadgeDirective, TuiPlatformModule, TuiIconModule],
+    imports: [TuiBadgeDirective, TuiPlatformModule, TuiIconComponent],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation,
