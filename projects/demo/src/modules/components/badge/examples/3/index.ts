b2a9ea6e925222188654bf07812dfdffbd4377ad
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-03-11 09:06:35 +0300

    chore(demo): migrated badge example 3 to standalone

--- projects/demo/src/modules/components/badge/examples/3/index.ts
+++ projects/demo/src/modules/components/badge/examples/3/index.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiPlatformModule} from '@taiga-ui/cdk';
+import {TuiIconModule} from '@taiga-ui/experimental';
+import {TuiBadgeDirective} from '@taiga-ui/kit';
 
 @Component({
+    standalone: true,
     selector: 'tui-badge-example-3',
+    imports: [TuiBadgeDirective, TuiPlatformModule, TuiIconModule],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation,
