669adc13d8cd90fff293996936e9a5f0ef2a6493
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-03-11 11:41:38 +0300

    refactor(demo): removed useless TuiIconModule

--- projects/demo/src/modules/components/badge/examples/4/index.ts
+++ projects/demo/src/modules/components/badge/examples/4/index.ts
@@ -1,13 +1,12 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiIconModule} from '@taiga-ui/experimental';
 import {TuiBadgeDirective, TuiFadeDirective} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
     selector: 'tui-badge-example-4',
-    imports: [TuiBadgeDirective, TuiIconModule, TuiFadeDirective],
+    imports: [TuiBadgeDirective, TuiFadeDirective],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation,
