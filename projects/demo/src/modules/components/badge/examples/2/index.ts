e64eab50057f1fd11b5acee10b76eb8d082a69d9
Stanislav Zaycev <102649815+KrollikRoddzer@users.noreply.github.com>
2024-03-11 12:22:19 +0300

    chore(demo): migrate documentation page `Badge` to standalone components (#6969)

--- projects/demo/src/modules/components/badge/examples/2/index.ts
+++ projects/demo/src/modules/components/badge/examples/2/index.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiBadgeDirective} from '@taiga-ui/kit';
 
 @Component({
+    standalone: true,
     selector: 'tui-badge-example-2',
+    imports: [TuiBadgeDirective],
     templateUrl: './index.html',
     encapsulation,
     changeDetection,
--- projects/demo/src/modules/components/badge/examples/6/index.ts
+++ projects/demo/src/modules/components/badge/examples/6/index.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {tuiBadgeOptionsProvider} from '@taiga-ui/kit';
+import {TuiBadgeDirective, tuiBadgeOptionsProvider} from '@taiga-ui/kit';
 
 @Component({
+    standalone: true,
     selector: 'tui-badge-example-6',
+    imports: [TuiBadgeDirective],
     templateUrl: './index.html',
     encapsulation,
     changeDetection,
