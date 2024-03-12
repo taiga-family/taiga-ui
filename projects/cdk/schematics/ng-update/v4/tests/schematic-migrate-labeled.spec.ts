9bdd5daa9b72fb6aecd99df123031503ea5c5365
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-03-12 17:02:11 +0300

    chore: `tui-checkbox-labeled`, `tui-radio-labeled` add migration (#6983)

---
+++ projects/cdk/schematics/ng-update/v4/tests/schematic-migrate-labeled.spec.ts
@@ -0,0 +1,103 @@
+import {HostTree} from '@angular-devkit/schematics';
+import {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
+import type {TuiSchema} from '@taiga-ui/cdk/schematics/ng-add/schema';
+import {
+    createProject,
+    createSourceFile,
+    resetActiveProject,
+    saveActiveProject,
+    setActiveProject,
+} from 'ng-morph';
+import {join} from 'path';
+
+const collectionPath = join(__dirname, '../../../migration.json');
+
+const COMPONENT_BEFORE = `
+import { TuiCheckboxLabeledModule } from "@taiga-ui/kit";
+
+@Component({
+    standalone: true,
+    templateUrl: './test.template.html',
+    imports: [TuiCheckboxLabeledModule]
+})
+export class TestComponent {
+}`;
+
+const COMPONENT_AFTER = `import { TuiCheckboxComponent, TuiRadioComponent } from "@taiga-ui/kit";
+import { TuiLabelComponent } from "@taiga-ui/core";
+
+@Component({
+    standalone: true,
+    templateUrl: './test.template.html',
+    imports: [TuiLabelComponent, TuiCheckboxComponent, TuiRadioComponent]
+})
+export class TestComponent {
+}`;
+
+const TEMPLATE_BEFORE = `
+
+<tui-checkbox-labeled [(ngModel)]="value">
+  Label
+</tui-checkbox-labeled>
+
+<tui-radio-labeled size="l" [formControl]="control" [item]="value" [identityMatcher]="matcher">
+  Label
+</tui-radio-labeled>
+`;
+
+const TEMPLATE_AFTER = `
+<label tuiLabel>
+<input tuiCheckbox type="checkbox" [(ngModel)]="value">
+  Label
+</label>
+
+<label tuiLabel>
+<input tuiRadio type="radio" size="m" [formControl]="control" [value]="value" [identityMatcher]="matcher">
+  Label
+</label>
+
+`;
+
+describe('ng-update', () => {
+    let host: UnitTestTree;
+    let runner: SchematicTestRunner;
+
+    beforeEach(() => {
+        host = new UnitTestTree(new HostTree());
+        runner = new SchematicTestRunner('schematics', collectionPath);
+
+        setActiveProject(createProject(host));
+
+        createMainFiles();
+
+        saveActiveProject();
+    });
+
+    it('should migrate labeled in template', async () => {
+        const tree = await runner.runSchematic(
+            'updateToV4',
+            {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
+            host,
+        );
+
+        expect(tree.readContent('test/app/test.template.html')).toEqual(TEMPLATE_AFTER);
+    });
+
+    it('should migrate labeled references in ts files', async () => {
+        const tree = await runner.runSchematic(
+            'updateToV4',
+            {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
+            host,
+        );
+
+        expect(tree.readContent('test/app/test.component.ts')).toEqual(COMPONENT_AFTER);
+    });
+
+    afterEach(() => resetActiveProject());
+});
+
+function createMainFiles(): void {
+    createSourceFile('test/app/test.component.ts', COMPONENT_BEFORE);
+
+    createSourceFile('test/app/test.template.html', TEMPLATE_BEFORE);
+}
