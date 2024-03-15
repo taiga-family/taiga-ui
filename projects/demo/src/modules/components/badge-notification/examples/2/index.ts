288fab7402478ddf375dac0acb30314b2edbd031
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-03-15 15:04:33 +0300

    refactor(demo): removed useless import

--- projects/demo/src/modules/components/badge-notification/examples/2/index.ts
+++ projects/demo/src/modules/components/badge-notification/examples/2/index.ts
@@ -1,13 +1,12 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiPlatformModule} from '@taiga-ui/cdk';
 import {TuiBadgeNotificationComponent} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
     selector: 'tui-badge-notification-example-2',
-    imports: [TuiPlatformModule, TuiBadgeNotificationComponent],
+    imports: [TuiBadgeNotificationComponent],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation,
