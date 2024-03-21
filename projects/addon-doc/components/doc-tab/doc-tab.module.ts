fb0793b1ae4d2ea0a2e3dc1e35cd5de409f50c69
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-03-21 16:06:25 +0300

    chore(addon-doc): remove redundant `TuiIconComponent` import

--- projects/addon-doc/components/doc-tab/doc-tab.module.ts
+++ projects/addon-doc/components/doc-tab/doc-tab.module.ts
@@ -1,10 +1,8 @@
 import {NgModule} from '@angular/core';
-import {TuiIconComponent} from '@taiga-ui/core';
 
 import {TuiDocTabComponent} from './doc-tab.component';
 
 @NgModule({
-    imports: [TuiIconComponent],
     declarations: [TuiDocTabComponent],
     exports: [TuiDocTabComponent],
 })
