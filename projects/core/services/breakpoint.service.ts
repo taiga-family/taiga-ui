47d53f954f0d759b6fb605e95d01e17e4722eb4a
splincode <omaxphp@yandex.ru>
2024-03-24 11:06:22 +0300

    feat(core): add breakpoint interceptor

--- projects/core/services/breakpoint.service.ts
+++ projects/core/services/breakpoint.service.ts
@@ -1,10 +1,14 @@
 import {inject, Injectable, NgZone} from '@angular/core';
-import {TUI_WINDOW_SIZE, tuiZoneOptimized} from '@taiga-ui/cdk';
+import type {TuiHandler} from '@taiga-ui/cdk';
+import {TUI_WINDOW_SIZE, tuiCreateToken, tuiZoneOptimized} from '@taiga-ui/cdk';
 import type {TuiMedia} from '@taiga-ui/core/interfaces';
 import {TUI_MEDIA} from '@taiga-ui/core/tokens';
-import {distinctUntilChanged, map, Observable, shareReplay} from 'rxjs';
+import {distinctUntilChanged, identity, map, Observable, shareReplay} from 'rxjs';
 
 export type TuiBreakpointMediaKey = keyof Omit<TuiMedia, 'tablet'>;
+
+export const TUI_BREAKPOINT_INTERCEPTOR =
+    tuiCreateToken<TuiHandler<TuiBreakpointMediaKey, TuiBreakpointMediaKey>>(identity);
 
 /**
  * Service to provide the current breakpoint based on Taiga UI's media queries
@@ -13,6 +17,7 @@ export type TuiBreakpointMediaKey = keyof Omit<TuiMedia, 'tablet'>;
     providedIn: 'root',
 })
 export class TuiBreakpointService extends Observable<TuiBreakpointMediaKey | null> {
+    private readonly mapper = inject(TUI_BREAKPOINT_INTERCEPTOR);
     private readonly media = inject(TUI_MEDIA);
     private readonly sorted: number[] = Object.values(this.media).sort((a, b) => a - b);
     private readonly invert: Record<number, TuiBreakpointMediaKey> = Object.keys(
@@ -28,6 +33,7 @@ export class TuiBreakpointService extends Observable<TuiBreakpointMediaKey | nul
     private readonly stream$ = inject(TUI_WINDOW_SIZE).pipe(
         map(({width}) => this.sorted.find(size => size > width)),
         map(key => this.invert[key || this.sorted[this.sorted.length - 1]]),
+        map(size => this.mapper(size)),
         distinctUntilChanged(),
         tuiZoneOptimized(inject(NgZone)),
         shareReplay({bufferSize: 1, refCount: true}),
