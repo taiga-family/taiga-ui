24e793dbaf1c6f7394aa872519a1b03f664d6954
waterplea <alexander@inkin.ru>
2024-03-29 07:11:58 +0000

    Update snapshots/demo/next/main to output generated at 6ebcb12

--- 4530.399993d09c0b3c9f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[4530],{4530:n=>{n.exports="```ts\nimport {NgModule} from '@angular/core';\nimport {TuiRadioComponent} from '@taiga-ui/experimental';\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiRadioComponent,\n  ],\n})\nexport class MyModule {}\n```\n"}}]);
--- 51118.dc909621bd100b89.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[51118],{51118:t=>{t.exports="@import '@taiga-ui/core/styles/taiga-ui-local';\n\n.description {\n    font: var(--tui-font-text-s);\n    margin-top: @space;\n    color: var(--tui-text-03);\n}\n"}}]);
