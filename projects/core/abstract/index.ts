8288e9b1de244fcc46d380fb8003b5d39d7ef510
waterplea <alexander@inkin.ru>
2024-04-02 16:58:43 +0700

    chore: refactor accessors

--- projects/core/abstract/index.ts
+++ projects/core/abstract/index.ts
@@ -1,6 +1,5 @@
 export * from './abstract-driver.directive';
 export * from './abstract-textfield-host';
+export * from './accessors';
 export * from './driver';
-export * from './position-accessor';
-export * from './rect-accessor';
 export * from './vehicle';
