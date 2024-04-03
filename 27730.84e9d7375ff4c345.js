e5691375f425c603513c690ffa76fb3b222fcb7b
splincode <omaxphp@yandex.ru>
2024-04-03 14:27:05 +0000

    Update snapshots/demo/next/main to output generated at 6acc07c

---
+++ 27730.84e9d7375ff4c345.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[27730],{27730:o=>{o.exports="```ts\n// ...\nimport {TuiRootModule} from '@taiga-ui/core';\n\nbootstrapApplication(AppComponent, {\n  providers: [\n    provideAnimations(),\n    importProvidersFrom(\n      TuiRootModule,\n      // ...\n    ),\n  ],\n}).catch(err => console.error(err));\n```\n"}}]);
