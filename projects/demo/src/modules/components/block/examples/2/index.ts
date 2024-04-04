c34d4950dc1d3801b51d814b395920c623ebd2e8
German Panov <german.panov.2018@mail.ru>
2024-04-04 12:28:05 +0300

    chore(demo): migrate documentation page `Block` to standalone components (#7165)

--- projects/demo/src/modules/components/block/examples/2/index.ts
+++ projects/demo/src/modules/components/block/examples/2/index.ts
@@ -1,16 +1,25 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiGroupDirective} from '@taiga-ui/core';
+import {TuiBlockDirective, TuiFadeDirective, TuiRadioComponent} from '@taiga-ui/kit';
 
 @Component({
-    selector: 'tui-block-example-2',
+    standalone: true,
+    imports: [
+        ReactiveFormsModule,
+        TuiGroupDirective,
+        TuiBlockDirective,
+        TuiRadioComponent,
+        TuiFadeDirective,
+    ],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation,
     changeDetection,
 })
-export class TuiBlockExample2 {
+export default class ExampleComponent {
     protected readonly testForm = new FormGroup({
         testValue: new FormControl('orange'),
     });
--- projects/demo/src/modules/components/block/examples/3/index.ts
+++ projects/demo/src/modules/components/block/examples/3/index.ts
@@ -1,16 +1,33 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiIconComponent, TuiTitleDirective, TuiTooltipModule} from '@taiga-ui/core';
+import {
+    TuiAvatarComponent,
+    TuiBlockDirective,
+    TuiCheckboxComponent,
+    TuiSwitchComponent,
+} from '@taiga-ui/kit';
 
 @Component({
-    selector: 'tui-block-example-3',
+    standalone: true,
+    imports: [
+        ReactiveFormsModule,
+        TuiAvatarComponent,
+        TuiTooltipModule,
+        TuiIconComponent,
+        TuiBlockDirective,
+        TuiCheckboxComponent,
+        TuiTitleDirective,
+        TuiSwitchComponent,
+    ],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation,
     changeDetection,
 })
-export class TuiBlockExample3 {
+export default class ExampleComponent {
     protected readonly testForm = new FormGroup({
         testValue1: new FormControl(false),
         testValue2: new FormControl(false),
