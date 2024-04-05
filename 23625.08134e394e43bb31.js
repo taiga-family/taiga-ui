8a491f3412a5679b8d46e32c02c9a039e2442c9a
waterplea <alexander@inkin.ru>
2024-04-05 10:40:57 +0000

    Update snapshots/demo/next/main to output generated at dce51b2

---
+++ 23625.08134e394e43bb31.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[23625],{23625:n=>{n.exports="```ts\nimport {TuiLinkDirective} from '@taiga-ui/core';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiLinkDirective,\n  ],\n  // ...\n})\nexport class MyModule {}\n```\n"}}]);
