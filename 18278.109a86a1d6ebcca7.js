fe7de52cf5d392670695bf9942ffa479a9d58ab9
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-04-05 06:01:51 +0000

    Update snapshots/demo/next/main to output generated at 11a4b8e

--- 18278.109a86a1d6ebcca7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[18278],{53141:n=>{n.exports="```ts\nimport {TuiThumbnailCardModule} from '@taiga-ui/experimental';\n\n// ...\n\n@NgModule({\n  imports: [\n    // ...\n    TuiThumbnailCardModule,\n  ],\n  // ...\n})\nexport class MyModule {}\n```\n"}}]);
