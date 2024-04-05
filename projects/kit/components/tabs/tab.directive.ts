e510283f6a008e030af6af8b7a69430567881654
waterplea <alexander@inkin.ru>
2024-04-05 17:44:30 +0700

    refactor(kit): `Tabs` use `Icons`

--- projects/kit/components/tabs/tab.directive.ts
+++ projects/kit/components/tabs/tab.directive.ts
@@ -4,6 +4,7 @@ import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {RouterLinkActive} from '@angular/router';
 import {MutationObserverService} from '@ng-web-apis/mutation-observer';
 import {tuiIsNativeFocused, tuiTypedFromEvent} from '@taiga-ui/cdk';
+import {TuiIconsDirective} from '@taiga-ui/core';
 import {EMPTY, filter, merge} from 'rxjs';
 
 export const TUI_TAB_ACTIVATE = 'tui-tab-activate';
@@ -12,6 +13,12 @@ export const TUI_TAB_ACTIVATE = 'tui-tab-activate';
     standalone: true,
     selector:
         'a[tuiTab]:not([routerLink]), a[tuiTab][routerLink][routerLinkActive], button[tuiTab]',
+    hostDirectives: [
+        {
+            directive: TuiIconsDirective,
+            inputs: ['iconLeft', 'iconRight'],
+        },
+    ],
     host: {
         type: 'button',
     },
