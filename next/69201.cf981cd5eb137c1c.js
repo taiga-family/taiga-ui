1388cd9db767c369331830b3e4611ebaa9b068d2
splincode <splincode@users.noreply.github.com>
2024-04-09 07:04:59 +0000

    deploy: e1a679dec50128b320ab890cc992a660b34aa4ca

--- next/69201.cf981cd5eb137c1c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[69201],{69201:e=>{e.exports=":host {\n    display: flex;\n    flex-direction: column;\n    align-items: flex-start;\n    gap: 1rem;\n}\n"}}]);
---
+++ next/91722.0417aa64b4f6c29e.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[91722],{91722:t=>{t.exports='<tui-segmented>\n    <button>Dynamic width</button>\n    <button>Items</button>\n</tui-segmented>\n\n<tui-segmented class="full">\n    <button>Fixed width</button>\n    <button tuiFade>with fadeout effect</button>\n</tui-segmented>\n'}}]);
