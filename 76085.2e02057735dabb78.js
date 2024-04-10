14fcdf734a396c0d8e288fb516bf1abae929411a
splincode <omaxphp@yandex.ru>
2024-04-10 13:59:10 +0000

    Update snapshots/demo/next/main to output generated at 759a561

---
+++ 76085.2e02057735dabb78.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[76085],{76085:n=>{n.exports='```html\n<tui-breadcrumbs [size]="size">\n  <ng-container *ngFor="let item of items">\n    <a\n      *tuiItem\n      tuiLink\n      [routerLink]="item.routerLink"\n    >\n      {{ item.caption }}\n    </a>\n  </ng-container>\n</tui-breadcrumbs>\n```\n'}}]);
