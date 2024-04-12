7ae542542105c53b409b019a55d907a8e7cca674
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-04-12 13:09:17 +0300

    chore(demo): chagned b-demo-steps in hint describe to tui-setup

--- projects/demo/src/modules/directives/hint-describe/hint-describe.module.ts
+++ projects/demo/src/modules/directives/hint-describe/hint-describe.module.ts
@@ -2,6 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
+import {TuiSetupComponent} from '@demo/utils';
 import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
 import {TuiButtonDirective, TuiHintModule, TuiTooltipModule} from '@taiga-ui/core';
 import {TuiInputModule} from '@taiga-ui/kit';
@@ -23,6 +24,7 @@ import {ExampleTuiHintDescribeComponent} from './hint-describe.component';
         InheritedDocumentationModule,
         TuiAddonDocModule,
         RouterModule.forChild(tuiGenerateRoutes(ExampleTuiHintDescribeComponent)),
+        TuiSetupComponent,
     ],
     declarations: [ExampleTuiHintDescribeComponent, TuiHintDescribeExample1],
     exports: [ExampleTuiHintDescribeComponent],
