f06808055086c3670a162bb4edba7e28c02f6663
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-04-12 11:23:08 +0300

    chore(demo): chagned b-demo-steps in expand to tui-setup

--- projects/demo/src/modules/components/expand/expand.module.ts
+++ projects/demo/src/modules/components/expand/expand.module.ts
@@ -2,6 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
+import {TuiSetupComponent} from '@demo/utils';
 import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
 import {TuiButtonDirective, TuiExpandModule} from '@taiga-ui/core';
 
@@ -17,6 +18,7 @@ import {ExampleTuiExpandComponent} from './expand.component';
         ReactiveFormsModule,
         TuiAddonDocModule,
         RouterModule.forChild(tuiGenerateRoutes(ExampleTuiExpandComponent)),
+        TuiSetupComponent,
     ],
     declarations: [ExampleTuiExpandComponent, TuiExpandExample1],
     exports: [ExampleTuiExpandComponent],
