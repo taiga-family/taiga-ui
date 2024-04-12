c5fefc4a7361e420a16c17e80c640ec9ea2d9664
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-04-12 12:01:25 +0300

    chore(demo): chagned b-demo-steps in element to tui-setup

--- projects/demo/src/modules/directives/element/element.module.ts
+++ projects/demo/src/modules/directives/element/element.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {TuiSetupComponent} from '@demo/utils';
 import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
 import {TuiElementModule} from '@taiga-ui/cdk';
 import {TuiButtonDirective} from '@taiga-ui/core';
@@ -15,6 +16,7 @@ import {TuiElementExample1} from './examples/1';
         TuiElementModule,
         TuiAddonDocModule,
         RouterModule.forChild(tuiGenerateRoutes(ExampleTuiElementComponent)),
+        TuiSetupComponent,
     ],
     declarations: [ExampleTuiElementComponent, TuiElementExample1],
     exports: [ExampleTuiElementComponent],
