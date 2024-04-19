7577e53e09f3e615fc6125a1ab028ce3e6e0048c
waterplea <alexander@inkin.ru>
2024-04-19 16:36:53 +0300

    feat(addon-mobile): `DropdownMobile` add sheet view
    Signed-off-by: waterplea <alexander@inkin.ru>

--- projects/addon-mobile/directives/dropdown-mobile/dropdown-mobile.component.ts
+++ projects/addon-mobile/directives/dropdown-mobile/dropdown-mobile.component.ts
@@ -1,5 +1,6 @@
 import {DOCUMENT} from '@angular/common';
 import {
+    AfterViewInit,
     ChangeDetectionStrategy,
     Component,
     ElementRef,
@@ -7,8 +8,9 @@ import {
     OnDestroy,
     ViewEncapsulation,
 } from '@angular/core';
+import {TuiDropdownMobileDirective} from '@taiga-ui/addon-mobile/directives';
 import {TuiKeyboardService} from '@taiga-ui/addon-mobile/services';
-import {tuiGetNativeFocused, tuiPx} from '@taiga-ui/cdk';
+import {TuiActiveZoneDirective, tuiGetNativeFocused, tuiPx} from '@taiga-ui/cdk';
 import {
     TUI_ANIMATIONS_DURATION,
     TuiDropdownDirective,
@@ -24,18 +26,22 @@ const GAP = 16;
     styleUrls: ['./dropdown-mobile.style.less'],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [TuiActiveZoneDirective],
     animations: [tuiSlideInTop, tuiFadeIn],
     host: {
         '[@tuiFadeIn]': 'animation',
         '[@tuiSlideInTop]': 'animation',
+        '[class._sheet]': 'directive.tuiDropdownMobile',
         '(mousedown.silent.prevent)': '0',
         '(document:click.silent.capture)': 'onClick($event)',
         '(window>scroll.silent.capture)': 'refresh($event.currentTarget.visualViewport)',
         '(visualViewport>resize.silent)': 'refresh($event.target)',
         '(visualViewport>scroll.silent)': 'refresh($event.target)',
+        '(scroll.silent)':
+            'close(!el.nativeElement.scrollTop && !el.nativeElement.matches(":active"))',
     },
 })
-export class TuiDropdownMobileComponent implements OnDestroy {
+export class TuiDropdownMobileComponent implements OnDestroy, AfterViewInit {
     private readonly scrollTop = this.doc.documentElement.scrollTop;
     private readonly observer = new ResizeObserver(() =>
         this.refresh(this.doc.defaultView!.visualViewport),
@@ -50,15 +56,15 @@ export class TuiDropdownMobileComponent implements OnDestroy {
     } as const;
 
     constructor(
+        @Inject(TuiActiveZoneDirective) _: any,
         @Inject(TuiKeyboardService) private readonly keyboard: TuiKeyboardService,
         @Inject(DOCUMENT) private readonly doc: Document,
-        @Inject(ElementRef) private readonly el: ElementRef<HTMLElement>,
         @Inject(TUI_ANIMATIONS_DURATION) private readonly duration: number,
-        @Inject(TuiDropdownDirective) readonly directive: TuiDropdownDirective,
-    ) {
-        this.observer.observe(this.directive.el.nativeElement);
-        this.doc.documentElement.style.setProperty('scroll-behavior', 'initial');
-    }
+        @Inject(ElementRef) readonly el: ElementRef<HTMLElement>,
+        @Inject(TuiDropdownDirective) readonly dropdown: TuiDropdownDirective,
+        @Inject(TuiDropdownMobileDirective)
+        readonly directive: TuiDropdownMobileDirective,
+    ) {}
 
     onClick(event: MouseEvent): void {
         if (!this.el.nativeElement?.contains(event.target as Node)) {
@@ -69,21 +75,39 @@ export class TuiDropdownMobileComponent implements OnDestroy {
     refresh({offsetTop, height}: VisualViewport): void {
         this.doc.body.style.removeProperty('--t-root-top');
 
-        if (!this.focused) {
+        if (
+            !this.focused ||
+            this.directive.tuiDropdownMobile ||
+            !this.doc.documentElement.style.getPropertyValue('scroll-behavior')
+        ) {
             return;
         }
 
-        const rect = this.directive.el.nativeElement.getBoundingClientRect();
+        const rect = this.dropdown.el.nativeElement.getBoundingClientRect();
         const offset = rect.height + GAP * 2;
 
         this.el.nativeElement.style.setProperty('top', tuiPx(offsetTop + offset));
         this.el.nativeElement.style.setProperty('height', tuiPx(height - offset));
         this.doc.body.classList.add('t-dropdown-mobile');
+        this.doc.documentElement.scrollTop = 0;
         this.doc.body.style.setProperty(
             '--t-root-top',
             tuiPx(offsetTop + GAP - rect.top),
         );
-        this.doc.documentElement.scrollTop = 0;
+    }
+
+    close(close: boolean): void {
+        if (close && this.directive.tuiDropdownMobile) {
+            this.dropdown.toggle(false);
+        }
+    }
+
+    ngAfterViewInit(): void {
+        this.observer.observe(this.dropdown.el.nativeElement);
+        this.doc.documentElement.style.setProperty('scroll-behavior', 'initial');
+        this.el.nativeElement.scrollTop = this.directive.tuiDropdownMobile
+            ? this.el.nativeElement.clientHeight
+            : 0;
     }
 
     ngOnDestroy(): void {
@@ -93,14 +117,12 @@ export class TuiDropdownMobileComponent implements OnDestroy {
         this.doc.documentElement.scrollTop = this.scrollTop;
         this.doc.documentElement.style.removeProperty('scroll-behavior');
 
-        if (!this.focused) {
-            return;
+        if (this.focused) {
+            this.keyboard.hide();
         }
-
-        this.keyboard.hide();
     }
 
     private get focused(): boolean {
-        return this.directive.el.nativeElement.contains(tuiGetNativeFocused(this.doc));
+        return this.dropdown.el.nativeElement.contains(tuiGetNativeFocused(this.doc));
     }
 }
