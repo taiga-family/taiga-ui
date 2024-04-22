b9183e69b41a38a94f79135264825ad842d95468
Maksim Ivanov <splincodewd@yandex.ru>
2024-04-22 12:41:45 +0300

    refactor: drop `cypress` route (#7265)

--- projects/demo/src/modules/cypress/cypress.component.ts
+++
@@ -1,8 +0,0 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-
-@Component({
-    selector: 'cypress-doc-page',
-    templateUrl: './cypress.template.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class CypressDocPageComponent {}
