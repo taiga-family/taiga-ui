ee81a181f57d3886de25dfdfcffdd3c892aabdca
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-04-22 09:39:27 +0300

    chore(demo): changed b-demo-steps in stringify-content pipe

--- projects/demo/src/modules/pipes/stringify-content/stringify-content.module.ts
+++ projects/demo/src/modules/pipes/stringify-content/stringify-content.module.ts
@@ -2,6 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
+import {TuiSetupComponent} from '@demo/utils';
 import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
 import {TuiDataListModule, TuiLinkDirective, TuiNotificationModule} from '@taiga-ui/core';
 import {
@@ -27,6 +28,7 @@ import {ExampleTuiStringifyContentComponent} from './stringify-content.component
         CommonModule,
         TuiAddonDocModule,
         RouterModule.forChild(tuiGenerateRoutes(ExampleTuiStringifyContentComponent)),
+        TuiSetupComponent,
     ],
     declarations: [ExampleTuiStringifyContentComponent, TuiStringifyContentExample1],
     exports: [ExampleTuiStringifyContentComponent],
