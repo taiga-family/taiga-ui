1d84dc64d007c90cb71521795f532e96cf80ef61
Svyatoslav Zaytsev <sviatsv@yandex.ru>
2024-04-30 18:38:29 +0500

    fix(core): fix loading defaultErrorMessage for tui-error (#7303)

--- projects/core/components/error/error.module.ts
+++ projects/core/components/error/error.module.ts
@@ -1,11 +1,12 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {TuiLetModule} from '@taiga-ui/cdk';
 import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
 
 import {TuiErrorComponent} from './error.component';
 
 @NgModule({
-    imports: [CommonModule, PolymorpheusModule],
+    imports: [CommonModule, PolymorpheusModule, TuiLetModule],
     declarations: [TuiErrorComponent],
     exports: [TuiErrorComponent],
 })
