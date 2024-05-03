412811a6b82491910f8c8024bf4b8e45a648bfd4
Maksim Ivanov <omaxphp@yandex.ru>
2024-05-03 17:06:10 +0300

    refactor: drop `TUI_PHONE_MASK` (#7330)

--- projects/kit/constants/index.ts
+++ projects/kit/constants/index.ts
@@ -1,7 +1,6 @@
 export * from './date-mode-maskito-adapter';
 export * from './date-time-separator';
 export * from './mask-after-code-regexp';
-export * from './masks';
 export * from './math';
 export * from './max-day-range-length-mapper';
 export * from './max-time-values';
