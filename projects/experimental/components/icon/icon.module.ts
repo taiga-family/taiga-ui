f95bb7fb77fb5f61c56ed237a4fdc44277f2ddb4
Maksim Ivanov <omaxphp@yandex.ru>
2024-05-06 18:27:11 +0300

    feat(core): add `tuiIcon` pipe (#7368)

--- projects/experimental/components/icon/icon.module.ts
+++ projects/experimental/components/icon/icon.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 
 import {TuiIconComponent} from './icon.component';
+import {TuiIconPipe} from './icon.pipe';
 
 @NgModule({
-    declarations: [TuiIconComponent],
-    exports: [TuiIconComponent],
+    declarations: [TuiIconComponent, TuiIconPipe],
+    exports: [TuiIconComponent, TuiIconPipe],
 })
 export class TuiIconModule {}
