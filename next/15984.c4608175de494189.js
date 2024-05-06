8a802e461a705d3c46da91f9ad0bdacdf2b6766c
splincode <splincode@users.noreply.github.com>
2024-05-06 15:47:57 +0000

    deploy: eb1dad6e63bec61ce73908850a2e999c730e8fe5

---
+++ next/15984.c4608175de494189.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[15984],{15984:e=>{e.exports=".poster {\n    height: 4rem;\n    display: flex;\n    align-items: center;\n    border: 5px solid var(--tui-base-03);\n    text-align: center;\n}\n"}}]);
--- next/83165.df2ded7f02dd7f44.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[83165],{83165:e=>{e.exports="li {\n    height: 2rem;\n    cursor: pointer;\n}\n"}}]);
