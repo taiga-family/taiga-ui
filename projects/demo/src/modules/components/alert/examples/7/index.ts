c44b374f438092f38412b64c835486cbff3d4d49
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-05-06 17:05:46 +0300

    chore(demo): add example `Concurrency limit` to documentation page `Alert` (#7361)

---
+++ projects/demo/src/modules/components/alert/examples/7/index.ts
@@ -0,0 +1,36 @@
+import {Component, inject} from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiAlertService, TuiButtonDirective} from '@taiga-ui/core';
+import type {Observable} from 'rxjs';
+import {mergeAll, Subject} from 'rxjs';
+
+const MAX_CONCURRENT = 3;
+
+@Component({
+    standalone: true,
+    imports: [TuiButtonDirective],
+    templateUrl: './index.html',
+    encapsulation,
+    changeDetection,
+})
+export default class ExampleComponent {
+    private readonly alerts = inject(TuiAlertService);
+    private readonly queue$ = new Subject<Observable<unknown>>();
+    private i = 0;
+
+    constructor() {
+        this.queue$.pipe(mergeAll(MAX_CONCURRENT), takeUntilDestroyed()).subscribe();
+    }
+
+    protected showNotification(): void {
+        this.queue$.next(
+            this.alerts.open(
+                `It is impossible to show more than ${MAX_CONCURRENT} alerts concurrently!<br/>` +
+                    `<strong>Index: ${this.i++}</strong>`,
+                {label: 'Use power of RxJS!'},
+            ),
+        );
+    }
+}
