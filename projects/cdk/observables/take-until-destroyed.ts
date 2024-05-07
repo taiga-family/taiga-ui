4d3dac895d9c03700487ea05dcf4cf43135f4b96
waterplea <alexander@inkin.ru>
2024-05-07 15:28:28 +0300

    feat(cdk): add `tuiTakeUntilDestroyed` helper
    Signed-off-by: waterplea <alexander@inkin.ru>

---
+++ projects/cdk/observables/take-until-destroyed.ts
@@ -0,0 +1,22 @@
+import type {DestroyRef} from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import type {MonoTypeOperatorFunction} from 'rxjs';
+import {catchError, defaultIfEmpty, EMPTY, NEVER, pipe, takeUntil} from 'rxjs';
+
+// NOTE: takeUntilDestroyed and DestroyRef can cause error:
+// NG0911: View has already been destroyed
+// https://github.com/angular/angular/issues/54527
+export function tuiTakeUntilDestroyed<T>(
+    destroyRef?: DestroyRef,
+): MonoTypeOperatorFunction<T> {
+    return pipe(
+        takeUntil(
+            NEVER.pipe(
+                // eslint-disable-next-line rxjs/no-unsafe-takeuntil
+                takeUntilDestroyed(destroyRef),
+                catchError(() => EMPTY),
+                defaultIfEmpty(null),
+            ),
+        ),
+    );
+}
