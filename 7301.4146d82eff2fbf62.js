665f2f1bf4c9210968b18b19c1c54264818e744b
splincode <omaxphp@yandex.ru>
2024-05-10 16:19:22 +0000

    Update snapshots/demo/next/main to output generated at 318026f

---
+++ 7301.4146d82eff2fbf62.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[7301],{7301:e=>{e.exports='```html\n<tui-mobile-calendar\n  [min]="min"\n  [max]="max"\n  [single]="false"\n  [disabledItemHandler]="disabledItemHandler"\n></tui-mobile-calendar>\n```\n'}}]);
