1d6da72f25101cd06f4fef728b7ed9bafc3c4654
waterplea <alexander@inkin.ru>
2024-05-13 13:40:42 +0100

    chore: fix test
    Signed-off-by: waterplea <alexander@inkin.ru>

--- projects/core/components/textfield/index.ts
+++ projects/core/components/textfield/index.ts
@@ -1,5 +1,7 @@
-export * from './select.directive';
 export * from './textfield.component';
 export * from './textfield.directive';
 export * from './textfield.module';
 export * from './textfield.options';
+
+// Keeping it manually at the bottom for proper build order
+export * from './select.directive';
--- projects/core/components/textfield/textfield.module.ts
+++ projects/core/components/textfield/textfield.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {TuiLabelDirective} from '@taiga-ui/core/components/label';
 
+// Keeping it manually at the bottom for proper build order
 import {TuiSelectDirective} from './select.directive';
 import {TuiTextfieldComponent} from './textfield.component';
 import {TuiTextfieldDirective} from './textfield.directive';
