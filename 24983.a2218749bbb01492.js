99513ff524573600d246078fbab67c6c8fbf9a61
splincode <omaxphp@yandex.ru>
2024-05-15 07:58:44 +0000

    Update snapshots/demo/next/main to output generated at 098f3ea

---
+++ 24983.a2218749bbb01492.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[24983],{24983:a=>{a.exports='<tui-axes\n    class="axes"\n    [axisXLabels]="labelsX"\n    [axisYLabels]="labelsY"\n>\n    <tui-bar-chart\n        [max]="10000"\n        [value]="value"\n    />\n</tui-axes>\n'}}]);
---
+++ 64038.3ba8333ad09da3cd.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[64038],{64038:n=>{n.exports="```ts\nimport {TuiBarChartComponent} from '@taiga-ui/addon-charts';\n\n//...\n\n@Component({\n  standalone: true,\n  imports: [\n    //  ...\n    TuiBarChartComponent,\n  ],\n  //...\n})\nexport class MyComponent {}\n```\n"}}]);
