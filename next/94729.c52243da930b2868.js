a2ae705aa2f3cc05fe1e8e10ac2a012d62b70dc6
splincode <splincode@users.noreply.github.com>
2024-05-15 12:14:42 +0000

    deploy: 5d4e0cc455e85ddb58ad619447dff86b1adcbab8

---
+++ next/94729.c52243da930b2868.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[94729],{94729:e=>{e.exports="@import '@taiga-ui/core/styles/taiga-ui-local';\n\n.scroll {\n    .scrollbar-hidden();\n    min-height: 64px;\n    max-height: 100%;\n}\n"}}]);
