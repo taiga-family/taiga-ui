ab07922b30ebd26c7c4db7ffc422b41b37972850
splincode <omaxphp@yandex.ru>
2024-05-17 10:19:53 +0000

    Update snapshots/demo/next/main to output generated at b4e8e89

--- 44170.3c4b3bd5cb1a247f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[44170],{44170:n=>{n.exports="```ts\nimport {TuiTabBarModule} from '@taiga-ui/addon-mobile';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiTabBarModule,\n  ],\n  // ...\n})\nexport class MyComponent {}\n```\n"}}]);
