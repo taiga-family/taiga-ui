fea8ab707e7b805f857df6ff6e81ce45acfe4af2
splincode <omaxphp@yandex.ru>
2024-05-19 20:18:29 +0300

    chore: update

--- projects/addon-doc/utils/parse-code-block.ts
+++ projects/addon-doc/utils/parse-code-block.ts
@@ -1,8 +1,7 @@
 import MarkdownIt from 'markdown-it';
-import type Token from 'markdown-it/lib/token';
 
 export function tuiTryParseMarkdownCodeBlock(text = ''): string[] {
-    const tokens: Token[] = new MarkdownIt().parse(text, {});
+    const tokens: MarkdownIt.Token[] = new MarkdownIt().parse(text, {});
     const result = tokens
         .filter(({tag, type}) => tag === 'code' && type === 'fence')
         .map(({content}) => content.trim());
