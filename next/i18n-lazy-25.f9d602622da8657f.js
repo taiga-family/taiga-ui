24db0af8fc6bcfbc47d43a025119db200de35c6b
nsbarsukov <nsbarsukov@users.noreply.github.com>
2024-05-20 16:30:21 +0000

    deploy: c55dfbf2f361c8a397fea6cce74edf22ff26ad83

--- next/i18n-lazy-25.f9d602622da8657f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[59173],{39170:(e,E,c)=>{c.r(E),c.d(E,{TUI_CHINESE_LANGUAGE_ADDON_COMMERCE:()=>d});const d={cardNumber:["号码","信用卡号码"],cardExpiry:["有效期","信用卡有效期"]}}}]);
