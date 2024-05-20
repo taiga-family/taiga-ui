fa6bc3cfd28cee0399034f1f0921a55116ba58df
splincode <omaxphp@yandex.ru>
2024-05-20 10:31:35 +0300

    chore: remove redundant

--- projects/layout/components/block-status/block-status.module.ts
+++ projects/layout/components/block-status/block-status.module.ts
@@ -1,11 +1,9 @@
-import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 
 import {TuiBlockStatusComponent} from './block-status.component';
 import {TuiBlockStatusDirective} from './block-status.directive';
 
 @NgModule({
-    imports: [CommonModule],
     declarations: [TuiBlockStatusComponent, TuiBlockStatusDirective],
     exports: [TuiBlockStatusComponent, TuiBlockStatusDirective],
 })
