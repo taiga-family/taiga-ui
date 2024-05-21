6ac0d534ce9b85cc0a300ad08eabdeec6f7f497c
splincode <splincode@users.noreply.github.com>
2024-05-21 16:34:22 +0000

    deploy: 3a37402a3caec07d3e960b432d0ead24d9e76cb5

---
+++ next/73057.c40a9f61c45318fa.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[73057],{73057:n=>{n.exports='```html\n<tui-combo-box\n  [stringify]="\'name\' | tuiStringify"\n  [(ngModel)]="value"\n>\n  \x3c!-- ... --\x3e\n</tui-combo-box>\n```\n'}}]);
