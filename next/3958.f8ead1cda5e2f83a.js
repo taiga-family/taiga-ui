b17e4ce1a7e7458ee92fb773d7b2022cf2631d71
splincode <splincode@users.noreply.github.com>
2024-05-21 21:49:24 +0000

    deploy: 267a0d734303e981f6ecaf451326b905ef7fe22f

---
+++ next/3958.f8ead1cda5e2f83a.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[3958],{3958:r=>{r.exports='```html\n<form [formGroup]="form">\n  <tui-input-card formControlName="card">Card number</tui-input-card>\n  <tui-input-expire formControlName="expire">Expire date</tui-input-expire>\n  <tui-input-cvc formControlName="cvc">CVC/CVV</tui-input-cvc>\n</form>\n```\n'}}]);
--- next/92593.23eb23b8b4692efa.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[92593],{92593:n=>{n.exports="```ts\nimport {TuiInputCardGroupedModule} from '@taiga-ui/addon-commerce';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiInputCardGroupedModule,\n  ],\n  // ...\n})\nexport class MyComponent {}\n```\n"}}]);
