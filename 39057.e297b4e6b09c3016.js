0c4292122b4dd6d5fdb7ac561008ce9b3aef2e18
splincode <omaxphp@yandex.ru>
2024-05-21 16:23:53 +0000

    Update snapshots/demo/next/main to output generated at 3a37402

--- 39057.e297b4e6b09c3016.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[39057],{39057:n=>{n.exports='```html\n<tui-combo-box\n  [stringify]="\'name\' | tuiStringify"\n  [(ngModel)]="value"\n>\n  \x3c!-- ... --\x3e\n</tui-combo-box>\n```\n'}}]);
