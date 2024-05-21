15465b481085e1bd16bb1ff500addd03ae4535e3
Maksim Ivanov <omaxphp@yandex.ru>
2024-05-21 10:53:19 +0300

    refactor: drop any in `tuiOverrideOptions` (#7494)

--- projects/core/utils/miscellaneous/override-options.ts
+++ projects/core/utils/miscellaneous/override-options.ts
@@ -3,11 +3,11 @@ export function tuiOverrideOptions<T>(
     fallback: T,
 ): (directive: T | null, options: T | null) => T {
     return (directive, options) => {
-        const result = directive || {...(options || fallback)};
+        const result: T = directive || {...(options || fallback)};
 
         Object.keys(override).forEach(key => {
             // Update directive props with new defaults before inputs are processed
-            (result as any)[key] = override[key as keyof T];
+            (result as Record<string, unknown>)[key] = override[key as keyof T];
         });
 
         return result;
