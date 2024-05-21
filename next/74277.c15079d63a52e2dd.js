f4435805b977b4ba47a386ebf5c61df4c247d7f8
splincode <splincode@users.noreply.github.com>
2024-05-21 16:58:52 +0000

    deploy: b12434855ac3b96d29e80103c1d1544f0fe548d3

---
+++ next/74277.c15079d63a52e2dd.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[74277],{74277:n=>{n.exports="```ts\nimport {TuiHint} from '@taiga-ui/core';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiHint,\n  ],\n  // ...\n})\nexport class MyComponent {}\n```\n"}}]);
