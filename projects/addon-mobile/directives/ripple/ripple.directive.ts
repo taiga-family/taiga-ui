160b2bace4f3159f250d1435bc8a8fa6420cd78b
Svyatoslav Zaytsev <143048525+MillerSvt@users.noreply.github.com>
2024-05-24 18:36:23 +0500

    feat(addon-mobile): `Ripple` allow global ripple definition (#5931)

--- projects/addon-mobile/directives/ripple/ripple.directive.ts
+++ projects/addon-mobile/directives/ripple/ripple.directive.ts
@@ -1,30 +1,41 @@
+import {DOCUMENT} from '@angular/common';
 import {
     ChangeDetectionStrategy,
     Component,
+    DestroyRef,
     Directive,
+    HostListener,
     inject,
     Input,
-    Renderer2,
     ViewEncapsulation,
 } from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {
-    TUI_FALSE_HANDLER,
-    TUI_TRUE_HANDLER,
-    tuiInjectElement,
-    tuiTypedFromEvent,
-    tuiWithStyles,
-} from '@taiga-ui/cdk';
-import {map, mergeMap, race, switchMap, take, takeUntil, tap, timer} from 'rxjs';
+import {tuiIsHTMLElement, tuiPx, tuiTypedFromEvent, tuiWithStyles} from '@taiga-ui/cdk';
+import {TUI_ANIMATIONS_SPEED, tuiGetDuration} from '@taiga-ui/core';
+import {first, merge, race, switchMap, tap} from 'rxjs';
 
-import {
-    RIPPLE_OFF,
-    TUI_RIPPLE_END,
-    TUI_RIPPLE_PROVIDERS,
-    TUI_RIPPLE_START,
-} from './ripple.providers';
+const DEFAULT_SELECTOR = ':scope';
+const TO_KEYFRAMES = [
+    {
+        transform: 'scale(0)',
+        opacity: '0.12',
+    },
+    {
+        opacity: '0.12',
+    },
+];
+const FROM_KEYFRAMES = [
+    {
+        opacity: '0.12',
+    },
+    {
+        opacity: '0',
+    },
+];
 
-const TOUCH_MOVE_DELAY = 100;
+function selectorScopeFallback(value: string): string {
+    return value === '' ? DEFAULT_SELECTOR : value;
+}
 
 @Component({
     standalone: true,
@@ -41,60 +52,92 @@ class TuiRippleStyles {}
 @Directive({
     standalone: true,
     selector: '[tuiRipple]',
-    providers: TUI_RIPPLE_PROVIDERS,
 })
 export class TuiRippleDirective {
-    private readonly el = tuiInjectElement();
-    private readonly renderer = inject(Renderer2);
-    private readonly start$ = inject(TUI_RIPPLE_START);
-    private readonly end$ = inject(TUI_RIPPLE_END);
+    private readonly document = inject(DOCUMENT);
+    private readonly destroyRef = inject(DestroyRef);
+    private readonly animationOptions = {
+        duration: tuiGetDuration(inject(TUI_ANIMATIONS_SPEED)),
+    };
 
-    @Input()
-    public tuiRipple?: string | '';
+    @Input({
+        alias: 'tuiRipple',
+        transform: selectorScopeFallback,
+    })
+    public selector = DEFAULT_SELECTOR;
 
     protected readonly nothing = tuiWithStyles(TuiRippleStyles);
 
-    constructor() {
-        const touchEnd$ = tuiTypedFromEvent(this.el, 'touchend');
-        const touchMove$ = tuiTypedFromEvent(this.el, 'touchmove');
+    @HostListener('pointerdown.silent', [
+        '$event.clientX',
+        '$event.clientY',
+        '$event.target',
+    ])
+    protected start(x: number, y: number, target: HTMLElement): void {
+        const element = target.closest(this.selector);
 
-        this.end$.subscribe(element => this.renderer.removeChild(this.el, element));
+        if (!tuiIsHTMLElement(element)) {
+            return;
+        }
 
-        this.start$
+        const ripple = this.createRipple(x, y, element.getBoundingClientRect());
+        const touchEnd$ = merge(
+            tuiTypedFromEvent(element, 'pointerup'),
+            tuiTypedFromEvent(element, 'pointercancel'),
+            tuiTypedFromEvent(element, 'pointermove'),
+        );
+
+        element.appendChild(ripple);
+
+        const animationEndOn$ = tuiTypedFromEvent(
+            ripple.animate(TO_KEYFRAMES, this.animationOptions),
+            'finish',
+        );
+
+        race(
+            touchEnd$.pipe(switchMap(() => animationEndOn$)),
+            animationEndOn$.pipe(switchMap(() => touchEnd$)),
+        )
             .pipe(
-                mergeMap(ripple => {
-                    const animationEndOn$ = tuiTypedFromEvent(ripple, 'animationend');
-
-                    return race(
-                        timer(TOUCH_MOVE_DELAY).pipe(map(TUI_FALSE_HANDLER)),
-                        touchEnd$.pipe(map(TUI_TRUE_HANDLER)),
-                    ).pipe(
-                        take(1),
-                        // eslint-disable-next-line rxjs/no-unsafe-takeuntil
-                        takeUntil(touchMove$),
-                        tap(() => {
-                            this.renderer.setStyle(
-                                ripple,
-                                'background',
-                                this.tuiRipple || null,
-                            );
-                            this.renderer.appendChild(this.el, ripple);
-                        }),
-                        switchMap(isTap =>
-                            isTap
-                                ? animationEndOn$
-                                : race(
-                                      touchEnd$.pipe(switchMap(() => animationEndOn$)),
-                                      animationEndOn$.pipe(switchMap(() => touchEnd$)),
-                                  ),
-                        ),
-                        map(() => ripple),
-                    );
-                }),
-                takeUntilDestroyed(),
+                first(),
+                switchMap(() =>
+                    tuiTypedFromEvent(
+                        ripple.animate(FROM_KEYFRAMES, this.animationOptions),
+                        'finish',
+                    ),
+                ),
+                first(),
+                tap(() => element.removeChild(ripple)),
+                takeUntilDestroyed(this.destroyRef),
             )
-            .subscribe(element =>
-                this.renderer.setStyle(element, 'animationName', RIPPLE_OFF),
-            );
+            .subscribe();
+    }
+
+    private createRipple(
+        clientX: number,
+        clientY: number,
+        {width, height, top, left}: DOMRect,
+    ): HTMLElement {
+        const ripple: HTMLElement = this.document.createElement('div');
+        const radius = Math.sqrt(width * width + height * height);
+        const dimension = radius * 2;
+        const x = clientX - left - radius;
+        const y = clientY - top - radius;
+
+        Object.assign(ripple.style, {
+            position: 'absolute',
+            top: tuiPx(y),
+            left: tuiPx(x),
+            width: tuiPx(dimension),
+            height: tuiPx(dimension),
+            zIndex: 100,
+            opacity: 0.12,
+            borderRadius: '100%',
+            background: 'var(--tui-ripple-background, currentColor)',
+            animationFillMode: 'forwards',
+            pointerEvents: 'none',
+        });
+
+        return ripple;
     }
 }
