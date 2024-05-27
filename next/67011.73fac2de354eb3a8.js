9de487f1f092b3758836449a9dce77517d22928a
splincode <splincode@users.noreply.github.com>
2024-05-27 16:41:39 +0000

    deploy: f8e71a52395a96abbe59115f36624999a1914a4c

--- next/67011.73fac2de354eb3a8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[67011],{67011:n=>{n.exports="```ts\nimport {TuiCalendarMonthModule} from '@taiga-ui/kit';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiCalendarMonthModule,\n  ],\n  // ...\n})\nexport class MyComponent {}\n```\n"}}]);
---
+++ next/67011.ba46a8b202268d2e.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[67011],{67011:n=>{n.exports="```ts\nimport {TuiCalendarMonthComponent} from '@taiga-ui/kit';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiCalendarMonthComponent,\n  ],\n  // ...\n})\nexport class MyComponent {}\n```\n"}}]);
