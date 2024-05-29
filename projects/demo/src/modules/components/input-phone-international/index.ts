027b550bb038892578964f17af43f9fb918564d2
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-05-29 16:34:35 +0300

    chore(demo): migrate documentation page `InputPhoneInternational` to standalone (#7571)

--- projects/demo/src/modules/components/input-phone-international/index.ts
+++ projects/demo/src/modules/components/input-phone-international/index.ts
@@ -1,23 +1,35 @@
 import {Component} from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
-import type {TuiDocExample} from '@taiga-ui/addon-doc';
+import {TuiDemo} from '@demo/utils';
 import {tuiProvide} from '@taiga-ui/cdk';
+import {TuiDropdownModule, TuiHint, TuiTextfieldControllerModule} from '@taiga-ui/core';
 import type {TuiCountryIsoCode} from '@taiga-ui/i18n';
-import {TUI_ALL_COUNTRIES_ISO_CODES} from '@taiga-ui/legacy';
+import {
+    TUI_ALL_COUNTRIES_ISO_CODES,
+    TuiInputPhoneInternationalComponent,
+} from '@taiga-ui/legacy';
 
 import {ABSTRACT_PROPS_ACCESSOR} from '../abstract/abstract-props-accessor';
 import {AbstractExampleTuiControl} from '../abstract/control';
+import {InheritedDocumentationComponent} from '../abstract/inherited-documentation';
 
 @Component({
-    selector: 'example-tui-input-phone-international',
-    templateUrl: './input-phone-international.template.html',
-    changeDetection,
-    providers: [
-        tuiProvide(ABSTRACT_PROPS_ACCESSOR, ExampleTuiInputPhoneInternationalComponent),
+    standalone: true,
+    imports: [
+        InheritedDocumentationComponent,
+        TuiDemo,
+        TuiDropdownModule,
+        TuiHint,
+        TuiInputPhoneInternationalComponent,
+        TuiTextfieldControllerModule,
+        ReactiveFormsModule,
     ],
+    templateUrl: './index.html',
+    changeDetection,
+    providers: [tuiProvide(ABSTRACT_PROPS_ACCESSOR, PageComponent)],
 })
-export class ExampleTuiInputPhoneInternationalComponent extends AbstractExampleTuiControl {
+export default class PageComponent extends AbstractExampleTuiControl {
     public override cleaner = false;
     public override labelOutside = true;
     public control = new FormControl('', [Validators.required, Validators.minLength(9)]);
@@ -25,18 +37,6 @@ export class ExampleTuiInputPhoneInternationalComponent extends AbstractExampleT
     protected readonly exampleForm = import('./examples/import/declare-form.md?raw');
     protected readonly exampleModule = import('./examples/import/import-module.md?raw');
     protected readonly exampleHtml = import('./examples/import/insert-template.md?raw');
-
-    protected readonly example1: TuiDocExample = {
-        TypeScript: import('./examples/1/index.ts?raw'),
-        HTML: import('./examples/1/index.html?raw'),
-        LESS: import('./examples/1/index.less?raw'),
-    };
-
-    protected readonly example2: TuiDocExample = {
-        TypeScript: import('./examples/2/index.ts?raw'),
-        HTML: import('./examples/2/index.html?raw'),
-        LESS: import('./examples/2/index.less?raw'),
-    };
 
     protected readonly countriesVariants: ReadonlyArray<readonly TuiCountryIsoCode[]> = [
         ['RU', 'KZ', 'UA', 'BY'],
