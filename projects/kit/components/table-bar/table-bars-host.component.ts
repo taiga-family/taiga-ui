f0ce18ac5475c52682fba55592ae9e43ca538b3e
Vladimir Potekhin <vladimir.potekh@gmail.com>
2024-05-30 17:35:44 +0300

    chore: add fade and icons

--- projects/kit/components/table-bar/table-bars-host.component.ts
+++ projects/kit/components/table-bar/table-bars-host.component.ts
@@ -1,12 +1,15 @@
 import {AsyncPipe, NgForOf} from '@angular/common';
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import type {TuiPopover} from '@taiga-ui/cdk';
 import {
     TUI_ANIMATIONS_SPEED,
+    tuiFadeIn,
     tuiParentAnimation,
     tuiSlideInTop,
     tuiToAnimationOptions,
 } from '@taiga-ui/core';
 import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
+import type {BehaviorSubject} from 'rxjs';
 
 import {TUI_TABLE_BARS} from './table-bars.token';
 
@@ -19,6 +22,7 @@ import {TUI_TABLE_BARS} from './table-bars.token';
             *ngFor="let item of bars$ | async"
             class="t-wrapper"
             @tuiParentAnimation
+            [@tuiFadeIn]="animation"
             [@tuiSlideInTop]="animation"
         >
             <ng-container *polymorpheusOutlet="item.component; context: item" />
@@ -26,9 +30,11 @@ import {TUI_TABLE_BARS} from './table-bars.token';
     `,
     styleUrls: ['./table-bars-host.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    animations: [tuiParentAnimation, tuiSlideInTop],
+    animations: [tuiParentAnimation, tuiSlideInTop, tuiFadeIn],
 })
 export class TuiTableBarsHostComponent {
-    protected bars$ = inject(TUI_TABLE_BARS);
+    protected bars$: BehaviorSubject<ReadonlyArray<TuiPopover<void, void>>> =
+        inject(TUI_TABLE_BARS);
+
     protected readonly animation = tuiToAnimationOptions(inject(TUI_ANIMATIONS_SPEED));
 }
