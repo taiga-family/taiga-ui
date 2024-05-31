8df080485a95a2a68eb4368621a10739299f2ed2
waterplea <alexander@inkin.ru>
2024-05-31 10:31:16 +0100

    chore: fix build
    Signed-off-by: waterplea <alexander@inkin.ru>

--- projects/experimental/components/navigation/navigation.module.ts
+++ projects/experimental/components/navigation/navigation.module.ts
@@ -1,11 +1,11 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {TuiScrollableDirective, TuiScrollbarComponent} from '@taiga-ui/core';
-import {TuiHintAsideDirective} from '@taiga-ui/experimental/components/navigation/hint-aside.directive';
 import {TuiFadeDirective} from '@taiga-ui/kit';
 
 import {TuiAsideComponent} from './aside.component';
 import {TuiHeaderComponent} from './header.component';
+import {TuiHintAsideDirective} from './hint-aside.directive';
 import {TuiLogoComponent} from './logo.component';
 import {TuiMainComponent} from './main.component';
 import {TuiNavComponent} from './nav.component';
