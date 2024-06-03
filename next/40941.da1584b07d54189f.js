9c665d4b04c5b8761d5520d6f87b36b912bb6915
splincode <splincode@users.noreply.github.com>
2024-06-03 13:52:02 +0000

    deploy: 7d4217b533a3f41ef488337eb838ec4682f9eff9

---
+++ next/40941.da1584b07d54189f.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[40941],{40941:n=>{n.exports='<button\n    tuiButton\n    [routerLink]="[\n        {\n            outlets: {\n                myOutlet: \'path/to/dialog\',\n            },\n        },\n    ]"\n>\n    Open dialog\n</button>\n\n<router-outlet name="myOutlet" />\n'}}]);
