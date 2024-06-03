518cbac22da07b66568865a9730ec4ca478a8a4c
splincode <omaxphp@yandex.ru>
2024-06-03 13:50:49 +0000

    Update snapshots/demo/next/main to output generated at 7d4217b

---
+++ 12965.b7b22af2d0d19bac.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[12965],{12965:n=>{n.exports='```html\n<tui-input\n  *ngIf="value | tuiEmails as emails"\n  [(ngModel)]="value"\n>\n  Введите адрес\n  <ng-template\n    *ngIf="emails.length"\n    [tuiDataList]\n  >\n    <tui-data-list-wrapper [items]="emails"></tui-data-list-wrapper>\n  </ng-template>\n</tui-input>\n```\n'}}]);
