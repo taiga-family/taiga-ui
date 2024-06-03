166cf07c5fe2096028d305ed222479da1a03f190
Andrei Belokopytov <belokopytov87@gmail.com>
2024-06-03 12:53:44 +0300

    chore: add focusable migration (#7600)
    Co-authored-by: Andrey Belokopytov <a.v.belokopytov@tinkoff.ru>

---
+++ projects/cdk/schematics/ng-update/v4/steps/templates/migrate-focusable.ts
@@ -0,0 +1,68 @@
+import type {UpdateRecorder} from '@angular-devkit/schematics';
+import type {DevkitFileSystem} from 'ng-morph';
+import type {Attribute, Location} from 'parse5/dist/common/token';
+
+import {findElementsWithAttributeOnTag} from '../../../../utils/templates/elements';
+import {
+    getTemplateFromTemplateResource,
+    getTemplateOffset,
+} from '../../../../utils/templates/template-resource';
+import type {TemplateResource} from '../../../interfaces';
+
+const LEGACY_ATTRIBUTE_NAME = '[tuiFocusable]'.toLowerCase();
+
+export function migrateFocusable({
+    resource,
+    recorder,
+    fileSystem,
+}: {
+    fileSystem: DevkitFileSystem;
+    recorder: UpdateRecorder;
+    resource: TemplateResource;
+}): void {
+    const template = getTemplateFromTemplateResource(resource, fileSystem);
+    const templateOffset = getTemplateOffset(resource);
+
+    const elements = findElementsWithAttributeOnTag(template, [LEGACY_ATTRIBUTE_NAME]);
+
+    elements.forEach(({attrs, sourceCodeLocation}) => {
+        if (!sourceCodeLocation) {
+            return;
+        }
+
+        const focusableAttr = attrs.find(attr => attr.name === LEGACY_ATTRIBUTE_NAME);
+        const attributeLocation = sourceCodeLocation.attrs?.[LEGACY_ATTRIBUTE_NAME];
+
+        if (!focusableAttr || !attributeLocation) {
+            return;
+        }
+
+        replaceAttribute({
+            attr: focusableAttr,
+            templateOffset,
+            location: attributeLocation,
+            recorder,
+        });
+    });
+}
+
+function replaceAttribute({
+    attr,
+    templateOffset,
+    location,
+    recorder,
+}: {
+    attr: Attribute;
+    templateOffset: number;
+    location: Location;
+    recorder: UpdateRecorder;
+}): void {
+    const startOffset = templateOffset + location.startOffset;
+    const length = location.endOffset - location.startOffset;
+
+    recorder.remove(startOffset, length);
+    recorder.insertRight(
+        templateOffset + startOffset,
+        `[tabIndex]="${attr.value} ? 0 : -1"`,
+    );
+}
---
+++ projects/cdk/schematics/ng-update/v4/tests/schematic-migrate-focusable.spec.ts
@@ -0,0 +1,98 @@
+import {join} from 'node:path';
+
+import {HostTree} from '@angular-devkit/schematics';
+import {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
+import type {TuiSchema} from '@taiga-ui/cdk/schematics/ng-add/schema';
+import {
+    createProject,
+    createSourceFile,
+    resetActiveProject,
+    saveActiveProject,
+    setActiveProject,
+} from 'ng-morph';
+
+import {createAngularJson} from '../../../utils/create-angular-json';
+
+const collectionPath = join(__dirname, '../../../migration.json');
+
+const COMPONENT_BEFORE = `
+import {TuiFocusableModule} from '@taiga-ui/cdk';
+
+@Component({
+    standalone: true,
+    templateUrl: './test.template.html',
+    imports: [TuiFocusableModule]
+})
+export class TestComponent {
+    focusable = true;
+}`;
+
+const COMPONENT_AFTER = `
+@Component({
+    standalone: true,
+    templateUrl: './test.template.html',
+    imports: []
+})
+export class TestComponent {
+    focusable = true;
+}`;
+
+const TEMPLATE_BEFORE = `
+    <div [tuiFocusable]="focusable"></div>
+`;
+
+const TEMPLATE_AFTER = `
+    <div [tabIndex]="focusable ? 0 : -1"></div>
+`;
+
+describe('ng-migrate', () => {
+    let host: UnitTestTree;
+    let runner: SchematicTestRunner;
+
+    beforeEach(() => {
+        host = new UnitTestTree(new HostTree());
+        runner = new SchematicTestRunner('schematics', collectionPath);
+
+        setActiveProject(createProject(host));
+
+        createMainFiles();
+
+        saveActiveProject();
+    });
+
+    it('should migrate focusable in template', async () => {
+        const tree = await runner.runSchematic(
+            'updateToV4',
+            {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
+            host,
+        );
+
+        expect(tree.readContent('test/app/test.template.html')).toEqual(TEMPLATE_AFTER);
+    });
+
+    it('should migrate focusable references in ts files', async () => {
+        const tree = await runner.runSchematic(
+            'updateToV4',
+            {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
+            host,
+        );
+
+        expect(tree.readContent('test/app/test.component.ts')).toEqual(COMPONENT_AFTER);
+    });
+
+    afterEach(() => {
+        resetActiveProject();
+    });
+});
+
+function createMainFiles(): void {
+    createSourceFile('test/app/test.component.ts', COMPONENT_BEFORE);
+
+    createSourceFile('test/app/test.template.html', TEMPLATE_BEFORE);
+
+    createAngularJson();
+    createSourceFile(
+        'package.json',
+        '{"dependencies": {"@angular/core": "~13.0.0", "@taiga-ui/cdk": "~3.59.0"}}',
+    );
+}
