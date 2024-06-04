8011f5a763bcff0d1bd8e2e42669effedb553f2a
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-06-04 11:50:41 +0300

    refactor(core): use `childNodes.length` instead of `textContent` to detect label for `Textfield`

--- projects/core/components/textfield/textfield.component.ts
+++ projects/core/components/textfield/textfield.component.ts
@@ -154,6 +154,6 @@ export class TuiTextfieldComponent<T>
     }
 
     protected get hasLabel(): boolean {
-        return Boolean(this.label?.nativeElement?.textContent?.trim());
+        return Boolean(this.label?.nativeElement?.childNodes.length);
     }
 }
