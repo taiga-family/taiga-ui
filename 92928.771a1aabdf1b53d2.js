a392f4e824b86ae647169dc96c9d27905fc4885c
waterplea <alexander@inkin.ru>
2024-06-04 09:15:44 +0000

    Update snapshots/demo/next/main to output generated at ccaf5a6

---
+++ 92928.771a1aabdf1b53d2.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[92928],{92928:n=>{n.exports="form {\n    display: flex;\n    flex-direction: column;\n    gap: 1rem;\n}\n\nsection {\n    display: flex;\n    gap: inherit;\n\n    tui-textfield {\n        flex: 1;\n    }\n}\n"}}]);
