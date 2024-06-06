7eb6d4146531ee1b4f44e11e041fa9382e4384cf
waterplea <alexander@inkin.ru>
2024-06-06 20:49:06 +0500

    chore: fix
    Signed-off-by: waterplea <alexander@inkin.ru>

--- projects/addon-table/components/table/providers/table.providers.ts
+++ projects/addon-table/components/table/providers/table.providers.ts
@@ -3,10 +3,10 @@ import {
     INTERSECTION_THRESHOLD,
     IntersectionObserverService,
 } from '@ng-web-apis/intersection-observer';
-import {TuiTableDirective} from '@taiga-ui/addon-table/components';
 import {tuiProvide} from '@taiga-ui/cdk';
 import {TUI_TEXTFIELD_OPTIONS} from '@taiga-ui/core';
 
+import {TuiTableDirective} from '../directives/table.directive';
 import {TUI_STUCK_PROVIDER} from './stuck.provider';
 
 export const TUI_TABLE_PROVIDERS = [
