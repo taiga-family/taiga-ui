434f1234f8d082678c2244c8a6d7209ffe14262a
splincode <omaxphp@yandex.ru>
2024-06-06 11:08:12 +0000

    Update snapshots/demo/next/main to output generated at b3c1617

--- 79939.a5e7cb83c590d156.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[79939],{79939:e=>{e.exports='<tui-progress-circle\n    *tuiRepeatTimes="let _ of 4"\n    new\n    size="l"\n    [max]="100"\n    [value]="60"\n/>\n'}}]);
