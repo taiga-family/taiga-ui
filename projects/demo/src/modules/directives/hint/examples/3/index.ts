e3782f5eac4a33bed58b1e0fb96e7b9cd88262b4
Vladimir Potekhin <vladimir.potekh@gmail.com>
2024-06-07 11:27:20 +0300

    feat(core): add ability to use nested hints

---
+++ projects/demo/src/modules/directives/hint/examples/3/index.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiHint} from '@taiga-ui/core';
+import {TuiBadgeDirective} from '@taiga-ui/kit';
+
+@Component({
+    standalone: true,
+    imports: [TuiHint, TuiBadgeDirective],
+    templateUrl: './index.html',
+    encapsulation,
+    changeDetection,
+})
+export default class ExampleComponent {}
