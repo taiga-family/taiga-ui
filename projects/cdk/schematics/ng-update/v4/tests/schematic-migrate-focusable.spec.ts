5470603649729bcd5ee5e3f4f3855c5022777772
Andrey Belokopytov <a.v.belokopytov@tinkoff.ru>
2024-06-10 21:05:49 +0300

    feat: improve migrate focusable schematics

--- projects/cdk/schematics/ng-update/v4/tests/schematic-migrate-focusable.spec.ts
+++ projects/cdk/schematics/ng-update/v4/tests/schematic-migrate-focusable.spec.ts
@@ -1,5 +1,6 @@
 import {join} from 'node:path';
 
+import type {Tree} from '@angular-devkit/schematics';
 import {HostTree} from '@angular-devkit/schematics';
 import {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
 import type {TuiSchema} from '@taiga-ui/cdk/schematics/ng-add/schema';
@@ -37,14 +38,6 @@ export class TestComponent {
     focusable = true;
 }`;
 
-const TEMPLATE_BEFORE = `
-    <div [tuiFocusable]="focusable"></div>
-`;
-
-const TEMPLATE_AFTER = `
-    <div [tabIndex]="focusable ? 0 : -1"></div>
-`;
-
 describe('ng-migrate', () => {
     let host: UnitTestTree;
     let runner: SchematicTestRunner;
@@ -52,32 +45,94 @@ describe('ng-migrate', () => {
     beforeEach(() => {
         host = new UnitTestTree(new HostTree());
         runner = new SchematicTestRunner('schematics', collectionPath);
-
-        setActiveProject(createProject(host));
-
-        createMainFiles();
-
-        saveActiveProject();
     });
 
-    it('should migrate focusable in template', async () => {
+    it('should migrate attribute assigned to property in template', async () => {
+        initComponent({
+            host,
+            component: COMPONENT_BEFORE,
+            template: '<div [tuiFocusable]="focusable"></div>',
+        });
+
         const tree = await runner.runSchematic(
             'updateToV4',
             {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
             host,
         );
 
-        expect(tree.readContent('test/app/test.template.html')).toEqual(TEMPLATE_AFTER);
+        expect(tree.readContent('test/app/test.template.html')).toBe(
+            '<div [tabIndex]="focusable ? 0 : -1"></div>',
+        );
+    });
+
+    it('should migrate attribute assigned to value "true" in template', async () => {
+        initComponent({
+            host,
+            component: COMPONENT_BEFORE,
+            template: '<div [tuiFocusable]="true"></div>',
+        });
+
+        const tree = await runner.runSchematic(
+            'updateToV4',
+            {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
+            host,
+        );
+
+        expect(tree.readContent('test/app/test.template.html')).toBe(
+            '<div [tabIndex]="0"></div>',
+        );
+    });
+
+    it('should migrate attribute assigned to value "false" in template', async () => {
+        initComponent({
+            host,
+            component: COMPONENT_BEFORE,
+            template: '<div [tuiFocusable]="false"></div>',
+        });
+
+        const tree = await runner.runSchematic(
+            'updateToV4',
+            {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
+            host,
+        );
+
+        expect(tree.readContent('test/app/test.template.html')).toBe(
+            '<div [tabIndex]="-1"></div>',
+        );
+    });
+
+    it('should migrate attribute assigned to logical expression in template', async () => {
+        initComponent({
+            host,
+            component: COMPONENT_BEFORE,
+            template: '<div [tuiFocusable]="!!(focusable$ | async)"></div>',
+        });
+
+        const tree = await runner.runSchematic(
+            'updateToV4',
+            {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
+            host,
+        );
+
+        expect(tree.readContent('test/app/test.template.html')).toBe(
+            '<div [tabIndex]="(!!(focusable$ | async)) ? 0 : -1"></div>',
+        );
     });
 
     it('should migrate focusable references in ts files', async () => {
+        initComponent({
+            host,
+            component: COMPONENT_BEFORE,
+            template: '<div [tuiFocusable]="focusable"></div>',
+        });
+
         const tree = await runner.runSchematic(
             'updateToV4',
             {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
             host,
         );
 
-        expect(tree.readContent('test/app/test.component.ts')).toEqual(COMPONENT_AFTER);
+        expect(tree.readContent('test/app/test.component.ts')).toBe(COMPONENT_AFTER);
     });
 
     afterEach(() => {
@@ -85,14 +140,24 @@ describe('ng-migrate', () => {
     });
 });
 
-function createMainFiles(): void {
-    createSourceFile('test/app/test.component.ts', COMPONENT_BEFORE);
+function initComponent({
+    host,
+    component,
+    template,
+}: {
+    host: Tree;
+    component: string;
+    template: string;
+}): void {
+    setActiveProject(createProject(host));
+    createSourceFile('test/app/test.component.ts', component);
 
-    createSourceFile('test/app/test.template.html', TEMPLATE_BEFORE);
+    createSourceFile('test/app/test.template.html', template);
 
     createAngularJson();
     createSourceFile(
         'package.json',
         '{"dependencies": {"@angular/core": "~13.0.0", "@taiga-ui/cdk": "~3.59.0"}}',
     );
+    saveActiveProject();
 }
