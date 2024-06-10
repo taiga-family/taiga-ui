c8f174e33acaf1f106f72badf49206393910770d
Maksim Ivanov <omaxphp@yandex.ru>
2024-06-10 11:41:52 +0300

    refactor: drop `tuiCreateOptions` (#7684)

--- projects/cdk/utils/miscellaneous/create-options.ts
+++
@@ -1,8 +0,0 @@
-import type {InjectionToken} from '@angular/core';
-
-import {tuiCreateTokenFromFactory} from './create-token';
-
-/** @deprecated use {@link tuiCreateToken} instead */
-export function tuiCreateOptions<T>(defaults: T): InjectionToken<T> {
-    return tuiCreateTokenFromFactory(() => defaults);
-}
