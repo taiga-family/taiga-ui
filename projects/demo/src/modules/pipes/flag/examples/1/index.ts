446ba9440530881b5312fcea886c030ea0696c53
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-06-11 13:58:55 +0300

    chore(demo): improve documentation page `Flag` (#7708)

--- projects/demo/src/modules/pipes/flag/examples/1/index.ts
+++ projects/demo/src/modules/pipes/flag/examples/1/index.ts
@@ -1,15 +1,19 @@
-import {Component} from '@angular/core';
+import {AsyncPipe} from '@angular/common';
+import {Component, inject} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiFlagPipe} from '@taiga-ui/core';
+import type {TuiCountryIsoCode} from '@taiga-ui/i18n';
+import {TUI_COUNTRIES} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
-    imports: [TuiFlagPipe],
+    imports: [TuiFlagPipe, AsyncPipe],
     templateUrl: './index.html',
     encapsulation,
     changeDetection,
 })
 export default class ExampleComponent {
-    protected readonly countryIsoCode = 'AE';
+    protected readonly countryIsoCode: TuiCountryIsoCode = 'AE';
+    protected readonly countriesNames$ = inject(TUI_COUNTRIES);
 }
