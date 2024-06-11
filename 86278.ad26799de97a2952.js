d0ece4a6a7c767d96c69ef423438ae20bb1ad362
nsbarsukov <nikita.s.barsukov@gmail.com>
2024-06-11 11:03:46 +0000

    Update snapshots/demo/next/main to output generated at 446ba94

--- 86278.ad26799de97a2952.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[86278],{86278:n=>{n.exports="```ts\nimport {TuiFlagPipeModule} from '@taiga-ui/core';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiFlagPipeModule,\n  ],\n  // ...\n})\nexport class MyComponent {}\n```\n"}}]);
