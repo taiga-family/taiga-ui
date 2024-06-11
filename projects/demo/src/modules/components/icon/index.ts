9e93842d234fb7af773c94a1885ebc2286950d80
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-06-11 11:27:12 +0300

    chore(demo): add `API` section for documentation page `Icon` (#7701)

--- projects/demo/src/modules/components/icon/index.ts
+++ projects/demo/src/modules/components/icon/index.ts
@@ -1,12 +1,28 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
-import {TuiNotificationComponent} from '@taiga-ui/core';
+import {TUI_USED_ICONS} from '@taiga-ui/cdk';
+import {TuiIconComponent} from '@taiga-ui/core';
+import {tuiInputNumberOptionsProvider} from '@taiga-ui/legacy';
 
 @Component({
     standalone: true,
-    imports: [TuiDemo, TuiNotificationComponent],
+    imports: [TuiDemo, TuiIconComponent],
     templateUrl: './index.html',
     changeDetection,
+    providers: [
+        tuiInputNumberOptionsProvider({
+            min: 0,
+        }),
+    ],
 })
-export default class PageComponent {}
+export default class PageComponent {
+    protected readonly iconVariants = ['', ...TUI_USED_ICONS];
+    protected readonly colorVariants = ['', 'var(--tui-text-01)', 'red', '#3aa981'];
+
+    protected icon = 'tuiIconHeartLarge';
+    protected backgroundIcon = '';
+    protected color = this.colorVariants[1];
+    protected backgroundColor = '';
+    protected size = 24;
+}
