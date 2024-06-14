8e8da502dc4451ec9705db2ca0c9b42a4feb7007
splincode <splincode@users.noreply.github.com>
2024-06-14 08:28:51 +0000

    deploy: f898ce88ebfdfaaca4b3ab52817456752c5eb0d5

--- next/27730.a9b1a9ca050bb253.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[27730],{27730:n=>{n.exports="```ts\n// ...\nimport {NG_EVENT_PLUGINS} from '@tinkoff/ng-event-plugins';\n\nbootstrapApplication(AppComponent, {\n  providers: [\n    provideAnimations(),\n    NG_EVENT_PLUGINS,\n    //...\n  ],\n}).catch(err => console.error(err));\n```\n"}}]);
