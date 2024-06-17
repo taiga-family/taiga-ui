6c3e31770460f1ed4cf077ce7992f36fa59d5610
Svyatoslav Zaytsev <sviatsv@yandex.ru>
2024-06-17 13:26:40 +0500

    feat(addon-table): tuiTableFilters pipe sync

--- projects/addon-table/directives/table-filters/table-filters.pipe.ts
+++ projects/addon-table/directives/table-filters/table-filters.pipe.ts
@@ -1,17 +1,34 @@
-import type {PipeTransform} from '@angular/core';
-import {inject, Pipe} from '@angular/core';
-import type {Observable} from 'rxjs';
+import type {PipeTransform, Signal} from '@angular/core';
+import {inject, INJECTOR, Pipe, untracked} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {tuiPure} from '@taiga-ui/cdk';
+import {Subject, takeUntil} from 'rxjs';
 
 import {TuiTableFiltersDirective} from './table-filters.directive';
 
 @Pipe({
     standalone: true,
     name: 'tuiTableFilters',
+    pure: false,
 })
 export class TuiTableFiltersPipe<T> implements PipeTransform {
     private readonly filters = inject(TuiTableFiltersDirective<T>);
+    private readonly destroy$ = new Subject<void>();
+    private readonly injector = inject(INJECTOR);
 
-    public transform(items: readonly T[]): Observable<readonly T[]> {
-        return this.filters.filter(items);
+    public transform(items: readonly T[]): readonly T[] {
+        return this.getSignal(items)();
+    }
+
+    @tuiPure
+    private getSignal(items: readonly T[]): Signal<readonly T[]> {
+        this.destroy$.next();
+
+        return untracked(() =>
+            toSignal(this.filters.filter(items).pipe(takeUntil(this.destroy$)), {
+                injector: this.injector,
+                initialValue: [],
+            }),
+        );
     }
 }
