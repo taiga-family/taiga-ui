3c76dcaf499c02ed3bbca0002a9f6c3eeb164238
Svyatoslav Zaytsev <sviatsv@yandex.ru>
2024-06-17 14:17:36 +0500

    feat(kit): tuiSortCountries pipe sync

--- projects/kit/pipes/sort-countries/sort-countries.pipe.ts
+++ projects/kit/pipes/sort-countries/sort-countries.pipe.ts
@@ -1,22 +1,41 @@
-import type {PipeTransform} from '@angular/core';
-import {inject, Pipe} from '@angular/core';
+import type {PipeTransform, Signal} from '@angular/core';
+import {inject, INJECTOR, Pipe, untracked} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {tuiPure} from '@taiga-ui/cdk';
 import type {TuiCountryIsoCode} from '@taiga-ui/i18n';
 import {TUI_COUNTRIES} from '@taiga-ui/kit/tokens';
-import type {Observable} from 'rxjs';
-import {map} from 'rxjs';
+import {map, Subject, takeUntil} from 'rxjs';
 
 @Pipe({
     standalone: true,
     name: 'tuiSortCountries',
+    pure: false,
 })
 export class TuiSortCountriesPipe implements PipeTransform {
     private readonly countriesNames$ = inject(TUI_COUNTRIES);
+    private readonly destroy$ = new Subject<void>();
+    private readonly injector = inject(INJECTOR);
 
-    public transform(
+    public transform(countries: readonly TuiCountryIsoCode[]): TuiCountryIsoCode[] {
+        return this.getSignal(countries)();
+    }
+
+    @tuiPure
+    private getSignal(
         countries: readonly TuiCountryIsoCode[],
-    ): Observable<TuiCountryIsoCode[]> {
-        return this.countriesNames$.pipe(
-            map(names => [...countries].sort((a, b) => names[a].localeCompare(names[b]))),
+    ): Signal<TuiCountryIsoCode[]> {
+        this.destroy$.next();
+
+        return untracked(() =>
+            toSignal(
+                this.countriesNames$.pipe(
+                    map(names =>
+                        [...countries].sort((a, b) => names[a].localeCompare(names[b])),
+                    ),
+                    takeUntil(this.destroy$),
+                ),
+                {injector: this.injector, initialValue: []},
+            ),
         );
     }
 }
