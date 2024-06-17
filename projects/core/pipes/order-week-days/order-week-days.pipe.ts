0441e666ee0d7fa251fba2602b45c4c896a25c3f
Svyatoslav Zaytsev <sviatsv@yandex.ru>
2024-06-17 13:54:02 +0500

    feat(kit): tuiOrderWeekDays pipe sync

--- projects/core/pipes/order-week-days/order-week-days.pipe.ts
+++ projects/core/pipes/order-week-days/order-week-days.pipe.ts
@@ -1,8 +1,10 @@
-import type {PipeTransform} from '@angular/core';
-import {inject, Pipe} from '@angular/core';
+import type {PipeTransform, Signal} from '@angular/core';
+import {inject, INJECTOR, Pipe, untracked} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {tuiPure} from '@taiga-ui/cdk';
 import {TUI_FIRST_DAY_OF_WEEK} from '@taiga-ui/core/tokens';
 import type {Observable} from 'rxjs';
-import {map} from 'rxjs';
+import {map, Subject, takeUntil} from 'rxjs';
 
 type WeekDays<T> = [T, T, T, T, T, T, T];
 
@@ -18,21 +20,39 @@ function convertToSundayFirstWeekFormat<T>(weekDaysNames: WeekDays<T>): WeekDays
 @Pipe({
     standalone: true,
     name: 'tuiOrderWeekDays',
+    pure: false,
 })
 export class TuiOrderWeekDaysPipe implements PipeTransform {
     private readonly firstDayOfWeekIndex = inject(TUI_FIRST_DAY_OF_WEEK);
+    private readonly injector = inject(INJECTOR);
+    private readonly destroy$ = new Subject<void>();
 
     public transform<T>(
         mondayFirstWeekDays$: Observable<WeekDays<T>>,
-    ): Observable<WeekDays<T>> {
-        return mondayFirstWeekDays$.pipe(
-            map(convertToSundayFirstWeekFormat),
-            map(
-                weekDays =>
-                    [
-                        ...weekDays.slice(this.firstDayOfWeekIndex),
-                        ...weekDays.slice(0, this.firstDayOfWeekIndex),
-                    ] as WeekDays<T>,
+    ): WeekDays<T> | null {
+        return this.getSignal(mondayFirstWeekDays$)();
+    }
+
+    @tuiPure
+    private getSignal<T>(
+        mondayFirstWeekDays$: Observable<WeekDays<T>>,
+    ): Signal<WeekDays<T> | null> {
+        this.destroy$.next();
+
+        return untracked(() =>
+            toSignal(
+                mondayFirstWeekDays$.pipe(
+                    map(convertToSundayFirstWeekFormat),
+                    map(
+                        weekDays =>
+                            [
+                                ...weekDays.slice(this.firstDayOfWeekIndex),
+                                ...weekDays.slice(0, this.firstDayOfWeekIndex),
+                            ] as WeekDays<T>,
+                    ),
+                    takeUntil(this.destroy$),
+                ),
+                {injector: this.injector, initialValue: null},
             ),
         );
     }
