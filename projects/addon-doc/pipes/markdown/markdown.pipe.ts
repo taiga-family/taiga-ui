66a9a0cda3a7306c20a77c82be7360f6b04fff42
Svyatoslav Zaytsev <sviatsv@yandex.ru>
2024-06-17 14:20:43 +0500

    feat(kit): tuiMarkdown pipe sync

--- projects/addon-doc/pipes/markdown/markdown.pipe.ts
+++ projects/addon-doc/pipes/markdown/markdown.pipe.ts
@@ -1,32 +1,53 @@
-import type {PipeTransform} from '@angular/core';
-import {Pipe} from '@angular/core';
+import type {PipeTransform, Signal} from '@angular/core';
+import {inject, INJECTOR, Pipe, untracked} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
 import type {TuiRawLoaderContent} from '@taiga-ui/addon-doc/types';
 import {tuiRawLoad} from '@taiga-ui/addon-doc/utils';
 import type {TuiSafeHtml, TuiStringHandler} from '@taiga-ui/cdk';
+import {tuiPure} from '@taiga-ui/cdk';
 import {marked} from 'marked';
-import type {Observable} from 'rxjs';
-import {identity, map, of, switchMap} from 'rxjs';
+import {identity, map, of, Subject, switchMap, takeUntil} from 'rxjs';
 
 @Pipe({
     standalone: true,
     name: 'tuiMarkdown',
+    pure: false,
 })
 export class TuiMarkdownPipe implements PipeTransform {
+    private readonly destroy$ = new Subject<void>();
+    private readonly injector = inject(INJECTOR);
+
     public transform(
         value: TuiRawLoaderContent,
         mapper: TuiStringHandler<string> = identity,
-    ): Observable<TuiSafeHtml> {
-        return of(value).pipe(
-            switchMap(tuiRawLoad),
-            map(mapper),
-            switchMap(async markdown =>
-                marked
-                    .use({
-                        async: true,
-                        pedantic: false,
-                        gfm: true,
-                    })
-                    .parse(markdown),
+    ): TuiSafeHtml | null {
+        return this.getSignal(value, mapper)();
+    }
+
+    @tuiPure
+    private getSignal(
+        value: TuiRawLoaderContent,
+        mapper: TuiStringHandler<string>,
+    ): Signal<TuiSafeHtml | null> {
+        this.destroy$.next();
+
+        return untracked(() =>
+            toSignal(
+                of(value).pipe(
+                    switchMap(tuiRawLoad),
+                    map(mapper),
+                    switchMap(async markdown =>
+                        marked
+                            .use({
+                                async: true,
+                                pedantic: false,
+                                gfm: true,
+                            })
+                            .parse(markdown),
+                    ),
+                    takeUntil(this.destroy$),
+                ),
+                {injector: this.injector, initialValue: null},
             ),
         );
     }
