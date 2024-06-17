b4af834f6de1604f02ec9ec2325282d4b138425f
splincode <splincode@users.noreply.github.com>
2024-06-17 11:12:40 +0000

    deploy: 1a38155a49f95d03f887db2fd41aec909fc028e6

--- next/71556.40780e30241b48d4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[71556],{71556:n=>{n.exports="```ts\nimport {Component} from '@angular/core';\nimport {TuiCardLargeDirective} from '@taiga-ui/layout';\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiCardLargeDirective,\n  ],\n})\nexport class Component {}\n```\n"}}]);
