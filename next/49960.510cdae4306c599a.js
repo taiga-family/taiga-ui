4028b9f819c3194876bee7391f5ff53a88164497
splincode <splincode@users.noreply.github.com>
2024-06-17 15:45:53 +0000

    deploy: b60d1d8f7a1f3ad0f9e7043fcc78addf787eb3f6

--- next/49960.510cdae4306c599a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[49960],{49960:n=>{n.exports="```ts\nimport {TuiPullToRefreshComponent} from '@taiga-ui/addon-mobile';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiPullToRefreshComponent,\n  ],\n  // ...\n})\nexport class MyComponent {}\n```\n"}}]);
