1decf9b15a8d6f0076e398a638a271897f03430f
splincode <splincode@users.noreply.github.com>
2024-06-17 14:19:03 +0000

    deploy: 212903ef497cc355a71f70c2f28e452dcefbbb83

--- next/50278.896d092dae2985f5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[50278],{50278:n=>{n.exports="```ts\nimport {Component} from '@angular/core';\nimport {TuiIconComponent} from '@taiga-ui/core';\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiIconComponent,\n  ],\n})\nexport class MyComponent {}\n```\n"}}]);
