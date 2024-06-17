69ac7cfc7acd058d0babcd2592572a6608936792
splincode <splincode@users.noreply.github.com>
2024-06-17 07:44:16 +0000

    deploy: 090f1fdecd6592a8c001b09a1cce381b2a9e7a4e

--- next/48030.acc55af9cb92f775.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[48030],{48030:n=>{n.exports="@import '@taiga-ui/core/styles/taiga-ui-local';\n\n:host {\n    display: block;\n    width: max-content;\n}\n\n.margin {\n    margin: 4 * @space;\n}\n"}}]);
