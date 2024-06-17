91255b038b0cc751bfd69a055aa576f019607a1c
splincode <splincode@users.noreply.github.com>
2024-06-17 11:22:39 +0000

    deploy: 2ce4e487964240bb4da2ae3551ae5b5fde7247fd

--- next/7232.38cd38b41db4dbf3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[7232],{7232:n=>{n.exports="```ts\nimport {TuiSwipeDirective} from '@taiga-ui/cdk';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiSwipeDirective,\n  ],\n  // ...\n})\nexport class MyComponent {}\n```\n"}}]);
