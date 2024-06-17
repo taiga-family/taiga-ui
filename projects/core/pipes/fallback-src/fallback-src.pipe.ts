57e13476b8796fd07eae5a7c9b4bc14ce4c06ae2
Svyatoslav Zaytsev <sviatsv@yandex.ru>
2024-06-17 13:21:26 +0500

    feat(core): tuiFallbackSrc pipe sync

--- projects/core/pipes/fallback-src/fallback-src.pipe.ts
+++ projects/core/pipes/fallback-src/fallback-src.pipe.ts
@@ -1,23 +1,43 @@
-import type {PipeTransform} from '@angular/core';
-import {Pipe} from '@angular/core';
-import {tuiInjectElement} from '@taiga-ui/cdk';
-import type {Observable} from 'rxjs';
-import {fromEvent, map, merge, startWith} from 'rxjs';
+import type {PipeTransform, Signal} from '@angular/core';
+import {inject, INJECTOR, Pipe, untracked} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {tuiInjectElement, tuiPure} from '@taiga-ui/cdk';
+import {fromEvent, map, merge, startWith, Subject, takeUntil} from 'rxjs';
 
 @Pipe({
     standalone: true,
     name: 'tuiFallbackSrc',
+    pure: false,
 })
 export class TuiFallbackSrcPipe implements PipeTransform {
     private readonly el = tuiInjectElement();
+    private readonly destroy$ = new Subject<void>();
+    private readonly injector = inject(INJECTOR);
+    private readonly error$ = merge(
+        fromEvent(this.el, 'tui-icon-error'),
+        fromEvent(this.el, 'error', {capture: true}),
+    );
 
-    public transform(src: string, fallback: string): Observable<string> {
-        return merge(
-            fromEvent(this.el, 'tui-icon-error'),
-            fromEvent(this.el, 'error', {capture: true}),
-        ).pipe(
-            map(() => fallback),
-            startWith(src),
+    public transform(src: string, fallback: string): string {
+        return this.getSignal(src, fallback)();
+    }
+
+    @tuiPure
+    private getSignal(src: string, fallback: string): Signal<string> {
+        this.destroy$.next();
+
+        return untracked(() =>
+            toSignal(
+                this.error$.pipe(
+                    map(() => fallback),
+                    startWith(src),
+                    takeUntil(this.destroy$),
+                ),
+                {
+                    requireSync: true,
+                    injector: this.injector,
+                },
+            ),
         );
     }
 }
