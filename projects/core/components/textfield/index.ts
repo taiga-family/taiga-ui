9a9f71dd5b0365d14101c808fd6430190d6b426d
Maksim Ivanov <omaxphp@yandex.ru>
2024-06-18 19:03:51 +0300

    chore: drop comment (#7818)

--- projects/core/components/textfield/index.ts
+++ projects/core/components/textfield/index.ts
@@ -1,7 +1,5 @@
+export * from './select.directive';
 export * from './textfield';
 export * from './textfield.component';
 export * from './textfield.directive';
 export * from './textfield.options';
-
-// Keeping it manually at the bottom for proper build order
-export * from './select.directive';
