9da06e72d729e39d982dada477d5ce6a37268ea6
Vladimir Potekhin <vladimir.potekh@gmail.com>
2024-06-18 18:01:40 +0300

    chore: add filled icons

--- projects/icons/scripts/prepare-lucide-icons.ts
+++ projects/icons/scripts/prepare-lucide-icons.ts
@@ -6,14 +6,22 @@ const fs = require('node:fs');
     const dest = process.argv[2] || path.join(process.cwd(), 'projects', 'icons', 'src');
 
     fs.readdirSync(src).forEach((filename: string) => {
-        const content = fs.readFileSync(path.join(src, filename), 'utf-8');
-
-        fs.writeFileSync(
-            path.join(dest, filename),
-            content.replaceAll(
+        const content = fs
+            .readFileSync(path.join(src, filename), 'utf-8')
+            .replaceAll(
                 /<(circle|ellipse|line|polygon|polyline|path|rect)/g,
                 '<$1 vector-effect="non-scaling-stroke"',
-            ),
-        );
+            );
+
+        const filled = content.replaceAll('fill="none"', 'fill="currentColor"');
+
+        fs.writeFileSync(path.join(dest, filename), content);
+        fs.writeFileSync(path.join(dest, renameToFilled(filename)), filled);
     });
 })();
+
+function renameToFilled(filename: string): string {
+    const [name, extension] = filename.split('.');
+
+    return `${name}-filled.${extension}`;
+}
