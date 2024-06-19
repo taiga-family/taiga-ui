ce0ab7c394ab97dddf80853cd072148249b3eeba
splincode <splincode@users.noreply.github.com>
2024-06-19 13:56:05 +0000

    deploy: 33dc632fb60781481e2a61b9e6d3dbfa5e38cbcf

--- next/50398.beef9ac943b1e81b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[50398],{50398:s=>{s.exports='```html\n<tui-svg [src]="src"></tui-svg>\n<tui-svg src="iconName"></tui-svg>\n<tui-svg src="https://angular-colored-svg.stackblitz.io/assets/ng-girls.svg#ng-girls"></tui-svg>\n```\n'}}]);
