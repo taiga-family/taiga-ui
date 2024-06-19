b20f04fd2e99a3331317881938c86f217d7ef74d
Maksim Ivanov <omaxphp@yandex.ru>
2024-06-19 17:32:13 +0300

    refactor: drop action bar module (#7841)

---
+++ projects/kit/components/action-bar/action-bar.ts
@@ -0,0 +1,4 @@
+import {TuiActionBarComponent} from './action-bar.component';
+import {TuiActionBarDirective} from './action-bar.directive';
+
+export const TuiActionBar = [TuiActionBarComponent, TuiActionBarDirective] as const;
