395fbdc385035f7778da427c57c78ae2a0709c84
splincode <omaxphp@yandex.ru>
2024-06-19 09:39:35 +0000

    Update snapshots/demo/next/main to output generated at 45c8dde

---
+++ 9654.e6a16b6b7d09857c.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[9654],{9654:n=>{n.exports="```ts\nimport {TuiItem} from '@taiga-ui/cdk';\nimport {TuiCarouselComponent} from '@taiga-ui/kit';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiItem,\n    TuiCarouselComponent,\n  ],\n  // ...\n})\nexport class MyComponent {}\n```\n"}}]);
