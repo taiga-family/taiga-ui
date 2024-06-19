7e9a4d1d9f103e2e48003db7407744f120458096
splincode <omaxphp@yandex.ru>
2024-06-19 18:31:04 +0300

    refactor: drop TabBar module

--- projects/addon-mobile/components/tab-bar/index.ts
+++ projects/addon-mobile/components/tab-bar/index.ts
@@ -1,4 +1,4 @@
+export * from './tab-bar';
 export * from './tab-bar.component';
-export * from './tab-bar.module';
 export * from './tab-bar-item.component';
 export * from './tab-bar-item.directive';
