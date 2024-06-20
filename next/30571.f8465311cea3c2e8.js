e96b649933e970b090547527359891fc65ffb520
nsbarsukov <nsbarsukov@users.noreply.github.com>
2024-06-20 09:58:11 +0000

    deploy: d100a98e9d633e8aa9c6c064931770b6a702cfdc

---
+++ next/30571.f8465311cea3c2e8.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[30571],{30571:n=>{n.exports="```ts\nimport {Component} from '@angular/core';\nimport {TuiPin} from '@taiga-ui/kit';\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiPin,\n  ],\n})\nexport class Example {}\n```\n"}}]);
