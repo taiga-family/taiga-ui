33ffe895e299c4d0929705799c04b6d3cc4b72b2
Maksim Ivanov <omaxphp@yandex.ru>
2024-06-20 11:54:11 +0300

    refactor: drop TabBar module (#7858)

---
+++ projects/addon-mobile/components/tab-bar/tab-bar.ts
@@ -0,0 +1,9 @@
+import {TuiTabBarComponent} from './tab-bar.component';
+import {TuiTabBarItemComponent} from './tab-bar-item.component';
+import {TuiTabBarItemDirective} from './tab-bar-item.directive';
+
+export const TuiTabBar = [
+    TuiTabBarComponent,
+    TuiTabBarItemComponent,
+    TuiTabBarItemDirective,
+] as const;
