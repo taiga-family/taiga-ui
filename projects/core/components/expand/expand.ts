e86e12d0075b1fe750bb40263d774b0abfc242a8
splincode <omaxphp@yandex.ru>
2024-06-20 11:54:09 +0300

    refactor: drop expand module

---
+++ projects/core/components/expand/expand.ts
@@ -0,0 +1,4 @@
+import {TuiExpandComponent} from './expand.component';
+import {TuiExpandContentDirective} from './expand-content.directive';
+
+export const TuiExpand = [TuiExpandComponent, TuiExpandContentDirective] as const;
