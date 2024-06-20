120e1ef118860695642dcaf28ff699eff2aec3ad
splincode <splincode@users.noreply.github.com>
2024-06-20 15:37:14 +0000

    deploy: b05e1fd194ef3ad2442df4217746921dd122662f

--- next/88495.80fcd4d94011cc75.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[88495],{88495:n=>{n.exports="```ts\nimport {TuiNavigationModule} from '@taiga-ui/experimental';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiNavigationModule,\n  ],\n  // ...\n})\nexport class Example {}\n```\n"}}]);
