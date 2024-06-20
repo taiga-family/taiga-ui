33401c803ca3d3d921444c97858a3a1dec365444
Maksim Ivanov <omaxphp@yandex.ru>
2024-06-20 17:52:31 +0300

    refactor: drop app bar module (#7880)

--- projects/layout/components/app-bar/index.ts
+++ projects/layout/components/app-bar/index.ts
@@ -1,5 +1,5 @@
+export * from './app-bar';
 export * from './app-bar.component';
 export * from './app-bar.directive';
-export * from './app-bar.module';
 export * from './app-bar.providers';
 export * from './app-bar-back.component';
