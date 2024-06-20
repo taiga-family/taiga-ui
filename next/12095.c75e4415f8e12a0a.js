9f1775108b3ca3acddfc9d69a8cfa280666975ec
splincode <splincode@users.noreply.github.com>
2024-06-20 10:56:30 +0000

    deploy: b22f3ba1cc77e526471412bb48f6bc8aa8f15295

---
+++ next/12095.c75e4415f8e12a0a.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[12095],{12095:n=>{n.exports="```ts\nimport {Component} from '@angular/core';\nimport {TuiRadio} from '@taiga-ui/kit';\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiRadio,\n  ],\n})\nexport class Example {}\n```\n"}}]);
