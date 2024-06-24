02be2ccff6232995bb5a079e3c0a16690b48286e
Maksim Ivanov <omaxphp@yandex.ru>
2024-06-24 11:20:26 +0300

    chore: invalid test (#7911)

--- projects/cdk/schematics/ng-update/v4/tests/schematic-migrate-proprietary.spec.ts
+++ projects/cdk/schematics/ng-update/v4/tests/schematic-migrate-proprietary.spec.ts
@@ -2,6 +2,7 @@ import {join} from 'node:path';
 
 import {HostTree} from '@angular-devkit/schematics';
 import {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
+import {TUI_VERSION} from '@taiga-ui/cdk/constants/version';
 import {TAIGA_VERSION} from '@taiga-ui/cdk/schematics';
 import type {TuiSchema} from '@taiga-ui/cdk/schematics/ng-add/schema';
 import {
@@ -102,7 +103,7 @@ const PACKAGE_JSON_AFTER = `
 {
     "dependencies": {
         "@angular/core": "~13.0.0",
-        "@taiga-ui/proprietary": "3.59.0"
+        "@taiga-ui/proprietary": "${TUI_VERSION}"
     }
 }
 `.trim();
