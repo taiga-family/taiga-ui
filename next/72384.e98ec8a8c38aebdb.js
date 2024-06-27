a7e13b82f8e38f8d5587b656a34b6e903ed17174
splincode <splincode@users.noreply.github.com>
2024-06-27 20:26:16 +0000

    deploy: b1f192709cba4bb380e79395c081bb5836659fef

---
+++ next/72384.e98ec8a8c38aebdb.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[72384],{72384:n=>{n.exports="```ts\nimport {TuiTabs} from '@taiga-ui/kit';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiTabs,\n  ],\n  // ...\n})\nexport class Example {}\n```\n"}}]);
