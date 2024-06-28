0c542f6cfeda8c6941f56f4f91b8614fa794ae2b
waterplea <alexander@inkin.ru>
2024-06-28 12:21:36 +0500

    chore: fix

--- projects/cdk/schematics/ng-update/v3/constants/types.ts
+++ projects/cdk/schematics/ng-update/v3/constants/types.ts
@@ -124,7 +124,7 @@ export const TYPES_TO_RENAME: readonly ReplacementType[] = [
     },
     {
         from: 'ScrollIntoViewDirective',
-        to: 'tuiScrollIntoViewLink',
+        to: 'TuiScrollIntoViewLink',
         moduleSpecifier: ['@taiga-ui/addon-doc'],
     },
     {
