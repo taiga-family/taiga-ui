490cfd980518086716078c9bcd7411d1c1671bf0
waterplea <waterplea@users.noreply.github.com>
2024-06-28 13:27:26 +0000

    deploy: b16d87a6989c7114ef13c63a7e3dd8bd1d39bb1a

---
+++ next/13645.a78c1162d8664126.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[13645],{13645:n=>{n.exports=":host {\n    display: flex;\n    flex-direction: column;\n    gap: 2rem;\n}\n\n.flex {\n    display: flex;\n    gap: 0.5rem;\n\n    [tuiSubtitle] {\n        color: var(--tui-text-positive);\n    }\n}\n"}}]);
