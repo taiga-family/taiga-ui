0e5ddb2789a6994cbdbe4a2a9284ed4d41c20687
splincode <omaxphp@yandex.ru>
2024-07-01 17:03:12 +0300

    fix(styles): emoji should be visible on safari

--- projects/demo/src/modules/components/textarea/examples/1/index.ts
+++ projects/demo/src/modules/components/textarea/examples/1/index.ts
@@ -11,7 +11,7 @@ import {encapsulation} from '@demo/emulate/encapsulation';
 })
 export class TuiTextareaExample1 {
     testForm = new FormGroup({
-        testValue1: new FormControl('A field', Validators.required),
-        testValue2: new FormControl('This one can be expanded', Validators.required),
+        testValue1: new FormControl('A field 😅', Validators.required),
+        testValue2: new FormControl('This one can be expanded 😀', Validators.required),
     });
 }
