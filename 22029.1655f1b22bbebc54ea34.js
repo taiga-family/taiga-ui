ef64f399eb62951ef79846a61fab2b7cb47feb74
splincode <splincode@users.noreply.github.com>
2024-07-02 08:41:20 +0000

    deploy: 9b0ec9068386bceb7ca137eaf881e8a7f7d0af16

--- 22029.1655f1b22bbebc54ea34.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[22029],{22029:a=>{a.exports='<tui-calendar\n    [value]="value"\n    (dayClick)="onDayClick($event)"\n></tui-calendar>\n<div *ngIf="value">Chosen date: {{ value }}</div>\n'}}]);
---
+++ 44460.5645fc15701a57fd5873.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[44460],{44460:n=>{n.exports='```html\n<tui-action-bar *tuiActionBar="open">content</tui-action-bar>\n```\n'}}]);
