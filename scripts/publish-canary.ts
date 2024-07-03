e9f9627a39ac73bc0d7ba908ffe696368b436f32
Maksim Ivanov <omaxphp@yandex.ru>
2024-07-03 13:10:24 +0300

    ci: publish latest version as well (#8017)

--- scripts/publish-canary.ts
+++ scripts/publish-canary.ts
@@ -5,12 +5,13 @@ import {infoLog} from '../projects/cdk/schematics/utils/colored-log';
 import {execute} from './shared/execute';
 import {IGNORABLE_TAIGA_PACKAGES} from './shared/ignorable-packages';
 import {overwriteVersion} from './shared/overwrite-version';
+import {parseVersion} from './shared/parse-version';
 import {syncVersions} from './shared/sync-versions';
 
 (function main(): void {
     const type = 'canary';
     const commit = execute('git rev-parse HEAD', {}).slice(0, 7);
-    const [major, minor, patch] = version.split(/[.-]/) as [string, string, string];
+    const {major, minor, patch} = parseVersion(version);
 
     // construct new version from base version x.y.z to become x.y.z-{type}.{shortSha}
     const newVersion = `${major}.${minor}.${patch}-${type}.${commit}`;
