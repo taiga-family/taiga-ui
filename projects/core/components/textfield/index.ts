e974effa60924b6d5ba87594100dc5867b3b0465
waterplea <alexander@inkin.ru>
2024-07-04 12:14:41 +0500

    chore: fix

--- projects/core/components/textfield/index.ts
+++ projects/core/components/textfield/index.ts
@@ -1,6 +1,7 @@
-export * from './select.directive';
 export * from './textfield';
 export * from './textfield.base';
 export * from './textfield.component';
 export * from './textfield.directive';
 export * from './textfield.options';
+// Moving it down for order of compilation
+export * from './select.directive';
