f7ba5f54ef1f6b950ec8f548367951b131d7be5f
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-07-08 07:35:24 +0000

    Update snapshots/demo/next/v3.x to output generated at 5177a6c

--- 1540.656cba25b11874ef6d8f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[1540],{1540:a=>{a.exports="@import '@taiga-ui/core/styles/taiga-ui-local';\n\ntui-app-bar {\n    box-shadow: var(--tui-shadow-navigation);\n    width: 20rem;\n    margin-bottom: 1rem;\n}\n"}}]);
