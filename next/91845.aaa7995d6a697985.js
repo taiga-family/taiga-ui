5fc5f7aeced2d7cbc3763ba56587005786b13a10
waterplea <waterplea@users.noreply.github.com>
2024-07-08 10:25:12 +0000

    deploy: 432412ced91dfa3488d3a3dca460648758f49d0a

---
+++ next/91845.aaa7995d6a697985.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[91845],{91845:n=>{n.exports="```ts\nimport {Component} from '@angular/core';\nimport {TuiStatus} from '@taiga-ui/kit';\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiStatus,\n  ],\n})\nexport class Example {}\n```\n"}}]);
