8ce4d795798331e2a3a61d51a5141cfd8b53f36f
German Panov <german.panov.2018@mail.ru>
2024-07-11 12:21:46 +0300

    feat(core): rename `TuiAlertDirective` to `TuiAlert` (#8075)

--- projects/core/components/alert/alert.service.ts
+++ projects/core/components/alert/alert.service.ts
@@ -1,13 +1,13 @@
 import {inject, Injectable} from '@angular/core';
 import {TuiPopoverService} from '@taiga-ui/cdk/services';
 
-import {TuiAlert} from './alert.component';
+import {TuiAlertComponent} from './alert.component';
 import type {TuiAlertOptions} from './alert.interfaces';
 import {TUI_ALERT_OPTIONS, TUI_ALERTS} from './alert.tokens';
 
 @Injectable({
     providedIn: 'root',
     useFactory: () =>
-        new TuiAlertService(TUI_ALERTS, TuiAlert, inject(TUI_ALERT_OPTIONS)),
+        new TuiAlertService(TUI_ALERTS, TuiAlertComponent, inject(TUI_ALERT_OPTIONS)),
 })
 export class TuiAlertService extends TuiPopoverService<TuiAlertOptions<any>> {}
