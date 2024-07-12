8ee019360b59f2abfcf6007d950ae3db4b50cc57
waterplea <alexander@inkin.ru>
2024-07-12 12:31:42 +0500

    chore: fix

--- projects/addon-table/components/table/td/td.component.ts
+++ projects/addon-table/components/table/td/td.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, ContentChild} from '@angular/core';
-import {AbstractTuiControl, TuiControlDirective} from '@taiga-ui/cdk';
+import {AbstractTuiControl} from '@taiga-ui/cdk';
 
 @Component({
     selector: 'th[tuiTd], td[tuiTd]',
@@ -9,13 +9,10 @@ import {AbstractTuiControl, TuiControlDirective} from '@taiga-ui/cdk';
     styleUrls: ['./td.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
-        '[class._editable]': 'current || legacy',
+        '[class._editable]': 'legacy',
     },
 })
 export class TuiTdComponent {
-    @ContentChild(TuiControlDirective)
-    protected readonly current: unknown;
-
     @ContentChild(AbstractTuiControl)
     protected readonly legacy: unknown;
 }
