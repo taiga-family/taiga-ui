1d63a76935d1483b07cfb07029087c74ec24081e
Максим <maxbeere@yandex.ru>
2024-07-15 09:32:48 +0300

    fix(addon-mobile): display mobile calendar range (#8090)
    Co-authored-by: m.y.tikhomirov <m.y.tikhomirov@tinkoff.ru>

--- projects/addon-mobile/components/mobile-calendar/mobile-calendar.component.ts
+++ projects/addon-mobile/components/mobile-calendar/mobile-calendar.component.ts
@@ -273,7 +273,7 @@ export class TuiMobileCalendar implements AfterViewInit {
             return this.value?.[0]?.year ?? this.today.year;
         }
 
-        return this.value.from.year;
+        return this.value.to.year;
     }
 
     private get initialMonth(): number {
@@ -294,8 +294,7 @@ export class TuiMobileCalendar implements AfterViewInit {
         }
 
         return (
-            this.value.from.month +
-            (this.value.from.year - STARTING_YEAR) * MONTHS_IN_YEAR
+            this.value.to.month + (this.value.to.year - STARTING_YEAR) * MONTHS_IN_YEAR
         );
     }
 
