294c37299dbecfabbaebca66888804ea23b075c6
Vladimir Potekhin <vladimir.potekh@gmail.com>
2024-07-16 13:14:03 +0300

    chore: fix flaky e2e

--- projects/demo-playwright/tests/deep/deep-select.spec.ts
+++ projects/demo-playwright/tests/deep/deep-select.spec.ts
@@ -1,8 +1,14 @@
 import {TuiDocumentationApiPagePO, tuiGoto, tuiMockImages} from '@demo-playwright/utils';
 import {expect, test} from '@playwright/test';
 
+import {DemoRoute} from '../../../demo/src/modules/app/demo-routes';
+
+const DEEP_SELECT_FLAKY = new Set<string>([DemoRoute.Avatar]);
+
 test.describe('Deep / Select', () => {
-    const deepPaths: string[] = JSON.parse(process.env['DEMO_PATHS']!);
+    const deepPaths: string[] = JSON.parse(process.env['DEMO_PATHS']!).filter(
+        (path: string) => !DEEP_SELECT_FLAKY.has(path),
+    );
 
     deepPaths.forEach((path) =>
         test(path, async ({page}) => {
