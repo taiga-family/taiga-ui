15c6f83b745cdd75f1218ff9f7eca01046a026eb
waterplea <alexander@inkin.ru>
2024-07-17 11:55:48 +0500

    feat: add hints to filter

--- projects/demo/src/modules/components/filter/examples/4/index.ts
+++ projects/demo/src/modules/components/filter/examples/4/index.ts
@@ -3,10 +3,8 @@ import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {tuiPure} from '@taiga-ui/cdk';
 import {TuiButton} from '@taiga-ui/core';
 import {TuiFilterComponent} from '@taiga-ui/kit';
-import type {Observable} from 'rxjs';
 import {BehaviorSubject, map} from 'rxjs';
 
 const Department = {
@@ -34,24 +32,15 @@ const Department = {
 })
 export default class Example {
     protected readonly items = Object.values(Department);
-
     protected readonly filters$ = new BehaviorSubject<readonly string[]>([]);
 
-    @tuiPure
-    protected get model$(): Observable<readonly string[]> {
-        return this.filters$.pipe(
-            map((value) => (value.length === this.items.length ? [] : value)),
-        );
-    }
+    protected readonly checked$ = this.filters$.pipe(
+        map(({length}) => (length === this.items.length ? 'checked' : '')),
+    );
 
-    @tuiPure
-    protected get buttonAppearance$(): Observable<string> {
-        return this.filters$.pipe(
-            map((value) =>
-                value.length === this.items.length ? 'whiteblock-active' : 'whiteblock',
-            ),
-        );
-    }
+    protected readonly model$ = this.filters$.pipe(
+        map((value) => (value.length === this.items.length ? [] : value)),
+    );
 
     protected onModelChange(model: readonly string[]): void {
         this.filters$.next(model);
