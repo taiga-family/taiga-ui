d54fa358f5029540e14c9731ea5a5377b2d68c0a
waterplea <alexander@inkin.ru>
2024-07-17 10:22:19 +0500

    chore: comments

---
+++ projects/core/directives/hint/hint-overflow.directive.ts
@@ -0,0 +1,24 @@
+import {Directive, inject} from '@angular/core';
+
+import {TuiHintDirective} from './hint.directive';
+
+@Directive({
+    standalone: true,
+    selector: '[tuiHintOverflow]',
+    host: {
+        '(mouseenter)': 'onMouseEnter($event.currentTarget)',
+    },
+    hostDirectives: [
+        {
+            directive: TuiHintDirective,
+            inputs: ['tuiHintAppearance'],
+        },
+    ],
+})
+export class TuiHintOverfow {
+    private readonly hint = inject(TuiHintDirective);
+
+    protected onMouseEnter({scrollWidth, clientWidth, textContent}: Element): void {
+        this.hint.tuiHint = scrollWidth > clientWidth ? textContent : '';
+    }
+}
--- projects/core/directives/hint/hint.ts
+++ projects/core/directives/hint/hint.ts
@@ -1,6 +1,6 @@
 import {TuiHintComponent} from './hint.component';
 import {TuiHintDirective} from './hint.directive';
-import {TuiHintAuto} from './hint-auto.directive';
+import {TuiHintOverfow} from './hint-overflow.directive';
 import {TuiHintDescribe} from './hint-describe.directive';
 import {TuiHintDriver} from './hint-driver.directive';
 import {TuiHintHost} from './hint-host.directive';
@@ -19,7 +19,7 @@ export const TuiHint = [
     TuiHintDriver,
     TuiHintPosition,
     TuiHintHover,
-    TuiHintAuto,
+    TuiHintOverfow,
     TuiHintDescribe,
     TuiHintHost,
     TuiHintManual,
