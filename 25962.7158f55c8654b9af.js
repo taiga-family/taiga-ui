46be187ecaed5524ed4a85876859e2ca1a09259d
waterplea <alexander@inkin.ru>
2024-07-17 06:48:25 +0000

    Update snapshots/demo/next/main to output generated at 9f25d6e

---
+++ 25962.7158f55c8654b9af.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[25962],{25962:e=>{e.exports="@import '@taiga-ui/core/styles/taiga-ui-local';\n\ndiv {\n    .text-overflow();\n    resize: horizontal;\n}\n"}}]);
