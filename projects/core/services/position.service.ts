1bbee15f1768ba479242832a36dcf2e26e30d331
splincode <omaxphp@yandex.ru>
2024-07-19 13:22:05 +0300

    feat(core): save battery user is not currently interacting with the page

--- projects/core/services/position.service.ts
+++ projects/core/services/position.service.ts
@@ -1,11 +1,21 @@
 import {inject, Injectable, NgZone} from '@angular/core';
-import {ANIMATION_FRAME} from '@ng-web-apis/common';
+import {ANIMATION_FRAME, NAVIGATOR} from '@ng-web-apis/common';
 import {EMPTY_CLIENT_RECT} from '@taiga-ui/cdk/constants';
 import {tuiZonefree} from '@taiga-ui/cdk/observables';
 import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
 import {TuiPositionAccessor} from '@taiga-ui/core/classes';
 import type {TuiPoint} from '@taiga-ui/core/types';
-import {finalize, map, Observable, startWith} from 'rxjs';
+import {filter, finalize, map, Observable, startWith} from 'rxjs';
+
+/**
+ * https://html.spec.whatwg.org/multipage/interaction.html#the-useractivation-interface
+ */
+interface UserActivation {
+    readonly hasBeenActive: boolean;
+    readonly isActive: boolean;
+}
+
+type ESNextNavigator = Navigator & {userActivation?: UserActivation};
 
 @Injectable()
 export class TuiPositionService extends Observable<TuiPoint> {
@@ -13,6 +23,7 @@ export class TuiPositionService extends Observable<TuiPoint> {
     private readonly accessor = inject(TuiPositionAccessor);
 
     constructor() {
+        const navigator = inject<ESNextNavigator>(NAVIGATOR);
         const animationFrame$ = inject(ANIMATION_FRAME);
         const zone = inject(NgZone);
 
@@ -20,6 +31,7 @@ export class TuiPositionService extends Observable<TuiPoint> {
             animationFrame$
                 .pipe(
                     startWith(null),
+                    filter(() => navigator?.userActivation?.isActive ?? true),
                     map(() => this.accessor.getPosition(this.el.getBoundingClientRect())),
                     tuiZonefree(zone),
                     finalize(() => this.accessor.getPosition(EMPTY_CLIENT_RECT)),
