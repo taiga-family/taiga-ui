f40d48df7f8de2505b4ac22484edfb0c5a24cd07
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-07-23 09:58:35 +0300

    chore(demo): remove some outdated ignorings of SSR errors (#8171)

--- projects/demo/src/modules/app/server-error-handler.ts
+++ projects/demo/src/modules/app/server-error-handler.ts
@@ -7,15 +7,6 @@ import {errorLog} from '@taiga-ui/cdk/schematics/utils/colored-log';
 // TODO
 const KNOWN_ISSUES: string[] = [
     'requestAnimationFrame is not defined', // hljs
-    /**
-     * TODO: drop it after update to @ng-web-apis/resize-observer@4.0.0
-     * @see https://github.com/taiga-family/ng-web-apis/pull/350
-     */
-    'ResizeObserver is not supported in your browser',
-    // TODO: drop all `Failed to parse URL from` errors after deletion of tui-svg component
-    'TypeError: Failed to parse URL from assets/icons/android.svg',
-    'TypeError: Failed to parse URL from assets/icons/ios.svg',
-    'TypeError: Failed to parse URL from assets/icons/polygon.svg',
 ];
 
 @Injectable()
