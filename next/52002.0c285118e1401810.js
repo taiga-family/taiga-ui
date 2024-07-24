5dc5853b3d8db222b4449785f91f9f9f4f793130
splincode <splincode@users.noreply.github.com>
2024-07-24 11:05:45 +0000

    deploy: a6e8d76c5b6b27716c2009ea67d757e7d8c2ac94

--- next/52002.0c285118e1401810.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[52002],{52002:e=>{e.exports="<p>\n    A token with a factory. It takes USER_AGENT token and parses it with a complex Regex to detect users with mobile\n    devices\n</p>\n"}}]);
