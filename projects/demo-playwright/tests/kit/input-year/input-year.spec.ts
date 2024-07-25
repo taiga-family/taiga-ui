9c8cc789227c6d23f3b6fff50dcc148c12290f2b
splincode <omaxphp@yandex.ru>
2024-07-25 10:37:53 +0300

    refactor: use `pressSequentially`

--- projects/demo-playwright/tests/kit/input-year/input-year.spec.ts
+++ projects/demo-playwright/tests/kit/input-year/input-year.spec.ts
@@ -18,25 +18,24 @@ test.describe('InputYear', () => {
         });
 
         test('12345 => 1234', async ({page}) => {
-            await input.fill('123456789');
-            await page.waitForTimeout(300);
+            await input.pressSequentially('123456789');
             await expect(page).toHaveScreenshot('01-input-year.png');
         });
 
         test('2040 => 2020', async ({page}) => {
-            await input.fill('2040');
+            await input.pressSequentially('2040');
             await expect(page).toHaveScreenshot('02-input-year.png');
         });
 
         test('0000 => blur => 0', async ({page}) => {
-            await input.fill('0000');
+            await input.pressSequentially('0000');
             await expect(page).toHaveScreenshot('03-input-year.png');
             await input.blur();
             await expect(page).toHaveScreenshot('04-input-year.png');
         });
 
         test('0040 => blur => 40', async ({page}) => {
-            await input.fill('0040');
+            await input.pressSequentially('0040');
             await expect(page).toHaveScreenshot('05-input-year.png');
             await input.blur();
             await expect(page).toHaveScreenshot('06-input-year.png');
@@ -54,7 +53,7 @@ test.describe('InputYear', () => {
         });
 
         test('can not be less than min', async ({page}) => {
-            await input.fill('2005');
+            await input.pressSequentially('2005');
             await expect(page).toHaveScreenshot('07-input-year.png');
             await input.blur();
             await expect(page).toHaveScreenshot('08-input-year.png');
@@ -88,7 +87,7 @@ test.describe('InputYear', () => {
         test('type 2020 => select new date via calendar => new date inside text field', async ({
             page,
         }) => {
-            await input.fill('2020');
+            await input.pressSequentially('2020');
 
             const cell = page.locator(
                 '[automation-id="tui-calendar-year__cell"]:has-text("2030")',
