98fbebf47c19d47286823ee3ae3256d088f5db95
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-07-30 19:24:14 +0300

    chore(schematics): fix migration for `tui-space` style mixin (it can be used with negative values)

--- projects/cdk/schematics/ng-update/v4/steps/styles/migrate-space-mixins.ts
+++ projects/cdk/schematics/ng-update/v4/steps/styles/migrate-space-mixins.ts
@@ -12,14 +12,14 @@ const SPACE = 0.25;
 
 // .tui-space(top, 4);
 // @include tui-space(top, 4);
-const LESS_MIXIN_RE = new RegExp(
-    `(?:@include\\s|\\.)tui-space\\((?<direction>${Object.keys(DIRECTION_MAPPING).join('|')}),\\s?(?<size>\\d+)\\);`,
+const MIXIN_RE = new RegExp(
+    `(?:@include\\s|\\.)tui-space\\((?<direction>${Object.keys(DIRECTION_MAPPING).join('|')}),\\s?(?<size>-?\\d+)\\);`,
     'g',
 );
 
 export function migrateSpaceMixins(fileContent: string): string {
     return fileContent.replaceAll(
-        LESS_MIXIN_RE,
+        MIXIN_RE,
         (_, direction: keyof typeof DIRECTION_MAPPING, size: string) =>
             `${DIRECTION_MAPPING[direction].map((property) => `${property}: ${Number(size) * SPACE}rem;`).join('\n')}`,
     );
