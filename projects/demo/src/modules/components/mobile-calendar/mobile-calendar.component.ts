8afc477d3383f91490efaef7ec05e3dd129a7c95
Svyatoslav Zaytsev <143048525+MillerSvt@users.noreply.github.com>
2024-07-31 14:45:10 +0500

    feat(addon-mobile): possibility to hide header of `MobileCalendar` (#8264)

--- projects/demo/src/modules/components/mobile-calendar/mobile-calendar.component.ts
+++ projects/demo/src/modules/components/mobile-calendar/mobile-calendar.component.ts
@@ -64,6 +64,12 @@ export class ExampleTuiMobileCalendarComponent {
         LESS: import('./examples/5/index.less?raw'),
     };
 
+    readonly example6: TuiDocExample = {
+        TypeScript: import('./examples/6/index.ts?raw'),
+        HTML: import('./examples/6/index.html?raw'),
+        LESS: import('./examples/6/index.less?raw'),
+    };
+
     minVariants = [TUI_FIRST_DAY, new TuiDay(2017, 2, 5), new TuiDay(1900, 0, 1)];
 
     min = this.minVariants[0];
