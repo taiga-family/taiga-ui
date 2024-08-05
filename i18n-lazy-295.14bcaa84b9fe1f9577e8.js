a21cd2aaa535d3ce03d716c3412d7d5d53712ccc
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-08-05 11:03:14 +0000

    Update snapshots/demo/next/v3.x to output generated at 1e1c574

--- i18n-lazy-295.14bcaa84b9fe1f9577e8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[33524],{31847:(e,a,n)=>{n.r(a),n.d(a,{TUI_UKRAINIAN_LANGUAGE_ADDON_TABLE:()=>s});const s={showHideText:"Показати/Приховати",paginationTexts:{pages:"Сторінок",linesPerPage:"Рядків на сторінку",of:"з"}}}}]);
