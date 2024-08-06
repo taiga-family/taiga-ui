eb3b9022b9175916b43220f0686c9bed97090737
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-08-06 10:41:55 +0300

    chore(kit): rename `TuiRatingComponent` => `TuiRating`

--- projects/demo/src/modules/components/rating/examples/1/index.ts
+++ projects/demo/src/modules/components/rating/examples/1/index.ts
@@ -2,11 +2,11 @@ import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiRatingComponent} from '@taiga-ui/kit';
+import {TuiRating} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
-    imports: [TuiRatingComponent, FormsModule],
+    imports: [TuiRating, FormsModule],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation,
