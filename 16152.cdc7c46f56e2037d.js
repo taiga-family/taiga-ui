7b856de6ff151ba7a34558763c0631af554d2ae2
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-08-07 12:07:45 +0000

    Update snapshots/demo/next/main to output generated at d2f74e3

---
+++ 16152.cdc7c46f56e2037d.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[16152],{16152:n=>{n.exports='<tui-input-time\n    [formControl]="control"\n    [items]="items"\n>\n    Input time\n</tui-input-time>\n\n<p>Stringified control value:</p>\n<p>\n    <code>{{ control.value }}</code>\n</p>\n'}}]);
