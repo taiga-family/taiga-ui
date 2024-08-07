6f1a4d7276eb367dcd70a8df7b0e42ef6ed50c70
waterplea <waterplea@users.noreply.github.com>
2024-08-07 14:25:53 +0000

    deploy: abdc8cfaedbdf591e143ba4a3f3b46c5fed6de8e

---
+++ next/50748.fd802a7869ce23cf.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[50748],{50748:e=>{e.exports=":host {\n    display: flex;\n    flex-direction: column;\n    max-width: 20rem;\n    gap: 1rem;\n}\n"}}]);
---
+++ next/62976.60533dac0465a301.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[62976],{87352:a=>{a.exports='```html\n<tui-tag [value]="tag"></tui-tag>\n\x3c!--Use can use tag as link--\x3e\n<a\n  tuiTag\n  href="https://somesite.com"\n  target="_blank"\n  [value]="tag"\n></a>\n```\n'}}]);
