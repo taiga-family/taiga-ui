9d9640687c866fec1d6ca8592069eb3432de27b1
Maksim Ivanov <omaxphp@yandex.ru>
2024-08-07 17:14:32 +0300

    refactor: drop old schematics (#8385)

--- projects/cdk/schematics/ng-update/v3-35/constants/constants.ts
+++
@@ -1,7 +0,0 @@
-import type {ReplacementIdentifier} from '../../interfaces/replacement-identifier';
-import {ICONS} from './icons';
-
-export const ICONS_TS: ReplacementIdentifier[] = ICONS.map(({from, to}) => ({
-    from: {name: from, moduleSpecifier: '@taiga-ui/proprietary-icons'},
-    to: {name: to, moduleSpecifier: '@taiga-ui/proprietary-tds-icons'},
-}));
