e6efc3f961bae3f2f8a49d0889d831332063fc68
splincode <omaxphp@yandex.ru>
2024-08-08 15:40:30 +0300

    feat(i18n): add japanese

---
+++ projects/i18n/languages/japan/addon-commerce.ts
@@ -0,0 +1,6 @@
+import type {TuiLanguageCommerce} from '@taiga-ui/i18n/types';
+
+export const TUI_JAPAN_LANGUAGE_ADDON_COMMERCE: TuiLanguageCommerce = {
+    cardNumber: ['番号', 'カード番号'],
+    cardExpiry: ['期限切れ', '有効期間'],
+};
