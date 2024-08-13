6a965ed61ab8f80d7520bf24ca6214aa0693c575
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-08-13 18:16:44 +0300

    chore(demo): `DocDemo` generates invalid urls during attempts to patch query params (#8482)

--- projects/addon-doc/tokens/url-state-handler.ts
+++ projects/addon-doc/tokens/url-state-handler.ts
@@ -2,5 +2,27 @@ import type {UrlTree} from '@angular/router';
 import type {TuiStringHandler} from '@taiga-ui/cdk/types';
 import {tuiCreateToken} from '@taiga-ui/cdk/utils/miscellaneous';
 
+/**
+ * TODO: delete it in 5.0
+ * Replace the following approach to append query params
+ * ```ts
+ * urlStateHandler = inject(TUI_DOC_URL_STATE_HANDLER);
+ * locationRef = inject(Location);
+ *
+ * this.locationRef.go(this.urlStateHandler(tree));
+ * ```
+ * with this one:
+ * ```ts
+ * router = inject(Router);
+ * activatedRoute = inject(ActivatedRoute);
+ *
+ * void this.router.navigate([], {
+ *     relativeTo: this.activatedRoute,
+ *     queryParams: {sandboxWidth: 300},
+ *     queryParamsHandling: 'merge',
+ * });
+ * ```
+ * It is more robust to the cases when application has base href.
+ */
 export const TUI_DOC_URL_STATE_HANDLER =
     tuiCreateToken<TuiStringHandler<UrlTree>>(String);
