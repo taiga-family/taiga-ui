691328f889c95346f18ced853fe2b91bd08e300f
Maksim Ivanov <omaxphp@yandex.ru>
2024-08-13 12:29:39 +0300

    feat(i18n): add korean (#8486)

---
+++ projects/i18n/languages/korean/addon-commerce.ts
@@ -0,0 +1,6 @@
+import type {TuiLanguageCommerce} from '@taiga-ui/i18n/types';
+
+export const TUI_KOREAN_LANGUAGE_ADDON_COMMERCE: TuiLanguageCommerce = {
+    cardNumber: ['숫자', '카드번호'],
+    cardExpiry: ['만료', '유효 기간:'],
+};
