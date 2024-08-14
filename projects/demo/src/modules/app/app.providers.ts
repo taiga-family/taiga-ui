9ad873b47441c5bb251a2515eedbcc85acbdf853
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-08-14 21:11:59 +0300

    chore(demo): use `baseHref=/` instead of `baseHref=./` (#8526)

--- projects/demo/src/modules/app/app.providers.ts
+++ projects/demo/src/modules/app/app.providers.ts
@@ -60,7 +60,8 @@ export const APP_PROVIDERS: Provider[] = [
     {
         provide: APP_BASE_HREF,
         // @note: By default, on webcontainer.io will not be provided APP_BASE_HREF, we use fallback
-        useFactory: () => inject(DOCUMENT).querySelector('base')?.href || '/',
+        useFactory: () =>
+            inject(DOCUMENT).querySelector('base')?.getAttribute('href') || '/',
     },
     {
         provide: TUI_IS_PLAYWRIGHT,
