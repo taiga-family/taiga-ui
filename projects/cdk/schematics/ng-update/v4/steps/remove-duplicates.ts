ac13cc63c3b0ea931b9f474078256d4de0d6d3cc
Vladimir Potekhin <vladimir.potekh@gmail.com>
2024-08-15 15:29:52 +0300

    chore: remove duplicates after migration

---
+++ projects/cdk/schematics/ng-update/v4/steps/remove-duplicates.ts
@@ -0,0 +1,56 @@
+import {getClasses, getDecorators, Node} from 'ng-morph';
+
+import {ALL_TS_FILES} from '../../../constants';
+import type {TuiSchema} from '../../../ng-add/schema';
+import {
+    infoLog,
+    REPLACE_SYMBOL,
+    SMALL_TAB_SYMBOL,
+    SUCCESS_SYMBOL,
+    successLog,
+} from '../../../utils/colored-log';
+
+export function removeDuplicates(options: TuiSchema): void {
+    !options['skip-logs'] &&
+        infoLog(`${SMALL_TAB_SYMBOL}${REPLACE_SYMBOL} removing duplicates...`);
+
+    const classes = getClasses(ALL_TS_FILES);
+    const modules = getDecorators(classes, {name: 'NgModule'});
+    const components = getDecorators(classes, {name: 'Component'});
+
+    [...modules, ...components].forEach((decorator) => {
+        const [metadata] = decorator.getArguments();
+
+        if (!metadata || !Node.isObjectLiteralExpression(metadata)) {
+            return;
+        }
+
+        const property = metadata.getProperty('imports');
+
+        if (!Node.isPropertyAssignment(property)) {
+            return;
+        }
+
+        const importsInitializer = property.getInitializer();
+
+        if (!importsInitializer || !Node.isArrayLiteralExpression(importsInitializer)) {
+            return;
+        }
+
+        const indexToRemove: number[] = [];
+        const existingImports = new Set();
+
+        importsInitializer.getElements().forEach((el, index) => {
+            if (existingImports.has(el.getText())) {
+                indexToRemove.push(index);
+            } else {
+                existingImports.add(el.getText());
+            }
+        });
+
+        indexToRemove.forEach((index) => importsInitializer.removeElement(index));
+    });
+
+    !options['skip-logs'] &&
+        successLog(`${SMALL_TAB_SYMBOL}${SUCCESS_SYMBOL} duplicates removed \n`);
+}
