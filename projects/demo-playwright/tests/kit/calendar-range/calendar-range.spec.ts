f7eef8f18673571332dd7d097318d27de7f0483e
Demyan <dyo171717@gmail.com>
2024-08-21 16:29:31 +0300

    fix(kit): delete useless screenshots and some fixes in test

--- projects/demo-playwright/tests/kit/calendar-range/calendar-range.spec.ts
+++ projects/demo-playwright/tests/kit/calendar-range/calendar-range.spec.ts
@@ -12,15 +12,18 @@ test.describe('CalendarRange', () => {
     let documentationPage!: TuiDocumentationPagePO;
 
     test.beforeEach(async ({page}) => {
-        await tuiGoto(page, DemoRoute.CalendarRange, {date: new Date(2024, 9, 31)});
+        await tuiGoto(page, DemoRoute.CalendarRange, {
+            date: new Date(2020, 8, 25),
+        });
 
         documentationPage = new TuiDocumentationPagePO(page);
-        example = documentationPage.getExample('#with-another-range-switcher');
-        calendarRange = new TuiCalendarRangePO(example.locator('tui-calendar-range'));
     });
 
     test.describe('Examples', () => {
         test('With another range switcher', async () => {
+            example = documentationPage.getExample('#with-another-range-switcher');
+            calendarRange = new TuiCalendarRangePO(example.locator('tui-calendar-range'));
+
             const getRangeSwitcher = (): Locator =>
                 example.locator('p button[data-appearance="link"]');
 
@@ -35,19 +38,19 @@ test.describe('CalendarRange', () => {
                 '05-calendar-range-correct-display-range-switcher-after-select-week.png',
             );
 
-            getRangeSwitcher().click();
+            await getRangeSwitcher().click();
 
             await expect(example).toHaveScreenshot(
                 '05-calendar-range-correct-display-items-and-values-after-click-on-month-range-switcher.png',
             );
 
-            getRangeSwitcher().click();
+            await getRangeSwitcher().click();
 
             await expect(example).toHaveScreenshot(
                 '05-calendar-range-correct-display-items-and-values-after-click-on-quarter-range-switcher.png',
             );
 
-            getRangeSwitcher().click();
+            await getRangeSwitcher().click();
 
             await expect(example).toHaveScreenshot(
                 '05-calendar-range-correct-display-defaults-items-and-values.png',
