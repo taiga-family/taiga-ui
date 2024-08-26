fe1260c6b3372bd9b86193bbe9c7754876b20056
waterplea <alexander@inkin.ru>
2024-08-26 12:56:23 +0000

    Update snapshots/demo/next/main to output generated at 7287016

---
+++ 93587.3a645750516ae30f.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[93587],{93587:e=>{e.exports="p {\n    display: flex;\n    gap: 1rem;\n}\n\ncode {\n    white-space: nowrap !important;\n}\n"}}]);
