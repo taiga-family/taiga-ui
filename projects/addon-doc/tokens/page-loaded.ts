5c3abad75d32f0e062047d8a8c082f3806eb59f3
splincode <omaxphp@yandex.ru>
2024-09-08 17:53:07 +0300

    refactor: add `tuiZonefreeScheduler` to `TUI_DOC_PAGE_LOADED`

--- projects/addon-doc/tokens/page-loaded.ts
+++ projects/addon-doc/tokens/page-loaded.ts
@@ -1,8 +1,9 @@
+import {tuiZonefreeScheduler} from '@taiga-ui/cdk/observables';
 import {tuiCreateToken} from '@taiga-ui/cdk/utils/miscellaneous';
 import {defer, of, switchMap, timer} from 'rxjs';
 /**
  * Stream that emits if loading of page is over (for example, to begin scrollIntoView)
  */
 export const TUI_DOC_PAGE_LOADED = tuiCreateToken(
-    defer(() => timer(200).pipe(switchMap(() => of(true)))),
+    defer(() => timer(200, tuiZonefreeScheduler()).pipe(switchMap(() => of(true)))),
 );
