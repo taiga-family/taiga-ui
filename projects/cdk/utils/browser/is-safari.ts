ae8718b03f3ded65436c40150d9d3b634d91d0c4
splincode <omaxphp@yandex.ru>
2024-09-10 15:03:43 +0300

    refactor: support `no-typeof-undefined`

--- projects/cdk/utils/browser/is-safari.ts
+++ projects/cdk/utils/browser/is-safari.ts
@@ -2,7 +2,7 @@ export function tuiIsSafari({ownerDocument: doc}: Element): boolean {
     const win = doc?.defaultView as unknown as Window & {safari?: any};
 
     const isMacOsSafari =
-        typeof win.safari !== 'undefined' &&
+        win.safari !== undefined &&
         win.safari?.pushNotification?.toString() === '[object SafariRemoteNotification]';
 
     const isIosSafari =
