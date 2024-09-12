3859231ae98fee09ff09921d6406204c46959c81
Maksim Ivanov <omaxphp@yandex.ru>
2024-09-12 18:13:56 +0300

    chore(demo): update data-list api example (#8869)

--- projects/demo/src/modules/components/data-list/index.ts
+++ projects/demo/src/modules/components/data-list/index.ts
@@ -1,14 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
+import {TUI_IS_E2E} from '@taiga-ui/cdk';
+import {TuiDataList, type TuiSizeL, type TuiSizeS} from '@taiga-ui/core';
+import {delay, of} from 'rxjs';
 
 @Component({
     standalone: true,
-    imports: [TuiDemo],
+    imports: [TuiDataList, TuiDemo],
     templateUrl: './index.html',
     changeDetection,
 })
 export default class Page {
+    private readonly items = ['Foo', 'Bar', 'Baz'];
+    protected readonly items$ = inject(TUI_IS_E2E)
+        ? of(this.items)
+        : of(this.items).pipe(delay(1e3));
+
+    protected readonly emptyContentVariants = ['Loading...', ''];
+    protected emptyContent = this.emptyContentVariants[0];
+
+    protected readonly sizeVariants: ReadonlyArray<TuiSizeL | TuiSizeS> = ['s', 'm', 'l'];
+    protected size = this.sizeVariants[0]!;
+
     protected readonly example4 = {
         TypeScript: import('./examples/4/index.ts?raw'),
         HTML: import('./examples/4/index.html?raw'),
