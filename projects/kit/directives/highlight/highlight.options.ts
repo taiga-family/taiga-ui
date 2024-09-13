7ac086a4f359245f3ea3f7211c2fe6cd56c0566c
Alexander <valverin95@gmail.com>
2024-09-13 14:38:41 +0300

    feat(kit): use directly { highlightColor: 'var(--tui-service-selection-background)', }

--- projects/kit/directives/highlight/highlight.options.ts
+++ projects/kit/directives/highlight/highlight.options.ts
@@ -1,14 +1,10 @@
 import {tuiCreateOptions} from '@taiga-ui/cdk/utils/di';
 
-const DEFAULT = {
-    highlightColor: 'var(--tui-service-selection-background)',
-} as const;
-
 export interface TuiHighlightOptions {
     readonly highlightColor: string;
 }
 
 export const [TUI_HIGHLIGHT_OPTIONS, tuiHighlightOptionsProvider] =
     tuiCreateOptions<TuiHighlightOptions>({
-        highlightColor: DEFAULT.highlightColor,
+        highlightColor: 'var(--tui-service-selection-background)',
     });
