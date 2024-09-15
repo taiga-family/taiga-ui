b15a38aca18bacd9ec708feee72c2609573b9a1a
Alexander <valverin95@gmail.com>
2024-09-15 03:43:53 +0300

    feat(kit): drop TuiHighlightOptions interface

--- projects/kit/directives/highlight/highlight.options.ts
+++ projects/kit/directives/highlight/highlight.options.ts
@@ -1,10 +1,5 @@
 import {tuiCreateOptions} from '@taiga-ui/cdk/utils/di';
 
-export interface TuiHighlightOptions {
-    readonly highlightColor: string;
-}
-
-export const [TUI_HIGHLIGHT_OPTIONS, tuiHighlightOptionsProvider] =
-    tuiCreateOptions<TuiHighlightOptions>({
-        highlightColor: 'var(--tui-service-selection-background)',
-    } as const);
+export const [TUI_HIGHLIGHT_OPTIONS, tuiHighlightOptionsProvider] = tuiCreateOptions({
+    highlightColor: 'var(--tui-service-selection-background)',
+} as const);
