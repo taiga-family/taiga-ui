601986355e64b21a232fe42a034be7810492b031
Alex Inkin <alexander@inkin.ru>
2024-09-17 16:56:18 +0400

    fix(layout): `AppBar` use proper appearance for back button (#9061)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/layout/components/app-bar/app-bar-back.component.ts
+++ projects/layout/components/app-bar/app-bar-back.component.ts
@@ -1,4 +1,9 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import type {TuiAppearanceOptions} from '@taiga-ui/core/directives/appearance';
+import {
+    TuiAppearance,
+    tuiAppearanceOptionsProvider,
+} from '@taiga-ui/core/directives/appearance';
 
 @Component({
     standalone: true,
@@ -6,5 +11,9 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
     templateUrl: './app-bar-back.template.html',
     styleUrls: ['./app-bar-back.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [tuiAppearanceOptionsProvider(TuiAppBarBack)],
+    hostDirectives: [TuiAppearance],
 })
-export class TuiAppBarBack {}
+export class TuiAppBarBack implements TuiAppearanceOptions {
+    public appearance = 'link';
+}
