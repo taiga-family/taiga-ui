83300f1c9552310b8a3ddc142a09d16829d8eaa1
Pavel Ivannikov <pavel.a.ivannikov@gmail.com>
2024-09-19 17:28:22 +0300

    fix(layout): `CardLarge` fix style leaking into `Badge` (#9097)

--- projects/demo/src/modules/components/card-large/examples/1/index.ts
+++ projects/demo/src/modules/components/card-large/examples/1/index.ts
@@ -3,11 +3,13 @@ import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiPlatform} from '@taiga-ui/cdk';
 import {TuiButton, TuiLink, TuiSurface, TuiTitle} from '@taiga-ui/core';
+import {TuiBadge} from '@taiga-ui/kit';
 import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
 
 @Component({
     standalone: true,
     imports: [
+        TuiBadge,
         TuiButton,
         TuiCardLarge,
         TuiHeader,
