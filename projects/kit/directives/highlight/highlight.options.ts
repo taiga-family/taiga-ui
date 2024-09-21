9156cb0cc6e9f28663c53fc04e064a5a2fb6e355
Alexander <valverin95@gmail.com>
2024-09-21 19:17:06 +0300

    feat(kit): review fixes

--- projects/kit/directives/highlight/highlight.options.ts
+++
@@ -1,5 +0,0 @@
-import {tuiCreateOptions} from '@taiga-ui/cdk/utils/di';
-
-export const [TUI_HIGHLIGHT_OPTIONS, tuiHighlightOptionsProvider] = tuiCreateOptions({
-    highlightColor: 'var(--tui-service-selection-background)',
-} as const);
