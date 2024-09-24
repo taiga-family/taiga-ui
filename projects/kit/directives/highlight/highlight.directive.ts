21a80c73ad750c6c82196405a047c2888833c4ce
Alexandr <42899726+al-march@users.noreply.github.com>
2024-09-24 12:39:18 +0300

    feat(kit): add an options token for customizing TuiHighlight (#8812)

--- projects/kit/directives/highlight/highlight.directive.ts
+++ projects/kit/directives/highlight/highlight.directive.ts
@@ -6,6 +6,11 @@ import {ResizeObserverService} from '@ng-web-apis/resize-observer';
 import {svgNodeFilter} from '@taiga-ui/cdk/constants';
 import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
 import {tuiPx} from '@taiga-ui/cdk/utils/miscellaneous';
+import {tuiCreateOptions} from '@taiga-ui/cdk/utils/di';
+
+export const [TUI_HIGHLIGHT_OPTIONS, tuiHighlightOptionsProvider] = tuiCreateOptions({
+    highlightColor: 'var(--tui-service-selection-background)',
+});
 
 @Directive({
     standalone: true,
@@ -20,6 +25,7 @@ export class TuiHighlight implements OnChanges {
     private readonly el = tuiInjectElement();
     private readonly renderer = inject(Renderer2);
     private readonly doc = inject(DOCUMENT);
+
     private readonly highlight: HTMLElement = this.setUpHighlight();
     private readonly treeWalker = this.doc.createTreeWalker(
         this.el,
@@ -31,7 +37,7 @@ export class TuiHighlight implements OnChanges {
     public tuiHighlight = '';
 
     @Input()
-    public tuiHighlightColor = 'var(--tui-service-selection-background)';
+    public tuiHighlightColor = inject(TUI_HIGHLIGHT_OPTIONS).highlightColor;
 
     constructor() {
         inject(ResizeObserverService, {self: true})
