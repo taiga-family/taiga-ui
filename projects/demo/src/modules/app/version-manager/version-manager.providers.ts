a1718b0755165482e4c2ee6da736cbe2f2eaeb4c
Maksim Ivanov <omaxphp@yandex.ru>
2024-09-27 11:41:18 +0300

    chore(demo): correct version list (#9228)

--- projects/demo/src/modules/app/version-manager/version-manager.providers.ts
+++ projects/demo/src/modules/app/version-manager/version-manager.providers.ts
@@ -1,23 +1,10 @@
-import {DOCUMENT} from '@angular/common';
-import {InjectionToken, Provider} from '@angular/core';
-import {TUI_BASE_HREF} from '@taiga-ui/cdk';
+import {InjectionToken} from '@angular/core';
 
 import {TUI_VERSIONS_META_MAP, TuiVersionMeta} from './versions.constants';
 
 export const TUI_SELECTED_VERSION_META = new InjectionToken<TuiVersionMeta | null>(
     '[TUI_SELECTED_VERSION_META]: Meta information about selected version of Taiga docs',
-);
-
-export const TUI_VERSION_MANAGER_PROVIDERS: Provider[] = [
     {
-        provide: TUI_SELECTED_VERSION_META,
-        deps: [TUI_BASE_HREF, DOCUMENT],
-        useFactory: (baseHref: string, doc: Document): TuiVersionMeta | null => {
-            const base = baseHref.replace(doc.location.origin, '');
-
-            return (
-                TUI_VERSIONS_META_MAP.get(base) ?? TUI_VERSIONS_META_MAP.get('/') ?? null
-            );
-        },
+        factory: () => TUI_VERSIONS_META_MAP.get('/v3/') || null,
     },
-];
+);
