737100eb7dc80c0aed59f7a079bbc3ba73dc48c0
Maksim Ivanov <omaxphp@yandex.ru>
2024-09-30 11:58:14 +0300

    chore: mark redundant some classes as deprecated (#9255)

--- projects/testing/exceptions/element-is-not-input.exception.ts
+++ projects/testing/exceptions/element-is-not-input.exception.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated: drop in v5
+ */
 export class TuiElementIsNotInputException extends Error {
     constructor(automationId: string) {
         super(`Element ${automationId} is not <input />`);
--- projects/testing/exceptions/field-not-found.exception.ts
+++ projects/testing/exceptions/field-not-found.exception.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated: drop in v5
+ */
 export class TuiFieldNotFoundException extends Error {
     constructor(automationId: string) {
         super(`Field ${automationId} not found`);
