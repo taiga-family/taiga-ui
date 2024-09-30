38c5b20c6e511c6be2ac8c89abcb317c1c182531
40oleg <60510512+40oleg@users.noreply.github.com>
2024-09-30 15:34:59 +0300

    feat(table-addon): add requireSort option to table (#9036)
    Co-authored-by: 40oleg <oleg.merkulov701@gmail.com>

--- projects/addon-table/components/table/table.options.ts
+++ projects/addon-table/components/table/table.options.ts
@@ -4,6 +4,7 @@ import type {TuiSizeL, TuiSizeS} from '@taiga-ui/core/types';
 
 export interface TuiTableOptions {
     readonly direction: -1 | 1;
+    readonly requiredSort: boolean;
     readonly open: boolean;
     readonly resizable: boolean;
     readonly size: TuiSizeL | TuiSizeS;
@@ -21,6 +22,7 @@ export const TUI_TABLE_DEFAULT_OPTIONS: TuiTableOptions = {
     open: true,
     size: 'm',
     direction: 1,
+    requiredSort: false,
     sortIcons: {
         asc: '@tui.chevron-up',
         desc: '@tui.chevron-down',
