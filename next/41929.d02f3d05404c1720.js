97c7504b482521f2492c522bd89a586dd23d18d9
splincode <splincode@users.noreply.github.com>
2024-10-10 21:02:36 +0000

    deploy: e0645495453c3b8d9bf6f99808064007ebb7b07d

---
+++ next/41929.d02f3d05404c1720.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[41929],{41929:n=>{n.exports="```ts\nimport '@taiga-ui/testing/setup-jest';\n\n// allows you to do runtime reflection on types\nimport 'reflect-metadata';\n\n// make custom mocks\n(window as any).AnimationEvent = {};\n(window as any).TransitionEvent = {};\n(window as any).DragEvent = class {};\n\n// ...\n```\n"}}]);
