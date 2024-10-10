d0577d6f34da51b478a101c40cc5a155c2ac9487
Maksim Ivanov <omaxphp@yandex.ru>
2024-10-10 10:18:49 +0300

    fix(core): support provide empty icon value (#9408)

--- projects/core/tokens/icon-resolver.ts
+++ projects/core/tokens/icon-resolver.ts
@@ -23,12 +23,10 @@ export function tuiInjectIconResolver(): TuiStringHandler<string> {
     const icons = inject(TUI_ICON_REGISTRY);
     const resolver = inject(TUI_ICON_RESOLVER);
 
-    return (icon) => (!icon || icon.includes('/') ? icon : icons[icon] || resolver(icon));
+    return (icon) =>
+        !icon || icon.includes('/') ? icon : (icons[icon] ?? resolver(icon));
 }
 
 export function tuiIconResolverProvider(useValue: TuiStringHandler<string>): Provider {
-    return {
-        provide: TUI_ICON_RESOLVER,
-        useValue,
-    };
+    return {provide: TUI_ICON_RESOLVER, useValue};
 }
