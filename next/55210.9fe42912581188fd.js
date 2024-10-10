e2ffdcc3c22abe7a6cea8200ccb26e53caebf97d
waterplea <waterplea@users.noreply.github.com>
2024-10-10 11:49:34 +0000

    deploy: b7f75d9344ca7c8ebdb12c118c0acfb1300fff00

---
+++ next/55210.9fe42912581188fd.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[55210],{55210:n=>{n.exports='<button\n    tuiSurface="floating"\n    type="button"\n>\n    Elevated\n</button>\n<button\n    tuiSurface="neutral"\n    type="button"\n>\n    Flat\n</button>\n'}}]);
