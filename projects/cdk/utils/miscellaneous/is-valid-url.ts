8317b67ea8e5fc89b4326f4e85661fbaec6c5ae1
Максим Иванов <omaxphp@yandex.ru>
2024-10-21 11:30:28 +0300

    fix(cdk): support `:` inside path segment in  url (#9549)

--- projects/cdk/utils/miscellaneous/is-valid-url.ts
+++ projects/cdk/utils/miscellaneous/is-valid-url.ts
@@ -6,7 +6,7 @@ export function tuiIsValidUrl(url: string): boolean {
         String.raw`^([a-zA-Z]+:\/\/)?` + // protocol
             String.raw`((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|localhost|` + // domain name
             String.raw`((\d{1,3}\.){3}\d{1,3}))` + // OR IP (v4) address
-            String.raw`(\:\d+)?(\/[-a-z\d%_.~+]*)*` + // port and path
+            String.raw`(\:\d+)?(\/[-a-z\d%_.~+\:]*)*` + // port and path
             String.raw`(\?[)(;&a-z\d%_.~+=-]*)?` + // query string
             String.raw`(\#[-a-z\d_]*)?$`, // fragment locator
         'i',
