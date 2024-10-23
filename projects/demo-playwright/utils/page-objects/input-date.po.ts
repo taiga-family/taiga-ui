fbde61c84a56054a564c239d1c435999ebd69bb7
mdlufy <german.panov.2018@mail.ru>
2024-10-23 15:52:18 +0300

    chore(demo-playwright): `InputDate` use button selector instead `automationd-id`

--- projects/demo-playwright/utils/page-objects/input-date.po.ts
+++ projects/demo-playwright/utils/page-objects/input-date.po.ts
@@ -3,14 +3,9 @@ import type {Locator} from '@playwright/test';
 export class TuiInputDatePO {
     public readonly textfield: Locator = this.host.getByRole('textbox');
     public readonly calendar: Locator = this.host.page().locator('tui-calendar');
+    public readonly itemButton: Locator = this.host
+        .page()
+        .locator('tui-dropdown tui-calendar ~ * button');
 
     constructor(private readonly host: Locator) {}
-
-    public async clickItemButton(): Promise<void> {
-        const itemButton = this.host
-            .page()
-            .locator('[automation-id="tui-input-date__button"]');
-
-        await itemButton.click();
-    }
 }
