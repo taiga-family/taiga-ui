223e867ce37df47bd87e9cb3b0a129dbe1d5900d
Alex Inkin <alexander@inkin.ru>
2024-10-24 15:33:44 +0400

    fix(core): `DropdownOpen` fix initial open state (#9581)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/core/directives/dropdown/dropdown.driver.ts
+++ projects/core/directives/dropdown/dropdown.driver.ts
@@ -1,16 +1,19 @@
 import {Directive, Injectable} from '@angular/core';
 import type {TuiDriver} from '@taiga-ui/core/classes';
 import {TuiDriverDirective} from '@taiga-ui/core/classes';
-import {Subject} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable()
-export class TuiDropdownDriver extends Subject<boolean> implements TuiDriver {
+export class TuiDropdownDriver extends BehaviorSubject<boolean> implements TuiDriver {
     public readonly type = 'dropdown';
+
+    constructor() {
+        super(false);
+    }
 }
 
 @Directive({
     standalone: true,
-    selector: '[tuiDropdownDriver]',
 })
 export class TuiDropdownDriverDirective extends TuiDriverDirective {
     public readonly type = 'dropdown';
--- projects/legacy/directives/legacy-dropdown-open-monitor/legacy-dropdown-open-monitor.ts
+++ projects/legacy/directives/legacy-dropdown-open-monitor/legacy-dropdown-open-monitor.ts
@@ -1,4 +1,5 @@
-import {Directive, inject} from '@angular/core';
+import type {AfterViewInit} from '@angular/core';
+import {DestroyRef, Directive, inject} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
 import {tuiGetClosestFocusable} from '@taiga-ui/cdk/utils/focus';
@@ -9,27 +10,30 @@ import {distinctUntilChanged} from 'rxjs';
     standalone: true,
     selector: '[tuiDropdownOpenMonitor]',
 })
-export class TuiLegacyDropdownOpenMonitorDirective {
+export class TuiLegacyDropdownOpenMonitorDirective implements AfterViewInit {
+    private readonly destroyRef = inject(DestroyRef);
     private readonly el = tuiInjectElement();
     private readonly host = inject(TuiDropdownOpen, {self: true});
     private readonly external = inject(TuiDropdownOpenLegacy, {
         optional: true,
     });
 
-    protected readonly $ = this.host.driver
-        .pipe(distinctUntilChanged(), takeUntilDestroyed())
-        .subscribe((open) => this.external?.tuiDropdownOpenChange.next(open));
+    public ngAfterViewInit(): void {
+        this.external?.tuiDropdownOpenChange
+            .pipe(distinctUntilChanged(), takeUntilDestroyed(this.destroyRef))
+            .subscribe((open) => {
+                if (open) {
+                    tuiGetClosestFocusable({
+                        initial: this.el,
+                        root: this.el,
+                    })?.focus();
+                }
 
-    protected readonly $1 = this.external?.tuiDropdownOpenChange
-        .pipe(distinctUntilChanged(), takeUntilDestroyed())
-        .subscribe((open) => {
-            if (open) {
-                tuiGetClosestFocusable({
-                    initial: this.el,
-                    root: this.el,
-                })?.focus();
-            }
+                this.host.toggle(open);
+            });
 
-            this.host.toggle(open);
-        });
+        this.host.driver
+            .pipe(distinctUntilChanged(), takeUntilDestroyed(this.destroyRef))
+            .subscribe((open) => this.external?.tuiDropdownOpenChange.next(open));
+    }
 }
