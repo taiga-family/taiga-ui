13fbeccb7bf8703da2b3a9b20fdc9b445c1257c4
mdlufy <german.panov.2018@mail.ru>
2024-11-05 20:27:58 +0300

    chore(kit): `Message` use appearance interface

--- projects/kit/components/message/message.options.ts
+++ projects/kit/components/message/message.options.ts
@@ -2,14 +2,14 @@ import type {Provider} from '@angular/core';
 import {tuiCreateToken, tuiProvideOptions} from '@taiga-ui/cdk/utils/miscellaneous';
 import type {TuiAppearanceOptions} from '@taiga-ui/core/directives/appearance';
 
-export interface TuiMessageOptions extends TuiAppearanceOptions {}
-
-export const TUI_MESSAGE_DEFAULT_OPTIONS: TuiMessageOptions = {
+export const TUI_MESSAGE_DEFAULT_OPTIONS: TuiAppearanceOptions = {
     appearance: 'neutral',
 };
 
 export const TUI_MESSAGE_OPTIONS = tuiCreateToken(TUI_MESSAGE_DEFAULT_OPTIONS);
 
-export function tuiMessageOptionsProvider(options: Partial<TuiMessageOptions>): Provider {
+export function tuiMessageOptionsProvider(
+    options: Partial<TuiAppearanceOptions>,
+): Provider {
     return tuiProvideOptions(TUI_MESSAGE_OPTIONS, options, TUI_MESSAGE_DEFAULT_OPTIONS);
 }
