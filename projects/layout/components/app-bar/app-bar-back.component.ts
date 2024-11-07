1e0928db9678064edb887a01800a40bb6651598b
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-11-07 15:49:43 +0300

    feat(layout): `AppBarBack` add ability to use custom appearance (#9715)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/layout/components/app-bar/app-bar-back.component.ts
+++ projects/layout/components/app-bar/app-bar-back.component.ts
@@ -2,8 +2,8 @@ import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {TuiIcon} from '@taiga-ui/core/components/icon';
 import type {TuiAppearanceOptions} from '@taiga-ui/core/directives/appearance';
 import {
-    TuiAppearance,
     tuiAppearanceOptionsProvider,
+    TuiWithAppearance,
 } from '@taiga-ui/core/directives/appearance';
 import {TUI_SPIN_ICONS} from '@taiga-ui/core/tokens';
 
@@ -15,7 +15,7 @@ import {TUI_SPIN_ICONS} from '@taiga-ui/core/tokens';
     styleUrls: ['./app-bar-back.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [tuiAppearanceOptionsProvider(TuiAppBarBack)],
-    hostDirectives: [TuiAppearance],
+    hostDirectives: [TuiWithAppearance],
 })
 export class TuiAppBarBack implements TuiAppearanceOptions {
     protected readonly icons = inject(TUI_SPIN_ICONS);
