d4344eb8a3ce864db9ed7d5f50f5624fa8c12d8a
Максим Иванов <omaxphp@yandex.ru>
2024-11-10 20:29:13 +0300

    chore: do not split previous diff png file (#9702)

--- scripts/visual-testing/combine-playwright-failed-screenshots.ts
+++ scripts/visual-testing/combine-playwright-failed-screenshots.ts
@@ -4,23 +4,28 @@ import {combineSnapshots} from './combine-snapshots';
 
 const FAILED_SCREENSHOTS_PATH = 'projects/demo-playwright/tests-results';
 const DIFF_IMAGE_POSTFIX = '-diff.png';
+const OUTPUT_DIFF_IMAGE_POSTFIX = '.diff.png';
 
 (async function combinePlaywrightFailedScreenshots(
     rootPath = FAILED_SCREENSHOTS_PATH,
 ): Promise<void> {
     const filesOrDirs = readdirSync(rootPath, {
         withFileTypes: true,
-    });
+    }).filter((x) => !/-retry\d$/.exec(x.name));
 
-    for (const {name} of filesOrDirs.filter(
-        (x) => x.isDirectory() && !/-retry\d$/.exec(x.name),
-    )) {
+    for (const {name} of filesOrDirs.filter((x) => x.isDirectory())) {
         await combinePlaywrightFailedScreenshots(`${rootPath}/${name}`);
     }
 
     const imagesPaths: string[] = filesOrDirs
-        .filter((x) => x.isFile() && x.name.endsWith('.png'))
+        .filter(
+            (x) =>
+                x.isFile() &&
+                x.name.endsWith('.png') &&
+                !x.name.endsWith(OUTPUT_DIFF_IMAGE_POSTFIX),
+        )
         .map(({name}) => `${rootPath}/${name}`);
+
     const diffImage = imagesPaths.find((path) => path.endsWith(DIFF_IMAGE_POSTFIX));
 
     if (!diffImage) {
@@ -30,5 +35,5 @@ const DIFF_IMAGE_POSTFIX = '-diff.png';
     const buffer = await combineSnapshots(imagesPaths);
     const diffImageName = diffImage.split('/').pop()!.replace(DIFF_IMAGE_POSTFIX, '');
 
-    writeFileSync(`${rootPath}/${diffImageName}.diff.png`, buffer);
+    writeFileSync(`${rootPath}/${diffImageName}${OUTPUT_DIFF_IMAGE_POSTFIX}`, buffer);
 })();
