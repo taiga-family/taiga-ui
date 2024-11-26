ba67cf7dac2d1f7cbd1f8db4dfb67f9f471d845a
mdlufy <german.panov.2018@mail.ru>
2024-11-26 17:35:18 +0300

    chore(demo): `InputDateTime` add example with custom validator

---
+++ projects/demo/src/modules/components/input-date-time/examples/7/index.ts
@@ -0,0 +1,44 @@
+import {AsyncPipe, JsonPipe} from '@angular/common';
+import {Component} from '@angular/core';
+import {
+    AbstractControl,
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    type ValidationErrors,
+    type ValidatorFn,
+} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiDay} from '@taiga-ui/cdk';
+import {TuiError} from '@taiga-ui/core';
+import {TuiFieldErrorPipe} from '@taiga-ui/kit';
+import {TuiInputDateTimeModule} from '@taiga-ui/legacy';
+
+const completeDateTimeValidator: ValidatorFn = (
+    control: AbstractControl,
+): ValidationErrors | null =>
+    control.value.every(Boolean) ? null : {incompleteDateTime: true};
+
+@Component({
+    standalone: true,
+    imports: [
+        AsyncPipe,
+        JsonPipe,
+        ReactiveFormsModule,
+        TuiInputDateTimeModule,
+        TuiError,
+        TuiFieldErrorPipe,
+    ],
+    templateUrl: './index.html',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected readonly testForm = new FormGroup({
+        testValue: new FormControl(
+            [new TuiDay(2017, 2, 15), null],
+            completeDateTimeValidator,
+        ),
+    });
+}
