12e9a29436b309f5d7b2229249c1113b06a2ff3d
Svyatoslav Zaytsev <143048525+MillerSvt@users.noreply.github.com>
2024-12-18 15:20:18 +0500

    fix(cdk): fix tuiFocusedIn error when focus events happened in reactive context (#10020)

--- projects/cdk/observables/index.ts
+++ projects/cdk/observables/index.ts
@@ -7,5 +7,6 @@ export * from './query-list-observable';
 export * from './scroll-from';
 export * from './take-until-destroyed';
 export * from './typed-from-event';
+export * from './untracked-scheduler';
 export * from './watch';
 export * from './zone';
--- projects/cdk/utils/focus/focused-in.ts
+++ projects/cdk/utils/focus/focused-in.ts
@@ -1,14 +1,15 @@
 import type {Signal} from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {TUI_FALSE_HANDLER, TUI_TRUE_HANDLER} from '@taiga-ui/cdk/constants';
-import {fromEvent, map, merge} from 'rxjs';
+import {tuiUntrackedScheduler} from '@taiga-ui/cdk/observables';
+import {fromEvent, map, merge, observeOn} from 'rxjs';
 
 export function tuiFocusedIn(node: Node): Signal<boolean> {
     return toSignal(
         merge(
             fromEvent(node, 'focusin').pipe(map(TUI_TRUE_HANDLER)),
             fromEvent(node, 'focusout').pipe(map(TUI_FALSE_HANDLER)),
-        ),
+        ).pipe(observeOn(tuiUntrackedScheduler)),
         {initialValue: false},
     );
 }
