c0a4b116a4298b4bc9046c0f799d54d67ae40b03
waterplea <alexander@inkin.ru>
2024-12-19 16:45:36 +0400

    chore: fix

--- projects/demo/src/modules/components/input-phone-international/examples/3/index.ts
+++ projects/demo/src/modules/components/input-phone-international/examples/3/index.ts
@@ -1,53 +1,43 @@
+import {AsyncPipe} from '@angular/common';
 import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiTextfield} from '@taiga-ui/core';
+import {TuiDropdownMobile} from '@taiga-ui/addon-mobile';
+import {TuiButton, TuiTextfield} from '@taiga-ui/core';
+import {TuiInputPhoneInternational} from '@taiga-ui/experimental';
 import type {TuiCountryIsoCode} from '@taiga-ui/i18n';
 import {
-    TuiInputPhoneInternational,
     tuiInputPhoneInternationalOptionsProvider,
+    TuiSortCountriesPipe,
 } from '@taiga-ui/kit';
+import {getCountries} from 'libphonenumber-js';
 import {defer} from 'rxjs';
 
 @Component({
     standalone: true,
-    imports: [FormsModule, TuiInputPhoneInternational, TuiTextfield],
+    imports: [
+        AsyncPipe,
+        FormsModule,
+        TuiButton,
+        TuiDropdownMobile,
+        TuiInputPhoneInternational,
+        TuiSortCountriesPipe,
+        TuiTextfield,
+    ],
     templateUrl: './index.html',
     encapsulation,
     changeDetection,
     providers: [
-        /**
-         * You can choose: lazily load metadata or include it in your bundle.
-         * Lazy loading:
-         */
         tuiInputPhoneInternationalOptionsProvider({
             metadata: defer(async () =>
                 import('libphonenumber-js/max/metadata').then((m) => m.default),
             ),
         }),
-        /**
-         * Eager loading:
-         * ```ts
-         * import metadata from 'libphonenumber-js/mobile/metadata';
-         * import {of} from 'rxjs';
-         * // [...]
-         * tuiInputPhoneInternationalOptionsProvider({
-         *     metadata: of(metadata),
-         * }),
-         * ```
-         */
     ],
 })
 export default class Example {
-    protected readonly countries: readonly TuiCountryIsoCode[] = [
-        'IN',
-        'CN',
-        'US',
-        'ID',
-        'PK',
-    ];
-
-    protected countryIsoCode: TuiCountryIsoCode = 'US';
-    protected value = '+12125552368';
+    protected readonly countries = getCountries();
+    protected countryIsoCode: TuiCountryIsoCode = 'CN';
+    protected value = '';
 }
--- projects/demo/src/modules/components/input-phone-international/examples/7/index.ts
+++ projects/demo/src/modules/components/input-phone-international/examples/7/index.ts
@@ -1,43 +1,53 @@
-import {AsyncPipe} from '@angular/common';
 import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiDropdownMobile} from '@taiga-ui/addon-mobile';
-import {TuiButton, TuiTextfield} from '@taiga-ui/core';
-import {TuiInputPhoneInternational} from '@taiga-ui/experimental';
+import {TuiTextfield} from '@taiga-ui/core';
 import type {TuiCountryIsoCode} from '@taiga-ui/i18n';
 import {
+    TuiInputPhoneInternational,
     tuiInputPhoneInternationalOptionsProvider,
-    TuiSortCountriesPipe,
 } from '@taiga-ui/kit';
-import {getCountries} from 'libphonenumber-js';
 import {defer} from 'rxjs';
 
 @Component({
     standalone: true,
-    imports: [
-        AsyncPipe,
-        FormsModule,
-        TuiButton,
-        TuiDropdownMobile,
-        TuiInputPhoneInternational,
-        TuiSortCountriesPipe,
-        TuiTextfield,
-    ],
+    imports: [FormsModule, TuiInputPhoneInternational, TuiTextfield],
     templateUrl: './index.html',
     encapsulation,
     changeDetection,
     providers: [
+        /**
+         * You can choose: lazily load metadata or include it in your bundle.
+         * Lazy loading:
+         */
         tuiInputPhoneInternationalOptionsProvider({
             metadata: defer(async () =>
                 import('libphonenumber-js/max/metadata').then((m) => m.default),
             ),
         }),
+        /**
+         * Eager loading:
+         * ```ts
+         * import metadata from 'libphonenumber-js/mobile/metadata';
+         * import {of} from 'rxjs';
+         * // [...]
+         * tuiInputPhoneInternationalOptionsProvider({
+         *     metadata: of(metadata),
+         * }),
+         * ```
+         */
     ],
 })
 export default class Example {
-    protected readonly countries = getCountries();
-    protected countryIsoCode: TuiCountryIsoCode = 'CN';
-    protected value = '';
+    protected readonly countries: readonly TuiCountryIsoCode[] = [
+        'IN',
+        'CN',
+        'US',
+        'ID',
+        'PK',
+    ];
+
+    protected countryIsoCode: TuiCountryIsoCode = 'US';
+    protected value = '+12125552368';
 }
