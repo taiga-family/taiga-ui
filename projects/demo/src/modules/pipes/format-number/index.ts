c12ceaa21040ffcba72dde2911228171fb6caa92
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-12-20 14:12:20 +0300

    chore(demo): use modern API page approach for documentation about `[tuiNumberFormat]` (#10034)

--- projects/demo/src/modules/pipes/format-number/index.ts
+++ projects/demo/src/modules/pipes/format-number/index.ts
@@ -1,31 +1,26 @@
 import {Component} from '@angular/core';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {TuiDocNumberFormat} from '@demo/components/number-format';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {DemoRoute} from '@demo/routes';
 import {TuiDemo} from '@demo/utils';
-import {tuiProvide} from '@taiga-ui/cdk';
 import {TuiFormatNumberPipe} from '@taiga-ui/core';
 import {TuiInputNumberModule, TuiTextfieldControllerModule} from '@taiga-ui/legacy';
-
-import {ABSTRACT_PROPS_ACCESSOR} from '../../components/abstract/abstract-props-accessor';
-import {AbstractExampleTuiNumberFormat} from '../../components/abstract/number-format';
-import {NumberFormatDocumentation} from '../../components/abstract/number-format-documentation';
 
 @Component({
     standalone: true,
     imports: [
-        NumberFormatDocumentation,
         ReactiveFormsModule,
         TuiDemo,
+        TuiDocNumberFormat,
         TuiFormatNumberPipe,
         TuiInputNumberModule,
         TuiTextfieldControllerModule,
     ],
     templateUrl: './index.html',
     changeDetection,
-    providers: [tuiProvide(ABSTRACT_PROPS_ACCESSOR, PageComponent)],
 })
-export default class PageComponent extends AbstractExampleTuiNumberFormat {
+export default class PageComponent {
     protected readonly routes = DemoRoute;
     public readonly control = new FormControl(100);
 }
