501a6ac7ff5d587c66193d07e63c8674c61e9a99
Alexey Zezin <alexey.zezin@gmail.com>
2024-12-24 22:18:26 +0300

    chore(demo): input-tag example inside table

--- projects/demo/src/modules/components/table/examples/8/index.ts
+++ projects/demo/src/modules/components/table/examples/8/index.ts
@@ -4,7 +4,11 @@ import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiTable} from '@taiga-ui/addon-table';
-import {TuiInputModule, TuiTextfieldControllerModule} from '@taiga-ui/legacy';
+import {
+    TuiInputModule,
+    TuiInputTagModule,
+    TuiTextfieldControllerModule,
+} from '@taiga-ui/legacy';
 
 @Component({
     standalone: true,
@@ -12,6 +16,7 @@ import {TuiInputModule, TuiTextfieldControllerModule} from '@taiga-ui/legacy';
         FormsModule,
         NgForOf,
         TuiInputModule,
+        TuiInputTagModule,
         TuiTable,
         TuiTextfieldControllerModule,
     ],
@@ -24,12 +29,14 @@ export default class Example {
         {
             name: 'Alex Inkin',
             balance: 1323525,
+            items: ['Wallet', 'Phone'],
             description:
                 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
         },
         {
             name: 'Roman Sedov',
             balance: '',
+            items: ['Wallet'],
             description:
                 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
         },
