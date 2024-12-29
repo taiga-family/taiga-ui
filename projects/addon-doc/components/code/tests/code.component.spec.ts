44ae40531b291cb081893eb0a67270510e482105
Alex Inkin <alexander@inkin.ru>
2024-12-29 11:46:52 +0800

    chore: fix

--- projects/addon-doc/components/code/tests/code.component.spec.ts
+++ projects/addon-doc/components/code/tests/code.component.spec.ts
@@ -89,7 +89,7 @@ describe('TuiDocCodeComponent', () => {
         expect(fixture.nativeElement.querySelector('.t-code')?.innerHTML.trim()).toEqual(
             `<code class="hljs"><span class="hljs-keyword">const</span> a = <span class="hljs-number">15</span>;</code>
     <div class="t-code-actions">
-        <button tuiappearance="" tuiicons="" tuibutton="" type="button" tuiiconbutton="" appearance="outline-grayscale" size="s" class="t-copy-button" data-appearance="outline-grayscale" style="--t-icon-start: url(assets/taiga-ui/icons/copy.svg);" data-size="s">
+        <button tuiappearance="" tuiicons="" type="button" tuiiconbutton="" appearance="outline-grayscale" size="s" class="t-copy-button" data-appearance="outline-grayscale" style="--t-icon-start: url(assets/taiga-ui/icons/copy.svg);" data-size="s">
         </button>
 \t
     </div>`.replace('\t', '        '), // prettier problem
