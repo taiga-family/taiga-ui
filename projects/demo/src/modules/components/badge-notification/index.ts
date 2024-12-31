33be44c59f5997edbc069b5c847b890c282765d8
shiv9604 <shivkounsalye@gmail.com>
2025-01-01 01:07:06 +0530

    chore(demo): migrate badage-notification docs API

--- projects/demo/src/modules/components/badge-notification/index.ts
+++ projects/demo/src/modules/components/badge-notification/index.ts
@@ -1,13 +1,14 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
+import {TuiDocAPI, TuiDocAPIItem} from '@taiga-ui/addon-doc';
 import type {TuiSizeL, TuiSizeXS} from '@taiga-ui/core';
 import {TuiLabel} from '@taiga-ui/core';
 import {TuiBadgeNotification} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
-    imports: [TuiBadgeNotification, TuiDemo, TuiLabel],
+    imports: [TuiBadgeNotification, TuiDemo, TuiDocAPI, TuiDocAPIItem, TuiLabel],
     templateUrl: './index.html',
     changeDetection,
 })
