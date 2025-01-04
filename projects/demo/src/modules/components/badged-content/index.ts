a1d1fca6ab2cb05f6794779ef84fa2619764e52c
shiv9604 <shivkounsalye@gmail.com>
2025-01-04 23:53:20 +0530

    chore(demo): migrate badged content doc api

--- projects/demo/src/modules/components/badged-content/index.ts
+++ projects/demo/src/modules/components/badged-content/index.ts
@@ -2,6 +2,7 @@ import {AsyncPipe} from '@angular/common';
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
+import {TuiDocAPI, TuiDocAPIItem} from '@taiga-ui/addon-doc';
 import {TuiFallbackSrcPipe} from '@taiga-ui/core';
 import {TuiAvatar, TuiBadgedContent, TuiBadgeNotification} from '@taiga-ui/kit';
 
@@ -13,6 +14,8 @@ import {TuiAvatar, TuiBadgedContent, TuiBadgeNotification} from '@taiga-ui/kit';
         TuiBadgedContent,
         TuiBadgeNotification,
         TuiDemo,
+        TuiDocAPI,
+        TuiDocAPIItem,
         TuiFallbackSrcPipe,
     ],
     templateUrl: './index.html',
