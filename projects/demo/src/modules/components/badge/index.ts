6a6689a992dabe55c377f3157574d272089d2a2c
shiv9604 <shivkounsalye@gmail.com>
2025-01-10 01:11:47 +0530

    chore(demo): resolve pr comments

--- projects/demo/src/modules/components/badge/index.ts
+++ projects/demo/src/modules/components/badge/index.ts
@@ -2,21 +2,12 @@ import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
-import {TuiDocAPI, TuiDocAPIItem} from '@taiga-ui/addon-doc';
 import type {TuiSizeS, TuiSizeXL} from '@taiga-ui/core';
 import {TuiBadge, TuiFade, TuiRadioList} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
-    imports: [
-        FormsModule,
-        TuiBadge,
-        TuiDemo,
-        TuiDocAPI,
-        TuiDocAPIItem,
-        TuiFade,
-        TuiRadioList,
-    ],
+    imports: [FormsModule, TuiBadge, TuiDemo, TuiFade, TuiRadioList],
     templateUrl: './index.html',
     changeDetection,
 })
