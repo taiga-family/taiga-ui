91969affddc320e4854c97df19ca1e2804f1582a
mdlufy <german.panov.2018@mail.ru>
2025-01-27 11:55:17 +0300

    fix(kit): `InputDate` show actual min/max in `Calendar`

--- projects/demo-playwright/tests/kit/input-date/input-date.spec.ts
+++ projects/demo-playwright/tests/kit/input-date/input-date.spec.ts
@@ -5,29 +5,29 @@ import {expect, test} from '@playwright/test';
 import {TuiCalendarPO, TuiInputDatePO} from '../../../utils';
 
 test.describe('InputDate', () => {
+    let documentationPage: TuiDocumentationPagePO;
+    let example: Locator;
+
+    let inputDate!: TuiInputDatePO;
+    let calendar!: TuiCalendarPO;
+
+    test.use({
+        viewport: {
+            width: 600,
+            height: 800,
+        },
+    });
+
+    test.beforeEach(({page}) => {
+        documentationPage = new TuiDocumentationPagePO(page);
+        example = documentationPage.apiPageExample;
+
+        inputDate = new TuiInputDatePO(example.locator('tui-input-date'));
+
+        calendar = new TuiCalendarPO(inputDate.calendar);
+    });
+
     test.describe('API', () => {
-        let documentationPage: TuiDocumentationPagePO;
-        let example: Locator;
-
-        let inputDate!: TuiInputDatePO;
-        let calendar!: TuiCalendarPO;
-
-        test.use({
-            viewport: {
-                width: 600,
-                height: 800,
-            },
-        });
-
-        test.beforeEach(({page}) => {
-            documentationPage = new TuiDocumentationPagePO(page);
-            example = documentationPage.apiPageExample;
-
-            inputDate = new TuiInputDatePO(example.locator('tui-input-date'));
-
-            calendar = new TuiCalendarPO(inputDate.calendar);
-        });
-
         test('Click any day after `Until today` was selected', async ({page}) => {
             await tuiGoto(page, 'components/input-date/API?items$=1');
 
@@ -85,6 +85,25 @@ test.describe('InputDate', () => {
             await expect(inputDate.textfield).toHaveValue('Until today');
             await expect(inputDate.textfield).toHaveScreenshot(
                 '04-input-date-item-name.png',
+            );
+        });
+    });
+
+    test.describe('Examples', () => {
+        test('Actual min/max in calendar', async ({page}) => {
+            await tuiGoto(page, 'components/input-date');
+
+            const example = documentationPage.getExample('#base');
+
+            const inputDate = new TuiInputDatePO(example.locator('tui-input-date'));
+
+            await inputDate.textfield.click();
+
+            await expect(inputDate.textfield).toHaveScreenshot(
+                '05-input-actual-min-max.png',
+            );
+            await expect(inputDate.calendar).toHaveScreenshot(
+                '05-calendar-actual-min-max.png',
             );
         });
     });
