5441781f697c72c08030fc7072b97ce31c8c9ee7
waterplea <alexander@inkin.ru>
2025-02-08 12:55:19 +0400

    fix(kit): `Avatar` fix icon color for appearances

--- projects/core/directives/icons/icons.directive.ts
+++ projects/core/directives/icons/icons.directive.ts
@@ -6,9 +6,15 @@ import {
     Input,
     ViewEncapsulation,
 } from '@angular/core';
+import type {SafeResourceUrl} from '@angular/platform-browser';
 import type {TuiStringHandler} from '@taiga-ui/cdk/types';
 import {tuiWithStyles} from '@taiga-ui/cdk/utils/miscellaneous';
 import {TUI_ICON_END, TUI_ICON_START, tuiInjectIconResolver} from '@taiga-ui/core/tokens';
+
+/**
+ * Workaround for {@link TuiAvatar} to properly handle icon color in {@link TuiAppearance}
+ */
+type Icon = SafeResourceUrl | string | null | undefined;
 
 @Component({
     standalone: true,
@@ -26,7 +32,8 @@ class TuiIconsStyles {}
     standalone: true,
     host: {
         tuiIcons: '',
-        '[style.--t-icon-start]': 'iconStart ? "url(" + resolver(iconStart) + ")" : null',
+        '[style.--t-icon-start]':
+            'iconStart ? "url(" + resolver(iconStart.toString()) + ")" : null',
         '[style.--t-icon-end]': 'iconEnd ? "url(" + resolver(iconEnd) + ")" : null',
     },
 })
@@ -35,7 +42,7 @@ export class TuiIcons {
     protected readonly resolver: TuiStringHandler<string> = tuiInjectIconResolver();
 
     @Input()
-    public iconStart = inject(TUI_ICON_START, {self: true, optional: true}) || '';
+    public iconStart: Icon = inject(TUI_ICON_START, {self: true, optional: true}) || '';
 
     @Input()
     public iconEnd = inject(TUI_ICON_END, {self: true, optional: true}) || '';
