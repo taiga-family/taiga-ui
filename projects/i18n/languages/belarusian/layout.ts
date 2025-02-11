2333122a4e3c96b62fc43eb0a171b13a79b782f7
pubiqq <serj.ese@gmail.com>
2025-02-11 22:19:52 +0300

    chore(i18n): update Belarusian translation

--- projects/i18n/languages/belarusian/layout.ts
+++ projects/i18n/languages/belarusian/layout.ts
@@ -2,11 +2,11 @@ import type {TuiLanguageLayout} from '@taiga-ui/i18n/types';
 
 export const TUI_BELARUSIAN_LANGUAGE_LAYOUT: TuiLanguageLayout = {
     inputSearch: {
-        popular: 'Popular',
-        history: 'Recent',
-        placeholder: 'Type query',
-        hotkey: 'to search',
-        all: 'All',
-        empty: 'Nothing found',
+        popular: 'Папулярныя',
+        history: 'Нядаўнія',
+        placeholder: 'Увядзіце запыт',
+        hotkey: 'для пошуку',
+        all: 'Усе вынікі',
+        empty: 'Нічога не знойдзена',
     },
 };
