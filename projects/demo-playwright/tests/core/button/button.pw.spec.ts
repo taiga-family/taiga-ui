5715f233ed7cfd1b97f1dfec3d0284daf58507a8
Vladimir Potekhin <vladimir.potekh@gmail.com>
2025-02-14 17:57:34 +0300

    chore: improve a11y

--- projects/demo-playwright/tests/core/button/button.pw.spec.ts
+++ projects/demo-playwright/tests/core/button/button.pw.spec.ts
@@ -1,7 +1,5 @@
-import {DemoRoute} from '@demo/routes';
 import {TuiDocumentationPagePO, tuiGoto} from '@demo-playwright/utils';
 import {expect, test} from '@playwright/test';
-import {checkA11y, injectAxe} from 'axe-playwright';
 
 test.describe('Button', () => {
     test('darkMode=true + appearance=icon + hovered state', async ({page}) => {
@@ -20,15 +18,5 @@ test.describe('Button', () => {
         await expect(button).toHaveScreenshot(
             '01-[darkMode=true]-[appearance=icon]-[data-state=hovered].png',
         );
-    });
-
-    test('a11y', async ({page}) => {
-        await tuiGoto(page, DemoRoute.Button);
-
-        await page.locator('tui-doc-example').all();
-
-        await injectAxe(page);
-
-        await checkA11y(page, 'tui-doc-example');
     });
 });
