3d0142dcf11e2cc6cf5f47ab9e197afa4599f7f2
Максим Иванов <splincodewd@yandex.ru>
2025-02-18 13:31:02 +0300

    chore(demo): add example (#10397)
    Co-authored-by: Alex Inkin <alexander@inkin.ru>

---
+++ projects/demo/src/modules/components/icon/examples/9/index.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiIcon} from '@taiga-ui/core';
+
+@Component({
+    standalone: true,
+    imports: [TuiIcon],
+    templateUrl: './index.html',
+    styleUrls: ['./index.less'],
+    encapsulation,
+    changeDetection,
+})
+export default class Example {}
