5a27a8e734d9b3242499fd18776efc3f8e4deaf8
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-02-21 17:38:45 +0300

    chore(kit): `InputNumber` is decomposed to directive + optional `TuiInputNumberStep` component

--- projects/kit/components/input-number/index.ts
+++ projects/kit/components/input-number/index.ts
@@ -1,2 +1,4 @@
-export * from './input-number.component';
+export * from './input-number';
+export * from './input-number.directive';
 export * from './input-number.options';
+export * from './step/input-number-step.component';
---
+++ projects/kit/components/input-number/step/input-number-step.component.ts
@@ -0,0 +1,67 @@
+import {NgIf} from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    inject,
+    Input,
+    signal,
+    ViewEncapsulation,
+} from '@angular/core';
+import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
+import {tuiClamp} from '@taiga-ui/cdk/utils/math';
+import {TuiButton} from '@taiga-ui/core/components/button';
+import {
+    TUI_TEXTFIELD_OPTIONS,
+    TuiTextfieldContent,
+} from '@taiga-ui/core/components/textfield';
+
+import {TuiInputNumberDirective} from '../input-number.directive';
+import {TUI_INPUT_NUMBER_OPTIONS} from '../input-number.options';
+
+@Component({
+    standalone: true,
+    selector: 'input[tuiInputNumber][step]',
+    templateUrl: './input-number-step.template.html',
+    imports: [NgIf, TuiButton, TuiTextfieldContent],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    styleUrls: ['./input-number-step.style.less'],
+    host: {
+        '(keydown.arrowDown)': 'onStep(-step())',
+        '(keydown.arrowUp)': 'onStep(step())',
+        '[class._with-buttons]': 'step()',
+    },
+})
+export class TuiInputNumberStep {
+    protected readonly element = tuiInjectElement<HTMLInputElement>();
+    protected readonly textfieldOptions = inject(TUI_TEXTFIELD_OPTIONS);
+    protected readonly options = inject(TUI_INPUT_NUMBER_OPTIONS);
+    protected readonly inputNumber = inject(TuiInputNumberDirective, {self: true});
+    protected readonly step = signal(this.options.step);
+
+    // TODO(v5): replace with signal input
+    @Input('step')
+    public set stepSetter(x: number) {
+        this.step.set(x);
+    }
+
+    protected onStep(step: number): void {
+        const {inputNumber} = this;
+        const newValue = tuiClamp(
+            (inputNumber.value() ?? 0) + step,
+            inputNumber.min(),
+            inputNumber.max(),
+        );
+
+        if (this.inputNumber.value() === null) {
+            setTimeout(() => {
+                const caretIndex =
+                    this.element.value.length - inputNumber.postfix().length;
+
+                this.element.setSelectionRange(caretIndex, caretIndex);
+            });
+        }
+
+        this.inputNumber.setValue(newValue);
+    }
+}
