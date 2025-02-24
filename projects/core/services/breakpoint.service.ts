b5289185a0c3f5be4e4b1feb7877a75789381b63
null <taiga-family-bot@yandex.ru>
2025-02-24 08:09:23 +0000

    feat(core): update dependency rxjs to v7.8.2

--- projects/core/services/breakpoint.service.ts
+++ projects/core/services/breakpoint.service.ts
@@ -28,7 +28,9 @@ export class TuiBreakpointService extends Observable<TuiBreakpointMediaKey | nul
 
     private readonly stream$ = inject(TUI_WINDOW_SIZE).pipe(
         map(({width}) => this.sorted.find((size) => size > width)),
-        map((key) => this.invert[key || this.sorted[this.sorted.length - 1] || 0]),
+        map(
+            (key) => this.invert[key || this.sorted[this.sorted.length - 1] || 0] ?? null,
+        ),
         distinctUntilChanged(),
         tuiZoneOptimized(),
         shareReplay({bufferSize: 1, refCount: true}),
