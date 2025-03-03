d653c8afae42fb2fe83c18bb228ae3e5d33b5894
mdlufy <german.panov.2018@mail.ru>
2025-03-03 23:07:42 +0300

    chore(demo): `TuiForm` add grouped example

---
+++ projects/demo/src/modules/components/form/examples/3/index.ts
@@ -0,0 +1,76 @@
+import {AsyncPipe, NgIf, NgSwitch, NgSwitchCase} from '@angular/common';
+import {Component, signal} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {
+    TuiAppearance,
+    TuiButton,
+    TuiError,
+    TuiIcon,
+    TuiTextfield,
+    TuiTitle,
+} from '@taiga-ui/core';
+import {
+    TuiFieldErrorPipe,
+    TuiSegmented,
+    TuiStepper,
+    TuiSwitch,
+    TuiTabs,
+    TuiTooltip,
+} from '@taiga-ui/kit';
+import {TuiCardLarge, TuiForm, TuiHeader} from '@taiga-ui/layout';
+
+@Component({
+    standalone: true,
+    imports: [
+        AsyncPipe,
+        NgIf,
+        NgSwitch,
+        NgSwitchCase,
+        ReactiveFormsModule,
+        TuiAppearance,
+        TuiButton,
+        TuiCardLarge,
+        TuiError,
+        TuiFieldErrorPipe,
+        TuiForm,
+        TuiHeader,
+        TuiIcon,
+        TuiSegmented,
+        TuiSwitch,
+        TuiTextfield,
+        TuiTitle,
+        TuiTooltip,
+        TuiStepper,
+        TuiTabs,
+    ],
+    templateUrl: './index.html',
+    styleUrls: ['./index.less'],
+    changeDetection,
+})
+export default class Example {
+    protected readonly index = signal(0);
+
+    protected segmentedIndex = 0;
+
+    protected readonly first = new FormGroup({
+        name: new FormControl('', Validators.required),
+        email: new FormControl(''),
+        subscribe: new FormControl(false),
+        basic: new FormControl(true),
+    });
+
+    protected readonly second = new FormGroup({
+        ip: new FormControl('', Validators.required),
+        mask: new FormControl(''),
+        basic: new FormControl(true),
+    });
+
+    protected previous() {
+        this.index.update((index) => index - 1);
+    }
+
+    protected next() {
+        this.index.update((index) => index + 1);
+    }
+}
