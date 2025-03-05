18d7b365e28dcfe7ee9e54ddfce9591e5716961b
Vladimir Potekhin <vladimir.potekh@gmail.com>
2025-03-05 19:53:51 +0300

    feat(layout): `DynamicHeader` add directive

---
+++ projects/demo/src/modules/components/app-bar/examples/5/index.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiPlatform, TuiRepeatTimes} from '@taiga-ui/cdk';
+import {TuiTitle} from '@taiga-ui/core';
+import {TuiAvatar, TuiSegmented} from '@taiga-ui/kit';
+import {TuiAppBar, TuiCell, TuiDynamicHeader, TuiHeader} from '@taiga-ui/layout';
+
+@Component({
+    standalone: true,
+    imports: [
+        TuiAppBar,
+        TuiAvatar,
+        TuiCell,
+        TuiDynamicHeader,
+        TuiHeader,
+        TuiPlatform,
+        TuiRepeatTimes,
+        TuiTitle,
+        TuiSegmented,
+    ],
+    templateUrl: './index.html',
+    styleUrls: ['./index.less'],
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected activeItemIndex = 0;
+}
