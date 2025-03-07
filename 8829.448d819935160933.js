f1fce3c11691e5b01f97b17a3b9b7f0d17de4319
splincode <omaxphp@yandex.ru>
2025-03-07 09:22:33 +0000

    Update snapshots/demo/next/main to output generated at 28a43ca

--- 8829.448d819935160933.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[8829],{8829:n=>{n.exports='<tui-input-month\n    [formControl]="control"\n    [max]="max"\n    [min]="min"\n    [tuiTextfieldCleaner]="true"\n>\n    Choose a month\n    <input\n        placeholder="Not September please"\n        tuiTextfieldLegacy\n    />\n</tui-input-month>\n'}}]);
