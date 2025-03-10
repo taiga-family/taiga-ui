841b87f41589d01d15d0be121b8a280ace8bf54a
German Panov <german.panov.2018@mail.ru>
2025-03-10 16:36:49 +0300

    chore(demo): `Icon` add example inline svg (#10543)

---
+++ projects/demo/src/modules/components/icon/examples/10/index.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiIcon} from '@taiga-ui/core';
+
+@Component({
+    standalone: true,
+    imports: [TuiIcon],
+    template: '<tui-icon [icon]="icon" />',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected readonly icon = `data:image/svg+xml;charset=UTF-8,${encodeURIComponent(
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg>',
+    )}`;
+}
