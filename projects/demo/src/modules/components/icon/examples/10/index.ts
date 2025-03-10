f17c4c5ba88cbb5d948ac34dca20fac0ac332e40
mdlufy <german.panov.2018@mail.ru>
2025-03-10 15:50:42 +0300

    chore(demo): `TuiIcon` simplify inline example

--- projects/demo/src/modules/components/icon/examples/10/index.ts
+++ projects/demo/src/modules/components/icon/examples/10/index.ts
@@ -6,12 +6,12 @@ import {TuiIcon} from '@taiga-ui/core';
 @Component({
     standalone: true,
     imports: [TuiIcon],
-    templateUrl: './index.html',
+    template: '<tui-icon [icon]="icon" />',
     encapsulation,
     changeDetection,
 })
 export default class Example {
     protected readonly icon = `data:image/svg+xml;charset=UTF-8,${encodeURIComponent(
-        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg>',
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg>',
     )}`;
 }
