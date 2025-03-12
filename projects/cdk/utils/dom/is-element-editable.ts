11586fb38898b2dc8f19063f5e8555ccfad564bf
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-03-12 18:39:18 +0300

    chore(kit): refactor dropdown logic for non-editable `InputMonth` textfield

--- projects/cdk/utils/dom/is-element-editable.ts
+++ projects/cdk/utils/dom/is-element-editable.ts
@@ -1,5 +1,8 @@
 import {tuiIsTextfield} from './element-checks';
 
 export function tuiIsElementEditable(element: HTMLElement): boolean {
-    return (tuiIsTextfield(element) && !element.readOnly) || !!element.isContentEditable;
+    return (
+        (tuiIsTextfield(element) && !element.readOnly && element.inputMode !== 'none') ||
+        element.isContentEditable
+    );
 }
