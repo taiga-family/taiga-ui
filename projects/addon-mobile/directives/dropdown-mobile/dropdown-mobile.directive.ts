83c079f794fe8bd1e23df651d247aaa3b76532d3
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-03-12 17:56:57 +0300

    feat(addon-mobile): `DropdownMobile` supports new `Select`

--- projects/addon-mobile/directives/dropdown-mobile/dropdown-mobile.directive.ts
+++ projects/addon-mobile/directives/dropdown-mobile/dropdown-mobile.directive.ts
@@ -1,9 +1,12 @@
 import {Directive, inject, Input} from '@angular/core';
 import {TUI_IS_MOBILE} from '@taiga-ui/cdk/tokens';
 import {tuiIsHTMLElement} from '@taiga-ui/cdk/utils/dom';
+import {TUI_OPTION_CONTENT} from '@taiga-ui/core/components/data-list';
+import {TuiTextfieldComponent} from '@taiga-ui/core/components/textfield';
 import {TUI_DROPDOWN_COMPONENT} from '@taiga-ui/core/directives/dropdown';
 
 import {TuiDropdownMobileComponent} from './dropdown-mobile.component';
+import {TuiOptionMobile} from './option-mobile/option-mobile.component';
 
 @Directive({
     standalone: true,
@@ -15,6 +18,15 @@ import {TuiDropdownMobileComponent} from './dropdown-mobile.component';
                 inject(TUI_IS_MOBILE)
                     ? TuiDropdownMobileComponent
                     : inject(TUI_DROPDOWN_COMPONENT, {skipSelf: true}),
+        },
+        {
+            provide: TUI_OPTION_CONTENT,
+            useFactory: () =>
+                inject(TUI_IS_MOBILE) &&
+                // TODO(v5): remove it after deletion of all legacy controls
+                inject(TuiTextfieldComponent, {optional: true})
+                    ? TuiOptionMobile
+                    : inject(TUI_OPTION_CONTENT, {skipSelf: true, optional: true}),
         },
     ],
     host: {
