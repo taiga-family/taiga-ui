633ed2763096b042f7f5e62de2187dbeb1a56ebb
Максим Иванов <splincodewd@yandex.ru>
2025-03-24 15:01:50 +0300

    feat(kit): elastic container integration in button group (#10651)

---
+++ projects/demo/src/modules/components/button-group/examples/4/index.ts
@@ -0,0 +1,51 @@
+import {NgForOf, NgIf} from '@angular/common';
+import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiIcon, TuiSurface, TuiTitle} from '@taiga-ui/core';
+import {TuiBadge, TuiButtonGroup, TuiCarousel, TuiElasticContainer} from '@taiga-ui/kit';
+import {TuiCardMedium} from '@taiga-ui/layout';
+
+@Component({
+    standalone: true,
+    imports: [
+        NgForOf,
+        NgIf,
+        TuiBadge,
+        TuiButtonGroup,
+        TuiCardMedium,
+        TuiCarousel,
+        TuiElasticContainer,
+        TuiIcon,
+        TuiSurface,
+        TuiTitle,
+    ],
+    templateUrl: './index.html',
+    styleUrls: ['./index.less'],
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected index = 0;
+
+    protected readonly items = [
+        {
+            title: 'RUB Account',
+            content: '10 000 ₽',
+            gradient: 'linear-gradient(334.83deg, #7d8ca0 0%, #647382 100%)',
+            color: '#7d8ca0',
+        },
+        {
+            title: 'USD Account',
+            content: '2 000 000 $',
+            gradient: 'linear-gradient(-90deg, #cf77f3 0%, #009bff 47%, #2ac9db 100%)',
+            color: 'rgb(0, 155, 255)',
+        },
+        {
+            title: 'EUR Account',
+            content: '30 000 €',
+            gradient: 'linear-gradient(135deg, #1AC07E, #DEA683)',
+            color: 'rgb(158 178 129)',
+        },
+    ];
+}
