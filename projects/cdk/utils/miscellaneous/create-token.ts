feadfcdae2b04a30650e4a2369a84d4715854f28
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-03-28 13:47:53 +0300

    fix(cdk): `tuiCreateToken` with no arguments should not create token with default `undefined`-value (#10682)

--- projects/cdk/utils/miscellaneous/create-token.ts
+++ projects/cdk/utils/miscellaneous/create-token.ts
@@ -1,7 +1,9 @@
 import {InjectionToken} from '@angular/core';
 
 export function tuiCreateToken<T>(defaults?: T): InjectionToken<T> {
-    return tuiCreateTokenFromFactory(() => defaults);
+    return defaults === undefined
+        ? new InjectionToken('')
+        : tuiCreateTokenFromFactory(() => defaults);
 }
 
 export function tuiCreateTokenFromFactory<T>(factory?: () => T): InjectionToken<T> {
