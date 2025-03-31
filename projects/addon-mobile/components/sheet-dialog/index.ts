6892d2d418cfcc329eabef887f0462d57427aca9
German Panov <german.panov.2018@mail.ru>
2025-03-31 10:26:37 +0300

    feat(addon-mobile): `SheetDialog` add close directive to listen close events (#10692)

--- projects/addon-mobile/components/sheet-dialog/index.ts
+++ projects/addon-mobile/components/sheet-dialog/index.ts
@@ -3,3 +3,4 @@ export * from './sheet-dialog.directive';
 export * from './sheet-dialog.module';
 export * from './sheet-dialog.options';
 export * from './sheet-dialog.service';
+export * from './sheet-dialog-close.directive';
