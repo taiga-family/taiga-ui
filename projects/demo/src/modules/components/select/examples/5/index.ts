701f15c72ed4f47a3342634ebee50c17e0e4b567
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-04-01 11:33:55 +0300

    chore(demo): remove `BadgeNotification` from example with virtual scroll

--- projects/demo/src/modules/components/select/examples/5/index.ts
+++ projects/demo/src/modules/components/select/examples/5/index.ts
@@ -4,13 +4,12 @@ import {
     CdkVirtualScrollViewport,
 } from '@angular/cdk/scrolling';
 import {AsyncPipe} from '@angular/common';
-import type {QueryList} from '@angular/core';
-import {Component, inject, signal, ViewChildren} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiDataList, TuiOptionNew, TuiScrollable, TuiTextfield} from '@taiga-ui/core';
-import {TUI_COUNTRIES, TuiBadgeNotification, TuiChevron, TuiSelect} from '@taiga-ui/kit';
+import {TuiDataList, TuiScrollable, TuiTextfield} from '@taiga-ui/core';
+import {TUI_COUNTRIES, TuiChevron, TuiSelect} from '@taiga-ui/kit';
 import {map} from 'rxjs';
 
 @Component({
@@ -21,7 +20,6 @@ import {map} from 'rxjs';
         CdkVirtualForOf,
         CdkVirtualScrollViewport,
         FormsModule,
-        TuiBadgeNotification,
         TuiChevron,
         TuiDataList,
         TuiScrollable,
@@ -35,11 +33,5 @@ import {map} from 'rxjs';
 })
 export default class Example {
     protected readonly countries = inject(TUI_COUNTRIES).pipe(map(Object.values));
-    protected readonly optionsCount = signal(0);
     protected value = null;
-
-    @ViewChildren(TuiOptionNew)
-    protected set calculateOptions(x: QueryList<unknown>) {
-        this.optionsCount.set(x.length);
-    }
 }
