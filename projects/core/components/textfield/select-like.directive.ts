33c0cca52e3530a64a63d142e4fecae4df8951c6
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-04-01 11:55:45 +0300

    fix(kit): `InputMonth` should have `cursor: pointer` (#10712)

--- projects/core/components/textfield/select-like.directive.ts
+++ projects/core/components/textfield/select-like.directive.ts
@@ -4,6 +4,7 @@ import {Directive} from '@angular/core';
     standalone: true,
     host: {
         inputmode: 'none',
+        '[style.cursor]': '"pointer"',
         // Click on cleaner icon does not trigger `beforeinput` event --> handle all kind of deletion in input event
         '(beforeinput)': '$event.inputType.includes("delete") || $event.preventDefault()',
         '(input.capture)': '$event.inputType?.includes("delete") && clear($event.target)',
