080cadfa32a25ca1cda4b63e44eff7ff3c3bdd8a
Максим Иванов <omaxphp@yandex.ru>
2025-04-02 09:16:41 +0300

    feat(core): add `<tui-popups />` (#10717)

--- projects/core/components/root/root.component.ts
+++ projects/core/components/root/root.component.ts
@@ -8,7 +8,6 @@ import {
 } from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {EVENT_MANAGER_PLUGINS} from '@angular/platform-browser';
-import {tuiAsPortal, TuiPortals} from '@taiga-ui/cdk/classes';
 import {TUI_VERSION} from '@taiga-ui/cdk/constants';
 import {TuiFontSize} from '@taiga-ui/cdk/directives/font-size';
 import {TuiPlatform} from '@taiga-ui/cdk/directives/platform';
@@ -21,9 +20,9 @@ import {
     TUI_SCROLLBAR_OPTIONS,
     TuiScrollControls,
 } from '@taiga-ui/core/components/scrollbar';
+import {TuiPopups} from '@taiga-ui/core/directives';
 import {TuiDropdowns} from '@taiga-ui/core/directives/dropdown';
 import {TuiHints} from '@taiga-ui/core/directives/hint';
-import {TuiPopupService} from '@taiga-ui/core/directives/popup';
 import {TuiBreakpointService} from '@taiga-ui/core/services';
 import {TUI_ANIMATIONS_SPEED, TUI_REDUCED_MOTION, TUI_THEME} from '@taiga-ui/core/tokens';
 import {tuiGetDuration} from '@taiga-ui/core/utils';
@@ -33,13 +32,20 @@ import {map} from 'rxjs';
 @Component({
     standalone: true,
     selector: 'tui-root',
-    imports: [NgIf, TuiAlerts, TuiDialogs, TuiDropdowns, TuiHints, TuiScrollControls],
+    imports: [
+        NgIf,
+        TuiAlerts,
+        TuiDialogs,
+        TuiDropdowns,
+        TuiHints,
+        TuiPopups,
+        TuiScrollControls,
+    ],
     templateUrl: './root.template.html',
     styleUrls: ['./root.style.less'],
     encapsulation: ViewEncapsulation.None,
     // eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
     changeDetection: ChangeDetectionStrategy.Default,
-    viewProviders: [tuiAsPortal(TuiPopupService)],
     hostDirectives: [TuiPlatform, TuiVisualViewport, TuiFontSize],
     host: {
         'data-tui-version': TUI_VERSION,
@@ -50,7 +56,7 @@ import {map} from 'rxjs';
         '(touchstart.passive.zoneless)': '0',
     },
 })
-export class TuiRoot extends TuiPortals {
+export class TuiRoot {
     protected readonly reducedMotion = inject(TUI_REDUCED_MOTION);
     protected readonly duration = tuiGetDuration(inject(TUI_ANIMATIONS_SPEED));
 
@@ -67,8 +73,6 @@ export class TuiRoot extends TuiPortals {
     protected readonly scrollbars = !this.nativeScrollbar && !inject(TUI_IS_MOBILE);
 
     constructor() {
-        super();
-
         inject(DOCUMENT).documentElement.setAttribute(
             'data-tui-theme',
             inject(TUI_THEME).toLowerCase(),
