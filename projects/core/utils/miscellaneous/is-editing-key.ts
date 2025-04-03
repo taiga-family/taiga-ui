dd21a728b2e6102319bc40a21d5852472b53487a
Konstantin Taraskin <mrtaracqk@gmail.com>
2025-04-03 14:00:21 +0300

    fix(core): `tuiIsEditingKey` safe argument for autofill empty key

--- projects/core/utils/miscellaneous/is-editing-key.ts
+++ projects/core/utils/miscellaneous/is-editing-key.ts
@@ -3,6 +3,6 @@ import {tuiEditingKeys} from '@taiga-ui/core/constants';
 /**
  * Check if pressed key is interactive in terms of input field
  */
-export function tuiIsEditingKey(key: string): boolean {
+export function tuiIsEditingKey(key = ''): boolean {
     return key.length === 1 || tuiEditingKeys.includes(key);
 }
