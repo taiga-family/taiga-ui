563f65aca062466fa69bf21fb1e9ba90afdcaa1e
waterplea <alexander@inkin.ru>
2025-04-07 18:58:54 +0700

    chore: fix

--- projects/core/components/textfield/select-like.directive.ts
+++ projects/core/components/textfield/select-like.directive.ts
@@ -5,6 +5,7 @@ import {Directive} from '@angular/core';
     host: {
         inputmode: 'none',
         '[style.cursor]': '"pointer"',
+        '[style.caret-color]': '"transparent"',
         // Click on cleaner icon does not trigger `beforeinput` event --> handle all kind of deletion in input event
         '(beforeinput)': '$event.inputType.includes("delete") || $event.preventDefault()',
         '(input.capture)': '$event.inputType?.includes("delete") && clear($event.target)',
