7a9105c4c113938e5ac9354720b2a1e7ad495ed1
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-04-08 18:17:53 +0300

    refactor(core): add `autocomplete: 'off'` for `TuiSelectLike`

--- projects/core/components/textfield/select-like.directive.ts
+++ projects/core/components/textfield/select-like.directive.ts
@@ -5,10 +5,16 @@ import {TUI_IS_ANDROID} from '@taiga-ui/cdk/tokens';
     standalone: true,
     host: {
         inputmode: 'none',
+        autocomplete: 'off',
         '[style.cursor]': '"pointer"',
         // Click on cleaner icon does not trigger `beforeinput` event --> handle all kind of deletion in input event
         '(beforeinput)': '$event.inputType.includes("delete") || $event.preventDefault()',
-        '(input.capture)': '$event.inputType?.includes("delete") && clear($event.target)',
+        /**
+         * All NOT `delete***`-like actions were already cancelled by prevented `beforeinput` event.
+         * The only possible case to trigger `input`-event (without preceding `beforeinput` event) - browser autofill for Chromium-based browsers.
+         * We should clear any possible autofill (when browser ignores `autocomplete=off`).
+         */
+        '(input.capture)': 'clear($event.target)',
         // Hide Android text select handle (bubble marker below transparent caret)
         '(mousedown)': 'prevent($event, $event.target)',
     },
