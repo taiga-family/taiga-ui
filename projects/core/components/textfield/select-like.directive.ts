db9f021763b717c42ce5580a7d16f383a6002533
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-04-08 12:12:49 +0300

    fix(core): `InputMonth` should hide Android text select handle (#10760)

--- projects/core/components/textfield/select-like.directive.ts
+++ projects/core/components/textfield/select-like.directive.ts
@@ -1,4 +1,5 @@
-import {Directive} from '@angular/core';
+import {Directive, inject} from '@angular/core';
+import {TUI_IS_ANDROID} from '@taiga-ui/cdk/tokens';
 
 @Directive({
     standalone: true,
@@ -8,10 +9,21 @@ import {Directive} from '@angular/core';
         // Click on cleaner icon does not trigger `beforeinput` event --> handle all kind of deletion in input event
         '(beforeinput)': '$event.inputType.includes("delete") || $event.preventDefault()',
         '(input.capture)': '$event.inputType?.includes("delete") && clear($event.target)',
+        // Hide Android text select handle (bubble marker below transparent caret)
+        '(mousedown)': 'prevent($event, $event.target)',
     },
 })
 export class TuiSelectLike {
+    private readonly isAndroid = inject(TUI_IS_ANDROID);
+
     protected clear(element: HTMLInputElement): void {
         element.value = '';
+    }
+
+    protected prevent(event: MouseEvent, element: HTMLInputElement): void {
+        if (this.isAndroid) {
+            event.preventDefault();
+            element.focus();
+        }
     }
 }
