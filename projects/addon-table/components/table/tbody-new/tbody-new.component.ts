9632da155d0b05e29ce17236283c289d67633560
Andrei <a.a.serebrennikov@tinkoff.ru>
2025-04-09 19:36:21 +0500

    chore: expandable content height calculation + minor fixes

--- projects/addon-table/components/table/tbody-new/tbody-new.component.ts
+++ projects/addon-table/components/table/tbody-new/tbody-new.component.ts
@@ -1,8 +1,9 @@
 import {NgIf} from '@angular/common';
-import {computed, type QueryList} from '@angular/core';
+import type {OnChanges, QueryList, SimpleChanges} from '@angular/core';
 import {
     ChangeDetectionStrategy,
     Component,
+    computed,
     ContentChildren,
     EventEmitter,
     forwardRef,
@@ -11,8 +12,10 @@ import {
     Output,
     signal,
 } from '@angular/core';
-import {tuiInjectElement} from '@taiga-ui/cdk';
 import {EMPTY_QUERY} from '@taiga-ui/cdk/constants';
+import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
+import {TUI_ANIMATIONS_SPEED} from '@taiga-ui/core/tokens';
+import {tuiGetDuration} from '@taiga-ui/core/utils/miscellaneous';
 
 import {TUI_TABLE_PROVIDER} from '../providers/table.provider';
 import {TUI_TABLE_OPTIONS} from '../table.options';
@@ -26,17 +29,27 @@ import {TuiTableTr} from '../tr/tr.component';
     templateUrl: './tbody-new.template.html',
     styleUrls: ['./tbody-new.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    host: {style: 'animation-iteration-count: 0'},
     providers: [
         TUI_TABLE_PROVIDER,
         {provide: TuiTableTbody, useExisting: forwardRef(() => TuiTableTbodyNew)},
     ],
 })
-export class TuiTableTbodyNew<T extends Partial<Record<keyof T, any>>> {
+export class TuiTableTbodyNew<T extends Partial<Record<keyof T, any>>>
+    implements OnChanges
+{
     private readonly options = inject(TUI_TABLE_OPTIONS);
+    private readonly el = tuiInjectElement();
+    private readonly animationSpeed = inject(TUI_ANIMATIONS_SPEED);
 
-    protected readonly height = computed((_ = this.open()) => this.data.length * 3.5);
+    protected readonly contentHeight = signal<number | null>(null);
     protected readonly transitioning = signal(false);
+    protected readonly showContent = computed(() => {
+        const contentHeight = this.contentHeight();
+        const open = this.open();
+        const transitioning = this.transitioning();
+
+        return contentHeight ? open && !transitioning : true;
+    });
 
     @ContentChildren(forwardRef(() => TuiTableTr))
     public readonly rows: QueryList<TuiTableTr<T>> = EMPTY_QUERY;
@@ -44,26 +57,49 @@ export class TuiTableTbodyNew<T extends Partial<Record<keyof T, any>>> {
     @Input()
     public data: readonly T[] = [];
 
-    @Input('open')
-    set openSetter(open: boolean) {
-        this.open.set(open);
-        this.transitioning.set(true);
-    }
-
     @Output()
     public readonly openChange = new EventEmitter<boolean>();
 
     public readonly open = signal(this.options.open);
 
     constructor() {
-        const el = tuiInjectElement();
+        this.updateContentHeight(this.el);
+    }
 
-        setTimeout(() => el.style.removeProperty('animation-iteration-count'), 1000);
+    @Input('open')
+    public set openSetter(open: boolean) {
+        this.open.set(open);
+        this.transitioning.set(true);
+    }
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (changes.data && !changes.data.firstChange) {
+            this.updateContentHeight(this.el);
+        }
     }
 
     public toggle(): void {
         this.open.set(!this.open());
         this.transitioning.set(true);
         this.openChange.emit(this.open());
+    }
+
+    private updateContentHeight(el: HTMLElement): void {
+        this.el.style.setProperty('animation-iteration-count', '0');
+        this.contentHeight.set(null);
+
+        requestAnimationFrame(() => {
+            const fullElHeight = el.getBoundingClientRect().height;
+            const headingHeight = el
+                .querySelector('[tuiTableExpandHeading]')
+                ?.getBoundingClientRect().height;
+
+            this.contentHeight.set(headingHeight ? fullElHeight - headingHeight : null);
+
+            setTimeout(
+                () => this.el.style.removeProperty('animation-iteration-count'),
+                tuiGetDuration(this.animationSpeed),
+            );
+        });
     }
 }
