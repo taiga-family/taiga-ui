2a0fb98f41b2d56213ed6adce5acff1785a73450
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-04-09 11:14:11 +0300

    chore: don't disable autofill totally (it can be optionally enabled by developer)

--- projects/core/components/textfield/select-like.directive.ts
+++ projects/core/components/textfield/select-like.directive.ts
@@ -9,12 +9,7 @@ import {TUI_IS_ANDROID} from '@taiga-ui/cdk/tokens';
         '[style.cursor]': '"pointer"',
         // Click on cleaner icon does not trigger `beforeinput` event --> handle all kind of deletion in input event
         '(beforeinput)': '$event.inputType.includes("delete") || $event.preventDefault()',
-        /**
-         * All NOT `delete***`-like actions were already cancelled by prevented `beforeinput` event.
-         * The only possible case to trigger `input`-event (without preceding `beforeinput` event) - browser autofill for Chromium-based browsers.
-         * We should clear any possible autofill (when browser ignores `autocomplete=off`).
-         */
-        '(input.capture)': 'clear($event.target)',
+        '(input.capture)': '$event.inputType?.includes("delete") && clear($event.target)',
         // Hide Android text select handle (bubble marker below transparent caret)
         '(mousedown)': 'prevent($event, $event.target)',
     },
