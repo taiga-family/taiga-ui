4a3b5ca6c280624db78ce2ec3307b0e17db70005
Andrei <a.a.serebrennikov@tinkoff.ru>
2025-04-17 19:24:03 +0500

    chore: tui-table-expand fixes, new tbody removed

--- projects/addon-table/components/table/table-expand/table-expand.component.ts
+++ projects/addon-table/components/table/table-expand/table-expand.component.ts
@@ -1,34 +1,81 @@
 import {
     ChangeDetectionStrategy,
     Component,
+    computed,
+    EventEmitter,
+    inject,
     Input,
+    Output,
     signal,
     ViewEncapsulation,
 } from '@angular/core';
+import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom/inject-element';
+import {TUI_ANIMATIONS_SPEED} from '@taiga-ui/core/tokens/animations-speed';
+import {tuiGetDuration} from '@taiga-ui/core/utils/miscellaneous/to-animation-options';
 
-import {TUI_TABLE_PROVIDER} from '../providers/table.provider';
+import {TUI_TABLE_OPTIONS} from '../table.options';
 
 @Component({
     standalone: true,
     selector: 'tui-table-expand',
-    template: '<ng-content />',
+    templateUrl: './table-expand.template.html',
     styleUrls: ['./table-expand.style.less'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
         class: 't-table-expand',
-        '[class.t-table-expand_closed]': '!this.expanded()',
+        style: 'animation-iteration-count: 0',
     },
-    providers: [TUI_TABLE_PROVIDER],
 })
 export class TuiTableExpand {
-    @Input('expanded')
-    public set expandedSetter(expanded: boolean) {
-        this.expanded.set(expanded);
-    }
-    public readonly expanded = signal(false);
+    private readonly options = inject(TUI_TABLE_OPTIONS);
+    private readonly el = tuiInjectElement();
+    private readonly animationSpeed = inject(TUI_ANIMATIONS_SPEED);
 
-    public expand(): void {
-        this.expanded.update((v) => !v);
+    protected readonly transitioning = signal(false);
+    protected readonly contentHeight = computed((_ = this.expanded()) =>
+        this.updateContentHeight(),
+    );
+
+    @Output()
+    public readonly expandedChange = new EventEmitter<boolean>();
+
+    public readonly expanded = signal(this.options.open);
+
+    constructor() {
+        setTimeout(
+            () => this.el.style.removeProperty('animation-iteration-count'),
+            tuiGetDuration(this.animationSpeed),
+        );
+    }
+
+    @Input('expanded')
+    public set expandedSetter(open: boolean) {
+        this.expanded.set(open);
+        this.transitioning.set(true);
+    }
+
+    public toggle(): void {
+        this.expanded.set(!this.expanded());
+        this.transitioning.set(true);
+        this.expandedChange.emit(this.expanded());
+    }
+
+    private updateContentHeight(): number {
+        const contentEl = this.el.querySelector<HTMLDivElement>(
+            '.t-table-expand__content',
+        );
+
+        if (contentEl) {
+            contentEl.style.setProperty('display', 'block');
+
+            const height = contentEl.getBoundingClientRect().height;
+
+            contentEl.style.removeProperty('display');
+
+            return height;
+        }
+
+        return 0;
     }
 }
