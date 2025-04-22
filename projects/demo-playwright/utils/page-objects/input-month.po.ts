d7059ef441b3ff1d080c485571333ae0c37ffa08
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-04-22 14:11:18 +0300

    fix(core): `Textfield` icons should focus input & open dropdown on click (#10879)

--- projects/demo-playwright/utils/page-objects/input-month.po.ts
+++ projects/demo-playwright/utils/page-objects/input-month.po.ts
@@ -1,4 +1,5 @@
 import type {Locator} from '@playwright/test';
+import {expect} from '@playwright/test';
 
 export class TuiInputMonthPO {
     public readonly cleaner = this.host.locator('.t-clear');
@@ -6,4 +7,26 @@ export class TuiInputMonthPO {
     public readonly calendar: Locator = this.host.page().locator('tui-calendar-month');
 
     constructor(private readonly host: Locator) {}
+
+    public async clickOnIcon(): Promise<void> {
+        const box = (await this.host.boundingBox())!;
+        const padding = await this.host
+            .evaluate((el) =>
+                globalThis.getComputedStyle(el).getPropertyValue('padding-right'),
+            )
+            .then(parseInt);
+        const iconWidth = await this.host
+            .evaluate((el) =>
+                globalThis.getComputedStyle(el, '::after').getPropertyValue('width'),
+            )
+            .then(parseInt);
+
+        expect(box).not.toBeFalsy();
+        expect(padding).not.toBeFalsy();
+        expect(iconWidth).not.toBeFalsy();
+
+        await this.host.click({
+            position: {x: box.width - padding - iconWidth, y: box.height / 2},
+        });
+    }
 }
