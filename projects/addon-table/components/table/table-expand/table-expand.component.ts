1a9f46b93f7d545ebfd13517a178b77ab158c15d
a.inkin <alexander@inkin.ru>
2025-04-24 16:40:53 +0700

    chore: fix

--- projects/addon-table/components/table/table-expand/table-expand.component.ts
+++ projects/addon-table/components/table/table-expand/table-expand.component.ts
@@ -12,8 +12,10 @@ import {
     signal,
     ViewChild,
 } from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
 import {TuiPresent} from '@taiga-ui/kit/directives/present';
+import {map, of, Subject, switchMap, timer} from 'rxjs';
 
 import {TUI_TABLE_OPTIONS} from '../table.options';
 
@@ -30,8 +32,7 @@ import {TUI_TABLE_OPTIONS} from '../table.options';
         },
     ],
     host: {
-        style: '--tui-duration: 0ms',
-        '(tuiPresentChange)': 'onDOM($event)',
+        '(tuiPresentChange)': 'visible$.next($event)',
     },
 })
 export class TuiTableExpand {
@@ -40,10 +41,16 @@ export class TuiTableExpand {
 
     private readonly el = tuiInjectElement();
     private readonly server = isPlatformServer(inject(PLATFORM_ID));
-    private enableTransitionTimer: any;
 
     protected readonly transitioning = signal(false);
     protected readonly contentHeight = computed((_ = this.expanded()) => this.update());
+    protected readonly visible$ = new Subject<boolean>();
+    protected readonly sub = this.visible$
+        .pipe(
+            switchMap((v) => (v ? timer(500).pipe(map(() => v)) : of(v))),
+            takeUntilDestroyed(),
+        )
+        .subscribe((visible) => this.el.classList.toggle('_visible', visible));
 
     @Output()
     public readonly expandedChange = new EventEmitter<boolean>();
@@ -60,22 +67,6 @@ export class TuiTableExpand {
         this.expanded.set(!this.expanded());
         this.transitioning.set(true);
         this.expandedChange.emit(this.expanded());
-    }
-
-    protected onDOM(visible: boolean): void {
-        if (this.enableTransitionTimer) {
-            clearTimeout(this.enableTransitionTimer);
-        }
-
-        if (visible) {
-            // Enabling transitions
-            this.enableTransitionTimer = setTimeout(
-                () => this.el.style.removeProperty('--tui-duration'),
-                500,
-            );
-        } else {
-            this.el.style.setProperty('--tui-duration', '0ms');
-        }
     }
 
     private update(): number {
