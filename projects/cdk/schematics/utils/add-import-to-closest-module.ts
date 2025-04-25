31e040a8dff58e1b6bb8ed976023893d588dad3f
Максим Иванов <omaxphp@yandex.ru>
2025-04-25 15:16:32 +0300

    chore: improve `saveAddedImports` (#10877)

--- projects/cdk/schematics/utils/add-import-to-closest-module.ts
+++ projects/cdk/schematics/utils/add-import-to-closest-module.ts
@@ -3,28 +3,46 @@ import {
     addImportToNgModule,
     findNgModule,
     getNgComponents,
+    infoLog,
     isStandaloneComponent,
+    REPLACE_SYMBOL,
+    SMALL_TAB_SYMBOL,
+    SUCCESS_SYMBOL,
+    successLog,
 } from 'ng-morph';
 
 import {ALL_TS_FILES} from '../constants/file-globs';
+import type {TuiSchema} from '../ng-add/schema';
 import {addUniqueImport} from './add-unique-import';
 
-const importsToAdd: Array<{
-    componentPath: string;
-    moduleName: string;
-    moduleSpecifier: string;
-}> = [];
+const importsToAdd = new Map<
+    string,
+    {
+        componentPath: string;
+        moduleName: string;
+        moduleSpecifier: string;
+    }
+>();
 
 export function addImportToClosestModule(
     componentPath: string,
     moduleName: string,
     moduleSpecifier: string,
 ): void {
-    importsToAdd.push({componentPath, moduleName, moduleSpecifier});
+    importsToAdd.set(`${componentPath}_${moduleName}_${moduleSpecifier}`, {
+        componentPath,
+        moduleName,
+        moduleSpecifier,
+    });
 }
 
-export function saveAddedImports(): void {
+export function saveAddedImports(options: TuiSchema): void {
     importsToAdd.forEach(({componentPath, moduleName, moduleSpecifier}) => {
+        !options['skip-logs'] &&
+            infoLog(
+                `${SMALL_TAB_SYMBOL}${REPLACE_SYMBOL} add ${moduleName} declaration from ${moduleSpecifier} to ${componentPath}`,
+            );
+
         const [ngComponent] = getNgComponents(componentPath);
 
         if (!ngComponent) {
@@ -38,6 +56,11 @@ export function saveAddedImports(): void {
                 moduleName,
                 moduleSpecifier,
             );
+
+            !options['skip-logs'] &&
+                successLog(
+                    `${SMALL_TAB_SYMBOL}${SUCCESS_SYMBOL} ${moduleName} added to\n`,
+                );
 
             return;
         }
@@ -53,6 +76,11 @@ export function saveAddedImports(): void {
                 moduleName,
                 moduleSpecifier,
             );
+
+            !options['skip-logs'] &&
+                successLog(
+                    `${SMALL_TAB_SYMBOL}${SUCCESS_SYMBOL} ${moduleName} added to\n`,
+                );
         }
     });
 }
