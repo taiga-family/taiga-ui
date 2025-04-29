9a5d81695f62d8c1a59cd7c026881c1398e88d27
Vladimir Potekhin <vladimir.potekh@gmail.com>
2025-04-29 18:36:45 +0300

    chore: add scroll

--- projects/demo/src/modules/components/input-chip/examples/2/index.ts
+++ projects/demo/src/modules/components/input-chip/examples/2/index.ts
@@ -3,11 +3,17 @@ import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiTextfield, TuiTextfieldItem} from '@taiga-ui/core';
-import {TuiDefaultChip, TuiInputChip} from '@taiga-ui/kit';
+import {TuiInputChipItem, TuiInputChip} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
-    imports: [FormsModule, TuiDefaultChip, TuiInputChip, TuiTextfield, TuiTextfieldItem],
+    imports: [
+        FormsModule,
+        TuiInputChipItem,
+        TuiInputChip,
+        TuiTextfield,
+        TuiTextfieldItem,
+    ],
     templateUrl: './index.html',
     encapsulation,
     changeDetection,
--- projects/kit/components/input-chip/index.ts
+++ projects/kit/components/input-chip/index.ts
@@ -1,4 +1,4 @@
-export * from './default-chip';
 export * from './input-chip';
 export * from './input-chip.directive';
-export type * from './input-chip.options';
+export * from './input-chip.options';
+export * from './input-chip-item/input-chip-item';
