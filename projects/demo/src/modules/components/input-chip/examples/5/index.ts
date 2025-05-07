21bc3e62b9faacd48f3cbf4afe6cfc19511f1d45
Vladimir Potekhin <vladimir.potekh@gmail.com>
2025-05-07 16:49:46 +0300

    chore: fix validation

--- projects/demo/src/modules/components/input-chip/examples/5/index.ts
+++ projects/demo/src/modules/components/input-chip/examples/5/index.ts
@@ -1,16 +1,28 @@
 import {Component} from '@angular/core';
-import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+    type AbstractControl,
+    FormControl,
+    FormsModule,
+    ReactiveFormsModule,
+    type ValidationErrors,
+} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiHintDirective, TuiTextfield, TuiTextfieldItem} from '@taiga-ui/core';
+import {TuiTextfield, TuiTextfieldItem} from '@taiga-ui/core';
 import {TuiInputChip, TuiInputChipItem} from '@taiga-ui/kit';
+import {TuiValidationError} from '@taiga-ui/cdk';
+
+export function customValidator({value}: AbstractControl): ValidationErrors | null {
+    return value.some((v: string) => v === 'invalid')
+        ? {invalid: new TuiValidationError('invalid chip')}
+        : null;
+}
 
 @Component({
     standalone: true,
     imports: [
         FormsModule,
         ReactiveFormsModule,
-        TuiHintDirective,
         TuiInputChip,
         TuiInputChipItem,
         TuiTextfield,
@@ -21,5 +33,5 @@ import {TuiInputChip, TuiInputChipItem} from '@taiga-ui/kit';
     changeDetection,
 })
 export default class Example {
-    public readonly control = new FormControl(['invalid', 'value']);
+    public readonly control = new FormControl(['invalid', 'value'], customValidator);
 }
