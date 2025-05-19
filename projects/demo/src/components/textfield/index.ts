64ac3bb82ea52c56dd366be467828f58bd336f3d
Vladimir Potekhin <vladimir.potekh@gmail.com>
2025-05-19 18:11:21 +0300

    chore: fix comments

--- projects/demo/src/components/textfield/index.ts
+++ projects/demo/src/components/textfield/index.ts
@@ -1,6 +1,6 @@
 import {NgIf} from '@angular/common';
 import {ChangeDetectionStrategy, Component, inject, Input} from '@angular/core';
-import {TuiDocAPIItem} from '@taiga-ui/addon-doc';
+import {TuiDocAPIItem, TuiDocAPINumberItem} from '@taiga-ui/addon-doc';
 import type {TuiLooseUnion} from '@taiga-ui/cdk';
 import type {TuiSizeL, TuiSizeS, TuiTextfieldOptions} from '@taiga-ui/core';
 import {TUI_TEXTFIELD_OPTIONS, TuiTitle} from '@taiga-ui/core';
@@ -8,7 +8,7 @@ import {TUI_TEXTFIELD_OPTIONS, TuiTitle} from '@taiga-ui/core';
 @Component({
     standalone: true,
     selector: 'tbody[tuiDocTextfield]',
-    imports: [NgIf, TuiDocAPIItem, TuiTitle],
+    imports: [NgIf, TuiDocAPIItem, TuiTitle, TuiDocAPINumberItem],
     templateUrl: './index.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -18,8 +18,9 @@ export class TuiDocTextfield {
     protected readonly sizeVariants: ReadonlyArray<TuiSizeL | TuiSizeS> = ['s', 'm', 'l'];
 
     @Input()
-    public hiddenOptions: Array<TuiLooseUnion<keyof TuiTextfieldOptions>> = [];
+    public hiddenOptions: Array<TuiLooseUnion<keyof TuiTextfieldOptions>> = ['rows'];
 
     public cleaner: boolean = this.options.cleaner();
     public size: TuiSizeL | TuiSizeS = this.options.size();
+    public rows = 100;
 }
