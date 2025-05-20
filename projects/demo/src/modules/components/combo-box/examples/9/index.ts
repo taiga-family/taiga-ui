169402e7abc963fdff8efbd750e61c9564b04aa2
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-05-20 11:21:09 +0300

    chore: delete `TUI_DROPDOWN_MOBILE_OPTIONS` and introduce `--t-dropdown-mobile-margin-top`

--- projects/demo/src/modules/components/combo-box/examples/9/index.ts
+++ projects/demo/src/modules/components/combo-box/examples/9/index.ts
@@ -2,10 +2,7 @@ import {AsyncPipe} from '@angular/common';
 import {Component, inject, ViewEncapsulation} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
-import {
-    TuiDropdownMobile,
-    tuiDropdownMobileOptionsProvider,
-} from '@taiga-ui/addon-mobile';
+import {TuiDropdownMobile} from '@taiga-ui/addon-mobile';
 import {TuiTextfield} from '@taiga-ui/core';
 import {
     TUI_COUNTRIES,
@@ -16,8 +13,6 @@ import {
 } from '@taiga-ui/kit';
 import type {Observable} from 'rxjs';
 import {map} from 'rxjs';
-
-const STICKY_HEADER_HEIGHT_PX = 67;
 
 @Component({
     standalone: true,
@@ -35,11 +30,6 @@ const STICKY_HEADER_HEIGHT_PX = 67;
     styleUrls: ['./index.less'],
     encapsulation: ViewEncapsulation.None,
     changeDetection,
-    providers: [
-        tuiDropdownMobileOptionsProvider({
-            topShift: STICKY_HEADER_HEIGHT_PX + 16,
-        }),
-    ],
 })
 export default class Example {
     protected readonly countries$: Observable<string[]> = inject(TUI_COUNTRIES).pipe(
