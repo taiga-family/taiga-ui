b08dfe0e9deb1dc809359cca2561d01b603eadd8
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-05-21 13:01:30 +0300

    chore(kit): refactor `InputTime` to new `Textfield`

---
+++ projects/kit/components/input-time/input-time.ts
@@ -0,0 +1,4 @@
+import {TuiInputTimeDirective} from './input-time.directive';
+import {TuiInputTimeComponent} from './input-time.component';
+
+export const TuiInputTime = [TuiInputTimeDirective, TuiInputTimeComponent] as const;
