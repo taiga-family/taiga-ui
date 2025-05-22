964fa26c0acd64685f72d536dc109a3c6c963f6b
waterplea <alexander@inkin.ru>
2025-05-22 13:40:04 +0000

    Update snapshots/demo/next/main to output generated at 87145a2

--- 80282.5cf6ea8daa5354ed.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[80282],{80282:n=>{n.exports="```ts\nimport {ReactiveFormsModule} from '@angular/forms';\nimport {TuiInputDateModule} from '@taiga-ui/legacy';\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    ReactiveFormsModule,\n    TuiInputDateModule,\n  ],\n  // ...\n})\nexport class Example {}\n```\n"}}]);
