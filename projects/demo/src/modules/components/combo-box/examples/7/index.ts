668817b7eab6ea262bc7c02010b484b7df4895c3
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-05-23 17:58:49 +0300

    chore(demo): improve datalist example

--- projects/demo/src/modules/components/combo-box/examples/7/index.ts
+++ projects/demo/src/modules/components/combo-box/examples/7/index.ts
@@ -1,67 +1,67 @@
-import {NgForOf} from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiDataList, TuiTextfield} from '@taiga-ui/core';
-import {TuiChevron, TuiComboBox, TuiFilterByInputPipe} from '@taiga-ui/kit';
+import {TuiChevron, TuiChip, TuiComboBox, TuiFilterByInputPipe} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
     imports: [
+        CommonModule,
         FormsModule,
-        NgForOf,
         TuiChevron,
+        TuiChip,
         TuiComboBox,
         TuiDataList,
         TuiFilterByInputPipe,
         TuiTextfield,
     ],
     templateUrl: './index.html',
+    styleUrls: ['./index.less'],
     encapsulation,
     changeDetection,
 })
 export default class Example {
-    protected readonly taigaFamilyLibs = [
-        {
-            name: 'Taiga UI',
-            libraries: [
-                '@taiga-ui/cdk',
-                '@taiga-ui/core',
-                '@taiga-ui/kit',
-                '@taiga-ui/addon-doc',
-                '@taiga-ui/addon-charts',
-                '@taiga-ui/addon-commerce',
-                '@taiga-ui/addon-table',
-                '@taiga-ui/addon-mobile',
-            ],
-        },
-        {
-            name: 'Maskito',
-            libraries: [
-                '@maskito/core',
-                '@maskito/kit',
-                '@maskito/phone',
-                '@maskito/react',
-                '@maskito/angular',
-                '@maskito/vue',
-            ],
-        },
-        {
-            name: 'Web APIs for Angular',
-            libraries: [
-                '@ng-web-apis/common',
-                '@ng-web-apis/platform',
-                '@ng-web-apis/intersection-observer',
-                '@ng-web-apis/resize-observer',
-                '@ng-web-apis/mutation-observer',
-                '@ng-web-apis/view-transition',
-                '@ng-web-apis/universal',
-                '@ng-web-apis/storage',
-                '@ng-web-apis/geolocation',
-            ],
-        },
-    ];
+    protected filmDatabase = {
+        action: [
+            'The Dark Knight',
+            'Inception',
+            'The Matrix',
+            'The Dark Knight Rises',
+            'Gladiator',
+        ],
+        comedy: [
+            'The Wolf of Wall Street',
+            'Back to the Future',
+            'Guardians of the Galaxy',
+            'The Truman Show',
+            'Deadpool',
+        ],
+        drama: [
+            'The Shawshank Redemption',
+            'The Godfather',
+            "Schindler's List",
+            '12 Angry Men',
+        ],
+        horror: ['The Silence of the Lambs', 'Alien', 'Psycho', 'The Shining'],
+        romance: [
+            'Forrest Gump',
+            'Titanic',
+            'Good Will Hunting',
+            'Eternal Sunshine of the Spotless Mind',
+            'Slumdog Millionaire',
+        ],
+    };
+
+    protected categories: string[] = Object.keys(this.filmDatabase);
+    protected filters: Record<string, boolean> = this.categories.reduce(
+        (acc, category, i) => ({...acc, [category]: i % 2 === 0}),
+        {},
+    );
+
+    protected selectedCategory = true;
 
     protected value: string | null = null;
 }
