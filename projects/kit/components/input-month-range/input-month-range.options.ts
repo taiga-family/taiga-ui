8f1e7770aec5308d616877d11b8f4151884db847
waterplea <alexander@inkin.ru>
2025-05-27 15:19:00 +0400

    chore: fix

--- projects/kit/components/input-month-range/input-month-range.options.ts
+++ projects/kit/components/input-month-range/input-month-range.options.ts
@@ -8,10 +8,8 @@ import {
 } from '@taiga-ui/cdk';
 import type {TuiHandler} from '@taiga-ui/cdk/types';
 import type {TuiSizeL, TuiSizeS} from '@taiga-ui/core/types';
-import {
-    TUI_INPUT_DATE_DEFAULT_OPTIONS_NEW,
-    TUI_INPUT_DATE_OPTIONS_NEW,
-} from '@taiga-ui/kit/components/input-date';
+import {TUI_INPUT_MONTH_OPTIONS} from '@taiga-ui/kit/components/input-month';
+import {TUI_INPUT_DATE_DEFAULT_OPTIONS_NEW} from '@taiga-ui/kit/components/input-date';
 
 export interface TuiInputMonthRangeOptions {
     readonly icon: TuiHandler<TuiSizeL | TuiSizeS, string>;
@@ -20,7 +18,7 @@ export interface TuiInputMonthRangeOptions {
 
 export const TUI_INPUT_MONTH_RANGE_OPTIONS =
     tuiCreateTokenFromFactory<TuiInputMonthRangeOptions>(() => ({
-        ...inject(TUI_INPUT_DATE_OPTIONS_NEW),
+        ...inject(TUI_INPUT_MONTH_OPTIONS),
         valueTransformer: TUI_IDENTITY_VALUE_TRANSFORMER,
     }));
 
