489118ba91b0e192ee718623cd2f53f0bf1a8638
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-05-29 17:19:37 +0300

    chore: small refactor

--- projects/kit/components/combo-box/combo-box.directive.ts
+++ projects/kit/components/combo-box/combo-box.directive.ts
@@ -136,7 +136,7 @@ export class TuiComboBox<T>
 
     public override writeValue(value: T | string | null): void {
         super.writeValue(value);
-        this.setValue(value);
+        this.textfield.value.set(this.stringify(value));
     }
 
     protected toggleDropdown(open = !this.open()): void {
