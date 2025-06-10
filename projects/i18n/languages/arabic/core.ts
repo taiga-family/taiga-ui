3f7cb83c700416bc4c985a5f5775041bf17fca3d
Mohamed El-Serafy <elserafy02@gmail.com>
2025-06-10 07:05:36 +0300

    chore(i18n): correct formating in arabic language files

--- projects/i18n/languages/arabic/core.ts
+++ projects/i18n/languages/arabic/core.ts
@@ -22,6 +22,6 @@ export const TUI_ARABIC_LANGUAGE_CORE: TuiLanguageCore = {
     nothingFoundMessage: 'لم يتم العثور على شيء',
     defaultErrorMessage: 'القيمة غير صالحة',
     spinTexts: ['السابق', 'التالي'],
-    shortWeekDays: ['إثنين', "ثلاثاء", 'أربعاء', "خميس", 'جمعة', 'سبت', 'أحد'],
+    shortWeekDays: ['إثنين', 'ثلاثاء', 'أربعاء', 'خميس', 'جمعة', 'سبت', 'أحد'],
     countries: TUI_ARABIC_LANGUAGE_COUNTRIES,
 };
