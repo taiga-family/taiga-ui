5fa3e2906fb308de17a21131492beefb8d40b4a2
Mohamed El Serafy <elserafy02@gmail.com>
2025-06-17 14:48:35 +0300

    feat(i18n): add Arabic (#11091)

---
+++ projects/i18n/languages/arabic/addon-commerce.ts
@@ -0,0 +1,6 @@
+import type {TuiLanguageCommerce} from '@taiga-ui/i18n/types';
+
+export const TUI_ARABIC_LANGUAGE_ADDON_COMMERCE: TuiLanguageCommerce = {
+    cardNumber: ['الرقم', 'رقم البطاقة'],
+    cardExpiry: ['ينتهي', 'صالح حتى'],
+};
