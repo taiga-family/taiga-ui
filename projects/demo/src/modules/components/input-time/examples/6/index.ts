06a3f6af442aa0e2713c3d6d01ef374da26f947a
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-06-17 14:51:11 +0300

    chore(demo): change order of examples

--- projects/demo/src/modules/components/input-time/examples/6/index.ts
+++ projects/demo/src/modules/components/input-time/examples/6/index.ts
@@ -1,61 +1,23 @@
-import {Component, signal} from '@angular/core';
+import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import type {TuiBooleanHandler, TuiStringMatcher} from '@taiga-ui/cdk';
 import {TuiTime} from '@taiga-ui/cdk';
-import {tuiItemsHandlersProvider, TuiTextfield} from '@taiga-ui/core';
-import {
-    tuiCreateTimePeriods,
-    TuiDataListWrapper,
-    TuiFilterByInputPipe,
-    TuiInputTime,
-} from '@taiga-ui/kit';
+import {TuiIcon, TuiTextfield} from '@taiga-ui/core';
+import {tuiCreateTimePeriods, TuiInputTime, TuiTooltip} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
-    imports: [
-        FormsModule,
-        TuiDataListWrapper,
-        TuiFilterByInputPipe,
-        TuiInputTime,
-        TuiTextfield,
-    ],
+    imports: [FormsModule, TuiIcon, TuiInputTime, TuiTextfield, TuiTooltip],
     templateUrl: './index.html',
     encapsulation,
     changeDetection,
-    providers: [
-        /**
-         * You can also use input props of `Textfield`
-         * (they will have more priority):
-         * ```html
-         * <tui-textfield
-         *     [identityMatcher]="..."
-         *     [stringify]="..."
-         * />
-         * ```
-         */
-        tuiItemsHandlersProvider({
-            stringify: signal((x: TuiTime) => x.toString('HH:MM')),
-            identityMatcher: signal(
-                (a: TuiTime | null, b: TuiTime | null) => a?.valueOf() === b?.valueOf(),
-            ),
-            // disabledItemHandler: signal((x: TuiTime) => x.hours > 18),
-        }),
-    ],
 })
 export default class Example {
     protected value: TuiTime | null = null;
-    protected items: readonly TuiTime[] = [
-        new TuiTime(16, 20),
-        new TuiTime(16, 45),
-        new TuiTime(17, 0),
-        ...tuiCreateTimePeriods(18, 20, [0, 15, 30, 45]),
+    protected acceptableValues = [
+        // Array of TuiTime from 10:00 to 18:00 every half of hour
+        ...tuiCreateTimePeriods(10, 18, [0, 30]),
+        new TuiTime(18, 0),
     ];
-
-    protected readonly disabledItemHandler: TuiBooleanHandler<TuiTime> = (x) =>
-        x?.valueOf() === this.items[0]!.valueOf();
-
-    protected readonly matcher: TuiStringMatcher<TuiTime> = (item, query) =>
-        item.toString('HH:MM').startsWith(query);
 }
