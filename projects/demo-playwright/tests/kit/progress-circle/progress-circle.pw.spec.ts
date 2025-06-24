15f9f60f31a1e4e186390b2799ed3781fb4eae4f
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-06-24 15:43:57 +0300

    feat(kit): `ProgressCircle` has arc mode

---
+++ projects/demo-playwright/tests/kit/progress-circle/progress-circle.pw.spec.ts
@@ -0,0 +1,38 @@
+import {DemoRoute} from '@demo/routes';
+import {TuiDocumentationPagePO, tuiGoto} from '@demo-playwright/utils';
+import {expect, test} from '@playwright/test';
+
+const {describe, beforeEach} = test;
+
+describe('TuiProgressCircle', () => {
+    beforeEach(async ({page}) => {
+        await tuiGoto(page, DemoRoute.ProgressCircle);
+    });
+
+    describe('Arc mode', () => {
+        test.use({viewport: {width: 225, height: 300}});
+
+        ['xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl'].forEach((size) => {
+            describe(`size=${size} & max=10`, () => {
+                [0, 2, 3, 5, 7, 9, 10].forEach((value) => {
+                    test(`value=${value}`, async ({page}) => {
+                        await tuiGoto(
+                            page,
+                            `${DemoRoute.ProgressCircle}/API?max=10&value=${value}&size=${size}&arc=true`,
+                        );
+
+                        await expect
+                            .soft(
+                                new TuiDocumentationPagePO(page).apiPageExample.locator(
+                                    'tui-progress-circle',
+                                ),
+                            )
+                            .toHaveScreenshot(
+                                `progress-circle--size-${size}--value-${value}--max-10.png`,
+                            );
+                    });
+                });
+            });
+        });
+    });
+});
