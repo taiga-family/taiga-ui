2dbad3d1abbf9c46023ecb91983d9e02f1942981
Vladimir Potekhin <vladimir.potekh@gmail.com>
2025-06-27 15:28:12 +0300

    chore: move input-password in separate example

--- projects/demo/src/modules/components/textfield/examples/6/index.ts
+++ projects/demo/src/modules/components/textfield/examples/6/index.ts
@@ -1,20 +1,46 @@
-import {Component, signal} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiTextfield} from '@taiga-ui/core';
-import {TuiSwitch} from '@taiga-ui/kit';
+import {MaskitoDirective} from '@maskito/angular';
+import type {MaskitoOptions} from '@maskito/core';
+import {
+    maskitoAddOnFocusPlugin,
+    maskitoCaretGuard,
+    maskitoNumberOptionsGenerator,
+    maskitoRemoveOnBlurPlugin,
+} from '@maskito/kit';
+import {TuiButton, TuiTextfield} from '@taiga-ui/core';
+
+const postfix = ' rad';
+const numberOptions = maskitoNumberOptionsGenerator({
+    postfix,
+    decimalSeparator: ',',
+    maximumFractionDigits: 8,
+    min: 0,
+});
 
 @Component({
     standalone: true,
-    imports: [FormsModule, TuiSwitch, TuiTextfield],
+    imports: [FormsModule, MaskitoDirective, NgIf, TuiButton, TuiTextfield],
     templateUrl: './index.html',
-    styleUrls: ['./index.less'],
     encapsulation,
     changeDetection,
 })
 export default class Example {
-    protected readonly disable = signal(true);
-    protected card = '1111 1111 1111 1111';
-    protected name = 'Test User';
+    protected value = Math.PI.toFixed(8);
+    protected readonly options: MaskitoOptions = {
+        ...numberOptions,
+        plugins: [
+            ...numberOptions.plugins,
+            maskitoCaretGuard((value) => [0, value.length - postfix.length]),
+            maskitoAddOnFocusPlugin(postfix),
+            maskitoRemoveOnBlurPlugin(postfix),
+        ],
+    };
+
+    protected clear(): void {
+        this.value = postfix;
+    }
 }
