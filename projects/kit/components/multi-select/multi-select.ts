1aba2612e79b611746712357c95f6420edf7e267
waterplea <alexander@inkin.ru>
2025-06-30 18:11:39 +0700

    feat(kit): `MultiSelectOption` add new `Option`

---
+++ projects/kit/components/multi-select/multi-select.ts
@@ -0,0 +1,7 @@
+import {TuiMultiSelectGroupComponent} from './multi-select-group/multi-select-group.component';
+import {TuiMultiSelectGroupDirective} from './multi-select-group/multi-select-group.directive';
+
+export const TuiMultiSelect = [
+    TuiMultiSelectGroupComponent,
+    TuiMultiSelectGroupDirective,
+];
