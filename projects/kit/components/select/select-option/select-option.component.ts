22d0022416f81e93f232cb1b2ab599514175992b
waterplea <alexander@inkin.ru>
2025-07-01 19:29:38 +0700

    chore: `SelectOption` refactor to use same code on mobile and desktop

--- projects/kit/components/select/select-option/select-option.component.ts
+++ projects/kit/components/select/select-option/select-option.component.ts
@@ -1,18 +1,30 @@
 import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
 import {tuiDirectiveBinding, tuiIsPresent} from '@taiga-ui/cdk/utils/miscellaneous';
-import {TuiOptionWithValue} from '@taiga-ui/core/components/data-list';
+import {
+    TuiDataListComponent,
+    TuiOptionWithValue,
+} from '@taiga-ui/core/components/data-list';
 import {TuiScrollIntoView} from '@taiga-ui/core/components/scrollbar';
-import {TuiIcons} from '@taiga-ui/core/directives/icons';
 import type {TuiItemsHandlers} from '@taiga-ui/core/directives/items-handlers';
 import {TUI_ITEMS_HANDLERS} from '@taiga-ui/core/directives/items-handlers';
-import {TUI_COMMON_ICONS} from '@taiga-ui/core/tokens';
+import {TuiCheckbox, tuiCheckboxOptionsProvider} from '@taiga-ui/kit/components/checkbox';
 import {tuiInjectValue} from '@taiga-ui/kit/utils';
 
 @Component({
     standalone: true,
-    template: '',
+    imports: [TuiCheckbox],
+    template: `
+        <input
+            tuiCheckbox
+            type="checkbox"
+            class="t-check"
+            [checked]="selected()"
+            [size]="datalist.size === 'l' ? 'm' : 's'"
+        />
+    `,
     styleUrls: ['./select-option.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    viewProviders: [tuiCheckboxOptionsProvider({appearance: () => 'action'})],
     hostDirectives: [TuiScrollIntoView],
     host: {
         tuiSelectOption: '',
@@ -24,13 +36,10 @@ export class TuiSelectOption<T> {
     private readonly handlers: TuiItemsHandlers<T> = inject(TUI_ITEMS_HANDLERS);
     private readonly value = tuiInjectValue<T>();
 
+    protected readonly datalist = inject(TuiDataListComponent);
     protected readonly option = inject<TuiOptionWithValue<T>>(TuiOptionWithValue, {
         optional: true,
     });
-
-    protected readonly icon =
-        this.option &&
-        tuiDirectiveBinding(TuiIcons, 'iconEnd', inject(TUI_COMMON_ICONS).check, {});
 
     protected readonly selected = computed(
         (controlValue = this.value(), optionValue = this.option?.value()) =>
