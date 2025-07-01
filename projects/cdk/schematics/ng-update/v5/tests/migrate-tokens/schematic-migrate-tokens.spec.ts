42d0109ff86aba53c099f03853212b051df93688
mdlufy <german.panov.2018@mail.ru>
2025-07-02 01:00:39 +0300

    chore(cdk): schematic-migrate-tokens.spec.ts skip tests

--- projects/cdk/schematics/ng-update/v5/tests/migrate-tokens/schematic-migrate-tokens.spec.ts
+++ projects/cdk/schematics/ng-update/v5/tests/migrate-tokens/schematic-migrate-tokens.spec.ts
@@ -2,7 +2,7 @@ import {resetActiveProject} from 'ng-morph';
 
 import {runMigration} from './utils';
 
-describe('ng-update should migrate tuiCreateToken and tuiCreateTokenFromFactory to InjectionToken', () => {
+describe.skip('ng-update should migrate tuiCreateToken and tuiCreateTokenFromFactory to InjectionToken', () => {
     it('should migrate basic tokens without existing InjectionToken import', async () => {
         expect(
             await runMigration(
