8383b9a6ebe08cc3f5f58f00905e58d21c6e5ef9
splincode <splincode@users.noreply.github.com>
2025-07-03 10:04:16 +0000

    deploy: d7dc3eaea2a6de89c29811de8061014b6d13cd33

---
+++ next/41714.a2028c668a1dbfc2.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[41714],{41714:e=>{e.exports=".bars {\n    block-size: 7.5rem;\n    inline-size: 5rem;\n    margin-block-start: 2rem;\n    box-shadow: 0 1px var(--tui-border-normal);\n}\n"}}]);
