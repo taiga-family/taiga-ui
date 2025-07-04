773531687d793c06ea65cc9988865ad91d182879
waterplea <alexander@inkin.ru>
2025-07-04 19:00:22 +0700

    chore: fix

--- projects/kit/components/multi-select/multi-select-native/multi-select-native.component.ts
+++ projects/kit/components/multi-select/multi-select-native/multi-select-native.component.ts
@@ -2,7 +2,7 @@ import {NgForOf, NgIf, NgTemplateOutlet} from '@angular/common';
 import {ChangeDetectionStrategy, Component, computed, inject, Input} from '@angular/core';
 import {TuiControl} from '@taiga-ui/cdk/classes';
 import {TUI_IS_MOBILE} from '@taiga-ui/cdk/tokens';
-import {tuiIsPresent} from '@taiga-ui/cdk/utils';
+import {tuiInjectElement, tuiIsPresent} from '@taiga-ui/cdk/utils';
 import {tuiAsOptionContent} from '@taiga-ui/core/components/data-list';
 import {
     TuiSelectLike,
@@ -29,6 +29,7 @@ import {TuiMultiSelectOption} from '../multi-select-option/multi-select-option.c
         multiple: '',
         '(click.stop.zoneless)': '0',
         '(change)': 'selectOption($event.target.options.selectedIndex)',
+        '(focus)': 'mobile && el.showPicker?.()',
     },
 })
 export class TuiMultiSelectNative<T> {
@@ -36,27 +37,15 @@ export class TuiMultiSelectNative<T> {
     private readonly textfield = inject(TuiTextfieldComponent);
 
     protected readonly isFlat = tuiIsFlat;
-    // protected readonly placeholder = signal('');
     protected readonly handlers: TuiItemsHandlers<T> = inject(TUI_ITEMS_HANDLERS);
     protected readonly mobile = inject(TUI_IS_MOBILE);
-
-    // protected readonly stringified = computed((value = this.value()) =>
-    //     tuiIsPresent(value) ? this.itemsHandlers.stringify()(value) : '',
-    // );
-    //
-    // protected readonly showPlaceholder = computed(
-    //     () => this.placeholder() && !this.stringified(),
-    // );
+    protected readonly el = tuiInjectElement<HTMLSelectElement>();
 
     protected readonly isSelected = computed(
         (value = this.control.value()) =>
             (x: T) =>
                 tuiIsPresent(value) && this.handlers.identityMatcher()(x, value),
     );
-
-    // protected readonly valueEffect = effect(() => {
-    //     this.textfield.value.set(this.stringified());
-    // }, TUI_ALLOW_SIGNAL_WRITES);
 
     @Input()
     public items: ReadonlyArray<readonly T[]> | readonly T[] | null = [];
@@ -69,16 +58,12 @@ export class TuiMultiSelectNative<T> {
         this.textfield.fillerSetter = placeholder;
     }
 
-    // public setValue(value: T | null): void {
-    //     this.onChange(value);
-    // }
-    //
-
     protected selectOption(index: number): void {
-        const items = this.items?.flat() ?? [];
+        const items = this.items || [];
+        const item = items.flat()[index];
 
-        if (items) {
-            this.textfield.handleOption(items[index]);
+        if (item) {
+            this.textfield.handleOption(item);
         }
     }
 }
