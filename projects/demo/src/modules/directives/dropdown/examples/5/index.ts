7d48a881cdff40a98cfde0bf67e2fe9e23f9c69c
Максим Иванов <splincodewd@yandex.ru>
2025-07-07 15:14:05 +0300

    chore(demo): changes in dropdown documentation (#11244)

--- projects/demo/src/modules/directives/dropdown/examples/5/index.ts
+++ projects/demo/src/modules/directives/dropdown/examples/5/index.ts
@@ -1,19 +1,80 @@
-import {Component} from '@angular/core';
+import {AsyncPipe} from '@angular/common';
+import {Component, inject, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiAppearance, TuiDropdown, TuiLabel} from '@taiga-ui/core';
-import {TuiSwitch} from '@taiga-ui/kit';
-import {TuiCardLarge} from '@taiga-ui/layout';
+import {assets} from '@demo/utils';
+import {TuiAmountPipe} from '@taiga-ui/addon-commerce';
+import {TuiDropdownMobile} from '@taiga-ui/addon-mobile';
+import {TuiButton, TuiDropdown, TuiTextfield, TuiTitle} from '@taiga-ui/core';
+import {
+    TUI_COUNTRIES,
+    TuiAvatar,
+    TuiChevron,
+    TuiDataListWrapper,
+    TuiFade,
+    TuiFilterByInputPipe,
+    TuiInputNumber,
+    TuiSelect,
+} from '@taiga-ui/kit';
+import {TuiCell} from '@taiga-ui/layout';
+import {
+    TuiComboBoxModule,
+    TuiMultiSelectModule,
+    TuiTextfieldControllerModule,
+} from '@taiga-ui/legacy';
+import {map} from 'rxjs';
+
+interface User {
+    readonly url: string;
+    readonly name: string;
+    readonly balance: number;
+}
 
 @Component({
     standalone: true,
-    imports: [FormsModule, TuiAppearance, TuiCardLarge, TuiDropdown, TuiLabel, TuiSwitch],
+    imports: [
+        AsyncPipe,
+        FormsModule,
+        TuiAmountPipe,
+        TuiAvatar,
+        TuiButton,
+        TuiCell,
+        TuiChevron,
+        TuiComboBoxModule,
+        TuiDataListWrapper,
+        TuiDropdown,
+        TuiDropdownMobile,
+        TuiFade,
+        TuiFilterByInputPipe,
+        TuiInputNumber,
+        TuiMultiSelectModule,
+        TuiSelect,
+        TuiTextfield,
+        TuiTextfieldControllerModule,
+        TuiTitle,
+    ],
     templateUrl: './index.html',
-    styleUrls: ['./index.less'],
     encapsulation,
     changeDetection,
 })
 export default class Example {
-    protected open = false;
+    protected country = null;
+    protected selected: readonly User[] = [];
+    protected sum = null;
+    protected user: User | null = null;
+
+    protected readonly open = signal(false);
+
+    protected readonly countries$ = inject(TUI_COUNTRIES).pipe(map(Object.values));
+
+    protected readonly users: readonly User[] = [
+        {name: 'Alex Inkin', balance: 1323525, url: assets`/images/avatar.jpg`},
+        {name: 'Roman Sedov', balance: 523242, url: 'RS'},
+        {name: 'Vladimir Potekhin', balance: 645465, url: 'VP'},
+        {name: 'Nikita Barsukov', balance: 468468, url: 'NB'},
+        {name: 'Maxim Ivanov', balance: 498654, url: 'MI'},
+    ];
+
+    protected readonly stringify = ({name}: User): string => name;
 }
