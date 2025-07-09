017dea27506675b559a708c31dee82e52a662653
waterplea <alexander@inkin.ru>
2025-07-09 17:23:38 +0700

    chore: fix

--- projects/kit/components/preview/preview.ts
+++ projects/kit/components/preview/preview.ts
@@ -1,6 +1,6 @@
+import {TuiPreviewAction} from './action/preview-action.directive';
 import {TuiPreviewPagination} from './pagination/preview-pagination.component';
 import {TuiPreviewComponent} from './preview.component';
-import {TuiPreviewAction} from './preview-action/preview-action.directive';
 import {TuiPreviewTitle} from './title/preview-title.component';
 import {TuiPreviewZoom} from './zoom/preview-zoom.component';
 
