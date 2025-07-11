6267299ab7d99569e48f29424ba22e8493e3c3d6
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-07-11 18:18:31 +0300

    chore: refactor inside native time pickers

--- projects/kit/components/input-date-time/input-date-time.component.ts
+++ projects/kit/components/input-date-time/input-date-time.component.ts
@@ -4,11 +4,11 @@ import {
     Component,
     computed,
     inject,
-    Input,
     ViewEncapsulation,
 } from '@angular/core';
 import {TuiDay, TuiTime} from '@taiga-ui/cdk/date-time';
 import {TuiTextfieldContent} from '@taiga-ui/core/components/textfield';
+import {TuiNativeTimePicker} from '@taiga-ui/kit/components/input-time';
 
 import {TuiInputDateTimeDirective} from './input-date-time.directive';
 
@@ -20,14 +20,12 @@ import {TuiInputDateTimeDirective} from './input-date-time.directive';
     styleUrls: ['./input-date-time.style.less'],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
-    host: {
-        ngSkipHydration: 'true',
-        '[type]': '"text"',
-        '[attr.list]': 'null',
-    },
+    host: {ngSkipHydration: 'true'},
 })
-export class TuiInputDateTimeComponent {
+export class TuiInputDateTimeComponent extends TuiNativeTimePicker {
     protected readonly host = inject(TuiInputDateTimeDirective);
+    protected readonly step = computed(() => this.getStep(this.host.timeMode()));
+    protected readonly value = computed(() => this.toISOString(this.host.value()));
 
     protected min = computed(() =>
         this.toISOString([this.host.min(), this.host.minTime()]),
@@ -36,33 +34,6 @@ export class TuiInputDateTimeComponent {
     protected max = computed(() =>
         this.toISOString([this.host.max(), this.host.maxTime()]),
     );
-
-    protected readonly step = computed((mode = this.host.timeMode()) => {
-        switch (mode) {
-            case 'HH:MM:SS':
-            case 'HH:MM:SS AA':
-                return 1;
-            case 'HH:MM:SS.MSS':
-            case 'HH:MM:SS.MSS AA':
-                return 0.001;
-            default:
-                return 60;
-        }
-    });
-
-    protected value = computed(() => this.toISOString(this.host.value()));
-
-    @Input()
-    public list: string | null = null;
-
-    protected toISOString(
-        date: TuiDay | readonly [TuiDay, TuiTime | null] | null,
-    ): string {
-        const [day, time] = Array.isArray(date) ? date : [date];
-        const timeString = time ? `T${time.toString(this.host.timeMode())}` : '';
-
-        return day ? day.toJSON() + timeString : '';
-    }
 
     protected onInput(value: string): void {
         if (!value) {
