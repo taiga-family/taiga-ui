8d992d514453529d0c3f181d51d0cb8fe1494a98
Максим Иванов <splincodewd@yandex.ru>
2025-07-14 16:48:54 +0300

    feat(testing): export cypress and playwright utils (#11312)

--- projects/testing/visual-testing/combine-cypress-failed-screenshots.ts
+++ projects/testing/visual-testing/combine-cypress-failed-screenshots.ts
@@ -1,6 +1,6 @@
 import {readFileSync, writeFileSync} from 'node:fs';
 
-import {combineSnapshots} from './combine-snapshots';
+import {tuiCombineSnapshots} from './combine-snapshots';
 
 const ROOT_PATH = 'projects/demo-cypress';
 const TEST_RESULTS_PATH = `${ROOT_PATH}/tests-results`;
@@ -17,7 +17,7 @@ interface Report {
     suites: Array<{tests: TestResult[]}>;
 }
 
-(async function combineCypressFailedScreenshots(): Promise<void> {
+export async function tuiCombineCypressFailedScreenshots(): Promise<void> {
     const reportSummary = readJSON<Report>(`${TEST_RESULTS_PATH}/report-summary.json`);
 
     if (!reportSummary) {
@@ -30,13 +30,13 @@ interface Report {
         .filter((x) => x.status === 'fail' && x.diffPath);
 
     for (const {baselinePath, diffPath, comparisonPath, name} of failedTestSnapshots) {
-        const buffer = await combineSnapshots(
+        const buffer = await tuiCombineSnapshots(
             [baselinePath, diffPath, comparisonPath].map((x) => `${ROOT_PATH}/${x}`),
         );
 
         writeFileSync(`${TEST_RESULTS_PATH}/${name}.diff.png`, buffer);
     }
-})();
+}
 
 function readJSON<T>(path: string): T | null {
     try {
