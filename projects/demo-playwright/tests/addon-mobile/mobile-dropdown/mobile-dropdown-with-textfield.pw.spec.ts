e1908d8586d827ab782e51a59611c3b5a41b9870
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-07-21 16:48:22 +0300

    fix(legacy): `MultiSelect` has missing checked indicator (#11385)

--- projects/demo-playwright/tests/addon-mobile/mobile-dropdown/mobile-dropdown-with-textfield.pw.spec.ts
+++ projects/demo-playwright/tests/addon-mobile/mobile-dropdown/mobile-dropdown-with-textfield.pw.spec.ts
@@ -9,7 +9,7 @@ const {describe} = test;
 describe('DropdownMobile for textfields', () => {
     test.use(TUI_PLAYWRIGHT_MOBILE);
 
-    test('with select', async ({page, browserName}) => {
+    test('with legacy select', async ({page, browserName}) => {
         // eslint-disable-next-line playwright/no-skipped-test
         test.skip(browserName === 'chromium', 'Font flaky');
 
@@ -23,5 +23,49 @@ describe('DropdownMobile for textfields', () => {
 
         await documentation.hideContent();
         await expect.soft(page).toHaveScreenshot('dropdown-mobile-with-select.png');
+    });
+
+    test('with legacy multi-select', async ({page}) => {
+        await page.goto(DemoRoute.Dropdown);
+
+        const documentation = new TuiDocumentationPagePO(page);
+        const example = documentation.getExample('#mobile');
+
+        await example.locator('tui-multi-select').click();
+        await page.locator('tui-dropdown-mobile [tuiOption]').first().click();
+
+        await expect
+            .soft(page)
+            .toHaveScreenshot('dropdown-mobile-with-legacy-multi-select.png');
+    });
+
+    test('with multi-select (full screen mode)', async ({page}) => {
+        await page.goto(DemoRoute.InputChip);
+
+        const example = new TuiDocumentationPagePO(page).getExample('#mobile');
+
+        await example.locator('tui-textfield[multi][tuiDropdownMobile=""]').click();
+        await page.locator('tui-dropdown-mobile [tuiOption]').first().click();
+
+        await expect
+            .soft(page)
+            .toHaveScreenshot('dropdown-mobile-with-multi-select-fullscreen.png');
+    });
+
+    test('with multi-select (sheet mode)', async ({page}) => {
+        await page.goto(DemoRoute.InputChip);
+
+        const documentation = new TuiDocumentationPagePO(page);
+        const example = documentation.getExample('#mobile');
+
+        await example
+            .locator('tui-textfield[multi][tuiDropdownMobile="Select Pythons"]')
+            .click();
+        await page.locator('tui-dropdown-mobile [tuiOption]').last().click();
+
+        await documentation.hideContent();
+        await expect
+            .soft(page)
+            .toHaveScreenshot('dropdown-mobile-with-multi-select-sheet.png');
     });
 });
