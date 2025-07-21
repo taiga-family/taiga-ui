6f01d98d0bd306fdd34a7d36ee86b380616d070a
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-07-21 15:48:14 +0300

    chore: add pw tests

--- projects/demo-playwright/playwright.options.ts
+++ projects/demo-playwright/playwright.options.ts
@@ -4,7 +4,7 @@ import type {test} from '@playwright/test';
 export const TUI_PLAYWRIGHT_MOBILE_USER_AGENT =
     'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1';
 // Samsung Galaxy Ace GT:
-export const TUI_PLAYWRIGHT_MOBILE_VIEWPORT_WIDTH = 320;
+export const TUI_PLAYWRIGHT_MOBILE_VIEWPORT_WIDTH = 375;
 export const TUI_PLAYWRIGHT_MOBILE_VIEWPORT_HEIGHT = 480;
 
 export const TUI_PLAYWRIGHT_MOBILE: Parameters<typeof test.use>[0] = {
