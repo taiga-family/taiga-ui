4d0aaa1164d88e77c332e97cc2091920730b8720
Максим Иванов <splincodewd@yandex.ru>
2025-07-22 12:49:44 +0300

    chore: infer MarkdownIt types (#11399)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/addon-doc/utils/parse-code-block.ts
+++ projects/addon-doc/utils/parse-code-block.ts
@@ -1,7 +1,7 @@
 import MarkdownIt from 'markdown-it';
 
 export function tuiTryParseMarkdownCodeBlock(text = ''): string[] {
-    const tokens: MarkdownIt.Token[] = new MarkdownIt().parse(text, {});
+    const tokens = new MarkdownIt().parse(text, {});
     const result = tokens
         .filter(({tag, type}) => tag === 'code' && type === 'fence')
         .map(({content}) => content.trim());
