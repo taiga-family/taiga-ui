520e4e052d88000b47e27b236887dcfd1661a2d7
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-07-24 10:24:35 +0300

    chore: new `search` property

--- projects/cdk/types/handler.ts
+++ projects/cdk/types/handler.ts
@@ -2,3 +2,4 @@ export type TuiHandler<T, G> = (item: T) => G;
 export type TuiBooleanHandler<T> = TuiHandler<T, boolean>;
 export type TuiStringHandler<T> = TuiHandler<T, string>;
 export type TuiNumberHandler<T> = TuiHandler<T, number>;
+export type TuiSearchHandler<T> = (query: string, items: T[]) => T | null | undefined;
