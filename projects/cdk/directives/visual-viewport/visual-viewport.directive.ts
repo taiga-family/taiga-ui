aa550bd9924b3917a15a893875a94da34dfa4cc5
splincode <omaxphp@yandex.ru>
2025-07-29 11:16:44 +0300

    fix(cdk): async updating variables of viewport for prevent volatile render mask-image in Safari

--- projects/cdk/directives/visual-viewport/visual-viewport.directive.ts
+++ projects/cdk/directives/visual-viewport/visual-viewport.directive.ts
@@ -1,26 +1,41 @@
-import {Directive, inject} from '@angular/core';
+import {Directive, inject, signal} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {WA_WINDOW} from '@ng-web-apis/common';
 import {ViewportService} from '@ng-web-apis/screen-orientation';
-import {tuiInjectElement, tuiPx} from '@taiga-ui/cdk/utils';
+import {tuiPx} from '@taiga-ui/cdk/utils';
 
 @Directive({
     standalone: true,
     selector: '[tuiVisualViewport]',
+    host: {
+        '[style.--tui-viewport-x]': 'x()',
+        '[style.--tui-viewport-y]': 'y()',
+        '[style.--tui-viewport-height]': 'height()',
+        '[style.--tui-viewport-width]': 'width()',
+        '[style.--tui-viewport-scale]': 'scale()',
+        '[style.--tui-viewport-vh]': 'vh()',
+        '[style.--tui-viewport-vw]': 'vw()',
+    },
 })
 export class TuiVisualViewport {
     private readonly w = inject(WA_WINDOW);
-    private readonly style = tuiInjectElement().style;
+    protected readonly x = signal('');
+    protected readonly y = signal('');
+    protected readonly height = signal('');
+    protected readonly width = signal('');
+    protected readonly scale = signal('');
+    protected readonly vh = signal('');
+    protected readonly vw = signal('');
 
     protected readonly $ = inject(ViewportService)
         .pipe(takeUntilDestroyed())
         .subscribe(({offsetLeft, offsetTop, height, width, scale}) => {
-            this.style.setProperty('--tui-viewport-x', tuiPx(offsetLeft));
-            this.style.setProperty('--tui-viewport-y', tuiPx(offsetTop));
-            this.style.setProperty('--tui-viewport-height', tuiPx(height));
-            this.style.setProperty('--tui-viewport-width', tuiPx(width));
-            this.style.setProperty('--tui-viewport-scale', String(scale));
-            this.style.setProperty('--tui-viewport-vh', tuiPx(this.w.innerHeight / 100));
-            this.style.setProperty('--tui-viewport-vw', tuiPx(this.w.innerWidth / 100));
+            this.x.set(tuiPx(offsetLeft));
+            this.y.set(tuiPx(offsetTop));
+            this.height.set(tuiPx(height));
+            this.width.set(tuiPx(width));
+            this.scale.set(String(scale));
+            this.vh.set(tuiPx(this.w.innerHeight / 100));
+            this.vw.set(tuiPx(this.w.innerWidth / 100));
         });
 }
