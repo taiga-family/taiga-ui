49b1144d78664139cf25500341a804a460ecbdb8
waterplea <alexander@inkin.ru>
2025-08-05 16:46:19 +0700

    chore: fix

--- projects/demo/src/modules/components/range/examples/2/index.ts
+++ projects/demo/src/modules/components/range/examples/2/index.ts
@@ -16,15 +16,15 @@ import {TuiRange} from '@taiga-ui/kit';
 })
 export default class Example {
     protected readonly min = 0;
-    protected readonly max = 1000;
-    protected readonly step = 250;
+    protected readonly max = 100;
+    protected readonly step = 25;
     protected readonly segments = 4;
     protected readonly labels = [...new Array(this.segments + 1).keys()].map(
         (i) => this.min + this.step * i,
     );
 
-    protected value = [0, 250];
+    protected value = [0, 25];
 
     // https://angular.dev/api/common/I18nPluralPipe#example
-    protected pluralMap = {'=0': '0', '=1': '# item', '=1000': 'MAX', other: '# items'};
+    protected pluralMap = {'=0': '0', '=1': '# item', '=100': 'MAX', other: '# items'};
 }
