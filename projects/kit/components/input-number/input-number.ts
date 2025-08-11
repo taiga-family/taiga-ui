19aef91d49c0c7a4f5fbc5377f1e0acb5d118851
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-08-11 10:13:55 +0300

    feat(kit): `InputNumber` & `InputSlider` support `quantum` property (#11567)

--- projects/kit/components/input-number/input-number.ts
+++ projects/kit/components/input-number/input-number.ts
@@ -1,4 +1,9 @@
 import {TuiInputNumberDirective} from './input-number.directive';
+import {TuiQuantumValueTransformer} from './quantum.directive';
 import {TuiInputNumberStep} from './step/input-number-step.component';
 
-export const TuiInputNumber = [TuiInputNumberDirective, TuiInputNumberStep] as const;
+export const TuiInputNumber = [
+    TuiInputNumberDirective,
+    TuiInputNumberStep,
+    TuiQuantumValueTransformer,
+] as const;
