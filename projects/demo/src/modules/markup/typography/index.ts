7ebfffd113e177adb54896b02ce7c0ee8a79622e
r.sergeev <r.sergeev@yadro.com>
2025-08-18 21:47:28 +0300

    chore: use computed font size

--- projects/demo/src/modules/markup/typography/index.ts
+++ projects/demo/src/modules/markup/typography/index.ts
@@ -1,5 +1,6 @@
 import {ClipboardModule} from '@angular/cdk/clipboard';
-import {Component} from '@angular/core';
+import {DOCUMENT, isPlatformServer} from '@angular/common';
+import {Component, inject, PLATFORM_ID} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {DemoRoute} from '@demo/routes';
 import {TuiDemo} from '@demo/utils';
@@ -13,4 +14,12 @@ import {TuiDemo} from '@demo/utils';
 })
 export default class Page {
     protected readonly routes = DemoRoute;
+    private readonly doc = inject(DOCUMENT);
+    private readonly isServer = isPlatformServer(inject(PLATFORM_ID));
+
+    protected computedFontSize(className: string): string | null {
+        if (this.isServer) return null;
+        const element = this.doc.getElementsByClassName(className)?.item(0);
+        return element ? getComputedStyle(element).fontSize : null;
+    }
 }
