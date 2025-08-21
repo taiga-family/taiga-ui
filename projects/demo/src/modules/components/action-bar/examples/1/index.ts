3d7f87c58ff4deb97a0a505e44bc7c79a80649a3
Vladimir Potekhin <vladimir.potekh@gmail.com>
2025-08-21 11:47:23 +0300

    chore: change example

--- projects/demo/src/modules/components/action-bar/examples/1/index.ts
+++ projects/demo/src/modules/components/action-bar/examples/1/index.ts
@@ -37,7 +37,7 @@ import {map} from 'rxjs';
     changeDetection,
 })
 export default class Example {
-    protected items = ['one', 'two', 'three'];
+    protected items = ['one', 'two', 'three', 'four'];
     protected control = new FormControl<string[]>([]);
     protected expanded = false;
 
