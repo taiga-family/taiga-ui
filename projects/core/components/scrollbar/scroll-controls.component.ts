05f412b564dbfc6ec562633ef3cb036c0f26081a
mdlufy <german.panov.2018@mail.ru>
2025-08-23 01:19:19 +0300

    chore(core): `Scrollbar` test performance optimization

--- projects/core/components/scrollbar/scroll-controls.component.ts
+++ projects/core/components/scrollbar/scroll-controls.component.ts
@@ -1,10 +1,26 @@
-import {AsyncPipe, NgIf} from '@angular/common';
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
-import {WA_ANIMATION_FRAME} from '@ng-web-apis/common';
+import {NgIf} from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    inject,
+    INJECTOR,
+    Injector,
+} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {
+    MutationObserverService,
+    provideMutationObserverInit,
+} from '@ng-web-apis/mutation-observer';
+import {ResizeObserverService} from '@ng-web-apis/resize-observer';
 import {TuiAnimated} from '@taiga-ui/cdk/directives/animated';
-import {tuiZonefreeScheduler, tuiZoneOptimized} from '@taiga-ui/cdk/observables';
+import {
+    tuiTypedFromEvent,
+    tuiZonefreeScheduler,
+    tuiZoneOptimized,
+} from '@taiga-ui/cdk/observables';
 import {TUI_SCROLL_REF} from '@taiga-ui/core/tokens';
-import {distinctUntilChanged, map, startWith, throttleTime} from 'rxjs';
+import {distinctUntilChanged, map, merge, startWith, throttleTime} from 'rxjs';
 
 import {TuiScrollbarDirective} from './scrollbar.directive';
 import {TUI_SCROLLBAR_OPTIONS} from './scrollbar.options';
@@ -12,22 +28,60 @@ import {TUI_SCROLLBAR_OPTIONS} from './scrollbar.options';
 @Component({
     standalone: true,
     selector: 'tui-scroll-controls',
-    imports: [AsyncPipe, NgIf, TuiAnimated, TuiScrollbarDirective],
+    imports: [NgIf, TuiAnimated, TuiScrollbarDirective],
     templateUrl: './scroll-controls.template.html',
     styleUrls: ['./scroll-controls.style.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TuiScrollControls {
-    private readonly scrollRef = inject(TUI_SCROLL_REF).nativeElement;
+    private readonly scrollRef: HTMLElement = inject(TUI_SCROLL_REF).nativeElement;
 
-    protected readonly nativeScrollbar = inject(TUI_SCROLLBAR_OPTIONS).mode === 'native';
-    protected readonly refresh$ = inject(WA_ANIMATION_FRAME).pipe(
+    private readonly injector = inject(INJECTOR);
+
+    private readonly resizeObserverService = Injector.create({
+        providers: [
+            ResizeObserverService,
+            {
+                provide: ElementRef,
+                useFactory: () => new ElementRef(this.scrollRef),
+            },
+        ],
+        parent: this.injector,
+    }).get(ResizeObserverService);
+
+    private readonly mutationObserverService = Injector.create({
+        providers: [
+            MutationObserverService,
+            provideMutationObserverInit({
+                childList: true,
+                characterData: true,
+                subtree: true,
+            }),
+            {
+                provide: ElementRef,
+                useFactory: () => new ElementRef(this.scrollRef),
+            },
+        ],
+        parent: this.injector,
+    }).get(MutationObserverService);
+
+    protected readonly refresh$ = merge(
+        this.resizeObserverService.pipe(map(() => null)),
+        this.mutationObserverService.pipe(map(() => null)),
+        tuiTypedFromEvent(this.scrollRef, 'scroll').pipe(map(() => null)),
+    ).pipe(
         throttleTime(300, tuiZonefreeScheduler()),
         map(() => this.scrollbars),
-        startWith([false, false]),
+        startWith([false, false] as [boolean, boolean]),
         distinctUntilChanged((a, b) => a[0] === b[0] && a[1] === b[1]),
         tuiZoneOptimized(),
     );
+
+    protected readonly refresh = toSignal(this.refresh$, {
+        initialValue: [false, false] as [boolean, boolean],
+    });
+
+    protected readonly nativeScrollbar = inject(TUI_SCROLLBAR_OPTIONS).mode === 'native';
 
     private get scrollbars(): [boolean, boolean] {
         const {clientHeight, scrollHeight, clientWidth, scrollWidth} = this.scrollRef;
