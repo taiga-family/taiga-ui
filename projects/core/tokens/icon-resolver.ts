2af7f614ebbfef65031a84bde4d4f2bca78d8119
Gleb Kiva <feironox5@gmail.com>
2025-08-29 02:21:41 +0300

    feat(core, kit): implement icon mode resolution system (#10938)

--- projects/core/tokens/icon-resolver.ts
+++ projects/core/tokens/icon-resolver.ts
@@ -4,14 +4,25 @@ import {type TuiStringHandler} from '@taiga-ui/cdk/types';
 import {TUI_ASSETS_PATH} from './assets-path';
 import {TUI_ICON_REGISTRY} from './icons';
 
+const ICON_MODE_PREFIXES = {
+    font: '@font.',
+    image: '@img.',
+};
+
+type TUI_ICON_MODE = 'font' | 'image' | 'svg';
+
 export const TUI_ICON_RESOLVER = new InjectionToken<TuiStringHandler<string>>(
     ngDevMode ? 'TUI_ICON_RESOLVER' : '',
     {
         factory: () => {
             const path = inject(TUI_ASSETS_PATH);
 
+            // regex for @{any text}. used as a default fallback, returns path to svg from icon pack
             return (icon) =>
-                `${path}/${icon.replace('@tui.', '').split('.').join('/')}.svg`;
+                `${path}/${icon
+                    .replace(/@[a-zA-Z]+\./, '')
+                    .split('.')
+                    .join('/')}.svg`;
         },
     },
 );
@@ -21,12 +32,37 @@ export const TUI_ICON_RESOLVER = new InjectionToken<TuiStringHandler<string>>(
  */
 export const TUI_ICON_START_RESOLVER = TUI_ICON_RESOLVER;
 
+export function tuiGetIconMode(icon?: string | null): TUI_ICON_MODE | null {
+    if (!icon) {
+        return null;
+    }
+
+    if (icon.startsWith(ICON_MODE_PREFIXES.image)) {
+        return 'image';
+    }
+
+    if (icon.startsWith(ICON_MODE_PREFIXES.font)) {
+        return 'font';
+    }
+
+    return 'svg';
+}
+
 export function tuiInjectIconResolver(): TuiStringHandler<string> {
     const icons = inject(TUI_ICON_REGISTRY);
     const resolver = inject(TUI_ICON_RESOLVER);
 
-    return (icon) =>
-        !icon || icon.includes('/') ? icon : (icons[icon] ?? resolver(icon));
+    return (icon) => {
+        if (!icon || icon.includes('/')) {
+            return icon;
+        }
+
+        if (tuiGetIconMode(icon) === 'font') {
+            return icon.slice(ICON_MODE_PREFIXES.font.length);
+        }
+
+        return icons[icon] ?? resolver(icon);
+    };
 }
 
 export function tuiIconResolverProvider(useValue: TuiStringHandler<string>): Provider {
