bc5a7786faf7e3e6bf97ed72ac26d64095682e5a
Alex Inkin <alexander@inkin.ru>
2025-09-01 22:06:37 +0700

    feat(cdk): `provideOptions` allow functions to extract options from DI (#11805)

--- projects/cdk/utils/di/create-options.ts
+++ projects/cdk/utils/di/create-options.ts
@@ -1,11 +1,14 @@
 import {type FactoryProvider, InjectionToken, type ProviderToken} from '@angular/core';
 import {tuiProvideOptions} from '@taiga-ui/cdk/utils/miscellaneous';
 
+// TODO: Drop ProviderToken in v.5
 export function tuiCreateOptions<T>(
     defaults: T,
 ): [
     token: InjectionToken<T>,
-    provider: (item: Partial<T> | ProviderToken<Partial<T>>) => FactoryProvider,
+    provider: (
+        item: Partial<T> | ProviderToken<Partial<T>> | (() => Partial<T>),
+    ) => FactoryProvider,
 ] {
     const token = new InjectionToken(ngDevMode ? 'token' : '', {
         factory: () => defaults,
--- projects/cdk/utils/miscellaneous/provide-options.ts
+++ projects/cdk/utils/miscellaneous/provide-options.ts
@@ -5,16 +5,19 @@ import {
     type ProviderToken,
 } from '@angular/core';
 
+// TODO: Drop ProviderToken in v.5
 export function tuiProvideOptions<T>(
     provide: InjectionToken<T>,
-    options: Partial<T> | ProviderToken<Partial<T>>,
+    options: Partial<T> | ProviderToken<Partial<T>> | (() => Partial<T>),
     fallback: T,
 ): FactoryProvider {
     return {
         provide,
         useFactory: (): T => ({
             ...(inject(provide, {optional: true, skipSelf: true}) || fallback),
-            ...(inject(options as any, {optional: true}) || options),
+            ...(inject(options as any, {optional: true}) ||
+                // @ts-ignore
+                (typeof options === 'function' ? options() : options)),
         }),
     };
 }
