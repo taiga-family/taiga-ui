459fb333ba423cacb744fee07d186036aeb75682
mdlufy <german.panov.2018@mail.ru>
2025-09-05 16:42:19 +0300

    fix(core): `TUI_DARK_MODE` factory effect in reactive context

--- projects/core/tokens/dark-mode.ts
+++ projects/core/tokens/dark-mode.ts
@@ -1,4 +1,11 @@
-import {effect, inject, InjectionToken, signal, type WritableSignal} from '@angular/core';
+import {
+    effect,
+    inject,
+    InjectionToken,
+    signal,
+    untracked,
+    type WritableSignal,
+} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {WA_LOCAL_STORAGE, WA_WINDOW} from '@ng-web-apis/common';
 import {filter, fromEvent} from 'rxjs';
@@ -13,41 +20,42 @@ export const TUI_DARK_MODE_KEY = new InjectionToken(
 export const TUI_DARK_MODE = new InjectionToken<
     WritableSignal<boolean> & {reset(): void}
 >(ngDevMode ? 'TUI_DARK_MODE' : '', {
-    factory: () => {
-        let automatic = true;
+    factory: () =>
+        untracked(() => {
+            let automatic = true;
 
-        const storage = inject(WA_LOCAL_STORAGE);
-        const key = inject(TUI_DARK_MODE_KEY);
-        const saved = storage?.getItem(key);
-        const media = inject(WA_WINDOW).matchMedia('(prefers-color-scheme: dark)');
-        const result = signal(Boolean((saved && JSON.parse(saved)) ?? media.matches));
+            const storage = inject(WA_LOCAL_STORAGE);
+            const key = inject(TUI_DARK_MODE_KEY);
+            const saved = storage?.getItem(key);
+            const media = inject(WA_WINDOW).matchMedia('(prefers-color-scheme: dark)');
+            const result = signal(Boolean((saved && JSON.parse(saved)) ?? media.matches));
 
-        fromEvent(media, 'change')
-            .pipe(
-                filter(() => !storage?.getItem(key)),
-                takeUntilDestroyed(),
-            )
-            .subscribe(() => {
-                automatic = true;
-                result.set(media.matches);
+            fromEvent(media, 'change')
+                .pipe(
+                    filter(() => !storage?.getItem(key)),
+                    takeUntilDestroyed(),
+                )
+                .subscribe(() => {
+                    automatic = true;
+                    result.set(media.matches);
+                });
+
+            effect(() => {
+                const value = String(result());
+
+                if (automatic) {
+                    automatic = false;
+                } else {
+                    storage?.setItem(key, value);
+                }
             });
 
-        effect(() => {
-            const value = String(result());
-
-            if (automatic) {
-                automatic = false;
-            } else {
-                storage?.setItem(key, value);
-            }
-        });
-
-        return Object.assign(result, {
-            reset: () => {
-                storage?.removeItem(key);
-                automatic = true;
-                result.set(media.matches);
-            },
-        });
-    },
+            return Object.assign(result, {
+                reset: () => {
+                    storage?.removeItem(key);
+                    automatic = true;
+                    result.set(media.matches);
+                },
+            });
+        }),
 });
