99d18100f15c263b5a5256c2762e5bb86203289e
splincode <omaxphp@yandex.ru>
2025-09-09 16:21:37 +0300

    chore(demo): add patch method in example to input-slider page

--- projects/demo/src/modules/components/input-slider/examples/5/index.ts
+++ projects/demo/src/modules/components/input-slider/examples/5/index.ts
@@ -3,12 +3,12 @@ import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiTextfield} from '@taiga-ui/core';
+import {TuiButton, TuiTextfield} from '@taiga-ui/core';
 import {TuiInputSlider} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
-    imports: [FormsModule, JsonPipe, TuiInputSlider, TuiTextfield],
+    imports: [FormsModule, JsonPipe, TuiButton, TuiInputSlider, TuiTextfield],
     templateUrl: './index.html',
     encapsulation,
     changeDetection,
