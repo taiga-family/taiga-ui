de54724307fb3f8a5c5285b0995f6c86c988e7f7
Vladimir Potekhin <vladimir.potekh@gmail.com>
2025-09-10 17:23:01 +0300

    chore: update algolia, add assistant

--- projects/demo/src/modules/app/search/env.ts
+++ projects/demo/src/modules/app/search/env.ts
@@ -7,5 +7,9 @@ export const SEARCH_CONFIG = new InjectionToken(ngDevMode ? 'SEARCH_CONFIG' : ''
         indexName: 'taiga-ui',
         // Public key
         apiKey: '954fc3fb1ed14929de8006f229f545a6',
+        askAi: {
+            indexName: 'taiga-ui-markdown',
+            assistantId: 'oYuUTDL7oItC',
+        },
     }),
 });
