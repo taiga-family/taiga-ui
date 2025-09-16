6c67ccfa3bb09c684b71fcd403e2a2b5fa3ca914
Alex Inkin <alexander@inkin.ru>
2025-09-16 23:34:04 +0700

    refactor(addon-commerce)!: `InputCard` drop options and `icon` input (#11940)

--- projects/addon-commerce/components/input-cvc/input-cvc.directive.ts
+++ projects/addon-commerce/components/input-cvc/input-cvc.directive.ts
@@ -1,6 +1,5 @@
-import {Directive, inject, Input, type OnChanges} from '@angular/core';
+import {Directive, Input, type OnChanges} from '@angular/core';
 import {MaskitoDirective} from '@maskito/angular';
-import {TUI_INPUT_CARD_OPTIONS} from '@taiga-ui/addon-commerce/components/input-card';
 import {TUI_MASK_CVC} from '@taiga-ui/addon-commerce/constants';
 import {TuiWithTextfield} from '@taiga-ui/core/components/textfield';
 import {tuiMaskito} from '@taiga-ui/kit/utils';
@@ -19,10 +18,6 @@ import {tuiMaskito} from '@taiga-ui/kit/utils';
 })
 export class TuiInputCVC implements OnChanges {
     private readonly mask = tuiMaskito(TUI_MASK_CVC(3));
-
-    /** @deprecated apparently "off" doesn't disable autocomplete */
-    @Input()
-    public autocomplete = inject(TUI_INPUT_CARD_OPTIONS).autocomplete;
 
     @Input()
     public hidden = true;
