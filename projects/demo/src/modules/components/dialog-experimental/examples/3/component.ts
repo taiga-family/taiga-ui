85c74efd2dec06770dc07eb484c00724f52a2a9b
waterplea <alexander@inkin.ru>
2025-09-18 13:34:09 +0400

    refactor(core): `Dialogs` drop overlay in favor of backdrop-filter

--- projects/demo/src/modules/components/dialog-experimental/examples/3/component.ts
+++ projects/demo/src/modules/components/dialog-experimental/examples/3/component.ts
@@ -1,22 +1,40 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {TuiAutoFocus} from '@taiga-ui/cdk';
+import {TuiDropdownMobile} from '@taiga-ui/addon-mobile';
 import {TuiButton, TuiTextfield} from '@taiga-ui/core';
 import {type TuiDialogContext} from '@taiga-ui/experimental';
+import {TuiChevron, TuiDataListWrapper, TuiSelect} from '@taiga-ui/kit';
 import {TuiForm} from '@taiga-ui/layout';
 import {injectContext} from '@taiga-ui/polymorpheus';
 
 @Component({
-    imports: [FormsModule, TuiAutoFocus, TuiButton, TuiForm, TuiTextfield],
+    imports: [
+        FormsModule,
+        TuiButton,
+        TuiForm,
+        TuiTextfield,
+        TuiSelect,
+        TuiChevron,
+        TuiDropdownMobile,
+        TuiDataListWrapper,
+    ],
     template: `
         <form tuiForm>
-            <tui-textfield>
+            <tui-textfield
+                tuiChevron
+                tuiDropdownMobile="Your name"
+                [tuiTextfieldCleaner]="false"
+            >
                 <label tuiLabel>Your name</label>
                 <input
                     name="name"
-                    tuiAutoFocus
-                    tuiTextfield
+                    tuiSelect
                     [(ngModel)]="value"
+                />
+                <tui-data-list-wrapper
+                    *tuiTextfieldDropdown
+                    new
+                    [items]="items"
                 />
             </tui-textfield>
             <footer>
@@ -42,6 +60,7 @@ import {injectContext} from '@taiga-ui/polymorpheus';
 })
 export class DialogComponent {
     protected readonly context = injectContext<TuiDialogContext<string, string>>();
+    protected readonly items = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
 
     protected value = this.context.data;
 }
