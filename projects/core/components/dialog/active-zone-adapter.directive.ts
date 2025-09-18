df93fc87f373ceb8ba37f41407ebb0772711a2d8
waterplea <alexander@inkin.ru>
2025-09-18 20:01:31 +0400

    feat(core): `Dialogs` auto attach to current active zone

---
+++ projects/core/components/dialog/active-zone-adapter.directive.ts
@@ -0,0 +1,36 @@
+import {DOCUMENT} from '@angular/common';
+import {Directive, inject, type OnDestroy, type OnInit} from '@angular/core';
+import {TuiActiveZone} from '@taiga-ui/cdk/directives/active-zone';
+import {tuiGetNativeFocused} from '@taiga-ui/cdk/utils/focus';
+
+@Directive({
+    selector: '[tuiActiveZoneAdapter]',
+    hostDirectives: [TuiActiveZone],
+})
+export class TuiActiveZoneAdapter implements OnDestroy, OnInit {
+    private readonly current = inject(TuiActiveZone);
+    private readonly parent = findActive(
+        inject(TuiActiveZone, {skipSelf: true}),
+        tuiGetNativeFocused(inject(DOCUMENT)),
+    );
+
+    public ngOnInit(): void {
+        this.current.tuiActiveZoneParentSetter = this.parent;
+    }
+
+    public ngOnDestroy(): void {
+        this.current.tuiActiveZoneParentSetter = null;
+    }
+}
+
+function findActive(zone: TuiActiveZone, element: Element | null): TuiActiveZone | null {
+    if (!element || !zone.contains(element)) {
+        return null;
+    } else if (!zone['subActiveZones'].length) {
+        return zone;
+    }
+
+    const active = zone['subActiveZones'].find((child) => child.contains(element));
+
+    return active ? findActive(active, element) : zone;
+}
