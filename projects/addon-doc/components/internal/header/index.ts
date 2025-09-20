c0b83a87d69a889ec425bdffdc1b406473487fbb
mdlufy <german.panov.2018@mail.ru>
2025-09-20 19:14:03 +0300

    chore(addon-doc): `Sidebar` detach

--- projects/addon-doc/components/internal/header/index.ts
+++ projects/addon-doc/components/internal/header/index.ts
@@ -2,10 +2,11 @@ import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {Router} from '@angular/router';
 import {TUI_DOC_ICONS, TUI_DOC_LOGO, TUI_DOC_MENU_TEXT} from '@taiga-ui/addon-doc/tokens';
-import {TuiSidebar} from '@taiga-ui/addon-mobile/directives/sidebar';
 import {TUI_FALSE_HANDLER} from '@taiga-ui/cdk/constants';
 import {TuiActiveZone} from '@taiga-ui/cdk/directives/active-zone';
 import {TuiButton} from '@taiga-ui/core/components/button';
+import {TuiPopup} from '@taiga-ui/core/directives/popup';
+import {TuiDrawer} from '@taiga-ui/kit/components/drawer';
 import {PolymorpheusOutlet} from '@taiga-ui/polymorpheus';
 import {distinctUntilChanged, map, merge, startWith, Subject} from 'rxjs';
 
@@ -13,14 +14,21 @@ import {TuiDocNavigation} from '../../navigation/navigation.component';
 
 @Component({
     selector: 'header[tuiDocHeader]',
-    imports: [PolymorpheusOutlet, TuiActiveZone, TuiButton, TuiDocNavigation, TuiSidebar],
+    imports: [
+        PolymorpheusOutlet,
+        TuiActiveZone,
+        TuiButton,
+        TuiDocNavigation,
+        TuiDrawer,
+        TuiPopup,
+    ],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TuiDocHeader {
-    private readonly stream$ = new Subject<boolean>();
     private readonly router = inject(Router);
+    protected readonly stream$ = new Subject<boolean>();
     protected readonly icons = inject(TUI_DOC_ICONS);
     protected readonly logo = inject(TUI_DOC_LOGO);
     protected readonly menu = inject(TUI_DOC_MENU_TEXT);
@@ -32,14 +40,4 @@ export class TuiDocHeader {
         ),
         {initialValue: false},
     );
-
-    protected onClick(): void {
-        this.stream$.next(true);
-    }
-
-    protected onActiveZone(active: boolean): void {
-        if (!active) {
-            this.stream$.next(false);
-        }
-    }
 }
