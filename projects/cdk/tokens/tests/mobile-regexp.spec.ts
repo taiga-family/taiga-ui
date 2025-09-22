e6b1834f265e73aee7eaa50f18f47aeea1fc6fd2
mdlufy <german.panov.2018@mail.ru>
2025-09-22 13:34:57 +0300

    chore(cdk): `TUI_MOBILE_REGEXP` add tests

--- projects/cdk/tokens/tests/mobile-regexp.spec.ts
+++ projects/cdk/tokens/tests/mobile-regexp.spec.ts
@@ -32,9 +32,13 @@ const DESKTOP_USER_AGENTS = [
 
     // Android tablet
     'Mozilla/5.0 (Linux; Android 10; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 11; Lenovo YT-J706X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+    'Dalvik/2.1.0 (Linux; U; Android 14; SM-P619N Build/UP1A.231005.007)',
+    'Dalvik/2.1.0 (Linux; U; Android 15; 24091RPADG Build/AQ3A.240801.002)',
 
     // iOS (iPad)
     'Mozilla/5.0 (iPad; CPU OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1',
+    'Mozilla/5.0 (iPad16,3; CPU OS 18_3_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Tropicana_NJ/5.7.1',
 
     // Linux
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
