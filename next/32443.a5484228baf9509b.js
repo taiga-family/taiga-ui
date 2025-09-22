f5de2c79c29f585cfdc9f3b4187e855e6431e347
waterplea <waterplea@users.noreply.github.com>
2025-09-22 05:39:18 +0000

    deploy: 6b221d8bf0456f56f5797a42b5db8805a9f044d9

--- next/32443.a5484228baf9509b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[32443],{32443:n=>{n.exports=".container {\n    inline-size: 12.5rem;\n    block-size: 12.5rem;\n}\n\n.sidebar {\n    inline-size: 100%;\n    block-size: 100%;\n}\n\n.container,\n.sidebar {\n    display: flex;\n    justify-content: center;\n    align-items: center;\n}\n"}}]);
