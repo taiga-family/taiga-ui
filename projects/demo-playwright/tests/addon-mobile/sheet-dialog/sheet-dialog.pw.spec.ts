49bed773b73ec8288bb808195f975b93af55124d
Максим Иванов <splincodewd@yandex.ru>
2025-09-23 16:18:58 +0300

    fix(addon-mobile): close sheet dialog when navigating to a new route (#12038)

---
+++ projects/demo-playwright/tests/addon-mobile/sheet-dialog/sheet-dialog.pw.spec.ts
@@ -0,0 +1,27 @@
+import {DemoRoute} from '@demo/routes';
+import {TuiDocumentationApiPagePO, tuiGoto} from '@demo-playwright/utils';
+import {expect, test} from '@playwright/test';
+
+import {TUI_PLAYWRIGHT_MOBILE} from '../../../playwright.options';
+
+test.describe('SheetDialog', () => {
+    test.use(TUI_PLAYWRIGHT_MOBILE);
+
+    test('Close sheet by route navigation', async ({page}) => {
+        await tuiGoto(page, DemoRoute.SheetDialog);
+        await page.locator('tui-tabs a:has-text("API")').click();
+
+        const example = new TuiDocumentationApiPagePO(page).apiPageExample;
+        const button = example.locator('button:has-text("Click")');
+
+        await button.click();
+        await expect.soft(page).toHaveScreenshot('01-sheet-dialog.png');
+
+        await page.evaluate(() => {
+            history.back();
+            history.back();
+        });
+
+        await expect.soft(page).toHaveScreenshot('02-sheet-dialog.png');
+    });
+});
