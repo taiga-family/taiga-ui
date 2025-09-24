b0cd8ec1d7a970c3cdae629b043606ff3e251c78
waterplea <alexander@inkin.ru>
2025-09-24 12:31:44 +0400

    chore: fix

--- projects/demo-playwright/tests/core/error/error.pw.spec.ts
+++ projects/demo-playwright/tests/core/error/error.pw.spec.ts
@@ -4,52 +4,45 @@ import {expect, test} from '@playwright/test';
 
 test.describe('TuiError', () => {
     test('No extra space between content', async ({page}) => {
-        await tuiGoto(page, `${DemoRoute.Error}#basic`);
-        const example = new TuiDocumentationPagePO(page).getExample('#basic');
+        await tuiGoto(page, DemoRoute.Error);
 
+        const example = new TuiDocumentationPagePO(page).getExample('#basic');
         const checkbox = example.locator('input[tuiSwitch]');
         const error = example.locator('tui-error');
 
         await expect(error).toBeHidden();
-
         await checkbox.click();
-
         await expect(checkbox).toBeChecked();
         await expect(error).toBeVisible();
         await expect(error).toHaveText('An error');
-
         await expect
             .soft(example)
             .toHaveScreenshot('01-error.png', {animations: 'allow'});
     });
 
     test('Errors of invalid control are shown correctly', async ({page}) => {
-        const example = new TuiDocumentationPagePO(page).getExample('#base');
-
         await tuiGoto(page, DemoRoute.Error);
 
+        const example = new TuiDocumentationPagePO(page).getExample('#basic');
         const input = example.locator('input').first();
 
         await input.focus();
         await input.blur();
-
         await page.waitForTimeout(500);
-
-        await expect.soft(example).toHaveScreenshot('02-error.png', {
-            animations: 'allow',
-        });
+        await expect
+            .soft(example)
+            .toHaveScreenshot('02-error.png', {animations: 'allow'});
     });
 
     test('Submit invalid form', async ({page}) => {
+        await tuiGoto(page, DemoRoute.Error);
+
         const example = new TuiDocumentationPagePO(page).getExample('#template');
 
-        await tuiGoto(page, DemoRoute.Error);
         await example.locator('button').last().click();
-
         await page.waitForTimeout(500);
-
-        await expect.soft(example).toHaveScreenshot('03-error.png', {
-            animations: 'allow',
-        });
+        await expect
+            .soft(example)
+            .toHaveScreenshot('03-error.png', {animations: 'allow'});
     });
 });
