a7a63e7bfce899fadea6b4a8b6178f1fc24fd6cc
waterplea <waterplea@users.noreply.github.com>
2025-09-24 13:18:10 +0000

    deploy: 77f8926e709fb4f9c409a6c38cec424db82ad3ef

--- next/1616.91568c2e11e540ed.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[1616],{1616:r=>{r.exports='```html\n<tui-error [error]="error"></tui-error>\n```\n'}}]);
--- next/95013.5afdaeb55bd12bbb.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[95013],{95013:e=>{e.exports=".table {\n    inline-size: 100%;\n    table-layout: fixed;\n}\n\n.error {\n    color: #fff;\n}\n"}}]);
