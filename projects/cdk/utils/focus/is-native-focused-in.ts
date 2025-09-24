d36c222c91e975531122bf27f3e468fa1e76ce39
Максим Иванов <splincodewd@yandex.ru>
2025-09-24 10:55:42 +0300

    refactor: deprecate some function (#12011)

--- projects/cdk/utils/focus/is-native-focused-in.ts
+++ projects/cdk/utils/focus/is-native-focused-in.ts
@@ -6,8 +6,15 @@ import {tuiGetNativeFocused} from './get-native-focused';
  * @param node
  * @return true if focused node is contained within element
  */
-export function tuiIsNativeFocusedIn(node?: Node | null): boolean {
+export function tuiIsFocusedIn(node?: Node | null): boolean {
     const focused = node?.ownerDocument && tuiGetNativeFocused(node.ownerDocument);
 
     return !!focused && node.contains(focused) && node.ownerDocument?.hasFocus();
 }
+
+export {
+    /**
+     * @deprecated use {@link tuiIsFocusedIn}
+     */
+    tuiIsFocusedIn as tuiIsNativeFocusedIn,
+};
