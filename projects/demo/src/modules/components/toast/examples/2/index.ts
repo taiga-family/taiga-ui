d8915e6aacc8f467fd97d7132aaf792e4fe29b68
Максим Иванов <splincodewd@yandex.ru>
2025-09-30 12:01:48 +0300

    feat(kit): `Toast` add service (#11959)

--- projects/demo/src/modules/components/toast/examples/2/index.ts
+++ projects/demo/src/modules/components/toast/examples/2/index.ts
@@ -1,16 +1,27 @@
-import {Component} from '@angular/core';
+import {isPlatformServer} from '@angular/common';
+import {Component, inject, PLATFORM_ID} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiPlatform} from '@taiga-ui/cdk';
+import {TUI_IS_E2E, TuiPlatform} from '@taiga-ui/cdk';
 import {TuiButton, TuiLoader} from '@taiga-ui/core';
-import {TuiToast} from '@taiga-ui/kit';
+import {TuiProgressCircle, TuiToast} from '@taiga-ui/kit';
+import {BehaviorSubject, of, switchMap, take, timer} from 'rxjs';
 
 @Component({
     standalone: true,
-    imports: [TuiButton, TuiLoader, TuiPlatform, TuiToast],
+    imports: [TuiButton, TuiLoader, TuiPlatform, TuiProgressCircle, TuiToast],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation,
     changeDetection,
 })
-export default class Example {}
+export default class Example {
+    protected readonly trigger$ = new BehaviorSubject(0);
+    protected readonly value = toSignal(
+        inject(TUI_IS_E2E) || isPlatformServer(inject(PLATFORM_ID))
+            ? of(30)
+            : this.trigger$.pipe(switchMap(() => timer(0, 200).pipe(take(100)))),
+        {initialValue: 0},
+    );
+}
---
+++ projects/kit/components/toast/toast.ts
@@ -0,0 +1,4 @@
+import {TuiToastDirective} from './toast.directive';
+import {TuiToastTemplate} from './toast.service';
+
+export const TuiToast = [TuiToastDirective, TuiToastTemplate] as const;
