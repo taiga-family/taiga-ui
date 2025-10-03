0d9fd09826678dc0ae14a4bfc6d95f8c155f5ef1
waterplea <alexander@inkin.ru>
2025-10-03 17:23:26 +0400

    chore: fix

--- projects/demo-cypress/src/tests/nested-root.cy.ts
+++ projects/demo-cypress/src/tests/nested-root.cy.ts
@@ -87,11 +87,11 @@ describe('Nested root', () => {
 
         cy.get('button').contains('Root dialog').click();
         cy.get('body').compareSnapshot('tui-nested-root__1');
-        cy.get('body').click();
+        cy.get('body').click(0, 0);
 
         cy.get('button').contains('Nested dialog').click();
         cy.get('body').compareSnapshot('tui-nested-root__2');
-        cy.get('body').click();
+        cy.get('body').click(0, 0);
 
         cy.get('tui-select').eq(1).click();
         cy.get('body').compareSnapshot('tui-nested-root__3');
