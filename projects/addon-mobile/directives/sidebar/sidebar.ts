88cb6e46a170b4ca282cdd90751f5cdb01e64cbe
mdlufy <german.panov.2018@mail.ru>
2025-10-06 17:06:28 +0300

    chore(addon-mobile): `Sidebar` remove

--- projects/addon-mobile/directives/sidebar/sidebar.ts
+++
@@ -1,7 +0,0 @@
-import {TuiSidebarComponent} from './sidebar.component';
-import {TuiSidebarDirective} from './sidebar.directive';
-
-/**
- * @deprecated use {@link TuiDrawer} instead
- */
-export const TuiSidebar = [TuiSidebarComponent, TuiSidebarDirective] as const;
