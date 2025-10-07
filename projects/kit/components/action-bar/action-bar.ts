5ee32fc6303e868d0b11417a2f0b4ac8a545d89d
waterplea <alexander@inkin.ru>
2025-10-07 16:15:59 +0400

    chore: fix

--- projects/kit/components/action-bar/action-bar.ts
+++
@@ -1,5 +0,0 @@
-import {TuiPopup} from '@taiga-ui/core/directives/popup';
-
-import {TuiActionBarComponent} from './action-bar.component';
-
-export const TuiActionBar = [TuiActionBarComponent, TuiPopup] as const;
--- projects/kit/components/action-bar/index.ts
+++ projects/kit/components/action-bar/index.ts
@@ -1,2 +1,1 @@
-export * from './action-bar';
 export * from './action-bar.component';
