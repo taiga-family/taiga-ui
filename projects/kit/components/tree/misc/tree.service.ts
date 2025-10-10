40f1691c8b35014df4ca59d55c23857e090a70e5
jonata biondi <jonata.biondi@gmail.com>
2025-10-10 15:41:41 +0200

    chore(kit): implement clear method

--- projects/kit/components/tree/misc/tree.service.ts
+++ projects/kit/components/tree/misc/tree.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {map, mergeMap, startWith, Subject, tap} from 'rxjs';
+import {map, merge, mergeMap, startWith, Subject, tap} from 'rxjs';
 
 import {type TuiTreeLoader} from './tree.interfaces';
 import {TUI_TREE_LOADER, TUI_TREE_LOADING, TUI_TREE_START} from './tree.tokens';
@@ -9,22 +9,34 @@ export class TuiTreeService<T> {
     private readonly loading = inject<T>(TUI_TREE_LOADING);
     private readonly start = inject<T>(TUI_TREE_START);
     private readonly loader = inject<TuiTreeLoader<T>>(TUI_TREE_LOADER);
-    private readonly map = new Map<T, readonly T[]>([[this.loading, []]]);
-    private readonly load$ = new Subject<T>();
 
-    public readonly data$ = this.load$.pipe(
-        mergeMap((item) =>
-            this.loader.loadChildren(item).pipe(
-                tap((children) => this.map.set(item, children)),
-                map((children) =>
-                    children.filter((item) => !this.loader.hasChildren(item)),
+    private map = new Map<T, readonly T[]>([[this.loading, []]]);
+
+    private readonly load$ = new Subject<T>();
+    private readonly reset$ = new Subject<void>();
+
+    public readonly data$ = merge(
+        this.load$.pipe(
+            mergeMap((item) =>
+                this.loader.loadChildren(item).pipe(
+                    tap((children) => this.map.set(item, children)),
+                    map((children) =>
+                        children.filter((child) => !this.loader.hasChildren(child)),
+                    ),
+                    tap((children) =>
+                        children.forEach((child) => this.map.set(child, [])),
+                    ),
+                    map(() => this.start),
                 ),
-                tap((children) => children.forEach((child) => this.map.set(child, []))),
             ),
         ),
-        startWith(null),
-        map(() => this.start),
-    );
+        this.reset$.pipe(
+            tap(() => {
+                this.resetMap();
+            }),
+            map(() => this.start),
+        ),
+    ).pipe(startWith(this.start));
 
     public getChildren(item: T): readonly T[] {
         return this.map.get(item) || [this.loading];
@@ -37,5 +49,13 @@ export class TuiTreeService<T> {
 
         this.map.set(item, [this.loading]);
         this.load$.next(item);
+    }
+
+    public clear(): void {
+        this.reset$.next();
+    }
+
+    private resetMap(): void {
+        this.map = new Map<T, readonly T[]>([[this.loading, []]]);
     }
 }
