a677a6cc8cd43e28a4badb26573b46dd5eb70361
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-10-11 12:16:01 +0200

    refactor(core): convert inputs to use reactive input and computed properties

--- projects/core/components/loader/loader.component.ts
+++ projects/core/components/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, inject, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, input, computed} from '@angular/core';
 import {TUI_IS_IOS} from '@taiga-ui/cdk/tokens';
 import {tuiIsSafari} from '@taiga-ui/cdk/utils/browser';
 import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
@@ -14,8 +14,8 @@ import {TUI_LOADER_OPTIONS} from './loader.options';
     styleUrl: './loader.style.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
-        '[class._loading]': 'loading',
-        '[attr.data-size]': 'size',
+        '[class._loading]': 'loading()',
+        '[attr.data-size]': 'size()',
     },
 })
 export class TuiLoader {
@@ -23,23 +23,16 @@ export class TuiLoader {
     private readonly options = inject(TUI_LOADER_OPTIONS);
     protected readonly isApple = tuiIsSafari(tuiInjectElement()) || this.isIOS;
 
-    @Input()
-    public size = this.options.size;
+    public size = input(this.options.size);
 
-    @Input()
-    public inheritColor = this.options.inheritColor;
+    public inheritColor = input(this.options.inheritColor);
 
-    @Input()
-    public overlay = this.options.overlay;
+    public overlay = input(this.options.overlay);
 
-    @Input()
-    public textContent: PolymorpheusContent;
+    public textContent = input<PolymorpheusContent>();
 
     // TODO: Drop alias in v5
-    @Input('showLoader')
-    public loading = true;
+    public loading = input(true, {alias: 'showLoader'});
 
-    protected get isHorizontal(): boolean {
-        return !tuiSizeBigger(this.size);
-    }
+    protected isHorizontal = computed(() => !tuiSizeBigger(this.size()));
 }
