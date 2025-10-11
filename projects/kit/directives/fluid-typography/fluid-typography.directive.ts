5d454488ee37bbbf2bce714424926c3500fce866
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-10-11 23:04:59 +0200

    refactor(kit): simplify fluid typography directive and remove unused inputs

--- projects/kit/directives/fluid-typography/fluid-typography.directive.ts
+++ projects/kit/directives/fluid-typography/fluid-typography.directive.ts
@@ -1,5 +1,5 @@
-import {Directive, inject, Input, type OnChanges} from '@angular/core';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {Directive, inject, input} from '@angular/core';
+import {takeUntilDestroyed, toObservable} from '@angular/core/rxjs-interop';
 import {
     MutationObserverService,
     WA_MUTATION_OBSERVER_INIT,
@@ -7,7 +7,7 @@ import {
 import {ResizeObserverService} from '@ng-web-apis/resize-observer';
 import {tuiZonefree} from '@taiga-ui/cdk/observables';
 import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
-import {fromEvent, merge, Subject} from 'rxjs';
+import {fromEvent, merge} from 'rxjs';
 
 import {TUI_FLUID_TYPOGRAPHY_OPTIONS} from './fluid-typography.options';
 
@@ -26,22 +26,22 @@ const STEP = 1 / 16;
     ],
     host: {tuiFluidTypography: ''},
 })
-export class TuiFluidTypography implements OnChanges {
-    // TODO: refactor to signal inputs after Angular update
-    private readonly changes$ = new Subject<void>();
+export class TuiFluidTypography {
     private readonly el = tuiInjectElement();
     private readonly options = inject(TUI_FLUID_TYPOGRAPHY_OPTIONS);
 
-    protected readonly sub = merge(
-        this.changes$,
+    public tuiFluidTypography = input<readonly [min: number, max: number] | ''>('');
+
+    public readonly sub = merge(
+        toObservable(this.tuiFluidTypography),
         inject(ResizeObserverService, {self: true}),
         inject(MutationObserverService, {self: true}),
         fromEvent(this.el, 'input'),
     )
         .pipe(tuiZonefree(), takeUntilDestroyed())
         .subscribe(() => {
-            const min = Number(this.tuiFluidTypography[0] || this.options.min);
-            const max = Number(this.tuiFluidTypography[1] || this.options.max);
+            const min = Number(this.tuiFluidTypography()[0] || this.options.min);
+            const max = Number(this.tuiFluidTypography()[1] || this.options.max);
 
             for (let i = max; i >= min; i -= STEP) {
                 this.el.style.fontSize = `${i}rem`;
@@ -51,11 +51,4 @@ export class TuiFluidTypography implements OnChanges {
                 }
             }
         });
-
-    @Input()
-    public tuiFluidTypography: readonly [min: number, max: number] | '' = '';
-
-    public ngOnChanges(): void {
-        this.changes$.next();
-    }
 }
