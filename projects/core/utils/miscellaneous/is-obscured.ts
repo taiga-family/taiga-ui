6d60fb3eef2fa9a55f5afdf44f619ecc6a54ba63
waterplea <alexander@inkin.ru>
2025-10-11 18:07:20 +0400

    feat(core): `Hint` move into popups portal

--- projects/core/utils/miscellaneous/is-obscured.ts
+++ projects/core/utils/miscellaneous/is-obscured.ts
@@ -3,6 +3,6 @@ import {tuiGetElementObscures} from '@taiga-ui/cdk/utils/dom';
 /**
  * @internal
  */
-export function tuiIsObscured(el: HTMLElement, exceptSelector = 'tui-hints'): boolean {
+export function tuiIsObscured(el: HTMLElement, exceptSelector = 'tui-popups'): boolean {
     return !!tuiGetElementObscures(el)?.some((el) => !el.closest(exceptSelector));
 }
--- projects/kit/components/line-clamp/line-clamp-box.component.ts
+++ projects/kit/components/line-clamp/line-clamp-box.component.ts
@@ -5,15 +5,12 @@ import {PolymorpheusOutlet} from '@taiga-ui/polymorpheus';
 @Component({
     selector: 'tui-line-clamp-box',
     imports: [PolymorpheusOutlet],
-    template: `
-        <ng-container *polymorpheusOutlet="content() as text">{{ text }}</ng-container>
-    `,
+    template:
+        '<ng-container *polymorpheusOutlet="content() as text">{{ text }}</ng-container>',
     styleUrl: './line-clamp-box.style.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: TUI_HINT_PROVIDERS,
-    host: {
-        '[style.min-width.px]': 'width',
-    },
+    host: {'[style.min-width.px]': 'width'},
 })
 export class TuiLineClampBox extends TuiHintComponent {
     protected get width(): number {
