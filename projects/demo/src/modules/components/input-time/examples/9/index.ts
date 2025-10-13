a1490b2b88666ac567b3b95f6193710fb7a06662
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-10-13 13:45:59 +0300

    chore(deps): bump Maskito to `4.0.0`

--- projects/demo/src/modules/components/input-time/examples/9/index.ts
+++ projects/demo/src/modules/components/input-time/examples/9/index.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiTime, type TuiTimeMode} from '@taiga-ui/cdk';
+import {type MaskitoTimeMode} from '@maskito/kit';
+import {TuiTime} from '@taiga-ui/cdk';
 import {TuiTextfield} from '@taiga-ui/core';
 import {TuiInputTime} from '@taiga-ui/kit';
 
@@ -14,7 +15,7 @@ import {TuiInputTime} from '@taiga-ui/kit';
     changeDetection,
 })
 export default class Example {
-    protected readonly supportedModes: readonly TuiTimeMode[] = [
+    protected readonly supportedModes: readonly MaskitoTimeMode[] = [
         'HH:MM',
         'HH:MM AA',
         'HH:MM:SS',
