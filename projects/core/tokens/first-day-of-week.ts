ebd889958b2750f3bdea24fac8e104fa25929137
Farouk Bakre <Faroukbakre@yahoo.com>
2025-10-14 19:59:11 +0100

    feat(core): make TUI_FIRST_DAY_OF_WEEK as number/signal

--- projects/core/tokens/first-day-of-week.ts
+++ projects/core/tokens/first-day-of-week.ts
@@ -1,12 +1,26 @@
 import {InjectionToken} from '@angular/core';
 import {TuiDayOfWeek} from '@taiga-ui/cdk/date-time';
+import {BehaviorSubject} from 'rxjs';
+
+export type TuiDayOfWeekType = (typeof TuiDayOfWeek)[keyof typeof TuiDayOfWeek];
+
+export type TuiFirstDayOfWeekSubject = BehaviorSubject<TuiDayOfWeekType> & {
+    valueOf(): TuiDayOfWeekType;
+};
 
 /**
  * The first day of the week index
  */
-export const TUI_FIRST_DAY_OF_WEEK = new InjectionToken<0 | 1 | 2 | 3 | 4 | 5 | 6>(
-    ngDevMode ? 'TUI_FIRST_DAY_OF_WEEK' : '',
-    {
-        factory: () => TuiDayOfWeek.Monday,
+export const TUI_FIRST_DAY_OF_WEEK = new InjectionToken<
+    TuiDayOfWeekType & TuiFirstDayOfWeekSubject
+>(ngDevMode ? 'TUI_FIRST_DAY_OF_WEEK' : '', {
+    factory: () => {
+        const subject = new BehaviorSubject<TuiDayOfWeekType>(
+            TuiDayOfWeek.Monday,
+        ) as TuiFirstDayOfWeekSubject;
+
+        subject.valueOf = () => subject.getValue();
+
+        return subject as TuiDayOfWeekType & TuiFirstDayOfWeekSubject;
     },
-);
+});
