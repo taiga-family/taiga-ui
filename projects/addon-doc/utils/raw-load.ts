c1ba7232fc6904e15a511149c9e34ea0d4f05dcb
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-10-15 11:27:39 +0300

    fix(addon-doc): `tuiRawLoad` is incompatible with esbuild loaders

--- projects/addon-doc/utils/raw-load.ts
+++ projects/addon-doc/utils/raw-load.ts
@@ -1,5 +1,7 @@
 import {type TuiRawLoaderContent} from '@taiga-ui/addon-doc/types';
 
 export async function tuiRawLoad(content: TuiRawLoaderContent): Promise<string> {
-    return content instanceof Promise ? (await content).default : content;
+    return Promise.resolve(content).then((x) =>
+        typeof x === 'object' && 'default' in x ? x.default : x,
+    );
 }
