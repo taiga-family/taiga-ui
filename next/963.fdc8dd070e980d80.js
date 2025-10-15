7468f8258405465d3c070d7242d00f6897d8f501
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2025-10-15 12:40:34 +0000

    deploy: f7d2773e735db4a454bccf48acc8c1d0c91babcb

---
+++ next/963.fdc8dd070e980d80.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[963],{963:e=>{e.exports='<img\n    [alt]="countriesNames()[countryIsoCode]"\n    [src]="countryIsoCode | tuiFlag"\n    [style.border-radius.%]="50"\n/>\n'}}]);
