2c0269fb36d881ae81471b63927f39ad095e2a92
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-10-18 14:57:56 +0200

    refactor(core): change value property in OptionDirective to optional input

--- projects/core/components/data-list/option/option.directive.ts
+++ projects/core/components/data-list/option/option.directive.ts
@@ -100,7 +100,7 @@ export class TuiOptionWithValue<T = unknown> {
 
     public readonly disabled = input(false);
 
-    public readonly value = input.required<T>();
+    public readonly value = input<T>();
 
     protected onClick(value = this.value()): void {
         if (this.host?.handleOption && value !== undefined) {
