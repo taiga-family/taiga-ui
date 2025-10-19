41eaeb8854bd60da2cb10eb99eab0b9f4516a2ff
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-10-19 05:25:32 +0200

    refactor(core): update `TuiDateFormat` and `TuiNumberFormat` to use computed inputs

--- projects/core/directives/date-format/date-format.directive.ts
+++ projects/core/directives/date-format/date-format.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, inject, input} from '@angular/core';
+import {computed, Directive, inject, input} from '@angular/core';
 import {toObservable} from '@angular/core/rxjs-interop';
 import {tuiProvide} from '@taiga-ui/cdk/utils/miscellaneous';
 import {TUI_DATE_FORMAT, type TuiDateFormatSettings} from '@taiga-ui/core/tokens';
@@ -10,12 +10,13 @@ import {combineLatest, map, Observable} from 'rxjs';
 })
 export class TuiDateFormat extends Observable<TuiDateFormatSettings> {
     private readonly parent = inject(TUI_DATE_FORMAT, {skipSelf: true});
+    private readonly changes = toObservable(computed(() => this.tuiDateFormat()));
 
     public readonly tuiDateFormat = input<Partial<TuiDateFormatSettings>>();
 
     constructor() {
         super((subscriber) =>
-            combineLatest([this.parent, toObservable(this.tuiDateFormat)])
+            combineLatest([this.parent, this.changes])
                 .pipe(map(([parent, settings]) => ({...parent, ...settings})))
                 .subscribe(subscriber),
         );
--- projects/core/directives/number-format/number-format.directive.ts
+++ projects/core/directives/number-format/number-format.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, inject, input} from '@angular/core';
+import {computed, Directive, inject, input} from '@angular/core';
 import {toObservable} from '@angular/core/rxjs-interop';
 import {tuiProvide} from '@taiga-ui/cdk/utils/miscellaneous';
 import {TUI_NUMBER_FORMAT, type TuiNumberFormatSettings} from '@taiga-ui/core/tokens';
@@ -10,12 +10,13 @@ import {combineLatest, map, Observable} from 'rxjs';
 })
 export class TuiNumberFormat extends Observable<TuiNumberFormatSettings> {
     private readonly parent = inject(TUI_NUMBER_FORMAT, {skipSelf: true});
+    private readonly changes = toObservable(computed(() => this.tuiNumberFormat()));
 
     public readonly tuiNumberFormat = input<Partial<TuiNumberFormatSettings>>();
 
     constructor() {
         super((subscriber) =>
-            combineLatest([this.parent, toObservable(this.tuiNumberFormat)])
+            combineLatest([this.parent, this.changes])
                 .pipe(map(([parent, settings]) => ({...parent, ...settings})))
                 .subscribe(subscriber),
         );
