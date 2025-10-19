2d5e6ee8e547150d17a008ff9d7d046965d32061
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-10-20 00:21:20 +0200

    refactor(core): update `TuiDateFormat` to use signal inputs and simplify subscription logic

--- projects/core/directives/date-format/date-format.directive.ts
+++ projects/core/directives/date-format/date-format.directive.ts
@@ -1,24 +1,25 @@
 import {computed, Directive, inject, input} from '@angular/core';
-import {toObservable} from '@angular/core/rxjs-interop';
+import {toObservable, toSignal} from '@angular/core/rxjs-interop';
 import {tuiProvide} from '@taiga-ui/cdk/utils/miscellaneous';
 import {TUI_DATE_FORMAT, type TuiDateFormatSettings} from '@taiga-ui/core/tokens';
-import {combineLatest, map, Observable} from 'rxjs';
+import {Observable} from 'rxjs';
 
 @Directive({
     selector: '[tuiDateFormat]',
     providers: [tuiProvide(TUI_DATE_FORMAT, TuiDateFormat)],
 })
 export class TuiDateFormat extends Observable<TuiDateFormatSettings> {
-    private readonly parent = inject(TUI_DATE_FORMAT, {skipSelf: true});
-    private readonly changes = toObservable(computed(() => this.tuiDateFormat()));
+    private readonly parent = toSignal(inject(TUI_DATE_FORMAT, {skipSelf: true}), {
+        requireSync: true,
+    });
+
+    private readonly settings = toObservable(
+        computed(() => ({...this.parent(), ...this.tuiDateFormat()})),
+    );
 
     public readonly tuiDateFormat = input<Partial<TuiDateFormatSettings>>();
 
     constructor() {
-        super((subscriber) =>
-            combineLatest([this.parent, this.changes])
-                .pipe(map(([parent, settings]) => ({...parent, ...settings})))
-                .subscribe(subscriber),
-        );
+        super((subscriber) => this.settings.subscribe(subscriber));
     }
 }
