22f96ced00a7ac7f2054c9fa7d2abce1bc269923
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-10-19 23:53:28 +0200

    refactor(core): revert changes

--- projects/core/directives/number-format/number-format.directive.ts
+++ projects/core/directives/number-format/number-format.directive.ts
@@ -1,24 +1,26 @@
-import {computed, Directive, inject, input} from '@angular/core';
-import {toObservable} from '@angular/core/rxjs-interop';
+import {Directive, inject, Input} from '@angular/core';
 import {tuiProvide} from '@taiga-ui/cdk/utils/miscellaneous';
 import {TUI_NUMBER_FORMAT, type TuiNumberFormatSettings} from '@taiga-ui/core/tokens';
-import {combineLatest, map, Observable} from 'rxjs';
+import {combineLatest, map, Observable, ReplaySubject} from 'rxjs';
 
 @Directive({
     selector: '[tuiNumberFormat]',
     providers: [tuiProvide(TUI_NUMBER_FORMAT, TuiNumberFormat)],
 })
 export class TuiNumberFormat extends Observable<TuiNumberFormatSettings> {
+    private readonly settings = new ReplaySubject<Partial<TuiNumberFormatSettings>>(1);
     private readonly parent = inject(TUI_NUMBER_FORMAT, {skipSelf: true});
-    private readonly changes = toObservable(computed(() => this.tuiNumberFormat()));
-
-    public readonly tuiNumberFormat = input<Partial<TuiNumberFormatSettings>>();
 
     constructor() {
         super((subscriber) =>
-            combineLatest([this.parent, this.changes])
+            combineLatest([this.parent, this.settings])
                 .pipe(map(([parent, settings]) => ({...parent, ...settings})))
                 .subscribe(subscriber),
         );
+    }
+
+    @Input()
+    public set tuiNumberFormat(format: Partial<TuiNumberFormatSettings>) {
+        this.settings.next(format);
     }
 }
