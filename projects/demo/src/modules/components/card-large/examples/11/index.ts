43c9f62ec67023d1cfa42f8203ffac767f4329c1
splincode <omaxphp@yandex.ru>
2025-10-21 16:51:44 +0300

    feat(kit): align close button to top for closable preset in tuiCell

--- projects/demo/src/modules/components/card-large/examples/11/index.ts
+++ projects/demo/src/modules/components/card-large/examples/11/index.ts
@@ -2,18 +2,23 @@ import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiAppearance, TuiButton, TuiTitle} from '@taiga-ui/core';
-import {TuiButtonClose} from '@taiga-ui/kit';
-import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import {TuiAvatar, TuiButtonClose, TuiFade} from '@taiga-ui/kit';
+import {TuiCardLarge, TuiCell, TuiHeader} from '@taiga-ui/layout';
+import {TuiPlatform} from '@taiga-ui/cdk';
 
 @Component({
     standalone: true,
     imports: [
-        TuiAppearance,
         TuiButton,
         TuiButtonClose,
         TuiCardLarge,
         TuiHeader,
         TuiTitle,
+        TuiAvatar,
+        TuiCell,
+        TuiAppearance,
+        TuiFade,
+        TuiPlatform,
     ],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
