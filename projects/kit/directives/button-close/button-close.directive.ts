daff59c7c2675d2e31a292c5426ac230655e2ca4
splincode <omaxphp@yandex.ru>
2025-10-23 14:51:23 +0300

    feat(kit): align close button to top for closable preset in tuiCell

--- projects/kit/directives/button-close/button-close.directive.ts
+++ projects/kit/directives/button-close/button-close.directive.ts
@@ -1,10 +1,10 @@
 import {Directive, inject} from '@angular/core';
-import {tuiButtonOptionsProvider} from '@taiga-ui/core/components/button';
+import {TuiButton, tuiButtonOptionsProvider} from '@taiga-ui/core/components/button';
 import {TUI_COMMON_ICONS, TUI_ICON_START} from '@taiga-ui/core/tokens';
 
 @Directive({
     standalone: true,
-    selector: '[tuiIconButton][tuiButtonClose]',
+    selector: '[tuiButtonClose]',
     providers: [
         tuiButtonOptionsProvider({appearance: 'neutral', size: 's'}),
         {
@@ -12,8 +12,16 @@ import {TUI_COMMON_ICONS, TUI_ICON_START} from '@taiga-ui/core/tokens';
             useFactory: () => inject(TUI_COMMON_ICONS).close,
         },
     ],
+    hostDirectives: [
+        {
+            directive: TuiButton,
+            inputs: ['size'],
+        },
+    ],
     host: {
+        tuiIconButton: '',
         '[style.--t-radius.%]': '100',
+        '[attr.type]': '"button"',
     },
 })
 export class TuiButtonClose {}
