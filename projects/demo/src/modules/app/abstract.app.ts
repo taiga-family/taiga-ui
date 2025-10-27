db0aa360d1d26706f6d34f57c0062d228af1fe46
splincode <omaxphp@yandex.ru>
2025-10-27 17:48:35 +0300

    chore(demo): support redirects in proprietary demo

--- projects/demo/src/modules/app/abstract.app.ts
+++ projects/demo/src/modules/app/abstract.app.ts
@@ -1,4 +1,4 @@
-import {Directive, inject} from '@angular/core';
+import {Directive, inject, type OnInit} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {type Router} from '@angular/router';
 import {ResizeObserverService} from '@ng-web-apis/resize-observer';
@@ -39,9 +39,13 @@ export const DEMO_PAGE_LOADED_PROVIDER = {
 @Directive({
     standalone: true,
 })
-export abstract class AbstractDemo {
+export abstract class AbstractDemo implements OnInit {
     protected abstract readonly storage: Storage;
     protected abstract readonly router: Router;
+
+    public ngOnInit(): void {
+        void this.replaceEnvInURI();
+    }
 
     @tuiPure
     protected get isChristmas(): boolean {
