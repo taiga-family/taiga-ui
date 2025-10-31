c05cd99a398daa4e0d907cdbde403f6dcf984fc8
jonata biondi <jonata.biondi@gmail.com>
2025-10-31 16:20:37 +0100

    feat(kit): scroll to input on focus

--- projects/kit/components/input-chip/input-chip.directive.ts
+++ projects/kit/components/input-chip/input-chip.directive.ts
@@ -35,7 +35,7 @@ import {TUI_INPUT_CHIP_OPTIONS} from './input-chip.options';
         '(input)': 'onInput()',
         '(paste.prevent)': 'onPaste($event)',
         '(drop.prevent)': 'onPaste($event)',
-        '(focus)': 'scrollTo()',
+        '(focus)': 'onFocus()',
     },
 })
 export class TuiInputChipBaseDirective<T>
@@ -79,6 +79,10 @@ export class TuiInputChipBaseDirective<T>
         this.onChange(
             this.unique ? Array.from(new Set(value.reverse())).reverse() : value,
         );
+    }
+
+    protected onFocus(): void {
+        this.scrollTo();
     }
 
     protected onEnter(): void {
