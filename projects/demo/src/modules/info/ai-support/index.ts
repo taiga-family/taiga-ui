999d6cea50c52a317f2ccac812c28bfc03f7436d
mdlufy <zidakbew@gmail.com>
2025-11-06 00:34:35 +0300

    chore(demo): `AI support` add section about MCP

--- projects/demo/src/modules/info/ai-support/index.ts
+++ projects/demo/src/modules/info/ai-support/index.ts
@@ -1,12 +1,25 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
-import {TuiDemo} from '@demo/utils';
+import {TuiDocCode, TuiDocPage} from '@taiga-ui/addon-doc';
+import {TuiLink} from '@taiga-ui/core';
 
 @Component({
     standalone: true,
-    imports: [TuiDemo],
+    imports: [TuiDocCode, TuiDocPage, TuiLink],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     changeDetection,
 })
-export default class Page {}
+export default class Page {
+    protected readonly installationCode = `{
+  mcpServers: {
+    'taiga-ui': {
+      command: 'npx',
+      args: [
+        '@taiga-ui/mcp@latest',
+        '--source-url=https://taiga-ui.dev/llms-full.txt', // or file from "/next" version, if you want
+      ],
+    },
+  },
+}`;
+}
