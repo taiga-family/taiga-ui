1beca3f91a83d1323580b001ef8934872a702ae7
waterplea <alexander@inkin.ru>
2025-11-10 11:15:15 +0000

    Update snapshots/demo/next/main to output generated at bda6650

--- 21880.f1f23ab66d63e2dd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[21880],{21880:n=>{n.exports='<button\n    size="m"\n    tuiButton\n    type="button"\n    (click)="showNotification()"\n>\n    Show\n</button>\n'}}]);
--- 94602.fccd2543a4a877bd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[94602],{94602:n=>{n.exports="```ts\nimport {TuiNotification} from '@taiga-ui/core';\n\n// ...\n\n@Component({\n  standalone: true,\n  imports: [\n    // ...\n    TuiNotification,\n  ],\n  // ...\n})\nexport class Example {}\n```\n"}}]);
