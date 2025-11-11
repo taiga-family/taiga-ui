382b0283bcb5a0711767aacc46627e070a36f094
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-11-11 16:46:47 +0100

    refactor(kit): replace `@Input` with `input` for `tuiSlot` in `TuiBadgedContentDirective`

--- projects/kit/components/badged-content/badged-content.directive.ts
+++ projects/kit/components/badged-content/badged-content.directive.ts
@@ -1,10 +1,9 @@
-import {Directive, Input} from '@angular/core';
+import {Directive, input} from '@angular/core';
 import {type TuiLooseUnion} from '@taiga-ui/cdk/types';
 
 @Directive({
     selector: '[tuiSlot]',
 })
 export class TuiBadgedContentDirective {
-    @Input()
-    public tuiSlot: TuiLooseUnion<'bottom' | 'top'> = 'top';
+    public readonly tuiSlot = input<TuiLooseUnion<'bottom' | 'top'>>('top');
 }
