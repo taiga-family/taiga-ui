744545c20f371a8bdb141da303d0190e0b61192b
jonata biondi <jonata.biondi@gmail.com>
2025-11-11 11:22:21 +0100

    feat(kit): simplify using host

--- projects/kit/components/input-chip/input-chip.directive.ts
+++ projects/kit/components/input-chip/input-chip.directive.ts
@@ -35,7 +35,7 @@ import {TUI_INPUT_CHIP_OPTIONS} from './input-chip.options';
         '(input)': 'onInput()',
         '(paste.prevent)': 'onPaste($event)',
         '(drop.prevent)': 'onPaste($event)',
-        '(focus)': 'onFocus()',
+        '(focus)': 'scrollTo()',
     },
 })
 export class TuiInputChipBaseDirective<T>
@@ -79,10 +79,6 @@ export class TuiInputChipBaseDirective<T>
         this.onChange(
             this.unique ? Array.from(new Set(value.reverse())).reverse() : value,
         );
-    }
-
-    protected onFocus(): void {
-        this.scrollTo();
     }
 
     protected onEnter(): void {
