55ee9c6047db29eab4dad0ebf0fb0fc6c9640e87
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2025-11-17 12:06:49 +0000

    Update snapshots/demo/next/main to output generated at eb01404

---
+++ chunk-A5K5ZOSL.js
@@ -0,0 +1,32 @@
+import"./chunk-42JZD6NG.js";var i=`import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiTextfield} from '@taiga-ui/core';
+import {TuiInputSlider, TuiPagination} from '@taiga-ui/kit';
+import {TuiTextfieldControllerModule} from '@taiga-ui/legacy';
+
+@Component({
+    imports: [
+        FormsModule,
+        TuiInputSlider,
+        TuiPagination,
+        TuiTextfield,
+        TuiTextfieldControllerModule,
+    ],
+    templateUrl: './index.html',
+    styleUrl: './index.less',
+    encapsulation,
+    changeDetection,
+})
+export default class Page {
+    protected length = 64;
+
+    protected index = 10;
+
+    protected goToPage(index: number): void {
+        this.index = index;
+        console.info('New page:', index);
+    }
+}
+`;export{i as default};
---
+++ chunk-YVP673YE.js
@@ -0,0 +1,24 @@
+import"./chunk-42JZD6NG.js";var e=`<tui-textfield
+    tuiTextfieldSize="m"
+    class="slider"
+>
+    <input
+        tuiInputSlider
+        [max]="6"
+        [min]="0"
+        [(ngModel)]="sidePadding"
+    />
+    <span>sidePadding</span>
+    <input
+        tuiSlider
+        type="range"
+        [step]="1"
+    />
+</tui-textfield>
+
+<tui-pagination
+    [index]="10"
+    [length]="64"
+    [sidePadding]="sidePadding"
+/>
+`;export{e as default};
