ddd71668301851ec854bfcea0c669bf5e8e7e80a
waterplea <waterplea@users.noreply.github.com>
2025-11-17 10:27:08 +0000

    deploy: cc93bb57686560401888106d6567a3b8e794ae3b

---
+++ next/chunk-G4JPFFE2.js
@@ -0,0 +1,31 @@
+import"./chunk-2TKEPZTY.js";import"./chunk-IFYVBBGW.js";import"./chunk-EZCTEIG5.js";import"./chunk-CZLBADDE.js";import"./chunk-PJOYTKW7.js";import"./chunk-GGQ7LRWG.js";import"./chunk-F5DBWA2F.js";import"./chunk-BC6SIBYK.js";import"./chunk-R4VWJZ3H.js";import"./chunk-5D2HKEPL.js";import{G as s}from"./chunk-HGCDV6QR.js";import"./chunk-3XXQBQ4L.js";import"./chunk-WKXULA3B.js";import"./chunk-PAATJCG7.js";import"./chunk-EJI7MVWO.js";import"./chunk-NKX3EWIC.js";import"./chunk-TCVX2L7M.js";import"./chunk-SX2FZ4JG.js";import"./chunk-OYH2WJ3O.js";import"./chunk-BMI5MM25.js";import"./chunk-NU6UCYQC.js";import"./chunk-ZOTEVDA5.js";import{Bc as c}from"./chunk-I2SFNYPJ.js";import"./chunk-HXUUARZB.js";import"./chunk-5ENH3QN6.js";import"./chunk-SVUDN6EH.js";import{Ub as n,Xc as i,sc as r}from"./chunk-X7N7YQIF.js";import"./chunk-42JZD6NG.js";var a=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path
+        vector-effect="non-scaling-stroke"
+        d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"
+    />
+</svg>
+`;var l=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path vector-effect="non-scaling-stroke" d="m21 21-4.34-4.34" />
+    <circle vector-effect="non-scaling-stroke" cx="11" cy="11" r="8" />
+</svg>
+`;var e=class e{};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=n({type:e,selectors:[["ng-component"]],features:[i([c({"@tui.heart":a,"@tui.search":l})])],decls:2,vars:0,consts:[["icon","@tui.heart"],["icon","@tui.search"]],template:function(t,u){t&1&&r(0,"tui-icon",0)(1,"tui-icon",1)},dependencies:[s],styles:["[_nghost-%COMP%]{display:flex;gap:1rem;color:var(--tui-text-action)}"],changeDetection:0});var o=e;export{o as default};
