cf0dcc0d9eead3527b1d53d9e73bdc3794c35fbd
splincode <omaxphp@yandex.ru>
2025-11-19 14:56:02 +0000

    Update snapshots/demo/next/main to output generated at 811ffeb

---
+++ chunk-OQKJLOHQ.js
@@ -0,0 +1,69 @@
+import"./chunk-42JZD6NG.js";var i=`import {Component, inject} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiLegendItem, TuiRingChart} from '@taiga-ui/addon-charts';
+import {TuiAmountPipe} from '@taiga-ui/addon-commerce';
+import {tuiPure, tuiSum} from '@taiga-ui/cdk';
+import {
+    tuiFormatNumber,
+    TuiIcon,
+    TuiNotification,
+    TuiNotificationService,
+} from '@taiga-ui/core';
+import {TuiCheckbox} from '@taiga-ui/kit';
+
+@Component({
+    imports: [
+        TuiAmountPipe,
+        TuiCheckbox,
+        TuiIcon,
+        TuiLegendItem,
+        TuiNotification,
+        TuiRingChart,
+    ],
+    templateUrl: './index.html',
+    styleUrl: './index.less',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    private readonly alerts = inject(TuiNotificationService);
+    private enabled = Array.from<unknown, boolean>({length: 5}, () => true);
+
+    protected readonly data = [13769, 12367, 10172, 3018, 2592];
+    protected readonly sum = tuiSum(...this.data);
+    protected readonly labels = ['Axes', 'Faxes', 'Taxes', 'Saxes', 'Other'];
+
+    protected get value(): readonly number[] {
+        return this.getValue(this.data, this.enabled);
+    }
+
+    protected isEnabled(index: number): boolean {
+        return this.enabled[index] ?? false;
+    }
+
+    protected toggle(index: number): void {
+        this.enabled = this.enabled.map((value, i) => (i === index ? !value : value));
+    }
+
+    protected onClick(index: number): void {
+        if (this.isEnabled(index)) {
+            this.alerts
+                .open(\`Category spending: \${tuiFormatNumber(this.data[index] ?? 0)} \u20BD\`, {
+                    label: this.labels[index],
+                })
+                .subscribe();
+        } else {
+            this.toggle(index);
+        }
+    }
+
+    @tuiPure
+    private getValue(
+        data: readonly number[],
+        enabled: readonly boolean[],
+    ): readonly number[] {
+        return data.map((value, index) => (enabled[index] ? value : 0));
+    }
+}
+`;export{i as default};
---
+++ chunk-UOPWGW2L.js
@@ -0,0 +1,47 @@
+import"./chunk-42JZD6NG.js";var t=`import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {DemoRoute} from '@demo/routes';
+import {TuiDemo} from '@demo/utils';
+import {TuiLink} from '@taiga-ui/core';
+import {TuiProgress, type TuiProgressBar} from '@taiga-ui/kit';
+
+@Component({
+    imports: [RouterLink, TuiDemo, TuiLink, TuiProgress],
+    templateUrl: './index.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class Page {
+    protected readonly routes = DemoRoute;
+    protected value = 3;
+    protected max = 5;
+    protected segments = this.max;
+
+    protected examples = ['Basic', 'Sizes', 'Colors', 'With labels', 'No round corners'];
+
+    protected readonly sizeVariants: ReadonlyArray<TuiProgressBar['size']> = [
+        'xs',
+        's',
+        'm',
+        'l',
+        'xl',
+        'xxl',
+    ];
+
+    protected size: TuiProgressBar['size'] = this.sizeVariants[2]!;
+
+    protected readonly colorsVariants: readonly string[][] = [
+        ['var(--tui-background-accent-1)'],
+        ['#39b54a', '#ffd450', '#ffd450', '#fcc521', '#fab619', '#f8a34d', '#e01f19'],
+        Array.from(
+            {length: 20},
+            (_, index) => \`var(--tui-chart-categorical-0\${index + 1})\`,
+        ),
+    ];
+
+    protected colors: string[] = this.colorsVariants[0] ?? [];
+
+    protected get computedColors(): string[] {
+        return this.colors.slice(0, this.segments);
+    }
+}
+`;export{t as default};
