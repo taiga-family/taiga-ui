557234cac09d5f3e162c2a750c5d14d57394cf2e
waterplea <alexander@inkin.ru>
2025-11-21 08:21:32 +0000

    Update snapshots/demo/next/main to output generated at 7134684

---
+++ chunk-DWZOORLS.js
@@ -0,0 +1,31 @@
+import"./chunk-DWP2OFB5.js";import"./chunk-ULNPES47.js";import"./chunk-JGM5DCFG.js";import"./chunk-O3ICEGLY.js";import"./chunk-W7S5MZGO.js";import"./chunk-P4FABSFT.js";import"./chunk-2YRZRZFY.js";import"./chunk-ACC7FCGI.js";import"./chunk-JIARLPZM.js";import{D as s}from"./chunk-OLHURNT4.js";import"./chunk-4TD7UD3K.js";import"./chunk-OKQUT7WQ.js";import"./chunk-K427JSMR.js";import{J as c}from"./chunk-HCAOLPOO.js";import"./chunk-A6G4ZKBL.js";import"./chunk-4GTWS6G4.js";import"./chunk-DBFYDFAL.js";import"./chunk-WFFIXRY2.js";import"./chunk-WJEXTC7N.js";import"./chunk-2JMFRR3T.js";import"./chunk-5PJUAT4Y.js";import"./chunk-B3SPWCQG.js";import"./chunk-PN6TDIYL.js";import"./chunk-NQQ6KNH7.js";import"./chunk-42NRN3OW.js";import{Ub as n,Xc as i,sc as r}from"./chunk-ET2ZSIIN.js";import"./chunk-42JZD6NG.js";var a=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path
+        vector-effect="non-scaling-stroke"
+        d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"
+    />
+</svg>
+`;var l=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path vector-effect="non-scaling-stroke" d="m21 21-4.34-4.34" />
+    <circle vector-effect="non-scaling-stroke" cx="11" cy="11" r="8" />
+</svg>
+`;var e=class e{};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=n({type:e,selectors:[["ng-component"]],features:[i([c({"@tui.heart":a,"@tui.search":l})])],decls:2,vars:0,consts:[["icon","@tui.heart"],["icon","@tui.search"]],template:function(t,u){t&1&&r(0,"tui-icon",0)(1,"tui-icon",1)},dependencies:[s],styles:["[_nghost-%COMP%]{display:flex;gap:1rem;color:var(--tui-text-action)}"],changeDetection:0});var o=e;export{o as default};
