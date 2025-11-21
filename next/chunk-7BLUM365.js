e974e92050e3e304511acb5c22b93c5ceedb23cd
waterplea <waterplea@users.noreply.github.com>
2025-11-21 13:03:04 +0000

    deploy: b34e23c3c533bd042ace76d1d862097cf1451538

---
+++ next/chunk-7BLUM365.js
@@ -0,0 +1,22 @@
+import"./chunk-42JZD6NG.js";var n=`<div
+    tuiDropdown="Dropdown content"
+    tuiDropdownAuto
+    tuiDropdownHover
+    tuiDropdownLimitWidth="fixed"
+    tuiGroup
+>
+    <button
+        tuiButton
+        type="button"
+    >
+        Won't open here
+    </button>
+    <button
+        #tuiDropdownHost
+        tuiButton
+        type="button"
+    >
+        Will open here
+    </button>
+</div>
+`;export{n as default};
