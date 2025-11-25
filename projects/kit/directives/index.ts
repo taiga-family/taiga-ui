ca43bcb62cec658dd03e123881a0b6695dcbf1d7
Максим Иванов <splincodewd@yandex.ru>
2025-11-25 18:56:20 +0300

    refactor!: drop `TuiImgLazyLoading` (#12648)

--- projects/kit/directives/index.ts
+++ projects/kit/directives/index.ts
@@ -7,7 +7,6 @@ export * from '@taiga-ui/kit/directives/data-list-dropdown-manager';
 export * from '@taiga-ui/kit/directives/fade';
 export * from '@taiga-ui/kit/directives/fluid-typography';
 export * from '@taiga-ui/kit/directives/highlight';
-export * from '@taiga-ui/kit/directives/lazy-loading';
 export * from '@taiga-ui/kit/directives/password';
 export * from '@taiga-ui/kit/directives/present';
 export * from '@taiga-ui/kit/directives/sensitive';
