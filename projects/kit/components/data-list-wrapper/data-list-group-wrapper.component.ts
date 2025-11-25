706690dcbd6d3bfae51fe26da3d813885e692362
splincode <omaxphp@yandex.ru>
2025-11-25 13:30:39 +0300

    refactor: use short name

--- projects/kit/components/data-list-wrapper/data-list-group-wrapper.component.ts
+++ projects/kit/components/data-list-wrapper/data-list-group-wrapper.component.ts
@@ -2,7 +2,7 @@ import {ChangeDetectionStrategy, Component, inject, input} from '@angular/core';
 import {tuiAsDataListAccessor, TuiDataList} from '@taiga-ui/core/components/data-list';
 import {TuiLoader} from '@taiga-ui/core/components/loader';
 import {
-    TuiMultiSelectGroupComponent,
+    TuiMultiSelect,
     TuiMultiSelectGroupDirective,
 } from '@taiga-ui/kit/components/multi-select';
 import {PolymorpheusOutlet} from '@taiga-ui/polymorpheus';
@@ -11,7 +11,7 @@ import {TuiDataListWrapperComponent} from './data-list-wrapper.component';
 
 @Component({
     selector: 'tui-data-list-wrapper[labels]',
-    imports: [PolymorpheusOutlet, TuiDataList, TuiLoader, TuiMultiSelectGroupComponent],
+    imports: [PolymorpheusOutlet, TuiDataList, TuiLoader, TuiMultiSelect],
     templateUrl: './data-list-group-wrapper.template.html',
     styleUrl: './data-list-wrapper.style.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
