87a11308477a5ace6e3e536af7a733af18f5139d
waterplea <alexander@inkin.ru>
2025-11-27 08:10:32 +0000

    Update snapshots/demo/next/main to output generated at cf98683

---
+++ chunk-Y3FTLCFR.js
@@ -0,0 +1,12 @@
+import"./chunk-42JZD6NG.js";var e=`<tui-pagination
+    [content]="content"
+    [length]="7"
+>
+    <ng-template
+        #content
+        let-index
+    >
+        {{ days[index] }}
+    </ng-template>
+</tui-pagination>
+`;export{e as default};
