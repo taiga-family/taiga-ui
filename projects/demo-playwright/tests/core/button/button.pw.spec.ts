bcf253c6a48af1f43aaf23f71c38ce881086ca5e
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2025-11-28 13:47:41 +0300

    chore: fix hover appearance specificity (#12687)

--- projects/demo-playwright/tests/core/button/button.pw.spec.ts
+++ projects/demo-playwright/tests/core/button/button.pw.spec.ts
@@ -1,3 +1,4 @@
+import {DemoRoute} from '@demo/routes';
 import {TuiDocumentationPagePO, tuiGoto} from '@demo-playwright/utils';
 import {expect, test} from '@playwright/test';
 
@@ -5,7 +6,7 @@ test.describe('Button', () => {
     test('darkMode=true + appearance=icon + hovered state', async ({page}) => {
         await tuiGoto(
             page,
-            '/components/button/API?darkMode=true&appearance=icon&iconStart=@tui.eye-off',
+            `${DemoRoute.Button}/API?darkMode=true&appearance=icon&iconStart=@tui.eye-off`,
         );
         const {apiPageExample} = new TuiDocumentationPagePO(page);
 
@@ -20,5 +21,33 @@ test.describe('Button', () => {
             .toHaveScreenshot(
                 '01-[darkMode=true]-[appearance=icon]-[data-state=hovered].png',
             );
+    });
+
+    test('hover state', async ({page}) => {
+        await tuiGoto(page, `${DemoRoute.Button}/API`);
+
+        const {apiPageExample} = new TuiDocumentationPagePO(page);
+        const button = apiPageExample.locator('[tuiButton]');
+
+        await button.hover();
+        await expect.soft(button).toHaveScreenshot('button-hover.png');
+    });
+
+    test('active state', async ({page}) => {
+        await tuiGoto(page, `${DemoRoute.Button}/API`);
+
+        const {apiPageExample} = new TuiDocumentationPagePO(page);
+        const button = apiPageExample.locator('[tuiButton]');
+        const {x, y, width, height} = (await button.boundingBox()) || {
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+        };
+
+        await page.mouse.move(x + width / 2, y + height / 2);
+        await page.mouse.down();
+
+        await expect.soft(button).toHaveScreenshot('button-active.png');
     });
 });
