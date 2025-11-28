47022e94cee72d6f4b50b5376e4fe140f815ae05
waterplea <alexander@inkin.ru>
2025-11-28 11:52:14 +0000

    Update snapshots/demo/next/main to output generated at 781d295

---
+++ chunk-DMRYKTS4.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-7QJ3VWIK.js";import"./chunk-N3DPGA4F.js";import"./chunk-42JZD6NG.js";export{a as pages};
--- chunk-WX4CSMKG.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-LLHGPEPP.js";import"./chunk-N3DPGA4F.js";import"./chunk-42JZD6NG.js";export{a as pages};
