b22ed2a36a6e179a8de0efab10c6a0c952be46ab
Dmitrii <41353182+wtorso@users.noreply.github.com>
2025-12-01 14:13:45 +0300

    fix(addon-table): `TableControl` fix empty list view (#12694)
    Co-authored-by: wtorso <wtorso@yandex.ru>

--- projects/addon-table/directives/table-control/table-control.directive.ts
+++ projects/addon-table/directives/table-control/table-control.directive.ts
@@ -13,11 +13,13 @@ import {type TuiCheckboxRowDirective} from './checkbox-row.directive';
 export class TuiTableControlDirective<T> extends TuiControl<readonly T[]> {
     private readonly children = signal<ReadonlyArray<TuiCheckboxRowDirective<T>>>([]);
 
-    public readonly checked: Signal<boolean> = computed(() =>
-        this.children().every((i) => this.value().includes(i.tuiCheckboxRow)),
+    public readonly checked: Signal<boolean> = computed(
+        () =>
+            !!this.children().length &&
+            this.children().every((i) => this.value().includes(i.tuiCheckboxRow)),
     );
 
-    public readonly indeterminate = computed(
+    public readonly indeterminate: Signal<boolean> = computed(
         () => !!this.value().length && !this.checked(),
     );
 
