2906969fe3ab9b5df0b9c8ec75f2a542cdc47722
nsbarsukov <nikita.s.barsukov@gmail.com>
2025-12-02 07:57:43 +0000

    Update snapshots/demo/next/main to output generated at a1b4622

---
+++ chunk-ADTDRJRJ.js
@@ -0,0 +1,12 @@
+import"./chunk-42JZD6NG.js";var i=`\`\`\`ts
+import {provideTaiga} from '@taiga-ui/core';
+// ...
+
+bootstrapApplication(App, {
+  providers: [
+    provideTaiga(),
+    //...
+  ],
+}).catch((err) => console.error(err));
+\`\`\`
+`;export{i as default};
