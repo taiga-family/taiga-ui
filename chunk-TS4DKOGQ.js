cf59896198b1a671397d76d2f473082dfb425ba6
waterplea <alexander@inkin.ru>
2025-12-02 09:18:14 +0000

    Update snapshots/demo/next/main to output generated at 9b62bf7

---
+++ chunk-TS4DKOGQ.js
@@ -0,0 +1,16 @@
+import"./chunk-42JZD6NG.js";var e=`\`\`\`ts
+import {TuiMobileCalendar} from '@taiga-ui/addon-mobile';
+
+// ...
+
+@Component({
+  standalone: true,
+  imports: [
+    // ...
+    TuiMobileCalendar,
+  ],
+  // ...
+})
+export class Example {}
+\`\`\`
+`;export{e as default};
