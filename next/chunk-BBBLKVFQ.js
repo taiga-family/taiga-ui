b4aa3d2018454915914b03f7704c8066283ac567
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2025-12-04 08:39:25 +0000

    deploy: f8d3471d595bf6368dd20a80a012f3656ff9550d

--- next/chunk-BBBLKVFQ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-N3DPGA4F.js";import"./chunk-42JZD6NG.js";export{a as DemoRoute};
---
+++ next/chunk-X64VMOBZ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-KG7ERNX3.js";import"./chunk-F5JCRAE2.js";import"./chunk-42JZD6NG.js";export{a as pages};
