a354bf6a1d0d0a2ee7d438d6a0d4e705dec0f193
Alex Inkin <alexander@inkin.ru>
2025-12-04 22:24:47 +0700

    chore: fix

--- projects/addon-table/components/table/directives/thead.directive.ts
+++ projects/addon-table/components/table/directives/thead.directive.ts
@@ -1,13 +1,11 @@
 import {Directive} from '@angular/core';
 import {WA_INTERSECTION_ROOT_MARGIN} from '@ng-web-apis/intersection-observer';
 
-import {tuiTableOptionsProvider} from '../table.options';
 import {TuiStuck} from './stuck.directive';
 
 @Directive({
     selector: 'thead[tuiThead]',
     providers: [
-        tuiTableOptionsProvider({sticky: true}),
         {
             provide: WA_INTERSECTION_ROOT_MARGIN,
             useValue: '0px 10000px 10000px 10000px',
