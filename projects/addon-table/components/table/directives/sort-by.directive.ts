2d23c871f348773f5ba41fe86ab65cd0dd6a8eea
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2025-12-04 16:11:07 +0300

    refactor!: `Table` drop legacy (#12626)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/addon-table/components/table/directives/sort-by.directive.ts
+++ projects/addon-table/components/table/directives/sort-by.directive.ts
@@ -1,14 +1,13 @@
 import {
+    computed,
     contentChildren,
     Directive,
     effect,
     inject,
     input,
-    untracked,
+    output,
 } from '@angular/core';
-import {outputFromObservable} from '@angular/core/rxjs-interop';
 import {type TuiComparator} from '@taiga-ui/addon-table/types';
-import {combineLatest, debounceTime, delay, filter, map, type Observable} from 'rxjs';
 
 import {type TuiSortChange} from '../table.options';
 import {TuiTableSortable} from './sortable.directive';
@@ -18,47 +17,24 @@ import {TuiTableDirective} from './table.directive';
     selector: 'table[tuiTable][tuiSortBy]',
 })
 export class TuiTableSortBy<T extends Partial<Record<keyof T, unknown>>> {
+    private readonly table = inject(TuiTableDirective<T>);
     private readonly sortables = contentChildren<TuiTableSortable<T>>(TuiTableSortable, {
         descendants: true,
     });
 
-    private readonly table = inject(TuiTableDirective<T>);
+    private readonly sortChange = computed<TuiSortChange<T>>(() => ({
+        sortKey: this.sortables().length ? this.getKey(this.table.sorter()) : null,
+        sortDirection: this.table.direction(),
+    }));
 
-    /**
-     * @deprecated
-     */
-    private readonly tuiSortByChange$ = this.table.sorterChange$.pipe(
-        // delay is for getting actual ContentChildren (sortables) https://github.com/angular/angular/issues/38976
-        delay(0),
-        filter(() => !!this.sortables().length),
-        map((sorter) => this.getKey(sorter)),
-    );
+    protected readonly sortOutput = effect(() => {
+        if (this.sortables().length) {
+            this.tuiSortChange.emit(this.sortChange());
+        }
+    });
 
-    /**
-     * @deprecated: use tuiSortChange
-     */
-    public readonly tuiSortByChange = outputFromObservable(this.tuiSortByChange$);
-
-    public readonly tuiSortChange$: Observable<TuiSortChange<T>> = combineLatest([
-        this.tuiSortByChange$,
-        this.table.directionChange$,
-    ]).pipe(
-        debounceTime(0),
-        map(([sortKey, sortDirection]) => ({
-            sortBy: sortKey,
-            orderBy: sortDirection,
-            sortKey,
-            sortDirection,
-        })),
-    );
-
-    public readonly tuiSortChange = outputFromObservable(this.tuiSortChange$);
-
+    public readonly tuiSortChange = output<TuiSortChange<T>>();
     public readonly tuiSortBy = input<string | keyof T | null>(null);
-
-    protected readonly checkSortables = effect((_, __ = this.tuiSortBy()) =>
-        untracked(this.sortables).forEach((s) => s.check()),
-    );
 
     private getKey(sorter: TuiComparator<T> | null): keyof T | null {
         return this.sortables().find((s) => s.sorter() === sorter)?.key || null;
