55e70d6a4fe8cf9a3be7338f804a4982cc020080
waterplea <alexander@inkin.ru>
2025-12-04 17:24:34 +0400

    chore: fix

--- projects/addon-table/components/table/directives/head.directive.ts
+++ projects/addon-table/components/table/directives/head.directive.ts
@@ -1,9 +1,7 @@
 import {Directive, inject, input, TemplateRef} from '@angular/core';
-import {tuiTableOptionsProvider} from '@taiga-ui/addon-table/components';
 
 @Directive({
     selector: '[tuiHead]',
-    providers: [tuiTableOptionsProvider({sticky: true})],
 })
 export class TuiTableHead<T extends Partial<Record<keyof T, unknown>>> {
     public readonly tuiHead = input.required<string | keyof T>();
