db424061e6db88fa4d244d4b02ac8027048558a1
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2025-12-04 08:35:17 +0000

    Update snapshots/demo/next/main to output generated at f8d3471

---
+++ chunk-CCPZN3NC.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-F5JCRAE2.js";import"./chunk-42JZD6NG.js";export{a as DemoRoute};
--- chunk-SGFUUANN.js
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-MIUANWA3.js";import"./chunk-42JZD6NG.js";export{b as DEFAULT_LANGUAGE_PAGE,a as SEE_ALSO_GROUPS};
