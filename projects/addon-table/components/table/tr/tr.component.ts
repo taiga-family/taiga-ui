48c74bd2a454d4a41c5d49d5c33021ec0b1b19cc
waterplea <alexander@inkin.ru>
2025-12-04 16:40:14 +0400

    chore(demo): `Table` improve examples

--- projects/addon-table/components/table/tr/tr.component.ts
+++ projects/addon-table/components/table/tr/tr.component.ts
@@ -28,9 +28,9 @@ import {TuiTableTd} from '../td/td.component';
         '[style.--t-row-height.px]': 'height()',
     },
 })
-export class TuiTableTr<
-    T extends Partial<Record<keyof T, unknown>>,
-> implements AfterContentInit {
+export class TuiTableTr<T extends Partial<Record<keyof T, unknown>>>
+    implements AfterContentInit
+{
     private readonly cells = contentChildren<TuiTableCell>(
         forwardRef(() => TuiTableCell),
     );
