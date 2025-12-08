48d3084484b1425f97afe4d36a214c8a2d8169d3
splincode <omaxphp@yandex.ru>
2025-12-08 11:42:19 +0300

    fix(core): hover event from `TuiHint` faster than hover event of `TuiHintPointer`

--- projects/demo/src/modules/directives/hint/examples/1/index.ts
+++ projects/demo/src/modules/directives/hint/examples/1/index.ts
@@ -1,15 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiAutoColorPipe, TuiHint} from '@taiga-ui/core';
+import {TuiAutoColorPipe, TuiButton, TuiHint} from '@taiga-ui/core';
 import {TuiAvatar} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
-    imports: [TuiAutoColorPipe, TuiAvatar, TuiHint],
+    imports: [TuiAutoColorPipe, TuiAvatar, TuiButton, TuiHint],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation,
     changeDetection,
 })
-export default class Example {}
+export default class Example {
+    protected readonly counter = signal(1);
+
+    protected onClick(): void {
+        this.counter.update((n) => n + 1);
+    }
+}
