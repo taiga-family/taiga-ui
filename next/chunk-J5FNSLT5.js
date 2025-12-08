f7333426d227714d1eb0e773c2bad1d7251f4f1f
splincode <splincode@users.noreply.github.com>
2025-12-08 20:31:46 +0000

    deploy: 1af8c92951518907614116645e7056689b2e6b73

---
+++ next/chunk-J5FNSLT5.js
@@ -0,0 +1,21 @@
+import"./chunk-42JZD6NG.js";var o=`import {Component, signal} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiHint} from '@taiga-ui/core';
+import {TuiAutoColorPipe, TuiAvatar} from '@taiga-ui/kit';
+
+@Component({
+    imports: [TuiAutoColorPipe, TuiAvatar, TuiHint],
+    templateUrl: './index.html',
+    styleUrl: './index.less',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected readonly counter = signal(1);
+
+    protected onClick(): void {
+        this.counter.update((n) => n + 1);
+    }
+}
+`;export{o as default};
