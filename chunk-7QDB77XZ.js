3aeffe30ad74e384328d4eb4a144eec3ee815e31
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2025-12-09 10:21:20 +0000

    Update snapshots/demo/next/main to output generated at 288a8d3

---
+++ chunk-7QDB77XZ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-WYBBDRN2.js";import"./chunk-EZWOHIKB.js";import"./chunk-42JZD6NG.js";export{a as pages};
--- chunk-CCPZN3NC.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-F5JCRAE2.js";import"./chunk-42JZD6NG.js";export{a as DemoRoute};
