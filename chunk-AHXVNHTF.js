7da8f1b2f6196ee93b154b006689078c4f38eb2e
waterplea <alexander@inkin.ru>
2025-12-09 12:44:31 +0000

    Update snapshots/demo/next/main to output generated at 1f47f12

---
+++ chunk-AHXVNHTF.js
@@ -0,0 +1,13 @@
+import"./chunk-42JZD6NG.js";var t=`import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiCalendarRange} from '@taiga-ui/kit';
+
+@Component({
+    imports: [TuiCalendarRange],
+    templateUrl: './index.html',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {}
+`;export{t as default};
--- chunk-ZLHLMOWI.js
+++
@@ -1,2 +0,0 @@
-import"./chunk-42JZD6NG.js";var n=`<tui-calendar-range />
-`;export{n as default};
