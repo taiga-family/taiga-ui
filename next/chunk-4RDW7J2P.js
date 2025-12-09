b655a4b48558d138114341eed5ef2415f8595f2f
waterplea <waterplea@users.noreply.github.com>
2025-12-09 12:12:52 +0000

    deploy: 4c61a734a21d53077eacf164df602d7ff2bd5834

---
+++ next/chunk-4RDW7J2P.js
@@ -0,0 +1,31 @@
+import"./chunk-2K3Q2SVT.js";import"./chunk-HCF65DFK.js";import"./chunk-RFB6BHYR.js";import"./chunk-FPA7SVW6.js";import"./chunk-TMDID367.js";import"./chunk-S7EVAP5E.js";import"./chunk-NIBCWXMS.js";import"./chunk-I2MATRAK.js";import"./chunk-GLK4PTUH.js";import{a as s}from"./chunk-FFW3L4YV.js";import"./chunk-3SKFO5OU.js";import"./chunk-XEU44L55.js";import"./chunk-TMPKIETU.js";import"./chunk-GNKA3B3S.js";import{x as c}from"./chunk-WCRJQZ7E.js";import"./chunk-P5WUWMHO.js";import"./chunk-HNO7IIVZ.js";import"./chunk-VYKXGBUL.js";import"./chunk-LGNM6WP5.js";import"./chunk-NCL7DJ3K.js";import"./chunk-NDS6MTTG.js";import"./chunk-EECTWPST.js";import"./chunk-IXOB2ZKN.js";import"./chunk-GJSD4CUD.js";import"./chunk-6YTI45NV.js";import"./chunk-VPRWCYPX.js";import"./chunk-JUKN4WRM.js";import"./chunk-RGJEWAQ3.js";import{Ub as n,Xc as i,sc as r}from"./chunk-YJQUARX2.js";import"./chunk-42JZD6NG.js";var a=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path
+        vector-effect="non-scaling-stroke"
+        d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"
+    />
+</svg>
+`;var l=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path vector-effect="non-scaling-stroke" d="m21 21-4.34-4.34" />
+    <circle vector-effect="non-scaling-stroke" cx="11" cy="11" r="8" />
+</svg>
+`;var e=class e{};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=n({type:e,selectors:[["ng-component"]],features:[i([c({"@tui.heart":a,"@tui.search":l})])],decls:2,vars:0,consts:[["icon","@tui.heart"],["icon","@tui.search"]],template:function(t,u){t&1&&r(0,"tui-icon",0)(1,"tui-icon",1)},dependencies:[s],styles:["[_nghost-%COMP%]{display:flex;gap:1rem;color:var(--tui-text-action)}"],changeDetection:0});var o=e;export{o as default};
---
+++ next/chunk-QG7ZZHUR.js
@@ -0,0 +1,4 @@
+import"./chunk-42JZD6NG.js";var f=`[tuiButton]::after {
+    font-size: 1rem;
+}
+`;export{f as default};
