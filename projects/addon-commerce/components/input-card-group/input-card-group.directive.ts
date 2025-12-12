2efc5a20b823f92043486e930a59c2f2d86afd7a
Vladimir Potekhin <vladimir.potekh@gmail.com>
2025-12-12 14:03:08 +0300

    chore(addon-commerce): clean up

--- projects/addon-commerce/components/input-card-group/input-card-group.directive.ts
+++ projects/addon-commerce/components/input-card-group/input-card-group.directive.ts
@@ -1,12 +1,10 @@
-import {computed, Directive, inject, input} from '@angular/core';
+import {Directive, inject, input} from '@angular/core';
 import {TUI_BREAKPOINT} from '@taiga-ui/core/tokens';
 
 @Directive({
-    host: {'[class._compact]': 'c()'},
+    host: {'[class._compact]': "compact() || breakpoint() === 'mobile'"},
 })
 export class TuiInputCardGroupDirective {
-    private readonly breakpoint = inject(TUI_BREAKPOINT);
+    protected readonly breakpoint = inject(TUI_BREAKPOINT);
     public readonly compact = input(false);
-
-    public readonly c = computed(() => this.compact() || this.breakpoint() === 'mobile');
 }
