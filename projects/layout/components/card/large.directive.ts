c5b2420f4f214731f3a1b8d59fb0ca16141236dd
waterplea <alexander@inkin.ru>
2025-12-13 15:11:50 +0400

    feat(layout): `Card` add options token

--- projects/layout/components/card/large.directive.ts
+++ projects/layout/components/card/large.directive.ts
@@ -2,10 +2,16 @@ import {
     ChangeDetectionStrategy,
     Component,
     Directive,
+    inject,
     Input,
     ViewEncapsulation,
 } from '@angular/core';
+import {tuiCreateOptions} from '@taiga-ui/cdk';
 import {tuiWithStyles} from '@taiga-ui/cdk/utils/miscellaneous';
+
+export const [TUI_CARD_OPTIONS, tuiCardOptionsProvider] = tuiCreateOptions({
+    space: 'normal' as 'compact' | 'normal',
+});
 
 @Component({
     standalone: true,
@@ -24,12 +30,13 @@ class TuiCardLargeStyles {}
     selector: '[tuiCardLarge]',
     host: {
         tuiCardLarge: '',
-        '[attr.data-space]': 'space || "normal"',
+        '[attr.data-space]': 'space || this.options.space',
     },
 })
 export class TuiCardLarge {
+    protected readonly options = inject(TUI_CARD_OPTIONS);
     protected readonly nothing = tuiWithStyles(TuiCardLargeStyles);
 
     @Input('tuiCardLarge')
-    public space: '' | 'compact' | 'normal' = 'normal';
+    public space: '' | 'compact' | 'normal' = '';
 }
