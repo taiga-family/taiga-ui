1de26d4f33e6345d1f3023c15b7828389fea1108
Alex Inkin <alexander@inkin.ru>
2025-12-15 15:42:11 +0700

    fix(kit): `MultiSelect` fix native on newer Androids

--- projects/kit/components/multi-select/multi-select-native/multi-select-native.component.ts
+++ projects/kit/components/multi-select/multi-select-native/multi-select-native.component.ts
@@ -29,6 +29,7 @@ import {TuiMultiSelectOption} from '../multi-select-option/multi-select-option.c
     hostDirectives: [TuiInputChipDirective, TuiSelectLike],
     host: {
         multiple: '',
+        size: '1',
         '(click.stop.zoneless)': '0',
         '(input)': 'onInput()',
     },
