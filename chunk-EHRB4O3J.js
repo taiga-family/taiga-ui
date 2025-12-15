5640a4b7f4bcce2218d5f93e1c8773378b5af1ac
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2025-12-15 10:35:39 +0000

    Update snapshots/demo/next/main to output generated at a5fae91

---
+++ chunk-EHRB4O3J.js
@@ -0,0 +1,17 @@
+import"./chunk-7BMO2MUL.js";import"./chunk-UW25J3UH.js";import"./chunk-APX5FNMN.js";import"./chunk-PRUZNOIN.js";import"./chunk-NUWJO35U.js";import"./chunk-GGRNEVCB.js";import{b as s}from"./chunk-7ZGKVEAD.js";import"./chunk-HYTZN3XX.js";import"./chunk-J6B73723.js";import"./chunk-DGADGGI3.js";import"./chunk-X4KX2FS3.js";import"./chunk-2DIX7QJC.js";import"./chunk-KQRRNRNM.js";import"./chunk-7VAZLETE.js";import"./chunk-IR4HWGAK.js";import"./chunk-524KSYG7.js";import"./chunk-HNO7IIVZ.js";import"./chunk-M3CQVDM7.js";import"./chunk-LGNM6WP5.js";import"./chunk-B6S2DZWP.js";import"./chunk-KEISYNHW.js";import"./chunk-QILBXJYP.js";import"./chunk-OWZ664YP.js";import"./chunk-Q4ASTREI.js";import"./chunk-KJ26EEKF.js";import"./chunk-OBFPZKWW.js";import"./chunk-PDRUVHJE.js";import"./chunk-VGA5QJKK.js";import{Eb as o,Oc as e,Vb as l,hc as d,ic as r,rc as t,sc as n,tc as u}from"./chunk-MGMBASVD.js";import"./chunk-42JZD6NG.js";var i=class i{};i.\u0275fac=function(a){return new(a||i)},i.\u0275cmp=l({type:i,selectors:[["ng-component"]],decls:34,vars:5,consts:[["iconStart","@tui.edit","tuiLink","","type","button"],["appearance","action-grayscale","tuiLink","","type","button"],["appearance","action-destructive","iconEnd","@tui.trash","tuiLink","","type","button",3,"textContent"],["appearance","","iconEnd","@tui.external-link","tuiLink","","type","button"],["tuiTheme","dark"],["appearance","","iconStart","@tui.paintbrush","tuiLink",""],["iconStart","@tui.heart","tuiLink","","type","button"],["appearance","","tuiLink","","type","button"]],template:function(a,p){a&1&&(e(0,`Links by default have
+`),t(1,"button",0)(2,"span"),e(3,"action"),n()(),e(4,`
+appearance which you can override with
+`),t(5,"button",1),e(6,` action-grayscale
+`),n(),e(7,`
+or
+`),u(8,"button",2),t(9,"p"),e(10," If you use no appearance, links would "),t(11,"button",3)(12,"span"),e(13,"inherit text color"),n()(),e(14,` in tandem with an underline decoration to make them visible.
+`),n(),t(15,"p",4),e(16," It would also work well together with "),t(17,"a",5)(18,"span"),e(19,"dark theme"),n()(),e(20,` and other text colors
+`),n(),e(21,` If you want, you can
+`),t(22,"button",6)(23,"span"),e(24,"enable"),n()(),e(25,`
+dashed underline for any appearance using
+`),t(26,"code"),e(27,"text-decoration-line: underline"),n(),e(28,`
+or switch
+`),t(29,"button",7),e(30,` inherited
+`),n(),e(31,`
+link underline to dashed style by
+`),t(32,"code"),e(33,"text-decoration-style: dashed"),n()),a&2&&(o(8),d("textContent","action-destructive"),o(14),r("text-decoration-line","underline"),o(7),r("text-decoration-style","dashed"))},dependencies:[s],styles:["[tuiTheme=dark][_ngcontent-%COMP%]{background:var(--tui-background-base);color:var(--tui-text-secondary);padding:1rem;margin:1rem -1rem}"],changeDetection:0});var c=i;export{c as default};
