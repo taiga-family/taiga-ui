9d1ac3cb243d85a6c8f887a2781039702602ffc3
waterplea <alexander@inkin.ru>
2025-12-16 08:48:45 +0000

    Update snapshots/demo/next/main to output generated at 40d4ad8

--- chunk-KEXU6FZE.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-ZFUTC36C.js";import"./chunk-MGMBASVD.js";import"./chunk-42JZD6NG.js";export{a as IconsGroupTemplate};
--- chunk-Z64MVJ6O.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-EZWOHIKB.js";import"./chunk-42JZD6NG.js";export{a as DemoRoute};
