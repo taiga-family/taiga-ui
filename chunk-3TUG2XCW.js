9c5c1d1e35f554082113c34062f830d5ce7a1987
waterplea <alexander@inkin.ru>
2025-12-16 09:33:54 +0000

    Update snapshots/demo/next/main to output generated at 4752788

--- chunk-3TUG2XCW.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-62HI6EII.js";import"./chunk-2FJ3NQGZ.js";import"./chunk-42JZD6NG.js";export{a as pages};
---
+++ chunk-HBRHVDC5.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-I7YBMUFL.js";import"./chunk-42JZD6NG.js";export{a as DemoRoute};
