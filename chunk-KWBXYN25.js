63809be49bb5535165e4b2af59918feb3e0e7432
splincode <omaxphp@yandex.ru>
2025-12-18 07:03:16 +0000

    Update snapshots/demo/next/main to output generated at 33db86b

---
+++ chunk-KWBXYN25.js
@@ -0,0 +1,9 @@
+import"./chunk-B4AJQJMI.js";var o=`<div
+    tuiHint="Wow! How exciting!"
+    tuiHintDirection="top-start"
+    tuiHintPointer
+    class="block"
+>
+    <p>In this block hint follows cursor</p>
+</div>
+`;export{o as default};
