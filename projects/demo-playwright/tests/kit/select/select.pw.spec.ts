15491f6200f0a7933b1cea66e20b097c97679010
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-12-18 11:17:56 +0300

    fix(kit): `Select` should ignore `Backspace`/`Delete` keyboard keys if cleaner is disabled (#12865)

--- projects/demo-playwright/tests/kit/select/select.pw.spec.ts
+++ projects/demo-playwright/tests/kit/select/select.pw.spec.ts
@@ -5,50 +5,96 @@ import {
     TuiSelectPO,
     waitIcons,
 } from '@demo-playwright/utils';
-import {expect, test} from '@playwright/test';
+import {expect, type Locator, test} from '@playwright/test';
 
-test.describe('Select', () => {
+const {describe, beforeEach} = test;
+
+describe('Select', () => {
     let documentationPage: TuiDocumentationPagePO;
 
-    test.beforeEach(async ({page}) => {
-        await tuiGoto(page, DemoRoute.Select);
-        documentationPage = new TuiDocumentationPagePO(page);
-    });
-
-    test('checkmark size', async ({page}) => {
-        const example = documentationPage.getExample('#content-customization');
-        const host = example.locator('tui-textfield').first();
-        const select = new TuiSelectPO(host);
-
-        await host.scrollIntoViewIfNeeded();
-
-        await select.textfield.click();
-
-        await expect(select.dropdown).toBeVisible();
-
-        await waitIcons({
-            page,
-            icons: await example.locator('tui-icon >> visible=true').all(),
+    describe('Examples', () => {
+        beforeEach(async ({page}) => {
+            await tuiGoto(page, DemoRoute.Select);
+            documentationPage = new TuiDocumentationPagePO(page);
         });
 
-        await expect.soft(example).toHaveScreenshot('01-checkmark-size.png');
-    });
+        test('checkmark size', async ({page}) => {
+            const example = documentationPage.getExample('#content-customization');
+            const host = example.locator('tui-textfield').first();
+            const select = new TuiSelectPO(host);
 
-    test('opens dropdown by click on icon', async ({page}) => {
-        const example = documentationPage.getExample('#items-handlers');
-        const host = example.locator('tui-textfield').first();
-        const select = new TuiSelectPO(host);
+            await host.scrollIntoViewIfNeeded();
 
-        await host.scrollIntoViewIfNeeded();
-        await select.textfield.click({position: {x: 200, y: 30}});
+            await select.textfield.click();
 
-        await expect(select.dropdown).toBeVisible();
+            await expect(select.dropdown).toBeVisible();
 
-        await waitIcons({
-            page,
-            icons: await example.locator('tui-icon >> visible=true').all(),
+            await waitIcons({
+                page,
+                icons: await example.locator('tui-icon >> visible=true').all(),
+            });
+
+            await expect.soft(example).toHaveScreenshot('01-checkmark-size.png');
         });
 
-        await expect.soft(select.dropdown).toHaveScreenshot('01-click-arrow.png');
+        test('opens dropdown by click on icon', async ({page}) => {
+            const example = documentationPage.getExample('#items-handlers');
+            const host = example.locator('tui-textfield').first();
+            const select = new TuiSelectPO(host);
+
+            await host.scrollIntoViewIfNeeded();
+            await select.textfield.click({position: {x: 200, y: 30}});
+
+            await expect(select.dropdown).toBeVisible();
+
+            await waitIcons({
+                page,
+                icons: await example.locator('tui-icon >> visible=true').all(),
+            });
+
+            await expect.soft(select.dropdown).toHaveScreenshot('01-click-arrow.png');
+        });
+    });
+
+    describe('Keyboard clearing', () => {
+        [true, false].forEach((cleanerEnabled) => {
+            describe(`tuiTextfieldCleaner=${cleanerEnabled}`, () => {
+                let example!: Locator;
+                let select!: TuiSelectPO;
+
+                beforeEach(async ({page}) => {
+                    await tuiGoto(
+                        page,
+                        `${DemoRoute.Select}/API?tuiTextfieldCleaner=${cleanerEnabled}`,
+                    );
+                    example = new TuiDocumentationPagePO(page).apiPageExample;
+                    select = new TuiSelectPO(
+                        example.locator('tui-textfield:has([tuiSelect])'),
+                    );
+
+                    await select.textfield.click();
+                    await select.selectOptions([1]);
+                    await select.textfield.click();
+
+                    await expect(select.dropdown).toBeVisible();
+                    await expect(select.textfield).toHaveValue('United Arab Emirates');
+                });
+
+                test('Backspace', async ({page}) => {
+                    await page.keyboard.press('Backspace');
+                    await expect(select.textfield).toHaveValue(
+                        cleanerEnabled ? '' : 'United Arab Emirates',
+                    );
+                });
+
+                test('Delete', async ({page}) => {
+                    await page.keyboard.press('ControlOrMeta+A');
+                    await page.keyboard.press('Delete');
+                    await expect(select.textfield).toHaveValue(
+                        cleanerEnabled ? '' : 'United Arab Emirates',
+                    );
+                });
+            });
+        });
     });
 });
