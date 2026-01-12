b3b54d9c018d239dfff8e0ca06af98601e34e4ff
splincode <omaxphp@yandex.ru>
2026-01-12 13:50:55 +0300

    refactor: compat with new `@taiga-ui/jest-config`

--- projects/testing/mocks/switch-ng-dev-mode.ts
+++
@@ -1,7 +0,0 @@
-declare const global: (Record<any, any> & Window) | undefined;
-
-export function tuiSwitchNgDevMode(enable: boolean): void {
-    if (global) {
-        global.ngDevMode = enable;
-    }
-}
