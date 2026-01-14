4920bec6006f8a822adfc40ffc47dbfa45f7f6e8
Vladimir Potekhin <vladimir.potekh@gmail.com>
2026-01-14 12:50:58 +0300

    chore: add pw tests

---
+++ projects/demo-playwright/utils/page-objects/input-chip.po.ts
@@ -0,0 +1,20 @@
+import {type Locator} from '@playwright/test';
+
+export class TuiInputChipPO {
+    public readonly input: Locator;
+    public readonly chips = this.host.locator('.t-items tui-textfield-item');
+    public readonly cleaner = this.host.getByRole('button', {name: 'Clear'});
+    public readonly dropdown = this.host
+        .page()
+        .locator('tui-dropdown,tui-dropdown-mobile,tui-sheet-dialog');
+
+    constructor(public readonly host: Locator) {
+        this.input = host.locator('tui-textfield').first().locator('input[tuiInputChip]');
+    }
+
+    public async addChip(value: string): Promise<void> {
+        await this.input.focus();
+        await this.input.fill(value);
+        await this.input.press('Enter');
+    }
+}
