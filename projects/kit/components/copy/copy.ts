14eed8ca6a4186de7a67f14838d1b2563468be66
splincode <omaxphp@yandex.ru>
2026-01-14 16:45:25 +0300

    feat(kit): add `tuiButtonCopy`

--- projects/kit/components/copy/copy.ts
+++ projects/kit/components/copy/copy.ts
@@ -1,4 +1,5 @@
+import {TuiButtonCopy} from './button-copy.component';
 import {TuiCopyComponent} from './copy.component';
 import {TuiCopyDirective} from './copy.directive';
 
-export const TuiCopy = [TuiCopyComponent, TuiCopyDirective] as const;
+export const TuiCopy = [TuiCopyComponent, TuiCopyDirective, TuiButtonCopy] as const;
--- projects/kit/components/copy/index.ts
+++ projects/kit/components/copy/index.ts
@@ -1,4 +1,5 @@
 // TODO: Move to components in v5
+export * from './button-copy.component';
 export * from './copy';
 export * from './copy.component';
 export * from './copy.directive';
