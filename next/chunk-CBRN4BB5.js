35c59090f530d71ad59c099a5778c19d58cf78ab
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2026-01-19 16:07:45 +0000

    deploy: 26e65faa242a1874120cfc6fb949f51de8f453bb

---
+++ next/chunk-CBRN4BB5.js
@@ -0,0 +1,17 @@
+import"./chunk-MTMFGCNK.js";import"./chunk-GSO5QF6I.js";import"./chunk-AFT52YPK.js";import"./chunk-2T4X2LV3.js";import"./chunk-BKPVRSY7.js";import"./chunk-ELY644L6.js";import{b as s}from"./chunk-SB4AYCIF.js";import"./chunk-V47ARJRW.js";import"./chunk-QOGPZLR7.js";import"./chunk-RPH7DL6Z.js";import"./chunk-XP2MHZRF.js";import"./chunk-BNMMA7ON.js";import"./chunk-VNJO5E4L.js";import"./chunk-ZRLH7FBT.js";import"./chunk-V2W56X3Q.js";import"./chunk-WAGUEL5G.js";import"./chunk-UKGYP7FE.js";import"./chunk-5PSQ6REG.js";import"./chunk-NQAPFFN6.js";import"./chunk-BY25M3JC.js";import"./chunk-P6GCRDNG.js";import"./chunk-K6OQRHIO.js";import"./chunk-RVPNA2KV.js";import"./chunk-XGV2XGQL.js";import"./chunk-JXMQFTJS.js";import"./chunk-JR7I2CRN.js";import"./chunk-GLSTXU6L.js";import"./chunk-JD445NGS.js";import{Cb as o,Mc as e,Tb as l,fc as d,gc as r,pc as t,qc as n,rc as u}from"./chunk-VQTANHN4.js";import"./chunk-B4AJQJMI.js";var i=class i{};i.\u0275fac=function(a){return new(a||i)},i.\u0275cmp=l({type:i,selectors:[["ng-component"]],decls:34,vars:5,consts:[["iconStart","@tui.edit","tuiLink","","type","button"],["appearance","action-grayscale","tuiLink","","type","button"],["appearance","action-destructive","iconEnd","@tui.trash","tuiLink","","type","button",3,"textContent"],["appearance","","iconEnd","@tui.external-link","tuiLink","","type","button"],["tuiTheme","dark"],["appearance","","iconStart","@tui.paintbrush","tuiLink",""],["iconStart","@tui.heart","tuiLink","","type","button"],["appearance","","tuiLink","","type","button"]],template:function(a,p){a&1&&(e(0,`Links by default have
+`),t(1,"button",0)(2,"span"),e(3,"action"),n()(),e(4,`
+appearance which you can override with
+`),t(5,"button",1),e(6,` action-grayscale
+`),n(),e(7,`
+or
+`),u(8,"button",2),t(9,"p"),e(10," If you use no appearance, links would "),t(11,"button",3)(12,"span"),e(13,"inherit text color"),n()(),e(14,` in tandem with an underline decoration to make them visible.
+`),n(),t(15,"p",4),e(16," It would also work well together with "),t(17,"a",5)(18,"span"),e(19,"dark theme"),n()(),e(20,` and other text colors
+`),n(),e(21,` If you want, you can
+`),t(22,"button",6)(23,"span"),e(24,"enable"),n()(),e(25,`
+dashed underline for any appearance using
+`),t(26,"code"),e(27,"text-decoration-line: underline"),n(),e(28,`
+or switch
+`),t(29,"button",7),e(30,` inherited
+`),n(),e(31,`
+link underline to dashed style by
+`),t(32,"code"),e(33,"text-decoration-style: dashed"),n()),a&2&&(o(8),d("textContent","action-destructive"),o(14),r("text-decoration-line","underline"),o(7),r("text-decoration-style","dashed"))},dependencies:[s],styles:["[tuiTheme=dark][_ngcontent-%COMP%]{background:var(--tui-background-base);color:var(--tui-text-secondary);padding:1rem;margin:1rem -1rem}"],changeDetection:0});var c=i;export{c as default};
