53bd1b76d20e9a11742d41b8a06339543fe6bb42
splincode <splincode@users.noreply.github.com>
2026-01-20 10:39:59 +0000

    deploy: 492f4c213d50d7daca85f2e17d0e174a113acefc

--- next/chunk-37ITL6RJ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-B4AJQJMI.js";var s="```ts\nexport default {\n  setupFilesAfterEnv: ['<rootDir>/setup-jest.js'],\n};\n```\n";export{s as default};
--- next/chunk-T4FGVBIP.js
+++
@@ -1,8 +0,0 @@
-import"./chunk-B4AJQJMI.js";var t=`\`\`\`json
-{
-  "jest": {
-    "setupFilesAfterEnv": ["<rootDir>/setup-jest.js"]
-  }
-}
-\`\`\`
-`;export{t as default};
