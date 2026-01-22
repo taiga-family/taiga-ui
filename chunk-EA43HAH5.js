f5596c91ed856ac1b7a40b712c1bc64c8b8bc568
waterplea <alexander@inkin.ru>
2026-01-22 17:36:53 +0000

    Update snapshots/demo/next/main to output generated at c343ecb

---
+++ chunk-EA43HAH5.js
@@ -0,0 +1,15 @@
+import"./chunk-B4AJQJMI.js";var e=`.explore {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    gap: 1rem;
+    margin: 1rem 0;
+
+    [tuiAvatar] {
+        background: var(--tui-background-base);
+    }
+
+    button {
+        cursor: pointer;
+    }
+}
+`;export{e as default};
--- chunk-N643YPNP.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-TBKRX6TV.js";import"./chunk-AXUCSVX7.js";import"./chunk-B4AJQJMI.js";export{a as DialogExample};
