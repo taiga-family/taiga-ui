db71c13d784bf4cb1b83c0a623184afd25308812
waterplea <alexander@inkin.ru>
2026-01-23 20:24:39 +0400

    chore(core): `DataList` only apply content to value options

--- projects/core/components/data-list/option.directive.ts
+++ projects/core/components/data-list/option.directive.ts
@@ -1,15 +1,4 @@
-import {
-    createComponent,
-    Directive,
-    EnvironmentInjector,
-    forwardRef,
-    inject,
-    INJECTOR,
-    input,
-    type OnDestroy,
-    type Type,
-    ViewContainerRef,
-} from '@angular/core';
+import {Directive, forwardRef, inject, input, type OnDestroy} from '@angular/core';
 import {WA_IS_MOBILE} from '@ng-web-apis/platform';
 import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
 import {tuiIsFocused} from '@taiga-ui/cdk/utils/focus';
@@ -18,7 +7,6 @@ import {TuiWithIcons} from '@taiga-ui/core/directives/icons';
 import {TuiDropdownDirective} from '@taiga-ui/core/portals/dropdown';
 
 import {TuiDataListComponent} from './data-list.component';
-import {TUI_OPTION_CONTENT} from './option-content.directive';
 
 @Directive({
     selector: 'button[tuiOption], a[tuiOption], label[tuiOption]',
@@ -32,7 +20,6 @@ import {TUI_OPTION_CONTENT} from './option-content.directive';
     },
 })
 export class TuiOption<T = unknown> implements OnDestroy {
-    private readonly vcr = inject(ViewContainerRef);
     private readonly isMobile = inject(WA_IS_MOBILE);
     private readonly el = tuiInjectElement();
     private readonly datalist = inject<TuiDataListComponent<T>>(
@@ -40,31 +27,12 @@ export class TuiOption<T = unknown> implements OnDestroy {
         {optional: true},
     );
 
-    private readonly content = inject<Type<any> | null>(TUI_OPTION_CONTENT, {
-        optional: true,
-    });
-
-    private readonly ref =
-        this.content &&
-        createComponent(this.content, {
-            environmentInjector: inject(EnvironmentInjector),
-            elementInjector: inject(INJECTOR),
-            hostElement: tuiInjectElement(),
-        });
-
     protected readonly dropdown = inject(TuiDropdownDirective, {
         self: true,
         optional: true,
     })?.ref;
 
     public readonly disabled = input(false);
-
-    constructor() {
-        if (this.ref) {
-            this.vcr.insert(this.ref.hostView);
-            this.ref.changeDetectorRef.detectChanges();
-        }
-    }
 
     // Preventing focus loss upon focused option removal
     public ngOnDestroy(): void {
