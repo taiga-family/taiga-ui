4e44757bcc29b7590b6410a1494a42e4b59dd54b
splincode <omaxphp@yandex.ru>
2026-01-26 12:52:43 +0000

    Update snapshots/demo/next/main to output generated at 602d43a

---
+++ chunk-Q74G745U.js
@@ -0,0 +1,1 @@
+import"./chunk-B4AJQJMI.js";var g="```bash\n$ nx migrate @taiga-ui/cdk\n```\n";export{g as default};
--- chunk-WBXTCMH5.js
+++
@@ -1,8 +0,0 @@
-import"./chunk-B4AJQJMI.js";var i=`\`\`\`bash
-# Angular CLI
-$ ng update @taiga-ui/cdk
-
-# Nx CLI
-$ nx migrate @taiga-ui/cdk
-\`\`\`
-`;export{i as default};
