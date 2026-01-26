a080f21aa97726b6a81fa2269bcc99653654571f
waterplea <waterplea@users.noreply.github.com>
2026-01-26 11:39:17 +0000

    deploy: 6e407da3ee2be25567a8c31b1c183bb8bb2016a3

--- next/chunk-QPKANQWK.js
+++
@@ -1,9 +0,0 @@
-import"./chunk-B4AJQJMI.js";var e=`[tuiGroup] {
-    inline-size: fit-content;
-    margin: 0 auto 1rem;
-}
-
-[tuiSubtitle] {
-    color: var(--tui-text-secondary);
-}
-`;export{e as default};
