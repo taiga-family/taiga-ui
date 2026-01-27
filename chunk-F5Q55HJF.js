9eb06c271d1bd1bf946f3537bad58981556bdcab
waterplea <alexander@inkin.ru>
2026-01-27 09:06:38 +0000

    Update snapshots/demo/next/main to output generated at 52cc60f

---
+++ chunk-F5Q55HJF.js
@@ -0,0 +1,1 @@
+import{ab as a}from"./chunk-EZMZGQEL.js";import{Pa as o,ta as e}from"./chunk-AXUCSVX7.js";var t=class t extends a{};t.\u0275fac=(()=>{let r;return function(c){return(r||(r=o(t)))(c||t)}})(),t.\u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"});var i=t;export{i as a};
