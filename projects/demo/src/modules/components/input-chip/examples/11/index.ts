db8b85a20002bfa089113bdf6e2b63c46377bfea
Максим Иванов <splincodewd@yandex.ru>
2026-02-02 11:35:47 +0300

    fix(kit): input chip filter should ignore empty stringified values (#13095)

---
+++ projects/demo/src/modules/components/input-chip/examples/11/index.ts
@@ -0,0 +1,34 @@
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiDataList, TuiTextfield} from '@taiga-ui/core';
+import {TuiChevron, TuiInputChip, TuiMultiSelect} from '@taiga-ui/kit';
+import {NgForOf} from '@angular/common';
+
+@Component({
+    standalone: true,
+    imports: [
+        FormsModule,
+        NgForOf,
+        TuiChevron,
+        TuiDataList,
+        TuiInputChip,
+        TuiMultiSelect,
+        TuiTextfield,
+    ],
+    templateUrl: './index.html',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected value: string[] = [];
+
+    protected items = [
+        {nickname: 'a.inkin', name: 'Alex Inkin'},
+        {nickname: 'r.sedov', name: 'Roman Sedov'},
+    ];
+
+    protected readonly stringify = (value: string): string =>
+        this.items.find((item) => item.nickname === value)?.name ?? '';
+}
--- projects/kit/components/input-chip/input-chip.directive.ts
+++ projects/kit/components/input-chip/input-chip.directive.ts
@@ -86,10 +86,13 @@ export class TuiInputChipBaseDirective<T>
         const items = this.separator ? value.split(this.separator) : [value];
 
         const valid = items
-            .map(tuiSanitizeText)
+            .map((item) => tuiSanitizeText(item) as T)
             .filter(
-                (item) => item && !this.handlers.disabledItemHandler()(item as T),
-            ) as T[];
+                (item) =>
+                    item &&
+                    !this.handlers.disabledItemHandler()(item) &&
+                    this.handlers.stringify()(item),
+            );
 
         if (!value || !valid.length) {
             return;
