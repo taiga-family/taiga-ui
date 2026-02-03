c576dd5399ebfba19a31859eabfb456b0f8199a6
Vladimir Potekhin <vladimir.potekh@gmail.com>
2026-02-03 18:55:54 +0300

    feat(addon-mobile): `SwipeAction` add onboarding animation

---
+++ projects/addon-mobile/components/swipe-action/swipe-actions-onboarding.directive.ts
@@ -0,0 +1,30 @@
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Directive,
+    model,
+    ViewEncapsulation,
+} from '@angular/core';
+import {tuiWithStyles} from '@taiga-ui/cdk/utils';
+
+@Component({
+    template: '',
+    styleUrl: './swipe-actions-onboarding.style.less',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {class: 'tui-swipe-actions'},
+})
+class Styles {}
+
+@Directive({
+    selector: 'tui-swipe-actions[onboarding]',
+    host: {
+        '[class._onboarding]': 'onboarding()',
+        '(animationend)': 'onboarding.set($event.animationName !== "onboarding-exit")',
+    },
+})
+export class TuiSwipeActionsOnboarding {
+    public readonly onboarding = model(true);
+
+    protected readonly nothing = tuiWithStyles(Styles);
+}
