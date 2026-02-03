b3401e6ac7ea80f3e97e0179054a4aa477f3ad06
waterplea <waterplea@users.noreply.github.com>
2026-02-03 09:14:17 +0000

    deploy: f78b22880cf4b5028d1b6808cd22961ea6fb0e4b

---
+++ next/chunk-PL63ALZA.js
@@ -0,0 +1,13 @@
+import"./chunk-B4AJQJMI.js";var i=`<tui-textfield
+    tuiTextfieldSize="s"
+    [style.-webkit-text-security]="'disc'"
+    [style.gap.rem]="0.75"
+    [style.width.rem]="9"
+>
+    <input
+        maxlength="3"
+        tuiInputPin
+        [formControl]="control"
+    />
+</tui-textfield>
+`;export{i as default};
