585c3dca33f13ed5c28002080c253a116fce5b9c
Максим Иванов <splincodewd@yandex.ru>
2026-02-04 14:51:00 +0300

    feat(layout): use signals in `tui-elastic-container` (#13163)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/layout/components/elastic-container/elastic-container.component.ts
+++ projects/layout/components/elastic-container/elastic-container.component.ts
@@ -1,4 +1,5 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {tuiPx} from '@taiga-ui/cdk/utils/miscellaneous';
 
 import {TuiElasticContainerDirective} from './elastic-container.directive';
 
@@ -8,15 +9,19 @@ import {TuiElasticContainerDirective} from './elastic-container.directive';
     templateUrl: './elastic-container.component.html',
     styleUrl: './elastic-container.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    host: {'[style.block-size]': 'transitions ? "auto" : height'},
+    host: {'[style.block-size]': 'transitions() ? "auto" : height()'},
 })
 export class TuiElasticContainer {
-    protected height = '';
-    protected transitions = 0;
+    protected height = signal('');
+    protected transitions = signal(0);
 
     protected onAnimation(name: string, count: number): void {
         if (name === 'height') {
-            this.transitions += count;
+            this.transitions.update((value) => value + count);
         }
+    }
+
+    protected updateHeight(height: number): void {
+        this.height.set(tuiPx(height));
     }
 }
