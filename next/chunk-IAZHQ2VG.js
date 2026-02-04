ef4b34774f2ff7239ed127a0d0ff6f638e3ca2df
splincode <splincode@users.noreply.github.com>
2026-02-04 10:16:46 +0000

    deploy: 22806312f9492d108ba3cf7b182fae9ddb74e3aa

---
+++ next/chunk-IAZHQ2VG.js
@@ -0,0 +1,20 @@
+import"./chunk-B4AJQJMI.js";var t=`import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {TuiDemo} from '@demo/utils';
+
+@Component({
+    imports: [TuiDemo],
+    templateUrl: './index.html',
+    changeDetection,
+})
+export default class Page {
+    protected readonly examples = [
+        'Basic',
+        'Custom',
+        'Autoclose',
+        'Dynamic actions',
+        'Fallback for desktop',
+        'Onboarding',
+    ];
+}
+`;export{t as default};
