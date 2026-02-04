41dbe03c66ecd829024f2fe9fcce9dcb3a661612
waterplea <alexander@inkin.ru>
2026-02-04 16:38:45 +0400

    chore: fix

--- projects/core/components/textfield/textfield-multi/textfield-multi.component.ts
+++ projects/core/components/textfield/textfield-multi/textfield-multi.component.ts
@@ -21,6 +21,7 @@ import {tuiProvide} from '@taiga-ui/cdk/utils/di';
 import {tuiIsElement} from '@taiga-ui/cdk/utils/dom';
 import {tuiArrayToggle, tuiPx} from '@taiga-ui/cdk/utils/miscellaneous';
 import {tuiButtonOptionsProvider} from '@taiga-ui/core/components/button';
+import {TuiCell} from '@taiga-ui/core/components/cell';
 import {tuiAsDataListHost} from '@taiga-ui/core/components/data-list';
 import {TUI_SCROLL_REF, TuiScrollControls} from '@taiga-ui/core/components/scrollbar';
 import {TuiButtonX} from '@taiga-ui/core/directives/button-x';
@@ -37,6 +38,7 @@ import {TUI_TEXTFIELD_ITEM} from './textfield-item.component';
         AsyncPipe,
         PolymorpheusOutlet,
         TuiButtonX,
+        TuiCell,
         TuiScrollControls,
         WaResizeObserver,
     ],
