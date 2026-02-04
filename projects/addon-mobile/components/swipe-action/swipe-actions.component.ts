260a3a41840dafd295d1536409b17c0d3bb4f0e1
Vladimir Potekhin <vladimir.potekh@gmail.com>
2026-02-04 17:58:50 +0300

    feat(addon-mobile): `SwipeActions` add onboarding animation

--- projects/addon-mobile/components/swipe-action/swipe-actions.component.ts
+++ projects/addon-mobile/components/swipe-action/swipe-actions.component.ts
@@ -3,6 +3,7 @@ import {
     ChangeDetectorRef,
     Component,
     inject,
+    signal,
 } from '@angular/core';
 import {WaResizeObserver} from '@ng-web-apis/resize-observer';
 
@@ -15,12 +16,15 @@ import {WaResizeObserver} from '@ng-web-apis/resize-observer';
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
         '[style.--t-actions-width]': 'actionsWidth',
+        '[style.overscroll-behavior-x]': 'scrolled() ?  "contain" : "none"',
+        '(scroll.zoneless)': 'scrolled.set($event.target.scrollLeft > 0)',
     },
 })
 export class TuiSwipeActions {
     protected actionsWidth = 0;
 
     protected readonly cdr = inject(ChangeDetectorRef);
+    protected readonly scrolled = signal(false);
 
     protected onResize({target}: ResizeObserverEntry): void {
         this.actionsWidth = target.clientWidth;
