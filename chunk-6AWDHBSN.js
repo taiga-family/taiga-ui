f57081aecabd4ae973842c2f25237a453d93988d
splincode <omaxphp@yandex.ru>
2026-02-06 14:53:37 +0000

    Update snapshots/demo/next/main to output generated at f24fabc

---
+++ chunk-6AWDHBSN.js
@@ -0,0 +1,12 @@
+import"./chunk-B4AJQJMI.js";var t=`\`\`\`ts
+import {Component} from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app',
+  templateUrl: './app.html',
+  styleUrl: './app.less',
+})
+export class App {}
+\`\`\`
+`;export{t as default};
