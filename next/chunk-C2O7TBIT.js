cf0fe749c56f136a1c7934e6e23175b2ee5cb3a9
splincode <splincode@users.noreply.github.com>
2026-02-09 09:39:16 +0000

    deploy: c4722e5c78619def5ecaf408ae1d851ab3d626c1

---
+++ next/chunk-C2O7TBIT.js
@@ -0,0 +1,10 @@
+import"./chunk-B4AJQJMI.js";var e=`<button
+    size="s"
+    tuiButton
+    type="button"
+    [disabled]="ref()"
+    (click)="openPopout()"
+>
+    Open a window mode
+</button>
+`;export{e as default};
--- next/chunk-KJEDCOE4.js
+++
@@ -1,5 +0,0 @@
-import"./chunk-B4AJQJMI.js";var l=`:host {
-    display: flex;
-    gap: 1rem;
-}
-`;export{l as default};
