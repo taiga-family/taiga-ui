74b41b8e1969bc02c84f16f9d8f7670d090e2869
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2026-02-09 18:40:25 +0300

    chore: fix replace-attrs migration (#13128)

---
+++ projects/cdk/schematics/ng-update/v5/tests/schematic-migrate-identifiers.spec.ts
@@ -0,0 +1,48 @@
+import {join} from 'node:path';
+
+import {resetActiveProject} from 'ng-morph';
+
+import {runMigration} from '../../../utils/run-migration';
+
+const collection = join(__dirname, '../../../migration.json');
+
+describe('ng-update identifiers migration', () => {
+    async function migrate(template: string): Promise<string> {
+        const {template: result} = await runMigration({
+            template,
+            collection,
+            component: `
+                import {Component} from '@angular/core';
+
+                @Component({
+                    templateUrl: './test.html',
+                })
+                export class Test {}
+            `,
+        });
+
+        return result;
+    }
+
+    it('replaces tuiButtonClose with tuiButtonX', async () => {
+        expect(await migrate('<button tuiButtonClose></button>')).toEqual(
+            '<button tuiButtonX></button>',
+        );
+    });
+
+    it('preserves attributes when replacing tuiButtonClose with tuiButtonX', async () => {
+        expect(
+            await migrate(
+                '<button tuiButtonClose appearance="primary" size="m"></button>',
+            ),
+        ).toEqual('<button tuiButtonX appearance="primary" size="m"></button>');
+    });
+
+    it('does not affect other buttons', async () => {
+        expect(await migrate('<button tuiButton>Click me</button>')).toEqual(
+            '<button tuiButton>Click me</button>',
+        );
+    });
+
+    afterEach(() => resetActiveProject());
+});
