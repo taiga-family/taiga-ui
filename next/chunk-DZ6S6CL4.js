a0c4afcd3793f96c2b97a3bc78834b2b54e13bf3
nsbarsukov <nsbarsukov@users.noreply.github.com>
2026-02-10 08:57:04 +0000

    deploy: ca946c3c5327c34b296d6054cceb9dce8cd7f2bf

---
+++ next/chunk-DZ6S6CL4.js
@@ -0,0 +1,9 @@
+import"./chunk-B4AJQJMI.js";var o=`<tui-textfield>
+    <label tuiLabel>Initially touched control</label>
+    <input
+        tuiInput
+        [formControl]="control"
+    />
+</tui-textfield>
+<tui-error [formControl]="control" />
+`;export{o as default};
