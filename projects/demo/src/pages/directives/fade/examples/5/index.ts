c8de585e18b1a5a0bf5e75bb5e047e27b001b212
Максим Иванов <splincodewd@yandex.ru>
2026-02-10 12:05:12 +0300

    fix(core): incorrect browser render on `tuiFade` (#13216)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

---
+++ projects/demo/src/pages/directives/fade/examples/5/index.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiTextfield} from '@taiga-ui/core';
+import {TuiInputChip} from '@taiga-ui/kit';
+
+@Component({
+    standalone: true,
+    imports: [FormsModule, TuiInputChip, TuiTextfield],
+    templateUrl: './index.html',
+    styleUrl: './index.less',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected value: string[] = ['3', '4', '5', 'Compartmentalization'];
+}
