bd647a545eda6727ba96e9b206e05f02ad94db0b
splincode <splincode@users.noreply.github.com>
2026-02-11 14:52:43 +0000

    deploy: 4107f2774d3b599df4f4ff780d4d3859494abe3d

--- next/chunk-CJC52TF2.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./chunk-SUPAT67U.js";import{a as n}from"./chunk-GOJWO2R3.js";import{wa as r,za as t}from"./chunk-2UPKG2B4.js";var E=new r(""),a=[{provide:E,useFactory:()=>{let o=t(n),i=(o.querySelector("base")?.href??"").replace(o.location.origin,"");return e.get(i)??e.get("/")??null}}];export{E as a,a as b};
---
+++ next/chunk-M6PUGBHM.js
@@ -0,0 +1,12 @@
+import"./chunk-B4AJQJMI.js";var a=`<tui-textfield iconStart="@tui.pencil">
+    <label tuiLabel>Your best thought</label>
+    <textarea
+        placeholder="Write something..."
+        required
+        tuiTextarea
+        [max]="4"
+        [min]="4"
+        [(ngModel)]="value"
+    ></textarea>
+</tui-textfield>
+`;export{a as default};
