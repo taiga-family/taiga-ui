512b936a05d2e929e3919e8d323abf0674f99f54
splincode <splincode@users.noreply.github.com>
2026-02-13 12:47:59 +0000

    deploy: 75ea51ca4f81badff59dac69a99b35215e821af4

---
+++ next/chunk-VL7UOU26.js
@@ -0,0 +1,11 @@
+import"./chunk-6XQGLAGD.js";import"./chunk-5ZNE3BVD.js";import"./chunk-WQ6TW6H5.js";import"./chunk-OC3X2O3F.js";import"./chunk-L6M7T63J.js";import"./chunk-SFRPWYQ6.js";import"./chunk-GMH6YA2G.js";import"./chunk-D5ZZQVHD.js";import"./chunk-QMYKWGAQ.js";import"./chunk-OYKE5OR7.js";import"./chunk-AAJWQVN3.js";import"./chunk-FTCBM4DE.js";import"./chunk-EBHFP2TI.js";import"./chunk-S25T7PW2.js";import{fa as l}from"./chunk-X5HSN2F7.js";import"./chunk-LL2PODKS.js";import"./chunk-KFO6RLOG.js";import"./chunk-5PABLJY7.js";import"./chunk-SXUQLDLW.js";import"./chunk-FDI6E7EX.js";import"./chunk-XPB6YNIV.js";import"./chunk-XHQXVLHQ.js";import"./chunk-PIT57PTL.js";import"./chunk-OBPPGIVQ.js";import"./chunk-TQEODDI3.js";import"./chunk-KWZHGPWS.js";import"./chunk-TDHYON55.js";import"./chunk-GOJWO2R3.js";import{Mc as t,Tb as p,pc as e,qc as n}from"./chunk-2UPKG2B4.js";import"./chunk-B4AJQJMI.js";var i=class i{};i.\u0275fac=function(a){return new(a||i)},i.\u0275cmp=p({type:i,selectors:[["ng-component"]],decls:29,vars:0,consts:[["appearance","primary","tuiButton","","type","button"],["appearance","accent","tuiButton","","type","button"],["appearance","secondary","tuiButton","","type","button"],["appearance","flat","tuiButton","","type","button"],["appearance","outline","tuiButton","","type","button"],["appearance","floating","tuiButton","","type","button"],["appearance","primary","disabled","","tuiButton","","type","button"],["appearance","outline","tuiAppearanceMode","checked","tuiButton","","type","button"],["appearance","outline-grayscale","tuiAppearanceMode","checked invalid","tuiButton","","type","button"]],template:function(a,u){a&1&&(e(0,"button",0),t(1,` Primary
+`),n(),e(2,"button",1),t(3,` Accent
+`),n(),e(4,"button",2),t(5,` Secondary
+`),n(),e(6,"button",3),t(7,` Flat
+`),n(),e(8,"button",4),t(9,` Outline
+`),n(),e(10,"button",5),t(11,` Floating
+`),n(),e(12,"button",6),t(13,` Primary disabled
+`),n(),e(14,"div"),t(15," Use "),e(16,"code"),t(17,"tuiAppearanceMode"),n(),t(18," to emulate "),e(19,"code"),t(20,":checked"),n(),t(21," / "),e(22,"code"),t(23,":invalid"),n(),t(24,` CSS state for outline appearance:
+`),n(),e(25,"button",7),t(26,` Outline
+`),n(),e(27,"button",8),t(28,` Outline
+`),n())},dependencies:[l],styles:["[_nghost-%COMP%]{display:flex;gap:1rem;flex-wrap:wrap}"],changeDetection:0});var o=i;export{o as default};
