15adf236bf4932e409404abc74008f03fb226d70
splincode <splincode@users.noreply.github.com>
2026-02-13 13:55:51 +0000

    deploy: 2ddd19af12b1a41ae3feb2e3bbd158d1da772864

--- next/chunk-7O7RAHZG.js
+++
@@ -1,8 +0,0 @@
-import"./chunk-B4AJQJMI.js";var i=`:host {
-    display: flex;
-    gap: 1rem;
-    color: var(--tui-text-action);
-
-    --tui-typography-icon: 'Material Symbols Outlined';
-}
-`;export{i as default};
