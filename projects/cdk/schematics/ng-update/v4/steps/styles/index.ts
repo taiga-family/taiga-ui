6e3586c27a14928eb68d0fdda99ce24ad028f501
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-02-16 11:44:57 +0300

    chore: fix build

--- projects/cdk/schematics/ng-update/v4/steps/styles/index.ts
+++ projects/cdk/schematics/ng-update/v4/steps/styles/index.ts
@@ -1,5 +1,5 @@
 /// <reference lib="es2021" />
-import {getActiveProject, saveActiveProject} from 'ng-morph';
+import {getSourceFiles, saveActiveProject} from 'ng-morph';
 
 import {ALL_STYLE_FILES} from '../../../../constants';
 import {migrateLegacyMixins} from './migrate-legacy-mixins';
@@ -13,49 +13,47 @@ export const TUI_RATING_WARNING =
     '// TODO: (Taiga UI migration): use css to customize rating gap and size. See https://taiga-ui.dev/components/rating#basic';
 
 export function migrateStyles(): void {
-    getActiveProject()
-        ?.getSourceFiles(ALL_STYLE_FILES)
-        .forEach((sourceFile) => {
-            let fullText = sourceFile.getFullText();
+    getSourceFiles(ALL_STYLE_FILES).forEach((sourceFile) => {
+        let fullText = sourceFile.getFullText();
 
-            fullText = fullText
-                // eslint-disable-next-line
-                .replace(/^(.*--tui-rating-size.*)$/gm, `${TUI_RATING_WARNING}\n$1`)
-                // eslint-disable-next-line
-                .replace(/^(.*--tui-rating-gap.*)$/gm, `${TUI_RATING_WARNING}\n$1`)
-                .replaceAll('--tui-link-icon-size', '--tui-icon-size')
-                .replaceAll(
-                    '@taiga-ui/proprietary-core/styles/tinkoff-fonts',
-                    '@taiga-ui/proprietary/styles/tbank-fonts',
-                )
-                .replaceAll(
-                    '@taiga-ui/proprietary-core/styles/theme-tinkoff-2023',
-                    '@taiga-ui/proprietary/styles/tbank-theme',
-                )
-                .replaceAll(
-                    '@taiga-ui/proprietary-core/styles/theme-tinkoff-mobile-2023',
-                    '@taiga-ui/proprietary/styles/tbank-theme-mobile.less',
-                )
-                .replaceAll(
-                    '@taiga-ui/proprietary-core/styles/theme-tinkoff-mobile',
-                    '@taiga-ui/proprietary/styles/tbank-theme-mobile.less',
-                )
-                .replaceAll(
-                    '@taiga-ui/proprietary-core/styles/theme-tinkoff',
-                    '@taiga-ui/proprietary/styles/tbank-theme',
-                );
+        fullText = fullText
+            // eslint-disable-next-line
+            .replace(/^(.*--tui-rating-size.*)$/gm, `${TUI_RATING_WARNING}\n$1`)
+            // eslint-disable-next-line
+            .replace(/^(.*--tui-rating-gap.*)$/gm, `${TUI_RATING_WARNING}\n$1`)
+            .replaceAll('--tui-link-icon-size', '--tui-icon-size')
+            .replaceAll(
+                '@taiga-ui/proprietary-core/styles/tinkoff-fonts',
+                '@taiga-ui/proprietary/styles/tbank-fonts',
+            )
+            .replaceAll(
+                '@taiga-ui/proprietary-core/styles/theme-tinkoff-2023',
+                '@taiga-ui/proprietary/styles/tbank-theme',
+            )
+            .replaceAll(
+                '@taiga-ui/proprietary-core/styles/theme-tinkoff-mobile-2023',
+                '@taiga-ui/proprietary/styles/tbank-theme-mobile.less',
+            )
+            .replaceAll(
+                '@taiga-ui/proprietary-core/styles/theme-tinkoff-mobile',
+                '@taiga-ui/proprietary/styles/tbank-theme-mobile.less',
+            )
+            .replaceAll(
+                '@taiga-ui/proprietary-core/styles/theme-tinkoff',
+                '@taiga-ui/proprietary/styles/tbank-theme',
+            );
 
-            const newFileContent = [
-                migrateSpaceMixins,
-                migrateTextMixins,
-                migrateShadowMixins,
-                migrateLessSCSSConstants,
-                migrateMixins,
-                migrateLegacyMixins,
-            ].reduce((text, migrate) => migrate(text), fullText);
+        const newFileContent = [
+            migrateSpaceMixins,
+            migrateTextMixins,
+            migrateShadowMixins,
+            migrateLessSCSSConstants,
+            migrateMixins,
+            migrateLegacyMixins,
+        ].reduce((text, migrate) => migrate(text), fullText);
 
-            sourceFile.replaceWithText(newFileContent);
-        });
+        sourceFile.replaceWithText(newFileContent);
+    });
 
     saveActiveProject();
 }
