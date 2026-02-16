663cc5585633ced30da58c6ebad354a1f29488f1
nsbarsukov <nsbarsukov@users.noreply.github.com>
2026-02-16 09:21:01 +0000

    deploy: c982dec0541b96c1524dcadcc4e70fa8af597c18

---
+++ next/chunk-VERLWPV4.js
@@ -0,0 +1,15 @@
+import"./chunk-B4AJQJMI.js";var r=`<input
+    tuiSlider
+    type="range"
+    [formControl]="formControl"
+/>
+
+<p>Custom</p>
+
+<input
+    tuiSlider
+    type="range"
+    [formControl]="formControl"
+    [style.--tui-thumb-size.rem]="1"
+/>
+`;export{r as default};
