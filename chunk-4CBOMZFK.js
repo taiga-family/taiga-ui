213281dc50416bdf81348d2656d3cf5307042ba0
nsbarsukov <nikita.s.barsukov@gmail.com>
2026-02-16 09:18:54 +0000

    Update snapshots/demo/next/main to output generated at c982dec

---
+++ chunk-4CBOMZFK.js
@@ -0,0 +1,9 @@
+import"./chunk-B4AJQJMI.js";var t=`<tui-range [(ngModel)]="value" />
+
+<p>Custom</p>
+
+<tui-range
+    [style.--tui-thumb-size.rem]="1"
+    [(ngModel)]="value"
+/>
+`;export{t as default};
