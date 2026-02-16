209f84e54be709e2e9df3313cea2613dc712b6f3
nsbarsukov <nikita.s.barsukov@gmail.com>
2026-02-16 10:00:06 +0000

    Update snapshots/demo/next/main to output generated at d3c0ab1

---
+++ chunk-CV4UF4UC.js
@@ -0,0 +1,15 @@
+import"./chunk-B4AJQJMI.js";var p=`\`\`\`ts
+import {TuiRoot} from '@taiga-ui/core';
+// ..
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    TuiRoot,
+    // ...
+  ],
+  templateUrl: './app.component.html',
+})
+export class App {}
+\`\`\`
+`;export{p as default};
