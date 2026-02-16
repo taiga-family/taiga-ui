af7905d82a8ca801c3079742e30b9136ca3ad165
Alex Inkin <alexander@inkin.ru>
2026-02-16 12:07:42 +0400

    feat(experimental): add `Popout` service (#13247)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/demo/src/pages/customization/portals/index.ts
+++ projects/demo/src/pages/customization/portals/index.ts
@@ -2,42 +2,23 @@ import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
 
+import {TuiPortalsExample1} from './examples/1';
+
 @Component({
-    imports: [TuiDemo],
+    imports: [TuiDemo, TuiPortalsExample1],
     templateUrl: './index.html',
     changeDetection,
 })
-export default class Page extends Array {
+export default class Page {
     protected host = import('./examples/setup/create-host.md');
     protected service = import('./examples/setup/create-service.md');
     protected insert = import('./examples/setup/insert-host.md');
 
-    protected readonly examples = [
-        'Custom portals',
-        'Popout window portals',
-        'Picture in Picture portals',
-    ];
-
-    protected readonly [0] = {
+    protected readonly example1 = {
+        TypeScript: import('./examples/1/index.ts?raw', {with: {loader: 'text'}}),
+        HTML: import('./examples/1/index.html'),
+        LESS: import('./examples/1/index.less'),
         'portal.ts': import('./examples/1/portal.ts?raw', {with: {loader: 'text'}}),
         'service.ts': import('./examples/1/service.ts?raw', {with: {loader: 'text'}}),
-    };
-
-    protected readonly [1] = {
-        'popout.html': import('./examples/2/popout.html'),
-        'popout.ts': import('./examples/2/popout.ts?raw', {with: {loader: 'text'}}),
-        'popout.less': import('./examples/2/popout.less'),
-        'popout.service.ts': import('./examples/2/popout.service.ts?raw', {
-            with: {loader: 'text'},
-        }),
-    };
-
-    protected readonly [2] = {
-        'popout.html': import('./examples/3/popout.html'),
-        'popout.ts': import('./examples/3/popout.ts?raw', {with: {loader: 'text'}}),
-        'popout.less': import('./examples/3/popout.less'),
-        'popout.service.ts': import('./examples/3/popout.service.ts?raw', {
-            with: {loader: 'text'},
-        }),
     };
 }
