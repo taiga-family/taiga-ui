085ee73ad007ea006a21aa8d980e5e610ccb6026
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2026-02-18 13:33:17 +0300

    chore(demo): add missing imports to card large (#13312)

--- projects/demo/src/modules/components/card-large/examples/5/index.ts
+++ projects/demo/src/modules/components/card-large/examples/5/index.ts
@@ -2,20 +2,20 @@ import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiRepeatTimes} from '@taiga-ui/cdk';
-import {TuiAppearance, TuiButton, TuiTitle} from '@taiga-ui/core';
+import {TuiButton, TuiSurface, TuiTitle} from '@taiga-ui/core';
 import {TuiAvatar} from '@taiga-ui/kit';
 import {TuiCardLarge, TuiCell, TuiHeader} from '@taiga-ui/layout';
 
 @Component({
     standalone: true,
     imports: [
-        TuiAppearance,
         TuiAvatar,
         TuiButton,
         TuiCardLarge,
         TuiCell,
         TuiHeader,
         TuiRepeatTimes,
+        TuiSurface,
         TuiTitle,
     ],
     templateUrl: './index.html',
