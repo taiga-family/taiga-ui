ca29850d308874dd1d1193a563148e0779fd46ed
mdlufy <german.panov.2018@mail.ru>
2026-02-18 16:44:45 +0300

    chore(cdk): `.tui-slider-ticks-labels(l) => .tui-slider-ticks-abels()` migration

--- projects/cdk/schematics/ng-update/v5/tests/schematic-migrate-mixins.spec.ts
+++ projects/cdk/schematics/ng-update/v5/tests/schematic-migrate-mixins.spec.ts
@@ -1,17 +1,10 @@
 import {join} from 'node:path';
 
-import {HostTree} from '@angular-devkit/schematics';
-import {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
-import {type TuiSchema} from '@taiga-ui/cdk/schematics/ng-add/schema';
-import {
-    createProject,
-    createSourceFile,
-    resetActiveProject,
-    saveActiveProject,
-    setActiveProject,
-} from 'ng-morph';
+import {resetActiveProject} from 'ng-morph';
 
-const collectionPath = join(__dirname, '../../../migration.json');
+import {runMigration} from '../../../utils/run-migration';
+
+const collection = join(__dirname, '../../../migration.json');
 
 const LESS_BEFORE = `
 @import '@taiga-ui/styles/utils';
@@ -80,44 +73,21 @@ const SCSS_AFTER = `
 `;
 
 describe('ng-update v5: migrate mixins', () => {
-    let host: UnitTestTree;
-    let runner: SchematicTestRunner;
+    async function migrate(styles: string): Promise<string> {
+        const {styles: result} = await runMigration({
+            styles,
+            collection,
+        });
 
-    beforeEach(() => {
-        host = new UnitTestTree(new HostTree());
-        runner = new SchematicTestRunner('schematics', collectionPath);
-
-        setActiveProject(createProject(host));
-
-        createSourceFile('package.json', '{}');
-
-        saveActiveProject();
-    });
+        return result;
+    }
 
     it('should migrate slider-ticks-labels mixin in LESS', async () => {
-        createSourceFile('test/styles.less', LESS_BEFORE);
-        saveActiveProject();
-
-        const tree = await runner.runSchematic(
-            'updateToV5',
-            {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
-            host,
-        );
-
-        expect(tree.readContent('test/styles.less')).toEqual(LESS_AFTER);
+        expect(await migrate(LESS_BEFORE)).toBe(LESS_AFTER);
     });
 
-    it('should migrate slider-ticks-labels mixin in SCSS', async () => {
-        createSourceFile('test/styles.scss', SCSS_BEFORE);
-        saveActiveProject();
-
-        const tree = await runner.runSchematic(
-            'updateToV5',
-            {'skip-logs': process.env['TUI_CI'] === 'true'} as Partial<TuiSchema>,
-            host,
-        );
-
-        expect(tree.readContent('test/styles.scss')).toEqual(SCSS_AFTER);
+    it('should migrate slider-ticks-labels mixin with SCSS @include syntax', async () => {
+        expect(await migrate(SCSS_BEFORE)).toBe(SCSS_AFTER);
     });
 
     afterEach(() => {
