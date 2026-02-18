efe6cd2cff8a4e18835f82733278e344481c5cdf
Vladimir Potekhin <vladimir.potekh@gmail.com>
2026-02-18 12:43:39 +0300

    fix(addon-mobile): `DropdownMobile` fix `_TuiActiveZone matches multiple times` error

--- projects/addon-mobile/directives/dropdown-mobile/dropdown-mobile.directive.ts
+++ projects/addon-mobile/directives/dropdown-mobile/dropdown-mobile.directive.ts
@@ -1,10 +1,12 @@
-import {Directive, inject, Input, signal} from '@angular/core';
+import {computed, Directive, inject, Input} from '@angular/core';
 import {TUI_IS_MOBILE} from '@taiga-ui/cdk/tokens';
 import {tuiIsHTMLElement} from '@taiga-ui/cdk/utils/dom';
-import {TUI_DROPDOWN_COMPONENT} from '@taiga-ui/core/directives/dropdown';
+import {
+    TUI_DROPDOWN_COMPONENT,
+    TuiDropdownDirective,
+} from '@taiga-ui/core/directives/dropdown';
 
 import {TuiDropdownMobileComponent} from './dropdown-mobile.component';
-import {TuiActiveZone} from '@taiga-ui/cdk/directives/active-zone';
 
 @Directive({
     standalone: true,
@@ -18,17 +20,19 @@ import {TuiActiveZone} from '@taiga-ui/cdk/directives/active-zone';
                     : inject(TUI_DROPDOWN_COMPONENT, {skipSelf: true}),
         },
     ],
-    hostDirectives: [{directive: TuiActiveZone, outputs: ['tuiActiveZoneChange']}],
     host: {
-        '[style.visibility]': 'visible()',
+        '[style.visibility]': 'visible() ? "visible" : ""',
         '(mousedown)': 'onMouseDown($event)',
-        '(tuiActiveZoneChange)': 'visible.set($event ? "visible" : "")',
     },
 })
 export class TuiDropdownMobile {
     private readonly isMobile = inject(TUI_IS_MOBILE);
+    private readonly dropdown = inject(TuiDropdownDirective, {
+        optional: true,
+        self: true,
+    });
 
-    protected readonly visible = signal('');
+    protected readonly visible = computed(() => !this.dropdown || this.dropdown.ref());
 
     @Input()
     public tuiDropdownMobile = '';
