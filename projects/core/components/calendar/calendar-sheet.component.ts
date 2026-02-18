c331b66efde0a312ab876d1db3dbc5fabb1da069
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-02-18 17:41:21 +0300

    refactor(core): use signal inputs in `CalendarSheet`

--- projects/core/components/calendar/calendar-sheet.component.ts
+++ projects/core/components/calendar/calendar-sheet.component.ts
@@ -2,10 +2,11 @@ import {AsyncPipe} from '@angular/common';
 import {
     ChangeDetectionStrategy,
     Component,
-    EventEmitter,
     inject,
     Input,
-    Output,
+    input,
+    model,
+    output,
 } from '@angular/core';
 import {toObservable} from '@angular/core/rxjs-interop';
 import {TUI_FALSE_HANDLER} from '@taiga-ui/cdk/constants';
@@ -44,23 +45,16 @@ export class TuiCalendarSheet {
     protected readonly unorderedWeekDays$ = toObservable(inject(TUI_SHORT_WEEK_DAYS));
     protected readonly dayType = inject(TUI_CALENDAR_OPTIONS).dayType;
 
-    @Input()
-    public month: TuiMonth = TuiMonth.currentLocal();
+    public readonly month = input(TuiMonth.currentLocal());
+    public readonly disabledItemHandler =
+        input<TuiBooleanHandler<TuiDay>>(TUI_FALSE_HANDLER);
 
-    @Input()
-    public disabledItemHandler: TuiBooleanHandler<TuiDay> = TUI_FALSE_HANDLER;
+    public readonly markerHandler = input<TuiMarkerHandler | null>(null);
+    public readonly value = input<TuiDay | TuiDayRange | readonly TuiDay[] | null>(null);
+    public readonly showAdjacent = input(true);
+    public readonly hoveredItem = model<TuiDay | null>(null);
 
-    @Input()
-    public markerHandler: TuiMarkerHandler | null = null;
-
-    @Input()
-    public value: TuiDay | TuiDayRange | readonly TuiDay[] | null = null;
-
-    @Input()
-    public hoveredItem: TuiDay | null = null;
-
-    @Input()
-    public showAdjacent = true;
+    public readonly dayClick = output<TuiDay>();
 
     /**
      * @deprecated use static DI options instead
@@ -72,41 +66,12 @@ export class TuiCalendarSheet {
     @Input()
     public single = true;
 
-    @Output()
-    public readonly hoveredItemChange = new EventEmitter<TuiDay | null>();
-
-    @Output()
-    public readonly dayClick = new EventEmitter<TuiDay>();
-
-    /**
-     * @deprecated TODO(v5): delete it. It is used nowhere except unit tests
-     */
-    public itemIsInterval(day: TuiDay): boolean {
-        const {value, hoveredItem} = this;
-
-        if (!(value instanceof TuiDayRange)) {
-            return false;
-        }
-
-        if (!value.isSingleDay) {
-            return value.from.daySameOrBefore(day) && value.to.dayAfter(day);
-        }
-
-        if (hoveredItem === null) {
-            return false;
-        }
-
-        const range = TuiDayRange.sort(value.from, hoveredItem);
-
-        return range.from.daySameOrBefore(day) && range.to.dayAfter(day);
-    }
-
     public onItemHovered(item: TuiDay | false): void {
         this.updateHoveredItem(item || null);
     }
 
     public getItemRange(item: TuiDay): 'active' | 'end' | 'middle' | 'start' | null {
-        const {value, hoveredItem} = this;
+        const value = this.value();
 
         if (!value) {
             return null;
@@ -124,7 +89,7 @@ export class TuiCalendarSheet {
             return value.find((day) => day.daySame(item)) ? 'active' : null;
         }
 
-        const range = this.getRange(value, hoveredItem);
+        const range = this.getRange(value, this.hoveredItem());
 
         if (range.isSingleDay && range.from.daySame(item)) {
             return 'active';
@@ -146,13 +111,15 @@ export class TuiCalendarSheet {
     }
 
     protected get isRangePicking(): boolean {
+        const value = this.value();
+
         return this.computedRangeMode
-            ? this.value instanceof TuiDay
+            ? value instanceof TuiDay
             : /**
                * Only for backward compatibility!
                * TODO(v5): replace with `this.options.rangeMode && this.value instanceof TuiDay`
                */
-              this.value instanceof TuiDayRange && this.value.isSingleDay;
+              value instanceof TuiDayRange && value.isSingleDay;
     }
 
     protected readonly toMarkers = (
@@ -175,11 +142,7 @@ export class TuiCalendarSheet {
     }
 
     protected itemIsUnavailable(item: TuiDay): boolean {
-        return !this.month.monthSame(item);
-    }
-
-    protected onItemClick(item: TuiDay): void {
-        this.dayClick.emit(item);
+        return !this.month().monthSame(item);
     }
 
     private getRange(
@@ -196,11 +159,10 @@ export class TuiCalendarSheet {
     }
 
     private updateHoveredItem(day: TuiDay | null): void {
-        if (tuiNullableSame(this.hoveredItem, day, (a, b) => a.daySame(b))) {
+        if (tuiNullableSame(this.hoveredItem(), day, (a, b) => a.daySame(b))) {
             return;
         }
 
-        this.hoveredItem = day;
-        this.hoveredItemChange.emit(day);
+        this.hoveredItem.set(day);
     }
 }
