b01bebd2e398b9fa6f43ca3c246b35322c0a5d49
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-02-18 11:12:28 +0300

    chore(schematics): migrate styles import inside `angular.json` / `project.json` too

--- projects/cdk/schematics/ng-update/v5/steps/styles/index.ts
+++ projects/cdk/schematics/ng-update/v5/steps/styles/index.ts
@@ -1,9 +1,9 @@
 import {getSourceFiles, saveActiveProject} from 'ng-morph';
 
-import {ALL_STYLE_FILES} from '../../../../constants';
+import {ALL_STYLE_FILES, PROJECT_JSON_FILES} from '../../../../constants';
 
 export function migrateStyles(): void {
-    getSourceFiles(ALL_STYLE_FILES).forEach((sourceFile) => {
+    getSourceFiles([...ALL_STYLE_FILES, ...PROJECT_JSON_FILES]).forEach((sourceFile) => {
         sourceFile.replaceWithText(
             sourceFile
                 .getFullText()
