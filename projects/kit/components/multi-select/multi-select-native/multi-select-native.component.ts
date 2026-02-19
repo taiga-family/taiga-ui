b1619d7c31a61aa9b12b72b8b8696abe6ac5d3dd
waterplea <alexander@inkin.ru>
2026-02-19 19:28:40 +0400

    chore: fix

--- projects/kit/components/multi-select/multi-select-native/multi-select-native.component.ts
+++ projects/kit/components/multi-select/multi-select-native/multi-select-native.component.ts
@@ -30,7 +30,6 @@ import {TuiMultiSelectOption} from '../multi-select-option/multi-select-option.c
     hostDirectives: [TuiInputChipDirective, TuiSelectLike],
     host: {
         multiple: '',
-        '[role]': 'null',
         '[size]': 'mobile ? 1 : 2',
         '(click.stop.zoneless)': '0',
         '(input)': 'onInput()',
