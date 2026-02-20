a49f68615fdfb7c36b5d626c495ccfac74723e75
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2026-02-20 11:43:29 +0300

    fix(addon-mobile): `DropdownMobile` fix visibility of inactive textfield (#13292)

--- projects/addon-mobile/directives/dropdown-mobile/dropdown-mobile.directive.ts
+++ projects/addon-mobile/directives/dropdown-mobile/dropdown-mobile.directive.ts
@@ -1,7 +1,10 @@
 import {Directive, inject} from '@angular/core';
 import {WA_IS_MOBILE} from '@ng-web-apis/platform';
 import {tuiIsHTMLElement} from '@taiga-ui/cdk/utils/dom';
-import {TUI_DROPDOWN_COMPONENT} from '@taiga-ui/core/portals/dropdown';
+import {
+    TUI_DROPDOWN_COMPONENT,
+    TuiDropdownDirective,
+} from '@taiga-ui/core/portals/dropdown';
 
 import {TuiDropdownMobileComponent} from './dropdown-mobile.component';
 
@@ -17,12 +20,13 @@ import {TuiDropdownMobileComponent} from './dropdown-mobile.component';
         },
     ],
     host: {
-        '[style.visibility]': '"visible"',
+        '[style.visibility]': 'dropdown.ref() ? "visible" : ""',
         '(mousedown)': 'onMouseDown($event)',
     },
 })
 export class TuiDropdownMobile {
     private readonly isMobile = inject(WA_IS_MOBILE);
+    protected readonly dropdown = inject(TuiDropdownDirective);
 
     protected onMouseDown(event: MouseEvent): void {
         if (
