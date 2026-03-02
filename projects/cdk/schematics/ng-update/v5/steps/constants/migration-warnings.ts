f4f6ec91b562f93a29ef391c2a2c86cfaa759509
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2026-03-02 16:47:54 +0300

    chore: migrate some identifiers (#13366)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/cdk/schematics/ng-update/v5/steps/constants/migration-warnings.ts
+++ projects/cdk/schematics/ng-update/v5/steps/constants/migration-warnings.ts
@@ -13,4 +13,16 @@ export const MIGRATION_WARNINGS: MigrationWarning[] = [
         message:
             'All values inside TuiDateMode are renamed: DMY -> dd/mm/yyyy, MDY -> mm/dd/yyyy, YMD -> yyyy/mm/dd',
     },
+    {
+        name: 'TuiScrollService',
+        moduleSpecifier: '@taiga-ui/cdk',
+        message:
+            'TuiScrollService has been removed because smooth scroll is natively supported by all modern browsers. Use native scrolling APIs (window.scrollTo, Element.scrollTo, Element.scrollIntoView) instead',
+    },
+    {
+        name: 'TuiFormatDatePipe',
+        moduleSpecifier: '@taiga-ui/core',
+        message:
+            'TuiFormatDatePipe has been removed. Use Angular built-in Date pipe instead. See https://angular.dev/api/common/DatePipe',
+    },
 ];
---
+++ projects/cdk/schematics/ng-update/v5/tests/schematic-migrate-experimental-hint.spec.ts
@@ -0,0 +1,38 @@
+import {join} from 'node:path';
+
+import {resetActiveProject} from 'ng-morph';
+
+import {runMigration} from '../../../utils/run-migration';
+
+const collection = join(__dirname, '../../../migration.json');
+
+describe('ng-update experimental hint provider', () => {
+    async function migrate(component: string): Promise<string> {
+        const {component: result} = await runMigration({
+            collection,
+            component,
+        });
+
+        return result;
+    }
+
+    it('removes tuiProvideExperimentalHint() from providers array and import', async () => {
+        const result = await migrate(`
+            import {ApplicationConfig} from '@angular/core';
+            import {tuiProvideExperimentalHint} from '@taiga-ui/experimental';
+
+            export const appConfig: ApplicationConfig = {
+                providers: [tuiProvideExperimentalHint()],
+            };
+        `);
+
+        expect(result).toEqual(`
+            import {ApplicationConfig} from '@angular/core';
+            export const appConfig: ApplicationConfig = {
+                providers: [],
+            };
+        `);
+    });
+
+    afterEach(() => resetActiveProject());
+});
