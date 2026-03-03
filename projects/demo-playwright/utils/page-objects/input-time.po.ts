6a9fe1b573cda81f4342965a06fd0a7297639024
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-03-03 14:22:49 +0300

    chore: refactor PO for PW tests

--- projects/demo-playwright/utils/page-objects/input-time.po.ts
+++ projects/demo-playwright/utils/page-objects/input-time.po.ts
@@ -1,32 +1,5 @@
-import {expect, type Locator} from '@playwright/test';
-
 import {TuiTextfieldWithDataListPO} from './textfield-with-data-list.po';
 
 export class TuiInputTimePO extends TuiTextfieldWithDataListPO {
     public readonly nativePicker = this.host.locator('input[type="time"]');
-
-    public async clickOnIcon(
-        options: Parameters<Locator['click']>[0] = {},
-    ): Promise<void> {
-        const box = (await this.host.boundingBox())!;
-        const padding = await this.host
-            .evaluate((el) =>
-                globalThis.getComputedStyle(el).getPropertyValue('padding-right'),
-            )
-            .then(parseInt);
-        const iconWidth = await this.host
-            .evaluate((el) =>
-                globalThis.getComputedStyle(el, '::after').getPropertyValue('width'),
-            )
-            .then(parseInt);
-
-        expect(box).not.toBeFalsy();
-        expect(padding).not.toBeFalsy();
-        expect(iconWidth).not.toBeFalsy();
-
-        await this.host.click({
-            ...options,
-            position: {x: box.width - padding - iconWidth, y: box.height / 2},
-        });
-    }
 }
---
+++ projects/demo-playwright/utils/page-objects/textfield.po.ts
@@ -0,0 +1,33 @@
+import {expect, type Locator} from '@playwright/test';
+
+export class TuiTextfieldPO {
+    public readonly textfield: Locator = this.host.locator('[tuiInput]');
+    public readonly cleaner = this.host.locator('[tuiButtonX]');
+
+    constructor(protected readonly host: Locator) {}
+
+    public async clickOnIcon(
+        options: Parameters<Locator['click']>[0] = {},
+    ): Promise<void> {
+        const box = (await this.host.boundingBox())!;
+        const padding = await this.host
+            .evaluate((el) =>
+                globalThis.getComputedStyle(el).getPropertyValue('padding-right'),
+            )
+            .then(parseInt);
+        const iconWidth = await this.host
+            .evaluate((el) =>
+                globalThis.getComputedStyle(el, '::after').getPropertyValue('width'),
+            )
+            .then(parseInt);
+
+        expect(box).not.toBeFalsy();
+        expect(padding).not.toBeFalsy();
+        expect(iconWidth).not.toBeFalsy();
+
+        await this.host.click({
+            ...options,
+            position: {x: box.width - padding - iconWidth, y: box.height / 2},
+        });
+    }
+}
