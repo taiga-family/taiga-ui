358a97b99fe018f24ac9a6ff6d5462bf2089bbd4
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-03-03 14:05:48 +0300

    fix(kit): `Input{Date,DateTime,Month,Time}` have broken native pickers

--- projects/demo-playwright/utils/page-objects/input-date.po.ts
+++ projects/demo-playwright/utils/page-objects/input-date.po.ts
@@ -3,6 +3,7 @@ import {type Locator} from '@playwright/test';
 export class TuiInputDatePO {
     public readonly textfield: Locator = this.host.getByRole('combobox');
     public readonly calendar: Locator = this.host.page().locator('tui-calendar');
+    public readonly nativePicker = this.host.locator('input[type="date"]');
 
     constructor(public readonly host: Locator) {}
 }
