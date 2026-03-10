9bfe267396d8e353ac652d87e7011a79c491eb83
waterplea <waterplea@users.noreply.github.com>
2026-03-10 10:32:34 +0000

    deploy: 0bf2dcf54446f663d944d28df89e986cef795535

---
+++ next/chunk-VP4AKROC.js
@@ -0,0 +1,10 @@
+import"./chunk-HU6DUUP4.js";var e=`:host {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+[tuiCell] {
+    background: var(--tui-background-neutral-1);
+}
+`;export{e as default};
