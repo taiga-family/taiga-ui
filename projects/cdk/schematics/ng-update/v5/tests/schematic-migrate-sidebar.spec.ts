4450f0de93789a0f0ba950ba9e0f5419bb7a0afb
mdlufy <german.panov.2018@mail.ru>
2026-03-13 18:18:07 +0300

    chore(cdk): remove expect

--- projects/cdk/schematics/ng-update/v5/tests/schematic-migrate-sidebar.spec.ts
+++ projects/cdk/schematics/ng-update/v5/tests/schematic-migrate-sidebar.spec.ts
@@ -43,129 +43,65 @@ describe('ng-update sidebar to drawer', () => {
 
     it(
         'migrates simple *tuiSidebar directive without extra comments',
-        migrate({template: '<div *tuiSidebar="open">Content</div>'}, ({template}) => {
-            expect(template).toEqual('<tui-drawer *tuiPopup="open">Content</tui-drawer>');
-        }),
+        migrate({template: '<div *tuiSidebar="open">Content</div>'}),
     );
 
     it(
         "migrates *tuiSidebar with direction: 'right' without extra comments",
-        migrate(
-            {template: '<div *tuiSidebar="open; direction: \'right\'">Content</div>'},
-            ({template}) => {
-                expect(template).toEqual(
-                    '<tui-drawer *tuiPopup="open" direction="end">Content</tui-drawer>',
-                );
-            },
-        ),
+        migrate({
+            template: '<div *tuiSidebar="open; direction: \'right\'">Content</div>',
+        }),
     );
 
     it(
         "migrates *tuiSidebar with direction: 'left' without extra comments",
-        migrate(
-            {template: '<div *tuiSidebar="open; direction: \'left\'">Content</div>'},
-            ({template}) => {
-                expect(template).toEqual(
-                    '<tui-drawer *tuiPopup="open" direction="start">Content</tui-drawer>',
-                );
-            },
-        ),
+        migrate({template: '<div *tuiSidebar="open; direction: \'left\'">Content</div>'}),
     );
 
     it(
         'migrates *tuiSidebar with dynamic direction expression and adds TODO comment',
-        migrate(
-            {template: '<div *tuiSidebar="open; direction: myDir">Content</div>'},
-            ({template}) => {
-                expect(template).toEqual(
-                    '<!-- TODO: (Taiga UI migration) Update direction variable values: left->start, right->end -->\n' +
-                        '<tui-drawer *tuiPopup="open" [direction]="myDir">Content</tui-drawer>',
-                );
-            },
-        ),
+        migrate({template: '<div *tuiSidebar="open; direction: myDir">Content</div>'}),
     );
 
     it(
         'migrates *tuiSidebar with autoWidth and adds TODO comment',
-        migrate(
-            {template: '<div *tuiSidebar="open; autoWidth: true">Content</div>'},
-            ({template}) => {
-                expect(template).toEqual(
-                    '<!-- TODO: (Taiga UI migration) tuiSidebarAutoWidth has no equivalent in TuiDrawer, adjust layout manually -->\n' +
-                        '<tui-drawer *tuiPopup="open">Content</tui-drawer>',
-                );
-            },
-        ),
+        migrate({template: '<div *tuiSidebar="open; autoWidth: true">Content</div>'}),
     );
 
     it(
         "migrates *tuiSidebar with direction: 'right' and autoWidth",
-        migrate(
-            {
-                template:
-                    '<div *tuiSidebar="open; direction: \'right\'; autoWidth: true">Content</div>',
-            },
-            ({template}) => {
-                expect(template).toEqual(
-                    '<!-- TODO: (Taiga UI migration) tuiSidebarAutoWidth has no equivalent in TuiDrawer, adjust layout manually -->\n' +
-                        '<tui-drawer *tuiPopup="open" direction="end">Content</tui-drawer>',
-                );
-            },
-        ),
+        migrate({
+            template:
+                '<div *tuiSidebar="open; direction: \'right\'; autoWidth: true">Content</div>',
+        }),
     );
 
     it(
         'preserves other attributes on the element',
-        migrate(
-            {
-                template:
-                    '<div class="my-class" *tuiSidebar="open" (click)="close()">Content</div>',
-            },
-            ({template}) => {
-                expect(template).toEqual(
-                    '<tui-drawer *tuiPopup="open" class="my-class" (click)="close()">Content</tui-drawer>',
-                );
-            },
-        ),
+        migrate({
+            template:
+                '<div class="my-class" *tuiSidebar="open" (click)="close()">Content</div>',
+        }),
     );
 
     it(
         'migrates multiline content inside *tuiSidebar',
-        migrate(
-            {
-                template: [
-                    '<div *tuiSidebar="open">',
-                    '    <header>Header</header>',
-                    '    <p>Content</p>',
-                    '</div>',
-                ].join('\n'),
-            },
-            ({template}) => {
-                expect(template).toEqual(
-                    [
-                        '<tui-drawer *tuiPopup="open">',
-                        '    <header>Header</header>',
-                        '    <p>Content</p>',
-                        '</tui-drawer>',
-                    ].join('\n'),
-                );
-            },
-        ),
+        migrate({
+            template: [
+                '<div *tuiSidebar="open">',
+                '    <header>Header</header>',
+                '    <p>Content</p>',
+                '</div>',
+            ].join('\n'),
+        }),
     );
 
     it(
         'migrates ng-template with *tuiSidebar',
-        migrate(
-            {
-                template:
-                    '<ng-template *tuiSidebar="open; direction: \'right\'"><span>Content</span></ng-template>',
-            },
-            ({template}) => {
-                expect(template).toEqual(
-                    '<tui-drawer *tuiPopup="open" direction="end"><span>Content</span></tui-drawer>',
-                );
-            },
-        ),
+        migrate({
+            template:
+                '<ng-template *tuiSidebar="open; direction: \'right\'"><span>Content</span></ng-template>',
+        }),
     );
 
     afterEach(() => resetActiveProject());
