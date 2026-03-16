cd283ef96d4794278fce20e8fd99fc148db6dfe2
l.golovina <l.golovina@tinkoff.ru>
2026-03-16 16:44:50 +0300

    fix(cdk): remove constants in tests

--- projects/cdk/schematics/ng-update/v5/tests/schematic-migrate-present-change.spec.ts
+++ projects/cdk/schematics/ng-update/v5/tests/schematic-migrate-present-change.spec.ts
@@ -3,22 +3,6 @@ import {join} from 'node:path';
 import {createMigration} from '@taiga-ui/cdk/schematics/utils/run-migration';
 import {resetActiveProject} from 'ng-morph';
 
-const COMPONENT = `import { Component } from "@angular/core";
-
-@Component({
-    standalone: true,
-    template: \`<span (tuiPresentChange)="onChangeEvent($event)">I am a changeable component</span>\`,
-    host: {
-      '[style.--tui-background-accent-2-pressed]': 'red',
-      '(tuiPresentChange)': 'onChangeEvent($event)',
-    }
-})
-export class Test {
-}`;
-
-const TEMPLATE =
-    '<span (tuiPresentChange)="onChangeEvent($event)">I am a changeable component</span>';
-
 describe('ng-update tuiPresentChange attribute', () => {
     const migrate = createMigration({
         collection: join(__dirname, '../../../migration.json'),
@@ -26,12 +10,29 @@ describe('ng-update tuiPresentChange attribute', () => {
 
     it(
         'should replace tuiPresentChange attribute to tuiPresent in ts files',
-        migrate({component: COMPONENT}),
+        migrate({
+            component: `
+            import { Component } from "@angular/core";
+
+            @Component({
+                standalone: true,
+                template: \`<span (tuiPresentChange)="onChangeEvent($event)">I am a changeable component</span>\`,
+                host: {
+                  '[style.--tui-background-accent-2-pressed]': 'red',
+                  '(tuiPresentChange)': 'onChangeEvent($event)',
+                }
+            })
+            export class Test {
+            }`,
+        }),
     );
 
     it(
         'should replace tuiPresentChange attribute to tuiPresent in html files',
-        migrate({template: TEMPLATE}),
+        migrate({
+            template:
+                '<span (tuiPresentChange)="onChangeEvent($event)">I am a changeable component</span>',
+        }),
     );
 
     afterEach(() => resetActiveProject());
