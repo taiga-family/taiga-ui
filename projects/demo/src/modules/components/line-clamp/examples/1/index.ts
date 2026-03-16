c6a4da1866dea504dd92e29f180fce3162ee72e3
splincode <omaxphp@yandex.ru>
2026-03-16 15:35:31 +0300

    fix(kit): update `tui-line-clamp` properly when toggling between empty and non-empty content

--- projects/demo/src/modules/components/line-clamp/examples/1/index.ts
+++ projects/demo/src/modules/components/line-clamp/examples/1/index.ts
@@ -1,15 +1,15 @@
 import {AsyncPipe, NgIf} from '@angular/common';
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {TUI_IS_E2E, tuiWatch} from '@taiga-ui/cdk';
-import {TuiNotification} from '@taiga-ui/core';
+import {TuiButton, TuiNotification} from '@taiga-ui/core';
 import {TuiChip, TuiLineClamp} from '@taiga-ui/kit';
 import {map, timer} from 'rxjs';
 
 @Component({
     standalone: true,
-    imports: [AsyncPipe, NgIf, TuiChip, TuiLineClamp, TuiNotification],
+    imports: [AsyncPipe, NgIf, TuiButton, TuiChip, TuiLineClamp, TuiNotification],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation,
@@ -18,8 +18,16 @@ import {map, timer} from 'rxjs';
 export default class Example {
     private readonly isE2E = inject(TUI_IS_E2E);
 
+    protected readonly value = signal('');
+
     protected value$ = timer(this.isE2E ? 0 : 4000).pipe(
         map(() => `${'async fake value, '.repeat(10)}end!`),
         tuiWatch(),
     );
+
+    public update(): void {
+        this.value.set(
+            'Daenerys of the House Targaryen, the First of Her Name, The Unburnt, Queen of the Andals, the Rhoynar and the First Men, Queen of Meereen, Khaleesi of the Great Grass Sea, Protector of the Realm, Lady Regent of the Seven Kingdoms, Breaker of Chains and Mother of Dragons',
+        );
+    }
 }
