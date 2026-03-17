071704fff94b26a3fcc726ddeba8f04a79d9deb4
German Panov <german.panov.2018@mail.ru>
2026-03-17 15:33:03 +0300

    chore(cdk): add TuiInputPassword migration for v5 (#13514)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

---
+++ projects/cdk/schematics/ng-update/v5/tests/schematic-migrate-input-password.spec.ts
@@ -0,0 +1,67 @@
+import {join} from 'node:path';
+
+import {resetActiveProject} from 'ng-morph';
+
+import {createMigration} from '../../../utils/run-migration';
+
+const migrate = createMigration({collection: join(__dirname, '../../../migration.json')});
+
+describe('ng-update', () => {
+    it(
+        'migrate TuiInputPasswordModule to TuiPassword',
+        migrate({
+            component: `
+                import {TuiInputPasswordModule} from '@taiga-ui/legacy';
+
+                @NgModule({
+                  imports: [
+                    // ...
+                    TuiInputPasswordModule,
+                  ],
+                  // ...
+                })
+                export class MyModule {}
+
+                @Component({
+                  standalone: true,
+                  imports: [
+                    // ...
+                    TuiInputPasswordModule,
+                  ],
+                  templateUrl: './test.html',
+                  // ...
+                })
+                export class MyComponent {}
+            `,
+            template: `
+                <tui-input-password>
+                </tui-input-password>
+
+                <tui-input-password
+                    [formControl]="control"
+                >
+                    Enter password
+                </tui-input-password>
+
+                <tui-input-password
+                    formControlName="password"
+                    [tuiTextfieldCleaner]="true"
+                >
+                    Password
+                    <input
+                        placeholder="••••••••"
+                        tuiTextfield
+                    />
+                </tui-input-password>
+
+                <tui-input-password
+                    [(ngModel)]="value"
+                >
+                    Confirm password
+                </tui-input-password>
+            `,
+        }),
+    );
+
+    afterEach(() => resetActiveProject());
+});
