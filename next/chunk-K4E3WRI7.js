930c7baa54ac948f32df991708a5c706e6a616d8
waterplea <waterplea@users.noreply.github.com>
2026-03-19 05:32:03 +0000

    deploy: 8401828e1434d06e6559896c7a72612a4203d7a3

---
+++ next/chunk-K4E3WRI7.js
@@ -0,0 +1,27 @@
+import"./chunk-HU6DUUP4.js";var l=`<tui-textfield>
+    <label
+        tuiFade
+        tuiLabel
+    >
+        Very long label that you must display without truncating
+    </label>
+    <input
+        tuiFade
+        tuiInput
+        [(ngModel)]="value"
+    />
+</tui-textfield>
+
+<tui-textfield multi>
+    <label
+        tuiFade
+        tuiLabel
+    >
+        Very long label that you must display without truncating
+    </label>
+    <input
+        tuiInputChip
+        [(ngModel)]="chips"
+    />
+</tui-textfield>
+`;export{l as default};
---
+++ next/chunk-ZUNCCL6S.js
@@ -0,0 +1,16 @@
+import"./chunk-HU6DUUP4.js";var t=`:host {
+    display: flex;
+    inline-size: 17rem;
+    flex-direction: column;
+    gap: 1rem;
+
+    tui-textfield[data-focus='true'] {
+        align-items: center;
+
+        [tuiLabel] {
+            transition: none;
+            white-space: normal;
+        }
+    }
+}
+`;export{t as default};
