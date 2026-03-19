bd976b5d2ad46f281b3db63c2a3ab389f148037e
splincode <splincode@users.noreply.github.com>
2026-03-19 14:25:53 +0000

    deploy: 44f1cd9ea54ba65539d2660f32ab8620a532b549

---
+++ next/chunk-JRWRIZUQ.js
@@ -0,0 +1,31 @@
+import"./chunk-WA6TTHHY.js";import"./chunk-DWHCXJ2B.js";import"./chunk-DNEH2FU7.js";import"./chunk-I7Q4W75M.js";import"./chunk-NUAU6ERC.js";import"./chunk-77R64QTL.js";import"./chunk-M4VRVJOE.js";import"./chunk-YS5IZCFK.js";import"./chunk-DXHVKB3I.js";import{r as s}from"./chunk-6FVBJV3Z.js";import"./chunk-2BOQTZG4.js";import"./chunk-7SDV2BC3.js";import"./chunk-FUUL6VJ7.js";import"./chunk-H5JSFGKJ.js";import{S as c}from"./chunk-YEJL3QEO.js";import"./chunk-HBKPVBCF.js";import"./chunk-5RDBFI3P.js";import"./chunk-QNRU3ESN.js";import"./chunk-L5TNPRV2.js";import"./chunk-7IQTVJGU.js";import"./chunk-657VISJO.js";import"./chunk-AUQMQA6B.js";import"./chunk-OFTWQ2VV.js";import"./chunk-S5GSHNE3.js";import"./chunk-7L62O2ER.js";import"./chunk-Y32PXR66.js";import"./chunk-D6XI4QZC.js";import"./chunk-555KG32Z.js";import{Tb as n,Wc as i,rc as r}from"./chunk-UM2Z6NQD.js";import"./chunk-HU6DUUP4.js";var a=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path
+        vector-effect="non-scaling-stroke"
+        d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"
+    />
+</svg>
+`;var l=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path vector-effect="non-scaling-stroke" d="m21 21-4.34-4.34" />
+    <circle vector-effect="non-scaling-stroke" cx="11" cy="11" r="8" />
+</svg>
+`;var e=class e{};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=n({type:e,selectors:[["ng-component"]],features:[i([c({"@tui.heart":a,"@tui.search":l})])],decls:2,vars:0,consts:[["icon","@tui.heart"],["icon","@tui.search"]],template:function(t,u){t&1&&r(0,"tui-icon",0)(1,"tui-icon",1)},dependencies:[s],styles:["[_nghost-%COMP%]{display:flex;gap:1rem;color:var(--tui-text-action)}"],changeDetection:0});var o=e;export{o as default};
