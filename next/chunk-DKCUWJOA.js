047cd41297a166622a1c35e4102cc666615ad82f
nsbarsukov <nsbarsukov@users.noreply.github.com>
2026-03-19 10:11:00 +0000

    deploy: 477d53794d264f0b79a96c8a145b256b56527673

---
+++ next/chunk-DKCUWJOA.js
@@ -0,0 +1,49 @@
+import"./chunk-HU6DUUP4.js";var i=`import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {type TuiPaymentSystem, TuiThumbnailCard} from '@taiga-ui/addon-commerce';
+import {type TuiStringHandler} from '@taiga-ui/cdk';
+import {type TuiFilterByInputOptions, TuiFilterByInputPipe} from '@taiga-ui/core';
+import {TuiChevron, TuiComboBox, TuiDataListWrapper} from '@taiga-ui/kit';
+
+interface Card {
+    name: string;
+    number: string;
+    paymentSystem: TuiPaymentSystem;
+}
+
+@Component({
+    imports: [
+        FormsModule,
+        TuiChevron,
+        TuiComboBox,
+        TuiDataListWrapper,
+        TuiFilterByInputPipe,
+        TuiThumbnailCard,
+    ],
+    templateUrl: './index.html',
+    styleUrl: './index.less',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected cards: Card[] = [
+        {name: 'Bitcoin', number: '5555555555554444', paymentSystem: 'mastercard'},
+        {name: 'Salary', number: '4242424242424242', paymentSystem: 'visa'},
+        {name: 'Charity', number: '2201382000000013', paymentSystem: 'mir'},
+        {name: 'Subscriptions', number: '6200000000000005', paymentSystem: 'unionpay'},
+    ];
+
+    protected value: Card | null = null;
+
+    protected readonly filter: TuiFilterByInputOptions<Card>['filter'] = (items, value) =>
+        items.filter(
+            (item) =>
+                item.name.toLowerCase().includes(value.toLowerCase()) ||
+                item.number.includes(value),
+        );
+
+    protected readonly stringify: TuiStringHandler<Card> = (x) => x.number;
+}
+`;export{i as default};
---
+++ next/chunk-U2HEC2NZ.js
@@ -0,0 +1,39 @@
+import"./chunk-HU6DUUP4.js";var e=`<form [formGroup]="form">
+    <tui-textfield
+        tuiChevron
+        [tuiTextfieldCleaner]="false"
+    >
+        <label tuiLabel>Search by last name</label>
+
+        <input
+            formControlName="user"
+            tuiComboBox
+        />
+
+        <tui-data-list-wrapper
+            *tuiDropdown
+            [items]="items | tuiFilterByInput: bySurname"
+        />
+    </tui-textfield>
+
+    <tui-textfield
+        tuiChevron
+        class="tui-space_top-5"
+        [stringify]="stringify"
+        [tuiTextfieldCleaner]="false"
+    >
+        <label tuiLabel>With ids</label>
+
+        <input
+            formControlName="user2"
+            tuiComboBox
+        />
+
+        <tui-data-list-wrapper
+            *tuiDropdown
+            [itemContent]="stringify | tuiStringifyContent"
+            [items]="users | tuiFilterByInput: byId"
+        />
+    </tui-textfield>
+</form>
+`;export{e as default};
