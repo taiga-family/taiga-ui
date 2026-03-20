3e46e0880dbd1d931841f28cc5351e41c1292d7d
splincode <omaxphp@yandex.ru>
2026-03-20 15:50:52 +0000

    Update snapshots/demo/next/main to output generated at 2db505c

---
+++ chunk-QOMIZLWU.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-7G7VHKRA.js";import"./chunk-YFTUPMNC.js";import"./chunk-HU6DUUP4.js";export{a as pages};
---
+++ chunk-SNZUEUVM.js
@@ -0,0 +1,6 @@
+import"./chunk-HU6DUUP4.js";var d=`.demo {
+    background: #87ceeb;
+    overflow: hidden;
+    resize: horizontal;
+}
+`;export{d as default};
