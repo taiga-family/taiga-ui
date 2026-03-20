74f884bba04a6f5cc17200d82d3220211551caeb
Максим Иванов <splincodewd@yandex.ru>
2026-03-20 17:23:55 +0300

    Update projects/demo-playwright/tests/core/textfield/textfield-filler-rtl.pw.spec.ts
    Co-authored-by: Nikita Barsukov <nikita.s.barsukov@gmail.com>

--- projects/demo-playwright/tests/core/textfield/textfield-filler-rtl.pw.spec.ts
+++ projects/demo-playwright/tests/core/textfield/textfield-filler-rtl.pw.spec.ts
@@ -10,7 +10,7 @@ test.describe('Textfield filler in RTL mode', () => {
         const input = example.locator('input[tuiTextfield]');
 
         await input.focus();
-        await input.pressSequentially('123');
+        await input.pressSequentially('12:3');
         await expect.soft(example).toHaveScreenshot('input-filler-ltr.png');
         await input.clear();
         await example.evaluate((node) => node.setAttribute('dir', 'rtl'));
