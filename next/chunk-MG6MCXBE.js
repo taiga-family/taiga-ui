f10814be18534dd1aee5de08ce1b6eb57cc78e70
waterplea <waterplea@users.noreply.github.com>
2026-03-21 13:39:25 +0000

    deploy: d71efc9802ec9fe86d862f685d68021eb46eebb6

---
+++ next/chunk-MG6MCXBE.js
@@ -0,0 +1,18 @@
+import"./chunk-HU6DUUP4.js";var i=`:host {
+    display: flex;
+    gap: 1rem;
+}
+
+tui-icon {
+    background: var(--tui-border-normal);
+    color: var(--tui-status-warning);
+
+    &:first-child {
+        color: var(--tui-status-positive);
+    }
+
+    &:last-child {
+        color: var(--tui-status-negative);
+    }
+}
+`;export{i as default};
---
+++ next/chunk-XGDWSFQW.js
@@ -0,0 +1,13 @@
+import"./chunk-HU6DUUP4.js";var o=`<tui-icon
+    background="@tui.wifi"
+    icon="@tui.wifi"
+/>
+<tui-icon
+    background="@tui.wifi"
+    icon="@tui.wifi-high"
+/>
+<tui-icon
+    background="@tui.wifi"
+    icon="@tui.wifi-low"
+/>
+`;export{o as default};
