7c8ed16073803625c96b814dc6d60eff292a80a1
Максим Иванов <splincodewd@yandex.ru>
2026-03-23 23:26:29 +0900

    refactor(addon-doc): drop optional (#13564)

--- projects/addon-doc/tokens/doc-icons.ts
+++ projects/addon-doc/tokens/doc-icons.ts
@@ -12,11 +12,8 @@ export interface TuiDocIcons {
     readonly check: string;
     readonly link: string;
     readonly languages: string;
-    /**
-     * TODO: drop optional in v5
-     */
-    readonly shrink?: string;
-    readonly expand?: string;
+    readonly shrink: string;
+    readonly expand: string;
     readonly ltr: {
         readonly preview: string;
         readonly direction: string;
