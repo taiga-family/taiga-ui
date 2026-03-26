e9e31b64a91fbe2fefa1c853b6d7a7b257ceafe9
splincode <splincode@users.noreply.github.com>
2026-03-26 09:06:45 +0000

    deploy: 8eb6c825befde3c113df2bd5bcfb1031ef651b34

---
+++ next/chunk-E33WUT5S.js
@@ -0,0 +1,45 @@
+import"./chunk-HU6DUUP4.js";var e=`<label tuiInputFiles>
+    <input
+        tuiInputFiles
+        [formControl]="control"
+    />
+    <ng-template let-dragged>
+        @if (dragged) {
+            <div
+                appearance="secondary"
+                size="l"
+                tuiAvatar="@tui.droplet"
+            ></div>
+            <div>
+                Drop it like it's hot!
+                <br />
+                <br />
+            </div>
+        } @else {
+            <div
+                appearance="secondary"
+                size="l"
+                tuiAvatar="@tui.cloud-upload"
+            ></div>
+            <div>
+                Drag and drop file here or
+                <a tuiLink>click to upload</a>
+            </div>
+            <div class="types">PNG, JPG, JPEG, PNG, etc</div>
+        }
+    </ng-template>
+</label>
+
+<tui-files class="tui-space_top-4">
+    <tui-file
+        [file]="file"
+        [leftContent]="content"
+    >
+        <span class="tui-text_body-s-2">file is on checking</span>
+    </tui-file>
+</tui-files>
+
+<ng-template #content>
+    <tui-icon icon="@tui.clock" />
+</ng-template>
+`;export{e as default};
