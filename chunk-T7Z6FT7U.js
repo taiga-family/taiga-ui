9624a8a6fe918d3f5e6b985ab5b00a5673351dca
waterplea <alexander@inkin.ru>
2026-03-31 08:25:53 +0000

    Update snapshots/demo/next/main to output generated at bbed2b1

---
+++ chunk-T7Z6FT7U.js
@@ -0,0 +1,26 @@
+import"./chunk-HU6DUUP4.js";var t=`import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiButton, TuiCell, TuiIcon, TuiTitle} from '@taiga-ui/core';
+import {TuiAvatar, TuiProgressBar, TuiTooltip} from '@taiga-ui/kit';
+import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+
+@Component({
+    imports: [
+        TuiAvatar,
+        TuiButton,
+        TuiCardLarge,
+        TuiCell,
+        TuiHeader,
+        TuiIcon,
+        TuiProgressBar,
+        TuiTitle,
+        TuiTooltip,
+    ],
+    templateUrl: './index.html',
+    styleUrl: './index.less',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {}
+`;export{t as default};
