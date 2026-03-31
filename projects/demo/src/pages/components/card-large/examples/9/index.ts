1f97be1d13c7d98b72b0e72705d3d290724320b2
Alex Inkin <alexander@inkin.ru>
2026-03-31 12:13:42 +0400

    fix(layout): `Card` fix styles (#13650)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/demo/src/pages/components/card-large/examples/9/index.ts
+++ projects/demo/src/pages/components/card-large/examples/9/index.ts
@@ -1,12 +1,22 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiButton, TuiCell, TuiTitle} from '@taiga-ui/core';
-import {TuiAvatar} from '@taiga-ui/kit';
+import {TuiButton, TuiCell, TuiIcon, TuiTitle} from '@taiga-ui/core';
+import {TuiAvatar, TuiProgressBar, TuiTooltip} from '@taiga-ui/kit';
 import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
 
 @Component({
-    imports: [TuiAvatar, TuiButton, TuiCardLarge, TuiCell, TuiHeader, TuiTitle],
+    imports: [
+        TuiAvatar,
+        TuiButton,
+        TuiCardLarge,
+        TuiCell,
+        TuiHeader,
+        TuiIcon,
+        TuiProgressBar,
+        TuiTitle,
+        TuiTooltip,
+    ],
     templateUrl: './index.html',
     styleUrl: './index.less',
     encapsulation,
