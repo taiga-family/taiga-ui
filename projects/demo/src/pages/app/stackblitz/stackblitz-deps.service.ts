80b036b754cb650e13628c4ce90a8c361ce695b9
Максим Иванов <splincodewd@yandex.ru>
2026-04-03 19:17:29 +0900

    chore: cleanup `stackblitz-deps.service.ts` (#13718)

--- projects/demo/src/pages/app/stackblitz/stackblitz-deps.service.ts
+++ projects/demo/src/pages/app/stackblitz/stackblitz-deps.service.ts
@@ -62,9 +62,8 @@ export class StackblitzDepsService {
 
         return {
             '@taiga-ui/polymorpheus': cdkDeps['@taiga-ui/polymorpheus'],
-            '@taiga-ui/design-tokens': await import('@taiga-ui/styles/package.json').then(
-                (x) => x.peerDependencies['@taiga-ui/design-tokens'],
-            ),
+            '@taiga-ui/design-tokens': (await import('@taiga-ui/styles/package.json'))
+                .peerDependencies['@taiga-ui/design-tokens'],
             '@ng-web-apis/common': cdkDeps['@ng-web-apis/common'],
             '@taiga-ui/event-plugins': cdkDeps['@taiga-ui/event-plugins'],
             '@taiga-ui/font-watcher': cdkDeps['@taiga-ui/font-watcher'],
