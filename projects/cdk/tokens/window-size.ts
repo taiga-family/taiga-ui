b914faf0c00fd6b6988995846da9c5c16749bfe8
waterplea <alexander@inkin.ru>
2026-04-03 16:28:56 +0400

    feat(cdk): `WindowSize` add signal util

--- projects/cdk/tokens/window-size.ts
+++ projects/cdk/tokens/window-size.ts
@@ -1,45 +1,45 @@
-import {inject, InjectionToken} from '@angular/core';
+import {inject, InjectionToken, type Signal} from '@angular/core';
+import {toObservable, toSignal} from '@angular/core/rxjs-interop';
 import {WA_WINDOW} from '@ng-web-apis/common';
-import {tuiTypedFromEvent} from '@taiga-ui/cdk/observables';
-import {map, type Observable, shareReplay, startWith} from 'rxjs';
+import {fromEvent, map, type Observable, startWith} from 'rxjs';
 
 export const TUI_WINDOW_SIZE = new InjectionToken<Observable<DOMRect>>(
     ngDevMode ? 'TUI_WINDOW_SIZE' : '',
-    {
-        factory: () => {
-            const w = inject(WA_WINDOW);
-
-            return tuiTypedFromEvent(w, 'resize').pipe(
-                startWith(null),
-                map(() => {
-                    const width = Math.max(
-                        w.document.documentElement.clientWidth || 0,
-                        w.innerWidth || 0,
-                        w.visualViewport?.width || 0,
-                    );
-                    const height = Math.max(
-                        w.document.documentElement.clientHeight || 0,
-                        w.innerHeight || 0,
-                        w.visualViewport?.height || 0,
-                    );
-                    const rect = {
-                        width,
-                        height,
-                        top: 0,
-                        left: 0,
-                        right: width,
-                        bottom: height,
-                        x: 0,
-                        y: 0,
-                    };
-
-                    return {
-                        ...rect,
-                        toJSON: () => JSON.stringify(rect),
-                    };
-                }),
-                shareReplay({bufferSize: 1, refCount: true}),
-            );
-        },
-    },
+    {factory: () => toObservable(tuiWindowSize(inject(WA_WINDOW)))},
 );
+
+export function tuiWindowSize(window: Window): Signal<DOMRect> {
+    return toSignal(
+        fromEvent(window, 'resize').pipe(
+            startWith(null),
+            map(() => {
+                const width = Math.max(
+                    window.document.documentElement.clientWidth || 0,
+                    window.innerWidth || 0,
+                    window.visualViewport?.width || 0,
+                );
+                const height = Math.max(
+                    window.document.documentElement.clientHeight || 0,
+                    window.innerHeight || 0,
+                    window.visualViewport?.height || 0,
+                );
+                const rect = {
+                    width,
+                    height,
+                    top: 0,
+                    left: 0,
+                    right: width,
+                    bottom: height,
+                    x: 0,
+                    y: 0,
+                };
+
+                return {
+                    ...rect,
+                    toJSON: () => JSON.stringify(rect),
+                };
+            }),
+        ),
+        {requireSync: true},
+    );
+}
