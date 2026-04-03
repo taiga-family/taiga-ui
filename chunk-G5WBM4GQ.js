bf2174c57d8222c023d5288ec8b8c7e21e5779c1
mdlufy <zidakbew@gmail.com>
2026-04-03 07:30:16 +0000

    Update snapshots/demo/next/main to output generated at dd5e688

---
+++ chunk-G5WBM4GQ.js
@@ -0,0 +1,16 @@
+import"./chunk-HU6DUUP4.js";var i=`import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {TuiDemo} from '@demo/utils';
+import {TuiLink} from '@taiga-ui/core';
+import {TuiList} from '@taiga-ui/layout';
+
+@Component({
+    imports: [TuiDemo, TuiLink, TuiList],
+    templateUrl: './index.html',
+    styleUrl: './index.less',
+    changeDetection,
+})
+export default class Page {
+    protected readonly installation = import('./snippets/installation.md');
+}
+`;export{i as default};
