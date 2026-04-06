730c4ffe6901a29b71ac794854605ea54b9207e0
Alex Inkin <alexander@inkin.ru>
2026-04-06 17:30:10 +0400

    fix(kit): `InputNumber` fix hydration (#13743)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/core/components/textfield/textfield-content.directive.ts
+++ projects/core/components/textfield/textfield-content.directive.ts
@@ -1,24 +1,44 @@
 import {
+    computed,
     Directive,
     type DoCheck,
     inject,
+    InjectionToken,
+    INJECTOR,
     type OnDestroy,
     TemplateRef,
+    type Type,
+    type ValueProvider,
 } from '@angular/core';
 
 import {TuiTextfieldComponent} from './textfield.component';
 
+export const TUI_TEXTFIELD_CONTENT = new InjectionToken<Type<any>>(
+    ngDevMode ? 'TUI_TEXTFIELD_CONTENT' : '',
+);
+
+export function tuiAsTextfieldContent(useValue: Type<any>): ValueProvider {
+    return {provide: TUI_TEXTFIELD_CONTENT, useValue};
+}
+
 @Directive({selector: 'ng-template[tuiTextfieldContent]'})
 export class TuiTextfieldContent implements DoCheck, OnDestroy {
-    private readonly ref = inject(TuiTextfieldComponent)
-        .vcr()
-        ?.createEmbeddedView(inject(TemplateRef));
+    private readonly vcr = inject(TuiTextfieldComponent).vcr;
+    private readonly options = {injector: inject(INJECTOR)};
+    private readonly content =
+        inject(TUI_TEXTFIELD_CONTENT, {optional: true}) || inject(TemplateRef);
+
+    private readonly ref = computed(() =>
+        this.content instanceof TemplateRef
+            ? this.vcr()?.createEmbeddedView(this.content)
+            : this.vcr()?.createComponent(this.content, this.options).hostView,
+    );
 
     public ngDoCheck(): void {
-        this.ref?.detectChanges();
+        this.ref()?.detectChanges();
     }
 
     public ngOnDestroy(): void {
-        this.ref?.destroy();
+        this.ref()?.destroy();
     }
 }
---
+++ projects/kit/components/input-number/step/input-number-step.directive.ts
@@ -0,0 +1,47 @@
+import {Directive, inject, input} from '@angular/core';
+import {TUI_VERSION} from '@taiga-ui/cdk/constants';
+import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
+import {tuiClamp, tuiSum} from '@taiga-ui/cdk/utils/math';
+import {
+    tuiAsTextfieldContent,
+    TuiTextfieldContent,
+} from '@taiga-ui/core/components/textfield';
+import {TuiAppearanceProxy} from '@taiga-ui/kit/directives/appearance-proxy';
+
+import {TuiInputNumberDirective} from '../input-number.directive';
+import {TUI_INPUT_NUMBER_OPTIONS} from '../input-number.options';
+import {TuiNumberMask} from '../number-mask.directive';
+import {TuiInputNumberStepButtons} from './input-number-step.component';
+
+@Directive({
+    selector: 'input[tuiInputNumber][step]',
+    providers: [tuiAsTextfieldContent(TuiInputNumberStepButtons)],
+    hostDirectives: [TuiAppearanceProxy, TuiTextfieldContent],
+    host: {
+        'data-tui-version': TUI_VERSION,
+        '(keydown.arrowDown.prevent)': 'onStep(-step())',
+        '(keydown.arrowUp.prevent)': 'onStep(step())',
+        '[class._with-buttons]': 'step()',
+    },
+})
+export class TuiInputNumberStep {
+    private readonly el = tuiInjectElement<HTMLInputElement>();
+    private readonly input = inject(TuiInputNumberDirective, {self: true});
+    private readonly mask = inject(TuiNumberMask, {self: true});
+
+    public readonly step = input(inject(TUI_INPUT_NUMBER_OPTIONS).step);
+
+    public onStep(step: bigint | number): void {
+        const current = this.input.parsed() || 0;
+        const updated =
+            typeof current === 'bigint'
+                ? current + BigInt(step)
+                : tuiSum(current, Number(step));
+
+        this.input.setValue(tuiClamp(updated, this.mask.min(), this.mask.max()));
+
+        setTimeout((end = Number.MAX_SAFE_INTEGER) => {
+            this.el.setSelectionRange(end, end);
+        });
+    }
+}
