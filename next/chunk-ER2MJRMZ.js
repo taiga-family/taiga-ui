503831c19055736b9b8030da54b9a93a357135a9
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2026-04-06 14:41:13 +0000

    deploy: 6dc4346dad70de07ef42ebffac7ffb0cdd05ca12

---
+++ next/chunk-ER2MJRMZ.js
@@ -0,0 +1,31 @@
+import"./chunk-JDW4OU2V.js";import"./chunk-7TM4QDQF.js";import"./chunk-SKSYBWQQ.js";import"./chunk-KXACV25Q.js";import"./chunk-6CJNLXQE.js";import"./chunk-CGSG6Q37.js";import"./chunk-ULVGWW2U.js";import"./chunk-OJBRV7UK.js";import"./chunk-DHALUPZN.js";import"./chunk-EWTBRNKO.js";import"./chunk-KKQE3O5P.js";import{f as s}from"./chunk-URIUKGJ5.js";import"./chunk-557P4HOD.js";import"./chunk-RBV6F563.js";import"./chunk-F24ZLD3R.js";import"./chunk-NYQN66L4.js";import"./chunk-BBTLQT4F.js";import"./chunk-LH7MN3HL.js";import"./chunk-E55D6EWC.js";import"./chunk-2FMNEN5E.js";import"./chunk-J7U5XIES.js";import"./chunk-O7H4UUZI.js";import"./chunk-VCA5TNVT.js";import{S as c}from"./chunk-QBZSQMQ5.js";import"./chunk-N4ZXSTQV.js";import"./chunk-UZHIVB6V.js";import"./chunk-2DZUDNPU.js";import"./chunk-L6Z24JAA.js";import"./chunk-HKTPBERU.js";import"./chunk-E4WTPEIR.js";import"./chunk-UQ4SMJ3Y.js";import"./chunk-3RTLOX6G.js";import{Ub as n,Zc as i,uc as r}from"./chunk-TQINLTZK.js";import"./chunk-VCG6AXSP.js";import"./chunk-HU6DUUP4.js";var a=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path
+        vector-effect="non-scaling-stroke"
+        d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"
+    />
+</svg>
+`;var l=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path vector-effect="non-scaling-stroke" d="m21 21-4.34-4.34" />
+    <circle vector-effect="non-scaling-stroke" cx="11" cy="11" r="8" />
+</svg>
+`;var e=class e{};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=n({type:e,selectors:[["ng-component"]],features:[i([c({"@tui.heart":a,"@tui.search":l})])],decls:2,vars:0,consts:[["icon","@tui.heart"],["icon","@tui.search"]],template:function(t,u){t&1&&r(0,"tui-icon",0)(1,"tui-icon",1)},dependencies:[s],styles:["[_nghost-%COMP%]{display:flex;gap:1rem;color:var(--tui-text-action)}"],changeDetection:0});var o=e;export{o as default};
