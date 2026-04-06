68a10c932aa7439752b46852bf19c1afea48b2e7
mdlufy <mdlufy@users.noreply.github.com>
2026-04-06 07:23:51 +0000

    deploy: 780253ddfc49c2515723c60d89d319cdd082cf13

---
+++ next/chunk-KQPUHOFT.js
@@ -0,0 +1,22 @@
+import"./chunk-HU6DUUP4.js";var e=`import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {TuiDemo} from '@demo/utils';
+import {type TuiHorizontalDirection, type TuiVerticalDirection} from '@taiga-ui/core';
+import {TuiComment} from '@taiga-ui/kit';
+
+@Component({
+    imports: [FormsModule, TuiComment, TuiDemo],
+    templateUrl: './index.html',
+    changeDetection,
+})
+export default class Page {
+    protected readonly examples = ['Basic', 'Customization'];
+
+    protected readonly directions: ReadonlyArray<
+        TuiHorizontalDirection | TuiVerticalDirection | ''
+    > = ['', 'top', 'bottom', 'start', 'end'];
+
+    protected direction: TuiHorizontalDirection | TuiVerticalDirection | '' = 'top';
+}
+`;export{e as default};
