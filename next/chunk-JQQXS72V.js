bd41ce6969165ef59ec3c2d43017a26aad461ed8
waterplea <waterplea@users.noreply.github.com>
2026-04-09 13:03:02 +0000

    deploy: ec81d966c001ac111b7c616180ed75fa60ade1c9

---
+++ next/chunk-JQQXS72V.js
@@ -0,0 +1,144 @@
+import"./chunk-HU6DUUP4.js";var e=`<header tuiNavigationHeader>
+    <label tuiNavigationLogo>
+        Custom color
+        <input
+            tuiSwitch
+            type="checkbox"
+            [ngModel]="color"
+            (ngModelChange)="onColor($event)"
+        />
+    </label>
+    <button
+        tuiButton
+        tuiChevron
+        tuiDropdown="Use TuiThemeColorService to control color"
+        tuiDropdownAuto
+        type="button"
+    >
+        How to?
+    </button>
+    <div
+        tuiDropdown
+        tuiDropdownAppearance=""
+        tuiDropdownAuto
+        [attr.tuiTheme]="dark() ? 'dark' : 'light'"
+    >
+        <button
+            tuiButton
+            tuiChevron
+            tuiTheme="dark"
+            type="button"
+        >
+            Keep dropdowns color?
+        </button>
+        <ol
+            *tuiDropdown
+            tuiList="s"
+            [style.padding-inline.rem]="1"
+        >
+            <li>Wrap your button in another tag</li>
+            <li>Move dropdown directives to that tag</li>
+            <li>
+                Reset navigation appearance by
+                <code>tuiDropdownAppearance=""</code>
+            </li>
+            <li>
+                Set
+                <code>tuiTheme="dark"</code>
+                on the button so it fits the header
+            </li>
+            <li>
+                Get app theme:
+                <code>readonly dark = inject(TUI_DARK_MODE)</code>
+            </li>
+            <li>
+                Use it to set theme on the wrapping tag:
+                <p>
+                    <code>[attr.tuiTheme]="dark() ? 'dark' : 'light'"</code>
+                </p>
+            </li>
+        </ol>
+    </div>
+</header>
+<main tuiNavigationMain>
+    <nav
+        compact
+        tuiSubheader
+    >
+        <a
+            iconStart="@tui.chevron-left"
+            tuiLink
+            [textContent]="'Repositories'"
+        ></a>
+        /
+        <strong tuiFade>Very long repository name</strong>
+        <div
+            size="xs"
+            tuiAvatar="@tui.lock"
+        ></div>
+        <tui-tabs>
+            <a tuiTab>Default view</a>
+            <a tuiTab>Contributors</a>
+            <a tuiTab>Code</a>
+        </tui-tabs>
+        <button
+            appearance="secondary"
+            tuiButton
+            type="button"
+        >
+            Button
+        </button>
+        <button
+            tuiButton
+            type="button"
+        >
+            Button
+        </button>
+        <button
+            appearance="secondary"
+            iconStart="@tui.ellipsis"
+            tuiDropdownAuto
+            tuiIconButton
+            type="button"
+            [tuiDropdown]="menu"
+        >
+            More
+            <ng-template #menu>
+                <tui-data-list>
+                    <button
+                        tuiOption
+                        type="button"
+                    >
+                        Button
+                    </button>
+                    <button
+                        tuiOption
+                        type="button"
+                    >
+                        Button
+                    </button>
+                    <button
+                        tuiOption
+                        type="button"
+                    >
+                        Button
+                    </button>
+                </tui-data-list>
+            </ng-template>
+        </button>
+    </nav>
+    @for (_ of '-'.repeat(10); track $index) {
+        <div
+            appearance="floating"
+            tuiCardLarge
+            tuiHeader
+            [style.grid-column]="'span 6'"
+        >
+            <h2 tuiTitle>
+                Some random content
+                <span tuiSubtitle>A subtitle</span>
+            </h2>
+        </div>
+    }
+</main>
+`;export{e as default};
