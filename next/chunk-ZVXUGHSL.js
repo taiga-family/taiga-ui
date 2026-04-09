975e32145b93ad5df26a3e335a6d4bfb34eac1ec
waterplea <waterplea@users.noreply.github.com>
2026-04-09 15:37:00 +0000

    deploy: 8c964a265cc7752d89384480c15f8529b350a2dc

---
+++ next/chunk-ZVXUGHSL.js
@@ -0,0 +1,14 @@
+import"./chunk-HU6DUUP4.js";var i=`@import '@taiga-ui/styles/utils';
+
+table {
+    inline-size: 100%;
+}
+
+thead {
+    white-space: nowrap;
+}
+
+:host-context(tui-root._mobile) thead {
+    display: none;
+}
+`;export{i as default};
