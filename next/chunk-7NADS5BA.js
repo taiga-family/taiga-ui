d29067563af4426d5bbd8d88a3e23f6555cbc02d
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2026-04-14 07:22:18 +0000

    deploy: ff72eb08f76fcadc110bde024f8f3e8abc936608

---
+++ next/chunk-7NADS5BA.js
@@ -0,0 +1,9 @@
+import"./chunk-HU6DUUP4.js";var t=`:host {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+
+    --tui-font-icon: 'Material Symbols Outlined';
+}
+`;export{t as default};
