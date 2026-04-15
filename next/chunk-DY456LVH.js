ea76f10504b11587c30d78b7c77f920f0126ed5f
nsbarsukov <nsbarsukov@users.noreply.github.com>
2026-04-15 13:27:26 +0000

    deploy: 0a441e4385039d4ee98858c6cef47ee38af1dc67

---
+++ next/chunk-DY456LVH.js
@@ -0,0 +1,28 @@
+import"./chunk-HU6DUUP4.js";var a=`<tui-doc-page [header]="\`Guide to update Taiga UI v\${tuiMajor - 1}\xA0->\xA0v\${tuiMajor}\`">
+    <ng-template pageTab>
+        <tui-doc-example
+            heading="Before you update"
+            [fullsize]="true"
+            [preview]="false"
+        >
+            <migration-guide-prerequisites />
+        </tui-doc-example>
+
+        <tui-doc-example
+            heading="Updating"
+            [fullsize]="true"
+            [preview]="false"
+        >
+            <migration-guide-actions />
+        </tui-doc-example>
+
+        <tui-doc-example
+            heading="Troubleshooting"
+            [fullsize]="true"
+            [preview]="false"
+        >
+            <migration-guide-troubleshooting />
+        </tui-doc-example>
+    </ng-template>
+</tui-doc-page>
+`;export{a as default};
