2c9d8e71871344a1135b113e0e42fd4ac3ac7a4e
nsbarsukov <nikita.s.barsukov@gmail.com>
2026-04-15 13:24:52 +0000

    Update snapshots/demo/next/main to output generated at 0a441e4

---
+++ chunk-URENLKBH.js
@@ -0,0 +1,25 @@
+import"./chunk-HU6DUUP4.js";var i=`tui-accordion {
+    border-radius: 0;
+
+    [tuiAccordion] {
+        white-space: normal;
+        block-size: fit-content;
+        padding-block: 1rem;
+        font-weight: normal;
+        box-shadow: none;
+        padding-inline-start: 0;
+        padding-block-start: 0;
+
+        [tuiBadge] {
+            align-self: flex-start;
+            margin-block-start: 0;
+            margin-inline-start: 0;
+        }
+    }
+
+    tui-expand {
+        padding-block-start: 0.125rem;
+        box-shadow: none;
+    }
+}
+`;export{i as default};
