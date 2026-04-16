2d922d7d274ba4b820511c4e5d2fa643f9de40ed
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-04-16 15:29:27 +0300

    fix(core): `SelectLike` fails to clear value on Backspace/Delete if caret is at the beginning/end (#13873)

--- projects/demo-playwright/tests/kit/select/select.pw.spec.ts
+++ projects/demo-playwright/tests/kit/select/select.pw.spec.ts
@@ -96,5 +96,46 @@ describe('Select', () => {
                 });
             });
         });
+
+        describe('[tuiTextfieldCleaner=true]', () => {
+            let example!: Locator;
+            let select!: TuiSelectPO;
+
+            beforeEach(async ({page}) => {
+                await tuiGoto(page, `${DemoRoute.Select}/API?tuiTextfieldCleaner=true`);
+                example = new TuiDocumentationPagePO(page).demo;
+                select = new TuiSelectPO(
+                    example.locator('tui-textfield:has([tuiSelect])'),
+                );
+            });
+
+            test('click on label + Backspace => Empty textfield', async ({page}) => {
+                await expect(select.textfield).toHaveValue('USA');
+
+                // eslint-disable-next-line playwright/no-force-option
+                await select.host.locator('[tuiLabel]').click({force: true});
+
+                await expect(select.textfield).toBeFocused();
+                await expect(select.dropdown).toBeVisible();
+
+                await page.keyboard.press('Backspace');
+
+                await expect(select.textfield).toHaveValue('');
+            });
+
+            test('click on label + Delete => Empty textfield', async ({page}) => {
+                await expect(select.textfield).toHaveValue('USA');
+
+                // eslint-disable-next-line playwright/no-force-option
+                await select.host.locator('[tuiLabel]').click({force: true});
+
+                await expect(select.textfield).toBeFocused();
+                await expect(select.dropdown).toBeVisible();
+
+                await page.keyboard.press('Delete');
+
+                await expect(select.textfield).toHaveValue('');
+            });
+        });
     });
 });
