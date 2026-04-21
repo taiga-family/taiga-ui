0ef197be644649240b4e3e1935b7c036eab07228
splincode <splincode@users.noreply.github.com>
2026-04-21 10:25:09 +0000

    deploy: 7b21438a4c35a695fddf0489f4ae4a40fe61aa41

--- next/chunk-DFGRQZX6.js
+++
@@ -1,1 +0,0 @@
-import{gb as a}from"./chunk-O6PPQ2JJ.js";import{Pa as o,ta as e}from"./chunk-K67SBWLP.js";var t=class t extends a{};t.\u0275fac=(()=>{let r;return function(c){return(r||(r=o(t)))(c||t)}})(),t.\u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"});var i=t;export{i as a};
