65e4b3c324c74da2093a8a148c677c797b497883
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-04-23 13:02:08 +0300

    fix(schematics): missed import `TuiIcon` during migration of `tuiTextfieldCustomContent`

--- projects/cdk/schematics/ng-update/v5/index.ts
+++ projects/cdk/schematics/ng-update/v5/index.ts
@@ -12,6 +12,7 @@ import {saveActiveProject} from 'ng-morph';
 import {TAIGA_VERSION} from '../../ng-add/constants/versions';
 import {type TuiSchema} from '../../ng-add/schema';
 import {FINISH_SYMBOL, START_SYMBOL, titleLog} from '../../utils/colored-log';
+import {saveAddedImports} from '../../utils/add-import-to-closest-module';
 import {
     formatMigrationStats,
     type MigrationStepTiming,
@@ -132,6 +133,7 @@ function main(options: TuiSchema, timings: MigrationStepTiming[]): Rule {
             timings,
         );
 
+        saveAddedImports(options);
         fileSystem.commitEdits();
         saveActiveProject();
 
