6b285af6bf64fa4e9fb936d4155a79aab5b6aae1
splincode <omaxphp@yandex.ru>
2026-04-24 11:40:45 +0000

    Update snapshots/demo/next/main to output generated at 59f7f59

--- chunk-CXI35BN5.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-IRA3DRJX.js";import"./chunk-K67SBWLP.js";import"./chunk-HU6DUUP4.js";export{a as AuthService};
--- chunk-LY2U35CY.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-CBJU5WRX.js";import"./chunk-K67SBWLP.js";import"./chunk-HU6DUUP4.js";export{a as DialogExample};
