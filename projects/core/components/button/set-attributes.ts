4ed528c7d348298f1c7f3766cdc1541075abc2fe
Vladimir Potekhin <vladimir.potekh@gmail.com>
2026-04-29 17:03:21 +0300

    chore: core add version encapsulation

--- projects/core/components/button/set-attributes.ts
+++ projects/core/components/button/set-attributes.ts
@@ -1,11 +1,11 @@
 import {TUI_VERSION} from '@taiga-ui/cdk/constants';
 import {tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
 
-export function tuiSetAttributes(): undefined {
+export function tuiSetAttributes(selectors: string[]): undefined {
     const el = tuiInjectElement();
 
     el.getAttributeNames()
-        .filter((name) => name.startsWith('tui'))
+        .filter((name) => selectors.some((selector) => name === selector.toLowerCase()))
         .forEach((name) => el.setAttribute(name, TUI_VERSION));
 
     return undefined;
