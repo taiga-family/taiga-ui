d4d1b578325ad4d8cf098e385ceaf5ef46e7684b
splincode <splincode@users.noreply.github.com>
2026-04-30 08:19:18 +0000

    deploy: 224f9c7413de09030f92dcacb0264cada05b0ab0

---
+++ next/chunk-P6N2N7RH.js
@@ -0,0 +1,31 @@
+import"./chunk-HU6DUUP4.js";var e=`<tui-segmented [disabled]="true">
+    <button type="button">Label</button>
+    <button type="button">Label</button>
+    <button type="button">Label</button>
+    <button type="button">Label</button>
+</tui-segmented>
+
+<tui-segmented>
+    <button
+        type="button"
+        [disabled]="true"
+    >
+        Label
+    </button>
+    <button type="button">Label</button>
+    <button
+        type="button"
+        [disabled]="true"
+    >
+        Label
+    </button>
+    <button type="button">Label</button>
+</tui-segmented>
+
+<tui-segmented inert>
+    <button type="button">Label</button>
+    <button type="button">Label</button>
+    <button type="button">Label</button>
+    <button type="button">Label</button>
+</tui-segmented>
+`;export{e as default};
