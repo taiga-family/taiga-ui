331df9b8a3de2bc401e92763fd1e62a75769e34d
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2026-04-30 17:41:39 +0300

    fix(layout): `CardLarge` fix nested cells selector (#14006)

--- projects/demo-cypress/src/tests/textfield-multi/textfield-multi.cy.ts
+++ projects/demo-cypress/src/tests/textfield-multi/textfield-multi.cy.ts
@@ -53,6 +53,23 @@ import {TuiCard} from '@taiga-ui/layout';
                 <tui-input-chip *tuiItem />
             </tui-textfield>
         </div>
+
+        <div
+            id="empty-textfield-nested"
+            tuiCardLarge
+        >
+            <div>
+                <tui-textfield multi>
+                    <label tuiLabel>Plain strings</label>
+                    <input
+                        placeholder="Placeholder"
+                        tuiInputChip
+                        [(ngModel)]="emptyValue"
+                    />
+                    <tui-input-chip *tuiItem />
+                </tui-textfield>
+            </div>
+        </div>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -76,5 +93,11 @@ describe('Textfield Multi', () => {
 
     it('should render correctly in card-large', () => {
         cy.get('#empty-textfield').compareSnapshot('textfield-multi-card-large');
+    });
+
+    it('should render correctly in nested elements in card-large', () => {
+        cy.get('#empty-textfield-nested').compareSnapshot(
+            'textfield-multi-card-large-nested',
+        );
     });
 });
