2e04d4015382c7a5c37c38e1f2482809eccc5fdd
nsbarsukov <nikita.s.barsukov@gmail.com>
2026-04-30 09:32:29 +0000

    Update snapshots/demo/next/main to output generated at bc50cf1

---
+++ chunk-5QB7QVFL.js
@@ -0,0 +1,31 @@
+import"./chunk-7YFSFC3B.js";import"./chunk-VYG3VX5K.js";import"./chunk-F6MQK75S.js";import"./chunk-N6R2BXMY.js";import"./chunk-CSTLLYBM.js";import"./chunk-5TSEIU6R.js";import"./chunk-7GJ3YZWE.js";import"./chunk-TK7GZRGK.js";import"./chunk-IBUADSLI.js";import{f as s}from"./chunk-SKQAA3HE.js";import"./chunk-E2BQWIFQ.js";import"./chunk-3Z2VS7RA.js";import"./chunk-D6JU4DWZ.js";import"./chunk-XUK6MGYQ.js";import"./chunk-WSX36SRI.js";import"./chunk-FIMVVC2Y.js";import"./chunk-ONXOK2WZ.js";import"./chunk-JV5SLO3T.js";import"./chunk-QVNM7BR3.js";import{S as c}from"./chunk-JQ4UJGTN.js";import"./chunk-B3JMXW2A.js";import"./chunk-W3XC7WVE.js";import"./chunk-VRWZNHCF.js";import"./chunk-KV4Y6QWM.js";import"./chunk-5KUJT4E6.js";import"./chunk-GVUOSEK7.js";import"./chunk-WUN4KIDJ.js";import"./chunk-WSXYPG2H.js";import{Ub as n,Xc as i,sc as r}from"./chunk-G2OXHOQV.js";import"./chunk-VCG6AXSP.js";import"./chunk-HU6DUUP4.js";var a=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path
+        vector-effect="non-scaling-stroke"
+        d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"
+    />
+</svg>
+`;var l=`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="calc((100vw - 100vh) / 10)"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path vector-effect="non-scaling-stroke" d="m21 21-4.34-4.34" />
+    <circle vector-effect="non-scaling-stroke" cx="11" cy="11" r="8" />
+</svg>
+`;var e=class e{};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=n({type:e,selectors:[["ng-component"]],features:[i([c({"@tui.heart":a,"@tui.search":l})])],decls:2,vars:0,consts:[["icon","@tui.heart"],["icon","@tui.search"]],template:function(t,u){t&1&&r(0,"tui-icon",0)(1,"tui-icon",1)},dependencies:[s],styles:["[_nghost-%COMP%]{display:flex;gap:1rem;color:var(--tui-text-action)}"],changeDetection:0});var o=e;export{o as default};
---
+++ chunk-ZNEOZ2RK.js
@@ -0,0 +1,17 @@
+import"./chunk-7YFSFC3B.js";import"./chunk-VYG3VX5K.js";import"./chunk-F6MQK75S.js";import"./chunk-N6R2BXMY.js";import"./chunk-CSTLLYBM.js";import"./chunk-5TSEIU6R.js";import{b as p}from"./chunk-7GJ3YZWE.js";import"./chunk-TK7GZRGK.js";import"./chunk-IBUADSLI.js";import"./chunk-SKQAA3HE.js";import"./chunk-E2BQWIFQ.js";import"./chunk-3Z2VS7RA.js";import"./chunk-D6JU4DWZ.js";import"./chunk-XUK6MGYQ.js";import"./chunk-WSX36SRI.js";import"./chunk-FIMVVC2Y.js";import"./chunk-ONXOK2WZ.js";import"./chunk-JV5SLO3T.js";import"./chunk-QVNM7BR3.js";import"./chunk-JQ4UJGTN.js";import"./chunk-B3JMXW2A.js";import"./chunk-W3XC7WVE.js";import"./chunk-VRWZNHCF.js";import"./chunk-KV4Y6QWM.js";import"./chunk-5KUJT4E6.js";import"./chunk-GVUOSEK7.js";import"./chunk-WUN4KIDJ.js";import"./chunk-WSXYPG2H.js";import{Db as o,Nc as e,Ub as l,gc as d,hc as r,qc as t,rc as n,sc as u}from"./chunk-G2OXHOQV.js";import"./chunk-VCG6AXSP.js";import"./chunk-HU6DUUP4.js";var i=class i{};i.\u0275fac=function(a){return new(a||i)},i.\u0275cmp=l({type:i,selectors:[["ng-component"]],decls:34,vars:5,consts:[["iconStart","@tui.edit","tuiLink","","type","button"],["appearance","action-grayscale","tuiLink","","type","button"],["appearance","action-destructive","iconEnd","@tui.trash","tuiLink","","type","button",3,"textContent"],["appearance","","iconEnd","@tui.external-link","tuiLink","","type","button"],["tuiTheme","dark"],["appearance","","iconStart","@tui.paintbrush","tuiLink",""],["iconStart","@tui.heart","tuiLink","","type","button"],["appearance","","tuiLink","","type","button"]],template:function(a,m){a&1&&(e(0,`Links by default have
+`),t(1,"button",0)(2,"span"),e(3,"action"),n()(),e(4,`
+appearance which you can override with
+`),t(5,"button",1),e(6,` action-grayscale
+`),n(),e(7,`
+or
+`),u(8,"button",2),t(9,"p"),e(10," If you use no appearance, links would "),t(11,"button",3)(12,"span"),e(13,"inherit text color"),n()(),e(14,` in tandem with an underline decoration to make them visible.
+`),n(),t(15,"p",4),e(16," It would also work well together with "),t(17,"a",5)(18,"span"),e(19,"dark theme"),n()(),e(20,` and other text colors
+`),n(),e(21,` If you want, you can
+`),t(22,"button",6)(23,"span"),e(24,"enable"),n()(),e(25,`
+dashed underline for any appearance using
+`),t(26,"code"),e(27,"text-decoration-line: underline"),n(),e(28,`
+or switch
+`),t(29,"button",7),e(30,` inherited
+`),n(),e(31,`
+link underline to dashed style by
+`),t(32,"code"),e(33,"text-decoration-style: dashed"),n()),a&2&&(o(8),d("textContent","action-destructive"),o(14),r("text-decoration-line","underline"),o(7),r("text-decoration-style","dashed"))},dependencies:[p],styles:["[tuiTheme=dark][_ngcontent-%COMP%]{background:var(--tui-background-base);color:var(--tui-text-secondary);padding:1rem;margin:1rem -1rem}"],changeDetection:0});var c=i;export{c as default};
