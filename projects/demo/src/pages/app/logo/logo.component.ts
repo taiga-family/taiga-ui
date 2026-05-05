348f018815440f1d6d141bec279a46155e53d790
waterplea <alexander@inkin.ru>
2026-05-05 14:57:20 +0400

    chore(demo): add loading indicator until hydration

--- projects/demo/src/pages/app/logo/logo.component.ts
+++ projects/demo/src/pages/app/logo/logo.component.ts
@@ -1,19 +1,21 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
+import {ChangeDetectionStrategy, Component, inject, PLATFORM_ID} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {DemoRoute} from '@demo/routes';
-import {TuiLink} from '@taiga-ui/core';
+import {TuiLoader} from '@taiga-ui/core';
+import {TuiSlides} from '@taiga-ui/layout';
 import {PolymorpheusComponent} from '@taiga-ui/polymorpheus';
 
 import {VersionManager} from '../version-manager/version-manager.component';
 
 @Component({
-    selector: 'logo',
-    imports: [RouterLink, TuiLink, VersionManager],
+    imports: [RouterLink, TuiLoader, TuiSlides, VersionManager],
     templateUrl: './logo.template.html',
     styleUrl: './logo.style.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Logo {
+    protected readonly browser = isPlatformBrowser(inject(PLATFORM_ID));
     protected readonly demoRoutes = DemoRoute;
 }
 
