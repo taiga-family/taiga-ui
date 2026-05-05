ca6aaffa0d4a3620f860da490bcb114bf689d8e0
waterplea <waterplea@users.noreply.github.com>
2026-05-05 13:39:53 +0000

    deploy: 8bb17f46e00db8d266339e610f0aad1cf4550094

---
+++ next/chunk-ADKWEKTH.js
@@ -0,0 +1,16 @@
+import"./chunk-HU6DUUP4.js";var a=`import {Component, signal} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiDay} from '@taiga-ui/cdk';
+import {TuiDatePicker} from '@taiga-ui/experimental';
+
+@Component({
+    imports: [TuiDatePicker],
+    templateUrl: './index.html',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected value = signal(TuiDay.currentLocal());
+}
+`;export{a as default};
--- next/chunk-NJRC6SHL.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-YQJ3SX3W.js";import"./chunk-HU6DUUP4.js";export{a as DemoRoute};
