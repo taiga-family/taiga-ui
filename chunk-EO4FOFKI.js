a258b55d6dc747741e00551670383873f97de362
waterplea <alexander@inkin.ru>
2026-05-05 13:37:17 +0000

    Update snapshots/demo/next/main to output generated at 8bb17f4

---
+++ chunk-EO4FOFKI.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-4VZI6YVG.js";import"./chunk-IL2UXQSZ.js";import"./chunk-HU6DUUP4.js";export{a as pages};
---
+++ chunk-EZDZZD3D.js
@@ -0,0 +1,2 @@
+import"./chunk-HU6DUUP4.js";var u=`<tui-date-picker [(value)]="value" />
+`;export{u as default};
