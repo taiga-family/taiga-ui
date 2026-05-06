2c43091cef877034af20ab18dfef7cc6c0c679a5
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2026-05-06 10:45:55 +0000

    deploy: 5536dddc2e16d0aa4dafe3bfeaacc3101b0dc6e7

---
+++ next/chunk-R2PJH46N.js
@@ -0,0 +1,33 @@
+import"./chunk-HU6DUUP4.js";var e=`import {JsonPipe} from '@angular/common';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiDay} from '@taiga-ui/cdk';
+import {TuiTextfield} from '@taiga-ui/core';
+import {TuiInputDateMulti, tuiInputDateMultiOptionsProvider} from '@taiga-ui/kit';
+
+@Component({
+    imports: [FormsModule, JsonPipe, TuiInputDateMulti, TuiTextfield],
+    templateUrl: './index.html',
+    encapsulation,
+    changeDetection,
+    providers: [
+        tuiInputDateMultiOptionsProvider({
+            valueTransformer: {
+                fromControlValue: (value: Date[]): TuiDay[] =>
+                    value.map((d) => TuiDay.fromUtcNativeDate(d)),
+                toControlValue: (value: TuiDay[]): Date[] =>
+                    value.map((day) => day.toUtcNativeDate()),
+            },
+        }),
+    ],
+})
+export default class Example {
+    protected value: Date[] | null = [
+        TuiDay.currentLocal().append({day: -1}).toLocalNativeDate(),
+        TuiDay.currentLocal().toLocalNativeDate(),
+        TuiDay.currentLocal().append({day: 1}).toLocalNativeDate(),
+    ];
+}
+`;export{e as default};
