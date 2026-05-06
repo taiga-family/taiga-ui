1e94f6a3b921d66a7da6145dd23f17da8ecbe97f
waterplea <waterplea@users.noreply.github.com>
2026-05-06 06:45:43 +0000

    deploy: 8e8dfb4b2d26f64f2cea6a80c547b1cb5636101e

---
+++ next/chunk-F4K3YS2D.js
@@ -0,0 +1,19 @@
+import"./chunk-HU6DUUP4.js";var e=`<a
+    class="t-link"
+    [routerLink]="demoRoutes.GettingStarted"
+>
+    <span tuiSlides>
+        @if (ready()) {
+            <img
+                alt="Logo"
+                src="assets/images/taiga.svg"
+                class="t-logo"
+            />
+        } @else {
+            <tui-loader />
+        }
+    </span>
+    Taiga UI
+</a>
+<version-manager />
+`;export{e as default};
