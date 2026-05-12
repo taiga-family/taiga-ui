bda3d94d9c1f5a4450739ecc8e29ea99e09b6420
waterplea <waterplea@users.noreply.github.com>
2026-05-12 12:55:59 +0000

    deploy: 5de15bcaba6ef79da2d20494cc00ee281dedc73c

---
+++ next/chunk-YXJWJFIK.js
@@ -0,0 +1,11 @@
+import"./chunk-HU6DUUP4.js";var t=`p {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+}
+
+tui-textfield {
+    inline-size: 100%;
+}
+`;export{t as default};
