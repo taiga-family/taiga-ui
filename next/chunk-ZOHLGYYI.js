50516476d611d0a33a5d875e26e06957e506bb11
mdlufy <mdlufy@users.noreply.github.com>
2026-05-12 16:01:45 +0000

    deploy: 58e5ee46a06850788ca2c37cb56bc1d8b9f41ada

---
+++ next/chunk-ZOHLGYYI.js
@@ -0,0 +1,14 @@
+import"./chunk-HU6DUUP4.js";var t=`import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiIcon} from '@taiga-ui/core';
+
+@Component({
+    imports: [TuiIcon],
+    templateUrl: './index.html',
+    styleUrl: './index.less',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {}
+`;export{t as default};
