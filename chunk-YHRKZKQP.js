9895b14eeb605809a22e6412f36c1589e1b96f7f
waterplea <alexander@inkin.ru>
2026-05-13 11:50:41 +0000

    Update snapshots/demo/next/main to output generated at 97d7337

---
+++ chunk-YHRKZKQP.js
@@ -0,0 +1,22 @@
+import"./chunk-HU6DUUP4.js";var i=`<tui-textfield
+    multi
+    [disabledItemHandler]="handler"
+    [style.width.rem]="19"
+    [tuiTextfieldCleaner]="false"
+>
+    <input
+        tuiInputChip
+        [formControl]="control"
+    />
+    @if (control.value.length > 2) {
+        <button
+            tabindex="-1"
+            tuiButtonX
+            (click)="control.setValue(this.required)"
+        >
+            Clear
+        </button>
+    }
+    <tui-input-chip *tuiItem />
+</tui-textfield>
+`;export{i as default};
