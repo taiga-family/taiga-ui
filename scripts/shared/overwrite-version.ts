56e1b45ed9f6abdc0b93bad8c763e34218591577
Максим Иванов <splincodewd@yandex.ru>
2026-05-13 21:06:57 +0900

    chore: prettier success (#14103)

--- scripts/shared/overwrite-version.ts
+++ scripts/shared/overwrite-version.ts
@@ -1,13 +1,9 @@
 import {writeFileSync} from 'node:fs';
 
 export function overwriteVersion(file: string, version: string): void {
-    writeFileSync(file, `export const TUI_VERSION = '${version}';\n`, {
-        encoding: 'utf-8',
-    });
+    writeFileSync(file, `export const TUI_VERSION = '${version}';\n`);
 }
 
 export function overwriteVersionLess(file: string, version: string): void {
-    writeFileSync(file, `@tui-version: '${version}';`, {
-        encoding: 'utf-8',
-    });
+    writeFileSync(file, `@tui-version: '${version}';\n`);
 }
