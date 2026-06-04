ed4b74f718469b2a46030237a5dc576c9e6cbf32
mdlufy <german.panov.2018@mail.ru>
2026-06-04 12:51:27 +0300

    chore: fix

--- projects/kit/components/input-phone-international/input-phone-international.component.ts
+++ projects/kit/components/input-phone-international/input-phone-international.component.ts
@@ -59,10 +59,10 @@ const NOT_FORM_CONTROL_SYMBOLS = /[^+\d]/g;
         '(input)': 'masked.set($event.target.value)',
     },
 })
+// TODO(v6): rename to TuiInputPhoneInternationalDirective
 export class TuiInputPhoneInternationalComponent extends TuiControl<string> {
     private readonly el = tuiInjectElement<HTMLInputElement>();
     private readonly options = inject(TUI_INPUT_PHONE_INTERNATIONAL_OPTIONS);
-
     protected readonly ios = inject(WA_IS_IOS);
     protected readonly dropdownEnabled = tuiDropdownEnabled(this.interactive);
     protected readonly masked = tuiValue(this.el);
