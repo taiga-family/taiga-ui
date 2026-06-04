e5523054355e6590e55de61701a8e3cf242707e7
German Panov <german.panov.2018@mail.ru>
2026-06-04 13:35:10 +0300

    fix(kit): `InputTime` hydration (#14201)
    Co-authored-by: taiga-family-bot <41898282+github-actions[bot]@users.noreply.github.com>

--- projects/kit/components/input-date-time/input-date-time.component.ts
+++ projects/kit/components/input-date-time/input-date-time.component.ts
@@ -27,7 +27,10 @@ import {TuiInputDateTimeDirective} from './input-date-time.directive';
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
     hostDirectives: [TuiWithNativePicker],
-    host: {'data-tui-version': TUI_VERSION, ngSkipHydration: 'true'},
+    host: {
+        'data-tui-version': TUI_VERSION,
+        ngSkipHydration: 'true',
+    },
 })
 export class TuiInputDateTimeComponent extends TuiNativeTimePicker {
     protected readonly host = inject(TuiInputDateTimeDirective);
