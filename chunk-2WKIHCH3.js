6f515c4a710a6279747d2a38a4028e1eaef21ece
mdlufy <zidakbew@gmail.com>
2026-06-08 08:09:28 +0000

    Update snapshots/demo/next/main to output generated at b490433

---
+++ chunk-2WKIHCH3.js
@@ -0,0 +1,8 @@
+import"./chunk-HU6DUUP4.js";var e=`<tui-textfield>
+    <input
+        placeholder="\u2022\u2022\u2022\u2022"
+        tuiInputPin
+        [formControl]="control"
+    />
+</tui-textfield>
+`;export{e as default};
