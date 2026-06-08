e2dd26eceaff87eba5057b2f2e4058039f6aa88b
waterplea <alexander@inkin.ru>
2026-06-08 12:32:16 +0400

    chore: fix

--- projects/demo/src/pages/components/badge/index.ts
+++ projects/demo/src/pages/components/badge/index.ts
@@ -1,17 +1,30 @@
 import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {TuiDocAppearance} from '@demo/components/appearance';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
 import {type TuiSizeS, type TuiSizeXL} from '@taiga-ui/core';
 import {TuiBadge, TuiFade, TuiRadioList} from '@taiga-ui/kit';
 
 @Component({
-    imports: [FormsModule, TuiBadge, TuiDemo, TuiDocAppearance, TuiFade, TuiRadioList],
+    imports: [FormsModule, TuiBadge, TuiDemo, TuiFade, TuiRadioList],
     templateUrl: './index.html',
     changeDetection,
 })
 export default class Page {
+    protected readonly appearanceVariants = [
+        '',
+        'accent',
+        'primary',
+        'custom',
+        'positive',
+        'negative',
+        'warning',
+        'info',
+        'neutral',
+    ];
+
+    protected appearance = this.appearanceVariants[0]!;
+
     protected readonly sizeVariants: ReadonlyArray<TuiSizeS | TuiSizeXL> = [
         's',
         'm',
