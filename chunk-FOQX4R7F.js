bea4361e5400665183c93aff64d2a5b4481aba10
mdlufy <zidakbew@gmail.com>
2026-06-08 12:06:41 +0000

    Update snapshots/demo/next/main to output generated at 1afdc7a

---
+++ chunk-FOQX4R7F.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-3IPNBSB2.js";import"./chunk-HU6DUUP4.js";export{a as default};
---
+++ chunk-W5PGMICJ.js
@@ -0,0 +1,20 @@
+import"./chunk-HU6DUUP4.js";var n=`@import '@taiga-ui/styles/utils';
+
+[tuiLink] {
+    text-decoration: none;
+
+    --tui-icon-size: 1.5rem;
+
+    &::after {
+        .transition(margin-inline-start);
+    }
+
+    &:hover {
+        color: var(--tui-text-action);
+
+        &::after {
+            margin-inline-start: 0.5rem;
+        }
+    }
+}
+`;export{n as default};
