14a2822c992463f5003d73db6e06b937c6652a0e
mdlufy <german.panov.2018@mail.ru>
2026-06-09 10:04:32 +0300

    chore: fix

--- projects/demo-cypress/src/tests/input-pin.cy.ts
+++ projects/demo-cypress/src/tests/input-pin.cy.ts
@@ -23,51 +23,54 @@ export class TestInputPin {
 }
 
 describe('InputPin', () => {
-    beforeEach(() => cy.mount(TestInputPin));
+    beforeEach(() => {
+        cy.mount(TestInputPin);
+        cy.get('#pin-input').as('pin');
+    });
 
     it('should focus input and place cursor at start when clicking on empty input', () => {
-        cy.get('#pin-input').click();
+        cy.get('@pin').click();
 
-        cy.get('#pin-input').should('have.focus');
-        cy.get('#pin-input').should('have.prop', 'selectionStart', 0);
-        cy.get('#pin-input').should('have.prop', 'selectionEnd', 0);
+        cy.get('@pin').should('have.focus');
+        cy.get('@pin').should('have.prop', 'selectionStart', 0);
+        cy.get('@pin').should('have.prop', 'selectionEnd', 0);
     });
 
     it('should select first character when focusing on filled input', () => {
-        cy.get('#pin-input').type('1234');
+        cy.get('@pin').type('1234');
 
-        cy.get('#pin-input').click();
+        cy.get('@pin').click();
 
-        cy.get('#pin-input').should('have.prop', 'selectionStart', 0);
-        cy.get('#pin-input').should('have.prop', 'selectionEnd', 1);
+        cy.get('@pin').should('have.prop', 'selectionStart', 0);
+        cy.get('@pin').should('have.prop', 'selectionEnd', 1);
     });
 
     it('should collapse cursor before last character when navigating past the end', () => {
-        cy.get('#pin-input').type('1234');
+        cy.get('@pin').type('1234');
 
-        cy.get('#pin-input').type('{rightArrow}'.repeat(4));
+        cy.get('@pin').type('{rightArrow}'.repeat(4));
 
-        cy.get('#pin-input').should('have.prop', 'selectionStart', 3);
-        cy.get('#pin-input').should('have.prop', 'selectionEnd', 3);
+        cy.get('@pin').should('have.prop', 'selectionStart', 3);
+        cy.get('@pin').should('have.prop', 'selectionEnd', 3);
     });
 
     it('should not move cursor when selection is in the middle', () => {
-        cy.get('#pin-input').type('12');
-        cy.get('#pin-input').type('{leftArrow}');
+        cy.get('@pin').type('12');
+        cy.get('@pin').type('{leftArrow}');
 
-        cy.get('#pin-input').should('have.prop', 'selectionStart', 1);
-        cy.get('#pin-input').should('have.prop', 'selectionEnd', 1);
+        cy.get('@pin').should('have.prop', 'selectionStart', 1);
+        cy.get('@pin').should('have.prop', 'selectionEnd', 1);
     });
 
     it('should allow typing PIN code', () => {
-        cy.get('#pin-input').type('1234');
+        cy.get('@pin').type('1234');
 
-        cy.get('#pin-input').should('have.value', '1234');
+        cy.get('@pin').should('have.value', '1234');
     });
 
     it('should respect maxlength attribute', () => {
-        cy.get('#pin-input').type('12345678');
+        cy.get('@pin').type('12345678');
 
-        cy.get('#pin-input').should('have.value', '1234');
+        cy.get('@pin').should('have.value', '1234');
     });
 });
