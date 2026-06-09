dd56e701594604bddaf7bfcf6d3a7b72eb7caaf9
waterplea <alexander@inkin.ru>
2026-06-09 16:43:51 +0400

    feat(kit): `Badge` add font scaling

--- projects/demo/src/pages/components/badge/examples/1/index.ts
+++ projects/demo/src/pages/components/badge/examples/1/index.ts
@@ -1,13 +1,25 @@
-import {Component} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+import {Component, inject, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiBadge, TuiStatus} from '@taiga-ui/kit';
+import {TuiPlatform} from '@taiga-ui/cdk';
+import {TuiSlider} from '@taiga-ui/core';
+import {TuiBadge} from '@taiga-ui/kit';
 
 @Component({
-    imports: [TuiBadge, TuiStatus],
+    imports: [FormsModule, TuiBadge, TuiPlatform, TuiSlider],
     templateUrl: './index.html',
     styleUrl: './index.less',
     encapsulation,
     changeDetection,
 })
-export default class Example {}
+export default class Example {
+    protected readonly doc = inject(DOCUMENT);
+    protected readonly value = signal(0);
+    protected readonly sizes = ['s', 'm', 'l', 'xl'] as const;
+
+    protected onValueChange(value: number): void {
+        this.doc.documentElement.style.setProperty('--t-font-offset', `${value}`);
+    }
+}
