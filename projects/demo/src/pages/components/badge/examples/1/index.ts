120ec765adaae5369f93dc3d27c1168c7442ad19
waterplea <alexander@inkin.ru>
2026-06-10 14:44:45 +0400

    chore: cleanup

--- projects/demo/src/pages/components/badge/examples/1/index.ts
+++ projects/demo/src/pages/components/badge/examples/1/index.ts
@@ -1,25 +1,13 @@
-import {DOCUMENT} from '@angular/common';
-import {Component, inject, signal} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiPlatform} from '@taiga-ui/cdk';
-import {TuiSlider} from '@taiga-ui/core';
-import {TuiBadge} from '@taiga-ui/kit';
+import {TuiBadge, TuiStatus} from '@taiga-ui/kit';
 
 @Component({
-    imports: [FormsModule, TuiBadge, TuiPlatform, TuiSlider],
+    imports: [TuiBadge, TuiStatus],
     templateUrl: './index.html',
     styleUrl: './index.less',
     encapsulation,
     changeDetection,
 })
-export default class Example {
-    protected readonly doc = inject(DOCUMENT);
-    protected readonly value = signal(0);
-    protected readonly sizes = ['s', 'm', 'l', 'xl'] as const;
-
-    protected onValueChange(value: number): void {
-        this.doc.documentElement.style.setProperty('--t-font-offset', `${value}`);
-    }
-}
+export default class Example {}
