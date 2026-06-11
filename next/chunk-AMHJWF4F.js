6712e800503f9db320f47a32143674c9b42e2983
splincode <splincode@users.noreply.github.com>
2026-06-11 14:39:12 +0000

    deploy: 25f8dc6d6307971cb0d6e8d0658c8f4ca20f2199

---
+++ next/chunk-AMHJWF4F.js
@@ -0,0 +1,60 @@
+import"./chunk-HU6DUUP4.js";var o=`<button
+    tuiButton
+    tuiDropdownHover
+    type="button"
+    [tuiDropdown]="dropdown"
+    [(tuiDropdownOpen)]="open"
+>
+    Dropdown hover
+</button>
+
+<ng-template #dropdown>
+    <div [style.padding]="'0 1rem'">
+        <tui-textfield
+            tuiChevron
+            class="margin"
+            [tuiTextfieldCleaner]="false"
+        >
+            <label tuiLabel>Nested select</label>
+
+            <input
+                tuiSelect
+                [formControl]="selected"
+            />
+
+            <tui-data-list-wrapper
+                *tuiDropdown
+                [items]="selectItems"
+            />
+        </tui-textfield>
+
+        <div>
+            <button
+                size="s"
+                tuiButton
+                tuiDropdownAuto
+                tuiDropdownHover
+                type="button"
+                [tuiDropdown]="content"
+            >
+                Nested dropdown hover
+            </button>
+        </div>
+    </div>
+
+    <tui-data-list>
+        @for (item of items; track item) {
+            <button
+                tuiOption
+                type="button"
+            >
+                {{ item }}
+            </button>
+        }
+    </tui-data-list>
+</ng-template>
+
+<ng-template #content>
+    <p class="tui-space_horizontal-2">Nested content!</p>
+</ng-template>
+`;export{o as default};
