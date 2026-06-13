c8077f5c35de6e0d20198f0dd79d1146d1f4f86f
mdlufy <german.panov.2018@mail.ru>
2026-06-14 02:27:14 +0300

    chore: fix

--- projects/layout/components/input-search/input-search-content.component.ts
+++ projects/layout/components/input-search/input-search-content.component.ts
@@ -5,9 +5,7 @@ import {
     inject,
     TemplateRef,
     viewChild,
-    ViewEncapsulation,
 } from '@angular/core';
-import {TUI_VERSION} from '@taiga-ui/cdk/constants';
 import {PolymorpheusOutlet} from '@taiga-ui/polymorpheus';
 
 import {TuiInputSearch} from './input-search.component';
@@ -16,14 +14,8 @@ import {TuiInputSearch} from './input-search.component';
     selector: 'tui-input-search-content',
     imports: [PolymorpheusOutlet],
     templateUrl: './input-search-content.template.html',
-    styles: `
-        [data-tui-version='${TUI_VERSION}'] {
-            @import './input-search.style.less';
-        }
-    `,
-    encapsulation: ViewEncapsulation.None,
+    styleUrl: './input-search.style.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    host: {'data-tui-version': TUI_VERSION},
 })
 export class TuiInputSearchContent {
     private readonly tpl = viewChild.required(TemplateRef);
