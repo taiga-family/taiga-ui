8ed93b6dd1f73580810f7d33bd9d5cc43e953394
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2026-06-17 12:04:04 +0000

    Update snapshots/demo/next/main to output generated at 4009f35

---
+++ chunk-ENJAFFTD.js
@@ -0,0 +1,26 @@
+import"./chunk-HU6DUUP4.js";var o=`.container {
+    max-block-size: 30rem;
+    inline-size: 20rem;
+    overflow: scroll;
+    overscroll-behavior: none;
+}
+
+tui-app-bar {
+    position: sticky;
+    z-index: 1;
+    inset-block-start: 0;
+    margin-block-end: 0.75rem;
+}
+
+.content {
+    padding: 0;
+
+    > [tuiHeader] {
+        padding: 1rem;
+    }
+}
+
+.block {
+    block-size: 20rem;
+}
+`;export{o as default};
---
+++ chunk-QJJLZG75.js
@@ -0,0 +1,22 @@
+import"./chunk-HU6DUUP4.js";var i=`import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {TuiDemo} from '@demo/utils';
+
+@Component({
+    imports: [TuiDemo],
+    templateUrl: './index.html',
+    changeDetection,
+})
+export default class Page {
+    protected readonly examples = [
+        'Mobile \u2014 medium size',
+        'Desktop \u2014 large size',
+        'Variants',
+        'Dialog',
+        'Dynamic header',
+        'iOS Liquid glass',
+        'Liquid glass dynamic header',
+        'Color background',
+    ];
+}
+`;export{i as default};
