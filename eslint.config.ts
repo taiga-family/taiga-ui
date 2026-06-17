c0e6103d0ea69fbdab61010705ad19e3d7bfe77f
splincode <omaxphp@yandex.ru>
2026-06-17 17:49:28 +0300

    ci: update linter package

--- eslint.config.ts
+++ eslint.config.ts
@@ -39,6 +39,7 @@ export default [
             '@angular-eslint/template/no-empty-control-flow': 'off', // TODO: fix later
             '@angular-eslint/template/no-non-null-assertion': 'off', // TODO: fix later
             '@angular-eslint/template/no-nested-tags': 'off', // TODO: fix later
+            '@angular-eslint/template/prefer-at-else': 'off', // TODO: fix later
         },
     },
 ];
