144065c931f5a2ce6de807c860f0d615989af65d
splincode <splincode@users.noreply.github.com>
2026-06-18 07:30:12 +0000

    deploy: d28aa4ff357f12e64a4e5a71e55793ace7a313ba

---
+++ next/chunk-BV2RW2EZ.js
@@ -0,0 +1,30 @@
+import"./chunk-HU6DUUP4.js";var t=`import {Component, inject, signal} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {WaIntersectionObserver} from '@ng-web-apis/intersection-observer';
+import {TUI_DARK_MODE, TUI_LIQUID_GLASS, TuiTitle} from '@taiga-ui/core';
+import {TuiAppBar, TuiDynamicHeader, TuiHeader} from '@taiga-ui/layout';
+
+@Component({
+    imports: [TuiAppBar, TuiDynamicHeader, TuiHeader, TuiTitle, WaIntersectionObserver],
+    templateUrl: './index.html',
+    styleUrl: './index.less',
+    encapsulation,
+    changeDetection,
+    providers: [{provide: TUI_LIQUID_GLASS, useValue: signal(true)}],
+})
+export default class Example {
+    protected readonly mode = inject(TUI_DARK_MODE);
+    protected readonly colors = ['#2f3f5b', '#3a3f66', '#5a2f3f', '#3f4e37', '#5a3851'];
+    protected readonly color = signal('');
+    protected readonly blur = signal(false);
+
+    protected onIntersection(isIntersecting: boolean, color: string): void {
+        if (isIntersecting) {
+            this.color.set(color);
+        } else if (this.color() === color) {
+            this.color.set('');
+        }
+    }
+}
+`;export{t as default};
