c8e25a5896c87903d267a5b4c1ac79d9f5551fe7
waterplea <alexander@inkin.ru>
2026-06-19 14:03:11 +0300

    chore(experimental): `DatePicker` connect to controls

--- projects/demo/src/pages/components/date-picker/examples/1/index.ts
+++ projects/demo/src/pages/components/date-picker/examples/1/index.ts
@@ -1,38 +1,43 @@
-import {JsonPipe} from '@angular/common';
-import {Component, signal} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiDay, TuiDayRange} from '@taiga-ui/cdk';
-import {TuiButton, TuiGroup} from '@taiga-ui/core';
+import {TuiDay} from '@taiga-ui/cdk';
+import {TuiButton} from '@taiga-ui/core';
 import {TuiDatePicker} from '@taiga-ui/experimental';
-import {TuiChevron, TuiDataListWrapper, TuiSelect} from '@taiga-ui/kit';
+import {
+    TuiButtonSelect,
+    TuiInputDate,
+    TuiInputDateMulti,
+    TuiInputDateRange,
+} from '@taiga-ui/kit';
+import {TuiForm} from '@taiga-ui/layout';
 
 @Component({
     imports: [
-        FormsModule,
-        JsonPipe,
+        ReactiveFormsModule,
         TuiButton,
-        TuiChevron,
-        TuiDataListWrapper,
+        TuiButtonSelect,
         TuiDatePicker,
-        TuiGroup,
-        TuiSelect,
+        TuiForm,
+        TuiInputDate,
+        TuiInputDateMulti,
+        TuiInputDateRange,
     ],
     templateUrl: './index.html',
-    styleUrl: './index.less',
     encapsulation,
     changeDetection,
 })
 export default class Example {
-    protected readonly modes = ['single', 'multi', 'range'] as const;
-    protected readonly mode = signal(this.modes[2]);
     protected readonly min = TuiDay.currentLocal().append({month: -2, year: -10});
     protected readonly max = TuiDay.currentLocal().append({month: 2, year: 10});
 
-    protected readonly value = signal<any>(
-        new TuiDayRange(TuiDay.currentLocal(), TuiDay.currentLocal().append({day: 7})),
-    );
+    protected readonly form = new FormGroup({
+        single: new FormControl(),
+        multi: new FormControl(),
+        range: new FormControl(),
+        button: new FormControl(),
+    });
 
     protected readonly disabledItemHandler = (item: TuiDay): boolean => item.day === 13;
 }
