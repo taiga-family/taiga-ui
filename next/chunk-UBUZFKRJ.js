c1fd62855cf2ec09f32c7f4555838d87585272cb
waterplea <waterplea@users.noreply.github.com>
2026-06-19 11:00:15 +0000

    deploy: 7dc4899e1b688c8974a1316f42e6840ad7884392

---
+++ next/chunk-UBUZFKRJ.js
@@ -0,0 +1,10 @@
+import"./chunk-HU6DUUP4.js";var e=`<button
+    iconEnd="@tui.calendar"
+    tuiButton
+    tuiButtonSelect
+    [(ngModel)]="value"
+>
+    {{ value || 'Select date' }}
+    <tui-calendar *tuiDropdown />
+</button>
+`;export{e as default};
