f6e2d945b3a9f19ec56bc48173a08d43151fea21
waterplea <alexander@inkin.ru>
2026-06-22 09:27:39 +0000

    Update snapshots/demo/next/main to output generated at d79f201

---
+++ chunk-KQECUTBX.js
@@ -0,0 +1,38 @@
+import"./chunk-LE4FBTPD.js";import"./chunk-FFJSGPRZ.js";import"./chunk-ASPVR5D2.js";import"./chunk-FT4LHMEJ.js";import"./chunk-3TAUPADK.js";import"./chunk-76M5QL5J.js";import"./chunk-JRNFM4AI.js";import"./chunk-O6YMWZZ6.js";import"./chunk-GTX3S3NX.js";import{J as c}from"./chunk-N5L545VX.js";import"./chunk-TPKRIXTO.js";import"./chunk-ZLHPQUIM.js";import"./chunk-EPQS6GHL.js";import"./chunk-26TK73UB.js";import"./chunk-6V42QAPU.js";import"./chunk-BQH2T4GB.js";import"./chunk-BC3DU3V7.js";import"./chunk-QQPUUUJF.js";import"./chunk-KBBX6QQL.js";import"./chunk-GPE4A7R3.js";import"./chunk-RLMFB26V.js";import{H as s}from"./chunk-32WKGY2Q.js";import"./chunk-PKS4FNUV.js";import"./chunk-QCWMZ6BU.js";import"./chunk-TDRXD3LC.js";import"./chunk-UZXHPZ36.js";import"./chunk-P5PNEB64.js";import"./chunk-G2L6WAFL.js";import"./chunk-C2MAWAEY.js";import{Ub as n,Zc as i,uc as r}from"./chunk-NUYCOIDP.js";import"./chunk-VCG6AXSP.js";import"./chunk-HU6DUUP4.js";var a=`
+<svg
+  
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="calc((100vw - 100vh) / 10)"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path vector-effect="non-scaling-stroke" d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5" />
+</svg>
+`;var l=`<svg xmlns="http://www.w3.org/2000/svg" overflow="visible" viewBox="0 0 32 32">
+    <g transform="translate(3, 8)">
+        <path
+            vector-effect="non-scaling-stroke"
+            d="M12.8333333,14.9858645 C15.0443072,13.5181166 16.5,11.0185601 16.5,8.18181818 C16.5,5.34507627 15.0443072,2.84551975 12.8333333,1.37777185 C14.1442565,0.50751969 15.7206989,0 17.4166667,0 C21.9730159,0 25.6666667,3.66312477 25.6666667,8.18181818 C25.6666667,12.7005116 21.9730159,16.3636364 17.4166667,16.3636364 C15.7206989,16.3636364 14.1442565,15.8561167 12.8333333,14.9858645 Z"
+            fill="#FBB735"
+            stroke="none"
+        />
+        <path
+            vector-effect="non-scaling-stroke"
+            d="M12.8333333,1.37777185 C10.6223595,2.84551975 9.16666667,5.34507627 9.16666667,8.18181818 C9.16666667,11.0185601 10.6223595,13.5181166 12.8333333,14.9858645 C11.5224101,15.8561167 9.94596776,16.3636364 8.25,16.3636364 C3.69365081,16.3636364 0,12.7005116 0,8.18181818 C0,3.66312477 3.69365081,0 8.25,0 C9.94596776,0 11.5224101,0.50751969 12.8333333,1.37777185 Z"
+            fill="#F03727"
+            stroke="none"
+        />
+        <path
+            vector-effect="non-scaling-stroke"
+            d="M12.8333333,1.37777185 C15.0443072,2.84551975 16.5,5.34507627 16.5,8.18181818 C16.5,11.0185601 15.0443072,13.5181166 12.8333333,14.9858645 C10.6223595,13.5181166 9.16666667,11.0185601 9.16666667,8.18181818 C9.16666667,5.34507627 10.6223595,2.84551975 12.8333333,1.37777185 Z"
+            fill="#F6772D"
+            stroke="none"
+        />
+    </g>
+</svg>
+`;var e=class e{};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=n({type:e,selectors:[["ng-component"]],features:[i([s({"@tui.heart":a,"@img.mastercard":l})])],decls:2,vars:0,consts:[["icon","@tui.heart"],["icon","@img.mastercard"]],template:function(t,g){t&1&&r(0,"tui-icon",0)(1,"tui-icon",1)},dependencies:[c],styles:["[_nghost-%COMP%]{display:flex;gap:1rem;color:var(--tui-text-action)}"],changeDetection:0});var o=e;export{o as default};
