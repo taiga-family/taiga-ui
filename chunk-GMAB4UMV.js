18741851fcf59917cbe76cbccef3dba239e08277
waterplea <alexander@inkin.ru>
2026-06-23 08:33:58 +0000

    Update snapshots/demo/next/main to output generated at dfe010d

---
+++ chunk-GMAB4UMV.js
@@ -0,0 +1,22 @@
+import"./chunk-HU6DUUP4.js";var i=`<button
+    iconEnd="@tui.settings"
+    tuiLink
+    type="button"
+    [textContent]="'Link with icon right'"
+></button>
+<p>
+    <button
+        iconStart="@tui.settings"
+        tuiLink
+        type="button"
+        [textContent]="'Link with icon left'"
+    ></button>
+</p>
+<button
+    iconEnd="@font.home"
+    tuiLink
+    type="button"
+    [style.--tui-font-icon]="'Material Symbols Outlined'"
+    [textContent]="'Link with font icon'"
+></button>
+`;export{i as default};
