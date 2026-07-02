ebc313be1565e40cecadb5c2d1057320a0f4bdf6
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-07-02 10:44:35 +0300

    chore: another approach

--- projects/addon-mobile/directives/dropdown-mobile/dropdown-mobile.directive.ts
+++ projects/addon-mobile/directives/dropdown-mobile/dropdown-mobile.directive.ts
@@ -1,7 +1,7 @@
 import {Directive, inject} from '@angular/core';
 import {WA_IS_MOBILE} from '@ng-web-apis/platform';
 import {tuiIsHTMLElement} from '@taiga-ui/cdk/utils/dom';
-import {tuiDataListOptionsProvider} from '@taiga-ui/core/components/data-list';
+import {TUI_DATA_LIST_SIZE} from '@taiga-ui/core/components/data-list';
 import {
     TUI_DROPDOWN_COMPONENT,
     TuiDropdownDirective,
@@ -19,7 +19,10 @@ import {TuiDropdownMobileComponent} from './dropdown-mobile.component';
                     ? TuiDropdownMobileComponent
                     : inject(TUI_DROPDOWN_COMPONENT, {skipSelf: true}),
         },
-        tuiDataListOptionsProvider(() => ({size: inject(WA_IS_MOBILE) ? 'l' : ''})),
+        {
+            provide: TUI_DATA_LIST_SIZE,
+            useFactory: () => (inject(WA_IS_MOBILE) ? 'l' : ''),
+        },
     ],
     host: {
         '[style.visibility]': 'dropdown.ref() ? "visible" : ""',
--- projects/core/components/data-list/data-list.tokens.ts
+++ projects/core/components/data-list/data-list.tokens.ts
@@ -5,7 +5,7 @@ import {
     type Signal,
     type Type,
 } from '@angular/core';
-import {tuiCreateOptions, tuiProvide} from '@taiga-ui/cdk/utils/di';
+import {tuiProvide} from '@taiga-ui/cdk/utils/di';
 import {type TuiSizeL, type TuiSizeS} from '@taiga-ui/core/types';
 
 export interface TuiDataListAccessor<T = unknown> {
@@ -25,15 +25,15 @@ export function tuiAsDataListHost<T>(host: Type<TuiDataListHost<T>>): Provider {
     return tuiProvide(TUI_DATA_LIST_HOST, host);
 }
 
-export const [TUI_DATA_LIST_OPTIONS, tuiDataListOptionsProvider] = tuiCreateOptions<{
-    size: TuiSizeL | TuiSizeS | '';
-}>({size: ''});
+export const TUI_DATA_LIST_SIZE = new InjectionToken<TuiSizeL | TuiSizeS | ''>(
+    ngDevMode ? 'TUI_DATA_LIST_SIZE' : '',
+);
 
 export function tuiInjectDataListSize(): TuiSizeL | TuiSizeS {
     const sizes = ['s', 'm', 'l'] as const;
 
     const size =
-        inject(TUI_DATA_LIST_OPTIONS).size ||
+        inject(TUI_DATA_LIST_SIZE, {optional: true}) ||
         inject(TUI_DATA_LIST_HOST, {optional: true})?.size;
 
     return size && sizes.includes(size) ? size : 'l';
