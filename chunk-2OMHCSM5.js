7980d86a026a9b531ff33ce8feb35593b02e83cd
splincode <omaxphp@yandex.ru>
2026-07-06 13:32:24 +0000

    Update snapshots/demo/next/main to output generated at fb81834

---
+++ chunk-2OMHCSM5.js
@@ -0,0 +1,9 @@
+import"./chunk-HU6DUUP4.js";var c=`.bars {
+    block-size: 6.25rem;
+    inline-size: 3.75rem;
+    background: linear-gradient(var(--tui-border-normal) 0 0) 0 66.6667% / 100% 1px no-repeat;
+
+    --tui-chart-categorical-00: var(--tui-background-accent-1);
+    --tui-chart-categorical-01: var(--tui-background-accent-1);
+}
+`;export{c as default};
