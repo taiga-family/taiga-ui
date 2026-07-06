2d60616278097e36f010492bf4d0b096d2be01e7
splincode <splincode@users.noreply.github.com>
2026-07-06 10:22:51 +0000

    deploy: 7b143e0466590fe7457ece5e828d828efb9f35d5

---
+++ next/chunk-Z3POIV6M.js
@@ -0,0 +1,6 @@
+import"./chunk-HU6DUUP4.js";var o=`import {Injectable} from '@angular/core';
+import {TuiPortalService} from '@taiga-ui/cdk';
+
+@Injectable({providedIn: 'root'})
+export class CustomPortalService extends TuiPortalService {}
+`;export{o as default};
