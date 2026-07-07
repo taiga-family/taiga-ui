4685d09828cefc4d4a794f37c04abd5bd72a3fde
waterplea <alexander@inkin.ru>
2026-07-07 13:41:22 +0300

    feat(core): `Tooltip` improve a11y

--- projects/core/portals/popup/popups.component.ts
+++ projects/core/portals/popup/popups.component.ts
@@ -13,5 +13,6 @@ import {TuiPopupService} from './popup.service';
     // eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
     changeDetection: ChangeDetectionStrategy.Default,
     providers: [tuiProvide(TuiPortalService, TuiPopupService)],
+    host: {'aria-live': 'polite'},
 })
 export class TuiPopups extends TuiPortals {}
