d89c15697e74351d23d3da1950ca3582c55542ba
waterplea <alexander@inkin.ru>
2026-07-09 17:35:38 +0300

    feat(core): `ScrollControls` automate use with virtual scroll, deprecate `Scrollable`

--- projects/demo/src/pages/components/select/examples/6/index.ts
+++ projects/demo/src/pages/components/select/examples/6/index.ts
@@ -3,7 +3,7 @@ import {Component, inject} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiDataList, TuiScrollable} from '@taiga-ui/core';
+import {TuiDataList, TuiScrollControls, TuiScrollRef} from '@taiga-ui/core';
 import {TUI_COUNTRIES, TuiChevron, TuiSelect} from '@taiga-ui/kit';
 
 @Component({
@@ -12,7 +12,8 @@ import {TUI_COUNTRIES, TuiChevron, TuiSelect} from '@taiga-ui/kit';
         ScrollingModule,
         TuiChevron,
         TuiDataList,
-        TuiScrollable,
+        TuiScrollControls,
+        TuiScrollRef,
         TuiSelect,
     ],
     templateUrl: './index.html',
