468848acce32053c235dafc6c75fac5ea43fcc1a
mdlufy <german.panov.2018@mail.ru>
2026-07-09 14:32:54 +0300

    chore(cdk): apply review to input-number migration
    - drop redundant TuiTextfield import from TuiInputNumberModule replacement
    - reuse shared LABEL_OUTSIDE_TODO_MESSAGE for the dynamic-label TODO

--- projects/cdk/schematics/ng-update/v5/steps/constants/identifiers-to-replace.ts
+++ projects/cdk/schematics/ng-update/v5/steps/constants/identifiers-to-replace.ts
@@ -52,16 +52,10 @@ export const IDENTIFIERS_TO_REPLACE: ReplacementIdentifierMulti[] = [
             name: 'TuiInputNumberModule',
             moduleSpecifier: '@taiga-ui/legacy',
         },
-        to: [
-            {
-                name: 'TuiInputNumber',
-                moduleSpecifier: '@taiga-ui/kit',
-            },
-            {
-                name: 'TuiTextfield',
-                moduleSpecifier: '@taiga-ui/core',
-            },
-        ],
+        to: {
+            name: 'TuiInputNumber',
+            moduleSpecifier: '@taiga-ui/kit',
+        },
     },
     {
         from: {
