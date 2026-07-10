2df965f5cddbbf24ccb12ecb879a62a55f1ed6b7
waterplea <alexander@inkin.ru>
2026-07-10 15:20:38 +0300

    chore: fix

--- projects/core/tokens/timeline-support.ts
+++ projects/core/tokens/timeline-support.ts
@@ -3,5 +3,5 @@ import {WA_CSS} from '@ng-web-apis/common';
 
 export const TUI_TIMELINE_SUPPORT = new InjectionToken<boolean>(
     ngDevMode ? 'TUI_TIMELINE_SUPPORT' : '',
-    {factory: () => inject(WA_CSS).supports('animation-timeline', '--name')},
+    {factory: () => inject(WA_CSS)?.supports('animation-timeline', '--name')},
 );
