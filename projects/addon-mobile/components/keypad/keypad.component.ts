164de0eeca3af83fd471b0f5e65158cc85687759
mdlufy <german.panov.2018@mail.ru>
2026-07-13 03:36:20 +0300

    refactor(addon-mobile): `Keypad` drop redundant value/aria-label fallbacks

--- projects/addon-mobile/components/keypad/keypad.component.ts
+++ projects/addon-mobile/components/keypad/keypad.component.ts
@@ -64,7 +64,7 @@ export class TuiKeypadComponent {
         const needsFallback =
             !this.templateMap().has(key) && this.content(key).startsWith('@tui.');
 
-        return this.ariaLabels()?.[key] || (needsFallback ? key : null);
+        return this.ariaLabels()[key] || (needsFallback ? key : null);
     }
 
     protected onKeyClick(key: TuiKeypadKey): void {
@@ -73,11 +73,11 @@ export class TuiKeypadComponent {
         }
 
         if (key === 'backspace') {
-            this.value.update((value) => (value || '').slice(0, -1));
+            this.value.update((value) => value.slice(0, -1));
         } else if (key === 'clear') {
             this.value.set('');
         } else if (key !== 'enter') {
-            this.value.update((value) => (value || '') + key);
+            this.value.update((value) => value + key);
         }
 
         this.key.emit(key);
