165f4525cca427a1ca747e6363e20442fd4a4760
jonata biondi <jonata.biondi@gmail.com>
2026-07-14 16:39:45 +0200

    refactor(core): simplify TuiClose directive and enhance TuiDialogCloseService

--- projects/core/directives/close/close.directive.ts
+++ projects/core/directives/close/close.directive.ts
@@ -1,12 +1,6 @@
-import {DOCUMENT} from '@angular/common';
 import {Directive, inject} from '@angular/core';
 import {outputFromObservable} from '@angular/core/rxjs-interop';
-import {WA_WINDOW} from '@ng-web-apis/common';
-import {tuiTypedFromEvent} from '@taiga-ui/cdk/observables';
-import {tuiGetActualTarget, tuiInjectElement} from '@taiga-ui/cdk/utils/dom';
 import {TuiDialogCloseService} from '@taiga-ui/core/portals/dialog';
-import {tuiGetViewportWidth} from '@taiga-ui/core/utils/dom';
-import {filter, merge, switchMap, take} from 'rxjs';
 
 /**
  * Emits on Esc/CloseWatcher and on clicks outside the host, tracked as
@@ -19,37 +13,5 @@ import {filter, merge, switchMap, take} from 'rxjs';
     providers: [TuiDialogCloseService],
 })
 export class TuiClose {
-    private readonly win = inject(WA_WINDOW);
-    private readonly doc = inject(DOCUMENT);
-    private readonly el = tuiInjectElement();
-
-    private readonly overlay$ = tuiTypedFromEvent(this.doc, 'mousedown').pipe(
-        filter(
-            (event) =>
-                tuiGetViewportWidth(this.win) - event.clientX > 17 &&
-                this.isOnOverlay(event),
-        ),
-        switchMap(() =>
-            tuiTypedFromEvent(this.doc, 'mouseup').pipe(
-                take(1),
-                filter((event) => this.isOnOverlay(event)),
-            ),
-        ),
-    );
-
-    public readonly tuiClose = outputFromObservable(
-        merge(inject(TuiDialogCloseService).pipe(), this.overlay$),
-    );
-    private isOnOverlay(event: MouseEvent): boolean {
-        const {clientX, clientY} = event;
-        const rect = this.el.getBoundingClientRect();
-
-        return (
-            tuiGetActualTarget(event) === this.el &&
-            (clientX < rect.left ||
-                clientX > rect.right ||
-                clientY < rect.top ||
-                clientY > rect.bottom)
-        );
-    }
+    public readonly tuiClose = outputFromObservable(inject(TuiDialogCloseService));
 }
