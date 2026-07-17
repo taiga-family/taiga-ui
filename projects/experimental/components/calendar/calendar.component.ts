6fae4a86f12835a2f05a798350fab8000d99c02c
waterplea <alexander@inkin.ru>
2026-07-17 17:41:06 +0300

    feat(experimental): `Calendar` add multi-month mode

--- projects/experimental/components/calendar/calendar.component.ts
+++ projects/experimental/components/calendar/calendar.component.ts
@@ -3,48 +3,31 @@ import {
     ChangeDetectionStrategy,
     Component,
     computed,
-    effect,
-    inject,
     input,
     model,
     signal,
     viewChild,
 } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {TuiDay, TuiDayRange, TuiMonth} from '@taiga-ui/cdk/date-time';
+import {type TuiDay, TuiDayRange, TuiMonth} from '@taiga-ui/cdk/date-time';
 import {TuiMapperPipe} from '@taiga-ui/cdk/pipes/mapper';
 import {type TuiBooleanHandler, type TuiContext} from '@taiga-ui/cdk/types';
 import {tuiProvide} from '@taiga-ui/cdk/utils/di';
-import {tuiArrayToggle} from '@taiga-ui/cdk/utils/miscellaneous';
 import {TuiButton, tuiButtonOptionsProvider} from '@taiga-ui/core/components/button';
 import {AbstractTuiCalendar} from '@taiga-ui/core/components/calendar';
 import {TuiCarousel, TuiCarouselComponent} from '@taiga-ui/core/components/carousel';
 import {TuiLink} from '@taiga-ui/core/components/link';
 import {tuiTextfieldOptionsProvider} from '@taiga-ui/core/components/textfield';
-import {TUI_DROPDOWN_HOST} from '@taiga-ui/core/portals/dropdown';
-import {
-    TUI_COMMON_ICONS,
-    TUI_MONTHS,
-    TUI_SPIN_TEXTS,
-    tuiAsAuxiliary,
-} from '@taiga-ui/core/tokens';
+import {tuiAsAuxiliary} from '@taiga-ui/core/tokens';
 import {TuiDataGrid} from '@taiga-ui/experimental/components/data-grid';
-import {
-    TUI_MONTH_OPTIONS,
-    TuiMonthComponent,
-} from '@taiga-ui/experimental/components/month';
+import {TuiMonthComponent} from '@taiga-ui/experimental/components/month';
 import {TuiChevron} from '@taiga-ui/kit/directives/chevron';
 import {TuiElasticContainer} from '@taiga-ui/layout/components/elastic-container';
 import {TuiSlides} from '@taiga-ui/layout/components/slides';
 import {type PolymorpheusContent} from '@taiga-ui/polymorpheus';
 
 import {TuiCalendarHeader} from './calendar-header.component';
-
-type DatePicker<T> = T extends 'single'
-    ? TuiDay
-    : T extends 'multi'
-      ? readonly TuiDay[]
-      : TuiDayRange;
+import {TuiDatePicker} from './date-picker';
 
 /**
  * @deprecated: work in progress, do not use!
@@ -69,22 +52,16 @@ type DatePicker<T> = T extends 'single'
     styleUrl: './calendar.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [
-        tuiAsAuxiliary(TuiCalendar),
-        tuiProvide(AbstractTuiCalendar, TuiCalendar),
+        tuiAsAuxiliary(TuiCalendarComponent),
+        tuiProvide(AbstractTuiCalendar, TuiCalendarComponent),
         tuiButtonOptionsProvider({size: 'xs', appearance: 'flat'}),
         tuiTextfieldOptionsProvider({size: signal('m'), cleaner: signal(false)}),
     ],
 })
-export class TuiCalendar<
-    T extends 'multi' | 'range' | 'single' = 'single',
-> extends AbstractTuiCalendar<DatePicker<T>> {
-    protected readonly options = inject(TUI_MONTH_OPTIONS);
+export class TuiCalendarComponent<
+    T extends 'multi' | 'range' | 'single',
+> extends TuiDatePicker<T> {
     protected readonly carousel = viewChild(TuiCarouselComponent);
-    protected readonly icons = inject(TUI_COMMON_ICONS);
-    protected readonly texts = inject(TUI_SPIN_TEXTS);
-    protected readonly i18n = inject(TUI_MONTHS);
-    protected readonly dropdown = inject(TUI_DROPDOWN_HOST, {optional: true});
-
     protected readonly content = computed<PolymorpheusContent<TuiContext<TuiMonth>>>(
         () => (c) => this.i18n()[c.$implicit.month],
     );
@@ -124,11 +101,6 @@ export class TuiCalendar<
         Math.floor((this.max().year - this.month().year + 6) / 12),
     );
 
-    protected readonly disabledDay = computed(
-        () => (day: TuiDay) =>
-            day < this.min() || day > this.max() || this.disabledItemHandler()(day),
-    );
-
     protected readonly disabledMonth = computed<TuiBooleanHandler<TuiMonth>>(
         () => (month) => month.monthBefore(this.min()) || month.monthAfter(this.max()),
     );
@@ -153,25 +125,10 @@ export class TuiCalendar<
         () => this.month().year * 12 + this.month().month,
     );
 
-    protected readonly sync = effect(() => {
-        const value = this.value();
-        const [day] = value instanceof TuiDayRange ? [value.from] : coerceArray(value);
-
-        if (!day || day.year < 9000) {
-            this.month.update(({month, year}) =>
-                (day || new TuiDay(year, month, 1)).dayLimit(this.min(), this.max()),
-            );
-        }
-    });
-
     public readonly view = model<'day' | 'month' | 'year'>('day');
-    public readonly mode = input<T>();
-    public readonly contentDay = input<PolymorpheusContent<TuiContext<TuiDay>>>();
     public readonly contentMonth = input<PolymorpheusContent<TuiContext<TuiMonth>>>();
     public readonly contentYear = input<PolymorpheusContent<TuiContext<number>>>();
-    public readonly dayType = input(this.options.dayType);
     public readonly showAdjacent = input(this.options.showAdjacent);
-    public readonly showWeek = input(this.options.showWeek);
 
     protected getMonth(index: number): TuiMonth {
         return new TuiMonth(Math.floor(index / 12), index % 12);
@@ -197,23 +154,5 @@ export class TuiCalendar<
     protected onMonth(month: TuiMonth): void {
         this.month.set(month);
         this.view.set('day');
-    }
-
-    protected onDay(day: TuiDay): void {
-        if (this.mode() === 'range') {
-            this.value.update((value): any =>
-                value instanceof TuiDayRange && value.from === value.to
-                    ? TuiDayRange.sort(value.to, day.append({}))
-                    : new TuiDayRange(day, day),
-            );
-        } else if (this.mode() === 'multi') {
-            this.value.update((value): any =>
-                Array.isArray(value)
-                    ? tuiArrayToggle(value, day, (a: TuiDay, b: TuiDay) => a.daySame(b))
-                    : [day],
-            );
-        } else {
-            this.value.set(day as any);
-        }
     }
 }
--- projects/experimental/components/calendar/index.ts
+++ projects/experimental/components/calendar/index.ts
@@ -1,2 +1,5 @@
+export * from './calendar';
 export * from './calendar.component';
 export * from './calendar-header.component';
+export * from './calendar-multi.component';
+export * from './date-picker';
