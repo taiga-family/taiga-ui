636bfd46086f2e25e5d83062b71b914162100127
Egor Borisevich <e.s.borisevich@tbank.ru>
2026-07-30 16:56:39 +0300

    feat(testing): implement locators package

---
+++ projects/testing/locators/range.locators.ts
@@ -0,0 +1,5 @@
+export const TUI_RANGE_LOCATORS = {
+    HOST: 'tui-range',
+    RANGE_START: 'input[automation-id="tui-range__left"]',
+    RANGE_END: 'input[automation-id="tui-range__right"]',
+};
---
+++ projects/testing/locators/textfield.locators.ts
@@ -0,0 +1,6 @@
+export const TUI_TEXTFIELD_LOCATORS = {
+    HOST: 'tui-textfield',
+    INPUT: '[tuiInput]',
+    CLEANER: '[tuiButtonX]',
+    CHIP: '[tuiInputChip]',
+};
