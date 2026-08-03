864f8f300c1f5e5f4e7ac4f96bc794108a47ba45
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2026-08-03 13:52:35 +0000

    Update snapshots/demo/next/main to output generated at 5c29506

---
+++ chunk-GCH4VWOV.js
@@ -0,0 +1,20 @@
+import"./chunk-HU6DUUP4.js";var n=`.content {
+    position: relative;
+    display: block;
+    inline-size: 18rem;
+    block-size: 30rem;
+    overflow: auto;
+    box-shadow: 0 0.25rem 1.25rem rgba(0, 0, 0, 0.1);
+    background: var(--tui-background-elevation-1);
+}
+
+.field {
+    margin: 1rem 1rem 0;
+}
+
+.action {
+    // shrink the single action to its content and pin it to the trailing edge
+    justify-self: end;
+    margin-inline: 1rem;
+}
+`;export{n as default};
