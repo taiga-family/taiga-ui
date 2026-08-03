488e87eb70ca188fefb2d8b10a656c379f9766d1
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2026-08-03 10:43:52 +0000

    Update snapshots/demo/next/main to output generated at 84940d4

---
+++ chunk-GO6CWZQJ.js
@@ -0,0 +1,4 @@
+import"./chunk-HU6DUUP4.js";var n=`:host {
+    inline-size: 100%;
+}
+`;export{n as default};
---
+++ chunk-XWL2UPQA.js
@@ -0,0 +1,17 @@
+import"./chunk-HU6DUUP4.js";var i=`<nav
+    tuiTabBar
+    [(activeItemIndex)]="activeItemIndex"
+>
+    @for (item of items; track item) {
+        <button
+            tuiTabBarItem
+            type="button"
+            [badge]="item.badge"
+            [icon]="item.icon"
+            (click)="onClick(item)"
+        >
+            {{ item.text }}
+        </button>
+    }
+</nav>
+`;export{i as default};
