d1dec5f46e22b685d0907665c0ed11f85061eed4
nsbarsukov <nikita.s.barsukov@gmail.com>
2026-08-03 12:05:38 +0000

    Update snapshots/demo/next/main to output generated at 9c3cbbb

---
+++ chunk-SO4QLFZ7.js
@@ -0,0 +1,21 @@
+import"./chunk-HU6DUUP4.js";var e=`tui-accordion {
+    .t-header {
+        display: flex;
+        align-items: stretch;
+
+        > [tuiAccordion] {
+            flex: 1;
+            justify-content: flex-start;
+            text-align: start;
+
+            &::after {
+                margin-inline-start: auto;
+            }
+        }
+    }
+
+    tui-expand {
+        box-shadow: none;
+    }
+}
+`;export{e as default};
