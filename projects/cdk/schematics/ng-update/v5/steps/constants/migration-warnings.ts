e1efe76b8c122b772d074785988e4b8ec2501b18
German Panov <german.panov.2018@mail.ru>
2026-08-03 00:05:50 -0700

    chore(schematics): warn on removed TuiValueContentContext/TuiAlertContext types in v5 (#11917) (#14662)

--- projects/cdk/schematics/ng-update/v5/steps/constants/migration-warnings.ts
+++ projects/cdk/schematics/ng-update/v5/steps/constants/migration-warnings.ts
@@ -160,6 +160,18 @@ export const MIGRATION_WARNINGS: MigrationWarning[] = [
             'tuiProvideMobileCalendar has been removed. Explicitly import TuiMobileCalendarDropdown from @taiga-ui/addon-mobile and add the directive to your input-date fields. See https://taiga-ui.dev/components/input-date#mobile',
     },
     {
+        name: 'TuiValueContentContext',
+        moduleSpecifier: '@taiga-ui/core',
+        message:
+            'TuiValueContentContext has been removed. Type custom value content with TuiContext<T> from @taiga-ui/cdk instead — the `active` flag is no longer part of the content context.',
+    },
+    {
+        name: 'TuiAlertContext',
+        moduleSpecifier: '@taiga-ui/core',
+        message:
+            'TuiAlertContext has been removed. Type the injected alert content context with TuiPortalContext<TuiNotificationOptions, O> from @taiga-ui/cdk (TuiPopoverContext -> TuiPortalContext, TuiAlertOptions -> TuiNotificationOptions).',
+    },
+    {
         name: 'TuiCarousel',
         moduleSpecifier: '@taiga-ui/legacy',
         message:
