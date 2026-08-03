4c8bbad7abceec79d5a58a252a976275c1deb426
mdlufy <zidakbew@gmail.com>
2026-08-03 15:39:57 +0300

    chore(demo): `FloatingContainer` move action styles onto button, add input to compact example

--- projects/demo/src/pages/components/floating-container/examples/7/index.ts
+++ projects/demo/src/pages/components/floating-container/examples/7/index.ts
@@ -1,15 +1,26 @@
 import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiButton, TuiCell, TuiTitle} from '@taiga-ui/core';
+import {TuiButton, TuiCell, TuiInput, TuiTitle} from '@taiga-ui/core';
 import {TuiAvatar} from '@taiga-ui/kit';
 import {TuiFloatingContainer} from '@taiga-ui/layout';
 
 @Component({
-    imports: [TuiAvatar, TuiButton, TuiCell, TuiFloatingContainer, TuiTitle],
+    imports: [
+        FormsModule,
+        TuiAvatar,
+        TuiButton,
+        TuiCell,
+        TuiFloatingContainer,
+        TuiInput,
+        TuiTitle,
+    ],
     templateUrl: './index.html',
     styleUrl: './index.less',
     encapsulation,
     changeDetection,
 })
-export default class Example {}
+export default class Example {
+    protected value = '';
+}
