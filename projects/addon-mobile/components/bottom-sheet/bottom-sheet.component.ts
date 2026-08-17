f4035541e2efdc05f30ae0cd44556c8854e7ce1e
jonata biondi <jonata.biondi@gmail.com>
2026-08-17 18:16:12 +0200

    fix: lint

--- projects/addon-mobile/components/bottom-sheet/bottom-sheet.component.ts
+++ projects/addon-mobile/components/bottom-sheet/bottom-sheet.component.ts
@@ -22,9 +22,9 @@ const OPTIONS = {
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [tuiHeaderOptionsProvider({size: 'h5'})],
     host: {
+        '[class._bar]': 'bar()',
         '[style.--t-initial]': 'stops()[0]',
         '[style.scroll-snap-type]': 'stops().length > 1 ? "y mandatory" : null',
-        '[class._bar]': 'bar()',
         '(resize)': 'onScroll()',
         '(scroll.zoneless)': 'onScroll()',
     },
