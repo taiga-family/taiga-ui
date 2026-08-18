9f33a6bc08bbbeecbfa92094b9a38822d3217107
Максим Иванов <omaxphp@yandex.ru>
2026-08-18 21:05:59 +0300

    fix(layout): align BlockDetails content in responsive dialog

--- projects/demo/src/pages/components/block-details/examples/3/index.ts
+++ projects/demo/src/pages/components/block-details/examples/3/index.ts
@@ -3,12 +3,20 @@ import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
 import {WA_IS_MOBILE} from '@ng-web-apis/platform';
 import {TuiAmountPipe} from '@taiga-ui/addon-commerce';
-import {TuiButton, TuiDialog, TuiTitle} from '@taiga-ui/core';
+import {TuiResponsiveDialog} from '@taiga-ui/addon-mobile';
+import {TuiButton, TuiTitle} from '@taiga-ui/core';
 import {TuiAvatar} from '@taiga-ui/kit';
 import {TuiBlockDetails} from '@taiga-ui/layout';
 
 @Component({
-    imports: [TuiAmountPipe, TuiAvatar, TuiBlockDetails, TuiButton, TuiDialog, TuiTitle],
+    imports: [
+        TuiAmountPipe,
+        TuiAvatar,
+        TuiBlockDetails,
+        TuiButton,
+        TuiResponsiveDialog,
+        TuiTitle,
+    ],
     templateUrl: './index.html',
     encapsulation,
     changeDetection,
