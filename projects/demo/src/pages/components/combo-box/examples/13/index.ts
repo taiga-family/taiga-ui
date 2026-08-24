8a6a600e4fb5a2c82ef56cdddf3fbfa4bbef7eeb
Alex Inkin <alexander@inkin.ru>
2026-08-24 13:46:07 +0400

    feat(kit): `ComboBox` abstract value handling so it can be overridden (#14795)

---
+++ projects/demo/src/pages/components/combo-box/examples/13/index.ts
@@ -0,0 +1,27 @@
+import {Component, inject} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiFilterByInputPipe, TuiNotificationService} from '@taiga-ui/core';
+import {TuiChevron, TuiComboBox, TuiDataListWrapper} from '@taiga-ui/kit';
+
+import {CustomComboBox} from './custom';
+
+@Component({
+    imports: [
+        CustomComboBox,
+        FormsModule,
+        TuiChevron,
+        TuiComboBox,
+        TuiDataListWrapper,
+        TuiFilterByInputPipe,
+    ],
+    templateUrl: './index.html',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected readonly alerts = inject(TuiNotificationService);
+    protected readonly items = inject<string[]>('Pythons' as any);
+    protected value: string | null = null;
+}
