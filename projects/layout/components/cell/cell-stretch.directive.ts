396bb362df04a57924b4815d847487781bc19dcb
Yan Merkushin <yan.merkushin@icloud.com>
2026-08-26 12:08:49 +0300

    chore(layout): `CellStretch` add dynamic inline padding (#10967)

--- projects/layout/components/cell/cell-stretch.directive.ts
+++ projects/layout/components/cell/cell-stretch.directive.ts
@@ -1,11 +1,8 @@
-import {Directive, inject} from '@angular/core';
-import {TUI_IS_MOBILE} from '@taiga-ui/cdk/tokens';
+import {Directive} from '@angular/core';
 
 @Directive({
     standalone: true,
     selector: '[tuiCell][tuiCellStretch]',
-    host: {'[style.border-radius]': 'isMobile ? 0 : null'},
+    host: {tuiCellStretch: ''},
 })
-export class TuiCellStretch {
-    protected readonly isMobile = inject(TUI_IS_MOBILE);
-}
+export class TuiCellStretch {}
