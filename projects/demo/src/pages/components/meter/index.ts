a33d076cb26dd6af2e39cf00424bbe2551a29bb2
waterplea <alexander@inkin.ru>
2026-08-26 11:33:16 +0300

    chore: fix

--- projects/demo/src/pages/components/meter/index.ts
+++ projects/demo/src/pages/components/meter/index.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
+import {TuiMeter} from '@taiga-ui/kit';
 
 @Component({
-    imports: [TuiDemo],
+    imports: [TuiDemo, TuiMeter],
     templateUrl: './index.html',
     changeDetection,
 })
-export default class Example {}
+export default class Example {
+    protected readonly min = signal('0');
+    protected readonly max = signal('1');
+    protected readonly low = signal('0');
+    protected readonly high = signal('1');
+    protected readonly optimum = signal('0.5');
+    protected readonly value = signal('0');
+}
