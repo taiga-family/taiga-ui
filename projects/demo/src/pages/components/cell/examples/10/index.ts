b0d03e4af0abfb3d966feafad08013b42e7ddab4
Yan Merkushin <yan.merkushin@icloud.com>
2026-08-28 14:22:21 +0300

    chore(layout): `CellStretch` add dynamic inline padding (#10976)

--- projects/demo/src/pages/components/cell/examples/10/index.ts
+++ projects/demo/src/pages/components/cell/examples/10/index.ts
@@ -4,18 +4,21 @@ import {encapsulation} from '@demo/emulate/encapsulation';
 import {TuiSheetDialog, type TuiSheetDialogOptions} from '@taiga-ui/addon-mobile';
 import {TuiButton, TuiCell, TuiCellStretch, TuiTitle} from '@taiga-ui/core';
 import {TuiAvatar, TuiBadgeNotification} from '@taiga-ui/kit';
+import {TuiCardLarge} from '@taiga-ui/layout';
 
 @Component({
     imports: [
         TuiAvatar,
         TuiBadgeNotification,
         TuiButton,
+        TuiCardLarge,
         TuiCell,
         TuiCellStretch,
         TuiSheetDialog,
         TuiTitle,
     ],
     templateUrl: './index.html',
+    styleUrl: './index.less',
     encapsulation,
     changeDetection,
 })
@@ -26,4 +29,22 @@ export default class Example {
         label: 'Navigation',
         closable: true,
     };
+
+    protected readonly items = [
+        {
+            icon: '@tui.eye',
+            title: 'Show more',
+            subtitle: 'Ctrl + Shift + M',
+        },
+        {
+            icon: '@tui.mail',
+            title: 'Send message',
+            subtitle: 'Keep it short',
+        },
+        {
+            icon: '@tui.lock',
+            title: 'Access',
+            subtitle: 'Block your account',
+        },
+    ];
 }
