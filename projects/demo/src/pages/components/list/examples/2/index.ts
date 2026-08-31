7a2af1485dfc69890efa70142a779ed52f7bd9e0
Alex Inkin <alexander@inkin.ru>
2026-08-31 15:56:26 +0400

    feat(layout): `List` add definition list support (#14867)
    Co-authored-by: taiga-family-bot <41898282+github-actions[bot]@users.noreply.github.com>

--- projects/demo/src/pages/components/list/examples/2/index.ts
+++ projects/demo/src/pages/components/list/examples/2/index.ts
@@ -1,12 +1,11 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiList} from '@taiga-ui/layout';
+import {TuiHeader, TuiList} from '@taiga-ui/layout';
 
 @Component({
-    imports: [TuiList],
+    imports: [TuiHeader, TuiList],
     templateUrl: './index.html',
-    styleUrl: '../1/index.less',
     encapsulation,
     changeDetection,
 })
--- projects/demo/src/pages/components/list/examples/6/index.ts
+++ projects/demo/src/pages/components/list/examples/6/index.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
-import {TuiTitle} from '@taiga-ui/core';
-import {TuiHeader, TuiList} from '@taiga-ui/layout';
+import {TuiList} from '@taiga-ui/layout';
 
 @Component({
-    imports: [TuiHeader, TuiList, TuiTitle],
+    imports: [TuiList],
     templateUrl: './index.html',
     encapsulation,
     changeDetection,
