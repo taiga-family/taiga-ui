a3083d8d4d91bf3cd7f7a1669d238bf95e41a8fc
Yan Merkushin <yan.merkushin@icloud.com>
2026-09-14 13:03:59 +0300

    feat(kit): `Tooltip` add tuiHintVisible to tuiHintDirective

---
+++ projects/demo/src/modules/components/tooltip/examples/5/index.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiIcon} from '@taiga-ui/core';
+import {TuiTooltip} from '@taiga-ui/kit';
+
+@Component({
+    standalone: true,
+    imports: [TuiIcon, TuiTooltip],
+    templateUrl: './index.html',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected visible = false;
+}
--- projects/demo/src/modules/components/tooltip/index.ts
+++ projects/demo/src/modules/components/tooltip/index.ts
@@ -1,11 +1,23 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
+import {tuiProvide} from '@taiga-ui/cdk';
+import {TuiIcon, type TuiSizeS} from '@taiga-ui/core';
+import {TuiTooltip} from '@taiga-ui/kit';
+
+import {ABSTRACT_PROPS_ACCESSOR} from '../../components/abstract/abstract-props-accessor';
+import {AbstractExampleTuiHint} from '../../components/abstract/hint';
+import {InheritedDocumentation} from '../../components/abstract/inherited-documentation';
 
 @Component({
     standalone: true,
-    imports: [TuiDemo],
+    imports: [InheritedDocumentation, TuiDemo, TuiIcon, TuiTooltip],
     templateUrl: './index.html',
     changeDetection,
+    providers: [tuiProvide(ABSTRACT_PROPS_ACCESSOR, PageComponent)],
 })
-export default class Page {}
+export default class PageComponent extends AbstractExampleTuiHint {
+    protected readonly sizeVariants: readonly TuiSizeS[] = ['s', 'm'];
+    protected size: TuiSizeS = 'm';
+    protected content = 'Tooltip text';
+}
