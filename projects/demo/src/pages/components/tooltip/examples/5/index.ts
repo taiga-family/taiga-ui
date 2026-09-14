bf39c5a9befe8c32682fed74d69c3d9d6056681b
Yan Merkushin <yan.merkushin@icloud.com>
2026-09-14 13:07:06 +0300

    feat(kit): `Tooltip` add tuiHintVisible to tuiHintDirective

---
+++ projects/demo/src/pages/components/tooltip/examples/5/index.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {changeDetection} from '@demo/emulate/change-detection';
+import {encapsulation} from '@demo/emulate/encapsulation';
+import {TuiIcon} from '@taiga-ui/core';
+import {TuiTooltip} from '@taiga-ui/kit';
+
+@Component({
+    imports: [TuiIcon, TuiTooltip],
+    templateUrl: './index.html',
+    encapsulation,
+    changeDetection,
+})
+export default class Example {
+    protected visible = false;
+}
--- projects/demo/src/pages/components/tooltip/index.ts
+++ projects/demo/src/pages/components/tooltip/index.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
+import {TuiDocHint} from '@demo/components/hint';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
+import {TuiIcon, type TuiSizeS} from '@taiga-ui/core';
+import {TuiTooltip} from '@taiga-ui/kit';
 
 @Component({
-    imports: [TuiDemo],
+    imports: [TuiDemo, TuiDocHint, TuiIcon, TuiTooltip],
     templateUrl: './index.html',
     changeDetection,
 })
@@ -13,5 +16,10 @@ export default class Page {
         'Custom host',
         'Repeating template',
         'Options',
+        'Visibility',
     ];
+
+    protected readonly sizeVariants: readonly TuiSizeS[] = ['s', 'm'];
+    protected size: TuiSizeS = 'm';
+    protected content = 'Tooltip text';
 }
