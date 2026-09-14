c593dc228ba521125218874732d02f0f463064ab
Yan Merkushin <yan.merkushin@icloud.com>
2026-09-14 14:32:50 +0300

    feat(kit): `Tooltip` add tuiHintVisible to tuiHintDirective (#14984)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/demo/src/modules/components/tooltip/index.ts
+++ projects/demo/src/modules/components/tooltip/index.ts
@@ -1,11 +1,23 @@
 import {Component} from '@angular/core';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {TuiDemo} from '@demo/utils';
+import {tuiProvide} from '@taiga-ui/cdk';
+import {TuiIcon, type TuiSizeS} from '@taiga-ui/core';
+import {TuiTooltip} from '@taiga-ui/kit';
+
+import {ABSTRACT_PROPS_ACCESSOR} from '../abstract/abstract-props-accessor';
+import {AbstractExampleTuiHint} from '../abstract/hint';
+import {InheritedDocumentation} from '../abstract/inherited-documentation';
 
 @Component({
     standalone: true,
-    imports: [TuiDemo],
+    imports: [InheritedDocumentation, TuiDemo, TuiIcon, TuiTooltip],
     templateUrl: './index.html',
     changeDetection,
+    providers: [tuiProvide(ABSTRACT_PROPS_ACCESSOR, PageComponent)],
 })
-export default class Page {}
+export default class PageComponent extends AbstractExampleTuiHint {
+    protected readonly sizeVariants: readonly TuiSizeS[] = ['s', 'm'];
+    protected size: TuiSizeS = 'm';
+    protected content = 'Tooltip text';
+}
--- projects/kit/directives/tooltip/tooltip.directive.ts
+++ projects/kit/directives/tooltip/tooltip.directive.ts
@@ -63,6 +63,7 @@ class TuiTooltipStyles {}
         {
             directive: TuiHintDirective,
             inputs: ['tuiHint: tuiTooltip', 'tuiHintAppearance', 'tuiHintContext'],
+            outputs: ['tuiHintVisible'],
         },
     ],
     host: {
