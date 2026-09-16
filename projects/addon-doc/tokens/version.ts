f88c1c4088ddcc1a58bacd2d2320de952e917324
mdlufy <zidakbew@gmail.com>
2026-09-16 10:30:48 +0300

    chore(addon-doc): generalize TUI_DOC_VERSION doc comment

--- projects/addon-doc/tokens/version.ts
+++ projects/addon-doc/tokens/version.ts
@@ -1,7 +1,7 @@
 import {InjectionToken} from '@angular/core';
 
 /**
- * Current library version used to derive "New"/"Updated" navigation badges.
+ * Current library version.
  */
 export const TUI_DOC_VERSION = new InjectionToken(ngDevMode ? 'TUI_DOC_VERSION' : '', {
     factory: () => '',
