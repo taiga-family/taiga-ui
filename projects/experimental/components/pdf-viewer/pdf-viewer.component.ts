874f8d3d11c9ae1631ed72f3711ec808b967a25d
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2026-09-21 14:47:12 +0300

    fix(experimental): `PdfViewer` fix style specificity (#15022)
    Co-authored-by: taiga-family-bot <taiga-family-bot@users.noreply.github.com>

--- projects/experimental/components/pdf-viewer/pdf-viewer.component.ts
+++ projects/experimental/components/pdf-viewer/pdf-viewer.component.ts
@@ -16,6 +16,7 @@ import {TUI_CLOSE_WORD, TUI_COMMON_ICONS} from '@taiga-ui/core/tokens';
 import {TuiAppBar} from '@taiga-ui/layout/components/app-bar';
 import {injectContext} from '@taiga-ui/polymorpheus';
 import {map} from 'rxjs';
+import {TUI_VERSION} from '@taiga-ui/cdk/constants';
 
 @Component({
     standalone: true,
@@ -26,12 +27,13 @@ import {map} from 'rxjs';
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [
-        tuiButtonOptionsProvider(() => ({
-            appearance: inject(TUI_IS_MOBILE) ? 'action' : 'glass',
-            size: 's',
+        tuiButtonOptionsProvider((mobile = inject(TUI_IS_MOBILE)) => ({
+            appearance: mobile ? 'action' : 'glass',
+            size: mobile ? 'l' : 's',
         })),
     ],
     host: {
+        tuiPdfViewerV: TUI_VERSION,
         '[attr.tuiTheme]': 'isMobileRes() ? "" : "dark"',
     },
 })
